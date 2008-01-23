9be9a19fa6d4e25194939477eb5db9e3a3cc8b40
0xd34df00d <0xd34df00d@gmail.com>
2008-01-23 09:55:39 +0000

    Because Windows' runtime linker really sucks, we should put the code for settings manager in each plugin separately.

--- src/plugins/torrent/xmlsettingsmanager.h
+++ src/plugins/torrent/xmlsettingsmanager.h
@@ -1,16 +1,26 @@
 #ifndef XMLSETTINGSMANAGER_H
 #define XMLSETTINGSMANAGER_H
+#include <QMap>
+#include <QPair>
 #include <QObject>
 #include <interfaces/interfaces.h>
-#include <plugininterface/basicsettingsmanager.h>
 
-class XmlSettingsManager : public BasicSettingsManager
+class QSettings;
+
+class XmlSettingsManager : public QObject
 {
     Q_OBJECT
+
+    QMap<QByteArray, QPair<QObject*, QByteArray> > Properties2Object_;
+    bool Initializing_;
 public:
     XmlSettingsManager ();
     static XmlSettingsManager* Instance ();
+    void Release ();
+    void RegisterObject (const QByteArray& propName, QObject* object, const QByteArray& funcName);
+    QVariant Property (const QString& propName, const QVariant& def);
 protected:
+    virtual bool event (QEvent*);
     virtual QSettings* BeginSettings ();
     virtual void EndSettings (QSettings*);
 };
