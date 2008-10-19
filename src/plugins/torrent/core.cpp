f66d3771d1938ced99a7839d06e5d5aef5338ef1
0xd34df00d <0xd34df00d@gmail.com>
2008-10-19 17:46:16 +0000

    * BitTorrent: Compatibility with 2836 libtorrent.

--- src/plugins/torrent/core.cpp
+++ src/plugins/torrent/core.cpp
@@ -1876,7 +1876,7 @@ void Core::setGeneralSettings ()
 		property ("InactivityTimeout").toInt ();
     settings.unchoke_interval = XmlSettingsManager::Instance ()->
 		property ("UnchokeInterval").toInt ();
-    settings.optimistic_unchoke_multiplier = XmlSettingsManager::Instance ()->
+    settings.optimistic_unchoke_interval = XmlSettingsManager::Instance ()->
 		property ("OptimisticUnchokeMultiplier").toInt ();
     try
     {
