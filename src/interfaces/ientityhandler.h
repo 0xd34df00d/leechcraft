cdace00c0a06dc7ad4ad558150e5ab2a58085968
0xd34df00d <0xd34df00d@gmail.com>
2009-02-19 16:38:49 +0300

    + Interfaces: IEntityHandler for entity handlers.

---
+++ src/interfaces/ientityhandler.h
@@ -0,0 +1,21 @@
+#ifndef IENTITYHANDLER_H
+#define IENTITYHANDLER_H
+#include <QByteArray>
+#include <QtPlugin>
+#include "structures.h"
+
+class IEntityHandler
+{
+public:
+	virtual bool CouldHandle (const QByteArray&,
+			LeechCraft::TaskParameters) const = 0;
+	virtual void Handle (const QByteArray&,
+			LeechCraft::TaskParameters) const = 0;
+
+	virtual ~IEntityHandler () {}
+};
+
+Q_DECLARE_INTERFACE (IEntityHandler, "org.Deviant.LeechCraft.IEntityHandler/1.0");
+
+#endif
+
