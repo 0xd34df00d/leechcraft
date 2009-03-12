ed3e62fd7319e6a38d9c00721bb5ba99f062ddd9
0xd34df00d <0xd34df00d@gmail.com>
2009-03-12 16:10:23 +0300

    - BitTorrent: Removed debugging output.

--- src/plugins/torrent/core.cpp
+++ src/plugins/torrent/core.cpp
@@ -241,10 +241,7 @@ bool Core::CouldDownload (const LeechCraft::DownloadEntity& e) const
 	else
 	{
 		if (QUrl (str).scheme () == "file")
-		{
 			str = QUrl (str).toLocalFile ();
-			qDebug () << Q_FUNC_INFO << str;
-		}
 		QFile file (str);
 		if (file.exists () &&
 				file.open (QIODevice::ReadOnly))
