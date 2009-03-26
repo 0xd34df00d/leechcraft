a72895a2cb5242553673ae236bb23ebd314a9cc1
0xd34df00d <0xd34df00d@gmail.com>
2009-03-26 12:43:55 +0300

    * Poshuku: Fixed segfault on delete in empty favorites view.

--- src/plugins/poshuku/favoritestreeview.cpp
+++ src/plugins/poshuku/favoritestreeview.cpp
@@ -12,7 +12,8 @@ FavoritesTreeView::~FavoritesTreeView ()
 
 void FavoritesTreeView::keyPressEvent (QKeyEvent *e)
 {
-	if (e->key () == Qt::Key_Delete)
+	if (e->key () == Qt::Key_Delete &&
+			selectionModel ()->currentIndex ().isValid ())
 		emit deleteSelected (selectionModel ()->currentIndex ());
 
 	QTreeView::keyPressEvent (e);
