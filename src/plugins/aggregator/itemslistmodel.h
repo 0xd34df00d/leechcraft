b0d08d5de498cf4d46af8fc1dcf81af578220e37
0xd34df00d <0xd34df00d@gmail.com>
2009-04-02 02:10:29 +0400

    * Aggregator: Forgot two files in previous commit.

---
+++ src/plugins/aggregator/itemslistmodel.h
@@ -0,0 +1,41 @@
+#ifndef ITEMSLISTMODEL_H
+#define ITEMSLISTMODEL_H
+#include <QAbstractItemModel>
+#include <QStringList>
+#include <QPair>
+#include "item.h"
+
+class ItemsListModel : public QAbstractItemModel
+{
+	Q_OBJECT
+
+    QStringList ItemHeaders_;
+	items_shorts_t CurrentItems_;
+	int CurrentRow_;
+	// First is ParentURL_ and second is Title_
+	QPair<QString, QString> CurrentChannelHash_;
+public:
+	ItemsListModel (QObject* = 0);
+
+	int GetSelectedRow () const;
+	const QPair<QString, QString>& GetHash () const;
+	void SetHash (const QPair<QString, QString>&);
+	void Selected (const QModelIndex&);
+	void MarkItemAsUnread (const QModelIndex&);
+	const ItemShort& GetItem (const QModelIndex&) const;
+	bool IsItemRead (int) const;
+	QStringList GetCategories (int) const;
+	void Reset (const QPair<QString, QString>&);
+	void ItemDataUpdated (Item_ptr);
+
+	int columnCount (const QModelIndex& = QModelIndex ()) const;
+	QVariant data (const QModelIndex&, int = Qt::DisplayRole) const;
+	Qt::ItemFlags flags (const QModelIndex&) const;
+	QVariant headerData (int, Qt::Orientation, int = Qt::DisplayRole) const;
+	QModelIndex index (int, int, const QModelIndex& = QModelIndex()) const;
+	QModelIndex parent (const QModelIndex&) const;
+	int rowCount (const QModelIndex& = QModelIndex ()) const;
+};
+
+#endif
+
