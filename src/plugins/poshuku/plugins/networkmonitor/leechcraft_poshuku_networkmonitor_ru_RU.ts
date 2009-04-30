1a622dd3d7849aa8ae07db9a1a5196f3d63c559d
0xd34df00d <0xd34df00d@gmail.com>
2009-04-30 10:48:18 +0400

    * Poshuku: NetworkMonitor: Updated translations.

--- src/plugins/poshuku/plugins/networkmonitor/leechcraft_poshuku_networkmonitor_ru_RU.ts
+++ src/plugins/poshuku/plugins/networkmonitor/leechcraft_poshuku_networkmonitor_ru_RU.ts
@@ -1,27 +1,86 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru_RU">
 <context>
-    <name>Dialog</name>
+    <name>HeaderModel</name>
+    <message>
+        <location filename="headermodel.cpp" line="8"/>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="headermodel.cpp" line="9"/>
+        <source>Value</source>
+        <translation>Значение</translation>
+    </message>
+</context>
+<context>
+    <name>NetworkMonitor</name>
     <message>
         <location filename="networkmonitor.ui" line="14"/>
         <source>Network Monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>Сетевой монитор</translation>
     </message>
     <message>
-        <location filename="networkmonitor.ui" line="22"/>
+        <location filename="networkmonitor.ui" line="20"/>
+        <source>Clear finished items</source>
+        <translation>Удалять завершившиеся запросы</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="32"/>
         <source>Requests</source>
-        <translation type="unfinished"></translation>
+        <translation>Запросы</translation>
     </message>
     <message>
-        <location filename="networkmonitor.ui" line="38"/>
+        <location filename="networkmonitor.ui" line="48"/>
         <source>Request headers</source>
-        <translation type="unfinished"></translation>
+        <translation>Заголовки запроса</translation>
     </message>
     <message>
-        <location filename="networkmonitor.ui" line="52"/>
+        <location filename="networkmonitor.ui" line="62"/>
         <source>Response headers</source>
-        <translation type="unfinished"></translation>
+        <translation>Заголовки ответа</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="networkmonitor.cpp" line="43"/>
+        <source>Monitors network activity.</source>
+        <translation>Следит за сетевой активностью.</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.cpp" line="78"/>
+        <source>Network monitor...</source>
+        <translation>Сетевой монитор...</translation>
+    </message>
+</context>
+<context>
+    <name>RequestModel</name>
+    <message>
+        <location filename="requestmodel.cpp" line="17"/>
+        <source>Date started</source>
+        <translation>Дата начала</translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="18"/>
+        <source>Date finished</source>
+        <translation>Дата завершения</translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="19"/>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="20"/>
+        <source>Host</source>
+        <translation>Хост</translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="60"/>
+        <source>In progress</source>
+        <translation>Выполняется</translation>
     </message>
 </context>
 </TS>
--- src/plugins/poshuku/plugins/networkmonitor/leechcraft_poshuku_networkmonitor_uk_UA.ts
+++ src/plugins/poshuku/plugins/networkmonitor/leechcraft_poshuku_networkmonitor_uk_UA.ts
@@ -2,25 +2,84 @@
 <!DOCTYPE TS>
 <TS version="2.0">
 <context>
-    <name>Dialog</name>
+    <name>HeaderModel</name>
+    <message>
+        <location filename="headermodel.cpp" line="8"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="headermodel.cpp" line="9"/>
+        <source>Value</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NetworkMonitor</name>
     <message>
         <location filename="networkmonitor.ui" line="14"/>
         <source>Network Monitor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="networkmonitor.ui" line="22"/>
+        <location filename="networkmonitor.ui" line="20"/>
+        <source>Clear finished items</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="32"/>
         <source>Requests</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="networkmonitor.ui" line="38"/>
+        <location filename="networkmonitor.ui" line="48"/>
         <source>Request headers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="networkmonitor.ui" line="52"/>
+        <location filename="networkmonitor.ui" line="62"/>
         <source>Response headers</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="networkmonitor.cpp" line="43"/>
+        <source>Monitors network activity.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.cpp" line="78"/>
+        <source>Network monitor...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RequestModel</name>
+    <message>
+        <location filename="requestmodel.cpp" line="17"/>
+        <source>Date started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="18"/>
+        <source>Date finished</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="19"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="20"/>
+        <source>Host</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="60"/>
+        <source>In progress</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
