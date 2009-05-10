9184bd84a767ff06f32f7c0de0bf9f31b4252d55
0xd34df00d <0xd34df00d@gmail.com>
2009-05-10 15:22:48 +0400

    * Core: compatibility with 4.5.1 network cache quirks.

--- src/networkdiskcache.cpp
+++ src/networkdiskcache.cpp
@@ -1,4 +1,6 @@
 #include "networkdiskcache.h"
+#include <QtDebug>
+#include <QDateTime>
 
 NetworkDiskCache::NetworkDiskCache (QObject *parent)
 : QNetworkDiskCache (parent)
@@ -7,7 +9,11 @@ NetworkDiskCache::NetworkDiskCache (QObject *parent)
 
 QIODevice* NetworkDiskCache::prepare (const QNetworkCacheMetaData& metadata)
 {
-	if (metadata.url ().scheme () == "https")
+	// Some quirks for disk cache, for example, don't cache https://
+	// stuff as well as scripts.
+	if (QString ("4.5.1") == qVersion () &&
+			(metadata.url ().scheme () == "https" ||
+			 metadata.url ().path ().endsWith ("js")))
 		return 0;
 	else
 		return QNetworkDiskCache::prepare (metadata);
