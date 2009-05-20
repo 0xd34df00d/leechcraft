d11d635e2e7dfa0c3fa86760a9ed69c3e19e0264
0xd34df00d <0xd34df00d@gmail.com>
2009-05-20 19:38:11 +0400

    * Core: Fixed QAction child handling.

--- src/childactioneventfilter.cpp
+++ src/childactioneventfilter.cpp
@@ -2,6 +2,7 @@
 #include <QEvent>
 #include <QAction>
 #include <QTabWidget>
+#include <QActionEvent>
 #include <QtDebug>
 #include "skinengine.h"
 
@@ -18,15 +19,22 @@ ChildActionEventFilter::~ChildActionEventFilter ()
 
 bool ChildActionEventFilter::eventFilter (QObject *obj, QEvent *event)
 {
-	if (event->type () == QEvent::ChildAdded)
+	if (event->type () == QEvent::ChildAdded ||
+			event->type () == QEvent::ChildPolished)
 	{
 		QChildEvent *e = dynamic_cast<QChildEvent*> (event);
 		e->child ()->installEventFilter (this);
 
-		SkinEngine::Instance ()
-			.UpdateIconSet (e->child ()->findChildren<QAction*> ());
-		SkinEngine::Instance ()
-			.UpdateIconSet (e->child ()->findChildren<QTabWidget*> ());
+		QAction *act = dynamic_cast<QAction*> (e->child ());
+		if (act)
+			SkinEngine::Instance ().UpdateIconSet (QList<QAction*> () << act);
+		else
+		{
+			SkinEngine::Instance ()
+				.UpdateIconSet (e->child ()->findChildren<QAction*> ());
+			SkinEngine::Instance ()
+				.UpdateIconSet (e->child ()->findChildren<QTabWidget*> ());
+		}
 		return false;
 	}
 	else
