d9ebe00dc9b59f956f8bc88b9134ee42af9a0c6b
0xd34df00d <0xd34df00d@gmail.com>
2009-05-26 18:20:12 +0400

    + Core: Show initialization progress.

--- src/pluginmanager.cpp
+++ src/pluginmanager.cpp
@@ -617,6 +617,8 @@ bool LeechCraft::PluginManager::InitializeSingle (LeechCraft::PluginManager::Dep
 	try
 	{
 		IInfo *ii = qobject_cast<IInfo*> (item->Plugin_);
+		QString name = ii->GetName ();
+		emit loadProgress (QString ("Initializing %1...").arg (name));
 		ii->Init (ICoreProxy_ptr (new CoreProxy ()));
 		item->Initialized_ = true;
 	}
