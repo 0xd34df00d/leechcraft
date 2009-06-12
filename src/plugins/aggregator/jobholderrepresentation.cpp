553c56ab2f562b5796a1dea57a9d1badbe702235
0xd34df00d <0xd34df00d@gmail.com>
2009-06-12 21:15:46 +0400

    Aggregator: Option to not show unread channels in the summary.

--- src/plugins/aggregator/jobholderrepresentation.cpp
+++ src/plugins/aggregator/jobholderrepresentation.cpp
@@ -2,11 +2,15 @@
 #include <QTimer>
 #include <QtDebug>
 #include "aggregator.h"
+#include "xmlsettingsmanager.h"
 
 JobHolderRepresentation::JobHolderRepresentation (QObject *parent)
 : QSortFilterProxyModel (parent)
 {
 	setDynamicSortFilter (true);
+	XmlSettingsManager::Instance ()->RegisterObject ("ShowInDashboard",
+			this,
+			"invalidate");
 }
 
 void JobHolderRepresentation::SelectionChanged (const QModelIndex& index)
@@ -21,10 +25,13 @@ void JobHolderRepresentation::SelectionChanged (const QModelIndex& index)
 bool JobHolderRepresentation::filterAcceptsRow (int row,
 		const QModelIndex&) const
 {
-	// The row won't show up anyway in the job list if it was empty, so
-	// we can just check if it has unread items or selected. Later means
-	// that user's just clicked last unread item there.
-	return sourceModel ()->index (row, 1).data ().toInt () ||
-		(Selected_.isValid () ? row == Selected_.row () : false);
+		// The row won't show up anyway in the job list if it was empty, so
+		// we can just check if it has unread items or selected. Later means
+		// that user's just clicked last unread item there.
+	if (XmlSettingsManager::Instance ()->property ("ShowInDashboard").toBool ())
+		return sourceModel ()->index (row, 1).data ().toInt () ||
+			(Selected_.isValid () ? row == Selected_.row () : false);
+	else
+		return false;
 }
 
