800dfa057f559418ac2877b357e72c618b720dcb
0xd34df00d <0xd34df00d@gmail.com>
2009-06-19 12:14:37 +0400

    Core: Italian translation by Dj SyNko. Thanks.

---
+++ src/leechcraft_it.ts
@@ -0,0 +1,560 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>AuthenticationDialog</name>
+    <message>
+        <source>Authentication required</source>
+        <translation type="unfinished">Autenticazione richiesta</translation>
+    </message>
+    <message>
+        <source>Ooops, I forgot to call the corrent constructor params.</source>
+        <translation type="unfinished">Ooops, ho dimenticato di chiamare il corrente parametro costruttore.</translation>
+    </message>
+    <message>
+        <source>Login:</source>
+        <translation type="unfinished">Accesso:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation type="unfinished">Parola chiave:</translation>
+    </message>
+    <message>
+        <source>Save credentials</source>
+        <translation type="unfinished">Salva le credenziali</translation>
+    </message>
+</context>
+<context>
+    <name>CommonJobAdder</name>
+    <message>
+        <source>Add task</source>
+        <translation type="unfinished">Aggiungi task</translation>
+    </message>
+    <message>
+        <source>Entity:</source>
+        <translation type="unfinished">Entità</translation>
+    </message>
+    <message>
+        <source>Browse...</source>
+        <translation type="unfinished">Sfoglia...</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation type="unfinished">Incolla</translation>
+    </message>
+    <message>
+        <source>Location:</source>
+        <translation type="unfinished">Locazione:</translation>
+    </message>
+</context>
+<context>
+    <name>HandlerChoiceDialog</name>
+    <message>
+        <source>Choose entity handler</source>
+        <translation type="unfinished">Scegliere il l&apos;apritore della entità</translation>
+    </message>
+    <message>
+        <source>LeechCraft has detected an entity:</source>
+        <translation type="unfinished">LeechCraft ha rilevato una entità:</translation>
+    </message>
+    <message>
+        <source>TextLabel</source>
+        <translation type="unfinished">EtichettaTesto</translation>
+    </message>
+    <message>
+        <source>It could be downloaded by:</source>
+        <translation type="unfinished">Può essere scaricato da:</translation>
+    </message>
+    <message>
+        <source>It could be handled by:</source>
+        <translation type="unfinished">Può essere aperto da:</translation>
+    </message>
+</context>
+<context>
+    <name>KeySequencer</name>
+    <message>
+        <source>Press key sequence</source>
+        <translation type="unfinished">Premi la sequenza chiave</translation>
+    </message>
+    <message>
+        <source>background-color: rgb(255, 255, 255);</source>
+        <translation type="unfinished">background-color: rgb(255, 255, 255);</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation type="unfinished">Pulisci</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft</name>
+    <message>
+        <source>LeechCraft</source>
+        <translation type="unfinished">LeechCraft</translation>
+    </message>
+    <message>
+        <source>Summary</source>
+        <translation type="unfinished">Sommario</translation>
+    </message>
+    <message>
+        <source>toolBar</source>
+        <translation type="unfinished">toolBar</translation>
+    </message>
+    <message>
+        <source>Add task</source>
+        <translation type="unfinished">Aggiungi task</translation>
+    </message>
+    <message>
+        <source>Adds job to a supporting plugin</source>
+        <translation type="unfinished">Aggiungi funzione a un plugin supportato</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished">Settaggi</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation type="unfinished">Abbandona</translation>
+    </message>
+    <message>
+        <source>exit</source>
+        <translation type="unfinished">esci</translation>
+    </message>
+    <message>
+        <source>Plugin manager</source>
+        <translation type="unfinished">Plugin manager</translation>
+    </message>
+    <message>
+        <source>Fullscreen mode</source>
+        <translation type="unfinished">Modalità schermo intero</translation>
+    </message>
+    <message>
+        <source>F11</source>
+        <translation type="unfinished">F11</translation>
+    </message>
+    <message>
+        <source>Log</source>
+        <translation type="unfinished">Traccia</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Application</name>
+    <message>
+        <source>Critical failure</source>
+        <translation type="unfinished">Fallimento critico</translation>
+    </message>
+    <message>
+        <source>Could not create path %1/.leechcraft</source>
+        <translation type="unfinished">Non può creare il percorso %1/.leechcraft</translation>
+    </message>
+    <message>
+        <source>bytes</source>
+        <translation type="unfinished">bytes</translation>
+    </message>
+    <message>
+        <source>KB</source>
+        <translation type="unfinished">KB</translation>
+    </message>
+    <message>
+        <source>MB</source>
+        <translation type="unfinished">MB</translation>
+    </message>
+    <message>
+        <source>GB</source>
+        <translation type="unfinished">GB</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::CategoryMerger</name>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished">Nome</translation>
+    </message>
+    <message>
+        <source>State</source>
+        <translation type="unfinished">Stato</translation>
+    </message>
+    <message>
+        <source>Progress</source>
+        <translation type="unfinished">Progresso</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::CommonJobAdder</name>
+    <message>
+        <source>Select file</source>
+        <translation type="unfinished">Seleziona un file</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Core</name>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished">Nome</translation>
+    </message>
+    <message>
+        <source>State</source>
+        <translation type="unfinished">Stato</translation>
+    </message>
+    <message>
+        <source>Progress</source>
+        <translation type="unfinished">Progresso</translation>
+    </message>
+    <message>
+        <source>Calculating dependencies...</source>
+        <translation type="unfinished">Calcolando le dipendenze...</translation>
+    </message>
+    <message>
+        <source>Setting up %1...</source>
+        <translation type="unfinished">Settando %1...</translation>
+    </message>
+    <message>
+        <source>No plugins are able to download &quot;%1&quot;</source>
+        <translation type="unfinished">Nessun plugin è in grado di scaricare &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <source>Too long to show</source>
+        <translation type="unfinished">Troppo lungo da mostrare</translation>
+    </message>
+    <message>
+        <source>Select save location</source>
+        <translation type="unfinished">Seleziona la locazione di salvataggio</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished">Errore</translation>
+    </message>
+    <message>
+        <source>Could not add job to the selected downloader, would you like to try another one?</source>
+        <translation type="unfinished">Non può aggiungere funzione allo scaricatore selezionato, vuoi provarne un altro?</translation>
+    </message>
+    <message>
+        <source>Could not handle job with the selected handler, would you like to try another one?</source>
+        <translation type="unfinished">Non può aggiungere funzione all&apos;apritore selezionato, vuoi provarne un altro?</translation>
+    </message>
+    <message>
+        <source>Could not handle download entity %1.</source>
+        <translation type="unfinished">Non può aprire l&apos;entità scaricata %1.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::FancyPopupManager</name>
+    <message>
+        <source>LeechCraft Notification</source>
+        <translation type="unfinished">Notifica LeechCraft</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::MainWindow</name>
+    <message>
+        <source>Initializing core and plugins...</source>
+        <translation type="unfinished">Inizializzando base e plugins...</translation>
+    </message>
+    <message>
+        <source>Plugins</source>
+        <translation type="unfinished">Plugins</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished">Errore</translation>
+    </message>
+    <message>
+        <source>Initializing interface...</source>
+        <translation type="unfinished">Inizializzando l&apos;interfaccia</translation>
+    </message>
+    <message>
+        <source>LeechCraft</source>
+        <translation type="unfinished">LeechCraft</translation>
+    </message>
+    <message>
+        <source>/s	</source>
+        <translation type="unfinished">/s	</translation>
+    </message>
+    <message>
+        <source>/s</source>
+        <translation type="unfinished">/s</translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation type="unfinished">Domanda</translation>
+    </message>
+    <message>
+        <source>Do you really want to quit?</source>
+        <translation type="unfinished">Vuoi veramente abbandonare?</translation>
+    </message>
+    <message>
+        <source>Plugin control</source>
+        <translation type="unfinished">Controllo plugin</translation>
+    </message>
+    <message>
+        <source>%1 down, %2 up</source>
+        <translation type="unfinished">%1 scarica, %2 manda</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetworkAccessManager</name>
+    <message>
+        <source>The URL&lt;br /&gt;&lt;code&gt;%1&lt;/code&gt;&lt;br /&gt;with realm&lt;br /&gt;&lt;em&gt;%2&lt;/em&gt;&lt;br /&gt;requires authentication.</source>
+        <translation type="unfinished">L&apos;indirizzo&lt;br /&gt;&lt;code&gt;%1&lt;/code&gt;&lt;br /&gt;con regno&lt;br /&gt;&lt;em&gt;%2&lt;/em&gt;&lt;br /&gt;richiede autenticazione.</translation>
+    </message>
+    <message>
+        <source>The proxy &lt;br /&gt;&lt;code&gt;%1&lt;/code&gt;&lt;br /&gt;with realm&lt;br /&gt;&lt;em&gt;%2&lt;/em&gt;&lt;br /&gt;requires authentication.</source>
+        <translation type="unfinished">Il proxy &lt;br /&gt;&lt;code&gt;%1&lt;/code&gt;&lt;br /&gt;con regno&lt;br /&gt;&lt;em&gt;%2&lt;/em&gt;&lt;br /&gt;richiede autenticazione.</translation>
+    </message>
+    <message>
+        <source>&lt;code&gt;%1&lt;/code&gt;&lt;br /&gt;has SSL errors. What do you want to do?</source>
+        <translation type="unfinished">&lt;code&gt;%1&lt;/code&gt;&lt;br /&gt;ha errori SSL. Cosa vuoi fare?</translation>
+    </message>
+    <message>
+        <source>Could not create Core directory.</source>
+        <translation type="unfinished">Impossibile creare la cartella base.</translation>
+    </message>
+    <message>
+        <source>Could not save cookies, error opening cookie file.</source>
+        <translation type="unfinished">Impossibile salvare i cookies, errore apertura cookie file.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::OperationalModel</name>
+    <message>
+        <source>Entity</source>
+        <translation type="unfinished">Entità</translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation type="unfinished">Categoria</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation type="unfinished">Informazione</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::PluginManager</name>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished">Nome</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation type="unfinished">File</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished">Attenzione</translation>
+    </message>
+    <message>
+        <source>Release of one or more plugins failed.</source>
+        <translation type="unfinished">Rilascio di uno o più plugin fallito.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::RequestNormalizer</name>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished">Nome</translation>
+    </message>
+    <message>
+        <source>State</source>
+        <translation type="unfinished">Stato</translation>
+    </message>
+    <message>
+        <source>Progress</source>
+        <translation type="unfinished">Progresso</translation>
+    </message>
+    <message>
+        <source>Request validation error: %1</source>
+        <translation type="unfinished">Errore nella validazione della richiesta: %1</translation>
+    </message>
+    <message>
+        <source>Request parsing error: %1</source>
+        <translation type="unfinished">Errore nella analisi della richiesta.</translation>
+    </message>
+    <message>
+        <source>Open/close braces mismatch</source>
+        <translation type="unfinished">Apertura/chiusura delle parentesi non coincide</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::SslErrorsDialog</name>
+    <message>
+        <source>Certificate</source>
+        <translation type="unfinished">Certificato</translation>
+    </message>
+    <message>
+        <source>(No certificate available for this error)</source>
+        <translation type="unfinished">(Nessun certificato disponibile per questo errore)</translation>
+    </message>
+    <message>
+        <source>Valid:</source>
+        <translation type="unfinished">Valido:</translation>
+    </message>
+    <message>
+        <source>yes</source>
+        <translation type="unfinished">si</translation>
+    </message>
+    <message>
+        <source>no</source>
+        <translation type="unfinished">no</translation>
+    </message>
+    <message>
+        <source>Effective date:</source>
+        <translation type="unfinished">Data effettiva:</translation>
+    </message>
+    <message>
+        <source>Expiry date:</source>
+        <translation type="unfinished">Data di scadenza:</translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation type="unfinished">Versione:</translation>
+    </message>
+    <message>
+        <source>Serial number:</source>
+        <translation type="unfinished">Numero seriale:</translation>
+    </message>
+    <message>
+        <source>MD5 digest:</source>
+        <translation type="unfinished">Raccolta MD5:</translation>
+    </message>
+    <message>
+        <source>SHA1 digest:</source>
+        <translation type="unfinished">Raccolta SHA1:</translation>
+    </message>
+    <message>
+        <source>Issuer info</source>
+        <translation type="unfinished">Informazione emittente</translation>
+    </message>
+    <message>
+        <source>Organization:</source>
+        <translation type="unfinished">Organizzazione:</translation>
+    </message>
+    <message>
+        <source>Common name:</source>
+        <translation type="unfinished">Nome comune:</translation>
+    </message>
+    <message>
+        <source>Locality:</source>
+        <translation type="unfinished">Località:</translation>
+    </message>
+    <message>
+        <source>Organizational unit name:</source>
+        <translation type="unfinished">Nome unità organizazzionale:</translation>
+    </message>
+    <message>
+        <source>Country name:</source>
+        <translation type="unfinished">Nome Paese:</translation>
+    </message>
+    <message>
+        <source>State or province name:</source>
+        <translation type="unfinished">Nome di stato o provincia:</translation>
+    </message>
+    <message>
+        <source>Subject info</source>
+        <translation type="unfinished">Informazioni soggetto</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::TagsViewer</name>
+    <message>
+        <source>Rename</source>
+        <translation type="unfinished">Rinomina</translation>
+    </message>
+    <message>
+        <source>Enter new tag name</source>
+        <translation type="unfinished">Inserisci un nuovo nome tag</translation>
+    </message>
+</context>
+<context>
+    <name>LogToolBox</name>
+    <message>
+        <source>Log</source>
+        <translation type="unfinished">Traccia</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation type="unfinished">Pulisci</translation>
+    </message>
+</context>
+<context>
+    <name>PluginManagerDialog</name>
+    <message>
+        <source>Plugin Manager</source>
+        <translation type="unfinished">Plugin Manager</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsSink</name>
+    <message>
+        <source>Preferences</source>
+        <translation type="unfinished">Preferenze</translation>
+    </message>
+</context>
+<context>
+    <name>ShortcutManager</name>
+    <message>
+        <source>Shortcut manager</source>
+        <translation type="unfinished">Manager scorciatoia</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished">Nome</translation>
+    </message>
+    <message>
+        <source>Shortcut</source>
+        <translation type="unfinished">Scorciatoia</translation>
+    </message>
+</context>
+<context>
+    <name>SslErrorsDialog</name>
+    <message>
+        <source>SSL errors occured</source>
+        <translation type="unfinished">Errori SSL avvenuti</translation>
+    </message>
+    <message>
+        <source>&lt;Here we say that errors happened&gt;</source>
+        <translation type="unfinished">&lt;Qui si può dire che sono avvenuti errori&gt;</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished">Nome</translation>
+    </message>
+    <message>
+        <source>Value</source>
+        <translation type="unfinished">Valore</translation>
+    </message>
+    <message>
+        <source>Don&apos;t remember my choice</source>
+        <translation type="unfinished">Non ricordare la mia scelta</translation>
+    </message>
+    <message>
+        <source>Remember my choice for the file</source>
+        <translation type="unfinished">Ricorda la mia scelta per il file</translation>
+    </message>
+    <message>
+        <source>Remember my choice for the whole host</source>
+        <translation type="unfinished">Ricorda la mia scelta per tutto l&apos;host</translation>
+    </message>
+</context>
+<context>
+    <name>TagsManager</name>
+    <message>
+        <source>untagged</source>
+        <translation type="unfinished">non taggato</translation>
+    </message>
+</context>
+<context>
+    <name>TagsViewer</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished">Form</translation>
+    </message>
+    <message>
+        <source>Rename...</source>
+        <translation type="unfinished">Rinomina...</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished">Rimuovi</translation>
+    </message>
+</context>
+</TS>
