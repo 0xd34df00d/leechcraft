a785bf88780f58820c74d176309626ff3901a336
0xd34df00d <0xd34df00d@gmail.com>
2009-07-20 16:30:27 +0400

    CSTP: Added Arabic translations.

---
+++ src/plugins/cstp/leechcraft_cstp_ar.ts
@@ -0,0 +1,202 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ar_SY" sourcelanguage="en">
+<context>
+    <name>AddTask</name>
+    <message>
+        <location filename="addtask.ui" line="13"/>
+        <source>Add task</source>
+        <translation>إضافة مهمّة</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="21"/>
+        <source>URL:</source>
+        <translation>المكان:</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="31"/>
+        <source>Local path:</source>
+        <translation>المكان المحلّي:</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="43"/>
+        <source>Browse...</source>
+        <translation>استكشاف...</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="55"/>
+        <source>Filename:</source>
+        <translation>أسم الملف:</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="65"/>
+        <source>Comment:</source>
+        <translation>التعليق:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::CSTP::AddTask</name>
+    <message>
+        <location filename="addtask.cpp" line="89"/>
+        <source>Directory %1 doesn&apos;t exist, would you like to select another?</source>
+        <translation>المكان %1 لا يوجد، هل تريد اختيار غيره؟</translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="92"/>
+        <source>Directory %1 isn&apos;t readable, would you like to select another?</source>
+        <translation>غير قادر على قراءة المكان %1، هل تريد اختيار غيره؟</translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="95"/>
+        <source>Directory %1 isn&apos;t writable, would you like to select another?</source>
+        <translation>غير قادر على الكتابة في المكان %1، هل تريد اختيار غيره؟</translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="98"/>
+        <source>%1 isn&apos;t a directory at all, would you like to select another?</source>
+        <translation>%1 ليس مكان مطلقاً، هل تريد اختيار مكان آخر؟</translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="107"/>
+        <source>LeechCraft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="139"/>
+        <source>Select directory</source>
+        <translation>اختر المكان</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::CSTP::CSTP</name>
+    <message>
+        <location filename="cstp.cpp" line="174"/>
+        <source>Remove</source>
+        <translation>إزالة</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="181"/>
+        <source>Remove all</source>
+        <translation>إزالة الكل</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="190"/>
+        <source>Start</source>
+        <translation>بدء</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="197"/>
+        <source>Stop</source>
+        <translation>إيقاف</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="204"/>
+        <source>Start all</source>
+        <translation>بدء الكل</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="211"/>
+        <source>Stop all</source>
+        <translation>إيقاف الكل</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="223"/>
+        <source>File exists</source>
+        <translation>الملف موجود</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="224"/>
+        <source>File %1 already exists, continue download?</source>
+        <translation>الملف %1 موجود، استمرار تحميله؟</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::CSTP::Core</name>
+    <message>
+        <location filename="core.cpp" line="36"/>
+        <source>URL</source>
+        <translation>المكان</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="37"/>
+        <source>State</source>
+        <translation>الحالة</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="38"/>
+        <source>Progress</source>
+        <translation>التقدّم</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="39"/>
+        <source>Speed</source>
+        <translation>السرعة</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="40"/>
+        <source>ETA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="41"/>
+        <source>DTA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="189"/>
+        <source>Could not truncate file </source>
+        <translation>غير قادر على تقطيع الملف</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="325"/>
+        <source>%1% (%2 of %3)</source>
+        <translation>%1% (%2 من %3)</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="330"/>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="338"/>
+        <source>/s</source>
+        <translation>ثوان</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="437"/>
+        <source>Could not open file </source>
+        <translation>غير قادر على فتح الملف</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::CSTP::MainViewDelegate</name>
+    <message>
+        <location filename="mainviewdelegate.cpp" line="53"/>
+        <source>Idle</source>
+        <translation>عاطل</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::CSTP::Task</name>
+    <message>
+        <location filename="task.cpp" line="168"/>
+        <source>Stopped</source>
+        <translation>متوفّف</translation>
+    </message>
+    <message>
+        <location filename="task.cpp" line="170"/>
+        <source>Finished</source>
+        <translation>منتهي</translation>
+    </message>
+    <message>
+        <location filename="task.cpp" line="172"/>
+        <source>Running</source>
+        <translation>تحميل</translation>
+    </message>
+    <message>
+        <location filename="task.cpp" line="193"/>
+        <source>Task isn&apos;t initialized properly</source>
+        <translation>المهمّة لَمْ تُشغّلْ بشكل صحيح</translation>
+    </message>
+</context>
+</TS>
