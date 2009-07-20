877f18416d86d4cac0c91190996f145676a82a59
0xd34df00d <0xd34df00d@gmail.com>
2009-07-20 16:42:56 +0400

    DeadLyrics: Arabic translation.

---
+++ src/plugins/deadlyrics/leechcraft_deadlyrics_ar.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ar" sourcelanguage="en">
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::Core</name>
+    <message>
+        <location filename="core.cpp" line="45"/>
+        <location filename="core.cpp" line="50"/>
+        <source>lyrics</source>
+        <translation>كلمات الأغاني</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::DeadLyRicS</name>
+    <message>
+        <location filename="deadlyrics.cpp" line="28"/>
+        <source>Lyrics Searcher</source>
+        <translation>باحث عن كلمات الأغاني</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::FindProxy</name>
+    <message>
+        <location filename="findproxy.cpp" line="116"/>
+        <source>Searching...</source>
+        <translation>جاري البحث...</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::LyricWikiSearcher</name>
+    <message>
+        <location filename="lyricwikisearcher.cpp" line="97"/>
+        <source>Lyrics fetch error</source>
+        <translation>خطأ في إحضار الكلمات</translation>
+    </message>
+    <message>
+        <location filename="lyricwikisearcher.cpp" line="105"/>
+        <source>No lyrics found</source>
+        <translation>غير موجود</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="searcher.cpp" line="42"/>
+        <source>Unknown %1 %2</source>
+        <translation>غير معروف %1 %2</translation>
+    </message>
+</context>
+</TS>
