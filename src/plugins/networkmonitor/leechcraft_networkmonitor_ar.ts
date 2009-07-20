e0b7d3fc50984eda2125c2bb4074d48c504172b0
0xd34df00d <0xd34df00d@gmail.com>
2009-07-20 16:52:08 +0400

    NetworkMonitor: Arabic translation.

--- src/plugins/networkmonitor/leechcraft_networkmonitor_ar.ts
+++ src/plugins/networkmonitor/leechcraft_networkmonitor_ar.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ar" sourcelanguage="en">
 <context>
     <name>LeechCraft::Plugins::NetworkMonitor::HeaderModel</name>
     <message>
         <location filename="headermodel.cpp" line="8"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>أسم</translation>
     </message>
     <message>
         <location filename="headermodel.cpp" line="9"/>
         <source>Value</source>
-        <translation type="unfinished"></translation>
+        <translation>القيمة</translation>
     </message>
 </context>
 <context>
@@ -19,12 +19,12 @@
     <message>
         <location filename="networkmonitor.cpp" line="41"/>
         <source>Network monitor...</source>
-        <translation type="unfinished"></translation>
+        <translation>مراقب الشبكة...</translation>
     </message>
     <message>
         <location filename="networkmonitor.cpp" line="63"/>
         <source>Monitors HTTP network requests and responses.</source>
-        <translation type="unfinished"></translation>
+        <translation>يراقب الطلبات و الإستجابات HTTP.</translation>
     </message>
 </context>
 <context>
@@ -32,27 +32,27 @@
     <message>
         <location filename="requestmodel.cpp" line="17"/>
         <source>Date started</source>
-        <translation type="unfinished"></translation>
+        <translation>مشغّل</translation>
     </message>
     <message>
         <location filename="requestmodel.cpp" line="18"/>
         <source>Date finished</source>
-        <translation type="unfinished"></translation>
+        <translation>متوقف</translation>
     </message>
     <message>
         <location filename="requestmodel.cpp" line="19"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>النوع</translation>
     </message>
     <message>
         <location filename="requestmodel.cpp" line="20"/>
         <source>Host</source>
-        <translation type="unfinished"></translation>
+        <translation>المضيف</translation>
     </message>
     <message>
         <location filename="requestmodel.cpp" line="60"/>
         <source>In progress</source>
-        <translation type="unfinished"></translation>
+        <translation>مستمر</translation>
     </message>
 </context>
 <context>
@@ -60,27 +60,27 @@
     <message>
         <location filename="networkmonitor.ui" line="14"/>
         <source>Network Monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>مراقب الشبكة</translation>
     </message>
     <message>
         <location filename="networkmonitor.ui" line="20"/>
         <source>Clear finished items</source>
-        <translation type="unfinished"></translation>
+        <translation>مسح الفقرات المُنتَهية</translation>
     </message>
     <message>
         <location filename="networkmonitor.ui" line="32"/>
         <source>Requests</source>
-        <translation type="unfinished"></translation>
+        <translation>الطلبات</translation>
     </message>
     <message>
         <location filename="networkmonitor.ui" line="48"/>
         <source>Request headers</source>
-        <translation type="unfinished"></translation>
+        <translation>أسماء الطلبات</translation>
     </message>
     <message>
         <location filename="networkmonitor.ui" line="69"/>
         <source>Response headers</source>
-        <translation type="unfinished"></translation>
+        <translation>أسماء الإستجابات</translation>
     </message>
 </context>
 </TS>
