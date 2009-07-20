51f6253a4310a8f4c995a40be6f70c5d625906f7
0xd34df00d <0xd34df00d@gmail.com>
2009-07-20 16:59:42 +0400

    Poshuku CleanWeb: Arabic translation.

---
+++ src/plugins/poshuku/plugins/cleanweb/leechcraft_poshuku_cleanweb_ar.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ar" sourcelanguage="en">
+<context>
+    <name>CleanWeb</name>
+    <message>
+        <location filename="cleanweb.cpp" line="53"/>
+        <source>Blocks unwanted ads.</source>
+        <translation>مَنْع الإعلاناتِ الغير مرغوبةِ.</translation>
+    </message>
+</context>
+<context>
+    <name>Core</name>
+    <message>
+        <location filename="core.cpp" line="582"/>
+        <source>Error</source>
+        <translation>o&apos;H</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="583"/>
+        <source>The subscription wasn&apos;t delegated.</source>
+        <translation>الإشتراك لَمْ يُوفَدْ.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Poshuku::Plugins::CleanWeb::Core</name>
+    <message>
+        <location filename="core.cpp" line="186"/>
+        <source>Name</source>
+        <translation>hGsl</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="187"/>
+        <source>Last updated</source>
+        <translation>آخر تحديث</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="188"/>
+        <source>URL</source>
+        <translation>المكان</translation>
+    </message>
+</context>
+<context>
+    <name>SubscriptionsManager</name>
+    <message>
+        <location filename="subscriptionsmanager.ui" line="14"/>
+        <source>Form</source>
+        <translation>النموذج</translation>
+    </message>
+    <message>
+        <location filename="subscriptionsmanager.ui" line="30"/>
+        <source>Remove</source>
+        <translation>إزالة</translation>
+    </message>
+</context>
+</TS>
