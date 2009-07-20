0b28a066e6a15569fe3c32e201babe8925efae00
0xd34df00d <0xd34df00d@gmail.com>
2009-07-20 16:50:23 +0400

    HistoryHolder: Updated Arabic translations.

---
+++ src/plugins/historyholder/leechcraft_historyholder_ar.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ar" sourcelanguage="en">
+<context>
+    <name>Core</name>
+    <message>
+        <location filename="core.cpp" line="154"/>
+        <source>Binary data</source>
+        <translation>البيانات القابلة للتّنفيذ</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::HistoryHolder::Core</name>
+    <message>
+        <location filename="core.cpp" line="48"/>
+        <source>Entity/location</source>
+        <translation>الكيان / المكان</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="49"/>
+        <source>Date</source>
+        <translation>التاريخ</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="50"/>
+        <source>Tags</source>
+        <translation>العلامات</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="65"/>
+        <source>Remove</source>
+        <translation>إزالة</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="historyholder.cpp" line="29"/>
+        <source>Holds history from various plugins</source>
+        <translation>يحفظ بيانات من الإضافات المتعددة</translation>
+    </message>
+</context>
+</TS>
