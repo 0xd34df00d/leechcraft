c42d9020b275c17a4af2786bbc32035ffe17667f
0xd34df00d <0xd34df00d@gmail.com>
2009-07-29 16:07:54 +0400

    LCFTP: updated translations.

--- src/plugins/lcftp/leechcraft_lcftp_ru_RU.ts
+++ src/plugins/lcftp/leechcraft_lcftp_ru_RU.ts
@@ -1,4 +1,141 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Core</name>
+    <message>
+        <source>LeechCraft</source>
+        <translation>LeechCraft</translation>
+    </message>
+    <message>
+        <source>%1 already exists. What do you want to do?</source>
+        <translation>%1 уже сущестует. Что Вы хотите сделать?</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>Продолжить</translation>
+    </message>
+    <message>
+        <source>Overwrite</source>
+        <translation>Начать заново</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <source>Error removing %1</source>
+        <translation>Ошибка удаления %1</translation>
+    </message>
+    <message>
+        <source>Choose new file name</source>
+        <translation>Выберите новое имя файла</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Core</name>
+    <message>
+        <source>Downloading at %1</source>
+        <translation>Скачивание на %1</translation>
+    </message>
+    <message>
+        <source>Uploading at %1</source>
+        <translation>Загрузка на %1</translation>
+    </message>
+    <message>
+        <source>Waiting</source>
+        <translation>Ожидание</translation>
+    </message>
+    <message>
+        <source>%1 of %2 (%3%)</source>
+        <translation>%1 из %2 (%3%)</translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <source>LeechCraft</source>
+        <translation>LeechCraft</translation>
+    </message>
+    <message>
+        <source>Download finished: %1</source>
+        <translation>Закачка окончена: %1</translation>
+    </message>
+    <message>
+        <source>While mirroring&lt;br /&gt;%1&lt;br /&gt;to&lt;br /&gt;%2&lt;br /&gt;an error occured:&lt;br /&gt;%3&lt;br /&gt; already exists.</source>
+        <translation>Во время зеркалирования&lt;br /&gt;%1&lt;br /&gt;в&lt;br /&gt;%2&lt;br /&gt;возникла ошибка:&lt;br /&gt;%3&lt;br /&gt;уже существует.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::LCFTP</name>
+    <message>
+        <source>A simple FTP client</source>
+        <translation>Простой FTP-клиент</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Pane</name>
+    <message>
+        <source>Transfer</source>
+        <translation>Передачть</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Размер</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation>Дата</translation>
+    </message>
+    <message>
+        <source>Directory</source>
+        <translation>Директория</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Worker</name>
+    <message>
+        <source>Could not open file&lt;br /&gt;%1&lt;br /&gt;%2</source>
+        <translation>Не могу открыть файл&lt;br /&gt;%1&lt;br /&gt;%2</translation>
+    </message>
+</context>
+<context>
+    <name>Pane</name>
+    <message>
+        <source>Form</source>
+        <translation>Форма</translation>
+    </message>
+    <message>
+        <source>..</source>
+        <translation>..</translation>
+    </message>
+    <message>
+        <source>/</source>
+        <translation>/</translation>
+    </message>
+</context>
+<context>
+    <name>TabWidget</name>
+    <message>
+        <source>Form</source>
+        <translation>Форма</translation>
+    </message>
+</context>
 </TS>
--- src/plugins/lcftp/leechcraft_lcftp_uk_UA.ts
+++ src/plugins/lcftp/leechcraft_lcftp_uk_UA.ts
@@ -1,4 +1,141 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0">
+<context>
+    <name>Core</name>
+    <message>
+        <source>LeechCraft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 already exists. What do you want to do?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Overwrite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error removing %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose new file name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Core</name>
+    <message>
+        <source>Downloading at %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Uploading at %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Waiting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 of %2 (%3%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>LeechCraft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download finished: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>While mirroring&lt;br /&gt;%1&lt;br /&gt;to&lt;br /&gt;%2&lt;br /&gt;an error occured:&lt;br /&gt;%3&lt;br /&gt; already exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::LCFTP</name>
+    <message>
+        <source>A simple FTP client</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Pane</name>
+    <message>
+        <source>Transfer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Worker</name>
+    <message>
+        <source>Could not open file&lt;br /&gt;%1&lt;br /&gt;%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Pane</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>..</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>/</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TabWidget</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
