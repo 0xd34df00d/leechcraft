db75f07b2a07de178e84d18df204d67deb1c435d
0xd34df00d <0xd34df00d@gmail.com>
2009-08-19 12:49:21 +0400

    Overall: marked headers.

--- src/xmlsettingsdialog/bytearray.cpp
+++ src/xmlsettingsdialog/bytearray.cpp
@@ -1,15 +1,21 @@
-/*
-	Copyright (c) 2008 by Rudoy Georg <0xd34df00d@gmail.com>
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2009  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
 
- ***************************************************************************
- *																		 *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or	 *
- *   (at your option) any later version.								   *
- *																		 *
- ***************************************************************************
-*/
 #include "bytearray.h"
 #include <QString>
 
