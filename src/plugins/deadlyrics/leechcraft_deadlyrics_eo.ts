85999098c85a6fb3f5a9dc617cb630468143da9a
0xd34df00d <0xd34df00d@gmail.com>
2009-08-22 13:19:33 +0400

    Overall: ran lupdate over the Esperanto translations.

---
+++ src/plugins/deadlyrics/leechcraft_deadlyrics_eo.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::Core</name>
+    <message>
+        <location filename="core.cpp" line="63"/>
+        <location filename="core.cpp" line="68"/>
+        <source>lyrics</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::DeadLyRicS</name>
+    <message>
+        <location filename="deadlyrics.cpp" line="46"/>
+        <source>Lyrics Searcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::FindProxy</name>
+    <message>
+        <location filename="findproxy.cpp" line="134"/>
+        <source>Searching...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::LyricWikiSearcher</name>
+    <message>
+        <location filename="lyricwikisearcher.cpp" line="115"/>
+        <source>Lyrics fetch error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="lyricwikisearcher.cpp" line="123"/>
+        <source>No lyrics found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="searcher.cpp" line="60"/>
+        <source>Unknown %1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
