a870aba96ab31f5acde11d2cf62a47fe464b9729
0xd34df00d <0xd34df00d@gmail.com>
2009-08-28 15:33:31 +0400

    Tools: geniconslist.sh made more handy.

--- tools/geniconslist.sh
+++ tools/geniconslist.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
 
-grep ActionIcon *
-for i in `ls plugins`; do echo "List for $i:"; grep ActionIcon plugins/$i/*; done
+touch tmp
+grep ActionIcon * > tmp
+for i in `ls plugins`; do echo "List for $i:" >> tmp; grep ActionIcon plugins/$i/* | sed 's/.*\".*\".*\"\(.*\)\"/\1/' > tmp2; sort -u tmp2 >> tmp; done
+
+
+cat tmp
+rm tmp2
+rm tmp
