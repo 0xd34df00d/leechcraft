d715a0a346a047cb032b769e15ff350125bc6248
0xd34df00d <0xd34df00d@gmail.com>
2009-08-30 19:29:36 +0400

    Poshuku CleanWeb: added Esperanto ts.

--- src/plugins/poshuku/plugins/cleanweb/leechcraft_poshuku_cleanweb_eo.ts
+++ src/plugins/poshuku/plugins/cleanweb/leechcraft_poshuku_cleanweb_eo.ts
@@ -1,12 +1,13 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru" sourcelanguage="en">
 <context>
     <name>CleanWeb</name>
     <message>
         <location filename="cleanweb.cpp" line="72"/>
         <source>Blocks unwanted ads.</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Хотя evitinda - скорее от to be avoided, думаю, здесь подойдет</translatorcomment>
+        <translation>Bloku evitindan anoncon.</translation>
     </message>
 </context>
 <context>
@@ -14,12 +15,12 @@
     <message>
         <location filename="core.cpp" line="595"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Eraro</translation>
     </message>
     <message>
         <location filename="core.cpp" line="596"/>
         <source>The subscription wasn&apos;t delegated.</source>
-        <translation type="unfinished"></translation>
+        <translation>La abono ne estis delegita.</translation>
     </message>
 </context>
 <context>
@@ -27,17 +28,17 @@
     <message>
         <location filename="core.cpp" line="208"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nomo</translation>
     </message>
     <message>
         <location filename="core.cpp" line="209"/>
         <source>Last updated</source>
-        <translation type="unfinished"></translation>
+        <translation>Laste ĝisdatigita</translation>
     </message>
     <message>
         <location filename="core.cpp" line="210"/>
         <source>URL</source>
-        <translation type="unfinished"></translation>
+        <translation>URL</translation>
     </message>
 </context>
 <context>
@@ -45,12 +46,12 @@
     <message>
         <location filename="subscriptionsmanager.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Formularo</translation>
     </message>
     <message>
         <location filename="subscriptionsmanager.ui" line="30"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Forigu</translation>
     </message>
 </context>
 </TS>
