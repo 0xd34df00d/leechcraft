507d948072f7611431ee5913532e3abb91d82487
0xd34df00d <0xd34df00d@gmail.com>
2009-09-18 01:56:58 +0400

    DCminator: compile the lib.

---
+++ src/plugins/dcminator/leechcraft_dcminator_ru_RU.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>LeechCraft::Plugins::DCminator::Plugin</name>
+    <message>
+        <location filename="dcminator.cpp" line="47"/>
+        <source>DirectConnect client</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
