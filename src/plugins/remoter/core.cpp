320f77bf300e50977e0de431fd02c2eec1670500
0xd34df00d <0xd34df00d@gmail.com>
2009-09-18 23:13:47 +0400

    Remoter: use the QWebClient.

--- src/plugins/remoter/core.cpp
+++ src/plugins/remoter/core.cpp
@@ -16,180 +16,36 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **********************************************************************/
 
-#include <WServer>
 #include "core.h"
-#include <WApplication>
-#include <WEnvironment>
-#include <QStringList>
-#include <QCoreApplication>
-#include <QTemporaryFile>
-#include <QUrl>
-#include <QToolBar>
-#include <QSettings>
-#include <QtDebug>
-#include <plugininterface/proxy.h>
-#include <plugininterface/mergemodel.h>
-#include "qtowabstractitemmodeladaptor.h"
-#include "qtowtoolbaradaptor.h"
-#include "interface.h"
+#include "webclient/src/webclient.h"
 
-using LeechCraft::Util::MergeModel;
-
-Wt::WApplication* CreateApplication (const Wt::WEnvironment& e)
+namespace LeechCraft
 {
-	return Core::Instance ().CreateApplication (e);;
-}
-
-Core::Core ()
-: Server_ (0)
-, TasksModel_ (0)
-, HistoryModel_ (0)
-{
-	InitializeServer ();
-}
-
-Core& Core::Instance ()
-{
-    static Core inst;
-    return inst;
-}
-
-void Core::Release ()
-{
-	try
+	namespace Plugins
 	{
-		if (Server_)
-			Server_->stop ();
-	}
-	catch (const Wt::WServer::Exception& e)
-	{
-		qWarning () << Q_FUNC_INFO << e.what ();
-	}
-	catch (const std::exception& e)
-	{
-		qWarning () << Q_FUNC_INFO << e.what ();
-	}
-	delete Server_;
-	Server_ = 0;
-}
-
-void Core::SetHistoryModel (MergeModel *model)
-{
-	HistoryModel_ = model;
-}
-
-void Core::SetDownloadersModel (MergeModel *model)
-{
-	TasksModel_ = model;
-}
-
-void Core::AddObject (QObject *object, const QString& feature)
-{
-	Objects_ << object;
-}
-
-Wt::WApplication* Core::CreateApplication (const Wt::WEnvironment& e)
-{
-	Wt::WApplication *result = new Wt::WApplication (e);
-	result->setTitle ("LeechCraft");
-
-	Interface *interface = new Interface (result, e);
-
-	return result;
-}
-
-MergeModel* Core::GetTasksModel () const
-{
-	return TasksModel_;
-}
-
-MergeModel* Core::GetHistoryModel () const
-{
-	return HistoryModel_;
-}
-
-void Core::InitializeServer ()
-{
-	delete Server_;
-	Server_ = new Wt::WServer ();
-
-	// Some black magic to reconstruct the argc/argv passed to us by the
-	// OS as Wt requires at.
-	int delta = 0;
-	QStringList args = QCoreApplication::arguments ();
-	if (!args.contains ("--docroot"))
-		delta += 2;
-	if (!args.contains ("--http-address"))
-		delta += 2;
-	if (!args.contains ("--http-port"))
-		delta += 2;
-
-	int argc = args.size ();
-	char **argv = new char* [argc + delta];
-	for (int i = 0; i < argc; ++i)
-	{
-		QByteArray wa = args.at (i).toLocal8Bit ();
-		argv [i] = new char [wa.size () + 1];
-		std::strcpy (argv [i], wa.constData ());
-	}
-	int allocDelta = delta;
-	if (!args.contains ("--docroot"))
-	{
-		argv [argc + allocDelta - 2] = "--docroot";
-		argv [argc + allocDelta - 1] = ".";
-		allocDelta -= 2;
-	}
-	if (!args.contains ("--http-address"))
-	{
-		argv [argc + allocDelta - 2] = "--http-address";
-		argv [argc + allocDelta - 1] = "127.0.0.1";
-		allocDelta -= 2;
-	}
-	if (!args.contains ("--http-port"))
-	{
-		argv [argc + allocDelta - 2] = "--http-port";
-		argv [argc + allocDelta - 1] = "14600";
-	}
-
-	try
-	{
-		Server_->setServerConfiguration (argc + delta, argv, "");
-	}
-	catch (const Wt::WServer::Exception& e)
-	{
-		qWarning () << Q_FUNC_INFO << "could not set server's configuration:";
-		qWarning () << e.what ();
-		delete Server_;
-		Server_ = 0;
-		return;
-	}
-
-	// As after every black magic, we should clean up.
-	for (int i = 0; i < argc; ++i)
-		delete [] argv [i];
-	delete [] argv;
-
-	Server_->addEntryPoint (Wt::WServer::Application, &::CreateApplication);
-	try
-	{
-		if (!Server_->start ())
+		namespace Remoter
 		{
-			qWarning () << Q_FUNC_INFO << "could not start server";
-		}
-	}
-	catch (const Wt::WServer::Exception& e)
-	{
-		qWarning () << Q_FUNC_INFO << "Failed to start server:";
-		qWarning () << e.what ();
-		delete Server_;
-		Server_ = 0;
-	}
-	catch (const std::exception& e)
-	{
-		qWarning () << Q_FUNC_INFO << "Failed to start server:";
-		qWarning () << e.what ();
-		delete Server_;
-		Server_ = 0;
-	}
-}
+			Core::Core ()
+			{
+			}
+
+			Core& Core::Instance ()
+			{
+				static Core inst;
+				return inst;
+			}
+
+			void Core::Init (ICoreProxy_ptr proxy)
+			{
+				WebClient *wc = new WebClient;
+				wc->setPort (8118);
+				wc->setRootWidget (proxy->GetMainWindow ());
+			}
+
+			void Core::Release ()
+			{
+			}
+		};
+	};
+};
 
--- src/plugins/remoter/core.h
+++ src/plugins/remoter/core.h
@@ -18,55 +18,32 @@
 
 #ifndef CORE_H
 #define CORE_H
-#include <QObjectList>
-#include <QDomDocument>
-#include <QDomElement>
-#include <QVariantList>
-#include <QMap>
-#include <QString>
-#include <QPair>
-#include <plugininterface/guarded.h>
+#include <QObject>
+#include <interfaces/iinfo.h>
 
-class QStringList;
 namespace LeechCraft
 {
 	namespace Util
 	{
 		class MergeModel;
 	};
-};
 
-namespace Wt
-{
-	class WServer;
-	class WEnvironment;
-	class WApplication;
-	class WContainerWidget;
-	class WTreeView;
-};
+	namespace Plugins
+	{
+		namespace Remoter
+		{
+			class Core : public QObject
+			{
+				Q_OBJECT
 
-class Core : public QObject
-{
-    Q_OBJECT
-
-	Wt::WServer *Server_;
-	LeechCraft::Util::Guarded<LeechCraft::Util::MergeModel*> TasksModel_,
-		HistoryModel_;
-	QObjectList Objects_;
-
-    Core ();
-public:
-    static Core& Instance ();
-    void Release ();
-	void SetHistoryModel (LeechCraft::Util::MergeModel*);
-	void SetDownloadersModel (LeechCraft::Util::MergeModel*);
-	void SelectedDownloaderChanged (QObject*);
-    void AddObject (QObject*, const QString& feature);
-	Wt::WApplication* CreateApplication (const Wt::WEnvironment&);
-	LeechCraft::Util::MergeModel* GetTasksModel () const;
-	LeechCraft::Util::MergeModel* GetHistoryModel () const;
-private:
-	void InitializeServer ();
+				Core ();
+			public:
+				static Core& Instance ();
+				void Init (ICoreProxy_ptr);
+				void Release ();
+			};
+		};
+	};
 };
 
 #endif
