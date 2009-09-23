3035e9b01585ec765e051e6f0f9053fc1538f2cd
0xd34df00d <0xd34df00d@gmail.com>
2009-09-23 15:49:46 +0400

    Aggregator: fixed a typo.

--- src/plugins/aggregator/startupthirdpage.cpp
+++ src/plugins/aggregator/startupthirdpage.cpp
@@ -52,7 +52,7 @@ namespace LeechCraft
 						"it; news; software", "http://www.opennet.ru/opennews/opennews_6.rss");
 				Sets_ ["ru"] << FeedInfo (QString::fromUtf8 ("Хабрахабр"),
 						"it; news; blogs; software; science", "http://habrahabr.ru/rss/");
-				Sets_ ["ru"] << FeedInfo (QString::fromutf8 ("Радио-Т"),
+				Sets_ ["ru"] << FeedInfo (QString::fromUtf8 ("Радио-Т"),
 						"it; blogs; podcasts", "http://feeds.rucast.net/radio-t");
 				Sets_ ["ru"] << FeedInfo (QString::fromUtf8 ("MEMBRANA: Люди. Идеи. Технологии."),
 						"news; science", "http://www.membrana.ru/export/rss.xml");
