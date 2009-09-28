6b5ba044fe14badabaa155b69c505f1d6cb64fba
0xd34df00d <0xd34df00d@gmail.com>
2009-09-29 02:03:42 +0400

    AnHero: the crash handler.

---
+++ src/plugins/anhero/leechcraft_anhero_ru_RU.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>LeechCraft::Plugins::YASD::Plugin</name>
+    <message>
+        <location filename="yasd.cpp" line="44"/>
+        <source>Yet Another Site Downloader</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- tools/repos/update-repository.sh
+++ tools/repos/update-repository.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-FTPARCHIVE="/home/d34df00d/Programming/apt/apt-0.7.20.2/bin/apt-ftparchive"
+FTPARCHIVE="apt-ftparchive"
 
-LD_LIBRARY_PATH=/home/d34df00d/Programming/apt/apt-0.7.20.2/bin ${FTPARCHIVE} generate apt-ftparchive.conf
+${FTPARCHIVE} generate apt-ftparchive.conf
 
-LD_LIBRARY_PATH=/home/d34df00d/Programming/apt/apt-0.7.20.2/bin ${FTPARCHIVE} -c apt-master-release.conf \
+${FTPARCHIVE} -c apt-master-release.conf \
 	release dists/master > dists/master/Release
-LD_LIBRARY_PATH=/home/d34df00d/Programming/apt/apt-0.7.20.2/bin ${FTPARCHIVE} -c apt-0.3-release.conf \
+${FTPARCHIVE} -c apt-0.3-release.conf \
 	release dists/0.3 > dists/0.3/Release
 
 for file in `find . -name Release` ; do
