7787bf70ab9847acef5e432b1728e4919d14a310
0xd34df00d <0xd34df00d@gmail.com>
2009-11-02 18:28:43 +0300

    Chatter: ar translations.

---
+++ src/plugins/chatter/leechcraft_chatter_ar.ts
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message>
+        <location filename="config.h" line="6"/>
+        <source>VERSION leechcraft::chatter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="config.h" line="7"/>
+        <source>leech well</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FsIrcView</name>
+    <message>
+        <location filename="fsircview.cpp" line="207"/>
+        <source>Private: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="212"/>
+        <source>Notice: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="258"/>
+        <source>Names for %1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="264"/>
+        <source>Topic for </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="269"/>
+        <source> is now known as </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="290"/>
+        <source> has joined </source>
+        <translation>انضم</translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="303"/>
+        <source> has left </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="308"/>
+        <source> has quit IRC: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="313"/>
+        <source> has kicked </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="313"/>
+        <source> out of </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="318"/>
+        <source> has set mode </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="318"/>
+        <source> on </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="405"/>
+        <source>RAW -&gt; </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="520"/>
+        <source>Private: * %1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.cpp" line="525"/>
+        <source>%1 sets topic to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IrcLayer</name>
+    <message>
+        <location filename="irc.cpp" line="64"/>
+        <source>Contacting IRC server...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="irc.cpp" line="562"/>
+        <source>Disconnected from server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="irc.cpp" line="600"/>
+        <source>Encoding has been set to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="irc.cpp" line="604"/>
+        <source>No such encoding!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="irc.cpp" line="623"/>
+        <source>Invalid IRC URI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="irc.h" line="47"/>
+        <source>See ya in hell!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IrcServer</name>
+    <message>
+        <location filename="ircserver.cpp" line="62"/>
+        <source>Contacting IRC server...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ircserver.cpp" line="75"/>
+        <source>Not connected to server!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ircserver.cpp" line="184"/>
+        <source>Logging in...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="chatter.cpp" line="45"/>
+        <source>Allows to chat in IRC</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fsIrcView</name>
+    <message>
+        <location filename="fsircview.ui" line="13"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="fsircview.ui" line="32"/>
+        <source>irc://irc.freenode.net/#fsirc</source>
+        <translation>irc://irc.freenode.net/#fsirc</translation>
+    </message>
+    <message>
+        <location filename="fsircview.ui" line="52"/>
+        <source>Open IRC URI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.ui" line="57"/>
+        <source>Change nick</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.ui" line="62"/>
+        <source>Set encoding</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.ui" line="67"/>
+        <source>Stop this senseless struggle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsircview.ui" line="96"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:10pt;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:10pt;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>fsMainWindow</name>
+    <message>
+        <location filename="fsmain.ui" line="13"/>
+        <source>fsirc</source>
+        <translation>fsirc</translation>
+    </message>
+</context>
+</TS>
