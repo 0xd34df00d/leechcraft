4e448d165576bdaf263f31c4b937658211fcc760
Georg Rudoy <0xd34df00d@gmail.com>
2009-11-22 18:46:18 +0300

    Summary: lupdated Russian translations.

--- src/plugins/summary/leechcraft_summary_ru_RU.ts
+++ src/plugins/summary/leechcraft_summary_ru_RU.ts
@@ -2,10 +2,107 @@
 <!DOCTYPE TS>
 <TS version="2.0">
 <context>
-    <name>LeechCraft::Plugins::YASD::Plugin</name>
+    <name>LeechCraft::Plugins::Summary::Core</name>
     <message>
-        <location filename="yasd.cpp" line="44"/>
-        <source>Yet Another Site Downloader</source>
+        <location filename="core.cpp" line="79"/>
+        <location filename="core.cpp" line="122"/>
+        <location filename="core.cpp" line="230"/>
+        <source>Summary</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Summary::OperationalModel</name>
+    <message>
+        <location filename="operationalmodel.cpp" line="30"/>
+        <source>Entity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="operationalmodel.cpp" line="31"/>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="operationalmodel.cpp" line="32"/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Summary::RequestNormalizer</name>
+    <message>
+        <location filename="requestnormalizer.cpp" line="48"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestnormalizer.cpp" line="49"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestnormalizer.cpp" line="50"/>
+        <source>State</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestnormalizer.cpp" line="66"/>
+        <source>Request validation error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestnormalizer.cpp" line="82"/>
+        <source>Request parsing error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="requestnormalizer.cpp" line="106"/>
+        <source>Open/close braces mismatch: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Summary::Summary</name>
+    <message>
+        <location filename="summary.cpp" line="86"/>
+        <source>Summary of downloads and recent events</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Summary::SummaryWidget</name>
+    <message>
+        <location filename="summarywidget.cpp" line="316"/>
+        <source>Remove this category</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SummaryWidget</name>
+    <message>
+        <location filename="summarywidget.ui" line="48"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="summarywidget.ui" line="56"/>
+        <source>String</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="summarywidget.ui" line="61"/>
+        <source>Wildcard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="summarywidget.ui" line="66"/>
+        <source>Regexp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="summarywidget.ui" line="71"/>
+        <source>Tags</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
