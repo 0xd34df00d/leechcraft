aea9911e67bf2cf8868a2a5399e226c1f614ad96
0xd34df00d <0xd34df00d@gmail.com>
2009-12-22 01:25:11 +0300

    PyLC: new plugin. Only base now.
    Python support for LeechCraft.

---
+++ src/plugins/pylc/pylc.cpp
@@ -0,0 +1,81 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2009  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "pylc.h"
+#include <QIcon>
+#include <plugininterface/util.h>
+
+namespace LeechCraft
+{
+	namespace Plugins
+	{
+		namespace PyLC
+		{
+			void Plugin::Init (ICoreProxy_ptr)
+			{
+				Translator_.reset (Util::InstallTranslator ("pylc"));
+			}
+
+			void Plugin::SecondInit ()
+			{
+			}
+
+			void Plugin::Release ()
+			{
+				Translator_.reset ();
+			}
+
+			QString Plugin::GetName () const
+			{
+				return "PyLC";
+			}
+
+			QString Plugin::GetInfo () const
+			{
+				return tr ("Python support for LeechCraft");
+			}
+
+			QIcon Plugin::GetIcon () const
+			{
+				return QIcon (":/resources/images/pylc.svg");
+			}
+
+			QStringList Plugin::Provides () const
+			{
+				return QStringList ();
+			}
+
+			QStringList Plugin::Needs () const
+			{
+				return QStringList ();
+			}
+
+			QStringList Plugin::Uses () const
+			{
+				return QStringList ();
+			}
+
+			void Plugin::SetProvider (QObject*, const QString&)
+			{
+			}
+		};
+	};
+};
+
+Q_EXPORT_PLUGIN2 (leechcraft_pylc, LeechCraft::Plugins::PyLC::Plugin);
+
