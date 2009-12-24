15e22e137f0439e4416e7a48306f62b297ca1e96
0xd34df00d <0xd34df00d@gmail.com>
2009-12-24 03:11:53 +0300

    PyLC: updated Russian translation.

--- src/plugins/pylc/leechcraft_pylc_ru_RU.ts
+++ src/plugins/pylc/leechcraft_pylc_ru_RU.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru_RU">
 <context>
-    <name>LeechCraft::Plugins::YASD::Plugin</name>
+    <name>LeechCraft::Plugins::PyLC::Plugin</name>
     <message>
-        <location filename="yasd.cpp" line="44"/>
-        <source>Yet Another Site Downloader</source>
-        <translation type="unfinished"></translation>
+        <location filename="pylc.cpp" line="50"/>
+        <source>Python support for LeechCraft</source>
+        <translation>Поддержка Python для LeechCraft</translation>
     </message>
 </context>
 </TS>
