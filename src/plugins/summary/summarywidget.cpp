acefe3a6d6c99b2db3b871e96b3de4e2cfbb817b
0xd34df00d <0xd34df00d@gmail.com>
2009-12-30 04:12:37 +0300

    Summary: fix actions with ActionIcon set but no icon.

--- src/plugins/summary/summarywidget.cpp
+++ src/plugins/summary/summarywidget.cpp
@@ -241,7 +241,16 @@ namespace LeechCraft
 
 					Toolbar_->clear ();
 					if (controls)
+					{
+						Q_FOREACH (QAction *action, controls->actions ())
+						{
+							QString ai = action->property ("ActionIcon").toString ();
+							if (! ai.isEmpty () &&
+									action->icon ().isNull ())
+								action->setIcon (Core::Instance ().GetProxy ()->GetIcon (ai));
+						}
 						Toolbar_->addActions (controls->actions ());
+					}
 					if (addiInfo)
 					{
 						Ui_.ControlsDockWidget_->setWidget (addiInfo);
