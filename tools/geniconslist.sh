6409bde102b32b7447d3f3f82cc227ca58b0c6a7
0xd34df00d <0xd34df00d@gmail.com>
2010-02-28 12:35:42 +0300

    Tools: updated the geniconslist.sh.

--- tools/geniconslist.sh
+++ tools/geniconslist.sh
@@ -1,8 +1,22 @@
 #! /bin/sh
 
+grcmd='grep -E "(ActionIcon|\<GetIcon)"'
+
 touch tmp
-grep ActionIcon * > tmp
-for i in `ls plugins`; do echo "List for $i:" >> tmp; grep ActionIcon plugins/$i/* | sed 's/.*\".*\".*\"\(.*\)\"/\1/' > tmp2; sort -u tmp2 >> tmp; done
+grep ActionIcon * | sed 's/.*\".*\".*\"\(.*\)\".*/\1/' > tmp2
+grep "GetIcon (\"" * | sed 's/.*GetIcon (\"\(.*\)\".*)/\1/' >> tmp2
+echo "List for Core:" >> tmp
+sort -u tmp2 >> tmp
+for i in `ls plugins`;
+	do
+		grep ActionIcon plugins/$i/* | sed 's/.*\".*\".*\"\(.*\)\".*/\1/' > tmp2
+		grep "GetIcon (\"" plugins/$i/* | sed 's/.*GetIcon (\"\(.*\)\".*)/\1/' >> tmp2
+#		if [ -n `cat $tmp2` ]
+#		then
+			echo "List for $i:" >> tmp
+			sort -u tmp2 >> tmp
+#		fi
+	done
 
 
 cat tmp
