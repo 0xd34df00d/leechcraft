e1049033647ed5759d1057ed3ee6044f4ebf25f8
0xd34df00d <0xd34df00d@gmail.com>
2010-03-11 22:37:14 +0300

    Summary: updated Russian translations.

--- src/plugins/summary/leechcraft_summary_ru_RU.ts
+++ src/plugins/summary/leechcraft_summary_ru_RU.ts
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru_RU">
 <context>
     <name>LeechCraft::Plugins::Summary::Core</name>
     <message>
         <location filename="core.cpp" line="121"/>
         <location filename="core.cpp" line="229"/>
         <source>Summary</source>
-        <translation type="unfinished"></translation>
+        <translation>Сводка</translation>
     </message>
 </context>
 <context>
@@ -15,17 +15,17 @@
     <message>
         <location filename="operationalmodel.cpp" line="30"/>
         <source>Entity</source>
-        <translation type="unfinished"></translation>
+        <translation>Сущность</translation>
     </message>
     <message>
         <location filename="operationalmodel.cpp" line="31"/>
         <source>Category</source>
-        <translation type="unfinished"></translation>
+        <translation>Категория</translation>
     </message>
     <message>
         <location filename="operationalmodel.cpp" line="32"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация</translation>
     </message>
 </context>
 <context>
@@ -33,32 +33,32 @@
     <message>
         <location filename="requestnormalizer.cpp" line="51"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Имя</translation>
     </message>
     <message>
         <location filename="requestnormalizer.cpp" line="52"/>
         <source>Status</source>
-        <translation type="unfinished"></translation>
+        <translation>Статус</translation>
     </message>
     <message>
         <location filename="requestnormalizer.cpp" line="53"/>
         <source>State</source>
-        <translation type="unfinished"></translation>
+        <translation>Состояние</translation>
     </message>
     <message>
         <location filename="requestnormalizer.cpp" line="69"/>
         <source>Request validation error: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка валидации запроса: %1</translation>
     </message>
     <message>
         <location filename="requestnormalizer.cpp" line="85"/>
         <source>Request parsing error: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка разбора запроса: %1</translation>
     </message>
     <message>
         <location filename="requestnormalizer.cpp" line="109"/>
         <source>Open/close braces mismatch: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Несоответствие открывающих и закрывающих скобок: %1</translation>
     </message>
 </context>
 <context>
@@ -66,7 +66,7 @@
     <message>
         <location filename="summary.cpp" line="88"/>
         <source>Summary of downloads and recent events</source>
-        <translation type="unfinished"></translation>
+        <translation>Сводка по закачкам и недавним событиям</translation>
     </message>
 </context>
 <context>
@@ -74,7 +74,7 @@
     <message>
         <location filename="summarywidget.cpp" line="334"/>
         <source>Remove this category</source>
-        <translation type="unfinished"></translation>
+        <translation>Удалить эту категорию</translation>
     </message>
 </context>
 <context>
@@ -82,27 +82,27 @@
     <message>
         <location filename="summarywidget.ui" line="51"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить</translation>
     </message>
     <message>
         <location filename="summarywidget.ui" line="59"/>
         <source>String</source>
-        <translation type="unfinished"></translation>
+        <translation>Строка</translation>
     </message>
     <message>
         <location filename="summarywidget.ui" line="64"/>
         <source>Wildcard</source>
-        <translation type="unfinished"></translation>
+        <translation>Маска</translation>
     </message>
     <message>
         <location filename="summarywidget.ui" line="69"/>
         <source>Regexp</source>
-        <translation type="unfinished"></translation>
+        <translation>Regexp</translation>
     </message>
     <message>
         <location filename="summarywidget.ui" line="74"/>
         <source>Tags</source>
-        <translation type="unfinished"></translation>
+        <translation>Теги</translation>
     </message>
 </context>
 </TS>
