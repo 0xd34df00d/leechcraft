b9e5a4d8be16a8ebabe7e5c82234a05645484ccc
Voker57 <voker57@gmail.com>
2010-03-18 22:49:35 +0300

    Chatter: Added userListChanged() signal

--- src/plugins/chatter/irc.cpp
+++ src/plugins/chatter/irc.cpp
@@ -43,6 +43,12 @@ IrcLayer::IrcLayer(QObject * parent, QString ircUri) : QObject(parent)
 	ircUseUri(ircUri);
 	connect(this, SIGNAL(gotKick(QHash<QString, QString>)), this, SLOT(checkKicked(QHash<QString, QString>)));
 	connect(this, SIGNAL(gotNames(QStringList)), this, SLOT(addNames(QStringList)));
+
+	connect(this, SIGNAL(gotPart()), SIGNAL(userListChanged()));
+	connect(this, SIGNAL(gotJoin()), SIGNAL(userListChanged()));
+	connect(this, SIGNAL(gotQuit()), SIGNAL(userListChanged()));
+	connect(this, SIGNAL(gotNick()), SIGNAL(userListChanged()));
+	connect(this, SIGNAL(gotKick()), SIGNAL(userListChanged()));
 //	connect(m_socket, SIGNAL(error()), this, SLOT(gotError()));
 }
 
