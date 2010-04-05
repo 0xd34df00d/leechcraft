81fe35a041fea52229dfa9327d4e1dd15721486d
0xd34df00d <0xd34df00d@gmail.com>
2010-04-05 11:43:59 +0400

    Tab++: Russian translations.

---
+++ src/plugins/tabpp/leechcraft_tabpp_ru_RU.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::Plugins::TabPP::Core</name>
+    <message>
+        <location filename="core.cpp" line="260"/>
+        <source>unknown</source>
+        <translation>неизвестно</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::TabPP::Plugin</name>
+    <message>
+        <location filename="tabpp.cpp" line="64"/>
+        <source>Enhances user experience with tabs.</source>
+        <translation>Улучшает взаимодействие пользователя с вкладками.</translation>
+    </message>
+    <message>
+        <location filename="tabpp.cpp" line="122"/>
+        <source>Show tab switcher</source>
+        <translation>Показать переключатель табов</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::TabPP::TabPPWidget</name>
+    <message>
+        <location filename="tabppwidget.cpp" line="44"/>
+        <source>F9</source>
+        <translation>F9</translation>
+    </message>
+</context>
+</TS>
