fae0f9a6706394ad9db50ab2fbb8d86d29a23edb
0xd34df00d <0xd34df00d@gmail.com>
2010-04-20 16:52:11 +0400

    Aggregator: added missing signals.

--- src/plugins/aggregator/aggregator.h
+++ src/plugins/aggregator/aggregator.h
@@ -131,6 +131,11 @@ namespace LeechCraft
 				void delegateEntity (const LeechCraft::DownloadEntity&,
 						int*, QObject**);
 				void bringToFront ();
+				void changeTabName (QWidget*, const QString&);
+				void changeTabIcon (QWidget*, const QIcon&);
+				void changeTooltip (QWidget*, QWidget*);
+				void statusBarChanged (QWidget*, const QString&);
+				void raiseTab (QWidget*);
 			};
 		};
 	};
