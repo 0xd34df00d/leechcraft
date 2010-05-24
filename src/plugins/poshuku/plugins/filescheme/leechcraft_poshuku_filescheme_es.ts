702423a72b62b59c529cf66a05d2569b21fa610b
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 23:00:14 +0400

    Poshuku FileScheme: ran lupdate over the rest of translations.

--- src/plugins/poshuku/plugins/filescheme/leechcraft_poshuku_filescheme_es.ts
+++ src/plugins/poshuku/plugins/filescheme/leechcraft_poshuku_filescheme_es.ts
@@ -2,41 +2,6 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="es">
 <context>
-    <name></name>
-    <message>
-        <source>Provides support for file:// scheme.</source>
-        <translation type="obsolete">Provee soporte para el esquema file://</translation>
-    </message>
-    <message>
-        <source>No error</source>
-        <translation type="obsolete">Sin errores</translation>
-    </message>
-    <message>
-        <source>%1: no such file or directory</source>
-        <translation type="obsolete">%1: no existe el archivo o directorio</translation>
-    </message>
-    <message>
-        <source>Unable to read %1</source>
-        <translation type="obsolete">Imposible leer %1</translation>
-    </message>
-    <message>
-        <source>Contents of %1</source>
-        <translation type="obsolete">Contenido de %1</translation>
-    </message>
-    <message>
-        <source>File</source>
-        <translation type="obsolete">Archivo</translation>
-    </message>
-    <message>
-        <source>Size</source>
-        <translation type="obsolete">Tamaño</translation>
-    </message>
-    <message>
-        <source>Modified</source>
-        <translation type="obsolete">Modificado</translation>
-    </message>
-</context>
-<context>
     <name>LeechCraft::Plugins::Poshuku::Plugins::FileScheme::FileScheme</name>
     <message>
         <location filename="filescheme.cpp" line="64"/>
