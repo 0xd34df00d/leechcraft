3ec8e587e8d4cc66e1e3dac0f20a04313c10342d
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:16:48 +0400

    HistoryHolder: added Occitan translations.

---
+++ src/plugins/historyholder/leechcraft_historyholder_oc.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="oc">
+<context>
+    <name>LeechCraft::Plugins::HistoryHolder::Core</name>
+    <message>
+        <location filename="core.cpp" line="67"/>
+        <source>Entity/location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="68"/>
+        <source>Date</source>
+        <translation>Data</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="69"/>
+        <source>Tags</source>
+        <translation>Etiquetas</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="84"/>
+        <source>Remove</source>
+        <translation>Suprimir</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="180"/>
+        <source>Binary data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::HistoryHolder::Plugin</name>
+    <message>
+        <location filename="historyholder.cpp" line="51"/>
+        <source>Holds history from various plugins</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
