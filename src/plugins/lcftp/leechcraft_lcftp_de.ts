0cda8d02ea05a08e8b636ee8395aacb1689e27e9
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:24:47 +0400

    LCFTP: added German translations.

--- src/plugins/lcftp/leechcraft_lcftp_de.ts
+++ src/plugins/lcftp/leechcraft_lcftp_de.ts
@@ -1,109 +1,96 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="de">
 <context>
     <name>LeechCraft::Plugins::LCFTP::Core</name>
     <message>
         <location filename="core.cpp" line="197"/>
         <source>Waiting</source>
-        <translation type="unfinished"></translation>
+        <translation>Warten</translation>
     </message>
     <message>
         <location filename="core.cpp" line="202"/>
         <source>Paused</source>
-        <translation type="unfinished"></translation>
+        <translation>Angehalten</translation>
     </message>
     <message>
         <location filename="core.cpp" line="204"/>
         <source>Downloading at %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Lade mit %1 herunter</translation>
     </message>
     <message>
         <location filename="core.cpp" line="207"/>
         <source>Uploading at %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Lade mit %1 hoch</translation>
     </message>
     <message>
         <location filename="core.cpp" line="220"/>
         <source>%1 of %2 (%3%)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="225"/>
-        <source>%1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 von %2 (%3%)</translation>
     </message>
     <message>
         <location filename="core.cpp" line="360"/>
         <source>%1 already exists. What do you want to do?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="366"/>
-        <source>Overwrite</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="368"/>
-        <source>Rename</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="370"/>
-        <source>Cancel</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="384"/>
-        <source>Error removing %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="398"/>
-        <source>Choose new file name</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="742"/>
-        <source>Pause</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="872"/>
-        <source>FTP transfer finished</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 existiert bereits. Was wollen Sie machen?</translation>
     </message>
     <message>
         <location filename="core.cpp" line="364"/>
         <location filename="core.cpp" line="750"/>
         <source>Resume</source>
-        <translation type="unfinished"></translation>
+        <translation>Fortsetzen</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="366"/>
+        <source>Overwrite</source>
+        <translation>Überschreiben</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="368"/>
+        <source>Rename</source>
+        <translation>Umbenennen</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="370"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="384"/>
+        <source>Error removing %1</source>
+        <translation>Fehler beim Entfernen von %1</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="398"/>
+        <source>Choose new file name</source>
+        <translation>Einen neuen Datenamen wählen</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="742"/>
+        <source>Pause</source>
+        <translation>Pause</translation>
     </message>
     <message>
         <location filename="core.cpp" line="758"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Entfernen</translation>
     </message>
     <message>
-        <location filename="core.cpp" line="359"/>
-        <location filename="core.cpp" line="383"/>
-        <location filename="core.cpp" line="858"/>
-        <location filename="core.cpp" line="900"/>
-        <source>LeechCraft</source>
-        <translation type="unfinished"></translation>
+        <location filename="core.cpp" line="859"/>
+        <source>FTP transfer finished: %1</source>
+        <translation>FTP Transfer beendet: %1</translation>
     </message>
     <message>
-        <location filename="core.cpp" line="901"/>
+        <location filename="core.cpp" line="883"/>
         <source>While mirroring&lt;br /&gt;%1&lt;br /&gt;to&lt;br /&gt;%2&lt;br /&gt;an error occured:&lt;br /&gt;%3&lt;br /&gt; already exists.</source>
-        <translation type="unfinished"></translation>
+        <translation>Beim Spiegeln von&lt;br /&gt;%1&lt;br /&gt;nach&lt;br /&gt;%2&lt;br /&gt;ist ein Fehler aufgetreten:&lt;br /&gt;%3&lt;br /&gt; existiert bereits.</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::Plugins::LCFTP::LCFTP</name>
     <message>
-        <location filename="lcftp.cpp" line="110"/>
+        <location filename="lcftp.cpp" line="106"/>
         <source>A simple FTP client</source>
-        <translation type="unfinished"></translation>
+        <translation>Ein einfacher FTP-Client</translation>
     </message>
 </context>
 <context>
@@ -111,37 +98,37 @@
     <message>
         <location filename="pane.cpp" line="54"/>
         <source>Transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Übertragen</translation>
     </message>
     <message>
         <location filename="pane.cpp" line="78"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Name</translation>
     </message>
     <message>
         <location filename="pane.cpp" line="79"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Größe</translation>
     </message>
     <message>
         <location filename="pane.cpp" line="80"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Typ</translation>
     </message>
     <message>
         <location filename="pane.cpp" line="81"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum</translation>
     </message>
     <message>
         <location filename="pane.cpp" line="239"/>
         <source>Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Verzeichnis</translation>
     </message>
     <message>
         <location filename="pane.cpp" line="240"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei</translation>
     </message>
 </context>
 <context>
@@ -150,63 +137,45 @@
         <location filename="worker.cpp" line="303"/>
         <location filename="worker.cpp" line="326"/>
         <source>Could not open file&lt;br /&gt;%1&lt;br /&gt;%2</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei &lt;br /&gt;%1&lt;br /&gt;%2 konnte nicht geöffnet werden</translation>
     </message>
 </context>
 <context>
     <name>Pane</name>
     <message>
-        <location filename="pane.ui" line="14"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="pane.ui" line="25"/>
         <source>..</source>
-        <translation type="unfinished"></translation>
+        <translation>..</translation>
     </message>
     <message>
         <location filename="pane.ui" line="32"/>
         <source>/</source>
-        <translation type="unfinished"></translation>
+        <translation>/</translation>
     </message>
 </context>
 <context>
     <name>SummaryTab</name>
     <message>
-        <location filename="summarytab.ui" line="14"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="summarytab.ui" line="25"/>
         <source>Download speed:</source>
-        <translation type="unfinished"></translation>
+        <translation>Geschwindigkeit (Herunterladen):</translation>
     </message>
     <message>
         <location filename="summarytab.ui" line="32"/>
         <source>Upload speed:</source>
-        <translation type="unfinished"></translation>
+        <translation>Geschwindigkeit (Hochladen):</translation>
     </message>
     <message>
         <location filename="summarytab.ui" line="39"/>
         <location filename="summarytab.ui" line="61"/>
         <source>Unlimited</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbegrenzt</translation>
     </message>
     <message>
         <location filename="summarytab.ui" line="42"/>
         <location filename="summarytab.ui" line="64"/>
         <source> KiB/s</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>TabWidget</name>
-    <message>
-        <location filename="tabwidget.ui" line="14"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation> KiB/s</translation>
     </message>
 </context>
 </TS>
