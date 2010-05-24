e220072fa8e3bc58db904051c52c901578dd9fc4
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 22:04:14 +0400

    Tab++: added German translations.

---
+++ src/plugins/tabpp/leechcraft_tabpp_de.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>LeechCraft::Plugins::TabPP::Core</name>
+    <message>
+        <location filename="core.cpp" line="260"/>
+        <source>unknown</source>
+        <translation>unbekannt</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::TabPP::Plugin</name>
+    <message>
+        <location filename="tabpp.cpp" line="66"/>
+        <source>Enhances user experience with tabs.</source>
+        <translation>Erweitert die Benutzererfahrung mit Reitern.</translation>
+    </message>
+    <message>
+        <location filename="tabpp.cpp" line="124"/>
+        <source>Show tab switcher</source>
+        <translation>Zeige Tab-Wechsler</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::TabPP::TabPPWidget</name>
+    <message>
+        <location filename="tabppwidget.cpp" line="44"/>
+        <source>F9</source>
+        <translation>F9</translation>
+    </message>
+</context>
+</TS>
