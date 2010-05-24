57eb7addb861b0ecb6e9cc4c1476691bf531a014
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 19:56:31 +0400

    Auscrie: added Spanish translations.

---
+++ src/plugins/auscrie/leechcraft_auscrie_es.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>LeechCraft::Plugins::Auscrie::Plugin</name>
+    <message>
+        <location filename="auscrie.cpp" line="48"/>
+        <source>Make a screenshot</source>
+        <translation>Tomar un Screenshot</translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="71"/>
+        <source>Simple auto screenshoter.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="135"/>
+        <source>Save as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="137"/>
+        <source>%1 files (*.%1);;All files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="176"/>
+        <source>Page parse failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="186"/>
+        <source>Image pasted: %1, the URL was copied to the clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="198"/>
+        <source>Upload of screenshot failed: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ShooterDialog</name>
+    <message>
+        <location filename="shooterdialog.ui" line="14"/>
+        <source>Make a screenshot</source>
+        <translation>Tomar un Screenshot</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="23"/>
+        <source>Action:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="31"/>
+        <source>upload to Imagebin.ca</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="36"/>
+        <source>save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="44"/>
+        <source>Timeout:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="51"/>
+        <source> s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="58"/>
+        <source>Format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="66"/>
+        <source>JPG</source>
+        <translation>JPG</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="71"/>
+        <source>PNG</source>
+        <translation>PNG</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="79"/>
+        <source>Quality:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
