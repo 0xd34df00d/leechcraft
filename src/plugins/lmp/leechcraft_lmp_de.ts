464503bb5d0d4b044a96f4307460492877feaa28
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:30:16 +0400

    LMP: added German translations, de_DE -> de.

---
+++ src/plugins/lmp/leechcraft_lmp_de.ts
@@ -0,0 +1,231 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>LeechCraft::Plugins::LMP::Core</name>
+    <message>
+        <location filename="core.cpp" line="38"/>
+        <source>Show LMP</source>
+        <translation>Zeige LMP</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LMP::Player</name>
+    <message>
+        <location filename="player.cpp" line="49"/>
+        <source>Source</source>
+        <translation>Quelle</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="50"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="113"/>
+        <source>No metadata</source>
+        <translation>Keine Metadaten</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="118"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="122"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="129"/>
+        <source>Audio CD</source>
+        <translation>Musik-CD</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="132"/>
+        <source>DVD</source>
+        <translation>DVD</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="135"/>
+        <source>Video CD</source>
+        <translation>Video-CD</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="138"/>
+        <source>Unknown disc type</source>
+        <translation>Unbekannter Mediumtyp</translation>
+    </message>
+    <message>
+        <location filename="player.cpp" line="144"/>
+        <source>Stream</source>
+        <translation>Stream</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LMP::PlayerWidget</name>
+    <message>
+        <location filename="playerwidget.cpp" line="213"/>
+        <source>Play</source>
+        <translation>Wiedergabe</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="223"/>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="233"/>
+        <source>Viewer settings</source>
+        <translation>Betrachter-Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="259"/>
+        <source>Toggle full screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="264"/>
+        <source>F</source>
+        <translation>F</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="271"/>
+        <source>Pause/resume</source>
+        <translation>Pause/Fortsetzen</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="277"/>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="278"/>
+        <source>R</source>
+        <translation>R</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="285"/>
+        <source>Volume up</source>
+        <translation>Lauter</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="299"/>
+        <source>Volume down</source>
+        <translation>Leiser</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="312"/>
+        <source>On-screen display</source>
+        <translation>On-Screen-Display</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="387"/>
+        <source>Initializing</source>
+        <translation>Initialisieren</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="390"/>
+        <source>Stopped</source>
+        <translation>Angehalten</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="393"/>
+        <source>Playing</source>
+        <translation>Wiedergabe</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="396"/>
+        <source>Buffering</source>
+        <translation>Puffern</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="399"/>
+        <source>Paused</source>
+        <translation>Angehalten</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="402"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="407"/>
+        <source> [</source>
+        <translation> [</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="413"/>
+        <source>/</source>
+        <translation>/</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="417"/>
+        <source>]</source>
+        <translation>]</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="419"/>
+        <source> from </source>
+        <translation> von </translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="427"/>
+        <source>nowhere</source>
+        <translation>nirgends</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="440"/>
+        <source> (CD)</source>
+        <translation> (CD)</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="443"/>
+        <source> (DVD)</source>
+        <translation> (DVD)</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="446"/>
+        <source> (VCD)</source>
+        <translation> (VCD)</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="449"/>
+        <source> (Unknown disc type)</source>
+        <translation> (Unbekannter Mediumtyp)</translation>
+    </message>
+    <message>
+        <location filename="playerwidget.cpp" line="454"/>
+        <source>stream</source>
+        <translation>Stream</translation>
+    </message>
+</context>
+<context>
+    <name>VideoSettings</name>
+    <message>
+        <location filename="videosettings.ui" line="13"/>
+        <source>Video settings</source>
+        <translation>Videoeinstellungen</translation>
+    </message>
+    <message>
+        <location filename="videosettings.ui" line="23"/>
+        <source>Brightness:</source>
+        <translation>Helligkeit:</translation>
+    </message>
+    <message>
+        <location filename="videosettings.ui" line="63"/>
+        <source>Contrast:</source>
+        <translation>Kontrast:</translation>
+    </message>
+    <message>
+        <location filename="videosettings.ui" line="103"/>
+        <source>Hue:</source>
+        <translation>Farbton:</translation>
+    </message>
+    <message>
+        <location filename="videosettings.ui" line="143"/>
+        <source>Saturation:</source>
+        <translation>Sättigung:</translation>
+    </message>
+</context>
+</TS>
