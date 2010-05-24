65e0a6ff498f259ad76641a99c8246ecba123c21
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:06:55 +0400

    DeadLyrics: added Spanish translations.

---
+++ src/plugins/deadlyrics/leechcraft_deadlyrics_es.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::Core</name>
+    <message>
+        <location filename="core.cpp" line="73"/>
+        <location filename="core.cpp" line="78"/>
+        <source>lyrics</source>
+        <translation>letras</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::DeadLyRicS</name>
+    <message>
+        <location filename="deadlyrics.cpp" line="56"/>
+        <source>Lyrics Searcher</source>
+        <translation>Buscador de Letras</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::FindProxy</name>
+    <message>
+        <location filename="findproxy.cpp" line="151"/>
+        <source>Searching...</source>
+        <translation>Buscando...</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="searcher.cpp" line="60"/>
+        <source>Unknown %1 %2</source>
+        <translation>Desconocido %1 %2</translation>
+    </message>
+</context>
+</TS>
