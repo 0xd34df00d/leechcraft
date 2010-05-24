1e814f7550f9ce33812f286d09d260e8b05b6287
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:38:05 +0400

    NetworkMonitor: added Spanish translations.

---
+++ src/plugins/networkmonitor/leechcraft_networkmonitor_es.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>LeechCraft::Plugins::NetworkMonitor::HeaderModel</name>
+    <message>
+        <location filename="headermodel.cpp" line="26"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="headermodel.cpp" line="27"/>
+        <source>Value</source>
+        <translation>Valor</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::NetworkMonitor::Plugin</name>
+    <message>
+        <location filename="networkmonitor.cpp" line="79"/>
+        <source>Network monitor...</source>
+        <translation>Monitor de red...</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.cpp" line="105"/>
+        <source>Monitors HTTP network requests and responses.</source>
+        <translation>Monitorea las peticiones y respuestas de la red HTTP.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::NetworkMonitor::RequestModel</name>
+    <message>
+        <location filename="requestmodel.cpp" line="35"/>
+        <source>Date started</source>
+        <translation>Fecha de inicio</translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="36"/>
+        <source>Date finished</source>
+        <translation>Fecha de finalización</translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="37"/>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="38"/>
+        <source>Host</source>
+        <translation>Host</translation>
+    </message>
+    <message>
+        <location filename="requestmodel.cpp" line="88"/>
+        <source>In progress</source>
+        <translation>En progreso</translation>
+    </message>
+</context>
+<context>
+    <name>NetworkMonitor</name>
+    <message>
+        <location filename="networkmonitor.ui" line="14"/>
+        <source>Network Monitor</source>
+        <translation>Monitor de Red</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="20"/>
+        <source>Clear finished items</source>
+        <translation>Limpiar elementos finalizados</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="36"/>
+        <source>String</source>
+        <translation>Cadena</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="41"/>
+        <source>Wildcard</source>
+        <translation>Comodín</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="46"/>
+        <source>Regexp</source>
+        <translation>Expresión Regular</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="58"/>
+        <source>Requests</source>
+        <translation>Peticiones</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="74"/>
+        <source>Request headers</source>
+        <translation>Encabezados de petición</translation>
+    </message>
+    <message>
+        <location filename="networkmonitor.ui" line="92"/>
+        <source>Response headers</source>
+        <translation>Encabezado de respuesta</translation>
+    </message>
+</context>
+</TS>
