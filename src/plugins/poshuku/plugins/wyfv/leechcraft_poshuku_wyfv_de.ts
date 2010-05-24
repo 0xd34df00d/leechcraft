e2dc985d59dce163d4ce7fa9c7757b23f8ee115d
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 22:28:43 +0400

    Poshuku WYFV: added German translations.

---
+++ src/plugins/poshuku/plugins/wyfv/leechcraft_poshuku_wyfv_de.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name></name>
+    <message>
+        <source>LeechCraft</source>
+        <comment>LeechCraft::Plugins::Poshuku::Plugins::WYFV::Player#1</comment>
+        <translation type="obsolete">LeechCraft</translation>
+    </message>
+    <message>
+        <source>Failed to load</source>
+        <translation type="obsolete">Laden fehlgeschlagen</translation>
+    </message>
+    <message>
+        <source>Related videos</source>
+        <translation type="obsolete">Ähnliche Videos</translation>
+    </message>
+    <message>
+        <source>Replaces Flash-based video player to play video without Flash installed.</source>
+        <translation type="obsolete">Ersetzt Flash-basiertes Videoplugin, um Videos ohne installiertes Flash abzuspielen.</translation>
+    </message>
+    <message>
+        <source>LeechCraft</source>
+        <comment>LeechCraft::Plugins::Poshuku::Plugins::WYFV::YoutubePlayer#1</comment>
+        <translation type="obsolete">LeechCraft</translation>
+    </message>
+    <message>
+        <source>This format is unavailable, please select another one.</source>
+        <translation type="obsolete">Dieses Format ist nicht verfügbar. Bitte wählen Sie ein anderes.</translation>
+    </message>
+    <message>
+        <source>Related...</source>
+        <translation type="obsolete">Ähnlich…</translation>
+    </message>
+    <message>
+        <source>%v</source>
+        <translation type="obsolete">%v</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Poshuku::Plugins::WYFV::RelatedItem</name>
+    <message>
+        <location filename="relateditem.cpp" line="108"/>
+        <source>Failed to load</source>
+        <translation>Laden fehlgeschlagen</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Poshuku::Plugins::WYFV::RelatedWidget</name>
+    <message>
+        <location filename="relatedwidget.cpp" line="52"/>
+        <source>Related videos</source>
+        <translation>Ähnliche Videos</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Poshuku::Plugins::WYFV::WYFV</name>
+    <message>
+        <location filename="wyfv.cpp" line="66"/>
+        <source>Replaces Flash-based video player to play video without Flash installed.</source>
+        <translation>Ersetzt Flash-basiertes Videoplugin, um Videos ohne installiertes Flash abzuspielen.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Poshuku::Plugins::WYFV::YoutubePlayer</name>
+    <message>
+        <location filename="youtubeplayer.cpp" line="221"/>
+        <source>This format is unavailable, please select another one.</source>
+        <translation>Dieses Format ist nicht verfügbar. Bitte wählen Sie ein anderes.</translation>
+    </message>
+</context>
+<context>
+    <name>Player</name>
+    <message>
+        <location filename="player.ui" line="28"/>
+        <source>Related...</source>
+        <translation>Ähnlich…</translation>
+    </message>
+</context>
+<context>
+    <name>RelatedItem</name>
+    <message>
+        <location filename="relateditem.ui" line="130"/>
+        <source>%v</source>
+        <translation>%v</translation>
+    </message>
+</context>
+</TS>
