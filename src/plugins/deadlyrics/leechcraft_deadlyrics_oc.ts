c88a80d8eda159f680059e16fd948497a2e30f26
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:09:37 +0400

    DeadLyrics: added Occitan translations.

---
+++ src/plugins/deadlyrics/leechcraft_deadlyrics_oc.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="oc">
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::Core</name>
+    <message>
+        <location filename="core.cpp" line="73"/>
+        <location filename="core.cpp" line="78"/>
+        <source>lyrics</source>
+        <translation>paraulas</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::DeadLyRicS</name>
+    <message>
+        <location filename="deadlyrics.cpp" line="56"/>
+        <source>Lyrics Searcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::DeadLyrics::FindProxy</name>
+    <message>
+        <location filename="findproxy.cpp" line="151"/>
+        <source>Searching...</source>
+        <translation>Recèrca en cors...</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="searcher.cpp" line="60"/>
+        <source>Unknown %1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
