d84914d492e4815f314c33322652aae5a510349a
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 19:55:30 +0400

    Auscrie: added German translations.

---
+++ src/plugins/auscrie/leechcraft_auscrie_de.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>LeechCraft::Plugins::Auscrie::Plugin</name>
+    <message>
+        <location filename="auscrie.cpp" line="48"/>
+        <source>Make a screenshot</source>
+        <translation>Erstelle einen Screenshot</translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="71"/>
+        <source>Simple auto screenshoter.</source>
+        <translation>Einfacher automatischer Screenshotersteller.</translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="135"/>
+        <source>Save as</source>
+        <translation>Speichern als</translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="137"/>
+        <source>%1 files (*.%1);;All files (*.*)</source>
+        <translation>%1 Dateien (*.%1);;Alle Dateien (*.*)</translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="176"/>
+        <source>Page parse failed</source>
+        <translation>Seitenanalysierung fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="186"/>
+        <source>Image pasted: %1, the URL was copied to the clipboard</source>
+        <translation>Bild eingefügt: %1, die URL wurde in die Zwischenablage kopiert</translation>
+    </message>
+    <message>
+        <location filename="auscrie.cpp" line="198"/>
+        <source>Upload of screenshot failed: %1</source>
+        <translation>Upload von Screenshot fehlgeschlagen: %1</translation>
+    </message>
+</context>
+<context>
+    <name>ShooterDialog</name>
+    <message>
+        <location filename="shooterdialog.ui" line="14"/>
+        <source>Make a screenshot</source>
+        <translation>Erstelle einen Screenshot</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="23"/>
+        <source>Action:</source>
+        <translation>Aktion:</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="31"/>
+        <source>upload to Imagebin.ca</source>
+        <translation>lade zu Imagebin.ca hoch</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="36"/>
+        <source>save</source>
+        <translation>speichern</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="44"/>
+        <source>Timeout:</source>
+        <translation>Zeitüberschreitung:</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="51"/>
+        <source> s</source>
+        <translation> s</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="58"/>
+        <source>Format:</source>
+        <translation>Format:</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="66"/>
+        <source>JPG</source>
+        <translation>JPG</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="71"/>
+        <source>PNG</source>
+        <translation>PNG</translation>
+    </message>
+    <message>
+        <location filename="shooterdialog.ui" line="79"/>
+        <source>Quality:</source>
+        <translation>Qualität:</translation>
+    </message>
+</context>
+</TS>
