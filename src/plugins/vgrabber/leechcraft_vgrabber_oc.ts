7cf633f4faf1861f9254f1c56e6ee7c48568985d
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 22:09:29 +0400

    vGrabber: added Occitan translations.

---
+++ src/plugins/vgrabber/leechcraft_vgrabber_oc.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="oc">
+<context>
+    <name>CategoriesSelector</name>
+    <message>
+        <location filename="categoriesselector.ui" line="33"/>
+        <source>Categories</source>
+        <translation>Categorias</translation>
+    </message>
+    <message>
+        <location filename="categoriesselector.ui" line="43"/>
+        <source>Add...</source>
+        <translation>Apondre...</translation>
+    </message>
+    <message>
+        <location filename="categoriesselector.ui" line="50"/>
+        <source>Modify...</source>
+        <translation>Modificar...</translation>
+    </message>
+    <message>
+        <location filename="categoriesselector.ui" line="57"/>
+        <source>Remove</source>
+        <translation>Suprimir</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::vGrabber::AudioFindProxy</name>
+    <message>
+        <location filename="audiofindproxy.cpp" line="53"/>
+        <source>Searching for %1...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="audiofindproxy.cpp" line="72"/>
+        <source>Audio vkontakte.ru</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="audiofindproxy.cpp" line="242"/>
+        <source>Nothing found for %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::vGrabber::CategoriesSelector</name>
+    <message>
+        <location filename="categoriesselector.cpp" line="166"/>
+        <source>Add category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="categoriesselector.cpp" line="183"/>
+        <source>Modify category</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::vGrabber::FindProxy</name>
+    <message>
+        <location filename="findproxy.cpp" line="41"/>
+        <source>Download</source>
+        <translation>Telecargar</translation>
+    </message>
+    <message>
+        <location filename="findproxy.cpp" line="48"/>
+        <source>Handle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="findproxy.cpp" line="55"/>
+        <source>Copy URL to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="findproxy.cpp" line="63"/>
+        <source>vGrabber menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="findproxy.cpp" line="80"/>
+        <source>Empty search string</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="findproxy.cpp" line="101"/>
+        <source>Job for request&lt;br /&gt;%1&lt;br /&gt;wasn&apos;t delegated.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="findproxy.cpp" line="181"/>
+        <source>Could not open file %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="findproxy.cpp" line="197"/>
+        <source>Search request for URL&lt;br /&gt;%1&lt;br /&gt;was delegated, but it failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::vGrabber::VideoFindProxy</name>
+    <message>
+        <location filename="videofindproxy.cpp" line="34"/>
+        <source>Searching for %1...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="videofindproxy.cpp" line="53"/>
+        <source>Video vkontakte.ru</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="videofindproxy.cpp" line="66"/>
+        <source>Video</source>
+        <translation>Vidèo</translation>
+    </message>
+    <message>
+        <location filename="videofindproxy.cpp" line="184"/>
+        <source>Nothing found for %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="videofindproxy.cpp" line="280"/>
+        <source>Job for request&lt;br /&gt;%1&lt;br /&gt;wasn&apos;t delegated.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::vGrabber::vGrabber</name>
+    <message>
+        <location filename="vgrabber.cpp" line="83"/>
+        <source>vkontakte.ru audio/video grabber.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
