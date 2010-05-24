4755188b9cbf42fcd3276c46dc561b3cf3392722
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 22:04:59 +0400

    Tab++: added Occitan translations.

---
+++ src/plugins/tabpp/leechcraft_tabpp_oc.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="oc">
+<context>
+    <name></name>
+    <message>
+        <source>unknown</source>
+        <translation type="obsolete">desconegut</translation>
+    </message>
+    <message>
+        <source>F9</source>
+        <translation type="obsolete">F9</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::TabPP::Core</name>
+    <message>
+        <location filename="core.cpp" line="260"/>
+        <source>unknown</source>
+        <translation>desconegut</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::TabPP::Plugin</name>
+    <message>
+        <location filename="tabpp.cpp" line="66"/>
+        <source>Enhances user experience with tabs.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tabpp.cpp" line="124"/>
+        <source>Show tab switcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::TabPP::TabPPWidget</name>
+    <message>
+        <location filename="tabppwidget.cpp" line="44"/>
+        <source>F9</source>
+        <translation>F9</translation>
+    </message>
+</context>
+</TS>
