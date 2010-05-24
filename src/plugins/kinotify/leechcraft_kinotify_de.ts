fc28239073ab420372388fc051a625714d75f4f6
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:20:55 +0400

    Kinotify: added German and Russian translations.

---
+++ src/plugins/kinotify/leechcraft_kinotify_de.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>LeechCraft::Plugins::Kinotify::Plugin</name>
+    <message>
+        <location filename="kinotify.cpp" line="51"/>
+        <source>Fancy Kinetic notifications.</source>
+        <translation>Raffinierte kinetische Benachrichtigungen.</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/kinotify/leechcraft_kinotify_ru_RU.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::Plugins::Kinotify::Plugin</name>
+    <message>
+        <location filename="kinotify.cpp" line="51"/>
+        <source>Fancy Kinetic notifications.</source>
+        <translation>Красивые Kinetic-уведомления.</translation>
+    </message>
+</context>
+</TS>
