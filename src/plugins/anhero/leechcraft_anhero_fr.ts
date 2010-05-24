9303843b28b477b4fe4a9b49a64a69582ce43330
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 19:51:35 +0400

    AnHero: added French translations.

---
+++ src/plugins/anhero/leechcraft_anhero_fr.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
+<context>
+    <name>LeechCraft::Plugins::AnHero::Plugin</name>
+    <message>
+        <location filename="anhero.cpp" line="60"/>
+        <source>Crash handler</source>
+        <translation>Gestionnaire de crash</translation>
+    </message>
+</context>
+</TS>
