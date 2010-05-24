0cb05a3b5cf397ea6492429a4f1ddc5b646d9d63
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 19:47:55 +0400

    AnHero: added German translations.

---
+++ src/plugins/anhero/leechcraft_anhero_de.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+    <extra-po-header-po_revision_date>2010-03-25 21:00+0000</extra-po-header-po_revision_date>
+    <extra-po-header-x_launchpad_export_date>2010-05-24 12:12+0000</extra-po-header-x_launchpad_export_date>
+    <extra-po-headers>Project-Id-Version,Report-Msgid-Bugs-To,POT-Creation-Date,PO-Revision-Date,Last-Translator,Language-Team,MIME-Version,Content-Type,Content-Transfer-Encoding,X-Launchpad-Export-Date,X-Generator</extra-po-headers>
+    <extra-po-header-x_generator>Launchpad (build Unknown)</extra-po-header-x_generator>
+    <extra-po-header-report_msgid_bugs_to>FULL NAME &lt;EMAIL@ADDRESS&gt;</extra-po-header-report_msgid_bugs_to>
+    <extra-po-header-language_team>German &lt;de@li.org&gt;</extra-po-header-language_team>
+    <extra-po-header-project_id_version>leechcraft</extra-po-header-project_id_version>
+    <extra-po-header-pot_creation_date>2010-03-12 01:19+0400</extra-po-header-pot_creation_date>
+    <extra-po-header_comment># German translation for leechcraft
+# Copyright (c) 2010 Rosetta Contributors and Canonical Ltd 2010
+# This file is distributed under the same license as the leechcraft package.
+# FIRST AUTHOR &lt;EMAIL@ADDRESS&gt;, 2010.
+#</extra-po-header_comment>
+    <extra-po-header-last_translator>lineak &lt;lineak@live.at&gt;</extra-po-header-last_translator>
+<context>
+    <name></name>
+    <message>
+        <location filename="LeechCraft::Plugins::AnHero:" line="0"/>
+        <source>Crash handler</source>
+        <translation>Absturzverwalter</translation>
+    </message>
+</context>
+</TS>
