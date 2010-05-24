39223512a1427628acaa06654eaf42f28742c37b
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 23:00:00 +0400

    Poshuku FUA: added German translations, de_DE -> de.

---
+++ src/plugins/poshuku/plugins/fua/leechcraft_poshuku_fua_de.ts
@@ -0,0 +1,153 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name></name>
+    <message>
+        <source>Fake user agent</source>
+        <translation type="obsolete">Gefälschter User Agent</translation>
+    </message>
+    <message>
+        <source>Domain:</source>
+        <translation type="obsolete">Domäne:</translation>
+    </message>
+    <message>
+        <source>Agent:</source>
+        <translation type="obsolete">Agent:</translation>
+    </message>
+    <message>
+        <source>ID string:</source>
+        <translation type="obsolete">ID:</translation>
+    </message>
+    <message>
+        <source>%Platform% and %Subplatform% are expanded to the windowing system and the operation system.
+%Security% is expanded to U if SSL is enabled, otherwise N.
+%Locale% is replaced with current locale name.
+%WebKitVersion% is expanded to WebKit version.
+%AppVersion% is expanded to the application&apos;s version.</source>
+        <translation type="obsolete">%Platform% und %Subplatform% werden durch das Fenstersystem und das Betriebssystem ersetzt.
+%Security% wird durch U ersetzt, wenn SSL aktiviert ist, ansonsten durch N.
+%Locale% wird durch den Namen des aktuellen Einstellungssatzes für Gebietsschemaparameter ersetzt.
+%WebKitVersion% wird durch die WebKit-Version ersetzt.
+%AppVersion% wird durch die Version der Anwendung ersetzt.</translation>
+    </message>
+    <message>
+        <source>Domain</source>
+        <translation type="obsolete">Domain</translation>
+    </message>
+    <message>
+        <source>Agent</source>
+        <translation type="obsolete">Agent</translation>
+    </message>
+    <message>
+        <source>Identification string</source>
+        <translation type="obsolete">Identifikations Nummer</translation>
+    </message>
+    <message>
+        <source>Allows to set fake user agents for different sites.</source>
+        <translation type="obsolete">Erlaubt die Einstellung gefälschter User Agents für verschiedene Websites.</translation>
+    </message>
+    <message>
+        <source>Form</source>
+        <translation type="obsolete">Formular</translation>
+    </message>
+    <message>
+        <source>Add...</source>
+        <translation type="obsolete">Hinzufügen...</translation>
+    </message>
+    <message>
+        <source>Modify...</source>
+        <translation type="obsolete">Bearbeiten...</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="obsolete">Entfernen</translation>
+    </message>
+    <message>
+        <source>Changes would be applied immediately</source>
+        <translation type="obsolete">Änderungen werden sofort übernommen</translation>
+    </message>
+</context>
+<context>
+    <name>Changer</name>
+    <message>
+        <location filename="changer.ui" line="14"/>
+        <source>Fake user agent</source>
+        <translation>Gefälschter User Agent</translation>
+    </message>
+    <message>
+        <location filename="changer.ui" line="25"/>
+        <source>Domain:</source>
+        <translation>Domäne:</translation>
+    </message>
+    <message>
+        <location filename="changer.ui" line="35"/>
+        <source>Agent:</source>
+        <translation>Agent:</translation>
+    </message>
+    <message>
+        <location filename="changer.ui" line="45"/>
+        <source>ID string:</source>
+        <translation>ID:</translation>
+    </message>
+    <message>
+        <location filename="changer.ui" line="57"/>
+        <source>%Platform% and %Subplatform% are expanded to the windowing system and the operation system.
+%Security% is expanded to U if SSL is enabled, otherwise N.
+%Locale% is replaced with current locale name.
+%WebKitVersion% is expanded to WebKit version.
+%AppVersion% is expanded to the application&apos;s version.</source>
+        <translation>%Platform% und %Subplatform% werden durch das Fenstersystem und das Betriebssystem ersetzt.
+%Security% wird durch U ersetzt, wenn SSL aktiviert ist, ansonsten durch N.
+%Locale% wird durch den Namen des aktuellen Einstellungssatzes für Gebietsschemaparameter ersetzt.
+%WebKitVersion% wird durch die WebKit-Version ersetzt.
+%AppVersion% wird durch die Version der Anwendung ersetzt.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Poshuku::Plugins::Fua::FUA</name>
+    <message>
+        <location filename="fua.cpp" line="60"/>
+        <source>Domain</source>
+        <translation>Domain</translation>
+    </message>
+    <message>
+        <location filename="fua.cpp" line="61"/>
+        <source>Agent</source>
+        <translation>Agent</translation>
+    </message>
+    <message>
+        <location filename="fua.cpp" line="62"/>
+        <source>Identification string</source>
+        <translation>Identifikations Nummer</translation>
+    </message>
+    <message>
+        <location filename="fua.cpp" line="101"/>
+        <source>Allows to set fake user agents for different sites.</source>
+        <translation>Erlaubt die Einstellung gefälschter User Agents für verschiedene Websites.</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="settings.ui" line="27"/>
+        <source>Add...</source>
+        <translation>Hinzufügen...</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="34"/>
+        <source>Modify...</source>
+        <translation>Bearbeiten...</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="41"/>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="65"/>
+        <source>Changes would be applied immediately</source>
+        <translation>Änderungen werden sofort übernommen</translation>
+    </message>
+</context>
+</TS>
