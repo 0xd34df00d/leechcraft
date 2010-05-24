75dba47ae5b768e7c547746ce95ce1c541328fb0
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:26:45 +0400

    LCFTP: added Occitan translations.

---
+++ src/plugins/lcftp/leechcraft_lcftp_oc.ts
@@ -0,0 +1,181 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="oc">
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Core</name>
+    <message>
+        <location filename="core.cpp" line="197"/>
+        <source>Waiting</source>
+        <translation>En espèra</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="202"/>
+        <source>Paused</source>
+        <translation>En pausa</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="204"/>
+        <source>Downloading at %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="207"/>
+        <source>Uploading at %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="220"/>
+        <source>%1 of %2 (%3%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="360"/>
+        <source>%1 already exists. What do you want to do?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="364"/>
+        <location filename="core.cpp" line="750"/>
+        <source>Resume</source>
+        <translation>Contunhar</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="366"/>
+        <source>Overwrite</source>
+        <translation>Espotir</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="368"/>
+        <source>Rename</source>
+        <translation>Tornar nomenar</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="370"/>
+        <source>Cancel</source>
+        <translation>Anullar</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="384"/>
+        <source>Error removing %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="398"/>
+        <source>Choose new file name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="742"/>
+        <source>Pause</source>
+        <translation>Pausa</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="758"/>
+        <source>Delete</source>
+        <translation>Suprimir</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="859"/>
+        <source>FTP transfer finished: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="883"/>
+        <source>While mirroring&lt;br /&gt;%1&lt;br /&gt;to&lt;br /&gt;%2&lt;br /&gt;an error occured:&lt;br /&gt;%3&lt;br /&gt; already exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::LCFTP</name>
+    <message>
+        <location filename="lcftp.cpp" line="106"/>
+        <source>A simple FTP client</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Pane</name>
+    <message>
+        <location filename="pane.cpp" line="54"/>
+        <source>Transfer</source>
+        <translation>Transferiment</translation>
+    </message>
+    <message>
+        <location filename="pane.cpp" line="78"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="pane.cpp" line="79"/>
+        <source>Size</source>
+        <translation>Talha</translation>
+    </message>
+    <message>
+        <location filename="pane.cpp" line="80"/>
+        <source>Type</source>
+        <translation>Tipe</translation>
+    </message>
+    <message>
+        <location filename="pane.cpp" line="81"/>
+        <source>Date</source>
+        <translation>Data</translation>
+    </message>
+    <message>
+        <location filename="pane.cpp" line="239"/>
+        <source>Directory</source>
+        <translation>Dorsièr</translation>
+    </message>
+    <message>
+        <location filename="pane.cpp" line="240"/>
+        <source>File</source>
+        <translation>Fichièr</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::LCFTP::Worker</name>
+    <message>
+        <location filename="worker.cpp" line="303"/>
+        <location filename="worker.cpp" line="326"/>
+        <source>Could not open file&lt;br /&gt;%1&lt;br /&gt;%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Pane</name>
+    <message>
+        <location filename="pane.ui" line="25"/>
+        <source>..</source>
+        <translation>..</translation>
+    </message>
+    <message>
+        <location filename="pane.ui" line="32"/>
+        <source>/</source>
+        <translation>/</translation>
+    </message>
+</context>
+<context>
+    <name>SummaryTab</name>
+    <message>
+        <location filename="summarytab.ui" line="25"/>
+        <source>Download speed:</source>
+        <translation>Velocitat de telecargament :</translation>
+    </message>
+    <message>
+        <location filename="summarytab.ui" line="32"/>
+        <source>Upload speed:</source>
+        <translation>Velocitat d&apos;emission :</translation>
+    </message>
+    <message>
+        <location filename="summarytab.ui" line="39"/>
+        <location filename="summarytab.ui" line="61"/>
+        <source>Unlimited</source>
+        <translation>Illimitat</translation>
+    </message>
+    <message>
+        <location filename="summarytab.ui" line="42"/>
+        <location filename="summarytab.ui" line="64"/>
+        <source> KiB/s</source>
+        <translation> Kio/s</translation>
+    </message>
+</context>
+</TS>
