99d98ae1ac11d77d34ab38e7848c5dd10a38a66b
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 21:39:57 +0400

    New Life: added German translations.

---
+++ src/plugins/newlife/leechcraft_newlife_de.ts
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>FeedsSettingsImportPage</name>
+    <message>
+        <location filename="feedssettingsimportpage.ui" line="14"/>
+        <source>WizardPage</source>
+        <translation>Assistenten Seite</translation>
+    </message>
+    <message>
+        <location filename="feedssettingsimportpage.ui" line="22"/>
+        <source>Location:</source>
+        <translation>Ort:</translation>
+    </message>
+    <message>
+        <location filename="feedssettingsimportpage.ui" line="34"/>
+        <source>Browse...</source>
+        <translation>Auswählen...</translation>
+    </message>
+    <message>
+        <location filename="feedssettingsimportpage.ui" line="45"/>
+        <source>Import %1 settings as well</source>
+        <extracomment>Application&apos;s name will be placed instead of %1, so please leave it where appropriate.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="firstpage.ui" line="14"/>
+        <source>WizardPage</source>
+        <translation>Assistenten Seite</translation>
+    </message>
+    <message>
+        <location filename="firstpage.ui" line="17"/>
+        <source>Choose application</source>
+        <translation>Wähle Anwendung</translation>
+    </message>
+    <message>
+        <location filename="firstpage.ui" line="20"/>
+        <source>Choose appication that you want your data to be imported from</source>
+        <translation>Wählen Sie die Anwendung, aus der Sie Ihre Daten importieren möchten</translation>
+    </message>
+    <message>
+        <location filename="firstpage.ui" line="28"/>
+        <source>Source application:</source>
+        <translation>Quellanwendung:</translation>
+    </message>
+</context>
+<context>
+    <name>ImportWizard</name>
+    <message>
+        <location filename="importwizard.ui" line="14"/>
+        <source>Wizard</source>
+        <translation>Assistent</translation>
+    </message>
+    <message>
+        <location filename="importwizard.ui" line="35"/>
+        <source>Now please prepare for the import of data from the selected application.</source>
+        <translation>Bereiten Sie jetzt bitte den Import von Daten aus der ausgewählten Anwendung vor.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::NewLife::AkregatorImportPage</name>
+    <message>
+        <location filename="akregatorimportpage.cpp" line="40"/>
+        <source>Akregator&apos;s feeds import</source>
+        <translation>Akregator&apos;s Feeds Import</translation>
+    </message>
+    <message>
+        <location filename="akregatorimportpage.cpp" line="41"/>
+        <source>Select Akregator&apos;s feeds file and options</source>
+        <translation>Wähle Akregator&apos;s Feed Datei und Optionen</translation>
+    </message>
+    <message>
+        <location filename="akregatorimportpage.cpp" line="103"/>
+        <source>Select Akregator&apos;s OPML file</source>
+        <translation>Wähle Akregator&apos;s OPML Datei</translation>
+    </message>
+    <message>
+        <location filename="akregatorimportpage.cpp" line="105"/>
+        <source>OPML files (*.opml *.xml);;All files (*.*)</source>
+        <translation>OPML Dateien (*.opml *.xml);;Alle Dateien (*.*)</translation>
+    </message>
+    <message>
+        <location filename="akregatorimportpage.cpp" line="112"/>
+        <source>The file you&apos;ve selected is not a valid OPML file.</source>
+        <translation>Die ausgewählte Datei ist keine gültige OPML-Datei.</translation>
+    </message>
+    <message>
+        <location filename="akregatorimportpage.cpp" line="155"/>
+        <source>Akregator settings</source>
+        <translation>Akregator Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="akregatorimportpage.cpp" line="160"/>
+        <source>Could not access or parse Akregator settings.</source>
+        <translation>Zugriff oder Analyse der Akregator-Einstellungen fehlgeschlagen.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::NewLife::KTorrentImportPage</name>
+    <message>
+        <location filename="ktorrentimportpage.cpp" line="41"/>
+        <source>KTorrent&apos;s torrents import</source>
+        <translation>KTorrent&apos;s Torrents Import</translation>
+    </message>
+    <message>
+        <location filename="ktorrentimportpage.cpp" line="42"/>
+        <source>Select KTorrent&apos;s torrents</source>
+        <translation>Wähle KTorrent&apos;s Torrents</translation>
+    </message>
+    <message>
+        <location filename="ktorrentimportpage.cpp" line="85"/>
+        <source>Select KTorrent&apos;s configuration file</source>
+        <translation>Wähle KTorrents Konfigurationsdatei</translation>
+    </message>
+    <message>
+        <location filename="ktorrentimportpage.cpp" line="95"/>
+        <source>Can&apos;t read KTorrent&apos;s configuration file</source>
+        <translation>Kann KTorrents Konfigurationsdatei nicht lesen</translation>
+    </message>
+    <message>
+        <location filename="ktorrentimportpage.cpp" line="182"/>
+        <source>KTorrent settings</source>
+        <translation>KTorrent Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="ktorrentimportpage.cpp" line="187"/>
+        <source>Could not access or parse KTorrent settings.</source>
+        <translation>Zugriff oder Analyse der KTorrent-Einstellungen fehlgeschlagen.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::NewLife::LifereaImportPage</name>
+    <message>
+        <location filename="lifereaimportpage.cpp" line="40"/>
+        <source>Liferea&apos;s feeds import</source>
+        <translation>Lifereas Feeds Import</translation>
+    </message>
+    <message>
+        <location filename="lifereaimportpage.cpp" line="41"/>
+        <source>Select Liferea&apos;s base location and options</source>
+        <translation>Wählen Sie Stammverzeichnis und Optionen für Liferea aus</translation>
+    </message>
+    <message>
+        <location filename="lifereaimportpage.cpp" line="108"/>
+        <source>Select Liferea&apos;s directory</source>
+        <translation>Wähle Lifereas Verzeichniss</translation>
+    </message>
+    <message>
+        <location filename="lifereaimportpage.cpp" line="117"/>
+        <source>The directory you&apos;ve selected is not Liferea&apos;s directory.</source>
+        <translation>Das ausgewählte Verzeichniss ist nicht Lifereas Verzeichniss.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::NewLife::Plugin</name>
+    <message>
+        <location filename="newlife.cpp" line="53"/>
+        <source>The settings importer.</source>
+        <translation>Der Einstellungsimporteur.</translation>
+    </message>
+    <message>
+        <location filename="newlife.cpp" line="87"/>
+        <source>Import settings...</source>
+        <translation>Importiere Einstellungen…</translation>
+    </message>
+</context>
+</TS>
