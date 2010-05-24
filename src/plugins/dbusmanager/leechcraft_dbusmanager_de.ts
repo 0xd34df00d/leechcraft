108297142bbec520d858a5a0ecb783e522eb56fa
0xd34df00d <0xd34df00d@gmail.com>
2010-05-24 20:54:04 +0400

    DBusManager: German translations, de_DE -> de.

--- src/plugins/dbusmanager/leechcraft_dbusmanager_de.ts
+++ src/plugins/dbusmanager/leechcraft_dbusmanager_de.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="de">
 <context>
     <name>LeechCraft::Plugins::DBusManager::Core</name>
     <message>
-        <location filename="core.cpp" line="70"/>
+        <location filename="core.cpp" line="72"/>
         <source>LeechCraft D-Bus general interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Allgemeines LeechCraft-D-Bus-Interface</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="dbusmanager.cpp" line="59"/>
         <source>DBus support for LeechCraft</source>
-        <translation type="unfinished"></translation>
+        <translation>DBus Unterstützung für LeechCraft</translation>
     </message>
 </context>
 <context>
@@ -23,12 +23,12 @@
         <location filename="general.cpp" line="58"/>
         <location filename="general.cpp" line="82"/>
         <source>Not found plugin %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Plugin %1 nicht gefunden.</translation>
     </message>
     <message>
         <location filename="general.cpp" line="76"/>
         <source>Could not save icon for plugin %1 to PNG %2x%2</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte Icon für Plugin %1 nicht als PNG %2x%2 speichern</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
         <location filename="tasks.cpp" line="60"/>
         <location filename="tasks.cpp" line="83"/>
         <source>Not found job holder %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Job-Eigentümer %1 nicht gefunden.</translation>
     </message>
 </context>
 </TS>
