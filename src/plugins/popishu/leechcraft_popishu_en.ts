a243db0823b826661351f93c3c237c6cc995612f
0xd34df00d <0xd34df00d@gmail.com>
2010-06-28 05:48:50 +0400

    Popishu: translations for ru, uk, en.

---
+++ src/plugins/popishu/leechcraft_popishu_en.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LeechCraft::Plugins::Popishu::EditorPage</name>
+    <message>
+        <location filename="editorpage.cpp" line="79"/>
+        <source>Document type</source>
+        <translation>Document type</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="99"/>
+        <source>Editor</source>
+        <translation>Editor</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="105"/>
+        <source>Whitespace visibility</source>
+        <translation>Whitespace visibility</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="155"/>
+        <location filename="editorpage.cpp" line="227"/>
+        <source>Untitled</source>
+        <translation>Untitled</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="160"/>
+        <source>The document &lt;em&gt;%1&lt;/em&gt; is modified. Do you want to save it now?</source>
+        <translation>The document &lt;em&gt;%1&lt;/em&gt; is modified. Do you want to save it now?</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="233"/>
+        <source>Select file to open</source>
+        <translation>Select file to open</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="242"/>
+        <source>Could not open file %1 for reading.</source>
+        <translation>Could not open file %1 for reading.</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="354"/>
+        <source>Script wrapping failed.</source>
+        <translation>Script wrapping failed.</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="375"/>
+        <location filename="editorpage.cpp" line="411"/>
+        <source>Script injection failed: %1</source>
+        <translation>Script injection failed: %1</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="468"/>
+        <source>Select file to save</source>
+        <translation>Select file to save</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="481"/>
+        <source>Could not open file %1 for writing.</source>
+        <translation>Could not open file %1 for writing.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Popishu::Plugin</name>
+    <message>
+        <location filename="popishu.cpp" line="83"/>
+        <source>Plain text editor with syntax highlighting and stuff.</source>
+        <translation>Plain text editor with syntax highlighting and stuff.</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/popishu/leechcraft_popishu_ru_RU.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::Plugins::Popishu::EditorPage</name>
+    <message>
+        <location filename="editorpage.cpp" line="79"/>
+        <source>Document type</source>
+        <translation>Тип документа</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="99"/>
+        <source>Editor</source>
+        <translation>Редактор</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="105"/>
+        <source>Whitespace visibility</source>
+        <translation>Видность пробелов</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="155"/>
+        <location filename="editorpage.cpp" line="227"/>
+        <source>Untitled</source>
+        <translation>Без названия</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="160"/>
+        <source>The document &lt;em&gt;%1&lt;/em&gt; is modified. Do you want to save it now?</source>
+        <translation>Документ &lt;em&gt;%1&lt;/em&gt; был модифицирован. Вы хотите сохранить его сейчас?</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="233"/>
+        <source>Select file to open</source>
+        <translation>Выберите файл для открытия</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="242"/>
+        <source>Could not open file %1 for reading.</source>
+        <translation>Невозможно открыть файл %1 на чтение.</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="354"/>
+        <source>Script wrapping failed.</source>
+        <translation>Оборачивание скрипта упало.</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="375"/>
+        <location filename="editorpage.cpp" line="411"/>
+        <source>Script injection failed: %1</source>
+        <translation>Оборачивание скрипта упало: %1</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="468"/>
+        <source>Select file to save</source>
+        <translation>Выберите файл для сохранения</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="481"/>
+        <source>Could not open file %1 for writing.</source>
+        <translation>Невозможно открыть файл %1 на запись.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Popishu::Plugin</name>
+    <message>
+        <location filename="popishu.cpp" line="83"/>
+        <source>Plain text editor with syntax highlighting and stuff.</source>
+        <translation>Текстовый редактор с подсветкой синтаксиса и прочим.</translation>
+    </message>
+</context>
+</TS>
