dc57f7b702de0d97578043a76e07e1c3ee2b909e
0xd34df00d <0xd34df00d@gmail.com>
2010-07-18 06:00:57 +0400

    Popishu: updated ru translations.

--- src/plugins/popishu/leechcraft_popishu_ru_RU.ts
+++ src/plugins/popishu/leechcraft_popishu_ru_RU.ts
@@ -4,153 +4,209 @@
 <context>
     <name>EditorPage</name>
     <message>
-        <location filename="editorpage.ui" line="40"/>
+        <location filename="editorpage.ui" line="32"/>
+        <source>Console</source>
+        <translation>Консоль</translation>
+    </message>
+    <message>
+        <location filename="editorpage.ui" line="45"/>
         <source>Inject</source>
         <translation>Впилить</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="47"/>
+        <location filename="editorpage.ui" line="52"/>
         <source>Release</source>
         <translation>Отпустить</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="73"/>
+        <location filename="editorpage.ui" line="80"/>
         <source>New</source>
         <translation>Новый</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="76"/>
+        <location filename="editorpage.ui" line="83"/>
         <source>Ctrl+N</source>
         <translation>Ctrl+N</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="84"/>
+        <location filename="editorpage.ui" line="91"/>
         <source>Open</source>
         <translation>Открыть</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="87"/>
+        <location filename="editorpage.ui" line="94"/>
         <source>Ctrl+O</source>
         <translation>Ctrl+O</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="95"/>
+        <location filename="editorpage.ui" line="102"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="98"/>
+        <location filename="editorpage.ui" line="105"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="106"/>
+        <location filename="editorpage.ui" line="113"/>
         <source>Save as...</source>
         <translation>Сохранить как...</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="109"/>
+        <location filename="editorpage.ui" line="116"/>
         <source>Ctrl+Shift+S</source>
         <translation>Ctrl+Shift+S</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="120"/>
+        <location filename="editorpage.ui" line="127"/>
         <source>Show end of lines</source>
         <translation>Показывать концы строк</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="131"/>
+        <location filename="editorpage.ui" line="138"/>
         <source>Show caret line</source>
         <translation>Выделять текущую строку</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="142"/>
+        <location filename="editorpage.ui" line="149"/>
         <source>Invisible</source>
         <translation>Невидимые</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="150"/>
-        <location filename="editorpage.ui" line="153"/>
+        <location filename="editorpage.ui" line="157"/>
+        <location filename="editorpage.ui" line="160"/>
         <source>Always visible</source>
         <translation>Всегда видимые</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="161"/>
+        <location filename="editorpage.ui" line="168"/>
         <source>Visible after indent</source>
         <translation>Видимые после отступов</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="169"/>
+        <location filename="editorpage.ui" line="176"/>
         <source>Automatic indentation</source>
         <translation>Автоматические отступы</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="177"/>
+        <location filename="editorpage.ui" line="184"/>
         <source>Show line numbers</source>
         <translation>Показывать номера строк</translation>
     </message>
     <message>
-        <location filename="editorpage.ui" line="188"/>
+        <location filename="editorpage.ui" line="195"/>
         <source>Enable folding</source>
         <translation>Включить сворачивание</translation>
+    </message>
+    <message>
+        <location filename="editorpage.ui" line="206"/>
+        <source>No wrap</source>
+        <translation>Без переноса</translation>
+    </message>
+    <message>
+        <location filename="editorpage.ui" line="214"/>
+        <source>Word</source>
+        <translation>По словам</translation>
+    </message>
+    <message>
+        <location filename="editorpage.ui" line="222"/>
+        <location filename="editorpage.ui" line="225"/>
+        <source>Character</source>
+        <translation>По символам</translation>
+    </message>
+    <message>
+        <location filename="editorpage.ui" line="230"/>
+        <source>Replace...</source>
+        <translation>Заменить...</translation>
+    </message>
+    <message>
+        <location filename="editorpage.ui" line="233"/>
+        <source>Ctrl+R</source>
+        <translation>Ctrl+R</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::Plugins::Popishu::EditorPage</name>
     <message>
-        <location filename="editorpage.cpp" line="80"/>
+        <location filename="editorpage.cpp" line="89"/>
         <source>Document type</source>
         <translation>Тип документа</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="100"/>
-        <source>Editor</source>
-        <translation>Редактор</translation>
-    </message>
-    <message>
-        <location filename="editorpage.cpp" line="106"/>
+        <location filename="editorpage.cpp" line="125"/>
         <source>Whitespace visibility</source>
         <translation>Видность пробелов</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="168"/>
-        <location filename="editorpage.cpp" line="240"/>
+        <location filename="editorpage.cpp" line="132"/>
+        <source>Wrapping mode</source>
+        <translation>Режим переноса</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="145"/>
+        <source>Edit</source>
+        <translation>Редактирование</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="148"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="216"/>
+        <location filename="editorpage.cpp" line="288"/>
         <source>Untitled</source>
         <translation>Без названия</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="173"/>
+        <location filename="editorpage.cpp" line="221"/>
         <source>The document &lt;em&gt;%1&lt;/em&gt; is modified. Do you want to save it now?</source>
         <translation>Документ &lt;em&gt;%1&lt;/em&gt; был модифицирован. Вы хотите сохранить его сейчас?</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="246"/>
+        <location filename="editorpage.cpp" line="294"/>
         <source>Select file to open</source>
         <translation>Выберите файл для открытия</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="255"/>
+        <location filename="editorpage.cpp" line="605"/>
+        <source>File not found</source>
+        <translation>Файл не найден</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="606"/>
+        <source>The requested file doesn&apos;t exist anymore.</source>
+        <translation>Запрошенный файл более не существует.</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="659"/>
         <source>Could not open file %1 for reading.</source>
         <translation>Невозможно открыть файл %1 на чтение.</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="374"/>
+        <location filename="editorpage.cpp" line="449"/>
         <source>Script wrapping failed.</source>
         <translation>Оборачивание скрипта упало.</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="395"/>
-        <location filename="editorpage.cpp" line="431"/>
+        <location filename="editorpage.cpp" line="113"/>
+        <source>Recent files</source>
+        <translation>Недавние файлы</translation>
+    </message>
+    <message>
+        <location filename="editorpage.cpp" line="470"/>
+        <location filename="editorpage.cpp" line="506"/>
         <source>Script injection failed: %1</source>
         <translation>Оборачивание скрипта упало: %1</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="507"/>
+        <location filename="editorpage.cpp" line="624"/>
         <source>Select file to save</source>
         <translation>Выберите файл для сохранения</translation>
     </message>
     <message>
-        <location filename="editorpage.cpp" line="520"/>
+        <location filename="editorpage.cpp" line="637"/>
         <source>Could not open file %1 for writing.</source>
         <translation>Невозможно открыть файл %1 на запись.</translation>
     </message>
@@ -158,9 +214,47 @@
 <context>
     <name>LeechCraft::Plugins::Popishu::Plugin</name>
     <message>
-        <location filename="popishu.cpp" line="93"/>
+        <location filename="popishu.cpp" line="97"/>
         <source>Plain text editor with syntax highlighting and stuff.</source>
         <translation>Текстовый редактор с подсветкой синтаксиса и прочим.</translation>
+    </message>
+</context>
+<context>
+    <name>ReplaceDialog</name>
+    <message>
+        <location filename="replacedialog.ui" line="14"/>
+        <source>Replace</source>
+        <translation>Заменить</translation>
+    </message>
+    <message>
+        <location filename="replacedialog.ui" line="25"/>
+        <source>Find:</source>
+        <translation>Найти:</translation>
+    </message>
+    <message>
+        <location filename="replacedialog.ui" line="35"/>
+        <source>Replace with:</source>
+        <translation>Заменить:</translation>
+    </message>
+    <message>
+        <location filename="replacedialog.ui" line="47"/>
+        <source>Case sensitive</source>
+        <translation>Регистрочувствительный поиск</translation>
+    </message>
+    <message>
+        <location filename="replacedialog.ui" line="57"/>
+        <source>Scope</source>
+        <translation>Область</translation>
+    </message>
+    <message>
+        <location filename="replacedialog.ui" line="63"/>
+        <source>All</source>
+        <translation>Все</translation>
+    </message>
+    <message>
+        <location filename="replacedialog.ui" line="73"/>
+        <source>Selected</source>
+        <translation>Выделение</translation>
     </message>
 </context>
 </TS>
