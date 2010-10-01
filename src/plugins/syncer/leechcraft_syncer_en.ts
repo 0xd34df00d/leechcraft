4c7e531dacb1354085718dae7e5ff3391d916d72
0xd34df00d <0xd34df00d@gmail.com>
2010-10-01 12:10:50 +0400

    Syncer: en/ru translations.

---
+++ src/plugins/syncer/leechcraft_syncer_en.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LeechCraft::Plugins::Syncer::Core</name>
+    <message>
+        <location filename="core.cpp" line="206"/>
+        <location filename="core.cpp" line="222"/>
+        <source>Sync failure</source>
+        <translation>Sync failure</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="207"/>
+        <source>Login error when synchronizing plugin %1.</source>
+        <translation>Login error when synchronizing plugin %1.</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="223"/>
+        <source>Connection error when synchronizing plugin %1.</source>
+        <translation>Connection error when synchronizing plugin %1.</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="240"/>
+        <source>Successfully synchronized plugin %1</source>
+        <translation>Successfully synchronized plugin %1</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="core.cpp" line="242"/>
+        <source>%n item(s) received</source>
+        <translation>
+            <numerusform>%n item received</numerusform>
+            <numerusform>%n items received</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="core.cpp" line="244"/>
+        <source>%n item(s) sent</source>
+        <translation>
+            <numerusform>%n item sent</numerusform>
+            <numerusform>%n items sent</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="245"/>
+        <source>Sync</source>
+        <translation>Sync</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Syncer::Plugin</name>
+    <message>
+        <location filename="syncer.cpp" line="59"/>
+        <source>Synchronization plugin for LeechCraft</source>
+        <translation>Synchronization plugin for LeechCraft</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/syncer/leechcraft_syncer_ru_RU.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::Plugins::Syncer::Core</name>
+    <message>
+        <location filename="core.cpp" line="206"/>
+        <location filename="core.cpp" line="222"/>
+        <source>Sync failure</source>
+        <translation>Ошибка синхронизации</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="207"/>
+        <source>Login error when synchronizing plugin %1.</source>
+        <translation>Ошибка входа при синхронизации плагина %1.</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="223"/>
+        <source>Connection error when synchronizing plugin %1.</source>
+        <translation>Ошибка соединения при синхронизации плагина %1.</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="core.cpp" line="242"/>
+        <source>%n item(s) received</source>
+        <translation>
+            <numerusform>%n элемент получен</numerusform>
+            <numerusform>%n элемента получено</numerusform>
+            <numerusform>%n элементов получено</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="core.cpp" line="244"/>
+        <source>%n item(s) sent</source>
+        <translation>
+            <numerusform>%n элемент отправлен</numerusform>
+            <numerusform>%n элемента отправлено</numerusform>
+            <numerusform>%n элементов отправлено</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="245"/>
+        <source>Sync</source>
+        <translation>Синхронизация</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="240"/>
+        <source>Successfully synchronized plugin %1</source>
+        <oldsource>Successfully synchronized plugin %1: %2 items received, %3 items sent.</oldsource>
+        <translation>Плагин %1 успешно синхронизирован</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Syncer::Plugin</name>
+    <message>
+        <location filename="syncer.cpp" line="59"/>
+        <source>Synchronization plugin for LeechCraft</source>
+        <translation>Плагин синхронизации для LeechCraft</translation>
+    </message>
+</context>
+</TS>
