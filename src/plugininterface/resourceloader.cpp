bff5bac44f272520ab50ed2840f0b557d90ebb4a
0xd34df00d <0xd34df00d@gmail.com>
2010-11-14 22:26:30 +0300

    PI: ResourceLoader.

---
+++ src/plugininterface/resourceloader.cpp
@@ -0,0 +1,73 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2009  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "resourceloader.h"
+#include <QFile>
+#include <QtDebug>
+
+namespace LeechCraft
+{
+	namespace Util
+	{
+		ResourceLoader::ResourceLoader (const QString& relPath, QObject* parent)
+		: QObject (parent)
+		, RelativePath_ (relPath)
+		{
+			if (RelativePath_.startsWith ('/'))
+				RelativePath_ = RelativePath_.mid (1);
+			if (!RelativePath_.endsWith ('/'))
+				RelativePath_.append ('/');
+		}
+
+		void ResourceLoader::AddLocalPrefix (QString prefix)
+		{
+			if (!prefix.isEmpty () &&
+					!prefix.endsWith ('/'))
+				prefix.append ('/');
+			LocalPrefixesChain_ << (QDir::homePath () + "/.leechcraft/data/" + prefix);
+		}
+
+		void ResourceLoader::AddGlobalPrefix (QString prefix)
+		{
+			if (!prefix.endsWith ('/'))
+				prefix.append ('/');
+			GlobalPrefixesChain_ << prefix;
+		}
+
+		QString ResourceLoader::GetPath (const QStringList& pathVariants) const
+		{
+			Q_FOREACH (const QString& prefix,
+					LocalPrefixesChain_ + GlobalPrefixesChain_)
+				Q_FOREACH (const QString& path, pathVariants)
+					if (QFile::exists (prefix + RelativePath_ + path))
+						return prefix + RelativePath_ + path;
+
+			return QString ();
+		}
+
+		QIODevice_ptr ResourceLoader::Load (const QStringList& pathVariants) const
+		{
+			QString path = GetPath (pathVariants);
+			if (path.isNull ())
+				return QIODevice_ptr ();
+
+			boost::shared_ptr<QFile> result (new QFile (path));
+			return result;
+		}
+	}
+}
---
+++ src/plugininterface/resourceloader.h
@@ -0,0 +1,51 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2009  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGININTERFACE_RESOURCELOADER_H
+#define PLUGININTERFACE_RESOURCELOADER_H
+#include <boost/shared_ptr.hpp>
+#include <QObject>
+#include <QStringList>
+#include "piconfig.h"
+
+namespace LeechCraft
+{
+	namespace Util
+	{
+		typedef boost::shared_ptr<QIODevice> QIODevice_ptr;
+
+		class PLUGININTERFACE_API ResourceLoader : public QObject
+		{
+			Q_OBJECT
+
+			QStringList LocalPrefixesChain_;
+			QStringList GlobalPrefixesChain_;
+			QString RelativePath_;
+		public:
+			ResourceLoader (const QString& relPath, QObject* = 0);
+
+			void AddGlobalPrefix (QString prefix);
+			void AddLocalPrefix (QString prefix = QString ());
+
+			QString GetPath (const QStringList& pathVariants) const;
+			QIODevice_ptr Load (const QStringList& pathVariants) const;
+		};
+	}
+}
+
+#endif
