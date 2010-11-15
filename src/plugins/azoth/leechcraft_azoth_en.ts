0eee9311296793e238f1fa11810d1ead81f59946
0xd34df00d <0xd34df00d@gmail.com>
2010-11-15 11:11:17 +0300

    Azoth: added translations.

---
+++ src/plugins/azoth/leechcraft_azoth_en.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LeechCraft::Plugins::Azoth::Core</name>
+    <message>
+        <location filename="core.cpp" line="193"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::MainWidget</name>
+    <message>
+        <location filename="mainwidget.cpp" line="35"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="36"/>
+        <source>New account</source>
+        <translation>New account</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugin</name>
+    <message>
+        <location filename="azoth.cpp" line="100"/>
+        <source>Extensible IM client for LeechCraft.</source>
+        <translation>Extensible IM client for LeechCraft.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWidget</name>
+    <message>
+        <location filename="mainwidget.ui" line="42"/>
+        <source>Add account</source>
+        <translation>Add account</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/azoth/leechcraft_azoth_ru_RU.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::Plugins::Azoth::Core</name>
+    <message>
+        <location filename="core.cpp" line="193"/>
+        <source>General</source>
+        <translation>Общие</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::MainWidget</name>
+    <message>
+        <location filename="mainwidget.cpp" line="35"/>
+        <source>General</source>
+        <translation>Общее</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="36"/>
+        <source>New account</source>
+        <translation>Новый аккаунт</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugin</name>
+    <message>
+        <location filename="azoth.cpp" line="100"/>
+        <source>Extensible IM client for LeechCraft.</source>
+        <translation>Расширяемый IM-клиент для LeechCraft.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWidget</name>
+    <message>
+        <location filename="mainwidget.ui" line="42"/>
+        <source>Add account</source>
+        <translation>Добавить аккаунт</translation>
+    </message>
+</context>
+</TS>
