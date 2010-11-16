e016af7513bc68d230a966f6feb4d138001e0beb
0xd34df00d <0xd34df00d@gmail.com>
2010-11-16 14:17:44 +0300

    Azoth Xoox: added the ts files themselves.

---
+++ src/plugins/azoth/plugins/xoox/leechcraft_azoth_xoox_en.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>GlooxAccountConfigurationDialog</name>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="25"/>
+        <source>JID:</source>
+        <translation>JID:</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="39"/>
+        <source>Nick:</source>
+        <translation>Nick:</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="14"/>
+        <source>Account configuration</source>
+        <translation>Account configuration</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="53"/>
+        <source>Resource:</source>
+        <translation>Resource:</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="60"/>
+        <source>Azoth</source>
+        <translation>Azoth</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="67"/>
+        <source>Priority:</source>
+        <translation>Priority:</translation>
+    </message>
+</context>
+<context>
+    <name>GlooxJoinGroupchatDialog</name>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="14"/>
+        <source>Join groupchat</source>
+        <translation>Join groupchat</translation>
+    </message>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="25"/>
+        <source>Account:</source>
+        <translation>Account:</translation>
+    </message>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="35"/>
+        <source>Server:</source>
+        <translation>Server:</translation>
+    </message>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="49"/>
+        <source>Room:</source>
+        <translation>Room:</translation>
+    </message>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="63"/>
+        <source>NIckname:</source>
+        <translation>NIckname:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::ClientConnection</name>
+    <message>
+        <location filename="clientconnection.cpp" line="164"/>
+        <source>stream error: %1</source>
+        <translation>stream error: %1</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="169"/>
+        <source>stream version not supported</source>
+        <translation>stream version not supported</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="172"/>
+        <source>stream has been closed by the server</source>
+        <translation>stream has been closed by the server</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="175"/>
+        <source>proxy server requires authentication</source>
+        <translation>proxy server requires authentication</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="178"/>
+        <source>proxy server authentication failed</source>
+        <translation>proxy server authentication failed</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="181"/>
+        <source>proxy server requires an unsupported authentication method</source>
+        <translation>proxy server requires an unsupported authentication method</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="184"/>
+        <source>an I/O error occured</source>
+        <translation>an I/O error occured</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="187"/>
+        <source>XML parse error occured</source>
+        <translation>XML parse error occured</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="190"/>
+        <source>connection was refused by the server</source>
+        <translation>connection was refused by the server</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="193"/>
+        <source>resolving the server&apos;s hostname failed</source>
+        <translation>resolving the server&apos;s hostname failed</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="196"/>
+        <source>out of memory</source>
+        <translation>out of memory</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="199"/>
+        <source>authentication mechanisms offered by the server are not supported</source>
+        <translation>authentication mechanisms offered by the server are not supported</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="202"/>
+        <source>server&apos;s certificate could not be verified or TLS handshake failed</source>
+        <translation>server&apos;s certificate could not be verified or TLS handshake failed</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="205"/>
+        <source>server didn&apos;t offer TLS</source>
+        <translation>server didn&apos;t offer TLS</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="208"/>
+        <source>initializing compression failed</source>
+        <translation>initializing compression failed</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="211"/>
+        <source>authentication failed, %1</source>
+        <translation>authentication failed, %1</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="214"/>
+        <source>user disconnect requested</source>
+        <translation>user disconnect requested</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="217"/>
+        <source>no active connection</source>
+        <translation>no active connection</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="227"/>
+        <source>error condition is unknown</source>
+        <translation>error condition is unknown</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="230"/>
+        <source>SASL aborted</source>
+        <translation>SASL aborted</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="233"/>
+        <source>incorrect encoding</source>
+        <translation>incorrect encoding</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="236"/>
+        <source>authzid provided by initiating entity is invalid</source>
+        <translation>authzid provided by initiating entity is invalid</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="239"/>
+        <source>initiating entity provided a mechanism not supported by the receiving entity</source>
+        <translation>initiating entity provided a mechanism not supported by the receiving entity</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="242"/>
+        <source>malformed request</source>
+        <translation>malformed request</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="245"/>
+        <source>mechanism requested by initiating entity is weaker than server policy permits</source>
+        <translation>mechanism requested by initiating entity is weaker than server policy permits</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="249"/>
+        <source>initiating entity did not provide valid credentials</source>
+        <translation>initiating entity did not provide valid credentials</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="252"/>
+        <source>temporary error withing receiving entity</source>
+        <translation>temporary error withing receiving entity</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="255"/>
+        <source>resource conflict</source>
+        <translation>resource conflict</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="258"/>
+        <source>required information not provided</source>
+        <translation>required information not provided</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="264"/>
+        <source>Disconnected, %1.</source>
+        <translation>Disconnected, %1.</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="268"/>
+        <source>Azoth connection error</source>
+        <translation>Azoth connection error</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::GlooxAccount</name>
+    <message>
+        <location filename="glooxaccount.cpp" line="119"/>
+        <source>Enter password for %1:</source>
+        <translation>Enter password for %1:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::GlooxProtocol</name>
+    <message>
+        <location filename="glooxprotocol.cpp" line="87"/>
+        <source>Enter new account name</source>
+        <translation>Enter new account name</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::Plugin</name>
+    <message>
+        <location filename="xoox.cpp" line="66"/>
+        <source>XMPP (Jabber) protocol support via Gloox library.</source>
+        <translation>XMPP (Jabber) protocol support via Gloox library.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::RoomCLEntry</name>
+    <message>
+        <location filename="roomclentry.cpp" line="76"/>
+        <source>Multiuser chatrooms</source>
+        <translation>Multiuser chatrooms</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::RoomParticipantEntry</name>
+    <message>
+        <location filename="roomparticipantentry.cpp" line="85"/>
+        <source>%1 participants</source>
+        <translation>%1 participants</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/azoth/plugins/xoox/leechcraft_azoth_xoox_ru_RU.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>GlooxAccountConfigurationDialog</name>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="25"/>
+        <source>JID:</source>
+        <translation>JID:</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="39"/>
+        <source>Nick:</source>
+        <translation>Ник:</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="14"/>
+        <source>Account configuration</source>
+        <translation>Настройки аккаунта</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="53"/>
+        <source>Resource:</source>
+        <translation>Ресурс:</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="60"/>
+        <source>Azoth</source>
+        <translation>Azoth</translation>
+    </message>
+    <message>
+        <location filename="glooxaccountconfigurationdialog.ui" line="67"/>
+        <source>Priority:</source>
+        <translation>Приоритет:</translation>
+    </message>
+</context>
+<context>
+    <name>GlooxJoinGroupchatDialog</name>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="14"/>
+        <source>Join groupchat</source>
+        <translation>Войти в конференцию</translation>
+    </message>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="25"/>
+        <source>Account:</source>
+        <translation>Аккаунт:</translation>
+    </message>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="35"/>
+        <source>Server:</source>
+        <translation>Сервер:</translation>
+    </message>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="49"/>
+        <source>Room:</source>
+        <translation>Комната:</translation>
+    </message>
+    <message>
+        <location filename="glooxjoingroupchatdialog.ui" line="63"/>
+        <source>NIckname:</source>
+        <translation>Ник:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::ClientConnection</name>
+    <message>
+        <location filename="clientconnection.cpp" line="164"/>
+        <source>stream error: %1</source>
+        <translation>ошибка потока: %1</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="169"/>
+        <source>stream version not supported</source>
+        <translation>версия потока не поддерживается</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="172"/>
+        <source>stream has been closed by the server</source>
+        <translation>поток был закрыт сервером</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="175"/>
+        <source>proxy server requires authentication</source>
+        <translation>прокси-сервер требует аутентификации</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="178"/>
+        <source>proxy server authentication failed</source>
+        <translation>неудачная аутентификация на прокси-сервере</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="181"/>
+        <source>proxy server requires an unsupported authentication method</source>
+        <translation>прокси-сервер требует неподдерживаемый метод аутентификации</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="184"/>
+        <source>an I/O error occured</source>
+        <translation>возникла ошибка ввода-вывода</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="187"/>
+        <source>XML parse error occured</source>
+        <translation>возникла ошибка парсинга XML</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="190"/>
+        <source>connection was refused by the server</source>
+        <translation>соединение было отклонено сервером</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="193"/>
+        <source>resolving the server&apos;s hostname failed</source>
+        <translation>ошибка разрешения имени сервера</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="196"/>
+        <source>out of memory</source>
+        <translation>память кончилась</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="199"/>
+        <source>authentication mechanisms offered by the server are not supported</source>
+        <translation>предложенные сервером механизмы аутентификации не поддерживаются</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="202"/>
+        <source>server&apos;s certificate could not be verified or TLS handshake failed</source>
+        <translation>сертификат сервера не может быть проверен, либо не удалось установить соединение TLS</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="205"/>
+        <source>server didn&apos;t offer TLS</source>
+        <translation>сервер не предложил TLS</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="208"/>
+        <source>initializing compression failed</source>
+        <translation>ошибка инициализации сжатия</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="211"/>
+        <source>authentication failed, %1</source>
+        <translation>ошибка аутентификации, %1</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="214"/>
+        <source>user disconnect requested</source>
+        <translation>было запрошено отключение пользователя</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="217"/>
+        <source>no active connection</source>
+        <translation>нет активного соединения</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="227"/>
+        <source>error condition is unknown</source>
+        <translation>условия ошибки неизвестны</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="230"/>
+        <source>SASL aborted</source>
+        <translation>SASL отменено</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="233"/>
+        <source>incorrect encoding</source>
+        <translation>некорректная кодировка</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="236"/>
+        <source>authzid provided by initiating entity is invalid</source>
+        <translation>предоставленный инициирующей стороной authzid невалиден</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="239"/>
+        <source>initiating entity provided a mechanism not supported by the receiving entity</source>
+        <translation>инициирующая сторона предоставила механизм, неподдерживаемый принимающей стороной</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="242"/>
+        <source>malformed request</source>
+        <translation>плохо сформированный запрос</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="245"/>
+        <source>mechanism requested by initiating entity is weaker than server policy permits</source>
+        <translation>запрошенный инициирующей стороной механизм слабее того, что разрешено на сервере</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="249"/>
+        <source>initiating entity did not provide valid credentials</source>
+        <translation>инициирующая сторона не обеспечила правильные параметры учетной записи</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="252"/>
+        <source>temporary error withing receiving entity</source>
+        <translation>временная ошибка получающей стороны</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="255"/>
+        <source>resource conflict</source>
+        <translation>конфликт ресурсов</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="258"/>
+        <source>required information not provided</source>
+        <translation>не предоставлена необходимая информация</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="264"/>
+        <source>Disconnected, %1.</source>
+        <translation>Произошло отключение, %1.</translation>
+    </message>
+    <message>
+        <location filename="clientconnection.cpp" line="268"/>
+        <source>Azoth connection error</source>
+        <translation>Ошибка соединения Azoth</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::GlooxAccount</name>
+    <message>
+        <location filename="glooxaccount.cpp" line="119"/>
+        <source>Enter password for %1:</source>
+        <translation>Введите пароль для %1:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::GlooxProtocol</name>
+    <message>
+        <location filename="glooxprotocol.cpp" line="87"/>
+        <source>Enter new account name</source>
+        <translation>Введите имя нового аккаунта</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::Plugin</name>
+    <message>
+        <location filename="xoox.cpp" line="66"/>
+        <source>XMPP (Jabber) protocol support via Gloox library.</source>
+        <translation>Поддержка протокола XMPP (Jabber) с помощью библиотеки Gloox.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::RoomCLEntry</name>
+    <message>
+        <location filename="roomclentry.cpp" line="76"/>
+        <source>Multiuser chatrooms</source>
+        <translation>Конференции</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Plugins::Azoth::Plugins::Xoox::RoomParticipantEntry</name>
+    <message>
+        <location filename="roomparticipantentry.cpp" line="85"/>
+        <source>%1 participants</source>
+        <translation>Участники %1</translation>
+    </message>
+</context>
+</TS>
