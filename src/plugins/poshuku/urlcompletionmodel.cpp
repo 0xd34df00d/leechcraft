84179734de67fc26e2ef1ab8e02df0a9a7e1057f
0xd34df00d <0xd34df00d@gmail.com>
2010-12-01 00:14:17 +0300

    Poshuku: category search suggestions.

--- src/plugins/poshuku/urlcompletionmodel.cpp
+++ src/plugins/poshuku/urlcompletionmodel.cpp
@@ -22,6 +22,7 @@
 #include <QtDebug>
 #include <plugininterface/defaulthookproxy.h>
 #include "core.h"
+#include <boost/graph/graph_concepts.hpp>
 
 namespace LeechCraft
 {
@@ -34,21 +35,21 @@ namespace LeechCraft
 			, Valid_ (false)
 			{
 			}
-			
+
 			URLCompletionModel::~URLCompletionModel ()
 			{
 			}
-			
+
 			int URLCompletionModel::columnCount (const QModelIndex&) const
 			{
 				return 1;
 			}
-			
+
 			QVariant URLCompletionModel::data (const QModelIndex& index, int role) const
 			{
 				if (!index.isValid ())
 					return QVariant ();
-			
+
 				if (role == Qt::DisplayRole)
 					return Items_ [index.row ()].Title_ + " [" + Items_ [index.row ()].URL_ + "]";
 				else if (role == Qt::DecorationRole)
@@ -60,39 +61,39 @@ namespace LeechCraft
 				else
 					return QVariant ();
 			}
-			
+
 			Qt::ItemFlags URLCompletionModel::flags (const QModelIndex&) const
 			{
 				return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 			}
-			
+
 			QVariant URLCompletionModel::headerData (int, Qt::Orientation, int) const
 			{
 				return QVariant ();
 			}
-			
+
 			QModelIndex URLCompletionModel::index (int row, int column,
 					const QModelIndex& parent) const
 			{
 				if (!hasIndex (row, column, parent))
 					return QModelIndex ();
-			
+
 				return createIndex (row, column);
 			}
-			
+
 			QModelIndex URLCompletionModel::parent (const QModelIndex&) const
 			{
 				return QModelIndex ();
 			}
-			
+
 			int URLCompletionModel::rowCount (const QModelIndex& index) const
 			{
 				if (index.isValid ())
 					return 0;
-			
+
 				return Items_.size ();
 			}
-			
+
 			void URLCompletionModel::addItem (const QString& title, const QString& url)
 			{
 				HistoryItem item =
@@ -111,7 +112,7 @@ namespace LeechCraft
 			{
 				Valid_ = false;
 				Base_ = str;
-			
+
 				Populate ();
 
 				Util::DefaultHookProxy_ptr proxy (new Util::DefaultHookProxy);
@@ -133,35 +134,54 @@ namespace LeechCraft
 
 				emit baseUpdated (sender ());
 			}
-			
+
 			void URLCompletionModel::handleItemAdded (const HistoryItem&)
 			{
 				Valid_ = false;
 			}
-			
+
 			void URLCompletionModel::Populate ()
 			{
 				if (!Valid_)
 				{
 					Valid_ = true;
-			
+
 					int size = Items_.size () - 1;
 					if (size > 0)
 						beginRemoveRows (QModelIndex (), 0, size);
 					Items_.clear ();
 					if (size > 0)
 						endRemoveRows ();
-			
-					try
+
+					if (Base_.startsWith ('!'))
 					{
-						Core::Instance ().GetStorageBackend ()->LoadResemblingHistory (Base_, Items_);
+						QStringList cats = Core::Instance ()
+								.GetProxy ()->GetSearchCategories ();
+						cats.sort ();
+						Q_FOREACH (const QString& cat, cats)
+						{
+							HistoryItem item =
+							{
+								cat,
+								QDateTime (),
+								"!" + cat
+							};
+							Items_.push_back (item);;
+						}
 					}
-					catch (const std::runtime_error& e)
+					else
 					{
-						qWarning () << Q_FUNC_INFO << e.what ();
-						Valid_ = false;
+						try
+						{
+							Core::Instance ().GetStorageBackend ()->LoadResemblingHistory (Base_, Items_);
+						}
+						catch (const std::runtime_error& e)
+						{
+							qWarning () << Q_FUNC_INFO << e.what ();
+							Valid_ = false;
+						}
 					}
-			
+
 					size = Items_.size () - 1;
 					if (size >= 0)
 					{
