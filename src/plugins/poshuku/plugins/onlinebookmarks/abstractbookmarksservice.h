02e878eaa47d2ac911826777b2429f8f43d0ab94
Maledictus <MaledictusDeMagog@gmail.com>
2010-12-26 20:40:44 +0200

    OnlineBookmarks: create interface for online bookmarks services

---
+++ src/plugins/poshuku/plugins/onlinebookmarks/abstractbookmarksservice.h
@@ -0,0 +1,54 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2010  Oleg Linkin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_ABSTRACTBOOKMARKSSERVICE_H
+#define PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_ABSTRACTBOOKMARKSSERVICE_H
+#include <QObject>
+#include <QIcon>
+
+namespace LeechCraft
+{
+	namespace Plugins
+	{
+		namespace Poshuku
+		{
+			namespace Plugins
+			{
+				namespace OnlineBookmarks
+				{
+					class AbstractBookmarksService : public QObject
+					{
+						Q_OBJECT
+					public:
+						AbstractBookmarksService (QObject *parent = 0) : QObject (parent) {}
+						virtual ~AbstractBookmarksService () {}
+
+						virtual QString GetName () const = 0;
+						virtual QIcon GetIcon () const = 0;
+						virtual void CheckValidAccountData (const QString&, const QString&) = 0;
+					signals:
+						void getValidReply (bool);
+					};
+				};
+			};
+		};
+	};
+};
+
+#endif
+
