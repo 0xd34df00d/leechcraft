c60cfbeba394c1e8d7bcb16e65f8e48b4fd71020
Maledictus <MaledictusDeMagog@gmail.com>
2010-12-27 02:38:48 +0800

    Poshuku Create plugin for work with online bookmarks service such as Del.icio.us

---
+++ src/plugins/poshuku/plugins/onlinebookmarks/core.h
@@ -0,0 +1,52 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2010  Oleg Linkin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_CORE_H
+#define PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_CORE_H
+#include <QObject>
+#include <interfaces/structures.h>
+
+namespace LeechCraft
+{
+	namespace Plugins
+	{
+		namespace Poshuku
+		{
+			namespace Plugins
+			{
+				namespace OnlineBookmarks
+				{
+					class Core : public QObject
+					{
+						Q_OBJECT
+
+						Core ();
+					public:
+						static Core& Instance ();
+						void SendEntity (const Entity&);
+					signals:
+						void gotEntity (const LeechCraft::Entity&);
+						void delegateEntity (const LeechCraft::Entity&, int*, QObject**);
+					};
+				};
+			};
+		};
+	};
+};
+
+#endif // PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_CORE_H
---
+++ src/plugins/poshuku/plugins/onlinebookmarks/settings.h
@@ -0,0 +1,92 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2010  Oleg Linkin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_SETTINGS_H
+#define PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_SETTINGS_H
+#include <QWidget>
+#include "interfaces/structures.h"
+#include "ui_settings.h"
+
+namespace Ui {
+	class Settings_;
+}
+
+class QStandardItemModel;
+class QFrame;
+class QCheckBox;
+class QLineEdit;
+class QPushButton;
+class QModelIndex;
+
+namespace LeechCraft
+{
+	namespace Plugins
+	{
+		namespace Poshuku
+		{
+			namespace Plugins
+			{
+				namespace OnlineBookmarks
+				{
+					class OnlineBookmarks;
+					class AbstractBookmarksService;
+					
+					class Settings : public QWidget
+					{
+						Q_OBJECT
+
+						Ui::Settings_ Ui_;
+						OnlineBookmarks *OnlineBookmarks_;
+						QStandardItemModel *Model_;
+						QStandardItemModel *ServicesModel_;
+						QFrame *LoginFrame_;
+						QCheckBox *YahooID_;
+						QPushButton *Apply_;
+						QLineEdit *Login_;
+						QLineEdit *Password_;
+						QList<AbstractBookmarksService*> BookmarksServices_;
+						QFrame *CreateLoginWidget (QWidget *parent = 0);
+						void ClearFrameState ();
+						void SetupServices ();
+						void SetPassword (const QString&, const QString&, const QString&);
+						QString GetPassword (const QString&, const QString&);
+						bool ConfirmDelete (const QString&, const QString&);
+						void ReadSettings ();
+					public:
+						Settings (QStandardItemModel*, OnlineBookmarks*);
+						~Settings ();
+						QString GetSelectedName () const;
+					public slots:
+						void accept ();
+					private slots:
+						void on_Add__toggled (bool);
+						void on_Edit__toggled (bool);
+						void on_Delete__released ();
+						void handleStuff ();
+						void handleLoginTextChanged (const QString&);
+						void handlePasswordTextChanged (const QString&);
+						void on_Services__currentIndexChanged (const QString&);
+						void on_AccauntsView__clicked (const QModelIndex&);
+						void checkServiceAnswer (bool);
+					};
+				};
+			};
+		};
+	};
+};
+#endif // PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_SETTINGS_H
