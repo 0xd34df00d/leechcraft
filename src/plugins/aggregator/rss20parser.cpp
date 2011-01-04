0161128c4e4686ba2c08b00a61e7b0bcdf099e4a
0xd34df00d <0xd34df00d@gmail.com>
2011-01-04 06:41:06 +0300

    Aggregator: removed dependency on boost.date_time.

--- src/plugins/aggregator/rss20parser.cpp
+++ src/plugins/aggregator/rss20parser.cpp
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **********************************************************************/
 
-#include <boost/date_time/posix_time/posix_time.hpp>
 #include <QDomDocument>
 #include <QDomElement>
 #include <QStringList>
