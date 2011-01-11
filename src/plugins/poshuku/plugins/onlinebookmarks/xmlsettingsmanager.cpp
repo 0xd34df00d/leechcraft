0xd34df00d/leechcraft

commit f65e03e36190d5bdcfaf3f03f2e9654059bdb8f4

XSD: use allKeys() instead of childKeys().

src/xmlsettingsdialog/basesettingsmanager.cpp
@@ -24,7 +24,7 @@ using namespace LeechCraft::Util;
 void BaseSettingsManager::Init ()
 {
 	Settings_ = BeginSettings ();
-	QStringList properties = Settings_->childKeys ();
+	QStringList properties = Settings_->allKeys ();
 	Initializing_ = true;
 	for (int i = 0; i < properties.size (); ++i)
 		setProperty (PROP2CHAR (properties.at (i)),

commit f69f81500fd9e437840c72e78742f34cbf3dbb4e

XSD: fixed a bug introduced in f65e03e36190d5bdcfa
But now those that wish to use XSD to perform custom reads/writes should
pass `true` to BaseSettingsManager ctor.

src/plugins/poshuku/plugins/onlinebookmarks/xmlsettingsmanager.cpp
@@ -22,8 +22,9 @@
 using namespace LeechCraft::Plugins::Poshuku::Plugins::OnlineBookmarks;
 
 XmlSettingsManager::XmlSettingsManager ()
+: Util::BaseSettingsManager (true)
 {
-	LeechCraft::Util::BaseSettingsManager::Init ();
+	Util::BaseSettingsManager::Init ();
 }
 
 XmlSettingsManager *XmlSettingsManager::Instance ()

src/xmlsettingsdialog/basesettingsmanager.cpp
@@ -21,10 +21,19 @@
 
 using namespace LeechCraft::Util;
 
+BaseSettingsManager::BaseSettingsManager (bool readAllKeys, QObject *parent)
+: QObject (parent)
+, Settings_ (0)
+, ReadAllKeys_ (readAllKeys)
+{
+}
+
 void BaseSettingsManager::Init ()
 {
 	Settings_ = BeginSettings ();
-	QStringList properties = Settings_->allKeys ();
+	QStringList properties = ReadAllKeys_ ?
+			Settings_->allKeys () :
+			Settings_->childKeys ();
 	Initializing_ = true;
 	for (int i = 0; i < properties.size (); ++i)
 		setProperty (PROP2CHAR (properties.at (i)),

src/xmlsettingsdialog/basesettingsmanager.h
@@ -45,7 +45,10 @@ namespace LeechCraft
 			QMap<QByteArray, QPair<QPointer<QObject>, QByteArray> > Properties2Object_;
 			bool Initializing_;
 			QSettings *Settings_;
+		protected:
+			bool ReadAllKeys_;
 		public:
+			BaseSettingsManager (bool readAllKeys = false, QObject* = 0);
 		 /** @brief Initalizes the settings manager.
 		  *
 		  * Loads all settings from the QSettings created by BeginSettings and