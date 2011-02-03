04d85188038f4430e62ceca3f54c02dd28f14298
0xd34df00d <0xd34df00d@gmail.com>
2011-02-03 23:23:40 +0300

    Auscrie: updated translations.

--- src/plugins/auscrie/leechcraft_auscrie_de.ts
+++ src/plugins/auscrie/leechcraft_auscrie_de.ts
@@ -55,6 +55,11 @@
         <translation>Aktion:</translation>
     </message>
     <message>
+        <location filename="shooterdialog.ui" line="31"/>
+        <source>upload to Savepic.ru</source>
+        <translation>lade zu Savepic.ru hoch</translation>
+    </message>
+    <message>
         <location filename="shooterdialog.ui" line="36"/>
         <source>upload to Imagebin.ca</source>
         <translation>lade zu Imagebin.ca hoch</translation>
--- src/plugins/auscrie/leechcraft_auscrie_oc.ts
+++ src/plugins/auscrie/leechcraft_auscrie_oc.ts
@@ -55,6 +55,11 @@
         <translation>Accion :</translation>
     </message>
     <message>
+        <location filename="shooterdialog.ui" line="31"/>
+        <source>upload to Savepic.ru</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="shooterdialog.ui" line="36"/>
         <source>upload to Imagebin.ca</source>
         <translation type="unfinished"></translation>
