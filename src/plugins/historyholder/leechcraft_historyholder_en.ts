42043ed0b7d59cc73137c89f39db2b0659b6e1b2
0xd34df00d <0xd34df00d@gmail.com>
2011-02-03 23:24:45 +0300

    HistoryHolder: updated translations.

--- src/plugins/historyholder/leechcraft_historyholder_en.ts
+++ src/plugins/historyholder/leechcraft_historyholder_en.ts
@@ -24,7 +24,7 @@
         <translation>Remove</translation>
     </message>
     <message>
-        <location filename="core.cpp" line="176"/>
+        <location filename="core.cpp" line="172"/>
         <source>Binary data</source>
         <translation>Binary data</translation>
     </message>
@@ -32,7 +32,7 @@
 <context>
     <name>LeechCraft::Plugins::HistoryHolder::Plugin</name>
     <message>
-        <location filename="historyholder.cpp" line="51"/>
+        <location filename="historyholder.cpp" line="56"/>
         <source>Holds history from various plugins</source>
         <translation>Holds history from various plugins</translation>
     </message>
--- src/plugins/historyholder/leechcraft_historyholder_fr.ts
+++ src/plugins/historyholder/leechcraft_historyholder_fr.ts
@@ -24,7 +24,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="core.cpp" line="180"/>
+        <location filename="core.cpp" line="172"/>
         <source>Binary data</source>
         <translation type="unfinished"></translation>
     </message>
@@ -32,7 +32,7 @@
 <context>
     <name>LeechCraft::Plugins::HistoryHolder::Plugin</name>
     <message>
-        <location filename="historyholder.cpp" line="51"/>
+        <location filename="historyholder.cpp" line="56"/>
         <source>Holds history from various plugins</source>
         <translation type="unfinished"></translation>
     </message>
