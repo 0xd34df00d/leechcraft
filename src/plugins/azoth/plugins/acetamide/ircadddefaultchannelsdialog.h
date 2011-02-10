fb585583acc4ebfb4885b49e763fdcba2083f183
Maledictus <MaledictusDeMagog@gmail.com>
2011-02-10 03:38:05 +0200

    Azoth Acetamide: add base Add Default Channels Dialog

---
+++ src/plugins/azoth/plugins/acetamide/ircadddefaultchannelsdialog.h
@@ -0,0 +1,45 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2010  Oleg Linkin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGINS_AZOTH_PLUGINS_ACETAMIDE_IRCADDDEFAULTCHANNELSDIALOG_H
+#define PLUGINS_AZOTH_PLUGINS_ACETAMIDE_IRCADDDEFAULTCHANNELSDIALOG_H
+
+#include <QDialog>
+#include "ui_ircadddefaultchannelsdialog.h"
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace Acetamide
+{
+	class IrcAddDefaultChannelsDialog : public QDialog
+	{
+		Q_OBJECT
+		
+		Ui::IrcAddDefaultChannelsDialog Ui_;
+		
+	public:
+		IrcAddDefaultChannelsDialog (QWidget* = 0);
+		QStringList GetChannels () const;
+		void SetChannels (const QStringList&);
+	};
+};
+};
+};
+#endif // PLUGINS_AZOTH_PLUGINS_ACETAMIDE_IRCADDDEFAULTCHANNELSDIALOG_H
