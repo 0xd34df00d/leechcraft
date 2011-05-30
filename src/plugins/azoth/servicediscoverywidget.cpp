2cef6cd7f5fc5ca07fdcebb299cebb65bcc508b7
0xd34df00d <0xd34df00d@gmail.com>
2011-05-30 22:01:30 +0400

    Azoth: show context menu in SD widget.

--- src/plugins/azoth/servicediscoverywidget.cpp
+++ src/plugins/azoth/servicediscoverywidget.cpp
@@ -21,6 +21,7 @@
 #include <QComboBox>
 #include <QLineEdit>
 #include <QTimer>
+#include <QMenu>
 #include "interfaces/iaccount.h"
 #include "interfaces/ihaveservicediscovery.h"
 #include "core.h"
@@ -125,6 +126,33 @@ namespace Azoth
 	{
 		DiscoveryTimer_->stop ();
 		DiscoveryTimer_->start ();
+	}
+	
+	void ServiceDiscoveryWidget::on_DiscoveryTree__customContextMenuRequested (const QPoint& point)
+	{
+		const QModelIndex& idx = Ui_.DiscoveryTree_->indexAt (point);
+		if (!idx.isValid ())
+			return;
+		
+		const QList<QPair<QByteArray, QString> >& actions =
+				SDSession_->GetActionsFor (idx);
+		if (actions.isEmpty ())
+			return;
+		
+		QMenu *menu = new QMenu (tr ("Discovery actions"));
+#define COMMA ,
+		Q_FOREACH (const QPair<QByteArray COMMA QString>& act, actions)
+			menu->addAction (act.second)->
+					setProperty ("Azoth/ID", act.first);
+#undef COMMA
+
+		QAction *result = menu->exec (Ui_.DiscoveryTree_->
+					viewport ()->mapToGlobal (point));
+		if (!result)
+			return;
+		
+		const QByteArray& id = result->property ("Azoth/ID").toByteArray ();
+		SDSession_->ExecuteAction (idx, id);
 	}
 	
 	void ServiceDiscoveryWidget::discover ()
