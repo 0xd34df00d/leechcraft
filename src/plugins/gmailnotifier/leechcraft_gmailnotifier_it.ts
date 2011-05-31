24e48ec385fde9d7eabd7d4cf380d10faaf63c03
0xd34df00d <0xd34df00d@gmail.com>
2011-05-31 21:32:39 +0400

    GMail Notifier: added de, fr, it translations.

---
+++ src/plugins/gmailnotifier/leechcraft_gmailnotifier_it.ts
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>LeechCraft::GmailNotifier::GmailChecker</name>
+    <message>
+        <location filename="gmailchecker.cpp" line="79"/>
+        <location filename="gmailchecker.cpp" line="103"/>
+        <location filename="gmailchecker.cpp" line="161"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="84"/>
+        <source>Username or password not set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="114"/>
+        <source>Cannot parse XML data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="133"/>
+        <source>No subject</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="136"/>
+        <source>No content</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="140"/>
+        <source>from</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="144"/>
+        <source>at</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="150"/>
+        <source>and %1 more</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="166"/>
+        <source>The username or password is incorrect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="168"/>
+        <source>Connection timeout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::GmailNotifier::GmailNotifier</name>
+    <message>
+        <location filename="gmailnotifier.cpp" line="97"/>
+        <source>Google mail notification plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>gmailnotifiersettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>GMail Notifier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Login settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Login:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Update settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Enable checking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Update interval:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source> s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Number of messages shown:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
