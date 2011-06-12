2e03f2e596389bc9c3c91481e8eb71d8fa0def43
Minh <nlminhtl@gmail.com>
2011-06-12 20:03:12 +0300

    Azoth Psto Plugin
    Copyright fixed

--- src/plugins/azoth/plugins/p100q/xmlsettingsmanager.h
+++ src/plugins/azoth/plugins/p100q/xmlsettingsmanager.h
@@ -1,6 +1,6 @@
 /*
     <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2011  <copyright holder> <email>
+    Copyright (C) 2011  Minh Ngo nlminhtl@gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
