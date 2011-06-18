7342f1b3a7c659c011ff1ba6f2b4a4f55007f2bf
nobodyzzz <nobodyzzz666@gmail.com>
2011-06-18 15:23:53 +0300

    Azoth embed media plugin

---
+++ src/plugins/azoth/plugins/embedmedia/embedmedia.cpp
@@ -0,0 +1,119 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "embedmedia.h"
+#include <QDebug>
+#include <QFile>
+#include <QIcon>
+#include <QTextCodec>
+#include <QTextStream>
+#include <QWebElement>
+#include <QWebFrame>
+#include <QWebPage>
+
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace EmbedMedia
+{
+	void Plugin::Init (ICoreProxy_ptr)
+	{
+		QFile embederJS (":/plugins/azoth/plugins/embedmedia/resources/scripts/embedmedia.js");
+
+		if (!embederJS.open (QIODevice::ReadOnly))
+		{
+			qWarning () << Q_FUNC_INFO
+					<< "unable to open script file"
+					<< embederJS.errorString ();
+			return;
+		}
+		QTextStream content (&embederJS); 
+
+		content.setCodec (QTextCodec::codecForName ("UTF-8")); 
+		ScriptContent_ = content.readAll ();
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}	
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.Azoth.EmbedMedia";
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "Embed media";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Allow embedin media in chat tabs");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon ();
+	}
+
+	QStringList Plugin::Provides () const
+	{
+		return QStringList ();
+	}
+
+	QStringList Plugin::Needs () const
+	{
+		return QStringList ();
+	}
+
+	QStringList Plugin::Uses () const
+	{
+		return QStringList ();
+	}
+
+	void Plugin::SetProvider (QObject*, const QString&)
+	{
+	}
+
+	QSet<QByteArray> Plugin::GetPluginClasses () const
+	{
+		QSet<QByteArray> result;
+		result << "org.LeechCraft.Plugins.Azoth.Plugins.IGeneralPlugin";
+		return result;
+	}
+
+	void Plugin::hookChatTabCreated( LeechCraft::IHookProxy_ptr proxy, 
+			QObject *chatTab, QObject *entry, QWebView *webView )
+	{
+		if (ScriptContent_.isEmpty ())
+			return;
+
+		webView->page ()->mainFrame ()->evaluateJavaScript (ScriptContent_);
+	}
+}
+}
+}
+
+Q_EXPORT_PLUGIN2 (leechcraft_azoth_embedmedia, LeechCraft::Azoth::EmbedMedia::Plugin);
---
+++ src/plugins/azoth/plugins/embedmedia/resources/scripts/embedmedia.js
@@ -0,0 +1,131 @@
+﻿function isNumber(n) {
+	return ! isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function addEvent(obj, sEvent, sFunc) {
+	if (obj.addEventListener) obj.addEventListener(sEvent, sFunc, false);
+	else if (obj.attachEvent) obj.attachEvent('on' + sEvent, sFunc);
+}
+
+function insertAfter(referenceNode, node) {
+	referenceNode.parentNode.insertBefore(node, referenceNode.nextSibling);
+}
+
+function clickOnVideoLink(e) {
+	var videoId;
+	if ((this.href.search(/youtube.com/i) > - 1) && (this.href.search(/v=/i) > - 1)) {
+		videoId = this.href.slice(this.href.search(/v=/i) + 2);
+		if (videoId.indexOf('&') > 1) {
+			videoId = videoId.slice(0, videoId.indexOf('&'));
+		}
+		videoId = 'http://www.youtube.com/embed/' + videoId + '?autoplay=1&fs=1';
+	}
+	else if (this.href.search(/vimeo.com/i) > - 1) {
+		videoId = this.href.slice(this.href.search(/vimeo.com/i) + 10);
+		if (isNumber(videoId)) {
+			videoId = 'http://player.vimeo.com/video/' + videoId + '?autoplay=1';
+		}
+	}
+	else if (this.href.search(/twitvid.com/i) > - 1) {
+		videoId = 'http://www.twitvid.com/embed.php?guid=' + this.href.slice(this.href.search(/twitvid.com/i) + 12);
+	}
+
+	if (videoId.length > 0) {
+		var playerMainDiv = document.createElement('div');
+		var newA;
+		var newDiv;
+		var iframeObj = document.createElement('iframe');
+		iframeObj.setAttribute('width', '480');
+		iframeObj.setAttribute('height', '360');
+		iframeObj.setAttribute('frameborder', '0');
+		iframeObj.setAttribute('src', videoId);
+		playerMainDiv.appendChild(iframeObj);
+
+		playerMainDiv.setAttribute('style', 'width: 480px;');
+		newDiv = document.createElement('p');
+		newDiv.setAttribute('style', 'float: left;');
+		newDiv.appendChild(document.createTextNode(' video size : '));
+
+		newA = document.createElement('a');
+		newA.setAttribute('href', '#');
+		newA.appendChild(document.createTextNode('normal'));
+		addEvent(newA, 'click', function(e) {
+			this.parentNode.parentNode.firstChild.setAttribute('width', '480');
+			this.parentNode.parentNode.setAttribute('style', 'width: 480px;');
+			this.parentNode.parentNode.firstChild.setAttribute('height', '360');
+			e.preventDefault();
+			return false;
+		});
+		newDiv.appendChild(newA);
+		newDiv.appendChild(document.createTextNode(' | '));
+
+		newA = document.createElement('a');
+		newA.setAttribute('href', '#');
+		newA.appendChild(document.createTextNode('big'));
+		addEvent(newA, 'click', function(e) {
+			this.parentNode.parentNode.firstChild.setAttribute('width', '640');
+			this.parentNode.parentNode.setAttribute('style', 'width: 640px;');
+			this.parentNode.parentNode.firstChild.setAttribute('height', '480');
+			e.preventDefault();
+			return false;
+		});
+		newDiv.appendChild(newA);
+		newDiv.appendChild(document.createTextNode(' | '));
+
+		newA = document.createElement('a');
+		newA.setAttribute('href', '#');
+		newA.appendChild(document.createTextNode('huge'));
+		addEvent(newA, 'click', function(e) {
+			this.parentNode.parentNode.firstChild.setAttribute('width', '800');
+			this.parentNode.parentNode.setAttribute('style', 'width: 800px;');
+			this.parentNode.parentNode.firstChild.setAttribute('height', '600');
+			e.preventDefault();
+			return false;
+		});
+		newDiv.appendChild(newA);
+		playerMainDiv.appendChild(newDiv);
+
+		newDiv = document.createElement('p');
+		newDiv.setAttribute('style', 'float: right;');
+		newA = document.createElement('a');
+		newA.setAttribute('href', '#');
+		newA.appendChild(document.createTextNode('close player'));
+		addEvent(newA, 'click', function(e) {
+			this.parentNode.parentNode.previousSibling.setAttribute('style', this.parentNode.parentNode.previousSibling.getAttribute('bkpstyle'));
+			this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+			e.preventDefault();
+			return false;
+		});
+		newDiv.appendChild(newA);
+		playerMainDiv.appendChild(newDiv);
+		newDiv = document.createElement('br');
+		newDiv.setAttribute('clear', 'all');
+		playerMainDiv.appendChild(newDiv);
+
+		insertAfter(this, playerMainDiv);
+		this.setAttribute('bkpstyle', this.getAttribute('style'));
+		this.setAttribute('style', 'display: none');
+
+		e.preventDefault();
+		return false;
+	}
+}
+
+document.addEventListener("DOMNodeInserted", function(evt) {
+    console.log("DOMNodeInserted");
+    var allLinksArray = evt.target.getElementsByTagName('a');
+
+    for (var i = 0; i < allLinksArray.length; i++) {
+        if (allLinksArray[i].href.search(/youtube.com/i) > - 1) {
+            addEvent(allLinksArray[i], 'click', clickOnVideoLink);
+        }
+        else if (allLinksArray[i].href.search(/vimeo.com/i) > - 1) {
+            addEvent(allLinksArray[i], 'click', clickOnVideoLink);
+        }
+        else if (allLinksArray[i].href.search(/twitvid.com/i) > - 1) {
+            addEvent(allLinksArray[i], 'click', clickOnVideoLink);
+        }
+    }
+},
+false);
+console.log("Yahoo");
