ba0e5fe16f8f801719dd6394a2851556014a9dcb
0xd34df00d <0xd34df00d@gmail.com>
2011-06-21 14:35:31 +0400

    Azoth Xtazy: stubs for tune publisher.

---
+++ src/plugins/azoth/plugins/xtazy/xtazy.cpp
@@ -0,0 +1,71 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "xtazy.h"
+#include <QIcon>
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace Xtazy
+{
+	void Plugin::Init (ICoreProxy_ptr proxy)
+	{
+		Proxy_ = proxy;
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}	
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.Azoth.Xtazy";
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "Azoth Xtazy";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Publishes current tune.");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon ();
+	}
+
+	QSet<QByteArray> Plugin::GetPluginClasses () const
+	{
+		QSet<QByteArray> result;
+		result << "org.LeechCraft.Plugins.Azoth.Plugins.IGeneralPlugin";
+		return result;
+	}
+}
+}
+}
+
+Q_EXPORT_PLUGIN2 (leechcraft_azoth_xtazy, LeechCraft::Azoth::Xtazy::Plugin);
