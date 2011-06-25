6b2ccaa8c55326473ed9d7c4ee3ea49028b3d6d8
0xd34df00d <0xd34df00d@gmail.com>
2011-06-25 14:17:26 +0400

    Azoth Herbicide: updated translations.

--- src/plugins/azoth/plugins/herbicide/leechcraft_azoth_herbicide_en.ts
+++ src/plugins/azoth/plugins/herbicide/leechcraft_azoth_herbicide_en.ts
@@ -2,11 +2,74 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
+    <name>ConfWidget</name>
+    <message>
+        <location filename="confwidget.ui" line="28"/>
+        <source>Quest style:</source>
+        <translation>Quest style:</translation>
+    </message>
+    <message>
+        <location filename="confwidget.ui" line="36"/>
+        <source>&lt;custom&gt;</source>
+        <translation>&lt;custom&gt;</translation>
+    </message>
+    <message>
+        <location filename="confwidget.ui" line="41"/>
+        <source>Math</source>
+        <translation>Math</translation>
+    </message>
+    <message>
+        <location filename="confwidget.ui" line="49"/>
+        <source>Quest variant:</source>
+        <translation>Quest variant:</translation>
+    </message>
+    <message>
+        <location filename="confwidget.ui" line="61"/>
+        <source>Question:</source>
+        <translation>Question:</translation>
+    </message>
+    <message>
+        <location filename="confwidget.ui" line="71"/>
+        <source>Answers (one per line):</source>
+        <translation>Answers (one per line):</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Azoth::Herbicide::Plugin</name>
     <message>
-        <location filename="herbicide.cpp" line="60"/>
+        <location filename="herbicide.cpp" line="70"/>
         <source>A simple antispam plugin for Azoth.</source>
-        <translation type="unfinished"></translation>
+        <translation>A simple antispam plugin for Azoth.</translation>
+    </message>
+    <message>
+        <location filename="herbicide.cpp" line="146"/>
+        <source>Please answer to the following question to verify you are not a bot and is welcome to communicate with me:
+%1</source>
+        <translation>Please answer to the following question to verify you are not a bot and is welcome to communicate with me:
+%1</translation>
+    </message>
+    <message>
+        <location filename="herbicide.cpp" line="160"/>
+        <source>Nice, seems like you&apos;ve answered correctly. Please write again now what you wanted to write.</source>
+        <translation>Nice, seems like you&apos;ve answered correctly. Please write again now what you wanted to write.</translation>
+    </message>
+    <message>
+        <location filename="herbicide.cpp" line="169"/>
+        <source>Sorry, you are wrong. Try again.</source>
+        <translation>Sorry, you are wrong. Try again.</translation>
+    </message>
+</context>
+<context>
+    <name>azothherbicidesettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Azoth Herbicide</source>
+        <translation>Azoth Herbicide</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Enable quest</source>
+        <translation>Enable quest</translation>
     </message>
 </context>
 </TS>
