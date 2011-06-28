b56977d45479582817b9b1a6a8ebc88d507390e6
0xd34df00d <0xd34df00d@gmail.com>
2011-06-28 16:04:03 +0400

    Azoth Herbicide: check for cast result.

--- src/plugins/azoth/plugins/herbicide/herbicide.cpp
+++ src/plugins/azoth/plugins/herbicide/herbicide.cpp
@@ -103,6 +103,9 @@ namespace Herbicide
 	bool Plugin::IsEntryAllowed (QObject *entryObj) const
 	{
 		ICLEntry *entry = qobject_cast<ICLEntry*> (entryObj);
+		if (!entry)
+			return true;
+
 		if ((entry->GetEntryFeatures () & ICLEntry::FMaskLongetivity) == ICLEntry::FPermanentEntry)
 			return true;
 		
