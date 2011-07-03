fa3ee9b2958444ebfd9868287d0e27c08d8f4a48
0xd34df00d <0xd34df00d@gmail.com>
2011-07-03 18:49:32 +0400

    Azoth Rosenthal: stubs for settings & translations.

---
+++ src/plugins/azoth/plugins/rosenthal/leechcraft_azoth_rosenthal_ru_RU.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::Azoth::Rosenthal::Plugin</name>
+    <message>
+        <location filename="rosenthal.cpp" line="74"/>
+        <source>Provides spellchecking for Azoth.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>azothrosenthalsettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Azoth Rosenthal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Languages to use</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Override autodetect with these locales:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
