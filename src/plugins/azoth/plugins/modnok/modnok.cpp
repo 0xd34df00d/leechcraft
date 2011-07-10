268e3434563626ddee3f7d3803f7db522a2a100e
0xd34df00d <0xd34df00d@gmail.com>
2011-07-10 04:48:24 +0400

    Azoth Modnok: LaTeX formatter.

---
+++ src/plugins/azoth/plugins/modnok/modnok.cpp
@@ -0,0 +1,195 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "modnok.h"
+#include <QIcon>
+#include <QTextDocument>
+#include <QProcess>
+#include <interfaces/imessage.h>
+#include <plugininterface/util.h>
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace Modnok
+{
+	void Plugin::Init (ICoreProxy_ptr)
+	{
+		FormulasCache_.setMaxCost (10 * 1024);
+
+		QStringList candidates;
+		candidates << "/usr/local/bin/lc_azoth_modnok_latexconvert.sh"
+				<< "/usr/bin/lc_azoth_modnok_latexconvert.sh";
+				
+		Q_FOREACH (const QString& path, candidates)
+		{
+			QFileInfo info (path);
+			if (info.exists () &&
+					info.isReadable () &&
+					info.isExecutable ())
+			{
+				ConvScriptPath_ = path;
+				break;
+			}
+		}
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}	
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.Azoth.Modnok";
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "Azoth Modnok";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Azoth Modnok allows to render LaTeX formulas and display them in the chat window.");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon ();
+	}
+
+	QSet<QByteArray> Plugin::GetPluginClasses () const
+	{
+		QSet<QByteArray> result;
+		result << "org.LeechCraft.Plugins.Azoth.Plugins.IGeneralPlugin";
+		return result;
+	}
+	
+	namespace
+	{
+		bool IsSecure (const QString& formula)
+		{
+			return true;
+		}
+	}
+	
+	QImage Plugin::GetRenderedImage (const QString& formula)
+	{
+		if (FormulasCache_.contains (formula))
+			return *FormulasCache_.object (formula);
+
+		const QString& filename = Util::GetTemporaryName ("lc_azoth_modnok.XXXXXX.png");
+
+		QStringList args;
+		args << QString ("-r %1x%2").arg (150).arg (150);
+		args << QString ("-o %1").arg (filename);
+
+		// TODO
+		const QString& incPath = QString ();
+		if (!incPath.isEmpty ())
+			args << (QString ("-x ") + incPath);
+		
+		args << formula;		
+		
+		QProcess::execute (ConvScriptPath_, args);
+		
+		QImage img (filename);
+		FormulasCache_.insert (formula, new QImage (filename), img.byteCount () / 1024);
+
+		QFile (filename).remove ();
+
+		return img;
+	}
+	
+	QString Plugin::HandleBody (QString body)
+	{
+		QRegExp rx ("\\$\\$.+\\$\\$");
+		rx.setMinimal (true);
+		
+		int pos = 0;
+		
+		QMap<QString, QString> replaceMap;
+		while (pos >= 0 && pos < body.size ())
+		{
+			pos = rx.indexIn (body, pos);
+			
+			if (pos < 0)
+				break;
+			
+			const QString& match = rx.cap (0);
+			pos += rx.matchedLength ();
+			
+			QString formula = match;
+			formula.remove ("$$");
+			formula = formula.trimmed ();
+			if (formula.isEmpty () || !IsSecure (formula))
+				continue;
+			
+			formula.replace ("&lt;", "<");
+			formula.replace ("&gt;", ">");
+			formula.replace ("&quot;", "\"");
+			formula.replace ("&amp;", "&");
+			
+			const QImage& rendered = GetRenderedImage (formula);
+			if (rendered.isNull ())
+				continue;
+
+			replaceMap [match] = Util::GetAsBase64Src (rendered);
+		}
+		
+		qDebug () << replaceMap.size ();
+		
+		if (replaceMap.isEmpty ())
+			return body;
+		
+		Q_FOREACH (QString key, replaceMap.keys ())
+		{
+			const QString escFormula = key.replace ('\"', "&quot;");
+			const QString img = QString ("<img src=\"%1\" alt=\"%2\" />")
+					.arg (replaceMap [key])
+					.arg (escFormula);
+			body.replace (key, img);
+		}
+		
+		return body;
+	}
+	
+	void Plugin::hookFormatBodyEnd (IHookProxy_ptr proxy,
+			QObject*, QString body, QObject *message)
+	{
+		if (ConvScriptPath_.isEmpty ())
+			return;
+
+		if (!body.contains ("$$"))
+			return;
+		
+		qDebug () << "had body" << body;
+		body = HandleBody (body);		
+		qDebug () << "got body" << body;
+		proxy->SetValue ("body", body);
+	}
+}
+}
+}
+
+Q_EXPORT_PLUGIN2 (leechcraft_azoth_modnok, LeechCraft::Azoth::Modnok::Plugin);
---
+++ src/plugins/azoth/plugins/modnok/modnok.h
@@ -0,0 +1,71 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGINS_AZOTH_PLUGINS_MODNOK_MODNOK_H
+#define PLUGINS_AZOTH_PLUGINS_MODNOK_MODNOK_H
+#include <QObject>
+#include <QRegExp>
+#include <QCache>
+#include <interfaces/iinfo.h>
+#include <interfaces/iplugin2.h>
+
+class QTranslator;
+class QImage;
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace Modnok
+{
+	class Plugin : public QObject
+				 , public IInfo
+				 , public IPlugin2
+	{
+		Q_OBJECT
+		Q_INTERFACES (IInfo IPlugin2)
+		
+		boost::shared_ptr<QTranslator> Translator_;
+
+		QString ConvScriptPath_;
+
+		QCache<QString, QImage> FormulasCache_;
+	public:
+		void Init (ICoreProxy_ptr);
+		void SecondInit ();
+		QByteArray GetUniqueID () const;
+		void Release ();
+		QString GetName () const;
+		QString GetInfo () const;
+		QIcon GetIcon () const;
+
+		QSet<QByteArray> GetPluginClasses () const;
+	private:
+		QImage GetRenderedImage (const QString&);
+		QString HandleBody (QString);
+	public slots:
+		void hookFormatBodyEnd (LeechCraft::IHookProxy_ptr proxy,
+				QObject *chatTab,
+				QString body,
+				QObject *message);
+	};
+}
+}
+}
+
+#endif
