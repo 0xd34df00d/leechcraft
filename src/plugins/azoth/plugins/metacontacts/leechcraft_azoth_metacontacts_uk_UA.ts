ff9cdbe9686a8b6ff0d1fe3a8f23d6cfb2d2f8ef
0xd34df00d <0xd34df00d@gmail.com>
2011-07-17 02:44:25 +0400

    Azoth Metacontact: stubs.

---
+++ src/plugins/azoth/plugins/metacontacts/leechcraft_azoth_metacontacts_uk_UA.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk_UA">
+<context>
+    <name>LeechCraft::Azoth::Metacontacts::Plugin</name>
+    <message>
+        <location filename="metacontacts.cpp" line="56"/>
+        <source>Azoth Metacontacts provides support for joining different contacts into one metacontact.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
