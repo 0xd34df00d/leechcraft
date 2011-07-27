eceaf95c8336dffb5252948234c9e64192b0435c
0xd34df00d <0xd34df00d@gmail.com>
2011-07-28 02:20:33 +0400

    AN: updated translations.

--- src/plugins/advancednotifications/leechcraft_advancednotifications_uk_UA.ts
+++ src/plugins/advancednotifications/leechcraft_advancednotifications_uk_UA.ts
@@ -2,9 +2,92 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="uk_UA">
 <context>
+    <name>LeechCraft::AdvancedNotifications::NotificationRulesWidget</name>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="56"/>
+        <source>Instant messaging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="58"/>
+        <source>Attention request</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="59"/>
+        <source>Incoming file transfer request</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="60"/>
+        <source>Incoming chat message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="61"/>
+        <source>MUC highlight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="62"/>
+        <source>General MUC message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="63"/>
+        <source>Contact status change</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="64"/>
+        <source>Authorization granted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="65"/>
+        <source>Authorization revoked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="66"/>
+        <source>Authorization requested</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="98"/>
+        <source>Incoming chat messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="103"/>
+        <source>MUC highlights</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="108"/>
+        <source>Incoming file transfers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="131"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="132"/>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="133"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::AdvancedNotifications::Plugin</name>
     <message>
-        <location filename="advancednotifications.cpp" line="56"/>
+        <location filename="advancednotifications.cpp" line="65"/>
         <source>Module for the advanced notifications framework.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -12,8 +95,104 @@
 <context>
     <name>LeechCraft::AdvancedNotifications::SystemTrayHandler</name>
     <message>
-        <location filename="systemtrayhandler.cpp" line="117"/>
+        <location filename="systemtrayhandler.cpp" line="172"/>
         <source>Dismiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationRulesWidget</name>
+    <message>
+        <location filename="notificationruleswidget.ui" line="25"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="32"/>
+        <location filename="notificationruleswidget.ui" line="178"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="39"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="52"/>
+        <source>Rule</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="65"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="75"/>
+        <source>Event category:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="85"/>
+        <source>Event type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="92"/>
+        <source>Notify via:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="120"/>
+        <location filename="notificationruleswidget.ui" line="188"/>
+        <source>Visual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="127"/>
+        <source>System tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="134"/>
+        <location filename="notificationruleswidget.ui" line="220"/>
+        <source>Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="146"/>
+        <source>Fields</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="152"/>
+        <source>Matches:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="164"/>
+        <source>Add...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="171"/>
+        <source>Modify...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="193"/>
+        <source>Tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="199"/>
+        <source>Show count</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="226"/>
+        <source>File:</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
