cedd3367d674cf463fcd1add671a8a2f79543722
0xd34df00d <0xd34df00d@gmail.com>
2011-07-31 05:48:09 +0400

    LMP: FindPhonon.cmake hacks.

--- src/plugins/lmp/cmake/FindPhonon.cmake
+++ src/plugins/lmp/cmake/FindPhonon.cmake
@@ -1,56 +1,37 @@
 # Find libphonon
 # Once done this will define
 #
-#  QT_PHONON_FOUND    - system has Phonon Library
-#  QT_PHONON_INCLUDES - the Phonon include directory
-#  QT_PHONON_LIBS     - link these to use Phonon
+#  PHONON_FOUND    - system has Phonon Library
+#  PHONON_INCLUDES - the Phonon include directory
+#  PHONON_LIBS     - link these to use Phonon
+#  PHONON_VERSION  - the version of the Phonon Library
 
 # Copyright (c) 2008, Matthias Kretz <kretz@kde.org>
-# Copyright (c) 2008, Voker57 <voker57@gmail.com>
-# Copyright (c) 2009, 0xd34df00d <0xd34df00d@gmail.com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-# 0xd34df00d, 11 Jan 2009
-# We should search for Qt's Phonon, which is all in lowercase etc.
+macro(_phonon_find_version)
+   set(_phonon_namespace_header_file "${PHONON_INCLUDE_DIR}/phonon/phononnamespace.h")
+   if (APPLE AND EXISTS "${PHONON_INCLUDE_DIR}/Headers/phononnamespace.h")
+      set(_phonon_namespace_header_file "${PHONON_INCLUDE_DIR}/Headers/phononnamespace.h")
+   endif (APPLE AND EXISTS "${PHONON_INCLUDE_DIR}/Headers/phononnamespace.h")
+   file(READ ${_phonon_namespace_header_file} _phonon_header LIMIT 5000 OFFSET 1000)
+   string(REGEX MATCH "define PHONON_VERSION_STR \"(4\\.[0-9]+\\.[0-9a-z]+)\"" _phonon_version_match "${_phonon_header}")
+   set(PHONON_VERSION "${CMAKE_MATCH_1}")
+endmacro(_phonon_find_version)
 
-message(STATUS "Called custom Phonon search script")
+# the dirs listed with HINTS are searched before the default sets of dirs
+find_library(PHONON_LIBRARY NAMES phonon HINTS ${KDE4_LIB_INSTALL_DIR} ${QT_LIBRARY_DIR})
+find_path(PHONON_INCLUDE_DIR NAMES phonon/phonon_export.h HINTS ${KDE4_INCLUDE_INSTALL_DIR} ${QT_INCLUDE_DIR} ${INCLUDE_INSTALL_DIR} ${QT_LIBRARY_DIR})
 
-find_library(QT_PHONON_LIBRARY NAMES phonon PATHS ${QT_LIBRARY_DIR})
+if(PHONON_INCLUDE_DIR AND PHONON_LIBRARY)
+   set(PHONON_LIBS ${phonon_LIB_DEPENDS} ${PHONON_LIBRARY})
+   set(PHONON_INCLUDES ${PHONON_INCLUDE_DIR}/KDE ${PHONON_INCLUDE_DIR})
+   _phonon_find_version()
+endif(PHONON_INCLUDE_DIR AND PHONON_LIBRARY)
 
-find_path(QT_PHONON_INCLUDE_DIR NAMES phonon/phonondefs.h PATHS ${QT_INCLUDE_DIR})
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Phonon  DEFAULT_MSG  PHONON_INCLUDE_DIR PHONON_LIBRARY)
 
-if(QT_PHONON_INCLUDE_DIR AND QT_PHONON_LIBRARY)
-  set(QT_PHONON_LIBS ${phonon_LIB_DEPENDS} ${QT_PHONON_LIBRARY})
-  set(QT_PHONON_INCLUDES ${QT_PHONON_INCLUDE_DIR})
-  set(QT_PHONON_FOUND TRUE)
-else(QT_PHONON_INCLUDE_DIR AND QT_PHONON_LIBRARY)
-  set(QT_PHONON_FOUND FALSE)
-endif(QT_PHONON_INCLUDE_DIR AND QT_PHONON_LIBRARY)
-
-if(QT_PHONON_FOUND)
-  if(NOT QT_PHONON_FIND_QUIETLY)
-	 message(STATUS "Found Phonon: ${QT_PHONON_LIBRARY}")
-	 message(STATUS "Found Phonon Includes: ${QT_PHONON_INCLUDES}")
-  endif(NOT QT_PHONON_FIND_QUIETLY)
-else(QT_PHONON_FOUND)
-  if(Phonon_FIND_REQUIRED)
-	 if(NOT QT_PHONON_INCLUDE_DIR)
-		message(STATUS "Phonon includes NOT found!")
-	 endif(NOT QT_PHONON_INCLUDE_DIR)
-	 if(NOT QT_PHONON_LIBRARY)
-		message(STATUS "Phonon library NOT found!")
-	 endif(NOT QT_PHONON_LIBRARY)
-	 message(FATAL_ERROR "Phonon library or includes NOT found!")
-  else(Phonon_FIND_REQUIRED)
-	 message(STATUS "Unable to find Phonon")
-  endif(Phonon_FIND_REQUIRED)
-
-endif(QT_PHONON_FOUND)
-
-set (PHONON_FOUND ${QT_PHONON_FOUND})
-set (PHONON_LIBS ${QT_PHONON_LIBS})
-set (PHONON_INCLUDES ${QT_PHONON_INCLUDES})
-
-mark_as_advanced(QT_PHONON_INCLUDE_DIR QT_PHONON_LIBRARY QT_PHONON_INCLUDES)
+mark_as_advanced(PHONON_INCLUDE_DIR PHONON_LIBRARY)
