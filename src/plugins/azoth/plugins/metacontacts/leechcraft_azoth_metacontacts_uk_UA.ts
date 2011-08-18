af2a71825c78713c3e547b9335c0da7c160a9650
0xd34df00d <0xd34df00d@gmail.com>
2011-08-18 13:56:53 +0400

    Azoth Metacontacts: ran lupdate & updated English.

--- src/plugins/azoth/plugins/metacontacts/leechcraft_azoth_metacontacts_uk_UA.ts
+++ src/plugins/azoth/plugins/metacontacts/leechcraft_azoth_metacontacts_uk_UA.ts
@@ -2,10 +2,113 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="uk_UA">
 <context>
+    <name>AddToMetacontactsDialog</name>
+    <message>
+        <location filename="addtometacontactsdialog.ui" line="14"/>
+        <source>Add to metacontact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addtometacontactsdialog.ui" line="22"/>
+        <source>Add entry:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addtometacontactsdialog.ui" line="36"/>
+        <source>To:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addtometacontactsdialog.ui" line="46"/>
+        <source>&lt;New metacontact&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Azoth::Metacontacts::ManageContactsDialog</name>
+    <message>
+        <location filename="managecontactsdialog.cpp" line="41"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="managecontactsdialog.cpp" line="42"/>
+        <source>ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="managecontactsdialog.cpp" line="43"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="managecontactsdialog.cpp" line="44"/>
+        <source>Protocol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="managecontactsdialog.cpp" line="117"/>
+        <source>Are you sure you want to remove %1 from this metacontact?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Azoth::Metacontacts::MetaAccount</name>
+    <message>
+        <location filename="metaaccount.cpp" line="60"/>
+        <source>Metacontacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Azoth::Metacontacts::MetaEntry</name>
+    <message>
+        <location filename="metaentry.cpp" line="45"/>
+        <source>Manage contacts...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Azoth::Metacontacts::MetaProtocol</name>
+    <message>
+        <location filename="metaprotocol.cpp" line="60"/>
+        <source>Metacontacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Azoth::Metacontacts::Plugin</name>
     <message>
-        <location filename="metacontacts.cpp" line="56"/>
+        <location filename="metacontacts.cpp" line="39"/>
+        <source>Add to a metacontact...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="metacontacts.cpp" line="66"/>
         <source>Azoth Metacontacts provides support for joining different contacts into one metacontact.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ManageContactsDialog</name>
+    <message>
+        <location filename="managecontactsdialog.ui" line="14"/>
+        <source>Manage contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="managecontactsdialog.ui" line="27"/>
+        <source>Move up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="managecontactsdialog.ui" line="34"/>
+        <source>Move down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="managecontactsdialog.ui" line="41"/>
+        <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
