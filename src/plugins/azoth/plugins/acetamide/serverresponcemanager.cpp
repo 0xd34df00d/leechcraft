dd8d90e47b3ba5f38710a7811972d13f2077823e
Oleg Linkin <MaledictusDeMagog@gmail.com>
2011-09-01 11:28:44 +0300

    Azoth Acetamide:show ISupport answer

--- src/plugins/azoth/plugins/acetamide/serverresponcemanager.cpp
+++ src/plugins/azoth/plugins/acetamide/serverresponcemanager.cpp
@@ -942,9 +942,15 @@ namespace Acetamide
 	}
 
 	void ServerResponceManager::GotISupport (const QString&, 
-			const QList<std::string>&, const QString&)
+			const QList<std::string>& params, const QString& msg)
 	{
 		ISH_->JoinFromQueue ();
+		//TODO save server settings
+		QString result;
+		Q_FOREACH (const std::string& param, params)
+			result.append (QString::fromUtf8 (param.c_str ())).append (" ");
+		result.append (" : ").append (msg);
+		ISH_->ShowAnswer (result);
 	}
 
 	void ServerResponceManager::GotChannelMode (const QString& nick, 
