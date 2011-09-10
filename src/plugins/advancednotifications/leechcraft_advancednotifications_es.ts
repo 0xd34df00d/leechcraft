d91b5d79a0ed65918a7aa8cbf4e7d8d7f2b73967
0xd34df00d <0xd34df00d@gmail.com>
2011-09-10 13:31:40 +0400

    AN: Es translations stubs.

---
+++ src/plugins/advancednotifications/leechcraft_advancednotifications_es.ts
@@ -0,0 +1,485 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>IntMatcherConfigWidget</name>
+    <message>
+        <location filename="intmatcherconfigwidget.ui" line="21"/>
+        <source>&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="intmatcherconfigwidget.ui" line="26"/>
+        <source>≥</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="intmatcherconfigwidget.ui" line="31"/>
+        <source>=</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="intmatcherconfigwidget.ui" line="36"/>
+        <source>≤</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="intmatcherconfigwidget.ui" line="41"/>
+        <source>&lt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="intmatcherconfigwidget.ui" line="49"/>
+        <source>then</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::AdvancedNotifications::MatchConfigDialog</name>
+    <message>
+        <location filename="matchconfigdialog.cpp" line="131"/>
+        <source>Invalid matcher type %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::AdvancedNotifications::NotificationRulesWidget</name>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="59"/>
+        <source>Instant messaging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="61"/>
+        <source>Attention request</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="62"/>
+        <source>Incoming file transfer request</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="63"/>
+        <source>Incoming chat message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="64"/>
+        <source>MUC highlight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="65"/>
+        <source>MUC invitation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="66"/>
+        <source>General MUC message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="67"/>
+        <source>Contact status change</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="68"/>
+        <source>Authorization granted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="69"/>
+        <source>Authorization revoked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="70"/>
+        <source>Authorization requested</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="71"/>
+        <source>Contact subscribed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="72"/>
+        <source>Contact unsubscribed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="114"/>
+        <source>Incoming chat messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="120"/>
+        <source>MUC highlights</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="126"/>
+        <source>MUC invitations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="132"/>
+        <source>Incoming file transfers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="137"/>
+        <source>Subscription requests</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="143"/>
+        <source>Subscription changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="151"/>
+        <source>Attention requests</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="175"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="176"/>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="177"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="186"/>
+        <source>Field name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="187"/>
+        <source>Rule description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="304"/>
+        <source>&lt;empty matcher&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="448"/>
+        <source>Are you sure you want to replace all rules with the default set?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="537"/>
+        <source>Select audio file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="539"/>
+        <source>Audio files (*.ogg *.wav *.flac *.mp3);;All files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="574"/>
+        <source>Please enter the argument:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.cpp" line="590"/>
+        <source>Please enter new argument text:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::AdvancedNotifications::Plugin</name>
+    <message>
+        <location filename="advancednotifications.cpp" line="77"/>
+        <source>Module for the advanced notifications framework.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::AdvancedNotifications::SystemTrayHandler</name>
+    <message>
+        <location filename="systemtrayhandler.cpp" line="182"/>
+        <source>Dismiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MatchConfigDialog</name>
+    <message>
+        <location filename="matchconfigdialog.ui" line="14"/>
+        <source>Field match</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchconfigdialog.ui" line="25"/>
+        <source>Source plugin:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchconfigdialog.ui" line="35"/>
+        <source>Field name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchconfigdialog.ui" line="45"/>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchconfigdialog.ui" line="67"/>
+        <source>Matcher configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationRulesWidget</name>
+    <message>
+        <location filename="notificationruleswidget.ui" line="30"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="37"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="51"/>
+        <source>Move up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="58"/>
+        <source>Move down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="72"/>
+        <location filename="notificationruleswidget.ui" line="249"/>
+        <location filename="notificationruleswidget.ui" line="363"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="79"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="110"/>
+        <source>Rule</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="126"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="136"/>
+        <source>Event category:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="146"/>
+        <source>Event type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="153"/>
+        <source>Notify via:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="181"/>
+        <source>Visual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="188"/>
+        <source>System tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="195"/>
+        <location filename="notificationruleswidget.ui" line="259"/>
+        <source>Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="202"/>
+        <location filename="notificationruleswidget.ui" line="310"/>
+        <source>Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="214"/>
+        <source>Fields</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="223"/>
+        <source>Matches:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="235"/>
+        <location filename="notificationruleswidget.ui" line="349"/>
+        <source>Add...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="242"/>
+        <location filename="notificationruleswidget.ui" line="356"/>
+        <source>Modify...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="271"/>
+        <source>File:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="290"/>
+        <source>Browse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="297"/>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="319"/>
+        <source>Command:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="329"/>
+        <source>Arguments:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="notificationruleswidget.ui" line="339"/>
+        <source>Argument</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="typedmatchers.cpp" line="147"/>
+        <source>contains pattern `%1`</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="typedmatchers.cpp" line="148"/>
+        <source>doesn&apos;t contain pattern `%1`</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="typedmatchers.cpp" line="166"/>
+        <source>contains element matching %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="typedmatchers.cpp" line="167"/>
+        <source>doesn&apos;t contain element matching %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="typedmatchers.cpp" line="220"/>
+        <source>is %1 then %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StringLikeMatcherConfigWidget</name>
+    <message>
+        <location filename="stringlikematcherconfigwidget.ui" line="21"/>
+        <source>contains</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="stringlikematcherconfigwidget.ui" line="26"/>
+        <source>doesn&apos;t contain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="stringlikematcherconfigwidget.ui" line="45"/>
+        <source>string</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="stringlikematcherconfigwidget.ui" line="50"/>
+        <source>wildcard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="stringlikematcherconfigwidget.ui" line="55"/>
+        <source>regexp</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>advancednotificationssettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Rules</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Methods</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Enable audio notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Audio theme:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Oxygen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>System tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>Counters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Enable for IM events</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
