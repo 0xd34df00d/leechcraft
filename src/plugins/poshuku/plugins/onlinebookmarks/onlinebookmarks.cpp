8b47476eb9e22da4111eb5114735d0ddcf4023eb
Maledictus <MaledictusDeMagog@gmail.com>
2011-09-18 00:23:03 +0300

    Poshuku OnlineBookmarks: add base class Plugin, settings, icon.

---
+++ src/plugins/poshuku/plugins/onlinebookmarks/onlinebookmarks.cpp
@@ -0,0 +1,85 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2010-2011  Oleg Linkin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "onlinebookmarks.h"
+#include <QIcon>
+#include <xmlsettingsdialog/xmlsettingsdialog.h>
+#include <util/util.h>
+#include "xmlsettingsmanager.h"
+
+namespace LeechCraft
+{
+namespace Poshuku
+{
+namespace OnlineBookmarks
+{
+	void Plugin::Init (ICoreProxy_ptr proxy)
+	{
+		Translator_.reset (Util::InstallTranslator ("poshuku_onlinebookmarks"));
+		SettingsDialog_.reset (new Util::XmlSettingsDialog);
+		SettingsDialog_->RegisterObject (XmlSettingsManager::Instance (),
+				"poshukuonlinebookmarkssettings.xml");
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.Poshuku.OnlineBookmarks";
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "Poshuku OnlineBookmarks";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Sync local bookmarks with your account in online bookmark services like Read It Later");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon (":/plugins/poshuku/plugins/onlinebookmarks/resources/images/onlinebookmarks.svg");
+	}
+
+	QSet<QByteArray> Plugin::GetPluginClasses () const
+	{
+		QSet<QByteArray> result;
+		result << "org.LeechCraft.Poshuku.Plugins/1.0";
+		return result;
+	}
+
+	Util::XmlSettingsDialog_ptr Plugin::GetSettingsDialog () const
+	{
+		return SettingsDialog_;
+	}
+
+}
+}
+}
+
+Q_EXPORT_PLUGIN2 (leechcraft_poshuku_onlinebookmarks,
+		LeechCraft::Poshuku::OnlineBookmarks::Plugin);
+ 
---
+++ src/plugins/poshuku/plugins/onlinebookmarks/onlinebookmarks.h
@@ -0,0 +1,64 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2010-2011  Oleg Linkin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_ONLINEBOOKMARKS_H
+#define PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_ONLINEBOOKMARKS_H
+
+#include <QObject>
+#include <QTranslator>
+#include <interfaces/iinfo.h>
+#include <interfaces/iplugin2.h>
+#include <interfaces/ihavesettings.h>
+
+namespace LeechCraft
+{
+namespace Poshuku
+{
+namespace OnlineBookmarks
+{
+	class Plugin : public QObject
+				, public IInfo
+				, public IPlugin2
+				, public IHaveSettings
+	{
+		Q_OBJECT
+		Q_INTERFACES (IInfo IPlugin2 IHaveSettings)
+
+		Util::XmlSettingsDialog_ptr SettingsDialog_;
+		boost::shared_ptr<QTranslator> Translator_;
+	public:
+		// IInfo methods
+		void Init (ICoreProxy_ptr);
+		void SecondInit ();
+		void Release ();
+		QByteArray GetUniqueID () const;
+		QString GetName () const;
+		QString GetInfo () const;
+		QIcon GetIcon () const;
+
+		// IPlugin2 methods
+		QSet<QByteArray> GetPluginClasses () const;
+
+		// IHaveSettings methods
+		Util::XmlSettingsDialog_ptr GetSettingsDialog() const;
+	};
+}
+}
+}
+
+#endif // PLUGINS_POSHUKU_PLUGINS_ONLINEBOOKMARKS_ONLINEBOOKMARKS_H
