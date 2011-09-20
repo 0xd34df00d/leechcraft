34eca9dce9d97ffe5d8f6c92b10fd5b2c2a313e9
Oleg Linkin <MaledictusDeMagog@gmail.com>
2011-09-20 17:39:12 +0300

    OnlineBookmarks ReadItLater: add base plugin

---
+++ src/plugins/poshuku/plugins/onlinebookmarks/plugins/readitlater/readitlater.cpp
@@ -0,0 +1,75 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2010-2011  Oleg Linkin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "readitlater.h"
+#include <QIcon>
+
+namespace LeechCraft
+{
+namespace Poshuku
+{
+namespace OnlineBookmarks
+{
+namespace ReadItLater
+{
+	void Plugin::Init (ICoreProxy_ptr proxy)
+	{
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}
+	
+	void Plugin::Release ()
+	{
+	}
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.Poshuku.OnlineBookmarks.ReadItLater";
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "OnlineBookmarks Read It Later";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Sync local bookmarks with your account in Read It Later");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon ();
+	}
+
+	QSet<QByteArray> Plugin::GetPluginClasses () const
+	{
+		QSet<QByteArray> classes;
+		classes << "org.LeechCraft.Plugins.Poshuku.Plugins.OnlineBookmarks.IGeneralPlugin";
+		return classes;
+	}
+
+}
+}
+}
+}
+
+Q_EXPORT_PLUGIN2 (leechcraft_poshuku_onlinebookmarks_readitlater,
+		LeechCraft::Poshuku::OnlineBookmarks::ReadItLater::Plugin);
