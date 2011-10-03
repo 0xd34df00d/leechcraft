b99c0e13e7f8a0b68e3269db80b0800a62a0dd96
0xd34df00d <0xd34df00d@gmail.com>
2011-10-03 23:55:49 +0400

    Aggregator: documented ProxyObject.

--- src/plugins/aggregator/proxyobject.h
+++ src/plugins/aggregator/proxyobject.h
@@ -33,9 +33,65 @@ namespace Aggregator
 	public:
 		ProxyObject (QObject* = 0);
 
-		void AddFeed (Feed_ptr);
-		void AddChannel (Channel_ptr);
-		void AddItem (Item_ptr);
+		/** @brief Adds the given feed to the storage.
+		 *
+		 * This method adds the given feed, all its channels and items
+		 * to the storage. The storage isn't checked whether it already
+		 * contains this feed.
+		 *
+		 * If the feed's FeedID_ member is set to a non-zero value, that
+		 * value is used as the feed's ID. In this case it's your duty
+		 * to make sure that this is a correct ID. Otherwise, if
+		 * Feed::FeedID_ is 0, a suitable ID is generated and used, and
+		 * the feed is updated accordingly. This way one could know the
+		 * assigned ID.
+		 *
+		 * This function may throw.
+		 *
+		 * @param[in,out] feed The feed to add.
+		 *
+		 * @exception std::exception
+		 */
+		void AddFeed (Feed_ptr feed);
+
+		/** @brief Adds the given channel to the storage.
+		 *
+		 * This method adds the given channel and all its items to the
+		 * storage. The storage isn't checked whether it already
+		 * contains this channel.
+		 *
+		 * It's your duty to ensure that the feed mentioned as the
+		 * channel's parent feed actually exists.
+		 *
+		 * The same applies to the channel's ChannelID_ as for FeedID_
+		 * in AddFeed().
+		 *
+		 * This function may throw.
+		 *
+		 * @param[in,out] channel The channel to add.
+		 *
+		 * @exception std::exception
+		 */
+		void AddChannel (Channel_ptr channel);
+
+		/** @brief Adds the given item to the storage.
+		 *
+		 * This method adds the given item to the storage. The storage
+		 * isn't checked whether it already contains this item.
+		 *
+		 * It's your duty to ensure that the channel mentioned as the
+		 * item's parent channel actually exists.
+		 *
+		 * The same applies to the item's ItemID_ as for FeedID_ in
+		 * AddFeed().
+		 *
+		 * This function may throw.
+		 *
+		 * @param[in,out] item The item to add.
+		 *
+		 * @exception std::exception
+		 */
+		void AddItem (Item_ptr item);
 	};
 }
 }
