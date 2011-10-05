0bb9fbb08cf912375ad77764c04d4c922e4a98bf
0xd34df00d <0xd34df00d@gmail.com>
2011-10-05 20:40:30 +0400

    Azoth: fixed segfault with menu on private chat entries.

--- src/plugins/azoth/core.cpp
+++ src/plugins/azoth/core.cpp
@@ -1803,6 +1803,11 @@ namespace Azoth
 				Entry2Actions_ [entry] ["authorization"]->setEnabled (isOnline);
 		}
 
+		IMUCEntry *thisMuc = qobject_cast<IMUCEntry*> (entry->GetObject ());
+		if (thisMuc)
+			Entry2Actions_ [entry] ["invite"]->
+					setEnabled (thisMuc->GetMUCFeatures () & IMUCEntry::MUCFCanInvite);
+
 		IMUCEntry *mucEntry =
 				qobject_cast<IMUCEntry*> (entry->GetParentCLEntry ());
 		if (entry->GetEntryType () == ICLEntry::ETPrivateChat &&
@@ -1812,10 +1817,6 @@ namespace Azoth
 					<< entry->GetObject ()
 					<< entry->GetParentCLEntry ()
 					<< "doesn't implement IMUCEntry";
-
-		if (mucEntry)
-			Entry2Actions_ [entry] ["invite"]->
-					setEnabled (mucEntry->GetMUCFeatures () & IMUCEntry::MUCFCanInvite);
 
 		IMUCPerms *mucPerms = qobject_cast<IMUCPerms*> (entry->GetParentCLEntry ());
 		if (entry->GetEntryType () == ICLEntry::ETPrivateChat)
