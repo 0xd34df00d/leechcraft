de605d513246766b676b23f39fdd77e0e1bfe0e4
0xd34df00d <0xd34df00d@gmail.com>
2011-10-07 21:59:49 +0400

    Azoth Xoox: emit "removedCLItems" on account removal.

--- src/plugins/azoth/plugins/xoox/glooxprotocol.cpp
+++ src/plugins/azoth/plugins/xoox/glooxprotocol.cpp
@@ -190,8 +190,14 @@ namespace Xoox
 	void GlooxProtocol::RemoveAccount (QObject *acc)
 	{
 		GlooxAccount *accObj = qobject_cast<GlooxAccount*> (acc);
+
+		QMetaObject::invokeMethod (accObj,
+				"removedCLItems",
+				Q_ARG (QList<QObject*>, accObj->GetCLEntries ()));
+
 		Accounts_.removeAll (accObj);
 		emit accountRemoved (accObj);
+
 		accObj->deleteLater ();
 		saveAccounts ();
 	}
