5bd4af462e4912e17b22f8519a33801bb61bc3ac
0xd34df00d <0xd34df00d@gmail.com>
2011-10-09 01:41:35 +0400

    Poshuku OB: recovered translations.

---
+++ src/plugins/poshuku/plugins/onlinebookmarks/leechcraft_poshuku_onlinebookmarks_en.ts
@@ -0,0 +1,227 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>AccountsSettings</name>
+    <message>
+        <location filename="accountssettings.ui" line="22"/>
+        <source>Bookmarks service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.ui" line="84"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.ui" line="103"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.ui" line="170"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.ui" line="177"/>
+        <source>Register</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.ui" line="191"/>
+        <source>Select accounts to sync:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.ui" line="198"/>
+        <source>Upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.ui" line="203"/>
+        <source>Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.ui" line="208"/>
+        <source>Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::OnlineBookmarks::AccountsSettings</name>
+    <message>
+        <location filename="accountssettings.cpp" line="43"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.cpp" line="44"/>
+        <source>Last upload date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountssettings.cpp" line="45"/>
+        <source>Last download date</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::OnlineBookmarks::Plugin</name>
+    <message>
+        <location filename="onlinebookmarks.cpp" line="78"/>
+        <source>Sync local bookmarks with your account in online bookmark services like Read It Later</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinebookmarks.cpp" line="115"/>
+        <source>Bookmarks Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinebookmarks.cpp" line="117"/>
+        <source>Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinebookmarks.cpp" line="120"/>
+        <source>Upload only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinebookmarks.cpp" line="123"/>
+        <source>Download only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinebookmarks.cpp" line="126"/>
+        <source>Download all</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>poshukuonlinebookmarkssettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>OnlineBookmarks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Sync options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Confirm sending bookmarks on addition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Ask about services to send bookmarks to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Upload bookmarks type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>everything with everything</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>only accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>only services</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Download options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="12"/>
+        <location filename="dummy.cpp" line="24"/>
+        <source>Send bookmarks to selected services every</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <location filename="dummy.cpp" line="25"/>
+        <source>on action</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="14"/>
+        <location filename="dummy.cpp" line="26"/>
+        <source>day</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="15"/>
+        <location filename="dummy.cpp" line="27"/>
+        <source>2 days</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="16"/>
+        <location filename="dummy.cpp" line="28"/>
+        <source>3 days</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="17"/>
+        <location filename="dummy.cpp" line="29"/>
+        <source>4 days</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="18"/>
+        <location filename="dummy.cpp" line="30"/>
+        <source>5 days</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="19"/>
+        <location filename="dummy.cpp" line="31"/>
+        <source>6 days</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="20"/>
+        <location filename="dummy.cpp" line="32"/>
+        <source>week</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="21"/>
+        <location filename="dummy.cpp" line="33"/>
+        <source>month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="22"/>
+        <location filename="dummy.cpp" line="34"/>
+        <source>year</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="23"/>
+        <source>Upload options</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/poshuku/plugins/onlinebookmarks/leechcraft_poshuku_onlinebookmarks_it.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>AccountsSettings</name>
+    <message>
+        <source>Bookmarks service</source>
+        <translation type="unfinished">Segnalibri servizio</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished">Aggiungi</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="unfinished">Elimina</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation type="unfinished">Applicare</translation>
+    </message>
+    <message>
+        <source>Register</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select accounts to sync:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sync</source>
+        <translation type="unfinished">Sincronizzazione</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::OnlineBookmarks::AccountsSettings</name>
+    <message>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last upload date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last download date</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::OnlineBookmarks::Plugin</name>
+    <message>
+        <source>Sync local bookmarks with your account in online bookmark services like Read It Later</source>
+        <translation type="unfinished">Sincronizza i segnalibri locale con il tuo account in servizi online come segnalibro Read It Later</translation>
+    </message>
+    <message>
+        <source>Bookmarks Sync</source>
+        <translation type="unfinished">Bookmarks Sync</translation>
+    </message>
+    <message>
+        <source>Sync</source>
+        <translation type="unfinished">Sincronizzazione</translation>
+    </message>
+    <message>
+        <source>Upload only</source>
+        <translation type="unfinished">Solo carica</translation>
+    </message>
+    <message>
+        <source>Download only</source>
+        <translation type="unfinished">Solo scarica</translation>
+    </message>
+    <message>
+        <source>Download all</source>
+        <translation type="unfinished">Scarica tutto</translation>
+    </message>
+</context>
+<context>
+    <name>poshukuonlinebookmarkssettings</name>
+    <message>
+        <source>day</source>
+        <translation>giorno</translation>
+    </message>
+    <message>
+        <source>week</source>
+        <translation>settimana</translation>
+    </message>
+    <message>
+        <source>year</source>
+        <translation>anno</translation>
+    </message>
+    <message>
+        <source>month</source>
+        <translation>mese</translation>
+    </message>
+    <message>
+        <source>Confirm sending bookmarks on addition</source>
+        <translation>Conferma di invio segnalibri su oltre </translation>
+    </message>
+    <message>
+        <source>Upload options</source>
+        <translation>Opzioni di caricamento</translation>
+    </message>
+    <message>
+        <source>2 days</source>
+        <translation>2 giorni</translation>
+    </message>
+    <message>
+        <source>Download options</source>
+        <translation>Opzioni di download</translation>
+    </message>
+    <message>
+        <source>3 days</source>
+        <translation>3 giorni</translation>
+    </message>
+    <message>
+        <source>4 days</source>
+        <translation>4 giorni</translation>
+    </message>
+    <message>
+        <source>5 days</source>
+        <translation>5 giorni </translation>
+    </message>
+    <message>
+        <source>6 days</source>
+        <translation>6 giorni</translation>
+    </message>
+    <message>
+        <source>Sync options</source>
+        <translation>Opzioni di sincronizzazione</translation>
+    </message>
+    <message>
+        <source>on action</source>
+        <translation>su azione</translation>
+    </message>
+    <message>
+        <source>OnlineBookmarks</source>
+        <translation>OnlineBookmarks</translation>
+    </message>
+    <message>
+        <source>Accounts</source>
+        <translation>Accounts</translation>
+    </message>
+    <message>
+        <source>Ask about services to send bookmarks to</source>
+        <translation>Richiedi informazioni sui servizi per l&apos;invio segnalibri per </translation>
+    </message>
+    <message>
+        <source>Send bookmarks to selected services every</source>
+        <translation>Inviare segnalibri di tutti i servizi selezionati </translation>
+    </message>
+    <message>
+        <source>Upload bookmarks type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>everything with everything</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>only accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>only services</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
