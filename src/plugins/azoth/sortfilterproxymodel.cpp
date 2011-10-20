2b3f474c2e05ca0a3f97a7c2ab89d7b43cf73b79
0xd34df00d <0xd34df00d@gmail.com>
2011-10-20 14:09:55 +0400

    Azoth: option to hide MUC participants.

--- src/plugins/azoth/sortfilterproxymodel.cpp
+++ src/plugins/azoth/sortfilterproxymodel.cpp
@@ -31,6 +31,7 @@ namespace Azoth
 	, ShowOffline_ (true)
 	, MUCMode_ (false)
 	, OrderByStatus_ (true)
+	, HideMUCParts_ (false)
 	, MUCEntry_ (0)
 	{
 		setDynamicSortFilter (true);
@@ -39,6 +40,10 @@ namespace Azoth
 		XmlSettingsManager::Instance ().RegisterObject ("OrderByStatus",
 				this, "handleStatusOrderingChanged");
 		handleStatusOrderingChanged ();
+
+		XmlSettingsManager::Instance ().RegisterObject ("HideMUCPartsInWholeCL",
+				this, "handleHideMUCPartsChanged");
+		handleHideMUCPartsChanged ();
 	}
 
 	void SortFilterProxyModel::SetMUCMode (bool muc)
@@ -78,6 +83,13 @@ namespace Azoth
 	{
 		OrderByStatus_ = XmlSettingsManager::Instance ()
 				.property ("OrderByStatus").toBool ();
+		invalidate ();
+	}
+
+	void SortFilterProxyModel::handleHideMUCPartsChanged ()
+	{
+		HideMUCParts_ = XmlSettingsManager::Instance ()
+				.property ("HideMUCPartsInWholeCL").toBool ();
 		invalidate ();
 	}
 
@@ -122,7 +134,7 @@ namespace Azoth
 				break;
 			}
 		}
-		else if (!ShowOffline_)
+		else
 		{
 			const QModelIndex& idx = sourceModel ()->index (row, 0, parent);
 			if (!filterRegExp ().isEmpty ())
@@ -134,11 +146,18 @@ namespace Azoth
 			{
 				ICLEntry *entry = GetEntry (idx);
 				const State state = entry->GetStatus ().State_;
-				if (state == SOffline &&
+
+				if (!ShowOffline_ &&
+						state == SOffline &&
 						!idx.data (Core::CLRUnreadMsgCount).toInt ())
+					return false;
+
+				if (HideMUCParts_ &&
+						entry->GetEntryType () == ICLEntry::ETPrivateChat)
 					return false;
 			}
 		}
+
 		return QSortFilterProxyModel::filterAcceptsRow (row, parent);
 	}
 
