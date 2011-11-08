57865fd6a43f7f677af148efd302b3f6b78c4d7d
0xd34df00d <0xd34df00d@gmail.com>
2011-11-08 19:29:41 +0400

    Snails: added progresslistener.

---
+++ src/plugins/snails/progresslistener.h
@@ -0,0 +1,44 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGINS_SNAILS_PROGRESSLISTENER_H
+#define PLUGINS_SNAILS_PROGRESSLISTENER_H
+#include <QObject>
+#include <vmime/utility/progressListener.hpp>
+
+namespace LeechCraft
+{
+namespace Snails
+{
+	class ProgressListener : public QObject
+						   , public vmime::utility::progressListener
+	{
+		Q_OBJECT
+	public:
+		ProgressListener (QObject* = 0);
+
+		bool cancel () const;
+	signals:
+		void start (const int);
+		void progress (const int, const int);
+		void stop (const int);
+	};
+}
+}
+
+#endif
