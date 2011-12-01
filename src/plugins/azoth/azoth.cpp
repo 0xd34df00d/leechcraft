e984e957acfcf8bf9b7d9fb3f6ba2229f08c37d6
0xd34df00d <0xd34df00d@gmail.com>
2011-12-02 00:07:37 +0400

    Azoth: icon for chattab class. (cherry picked from commit 7024d0d7cab068885f56dc04ffac80337eb09f25)

--- src/plugins/azoth/azoth.cpp
+++ src/plugins/azoth/azoth.cpp
@@ -148,7 +148,7 @@ namespace Azoth
 			"ChatTab",
 			tr ("Chat"),
 			tr ("A tab with a chat session"),
-			QIcon (),
+			QIcon (":/plugins/azoth/resources/images/chattabclass.svg"),
 			0,
 			TFEmpty
 		};
