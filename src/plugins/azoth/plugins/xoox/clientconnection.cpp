4045ac857120ce423710efb3144b39c46f87ec4d
0xd34df00d <0xd34df00d@gmail.com>
2011-12-10 07:43:40 +0400

    Azoth Xoox: handle self PEP events.

--- src/plugins/azoth/plugins/xoox/clientconnection.cpp
+++ src/plugins/azoth/plugins/xoox/clientconnection.cpp
@@ -1156,7 +1156,9 @@ namespace Xoox
 		QString resource;
 		Split (from, &bare, &resource);
 
-		if (!JID2CLEntry_.contains (bare))
+		if (bare == OurBareJID_)
+			SelfContact_->HandlePEPEvent (resource, event);
+		else if (!JID2CLEntry_.contains (bare))
 		{
 			if (JID2CLEntry_.isEmpty ())
 				InitialEventQueue_ << qMakePair (from, event->Clone ());
@@ -1165,10 +1167,9 @@ namespace Xoox
 						<< "unknown PEP event source"
 						<< from
 						<< JID2CLEntry_.keys ();
-			return;
 		}
-
-		JID2CLEntry_ [bare]->HandlePEPEvent (resource, event);
+		else
+			JID2CLEntry_ [bare]->HandlePEPEvent (resource, event);
 	}
 
 	void ClientConnection::handlePEPAvatarUpdated (const QString& from, const QImage& image)
