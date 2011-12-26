14a2e1a208a3230d83ecc3d998186b93b39fa912
0xd34df00d <0xd34df00d@gmail.com>
2011-12-26 12:11:28 +0400

    Azoth Vader: MRIMBuddy class.

---
+++ src/plugins/azoth/plugins/vader/mrimbuddy.cpp
@@ -0,0 +1,145 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "mrimbuddy.h"
+#include <QImage>
+#include "mrimaccount.h"
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace Vader
+{
+	MRIMBuddy::MRIMBuddy (const Proto::ContactInfo& info, MRIMAccount *acc)
+	: QObject (acc)
+	, A_ (acc)
+	, Info_ (info)
+	{
+	}
+
+	QObject* MRIMBuddy::GetObject ()
+	{
+		return this;
+	}
+
+	QObject* MRIMBuddy::GetParentAccount () const
+	{
+		return A_;
+	}
+
+	ICLEntry::Features MRIMBuddy::GetEntryFeatures () const
+	{
+		return FPermanentEntry;
+	}
+
+	ICLEntry::EntryType MRIMBuddy::GetEntryType () const
+	{
+		return ETChat;
+	}
+
+	QString MRIMBuddy::GetEntryName () const
+	{
+		return Info_.Alias_.isEmpty () ?
+				Info_.Email_ :
+				Info_.Alias_;
+	}
+
+	void MRIMBuddy::SetEntryName (const QString&)
+	{
+	}
+
+	QString MRIMBuddy::GetEntryID () const
+	{
+		return A_->GetAccountID () + "_" + Info_.Email_;
+	}
+
+	QString MRIMBuddy::GetHumanReadableID () const
+	{
+		return Info_.Email_;
+	}
+
+	QStringList MRIMBuddy::Groups () const
+	{
+		return QStringList ();
+	}
+
+	void MRIMBuddy::SetGroups (const QStringList&)
+	{
+	}
+
+	QStringList MRIMBuddy::Variants() const
+	{
+		return Status_.State_ != SOffline ?
+				QStringList (QString ()) :
+				QStringList ();
+	}
+
+	QObject* MRIMBuddy::CreateMessage (IMessage::MessageType , const QString& , const QString&)
+	{
+		return 0;
+	}
+
+	QList<QObject*> MRIMBuddy::GetAllMessages () const
+	{
+		return QList<QObject*> ();
+	}
+
+	void MRIMBuddy::PurgeMessages (const QDateTime&)
+	{
+	}
+
+	void MRIMBuddy::SetChatPartState (ChatPartState , const QString&)
+	{
+	}
+
+	EntryStatus MRIMBuddy::GetStatus (const QString&) const
+	{
+		return Status_;
+	}
+
+	QImage MRIMBuddy::GetAvatar () const
+	{
+		return QImage ();
+	}
+
+	QString MRIMBuddy::GetRawInfo () const
+	{
+		return QString ();
+	}
+
+	void MRIMBuddy::ShowInfo ()
+	{
+	}
+
+	QList<QAction*> MRIMBuddy::GetActions () const
+	{
+		return QList<QAction*> ();
+	}
+
+	QMap<QString, QVariant> MRIMBuddy::GetClientInfo (const QString&) const
+	{
+		return QMap<QString, QVariant> ();
+	}
+
+	void MRIMBuddy::MarkMsgsRead ()
+	{
+	}
+}
+}
+}
---
+++ src/plugins/azoth/plugins/vader/mrimbuddy.h
@@ -0,0 +1,85 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef PLUGINS_AZOTH_PLUGINS_VADER_MRIMBUDDY_H
+#define PLUGINS_AZOTH_PLUGINS_VADER_MRIMBUDDY_H
+#include <QObject>
+#include <interfaces/iclentry.h>
+#include "proto/contactinfo.h"
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace Vader
+{
+	class MRIMAccount;
+
+	class MRIMBuddy : public QObject
+					, public ICLEntry
+	{
+		Q_OBJECT
+		Q_INTERFACES (LeechCraft::Azoth::ICLEntry);
+
+		MRIMAccount *A_;
+		Proto::ContactInfo Info_;
+
+		EntryStatus Status_;
+	public:
+		MRIMBuddy (const Proto::ContactInfo&, MRIMAccount*);
+
+		// ICLEntry
+		QObject* GetObject ();
+		QObject* GetParentAccount () const;
+		Features GetEntryFeatures () const;
+		EntryType GetEntryType () const;
+		QString GetEntryName () const;
+		void SetEntryName (const QString& name);
+		QString GetEntryID () const;
+		QString GetHumanReadableID () const;
+		QStringList Groups () const;
+		void SetGroups (const QStringList& groups);
+		QStringList Variants () const;
+		QObject* CreateMessage (IMessage::MessageType, const QString&, const QString&);
+		QList<QObject*> GetAllMessages () const;
+		void PurgeMessages (const QDateTime&);
+		void SetChatPartState (ChatPartState, const QString&);
+		EntryStatus GetStatus (const QString&) const;
+		QImage GetAvatar () const;
+		QString GetRawInfo () const;
+		void ShowInfo ();
+		QList<QAction*> GetActions () const;
+		QMap<QString, QVariant> GetClientInfo (const QString&) const;
+		void MarkMsgsRead ();
+	signals:
+		void gotMessage (QObject*);
+		void statusChanged (const EntryStatus&, const QString&);
+		void availableVariantsChanged (const QStringList&);
+		void avatarChanged (const QImage&);
+		void rawinfoChanged (const QString&);
+		void nameChanged (const QString&);
+		void groupsChanged (const QStringList&);
+		void chatPartStateChanged (const ChatPartState&, const QString&);
+		void permsChanged ();
+		void entryGenerallyChanged ();
+	};
+}
+}
+}
+
+#endif
