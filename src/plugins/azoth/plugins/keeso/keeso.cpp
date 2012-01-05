e0d7c47d7d2c1723ff957ad2bea75126d9f5d78a
0xd34df00d <0xd34df00d@gmail.com>
2012-01-05 19:12:16 +0400

    Azoth Keeso: added Keeso plugin.

---
+++ src/plugins/azoth/plugins/keeso/keeso.cpp
@@ -0,0 +1,94 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "keeso.h"
+#include <QIcon>
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace Keeso
+{
+	void Plugin::Init (ICoreProxy_ptr)
+	{
+	}
+	
+	void Plugin::SecondInit ()
+	{
+	}
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.Azoth.Keeso";
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "Azoth Keeso";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Allows one to WrItE lIkE tHiS easily.");
+	}
+	
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon (":/plugins/azoth/plugins/keeso/resources/images/keeso.svg");
+	}
+
+	QSet<QByteArray> Plugin::GetPluginClasses () const
+	{
+		QSet<QByteArray> result;
+		result << "org.LeechCraft.Plugins.Azoth.Plugins.IGeneralPlugin";
+		return result;
+	}
+	
+	void Plugin::hookMessageWillCreated (IHookProxy_ptr proxy,
+			QObject*, QObject*, int, QString)
+	{
+		QString text = proxy->GetValue ("text").toString ();
+		if (!text.startsWith ("/keeso "))
+			return;
+		
+		text = text.mid (QString ("/keeso ").length ()).trimmed ();
+		bool isUpper = false;
+		for (int i = 0, length = text.length (); i < length; ++i)
+		{
+			QChar c = text.at (i);
+			const QChar& u = c.toUpper ();
+			const QChar& l = c.toLower ();
+			if (u == l)
+				continue;
+			
+			text [i] = isUpper ? u : l;
+			isUpper = !isUpper;			
+		}
+		
+		proxy->SetValue ("text", text);
+	}
+}
+}
+}
+
+Q_EXPORT_PLUGIN2 (leechcraft_azoth_keeso, LeechCraft::Azoth::Keeso::Plugin);
