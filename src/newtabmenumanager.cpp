6aa84e5524f62b05b2590a2c26a41329b068c8cb
0xd34df00d <0xd34df00d@gmail.com>
2012-01-16 17:56:42 +0400

    Core: fixed NewTabMenuManager for cases with little number of plugins. (cherry picked from commit 882c2eecd5bc28cff9bf84e837507e0211137fdc)

--- src/newtabmenumanager.cpp
+++ src/newtabmenumanager.cpp
@@ -37,7 +37,7 @@ namespace LeechCraft
 		IHaveTabs *imt = qobject_cast<IHaveTabs*> (obj);
 		if (!imt || RegisteredMultiTabs_.contains (obj))
 			return;
-		
+
 		IInfo *ii = qobject_cast<IInfo*> (obj);
 
 		Q_FOREACH (const TabClassInfo& info, imt->GetTabClasses ())
@@ -58,9 +58,9 @@ namespace LeechCraft
 					static_cast<bool> (info.Features_ & TFSingle));
 			newAct->setStatusTip (info.Description_);
 			newAct->setToolTip (info.Description_);
-			
+
 			InsertAction (newAct);
-			
+
 			if (info.Features_ & TFSingle ||
 					info.Features_ & TFByDefault)
 			{
@@ -99,7 +99,7 @@ namespace LeechCraft
 			AdditionalTabMenu_->addActions (list);
 		}
 	}
-	
+
 	void NewTabMenuManager::SingleRemoved (ITabWidget *itw)
 	{
 		const QByteArray& tabClass = itw->GetTabClassInfo ().TabClass_;
@@ -113,9 +113,9 @@ namespace LeechCraft
 					<< itw->ParentMultiTabs ();
 			return;
 		}
-		
+
 		ToggleHide (itw->ParentMultiTabs (), tabClass, true);
-		
+
 		InsertAction (act);
 	}
 
@@ -126,8 +126,11 @@ namespace LeechCraft
 
 	QMenu* NewTabMenuManager::GetAdditionalMenu ()
 	{
-		AdditionalTabMenu_->insertMenu (AdditionalTabMenu_->actions ().first (),
-				NewTabMenu_);
+		if (!AdditionalTabMenu_->actions ().isEmpty ())
+			AdditionalTabMenu_->insertMenu (AdditionalTabMenu_->actions ().first (),
+					NewTabMenu_);
+		else
+			AdditionalTabMenu_->addMenu (NewTabMenu_);
 		return AdditionalTabMenu_;
 	}
 
@@ -146,7 +149,7 @@ namespace LeechCraft
 		}
 		return name;
 	}
-	
+
 	void NewTabMenuManager::ToggleHide (QObject *obj,
 			const QByteArray& tabClass, bool hide)
 	{
@@ -162,7 +165,7 @@ namespace LeechCraft
 		const QByteArray& id = ii->GetUniqueID () + '|' + tabClass;
 		XmlSettingsManager::Instance ()->setProperty ("Hide" + id, hide);
 	}
-	
+
 	void NewTabMenuManager::OpenTab (QAction *action)
 	{
 		QObject *pObj = action->property ("PluginObj").value<QObject*> ();
@@ -184,7 +187,7 @@ namespace LeechCraft
 			ToggleHide (pObj, tabClass, false);
 		}
 	}
-	
+
 	void NewTabMenuManager::InsertAction (QAction *act)
 	{
 		bool inserted = false;
