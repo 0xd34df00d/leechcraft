7222ed49aa2bb2e0c91df77d51e1b3989196a991
0xd34df00d <0xd34df00d@gmail.com>
2012-01-27 19:41:38 +0400

    LastFMS: added plugin icon.

--- src/plugins/lastfmscrobble/lastfmscrobble.cpp
+++ src/plugins/lastfmscrobble/lastfmscrobble.cpp
@@ -37,69 +37,69 @@ namespace Lastfmscrobble
 		XmlSettingsDialog_.reset (new Util::XmlSettingsDialog ());
 		XmlSettingsDialog_->RegisterObject (&XmlSettingsManager::Instance (),
 				"lastfmscrobblesettings.xml");
-		
+
 		LFSubmitter_ = new LastFMSubmitter (this);
 
 		XmlSettingsManager::Instance ().RegisterObject ("lastfm.login",
 				this, "handleSubmitterInit");
 		handleSubmitterInit ();
 	}
-	
+
 	void Plugin::SecondInit ()
 	{
 	}
-	
+
 	QByteArray Plugin::GetUniqueID () const
 	{
 		return "org.LeechCraft.Lastfmscrobble";
 	}
-	
+
 	QString Plugin::GetName () const
 	{
 		return "Last.FM Scrobbler";
 	}
-	
+
 	QString Plugin::GetInfo () const
 	{
 		return tr ("Submits information about tracks you've listened to Last.FM.");
 	}
-	
+
 	void Plugin::Release ()
 	{
 	}
-	
+
 	QIcon Plugin::GetIcon () const
 	{
-		return QIcon ();
+		return QIcon (":/resources/images/lastfmscrobble.svg");
 	}
-		
+
 	EntityTestHandleResult Plugin::CouldHandle (const Entity& entity) const
 	{
 		return entity.Mime_ == "x-leechcraft/now-playing-track-info" ?
 				EntityTestHandleResult (EntityTestHandleResult::PIdeal) :
 				EntityTestHandleResult ();
 	}
-	
+
 	void Plugin::Handle (Entity entity)
 	{
 		LFSubmitter_->submit ();
-		
+
 		MediaMeta meta (entity.Additional_);
-		
+
 		LFSubmitter_->sendTrack (meta);
 	}
-		
+
 	Util::XmlSettingsDialog_ptr Plugin::GetSettingsDialog () const
 	{
 		return XmlSettingsDialog_;
 	}
-	
+
 	void Plugin::handleSubmitterInit ()
 	{
 		const QString& login = XmlSettingsManager::Instance ()
 				.property ("lastfm.login").toString ();
 		LFSubmitter_->SetUsername (login);
-		
+
 		QString password;
 		if (!login.isEmpty ())
 		{
@@ -109,7 +109,7 @@ namespace Lastfmscrobble
 					this);
 			return;
 		}
-		
+
 		LFSubmitter_->SetPassword (password);
 		LFSubmitter_->Init (Proxy_->GetNetworkAccessManager ());
 	}
