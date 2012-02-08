cf20729c885a28d0034ec695db1064a859933fad
0xd34df00d <0xd34df00d@gmail.com>
2012-02-08 20:08:43 +0400

    LHTR: better handling of plaintext content setting.

--- src/plugins/lhtr/richeditorwidget.cpp
+++ src/plugins/lhtr/richeditorwidget.cpp
@@ -241,7 +241,7 @@ namespace LHTR
 			Ui_.View_->setHtml (contents);
 			break;
 		case ContentType::PlainText:
-			Ui_.View_->setContent (contents.toUtf8 (), "text/plain");
+			Ui_.View_->setHtml ("<html><head><meta http-equiv='content-type' content='text/html; charset=utf-8' /><title></title></head><body><pre>" + contents + "</pre></body></html>");
 			break;
 		}
 	}
