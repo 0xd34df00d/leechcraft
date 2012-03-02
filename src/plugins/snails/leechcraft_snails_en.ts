40a905a45507f859cfad9e117c58e7bbd324786c
0xd34df00d <0xd34df00d@gmail.com>
2012-03-03 02:55:21 +0400

    Snails: created translations.

---
+++ src/plugins/snails/leechcraft_snails_en.ts
@@ -0,0 +1,461 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>AccountConfigDialog</name>
+    <message>
+        <location filename="accountconfigdialog.ui" line="14"/>
+        <source>Account configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="24"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="30"/>
+        <source>General options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="39"/>
+        <source>Account name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="49"/>
+        <source>Your name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="59"/>
+        <source>Your address:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="86"/>
+        <source>Incoming</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="92"/>
+        <source>Incoming options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="98"/>
+        <location filename="accountconfigdialog.ui" line="350"/>
+        <source>Login:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="108"/>
+        <location filename="accountconfigdialog.ui" line="252"/>
+        <source>Type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="116"/>
+        <source>IMAP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="121"/>
+        <source>POP3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="126"/>
+        <source>Maildir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="137"/>
+        <source>Connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="145"/>
+        <source>Host:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="155"/>
+        <location filename="accountconfigdialog.ui" line="283"/>
+        <source>Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="175"/>
+        <location filename="accountconfigdialog.ui" line="303"/>
+        <source>Security:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="185"/>
+        <location filename="accountconfigdialog.ui" line="313"/>
+        <source>TLS/STARTTLS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="190"/>
+        <location filename="accountconfigdialog.ui" line="318"/>
+        <source>SSL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="198"/>
+        <location filename="accountconfigdialog.ui" line="326"/>
+        <source>Required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="209"/>
+        <source>Use SASL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="216"/>
+        <source>SASL required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="240"/>
+        <source>Outgoing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="246"/>
+        <source>Outgoing options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="260"/>
+        <source>SMTP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="265"/>
+        <source>Sendmail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="273"/>
+        <source>Address:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="338"/>
+        <source>Custom authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="377"/>
+        <source>Auth options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="383"/>
+        <source>SMTP needs authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="390"/>
+        <source>APOP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="397"/>
+        <source>APOP required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="418"/>
+        <source>Sync options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="426"/>
+        <source>Folders to sync:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="442"/>
+        <source>Select...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountconfigdialog.ui" line="451"/>
+        <source>Outgoing messages folder:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AccountsListWidget</name>
+    <message>
+        <location filename="accountslistwidget.ui" line="28"/>
+        <source>Add...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.ui" line="35"/>
+        <source>Modify...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.ui" line="42"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ComposeMessageTab</name>
+    <message>
+        <location filename="composemessagetab.ui" line="28"/>
+        <source>To:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.ui" line="38"/>
+        <source>CC:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.ui" line="48"/>
+        <source>BCC:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.ui" line="58"/>
+        <source>Subject:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::Account</name>
+    <message>
+        <location filename="account.cpp" line="537"/>
+        <source>Enter password for account %1:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::AccountConfigDialog</name>
+    <message>
+        <location filename="accountconfigdialog.cpp" line="30"/>
+        <source>Folders to sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::AccountThreadWorker</name>
+    <message>
+        <location filename="accountthreadworker.cpp" line="335"/>
+        <location filename="accountthreadworker.cpp" line="407"/>
+        <source>Fetching headers for %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountthreadworker.cpp" line="542"/>
+        <source>Fetching messages for %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountthreadworker.cpp" line="708"/>
+        <source>Fetching attachment %1...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountthreadworker.cpp" line="711"/>
+        <source>Attachment %1 fetched successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountthreadworker.cpp" line="794"/>
+        <source>Sending message %1...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::ComposeMessageTab</name>
+    <message>
+        <location filename="composemessagetab.cpp" line="57"/>
+        <source>Compose tab bar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="63"/>
+        <source>Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="71"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="86"/>
+        <source>Attachments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="90"/>
+        <source>Add...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="268"/>
+        <source>Select file to attach</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="277"/>
+        <source>Error attaching file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="278"/>
+        <source>Error attaching file: %1 cannot be read</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="284"/>
+        <source>Attachment description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="composemessagetab.cpp" line="285"/>
+        <source>Enter optional attachment description (you may leave it blank):</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::Core</name>
+    <message>
+        <location filename="core.cpp" line="70"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="71"/>
+        <source>Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="72"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="136"/>
+        <source>&lt;h1 style=&apos;color:#f00&apos;&gt;Unable to load message view template.&lt;/h1&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::MailModelManager</name>
+    <message>
+        <location filename="mailmodelmanager.cpp" line="70"/>
+        <source>From</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailmodelmanager.cpp" line="71"/>
+        <source>Subject</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailmodelmanager.cpp" line="72"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailmodelmanager.cpp" line="73"/>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::MailTab</name>
+    <message>
+        <location filename="mailtab.cpp" line="39"/>
+        <source>Message actions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailtab.cpp" line="65"/>
+        <source>Fetch new mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailtab.cpp" line="97"/>
+        <source>Reply...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailtab.cpp" line="104"/>
+        <source>Attachments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailtab.cpp" line="202"/>
+        <source>&lt;h2&gt;Unable to load mail&lt;/h2&gt;&lt;em&gt;%1&lt;/em&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailtab.cpp" line="224"/>
+        <source>Fetching the message...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mailtab.cpp" line="264"/>
+        <source>Save attachment</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::Plugin</name>
+    <message>
+        <location filename="snails.cpp" line="35"/>
+        <source>Mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="snails.cpp" line="36"/>
+        <source>Mail tab.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="snails.cpp" line="38"/>
+        <source>Compose mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="snails.cpp" line="39"/>
+        <source>Allows one to compose outgoing mail messages.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="snails.cpp" line="87"/>
+        <source>LeechCraft mail client.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Snails::ProgressManager</name>
+    <message>
+        <location filename="progressmanager.cpp" line="64"/>
+        <source>Running...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MailTab</name>
+    <message>
+        <location filename="mailtab.ui" line="57"/>
+        <source>about:blank</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
