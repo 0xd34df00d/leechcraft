ef1723ab61362cd76317a8d4a0a2d9a53b8ad1bd
Maledictus <MaledictusDeMagog@gmail.com>
2012-03-05 01:36:04 +0300

    ICoreTabWidget: update interface

--- src/interfaces/core/icoretabwidget.h
+++ src/interfaces/core/icoretabwidget.h
@@ -18,7 +18,9 @@
 
 #ifndef INTERFACES_CORE_ICORETABWIDGET_H
 #define INTERFACES_CORE_ICORETABWIDGET_H
+
 #include <QTabBar>
+#include <QVariant>
 
 class QObject;
 class QWidget;
@@ -30,17 +32,32 @@ public:
 	virtual ~ICoreTabWidget () {}
 
 	virtual QObject* GetObject () = 0;
+
 	virtual int WidgetCount () const = 0;
 	virtual QWidget* Widget (int) const = 0;
-	virtual void AddAction2TabBarLayout (QTabBar::ButtonPosition, QAction*) = 0;
-	virtual int IndexOf (QWidget*) const = 0;
-	virtual QIcon TabIcon (int) const = 0;
-	virtual QString TabText (int) const = 0;
-	virtual bool IsPinTab (int) const = 0;
+
+	virtual QList<QAction*> GetPermanentActions () const = 0;
+
+	virtual QVariant TabData (int index) const = 0;
+	virtual void SetTabData (int index, QVariant data) = 0;
+
+	virtual QString TabText (int index) const = 0;
+	virtual void SetTabText (int index, const QString& text) = 0;
+
+	virtual QIcon TabIcon (int index) const = 0;
+
+	virtual QWidget* TabButton (int index, QTabBar::ButtonPosition positioin) const = 0;
+	virtual QTabBar::ButtonPosition GetCloseButtonPosition () const = 0;
+	virtual void SetTabClosable (int index, bool closable, QWidget *closeButton = 0) = 0;
+
 	virtual int CurrentIndex () const = 0;
 
-	virtual void setCurrentIndex (int) = 0;
-	virtual void setCurrentWidget (QWidget*) = 0;
+	virtual void MoveTab (int from, int to) = 0;
+
+	virtual void setCurrentIndex (int index) = 0;
+	virtual void setCurrentWidget (QWidget *widget) = 0;
+
+	virtual void tabWasInserted (int index) = 0;
 };
 
 Q_DECLARE_INTERFACE (ICoreTabWidget, "org.Deviant.LeechCraft.ICoreTabWidget/1.0");
