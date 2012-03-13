05dadd2cf49db697afaaab78b52176b55e2e04d6
0xd34df00d <0xd34df00d@gmail.com>
2012-03-13 14:17:29 +0400

    Azoth Vader: updated translations.

--- src/plugins/azoth/plugins/vader/leechcraft_azoth_vader_ru_RU.ts
+++ src/plugins/azoth/plugins/vader/leechcraft_azoth_vader_ru_RU.ts
@@ -4,30 +4,138 @@
 <context>
     <name>LeechCraft::Azoth::Vader::MRIMAccount</name>
     <message>
-        <location filename="mrimaccount.cpp" line="88"/>
+        <location filename="mrimaccount.cpp" line="115"/>
         <source>Open mailbox...</source>
         <translation>Открыть почтовый ящик...</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="297"/>
+        <location filename="mrimaccount.cpp" line="342"/>
         <source>from</source>
         <translation>от</translation>
+    </message>
+    <message>
+        <location filename="mrimaccount.cpp" line="471"/>
+        <source>database error</source>
+        <translation>ошибка базы данных</translation>
+    </message>
+    <message>
+        <location filename="mrimaccount.cpp" line="474"/>
+        <source>no such user</source>
+        <translation>нет такого пользователя</translation>
+    </message>
+    <message>
+        <location filename="mrimaccount.cpp" line="477"/>
+        <source>rate limit exceeded</source>
+        <translation>превышен предел количества запросов</translation>
+    </message>
+    <message>
+        <location filename="mrimaccount.cpp" line="480"/>
+        <source>unknown error</source>
+        <translation>неизвестная ошибка</translation>
+    </message>
+    <message>
+        <location filename="mrimaccount.cpp" line="485"/>
+        <source>Error fetching user info for %1: %2.</source>
+        <translation>Ошибка получения информации о пользователе %1: %2.</translation>
+    </message>
+    <message>
+        <location filename="mrimaccount.cpp" line="620"/>
+        <source>New mail from %1: %2.</source>
+        <translation>Новое письмо от %1: %2.</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::Azoth::Vader::MRIMBuddy</name>
     <message>
-        <location filename="mrimbuddy.cpp" line="78"/>
+        <location filename="mrimbuddy.cpp" line="47"/>
+        <source>Send SMS...</source>
+        <translation>Отправить SMS...</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="105"/>
         <source>Unauthorized</source>
         <translation>Неавторизованные</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="333"/>
+        <source>Mail.Ru Agent</source>
+        <translation>Mail.Ru Агент</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="395"/>
+        <source>SMS has been sent to %1.</source>
+        <translation>SMS для %1 отправлено.</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="406"/>
+        <source>Failed to send SMS to %1: bad parameters.</source>
+        <translation>Ошибка отправки SMS для %1: некорректные параметры.</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="417"/>
+        <source>Failed to send SMS to %1: service unavailable.</source>
+        <translation>Ошибка отправки SMS для %1: сервис недоступен.</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::Azoth::Vader::Plugin</name>
     <message>
-        <location filename="vader.cpp" line="66"/>
+        <location filename="vader.cpp" line="74"/>
         <source>Support for the Mail.ru Agent protocol.</source>
         <translation>Поддержка протокола Mail.ru-агента.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Azoth::Vader::VCardDialog</name>
+    <message>
+        <location filename="vcarddialog.cpp" line="51"/>
+        <source>Nickname: %1.</source>
+        <translation>Ник: %1.</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="52"/>
+        <source>First name: %1.</source>
+        <translation>Имя: %1.</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="53"/>
+        <source>Last name: %1.</source>
+        <translation>Фамилия: %1.</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="56"/>
+        <source>male</source>
+        <translation>мужской</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="56"/>
+        <source>female</source>
+        <translation>женский</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="57"/>
+        <source>Gender: %1.</source>
+        <translation>Пол: %1.</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="59"/>
+        <source>Birthday: %1.</source>
+        <translation>День рожденья: %1.</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="60"/>
+        <source>Zodiac sign: %1.</source>
+        <translation>Знак зодиака: %1.</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="61"/>
+        <source>Location: %1.</source>
+        <translation>Местоположение: %1.</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.cpp" line="62"/>
+        <source>Phone number: %1.</source>
+        <translation>Номер телефона: %1.</translation>
     </message>
 </context>
 <context>
@@ -64,6 +172,55 @@
         <location filename="vaderutil.cpp" line="65"/>
         <source>Blogs@Mail.ru</source>
         <translation>Блоги@Mail.ru</translation>
+    </message>
+</context>
+<context>
+    <name>SMSDialog</name>
+    <message>
+        <location filename="smsdialog.ui" line="14"/>
+        <source>Send SMS</source>
+        <translation>Отправка SMS</translation>
+    </message>
+    <message>
+        <location filename="smsdialog.ui" line="22"/>
+        <source>Phone:</source>
+        <translation>Телефон:</translation>
+    </message>
+    <message>
+        <location filename="smsdialog.ui" line="32"/>
+        <source>Text:</source>
+        <translation>Текст:</translation>
+    </message>
+</context>
+<context>
+    <name>VCardDialog</name>
+    <message>
+        <location filename="vcarddialog.ui" line="14"/>
+        <source>vCard</source>
+        <translation>vCard</translation>
+    </message>
+    <message>
+        <location filename="vcarddialog.ui" line="25"/>
+        <source>No avatar</source>
+        <translation>Нет аватара</translation>
+    </message>
+</context>
+<context>
+    <name>azothvadersettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Vader</source>
+        <translation>Vader</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Contacts</source>
+        <translation>Контакты</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Show Technical Support contact</source>
+        <translation>Показывать контакт Службы Поддержки</translation>
     </message>
 </context>
 </TS>
