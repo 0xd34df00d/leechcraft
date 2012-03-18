95bc81ac9014f2f1ae710631133afa822d9e90d0
0xd34df00d <0xd34df00d@gmail.com>
2012-03-18 16:28:52 +0400

    Azoth: ChatStyleOptionManager stub.

---
+++ src/plugins/azoth/chatstyleoptionmanager.h
@@ -0,0 +1,36 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#pragma once
+
+#include <QObject>
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+	class ChatStyleOptionManager : public QObject
+	{
+		Q_OBJECT
+
+		QString OptionName_;
+	public:
+		ChatStyleOptionManager (const QString& optionName, QObject* = 0);
+	};
+}
+}
