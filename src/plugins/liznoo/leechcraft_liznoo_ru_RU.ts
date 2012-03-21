1763fab0b85b71ff2aa21f10cc367eff5d2f3413
0xd34df00d <0xd34df00d@gmail.com>
2012-03-21 22:55:56 +0400

    Liznoo: updated translations.

--- src/plugins/liznoo/leechcraft_liznoo_ru_RU.ts
+++ src/plugins/liznoo/leechcraft_liznoo_ru_RU.ts
@@ -81,54 +81,123 @@
 <context>
     <name>LeechCraft::Liznoo::Plugin</name>
     <message>
-        <location filename="liznoo.cpp" line="60"/>
+        <location filename="liznoo.cpp" line="67"/>
         <source>Suspend</source>
         <translation>Ждущий режим</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="67"/>
+        <location filename="liznoo.cpp" line="74"/>
         <source>Hibernate</source>
         <translation>Спящий режим</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="97"/>
+        <location filename="liznoo.cpp" line="104"/>
         <source>UPower/WinAPI-based power manager.</source>
         <translation>Управление энергией, основанное на UPower/WinAPI.</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="173"/>
+        <location filename="liznoo.cpp" line="169"/>
         <source>(charging)</source>
         <translation>(заряжается)</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="175"/>
+        <location filename="liznoo.cpp" line="171"/>
         <source>(discharging)</source>
         <translation>(разряжается)</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="179"/>
+        <location filename="liznoo.cpp" line="175"/>
         <source>Battery</source>
         <translation>Батарея</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="190"/>
+        <location filename="liznoo.cpp" line="186"/>
         <source>Battery technology: %1</source>
         <translation>Технология: %1</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="195"/>
+        <location filename="liznoo.cpp" line="191"/>
         <source>Energy rate: %1 W</source>
         <translation>Мощность: %1 W</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="201"/>
+        <location filename="liznoo.cpp" line="197"/>
         <source>Remaining energy: %1 Wh</source>
         <translation>Осталось энергии: %1 Вт·ч</translation>
     </message>
     <message>
-        <location filename="liznoo.cpp" line="207"/>
+        <location filename="liznoo.cpp" line="203"/>
         <source>Full energy capacity: %1 Wh</source>
         <translation>Полная емкость: %1 Вт·ч</translation>
+    </message>
+    <message>
+        <location filename="liznoo.cpp" line="232"/>
+        <source>Battery charge level is below %1.</source>
+        <translation>Заряд батареи ниже %1.</translation>
+    </message>
+    <message>
+        <location filename="liznoo.cpp" line="245"/>
+        <source>The device started charging.</source>
+        <translation>Устройство начало заряжаться.</translation>
+    </message>
+    <message>
+        <location filename="liznoo.cpp" line="249"/>
+        <source>The device started discharging.</source>
+        <translation>Устройство начало разряжаться.</translation>
+    </message>
+    <message>
+        <location filename="liznoo.cpp" line="258"/>
+        <source>Battery status</source>
+        <translation>Статус батареи</translation>
+    </message>
+</context>
+<context>
+    <name>liznoosettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>General</source>
+        <translation>Общее</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Power levels</source>
+        <translation>Уровни заряда</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Low power level:</source>
+        <translation>Низкий уровень заряда:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <location filename="dummy.cpp" line="7"/>
+        <source>%</source>
+        <translation>%</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Extremely low power level:</source>
+        <translation>Критический уровень заряда:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Notifications</source>
+        <translation>Уведомления</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>Notify on power transitions</source>
+        <translation>Уведомлять при изменении состояния сети</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Notify on low power</source>
+        <translation>Уведомлять при низком уровне заряда</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Notify on extremely low power</source>
+        <translation>Уведомлять при критическом уровне заряда</translation>
     </message>
 </context>
 </TS>
