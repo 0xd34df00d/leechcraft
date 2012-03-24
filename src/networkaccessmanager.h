451e0c4efc7d5056a32d1040039a586d81155578
0xd34df00d <0xd34df00d@gmail.com>
2012-03-24 18:43:48 +0400

    Core: support getting/setting req locales in NAM.

--- src/networkaccessmanager.h
+++ src/networkaccessmanager.h
@@ -21,6 +21,7 @@
 #include <memory>
 #include <QNetworkAccessManager>
 #include <QTimer>
+#include <QLocale>
 #include "interfaces/core/ihookproxy.h"
 
 namespace LeechCraft
@@ -32,9 +33,14 @@ namespace LeechCraft
 		Q_OBJECT
 
 		std::auto_ptr<QTimer> CookieSaveTimer_;
+		QList<QLocale> Locales_;
+		QString LocaleStr_;
 	public:
 		NetworkAccessManager (QObject* = 0);
 		virtual ~NetworkAccessManager ();
+
+		QList<QLocale> GetAcceptLangs () const;
+		void SetAcceptLangs (const QList<QLocale>&);
 	protected:
 		QNetworkReply* createRequest (Operation,
 				const QNetworkRequest&, QIODevice*);
@@ -50,6 +56,7 @@ namespace LeechCraft
 		void requestCreated (QNetworkAccessManager::Operation,
 				const QNetworkRequest&, QNetworkReply*);
 		void error (const QString&) const;
+		void acceptableLanguagesChanged ();
 
 		void hookNAMCreateRequest (LeechCraft::IHookProxy_ptr proxy,
 					QNetworkAccessManager *manager,
