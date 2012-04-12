352568fbafb419f48dc6986eba3762449cbee40e
0xd34df00d <0xd34df00d@gmail.com>
2012-04-12 14:05:39 +0400

    Azoth Xoox: reinit ClientConnection if it's offline.

--- src/plugins/azoth/plugins/xoox/glooxaccount.cpp
+++ src/plugins/azoth/plugins/xoox/glooxaccount.cpp
@@ -276,10 +276,8 @@ namespace Xoox
 			 Resource_ != w->GetResource () ||
 			 Host_ != w->GetHost () ||
 			 Port_ != w->GetPort ()))
-		{
 			ChangeState (EntryStatus (SOffline, AccState_.Status_));
-			ClientConnection_->SetOurJID (w->GetJID () + "/" + w->GetResource ());
-		}
+		ClientConnection_->SetOurJID (w->GetJID () + "/" + w->GetResource ());
 
 		JID_ = w->GetJID ();
 		Nick_ = w->GetNick ();
