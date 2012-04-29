260a80402e35e3071e309f380b88b72c63846df0
0xd34df00d <0xd34df00d@gmail.com>
2012-04-29 20:46:03 +0400

    LMP: more gcc 4.6 fixes.

--- src/plugins/lmp/artistsinfodisplay.cpp
+++ src/plugins/lmp/artistsinfodisplay.cpp
@@ -17,6 +17,7 @@
  **********************************************************************/
 
 #include "artistsinfodisplay.h"
+#include <algorithm>
 #include <QStandardItemModel>
 #include <QDeclarativeContext>
 
