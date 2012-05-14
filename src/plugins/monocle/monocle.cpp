221d6e39abc62bc11785fc429c354ec05e20826b
0xd34df00d <0xd34df00d@gmail.com>
2012-05-14 22:11:37 +0400

    Monocle: base & pdf plugins stubs.

---
+++ src/plugins/monocle/monocle.cpp
@@ -0,0 +1,61 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "monocle.h"
+#include <QIcon>
+
+namespace LeechCraft
+{
+namespace Monocle
+{
+	void Plugin::Init (ICoreProxy_ptr proxy)
+	{
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.Monocle";
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "Monocle";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Modular document viewer for LeechCraft.");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon ();
+	}
+}
+}
+
+LC_EXPORT_PLUGIN (leechcraft_monocle, LeechCraft::Monocle::Plugin);
+
