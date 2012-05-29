1024d54fe8734c7fdb99fb32476ebf4304fd70cb
0xd34df00d <0xd34df00d@gmail.com>
2012-05-29 15:34:05 +0400

    LastFMScrobble: fixed copyrights.

--- src/plugins/lastfmscrobble/authenticator.h
+++ src/plugins/lastfmscrobble/authenticator.h
@@ -1,6 +1,5 @@
 /**********************************************************************
  * LeechCraft - modular cross-platform feature rich internet client.
- * Copyright (C) 2011 Minh Ngo
  * Copyright (C) 2006-2012  Georg Rudoy
  *
  * This program is free software: you can redistribute it and/or modify
