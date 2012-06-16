8db9d19a3494002bdeacc160eb3129c5a0eeff75
0xd34df00d <0xd34df00d@gmail.com>
2012-06-16 18:30:20 +0400

    LMP: more additional info parsing.

--- src/plugins/lmp/nowplayingwidget.cpp
+++ src/plugins/lmp/nowplayingwidget.cpp
@@ -18,10 +18,25 @@
 
 #include "nowplayingwidget.h"
 #include <algorithm>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/for_each.hpp>
 #include <QStandardItemModel>
 #include <QMutexLocker>
 #include <taglib/fileref.h>
 #include <taglib/tag.h>
+#include <taglib/apeproperties.h>
+#include <taglib/asfproperties.h>
+#include <taglib/flacproperties.h>
+#include <taglib/mp4properties.h>
+#include <taglib/mpcproperties.h>
+#include <taglib/mpegproperties.h>
+#include <taglib/speexproperties.h>
+#include <taglib/vorbisproperties.h>
+#include <taglib/aiffproperties.h>
+#include <taglib/wavproperties.h>
+#include <taglib/trueaudioproperties.h>
+#include <taglib/wavpackproperties.h>
+#include <taglib/taglib.h>
 #include "mediainfo.h"
 #include "core.h"
 #include "localcollection.h"
@@ -136,10 +151,148 @@ namespace LMP
 		QMap<QString, QString> GetGenericProps (TagLib::AudioProperties *props)
 		{
 			QMap<QString, QString> result;
-			result [NowPlayingWidget::tr ("Bitrate")] = QString::number (props->bitrate ());
+			result [NowPlayingWidget::tr ("Bitrate")] = QString::number (props->bitrate ()) + " kbps";
 			result [NowPlayingWidget::tr ("Channels")] = QString::number (props->channels ());
-			result [NowPlayingWidget::tr ("SampleRate")] = QString::number (props->sampleRate ());
+			result [NowPlayingWidget::tr ("Sample rate")] = QString::number (props->sampleRate ()) + " Hz";
 			return result;
+		}
+
+		template<typename Appender>
+		struct PropsGetter
+		{
+			Appender F_;
+			std::function<void (QString, int)> IF_;
+			TagLib::AudioProperties *Props_;
+
+			PropsGetter (Appender app, TagLib::AudioProperties *props)
+			: F_ (app)
+			, Props_ (props)
+			{
+				IF_ = [this] (const QString& name, int val) { F_ (name, QString::number (val)); };
+			}
+
+			template<typename T>
+			void operator() (T *t)
+			{
+				if ((t = dynamic_cast<T*> (Props_)))
+					F_ (NowPlayingWidget::tr ("File type"), Parse (t));
+			}
+
+			QString Parse (TagLib::APE::Properties *props)
+			{
+				IF_ ("APE version", props->version ());
+				IF_ ("Bits per sample", props->bitsPerSample ());
+				return "APE";
+			}
+
+			QString Parse (TagLib::ASF::Properties*)
+			{
+				return "ASF";
+			}
+
+			QString Parse (TagLib::FLAC::Properties *props)
+			{
+				IF_ ("Sample width", props->sampleWidth ());
+				return "FLAC";
+			}
+
+			QString Parse (TagLib::MP4::Properties *props)
+			{
+				IF_ ("Bits per sample", props->bitsPerSample ());
+				return "MP4";
+			}
+
+			QString Parse (TagLib::MPC::Properties *props)
+			{
+				IF_ ("MPC version", props->mpcVersion ());
+				return "MPC";
+			}
+
+			QString Parse (TagLib::MPEG::Properties *props)
+			{
+				switch (props->version ())
+				{
+				case TagLib::MPEG::Header::Version1:
+					IF_ ("MPEG version", 1);
+					break;
+				case TagLib::MPEG::Header::Version2:
+					IF_ ("MPEG version", 2);
+					break;
+				case TagLib::MPEG::Header::Version2_5:
+					F_ ("MPEG version", "2.5");
+					break;
+				}
+
+				IF_ ("MPEG layer", props->layer ());
+				F_ ("Protected", props->protectionEnabled () ? "true" : "false");
+				F_ ("Copyrighted", props->isCopyrighted () ? "true" : "false");
+				F_ ("Original", props->isOriginal () ? "true" : "false");
+
+				switch (props->channelMode ())
+				{
+				case TagLib::MPEG::Header::Stereo:
+					F_ ("Channel mode", "Stereo");
+					break;
+				case TagLib::MPEG::Header::JointStereo:
+					F_ ("Channel mode", "Joint Stereo");
+					break;
+				case TagLib::MPEG::Header::DualChannel:
+					F_ ("Channel mode", "Dual Mono");
+					break;
+				case TagLib::MPEG::Header::SingleChannel:
+					F_ ("Channel mode", "Mono");
+					break;
+				}
+
+				return "MPEG";
+			}
+
+			QString Parse (TagLib::Ogg::Speex::Properties *props)
+			{
+				IF_ ("Speex version", props->speexVersion ());
+				return "Speex";
+			}
+
+			QString Parse (TagLib::Vorbis::Properties *props)
+			{
+				IF_ ("Vorbis version", props->vorbisVersion ());
+				IF_ ("Minimum bitrate", props->bitrateMinimum ());
+				IF_ ("Maximum bitrate", props->bitrateMaximum ());
+				IF_ ("Nominal bitrate", props->bitrateNominal ());
+				return "OGG Vorbis";
+			}
+
+			QString Parse (TagLib::RIFF::AIFF::Properties *props)
+			{
+				IF_ ("Sample width", props->sampleWidth ());
+				return "AIFF";
+			}
+
+			QString Parse (TagLib::RIFF::WAV::Properties *props)
+			{
+				IF_ ("Sample width", props->sampleWidth ());
+				return "WAV";
+			}
+
+			QString Parse (TagLib::TrueAudio::Properties *props)
+			{
+				IF_ ("Bits per sample", props->bitsPerSample ());
+				IF_ ("TTA version", props->ttaVersion ());
+				return "TTA (TrueAudio)";
+			}
+
+			QString Parse (TagLib::WavPack::Properties *props)
+			{
+				IF_ ("Bits per sample", props->bitsPerSample ());
+				IF_ ("WavPack version", props->version ());
+				return "WavPack";
+			}
+		};
+
+		template<typename T>
+		PropsGetter<T> MakeGetter (T f, TagLib::AudioProperties *props)
+		{
+			return PropsGetter<T> (f, props);
 		}
 	}
 
@@ -190,6 +343,20 @@ namespace LMP
 		};
 
 		addMap (GetGenericProps (props));
+
+		boost::mpl::vector<TagLib::APE::Properties*,
+				TagLib::ASF::Properties*,
+				TagLib::FLAC::Properties*,
+				TagLib::MP4::Properties*,
+				TagLib::MPC::Properties*,
+				TagLib::MPEG::Properties*,
+				TagLib::Ogg::Speex::Properties*,
+				TagLib::Vorbis::Properties*,
+				TagLib::RIFF::AIFF::Properties*,
+				TagLib::RIFF::WAV::Properties*,
+				TagLib::TrueAudio::Properties*,
+				TagLib::WavPack::Properties*> propsTypes;
+		boost::mpl::for_each (MakeGetter (append, props), &propsTypes);
 	}
 
 	void NowPlayingWidget::resetSimilarArtists ()
