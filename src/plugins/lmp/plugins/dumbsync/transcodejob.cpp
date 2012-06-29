62e0ac21d3680f7b2d9187250f4f2e02f8477a50
0xd34df00d <0xd34df00d@gmail.com>
2012-06-29 15:56:10 +0400

    LMP DS: TranscodeJob.

---
+++ src/plugins/lmp/plugins/dumbsync/transcodejob.cpp
@@ -0,0 +1,86 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "transcodejob.h"
+#include <stdexcept>
+#include <functional>
+#include <QMap>
+#include <QDir>
+#include <QtDebug>
+#include "transcodingparams.h"
+
+namespace LeechCraft
+{
+namespace LMP
+{
+namespace DumbSync
+{
+	namespace
+	{
+		QStringList OggParams (const TranscodingParams& params)
+		{
+			return { "-acodec", "libvorbis", "-aq", QString::number (params.Quality_) };
+		}
+	}
+
+	TranscodeJob::TranscodeJob (const QString& path, const TranscodingParams& params, QObject* parent)
+	: QObject (parent)
+	, Process_ (new QProcess (this))
+	{
+		QMap<QString, std::function<QStringList (TranscodingParams)>> trans;
+		trans ["ogg"] = OggParams;
+
+		QDir dir = QDir::temp ();
+		if (!dir.exists ("lmp_transcode"))
+			dir.mkdir ("lmp_transcode");
+		if (!dir.cd ("lmp_transcode"))
+			throw std::runtime_error ("unable to cd into temp dir");
+
+		const QFileInfo fi (path);
+		auto targetPath = dir.absoluteFilePath (fi.fileName () + '.' + params.Format_);
+
+		QStringList args;
+		args << "-i" << path;
+		args << trans [params.Format_] (params);
+		args << "-map_metadata" << "0";
+		args << targetPath;
+
+		connect (Process_,
+				SIGNAL (finished (int, QProcess::ExitStatus)),
+				this,
+				SLOT (handleFinished (int, QProcess::ExitStatus)));
+		connect (Process_,
+				SIGNAL (readyRead ()),
+				this,
+				SLOT (handleReadyRead ()));
+		Process_->start ("ffmpeg", args);
+	}
+
+	void TranscodeJob::handleFinished (int code, QProcess::ExitStatus status)
+	{
+		qDebug () << Q_FUNC_INFO << code << status;
+		qDebug () << Process_->readAllStandardError ();
+		emit done (!code);
+	}
+
+	void TranscodeJob::handleReadyRead ()
+	{
+	}
+}
+}
+}
