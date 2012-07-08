d40876c94192749c2213ed33d49941e8838abe72
0xd34df00d <0xd34df00d@gmail.com>
2012-07-08 19:09:21 +0400

    Azoth: removed some debugging output.

--- src/plugins/azoth/chattab.cpp
+++ src/plugins/azoth/chattab.cpp
@@ -261,7 +261,6 @@ namespace Azoth
 
 	void ChatTab::TabMadeCurrent ()
 	{
-		qDebug () << Q_FUNC_INFO << GetEntry<ICLEntry> ()->GetEntryName ();
 		Core::Instance ().GetChatTabsManager ()->ChatMadeCurrent (this);
 		Core::Instance ().FrameFocused (GetEntry<QObject> (), Ui_.View_->page ()->mainFrame ());
 
