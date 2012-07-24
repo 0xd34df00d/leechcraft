18ca290d8588f97f15bf68e46b62932db70ae3e6
0xd34df00d <0xd34df00d@gmail.com>
2012-07-24 20:15:54 +0400

    LMP MP3Tunes: added plugin stubs.

---
+++ src/plugins/lmp/plugins/mp3tunes/mp3tunes.cpp
@@ -0,0 +1,74 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "mp3tunes.h"
+#include <QIcon>
+
+namespace LeechCraft
+{
+namespace LMP
+{
+namespace MP3Tunes
+{
+	void Plugin::Init (ICoreProxy_ptr)
+	{
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.LMP.MP3Tunes";
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "LMP MP3Tunes";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Supports using the mp3tunes.com service, including its playlist and locker facilities.");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon ();
+	}
+
+	QSet<QByteArray> Plugin::GetPluginClasses () const
+	{
+		QSet<QByteArray> result;
+		result << "org.LeechCraft.LMP.WebSync";
+		return result;
+	}
+
+	void Plugin::SetLMPProxy (ILMPProxy*)
+	{
+	}
+}
+}
+}
+
+LC_EXPORT_PLUGIN (leechcraft_lmp_mp3tunes, LeechCraft::LMP::MP3Tunes::Plugin);
