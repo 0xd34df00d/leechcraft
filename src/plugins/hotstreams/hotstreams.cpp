c10fdebbf15c9e10f47df9b8fe089a30f7562609
0xd34df00d <0xd34df00d@gmail.com>
2012-08-15 13:11:16 +0400

    HotStreams: plugin stubs.

---
+++ src/plugins/hotstreams/hotstreams.cpp
@@ -0,0 +1,61 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "hotstreams.h"
+#include <QIcon>
+
+namespace LeechCraft
+{
+namespace HotStreams
+{
+	void Plugin::Init (ICoreProxy_ptr proxy)
+	{
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.HotStreams";
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "HotStreams";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Provides some radio streams like Digitally Imported and SomaFM to other plugins.");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon ();
+	}
+}
+}
+
+LC_EXPORT_PLUGIN (leechcraft_hotstreams, LeechCraft::HotStreams::Plugin);
+
