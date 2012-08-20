a38870f4d7eeb0969838d7f79d80b5354eb67fc2
0xd34df00d <0xd34df00d@gmail.com>
2012-08-20 13:51:22 +0400

    LastFMScrobble: added RecEventsFetcher.

---
+++ src/plugins/lastfmscrobble/receventsfetcher.cpp
@@ -0,0 +1,147 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "receventsfetcher.h"
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QLocale>
+#include <QtDebug>
+#include <QDomDocument>
+#include "util.h"
+#include "authenticator.h"
+
+namespace LeechCraft
+{
+namespace Lastfmscrobble
+{
+	RecEventsFetcher::RecEventsFetcher (Authenticator *auth, QNetworkAccessManager *nam, QObject *parent)
+	: QObject (parent)
+	, NAM_ (nam)
+	{
+		if (auth->IsAuthenticated ())
+			request ();
+		else
+			connect (auth,
+					SIGNAL (authenticated ()),
+					this,
+					SLOT (request ()));
+	}
+
+	void RecEventsFetcher::request ()
+	{
+		QMap<QString, QString> params;
+		params ["country"] = "Russia";
+		AddLanguageParam (params);
+		auto reply = Request ("user.getRecommendedEvents", NAM_, params);
+		connect (reply,
+				SIGNAL (finished ()),
+				this,
+				SLOT (handleFinished ()));
+		connect (reply,
+				SIGNAL (error (QNetworkReply::NetworkError)),
+				this,
+				SLOT (handleError ()));
+	}
+
+	namespace
+	{
+		QStringList GetElemsList (const QDomElement& parent, const QString& elemName)
+		{
+			QStringList result;
+			auto elem = parent.firstChildElement (elemName);
+			while (!elem.isNull ())
+			{
+				result << elem.text ();
+				elem = elem.nextSiblingElement (elemName);
+			}
+			return result;
+		}
+	}
+
+	void RecEventsFetcher::handleFinished ()
+	{
+		auto reply = qobject_cast<QNetworkReply*> (sender ());
+		if (!reply)
+			return;
+
+		reply->deleteLater ();
+		deleteLater ();
+
+		const auto& data = reply->readAll ();
+		qDebug () << data;
+
+		QDomDocument doc;
+		if (!doc.setContent (data))
+		{
+			qWarning () << Q_FUNC_INFO
+					<< "error parsing reply";
+			return;
+		}
+
+		Media::EventInfos_t result;
+
+		auto eventElem = doc
+				.documentElement ()
+				.firstChildElement ("events")
+				.firstChildElement ("event");
+		while (!eventElem.isNull ())
+		{
+			auto artistsElem = eventElem.firstChildElement ("artists");
+			auto venueElem = eventElem.firstChildElement ("venue");
+			auto locationElem = venueElem.firstChildElement ("location");
+			auto pointElem = locationElem.firstChildElement ("point");
+
+			Media::EventInfo info =
+			{
+				eventElem.firstChildElement ("title").text (),
+				QString (),
+				QLocale ("en_US").toDateTime (eventElem.firstChildElement ("startDate").text (),
+							"ddd, dd MMM yyyy hh:mm:ss"),
+				eventElem.firstChildElement ("url").text (),
+				GetImage (eventElem, "medium"),
+				GetImage (eventElem, "extralarge"),
+				GetElemsList (artistsElem, "artist"),
+				artistsElem.firstChildElement ("headliner").text (),
+				GetElemsList (eventElem.firstChildElement ("tags"), "tag"),
+				eventElem.firstChildElement ("attendance").text ().toInt (),
+				venueElem.firstChildElement ("name").text (),
+				pointElem.firstChildElement ("lat").text ().toDouble (),
+				pointElem.firstChildElement ("long").text ().toDouble (),
+				locationElem.firstChildElement ("city").text (),
+				locationElem.firstChildElement ("street").text ()
+			};
+			result << info;
+
+			eventElem = eventElem.nextSiblingElement ("event");
+		}
+
+		if (!result.isEmpty ())
+			emit gotRecommendedEvents (result);
+	}
+
+	void RecEventsFetcher::handleError ()
+	{
+		qWarning () << Q_FUNC_INFO
+				<< "error fetching events"
+				<< qobject_cast<QNetworkReply*> (sender ())->errorString ();
+
+		sender ()->deleteLater ();
+		deleteLater ();
+	}
+}
+}
