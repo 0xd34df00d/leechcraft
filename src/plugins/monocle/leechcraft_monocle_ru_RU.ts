73ad94323c07b341b270c55209c801117d0a887b
0xd34df00d <0xd34df00d@gmail.com>
2012-08-26 01:04:08 +0400

    Monocle: updated translations.

--- src/plugins/monocle/leechcraft_monocle_ru_RU.ts
+++ src/plugins/monocle/leechcraft_monocle_ru_RU.ts
@@ -2,84 +2,120 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
+    <name>ChooseBackendDialog</name>
+    <message>
+        <location filename="choosebackenddialog.ui" line="14"/>
+        <source>Choose backend</source>
+        <translation>Выбор модуля</translation>
+    </message>
+    <message>
+        <location filename="choosebackenddialog.ui" line="22"/>
+        <source>Backend to use:</source>
+        <translation>Модуль поддержки формата:</translation>
+    </message>
+    <message>
+        <location filename="choosebackenddialog.ui" line="41"/>
+        <source>Remember my choice</source>
+        <translation>Запомнить выбор</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Monocle::DefaultBackendManager</name>
+    <message>
+        <location filename="defaultbackendmanager.cpp" line="37"/>
+        <source>Backends</source>
+        <translation>Модули поддержки форматов</translation>
+    </message>
+    <message>
+        <location filename="defaultbackendmanager.cpp" line="37"/>
+        <source>Choice</source>
+        <translation>Выбор</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Monocle::DocumentTab</name>
     <message>
-        <location filename="documenttab.cpp" line="69"/>
+        <location filename="documenttab.cpp" line="74"/>
         <source>Table of contents</source>
         <translation>Содержание</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="196"/>
+        <location filename="documenttab.cpp" line="275"/>
         <source>Open...</source>
         <translation>Открыть...</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="205"/>
+        <location filename="documenttab.cpp" line="295"/>
         <source>Print...</source>
         <translation>Печать...</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="213"/>
+        <location filename="documenttab.cpp" line="303"/>
         <source>Presentation...</source>
         <translation>Презентация...</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="223"/>
+        <location filename="documenttab.cpp" line="313"/>
         <source>Previous page</source>
         <translation>Предыдущая страница</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="248"/>
+        <location filename="documenttab.cpp" line="338"/>
         <source>Next page</source>
         <translation>Следующая страница</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="259"/>
+        <location filename="documenttab.cpp" line="349"/>
         <source>Fit width</source>
         <translation>По ширине</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="260"/>
+        <location filename="documenttab.cpp" line="350"/>
         <source>Fit page</source>
         <translation>Страница целиком</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="272"/>
+        <location filename="documenttab.cpp" line="362"/>
         <source>One page</source>
         <translation>Одна страница</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="283"/>
+        <location filename="documenttab.cpp" line="373"/>
         <source>Two pages</source>
         <translation>Две страницы</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="296"/>
+        <location filename="documenttab.cpp" line="386"/>
         <source>Move mode</source>
         <translation>Режим перемещения</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="307"/>
+        <location filename="documenttab.cpp" line="397"/>
         <source>Selection mode</source>
         <translation>Режим выделения</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="368"/>
+        <location filename="documenttab.cpp" line="209"/>
         <source>Unable to open document %1.</source>
         <translation>Невозможно открыть документ %1.</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="523"/>
+        <location filename="documenttab.cpp" line="570"/>
+        <source>Seems like file %1 doesn&apos;t exist anymore.</source>
+        <translation>Похоже, что файл %1 больше не существует.</translation>
+    </message>
+    <message>
+        <location filename="documenttab.cpp" line="581"/>
         <source>Select file</source>
         <translation>Выбор файла</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="668"/>
+        <location filename="documenttab.cpp" line="726"/>
         <source>Copy selection as image</source>
         <translation>Скопировать как изображение.</translation>
     </message>
     <message>
-        <location filename="documenttab.cpp" line="677"/>
+        <location filename="documenttab.cpp" line="735"/>
         <source>Copy selection as text</source>
         <translation>Скопировать как текст</translation>
     </message>
@@ -87,9 +123,60 @@
 <context>
     <name>LeechCraft::Monocle::Plugin</name>
     <message>
-        <location filename="monocle.cpp" line="63"/>
+        <location filename="monocle.cpp" line="77"/>
         <source>Modular document viewer for LeechCraft.</source>
         <translation>Модульный просмотрщик документов для LeechCraft.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Monocle::RecentlyOpenedManager</name>
+    <message>
+        <location filename="recentlyopenedmanager.cpp" line="46"/>
+        <source>Recently opened</source>
+        <translation>Недавно открытые</translation>
+    </message>
+    <message>
+        <location filename="recentlyopenedmanager.cpp" line="102"/>
+        <source>Seems like file %1 doesn&apos;t exist anymore.</source>
+        <translation>Похоже, что файл %1 больше не существует.</translation>
+    </message>
+</context>
+<context>
+    <name>monoclesettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Maximum number of items in Recently Opened list:</source>
+        <translation>Максимальное число записей в списке недавно открытых:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Performance</source>
+        <translation>Производительность</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Pixmap cache size:</source>
+        <translation>Размер кеша изображений:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source> MiB</source>
+        <translation> МиБ</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Default backends</source>
+        <translation>Модули по умолчанию</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Default backends:</source>
+        <translation>Модули поддержки форматов по умолчанию:</translation>
     </message>
 </context>
 </TS>
