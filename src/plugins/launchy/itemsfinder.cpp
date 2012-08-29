f9813041cda84646a2c8d53ac11ae90e8d4bc090
0xd34df00d <0xd34df00d@gmail.com>
2012-08-29 22:37:18 +0400

    Launch: added ItemsFinder.

---
+++ src/plugins/launchy/itemsfinder.cpp
@@ -0,0 +1,119 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "itemsfinder.h"
+#include <QDir>
+#include <QTimer>
+#include <QtDebug>
+#include "item.h"
+
+namespace LeechCraft
+{
+namespace Launchy
+{
+	ItemsFinder::ItemsFinder (ICoreProxy_ptr proxy, QObject *parent)
+	: QObject (parent)
+	, Proxy_ (proxy)
+	{
+		QTimer::singleShot (1000, this, SLOT (update ()));
+	}
+
+	QHash<QString, Item_ptr> ItemsFinder::GetItems () const
+	{
+		return Items_;
+	}
+
+	namespace
+	{
+		QStringList ScanDir (const QString& path)
+		{
+			const auto& infos = QDir (path).entryInfoList (QStringList ("*.desktop"));
+			QStringList result;
+			for (const auto& info : infos)
+				result << info.absoluteFilePath ();
+			return result;
+		}
+
+		QIcon LoadIcon (ICoreProxy_ptr proxy, QString name)
+		{
+			if (name.isEmpty ())
+				return QIcon ();
+
+			if (name.endsWith (".png"))
+				name.chop (4);
+
+			auto result = proxy->GetIcon (name);
+			if (!result.isNull ())
+				return result;
+
+			result = QIcon ("/usr/share/pixmaps/" + name + ".png");
+			if (!result.isNull ())
+				return result;
+
+			result = QIcon ("/usr/local/share/pixmaps/" + name + ".png");
+
+			if (result.isNull ())
+				qDebug () << Q_FUNC_INFO << name << "not found";
+
+			return result;
+		}
+	}
+
+	void ItemsFinder::update ()
+	{
+		qDebug () << Q_FUNC_INFO;
+		Items_.clear ();
+		auto paths = ScanDir ("/usr/share/applications");
+		qDebug () << "scanned";
+
+		for (const auto& path : paths)
+		{
+			Item_ptr item;
+			try
+			{
+				item = Item::FromDesktopFile (path);
+			}
+			catch (const std::exception& e)
+			{
+				qWarning () << Q_FUNC_INFO
+						<< "error parsing"
+						<< path
+						<< e.what ();
+				continue;
+			}
+			if (!item->IsValid ())
+			{
+				qWarning () << Q_FUNC_INFO
+						<< "invalid item"
+						<< path;
+				continue;
+			}
+
+			item->SetIcon (LoadIcon (Proxy_, item->GetIconName ()));
+
+			for (const auto& cat : item->GetCategories ())
+				Items_ [cat] = item;
+		}
+
+		qDebug () << Items_.keys ();
+		qDebug () << "done";
+
+		emit itemsListChanged ();
+	}
+}
+}
---
+++ src/plugins/launchy/itemsfinder.h
@@ -0,0 +1,49 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#pragma once
+
+#include <memory>
+#include <QObject>
+#include <QHash>
+#include <interfaces/core/icoreproxy.h>
+
+namespace LeechCraft
+{
+namespace Launchy
+{
+	class Item;
+	typedef std::shared_ptr<Item> Item_ptr;
+
+	class ItemsFinder : public QObject
+	{
+		Q_OBJECT
+
+		ICoreProxy_ptr Proxy_;
+		QHash<QString, Item_ptr> Items_;
+	public:
+		ItemsFinder (ICoreProxy_ptr, QObject* = 0);
+
+		QHash<QString, Item_ptr> GetItems () const;
+	public slots:
+		void update ();
+	signals:
+		void itemsListChanged ();
+	};
+}
+}
