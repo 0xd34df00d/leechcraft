9cf0221d8effd6e1a6cc9780b0f8c380348619d1
0xd34df00d <0xd34df00d@gmail.com>
2012-09-09 20:30:26 +0400

    Azoth: added ContactDropFilter.

---
+++ src/plugins/azoth/contactdropfilter.cpp
@@ -0,0 +1,55 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "contactdropfilter.h"
+#include <QDropEvent>
+#include <QImage>
+#include <QUrl>
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+	ContactDropFilter::ContactDropFilter (QObject *parent)
+	: QObject (parent)
+	{
+	}
+
+	bool ContactDropFilter::eventFilter (QObject *obj, QEvent *e)
+	{
+		if (e->type () != QEvent::Drop)
+			return false;
+
+		auto data = static_cast<QDropEvent*> (e)->mimeData ();
+		const auto& imgData = data->imageData ();
+
+		if (data->hasImage () && data->hasUrls () && data->urls ().size () == 1)
+			emit localImageDropped (imgData.value<QImage> (), data->urls ().value (0));
+		else if (data->hasImage ())
+			emit imageDropped (imgData.value<QImage> ());
+		else if (data->hasUrls ())
+		{
+			const auto& urls = data->urls ();
+			if (!urls.isEmpty ())
+				emit filesDropped (urls);
+		}
+
+		return true;
+	}
+}
+}
