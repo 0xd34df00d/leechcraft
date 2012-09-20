205a16687fd06031717fba69a7d6312374dfc4dd
0xd34df00d <0xd34df00d@gmail.com>
2012-09-20 04:12:30 +0400

    Poshuku CW: removed some debugging output.

--- src/plugins/poshuku/plugins/cleanweb/core.cpp
+++ src/plugins/poshuku/plugins/cleanweb/core.cpp
@@ -291,11 +291,7 @@ namespace CleanWeb
 			return 0;
 
 		if (req.url ().scheme () == "data")
-		{
-			qDebug () << Q_FUNC_INFO
-				<< "not checking data: urls";
 			return 0;
-		}
 
 		QString matched;
 		if (!ShouldReject (req, &matched))
