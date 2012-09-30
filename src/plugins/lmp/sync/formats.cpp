e02c3664488472f88dcb036624dba535487c580d
0xd34df00d <0xd34df00d@gmail.com>
2012-10-01 03:19:28 +0400

    LMP: added Formats DB.

---
+++ src/plugins/lmp/sync/formats.cpp
@@ -0,0 +1,97 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "formats.h"
+#include <QtDebug>
+#include "transcodingparams.h"
+
+namespace LeechCraft
+{
+namespace LMP
+{
+	class OggFormat : public Format
+	{
+	public:
+		QString GetFormatID () const
+		{
+			return "ogg";
+		}
+
+		QString GetFormatName () const
+		{
+			return "OGG Vorbis";
+		}
+
+		QList<BitrateType> GetSupportedBitrates() const
+		{
+			return { BitrateType::VBR, BitrateType::CBR };
+		}
+
+		QList<int> GetBitrateLabels (BitrateType type) const
+		{
+			switch (type)
+			{
+			case BitrateType::CBR:
+				return { 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 500 };
+			case BitrateType::VBR:
+				return { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			}
+
+			qWarning () << Q_FUNC_INFO
+					<< "unknown bitrate type";
+			return QList<int> ();
+		}
+
+		QStringList ToFFmpeg (const TranscodingParams& params) const
+		{
+			QStringList result;
+			result << "-acodec" << "libvorbis";
+			switch (params.BitrateType_)
+			{
+			case BitrateType::CBR:
+				result << "-ab"
+						<< (QString::number (params.Quality_) + "k");
+				break;
+			case BitrateType::VBR:
+				result << "-aq"
+						<< QString::number (params.Quality_);
+				break;
+			}
+		}
+	};
+
+	Formats::Formats ()
+	{
+		Formats_ << Format_ptr (new OggFormat);
+	}
+
+	QList<Format_ptr> Formats::GetFormats () const
+	{
+		return Formats_;
+	}
+
+	Format_ptr Formats::GetFormat (const QString& id) const
+	{
+		const auto pos = std::find_if (Formats_.begin (), Formats_.end (),
+				[&id] (const Format_ptr format) { return format->GetFormatID () == id; });
+		return pos == Formats_.end () ?
+				Format_ptr () :
+				*pos;
+	}
+}
+}
---
+++ src/plugins/lmp/sync/formats.h
@@ -0,0 +1,58 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#pragma once
+
+#include <memory>
+#include <QStringList>
+
+namespace LeechCraft
+{
+namespace LMP
+{
+	class TranscodingParams;
+
+	class Format
+	{
+	public:
+		virtual QString GetFormatID () const = 0;
+		virtual QString GetFormatName () const = 0;
+
+		enum class BitrateType
+		{
+			VBR,
+			CBR
+		};
+		virtual QList<BitrateType> GetSupportedBitrates () const = 0;
+		virtual QList<int> GetBitrateLabels (BitrateType) const = 0;
+
+		virtual QStringList ToFFmpeg (const TranscodingParams&) const = 0;
+	};
+	typedef std::shared_ptr<Format> Format_ptr;
+
+	class Formats
+	{
+		QList<Format_ptr> Formats_;
+	public:
+		Formats ();
+
+		QList<Format_ptr> GetFormats () const;
+		Format_ptr GetFormat (const QString&) const;
+	};
+}
+}
