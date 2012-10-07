fa5d01dbc193ded06f336b51dea65615209698eb
0xd34df00d <0xd34df00d@gmail.com>
2012-10-07 21:24:36 +0400

    Interfaces: added IDataFilter.

---
+++ src/interfaces/idatafilter.h
@@ -0,0 +1,51 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#pragma once
+
+#include <QtPlugin>
+#include <QByteArray>
+#include <QString>
+#include <QList>
+
+/** @brief Base interface for data filter plugins.
+ *
+ * Data filter plugins provide some means to manipulate and alter data.
+ * Examples of such plugins are image uploaders to various image bins,
+ * text finders, etc.
+ *
+ * Plugins implementing this interface are also expected to implement
+ * IEntityHandler, considering (and accepting) entities with MIME
+ * "x-leechcraft/data-filter".
+ */
+class IDataFilter
+{
+public:
+	struct FilterVariant
+	{
+		QByteArray ID_;
+		QString Name_;
+		QString Description_;
+	};
+
+	virtual ~IDataFilter () {}
+
+	virtual QList<FilterVariant> GetFilterVariants () const = 0;
+};
+
+Q_DECLARE_INTERFACE (IDataFilter, "org.Deviant.LeechCraft.IDataFilter/1.0");
