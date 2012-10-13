8e875aed9b32cddad6c63787bf196dfe3b0f2ce3
0xd34df00d <0xd34df00d@gmail.com>
2012-10-13 18:11:42 +0400

    BitTorrent: added toolbuttons to to the tab as well.

--- src/plugins/bittorrent/torrenttab.h
+++ src/plugins/bittorrent/torrenttab.h
@@ -38,6 +38,24 @@ namespace BitTorrent
 
 		const TabClassInfo TC_;
 		QObject *ParentMT_;
+
+		QToolBar *Toolbar_;
+		QAction *OpenTorrent_;
+		QAction *RemoveTorrent_;
+		QAction *Resume_;
+		QAction *Stop_;
+		QAction *CreateTorrent_;
+		QAction *MoveUp_;
+		QAction *MoveDown_;
+		QAction *MoveToTop_;
+		QAction *MoveToBottom_;
+		QAction *ForceReannounce_;
+		QAction *ForceRecheck_;
+		QAction *OpenMultipleTorrents_;
+		QAction *IPFilter_;
+		QAction *MoveFiles_;
+		QAction *ChangeTrackers_;
+		QAction *MakeMagnetLink_;
 	public:
 		TorrentTab (const TabClassInfo&, QObject*);
 
@@ -45,8 +63,30 @@ namespace BitTorrent
 		QObject* ParentMultiTabs ();
 		QToolBar* GetToolBar () const;
 		void Remove ();
+	private:
+		int GetCurrentTorrent () const;
+		QList<int> GetSelectedRows () const;
+		QModelIndexList GetSelectedRowIndexes () const;
 	private slots:
 		void handleTorrentSelected (const QModelIndex&);
+		void setActionsEnabled ();
+
+		void on_OpenTorrent__triggered ();
+		void on_OpenMultipleTorrents__triggered ();
+		void on_IPFilter__triggered ();
+		void on_CreateTorrent__triggered ();
+		void on_RemoveTorrent__triggered ();
+		void on_Resume__triggered ();
+		void on_Stop__triggered ();
+		void on_MoveUp__triggered ();
+		void on_MoveDown__triggered ();
+		void on_MoveToTop__triggered ();
+		void on_MoveToBottom__triggered ();
+		void on_ForceReannounce__triggered ();
+		void on_ForceRecheck__triggered ();
+		void on_ChangeTrackers__triggered ();
+		void on_MoveFiles__triggered ();
+		void on_MakeMagnetLink__triggered ();
 	signals:
 		void removeTab (QWidget*);
 	};
