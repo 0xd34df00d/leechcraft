202ab1d2ee0a8dc48cf4951bb686136cc6d234ce
0xd34df00d <0xd34df00d@gmail.com>
2012-10-28 13:26:45 +0400

    TPI: added InfoModelManager.

---
+++ src/plugins/tpi/infomodelmanager.h
@@ -0,0 +1,45 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#pragma once
+
+#include <QObject>
+#include <interfaces/core/icoreproxy.h>
+
+class QStandardItemModel;
+class QAbstractItemModel;
+
+namespace LeechCraft
+{
+namespace TPI
+{
+	class InfoModelManager : public QObject
+	{
+		Q_OBJECT
+
+		ICoreProxy_ptr Proxy_;
+		QStandardItemModel *Model_;
+	public:
+		InfoModelManager (ICoreProxy_ptr, QObject* = 0);
+
+		QAbstractItemModel* GetModel () const;
+
+		void SecondInit ();
+	};
+}
+}
