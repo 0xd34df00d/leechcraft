c4309122aae7cc72053d16dafbd53c841029554d
0xd34df00d <0xd34df00d@gmail.com>
2012-11-01 19:34:53 +0400

    Azoth SHX: plugin stubs.

---
+++ src/plugins/azoth/plugins/shx/shx.cpp
@@ -0,0 +1,72 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include "shx.h"
+
+namespace LeechCraft
+{
+namespace Azoth
+{
+namespace SHX
+{
+	void Plugin::Init (ICoreProxy_ptr proxy)
+	{
+	}
+
+	void Plugin::SecondInit ()
+	{
+	}
+
+	QByteArray Plugin::GetUniqueID () const
+	{
+		return "org.LeechCraft.Azoth.SHX";
+	}
+
+	void Plugin::Release ()
+	{
+	}
+
+	QString Plugin::GetName () const
+	{
+		return "Azoth SHX";
+	}
+
+	QString Plugin::GetInfo () const
+	{
+		return tr ("Allows one to execute arbitrary shell commands and paste their result.");
+	}
+
+	QIcon Plugin::GetIcon () const
+	{
+		return QIcon ();
+	}
+
+	QSet<QByteArray> Plugin::GetPluginClasses () const
+	{
+		QSet<QByteArray> result;
+		result << "org.LeechCraft.Plugins.Azoth.Plugins.IGeneralPlugin";
+		return result;
+	}
+
+	void Plugin::hookMessageWillCreated (LeechCraft::IHookProxy_ptr proxy,
+			QObject*, QObject *entry, int, QString text)
+	{
+	}
+}
+}
+}
