e66330015f20f23adf0c4fd5b11bfd7202e46c9b
0xd34df00d <0xd34df00d@gmail.com>
2012-12-16 14:37:45 +0400

    TS: added AuthManager.

---
+++ src/plugins/touchstreams/authmanager.h
@@ -0,0 +1,58 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2012  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#pragma once
+
+#include <QObject>
+#include <QDateTime>
+#include <interfaces/core/icoreproxy.h>
+
+class QUrl;
+
+namespace LeechCraft
+{
+namespace TouchStreams
+{
+	class AuthManager : public QObject
+	{
+		Q_OBJECT
+
+		ICoreProxy_ptr Proxy_;
+
+		QString Token_;
+		QDateTime ReceivedAt_;
+		qint32 ValidFor_;
+
+		bool IsRequesting_;
+	public:
+		AuthManager (ICoreProxy_ptr, QObject* = 0);
+
+		void GetAuthKey ();
+	private:
+		void HandleError ();
+		void RequestURL (const QUrl&);
+		void RequestAuthKey ();
+		bool CheckIsBlank (QUrl);
+	private slots:
+		void handleGotForm ();
+		void handleFormFetchError ();
+	signals:
+		void gotAuthKey (const QString&);
+	};
+}
+}
