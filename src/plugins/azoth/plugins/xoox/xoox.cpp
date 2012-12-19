5e2d9783b888783b31cef1f56bda7b95f2715422
0xd34df00d <0xd34df00d@gmail.com>
2012-12-20 00:44:14 +0400

    Azoth Xoox: removed some old unused functions.

--- src/plugins/azoth/plugins/xoox/xoox.cpp
+++ src/plugins/azoth/plugins/xoox/xoox.cpp
@@ -80,25 +80,6 @@ namespace Xoox
 		return icon;
 	}
 
-	QStringList Plugin::Provides () const
-	{
-		return QStringList ();
-	}
-
-	QStringList Plugin::Needs () const
-	{
-		return QStringList ();
-	}
-
-	QStringList Plugin::Uses () const
-	{
-		return QStringList ();
-	}
-
-	void Plugin::SetProvider (QObject*, const QString&)
-	{
-	}
-
 	QSet<QByteArray> Plugin::GetPluginClasses () const
 	{
 		QSet<QByteArray> classes;
