611a8d71e370540a57f60d9a3bf9f9364b85164c
0xd34df00d <0xd34df00d@gmail.com>
2012-12-21 14:48:52 +0400

    DeadLyrics: get rid of settings (they are empty now).

--- src/plugins/deadlyrics/deadlyrics.h
+++ src/plugins/deadlyrics/deadlyrics.h
@@ -23,7 +23,6 @@
 #include <QTranslator>
 #include <interfaces/iinfo.h>
 #include <interfaces/ifinder.h>
-#include <interfaces/ihavesettings.h>
 #include <interfaces/media/ilyricsfinder.h>
 #include "searcher.h"
 
@@ -33,13 +32,11 @@ namespace DeadLyrics
 {
 	class DeadLyRicS : public QObject
 						, public IInfo
-						, public IHaveSettings
 						, public Media::ILyricsFinder
 	{
 		Q_OBJECT
-		Q_INTERFACES (IInfo IHaveSettings Media::ILyricsFinder)
+		Q_INTERFACES (IInfo Media::ILyricsFinder)
 
-		Util::XmlSettingsDialog_ptr SettingsDialog_;
 		ICoreProxy_ptr Proxy_;
 		Searchers_t Searchers_;
 	public:
@@ -50,8 +47,6 @@ namespace DeadLyrics
 		QString GetName () const;
 		QString GetInfo () const;
 		QIcon GetIcon () const;
-
-		Util::XmlSettingsDialog_ptr GetSettingsDialog () const;
 
 		void RequestLyrics (const Media::LyricsQuery&, Media::QueryOptions);
 	signals:
