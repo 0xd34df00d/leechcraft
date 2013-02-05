e96d1af6e99f244cc1bb74a46e97c9516a7829c9
0xd34df00d <0xd34df00d@gmail.com>
2013-02-06 00:54:58 +0400

    Util: FitRectScreen shifts instead of mirroring.

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -356,13 +356,13 @@ QVariantList LeechCraft::Util::GetPersistentData (const QList<QVariant>& keys,
 QPoint LeechCraft::Util::FitRectScreen (QPoint pos, const QSize& size)
 {
 	const QRect& geometry = QApplication::desktop ()->screenGeometry (pos);
-	const bool dropDown = pos.y () + size.height () < geometry.height () + geometry.y ();
-	const bool dropRight = pos.x () + size.width () < geometry.width () + geometry.x ();
+	const int dropDownDiff = pos.y () + size.height () - (geometry.height () + geometry.y ());
+	const int dropRightDiff = pos.x () + size.width () - (geometry.width () + geometry.x ());
 
-	if (!dropDown)
-		pos.ry () -= size.height ();
-	if (!dropRight)
-		pos.rx () -= size.width ();
+	if (dropDownDiff > 0)
+		pos.ry () -= dropDownDiff;
+	if (dropRightDiff > 0)
+		pos.rx () -= dropRightDiff;
 
 	return pos;
 }
