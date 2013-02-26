0c495e868b02b69b6597dbe6059f7dd9b9aeb784
0xd34df00d <0xd34df00d@gmail.com>
2013-02-26 19:59:00 +0400

    Azoth Modnok: not so silent script.

--- src/plugins/azoth/plugins/modnok/lc_azoth_modnok_latexconvert.sh
+++ src/plugins/azoth/plugins/modnok/lc_azoth_modnok_latexconvert.sh
@@ -192,7 +192,7 @@ cd $tmpdir
 for f in $homedir/*.eps; do
     test -f ${f##*/} || ln -s $f . # multi-processing!
 done
-latex -interaction=batchmode -halt-on-error out.tex > /dev/null
+latex -interaction=batchmode -halt-on-error out.tex
 [ ! -e out.dvi ] && exit 1;
 cd "$homedir"
 dvips -o $tmpdir/out.eps -E $tmpdir/out.dvi 2> /dev/null
