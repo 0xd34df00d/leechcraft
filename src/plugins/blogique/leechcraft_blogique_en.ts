fd0599b71336bb745ee754271e239e72c67f9746
0xd34df00d <0xd34df00d@gmail.com>
2013-03-06 11:00:50 +0400

    Blogique: cleaned up CMakeLists.txt & added translations.

---
+++ src/plugins/blogique/leechcraft_blogique_en.ts
@@ -0,0 +1,471 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>AccountsListWidget</name>
+    <message>
+        <location filename="accountslistwidget.ui" line="14"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.ui" line="46"/>
+        <source>Add...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.ui" line="60"/>
+        <source>Modify...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.ui" line="67"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.ui" line="81"/>
+        <source>Profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AccountsSelectDialog</name>
+    <message>
+        <location filename="accountsselectdialog.ui" line="14"/>
+        <source>Backup accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountsselectdialog.ui" line="23"/>
+        <source>Select blogs you want to backup (only these blogs support backup):</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AddAccountWizardFirstPage</name>
+    <message>
+        <location filename="addaccountwizardfirstpage.ui" line="17"/>
+        <source>Account name and type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addaccountwizardfirstpage.ui" line="20"/>
+        <source>Enter the name of the account to be created and select the protocol for which you would like to create the account.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addaccountwizardfirstpage.ui" line="29"/>
+        <source>Account name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addaccountwizardfirstpage.ui" line="39"/>
+        <source>Blogging platform</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addaccountwizardfirstpage.ui" line="53"/>
+        <source>Register new account</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BlogEntriesWidget</name>
+    <message>
+        <location filename="blogentrieswidget.ui" line="43"/>
+        <source>Filter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.ui" line="75"/>
+        <source>Show calendar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.ui" line="93"/>
+        <source>Show last entries.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.ui" line="96"/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.ui" line="106"/>
+        <source>Post will be removed from local storage and from user&apos;s blog.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.ui" line="109"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BlogiqueWidget</name>
+    <message>
+        <location filename="blogiquewidget.ui" line="36"/>
+        <source>Title:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.ui" line="103"/>
+        <source>Blog posts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.ui" line="115"/>
+        <source>Save as draft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.ui" line="120"/>
+        <source>Open in browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.ui" line="125"/>
+        <source>Submit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.ui" line="130"/>
+        <source>Update profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.ui" line="135"/>
+        <source>Save as a new draft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.ui" line="138"/>
+        <source>Save entry as a new draft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.ui" line="143"/>
+        <source>New post</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DraftEntriesWidget</name>
+    <message>
+        <location filename="draftentrieswidget.ui" line="37"/>
+        <source>Filter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.ui" line="66"/>
+        <source>Show calendar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.ui" line="76"/>
+        <source>Remove draft from local storage.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.ui" line="79"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.ui" line="86"/>
+        <source>Publish draft to user&apos;s blog.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.ui" line="89"/>
+        <source>Publish</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::AccountsListWidget</name>
+    <message>
+        <location filename="accountslistwidget.cpp" line="68"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.cpp" line="68"/>
+        <location filename="accountslistwidget.cpp" line="92"/>
+        <location filename="accountslistwidget.cpp" line="178"/>
+        <source>Validated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.cpp" line="93"/>
+        <location filename="accountslistwidget.cpp" line="179"/>
+        <source>Not validated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.cpp" line="109"/>
+        <location filename="accountslistwidget.cpp" line="148"/>
+        <source>LeechCraft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.cpp" line="110"/>
+        <source>Error adding account.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.cpp" line="149"/>
+        <source>Error removing account.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.cpp" line="187"/>
+        <source>Add account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.cpp" line="223"/>
+        <source>Are you sure you want to remove the account %1?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::AccountsSelectDialog</name>
+    <message>
+        <location filename="accountsselectdialog.cpp" line="34"/>
+        <source>Account name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::AddAccountWizardFirstPage</name>
+    <message>
+        <location filename="addaccountwizardfirstpage.cpp" line="120"/>
+        <source>%1 options</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::BlogEntriesWidget</name>
+    <message>
+        <location filename="blogentrieswidget.cpp" line="61"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.cpp" line="61"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.cpp" line="65"/>
+        <source>Open in new tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.cpp" line="66"/>
+        <source>Open here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.cpp" line="86"/>
+        <source>Blog entries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogentrieswidget.cpp" line="222"/>
+        <source>Are you sure you want to delete entry %1 from the blog?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::BlogiqueWidget</name>
+    <message>
+        <location filename="blogiquewidget.cpp" line="351"/>
+        <source>LeechCraft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.cpp" line="352"/>
+        <source>Entry can&apos;t be emprty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.cpp" line="481"/>
+        <source>You have unsaved changes in your current tab. Do you want to open this entry in a new tab instead?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.cpp" line="523"/>
+        <source>Do you want to save current entry?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.cpp" line="621"/>
+        <source>Do you want to update entry or to post new one?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.cpp" line="625"/>
+        <source>Update post</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogiquewidget.cpp" line="626"/>
+        <source>Post new</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::Core</name>
+    <message>
+        <location filename="core.cpp" line="282"/>
+        <source>Entry was posted successfully:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="297"/>
+        <source>Entry was removed successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="312"/>
+        <source>Entry was updated successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="328"/>
+        <source>Entries were backuped successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::DraftEntriesWidget</name>
+    <message>
+        <location filename="draftentrieswidget.cpp" line="63"/>
+        <source>Open in new tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.cpp" line="64"/>
+        <source>Open here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.cpp" line="65"/>
+        <source>Show all entries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.cpp" line="67"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.cpp" line="67"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.cpp" line="94"/>
+        <source>Drafts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.cpp" line="269"/>
+        <source>Do you want to delete selected entry?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="draftentrieswidget.cpp" line="274"/>
+        <source>Always</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::Plugin</name>
+    <message>
+        <location filename="blogique.cpp" line="76"/>
+        <source>Backup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="blogique.cpp" line="110"/>
+        <source>Blogging client</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProfileDialog</name>
+    <message>
+        <location filename="profiledialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UpdateEntriesDialog</name>
+    <message>
+        <location filename="updateentriesdialog.ui" line="14"/>
+        <source>Select update type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="updateentriesdialog.ui" line="49"/>
+        <source>Number of entries to fetch:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="updateentriesdialog.ui" line="81"/>
+        <source>Don&apos;t ask again</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>blogiquesettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Behavior</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Local entries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Open draft by double click in:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Current tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <location filename="dummy.cpp" line="11"/>
+        <source>New tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>RemotePosts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>Open entry by double click in:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
