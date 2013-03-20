493f6cf1b7da6bf8b0b204d050f9feba2f434157
0xd34df00d <0xd34df00d@gmail.com>
2013-03-20 16:46:41 +0400

    Kinotify: updated translations.

--- src/plugins/kinotify/leechcraft_kinotify_en.ts
+++ src/plugins/kinotify/leechcraft_kinotify_en.ts
@@ -2,9 +2,9 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
-    <name>LeechCraft::Plugins::Kinotify::Plugin</name>
+    <name>LeechCraft::Kinotify::Plugin</name>
     <message>
-        <location filename="kinotify.cpp" line="71"/>
+        <location filename="kinotify.cpp" line="78"/>
         <source>Fancy Kinetic notifications.</source>
         <translation>Fancy Kinetic notifications.</translation>
     </message>
@@ -13,8 +13,8 @@
     <name>kinotifysettings</name>
     <message>
         <location filename="dummy.cpp" line="2"/>
-        <source>Appearance</source>
-        <translation>Appearance</translation>
+        <source>Kinotify</source>
+        <translation>Kinotify</translation>
     </message>
     <message>
         <location filename="dummy.cpp" line="3"/>
@@ -28,8 +28,53 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="5"/>
-        <source>Adaptive</source>
-        <translation>Adaptive</translation>
+        <source>Commie</source>
+        <translation>Commie</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Behavior</source>
+        <translation>Behavior</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Don&apos;t show notifications when there is a fullscreen application</source>
+        <translation>Don&apos;t show notifications when there is a fullscreen application</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Show notification on the screen containing the mouse</source>
+        <translation>Show notification on the screen containing the mouse</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>If this option is disabled notifications are shown on the screen containing the LeechCraft window.</source>
+        <translation>If this option is disabled notifications are shown on the screen containing the LeechCraft window.</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Notificator position:</source>
+        <translation>Notificator position:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Top left</source>
+        <translation>Top left</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="12"/>
+        <source>Top right</source>
+        <translation>Top right</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <source>Bottom left</source>
+        <translation>Bottom left</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="14"/>
+        <source>Bottom right</source>
+        <translation>Bottom right</translation>
     </message>
 </context>
 </TS>
--- src/plugins/kinotify/leechcraft_kinotify_ru_RU.ts
+++ src/plugins/kinotify/leechcraft_kinotify_ru_RU.ts
@@ -2,19 +2,19 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
-    <name>LeechCraft::Plugins::Kinotify::Plugin</name>
+    <name>LeechCraft::Kinotify::Plugin</name>
     <message>
-        <location filename="kinotify.cpp" line="71"/>
+        <location filename="kinotify.cpp" line="78"/>
         <source>Fancy Kinetic notifications.</source>
-        <translation>Красивые Kinetic-уведомления.</translation>
+        <translation>Красивые плавные уведомления.</translation>
     </message>
 </context>
 <context>
     <name>kinotifysettings</name>
     <message>
         <location filename="dummy.cpp" line="2"/>
-        <source>Appearance</source>
-        <translation>Внешний вид</translation>
+        <source>Kinotify</source>
+        <translation>Kinotify</translation>
     </message>
     <message>
         <location filename="dummy.cpp" line="3"/>
@@ -28,8 +28,53 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="5"/>
-        <source>Adaptive</source>
-        <translation>Adaptive</translation>
+        <source>Commie</source>
+        <translation>Commie</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Behavior</source>
+        <translation>Поведение</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Don&apos;t show notifications when there is a fullscreen application</source>
+        <translation>Не показывать уведомления при наличии полноэкранного приложения</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Show notification on the screen containing the mouse</source>
+        <translation>Показывать уведомления на экране, содержащем курсор мыши</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>If this option is disabled notifications are shown on the screen containing the LeechCraft window.</source>
+        <translation>Если эта опция отключена, уведомления будут показаны на экране, содержащем окно LeechCraft.</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Notificator position:</source>
+        <translation>Позиция уведомления:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Top left</source>
+        <translation>Слева сверху</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="12"/>
+        <source>Top right</source>
+        <translation>Справа сверху</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <source>Bottom left</source>
+        <translation>Слева снизу</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="14"/>
+        <source>Bottom right</source>
+        <translation>Справа снизу</translation>
     </message>
 </context>
 </TS>
