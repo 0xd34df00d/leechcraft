c39e7897393ba6314e7a09a62af1136a62dfccb9
0xd34df00d <0xd34df00d@gmail.com>
2013-04-01 12:02:11 +0400

    Interfaces: documented IArtistBioFetcher.

--- src/interfaces/media/iartistbiofetcher.h
+++ src/interfaces/media/iartistbiofetcher.h
@@ -24,31 +24,107 @@ class QObject;
 
 namespace Media
 {
+	/** @brief Information about artist biography.
+	 *
+	 * For now this structure only contains basic information about the
+	 * artist, duplicating ArtistInfo. This may be changed/extended some
+	 * time in the future, though.
+	 *
+	 * @sa ArtistInfo
+	 */
 	struct ArtistBio
 	{
+		/** Basic information about this artist.
+		 */
 		ArtistInfo BasicInfo_;
 	};
 
+	/** @brief Pending biography request handle.
+	 *
+	 * A descendant of this class is returned from the
+	 * IArtistBioFetcher::RequestArtistBio() method and is used to track
+	 * the status of biography requests.
+	 *
+	 * This class has some signals (ready() and error()), and one can use
+	 * the GetQObject() method to get an object of this class as a
+	 * QObject and connect to those signals.
+	 *
+	 * @note The object of this class should schedule its deletion (via
+	 * <code>QObject::deleteLater()</code>, for example) after ready() or
+	 * error() signal is emitted. Thus the calling code should never
+	 * delete it explicitly, neither it should use this object after
+	 * ready() or error() signals or connect to this signals via
+	 * <code>Qt::QueuedConnection</code>.
+	 */
 	class Q_DECL_EXPORT IPendingArtistBio
 	{
 	public:
 		virtual ~IPendingArtistBio () {}
 
+		/** @brief Returns this object as a QObject.
+		 *
+		 * This function can be used to connect to the signals of this
+		 * class.
+		 *
+		 * @return This object as a QObject.
+		 */
 		virtual QObject* GetQObject () = 0;
 
+		/** @brief Returns the artist biography.
+		 *
+		 * This function returns the fetched artist biography, or an
+		 * empty biography if it is not found or search isn't completed
+		 * yet.
+		 *
+		 * @return The fetched artist biography.
+		 */
 		virtual ArtistBio GetArtistBio () const = 0;
 	protected:
+		/** @brief Emitted when the biography is ready and fetched.
+		 *
+		 * The object will be invalid after this signal is emitted and
+		 * the event loop is run.
+		 */
 		virtual void ready () = 0;
+
+		/** @brief Emitted when there is an error fetching the biography.
+		 *
+		 * The object will be invalid after this signal is emitted and
+		 * the event loop is run.
+		 */
 		virtual void error () = 0;
 	};
 
+	/** @brief Interface for plugins supporting fetching artist biography.
+	 *
+	 * Plugins that support fetching artist biography from the sources
+	 * Last.FM should implement this interface.
+	 */
 	class Q_DECL_EXPORT IArtistBioFetcher
 	{
 	public:
 		virtual ~IArtistBioFetcher () {}
 
+		/** @brief Returns the service name.
+		 *
+		 * This string returns a human-readable string with the service
+		 * name, like "Last.FM".
+		 *
+		 * @return The human-readable service name.
+		 */
 		virtual QString GetServiceName () const = 0;
 
+		/** @brief Requests the biography of the given artist.
+		 *
+		 * This function initiates a search for artist biography and
+		 * returns a handle through which the results of the search could
+		 * be obtained. The handle owns itself and deletes itself after
+		 * results are available — see its documentation for more
+		 * information.
+		 *
+		 * @param[in] artist The artist name.
+		 * @return The pending biography search handle.
+		 */
 		virtual IPendingArtistBio* RequestArtistBio (const QString& artist) = 0;
 	};
 }
