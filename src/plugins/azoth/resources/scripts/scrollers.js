4a83fc36dd9f554192dc0b1743ac88b54556e9f7
0xd34df00d <0xd34df00d@gmail.com>
2013-05-20 01:33:38 +0400

    Azoth: fixed viewport resize scrolling.

--- src/plugins/azoth/resources/scripts/scrollers.js
+++ src/plugins/azoth/resources/scripts/scrollers.js
@@ -3,11 +3,11 @@ function ScrollToBottom() {
 		window.scroll(0, document.height - window.innerHeight + 100);
 }
 function TestScroll() {
-	window.ShouldScroll = document.height <= (window.innerHeight + window.pageYOffset + window.innerHeight / 3);
+	window.ShouldScroll = document.height <= (window.innerHeight + window.pageYOffset + window.innerHeight / 5);
 }
 function InstallEventListeners() {
 	window.ShouldScroll = true;
 	document.body.addEventListener ("DOMNodeInserted", function () { setTimeout (ScrollToBottom, 0); }, false);
-	window.onresize = ScrollToBottom;
+	window.addEventListener ("resize", function () { setTimeout (ScrollToBottom, 2); });
 	window.addEventListener ("scroll", TestScroll);
 }
