021620a590764e63f49521795ff754fc34305fdb
Oleg Linkin <MaledictusDeMagog@gmail.com>
2013-05-20 23:31:21 +0300

    Blogique Metida: remove unused variable

--- src/plugins/blogique/plugins/metida/polldialog.cpp
+++ src/plugins/blogique/plugins/metida/polldialog.cpp
@@ -346,7 +346,7 @@ namespace Metida
 		ItemIsChanged_ = false;
 	}
 
-	void PollDialog::handleItemChanged (QStandardItem *item)
+	void PollDialog::handleItemChanged (QStandardItem*)
 	{
 		ItemIsChanged_ = true;
 	}
