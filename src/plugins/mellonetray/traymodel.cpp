cb92b41754c998c9f9f3b613801e646119b30e0d
0xd34df00d <0xd34df00d@gmail.com>
2013-05-25 23:15:45 +0400

    MT: don't add LC's own icons.

--- src/plugins/mellonetray/traymodel.cpp
+++ src/plugins/mellonetray/traymodel.cpp
@@ -226,6 +226,9 @@ namespace Mellonetray
 		if (FindItem (wid) != Items_.end ())
 			return;
 
+		if (Util::XWrapper::Instance ().IsLCWindow (wid))
+			return;
+
 		beginInsertRows ({}, Items_.size (), Items_.size ());
 		Items_.append ({ wid });
 		endInsertRows ();
