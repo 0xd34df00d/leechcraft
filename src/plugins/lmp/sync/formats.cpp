4df9f59c525b5381f809625440c0c82c45815a03
0xd34df00d <0xd34df00d@gmail.com>
2013-06-20 16:23:05 +0400

    [LMP] Updated codec names.

--- src/plugins/lmp/sync/formats.cpp
+++ src/plugins/lmp/sync/formats.cpp
@@ -82,7 +82,7 @@ namespace LMP
 
 		QString GetCodecName () const
 		{
-			return "libvorbis";
+			return "vorbis";
 		}
 
 		QList<BitrateType> GetSupportedBitrates() const
@@ -213,7 +213,7 @@ namespace LMP
 
 		QString GetCodecName () const
 		{
-			return "libmp3lame";
+			return "mp3";
 		}
 
 		QList<BitrateType> GetSupportedBitrates () const
