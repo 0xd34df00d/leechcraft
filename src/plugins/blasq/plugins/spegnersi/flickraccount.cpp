cf368c410d31fcdde719e15bc679365b408fefbf
0xd34df00d <0xd34df00d@gmail.com>
2013-06-26 13:05:03 +0400

    [Blasq Spegnersi] Request more pages.

--- src/plugins/blasq/plugins/spegnersi/flickraccount.cpp
+++ src/plugins/blasq/plugins/spegnersi/flickraccount.cpp
@@ -224,9 +224,19 @@ namespace Spegnersi
 		const auto& photos = doc
 				.documentElement ()
 				.firstChildElement ("photos");
-		if (photos.attribute ("pages").toInt () > 1)
+		const auto thisPage = photos.attribute ("page").toInt ();
+		if (thisPage != photos.attribute ("pages").toInt ())
 		{
-			// TODO request more
+			auto req = MakeRequest (QString ("http://api.flickr.com/services/rest/"), KQOAuthRequest::AuthorizedRequest);
+			req->setAdditionalParameters (Util::MakeMap<QString, QString> ({
+						{ "method", "flickr.photos.search" },
+						{ "user_id", "me" },
+						{ "format", "rest" },
+						{ "per_page", "500" },
+						{ "page", QString::number (thisPage + 1) },
+						{ "extras", "url_o,url_z,url_m" }
+					}));
+			AuthMgr_->executeRequest (req);
 		}
 
 		auto photo = photos.firstChildElement ("photo");
