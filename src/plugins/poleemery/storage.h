45b5c6859c67376913a4b4c7d7c2348289da0ef2
0xd34df00d <0xd34df00d@gmail.com>
2013-07-15 17:53:25 +0400

    [PY] Support Rate in Storage.

--- src/plugins/poleemery/storage.h
+++ src/plugins/poleemery/storage.h
@@ -67,6 +67,9 @@ namespace Poleemery
 		void AddReceiptEntry (ReceiptEntry&);
 		void UpdateReceiptEntry (const ReceiptEntry&);
 		void DeleteReceiptEntry (const ReceiptEntry&);
+
+		QList<Rate> GetRates ();
+		void AddRate (Rate&);
 	private:
 		Category AddCategory (const QString&);
 		void AddNewCategories (const ExpenseEntry&, const QStringList&);
