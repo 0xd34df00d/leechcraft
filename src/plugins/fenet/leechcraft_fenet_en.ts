9ca48fb8d42e3d6f89f1c7146456e89ad670568e
0xd34df00d <0xd34df00d@gmail.com>
2013-07-26 16:55:27 +0400

    [Fenet] Updated translations.

---
+++ src/plugins/fenet/leechcraft_fenet_en.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LeechCraft::Fenet::CompParamsManager</name>
+    <message>
+        <location filename="compparamsmanager.cpp" line="44"/>
+        <source>Option</source>
+        <translation>Option</translation>
+    </message>
+    <message>
+        <location filename="compparamsmanager.cpp" line="44"/>
+        <source>Value</source>
+        <translation>Value</translation>
+    </message>
+    <message>
+        <location filename="compparamsmanager.cpp" line="44"/>
+        <source>Flag</source>
+        <translation>Flag</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Fenet::Plugin</name>
+    <message>
+        <location filename="fenet.cpp" line="127"/>
+        <source>Window manager control plugin.</source>
+        <translation>Window manager control plugin.</translation>
+    </message>
+</context>
+<context>
+    <name>fenetsettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Window manager</source>
+        <translation>Window manager</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Window manager:</source>
+        <translation>Window manager:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Use compositor</source>
+        <translation>Use compositor</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Compositor:</source>
+        <translation>Compositor:</translation>
+    </message>
+</context>
+</TS>
