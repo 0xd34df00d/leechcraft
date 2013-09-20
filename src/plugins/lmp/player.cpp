2feb77591b8dee22052eff402e2e8a9a0812db5e
0xd34df00d <0xd34df00d@gmail.com>
2013-09-20 16:16:27 +0400

    [LMP] Proper sorting if there are non-local files.

--- src/plugins/lmp/player.cpp
+++ src/plugins/lmp/player.cpp
@@ -649,10 +649,14 @@ namespace LMP
 			std::sort (result.begin (), result.end (),
 					[sorter] (decltype (result.at (0)) s1, decltype (result.at (0)) s2) -> bool
 					{
-						if (!s1.first.IsLocalFile () || !s2.first.IsLocalFile ())
+						if (s1.first.IsLocalFile () && !s2.first.IsLocalFile ())
+							return true;
+						else if (!s1.first.IsLocalFile () && s2.first.IsLocalFile ())
+							return false;
+						else if (!s1.first.IsLocalFile () || !s2.first.IsLocalFile ())
 							return qHash (s1.first) < qHash (s2.first);
-
-						return sorter (s1.second, s2.second);
+						else
+							return sorter (s1.second, s2.second);
 					});
 
 			return result;
