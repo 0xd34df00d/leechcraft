84c3432348fa22dc19b7ca6f99f9752e01c5db97
0xd34df00d <0xd34df00d@gmail.com>
2013-09-23 15:12:45 +0400

    [Azoth] Install scroll handler on DOMSubtreeModified as well.
    Fixes #1283.

--- src/plugins/azoth/resources/scripts/scrollers.js
+++ src/plugins/azoth/resources/scripts/scrollers.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function ScrollToBottom() {
 	if (window.ShouldScroll)
 		document.body.scrollTop = document.height - window.innerHeight;
@@ -8,6 +10,7 @@ function TestScroll() {
 function InstallEventListeners() {
 	window.ShouldScroll = true;
 	document.body.addEventListener ("DOMNodeInserted", function () { setTimeout (ScrollToBottom, 0); }, false);
+	document.body.addEventListener ("DOMSubtreeModified", function () { setTimeout (ScrollToBottom, 0); }, false);
 	window.addEventListener ("resize", function () { setTimeout (ScrollToBottom, 0); });
 	window.addEventListener ("scroll", TestScroll);
 }
