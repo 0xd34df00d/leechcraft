defc2eca1b86a5269e2b5bc454b4bfed97085927
0xd34df00d <0xd34df00d@gmail.com>
2013-10-05 19:22:03 +0400

    [Azoth Murm] Updated translations.

--- src/plugins/azoth/plugins/murm/leechcraft_azoth_murm_en.ts
+++ src/plugins/azoth/plugins/murm/leechcraft_azoth_murm_en.ts
@@ -2,6 +2,45 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
+    <name>LeechCraft::Azoth::Murm::EntryBase</name>
+    <message>
+        <location filename="entrybase.cpp" line="140"/>
+        <source>Enqueue</source>
+        <translation>Enqueue</translation>
+    </message>
+    <message>
+        <location filename="entrybase.cpp" line="144"/>
+        <source>Play</source>
+        <translation>Play</translation>
+    </message>
+    <message>
+        <location filename="entrybase.cpp" line="148"/>
+        <source>Download</source>
+        <translation>Download</translation>
+    </message>
+    <message>
+        <location filename="entrybase.cpp" line="264"/>
+        <source>Posted on: %1</source>
+        <translation>Posted on: %1</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="entrybase.cpp" line="267"/>
+        <source>%n like(s)</source>
+        <translation>
+            <numerusform>%n like</numerusform>
+            <numerusform>%n likes</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="entrybase.cpp" line="269"/>
+        <source>%n repost(s)</source>
+        <translation>
+            <numerusform>%n repost</numerusform>
+            <numerusform>%n reposts</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Azoth::Murm::Plugin</name>
     <message>
         <location filename="murm.cpp" line="73"/>
@@ -25,17 +64,66 @@
 <context>
     <name>LeechCraft::Azoth::Murm::VkAccount</name>
     <message>
-        <location filename="vkaccount.cpp" line="192"/>
+        <location filename="vkaccount.cpp" line="233"/>
         <source>me</source>
         <translation>me</translation>
     </message>
 </context>
 <context>
+    <name>LeechCraft::Azoth::Murm::VkChatEntry</name>
+    <message>
+        <location filename="vkchatentry.cpp" line="156"/>
+        <source>Chats</source>
+        <translation>Chats</translation>
+    </message>
+    <message>
+        <location filename="vkchatentry.cpp" line="259"/>
+        <source>Participants of %1</source>
+        <translation>Participants of %1</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Azoth::Murm::VkEntry</name>
+    <message>
+        <location filename="vkentry.cpp" line="100"/>
+        <source>%1 is now on the site again</source>
+        <translation>%1 is now on the site again</translation>
+    </message>
+    <message>
+        <location filename="vkentry.cpp" line="101"/>
+        <source>%1 has left the site</source>
+        <translation>%1 has left the site</translation>
+    </message>
+    <message>
+        <location filename="vkentry.cpp" line="378"/>
+        <source>Self contact</source>
+        <translation>Self contact</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Azoth::Murm::VkProtocol</name>
     <message>
-        <location filename="vkprotocol.cpp" line="84"/>
+        <location filename="vkprotocol.cpp" line="85"/>
         <source>VKontakte</source>
         <translation>VKontakte</translation>
+    </message>
+</context>
+<context>
+    <name>MucJoinWidget</name>
+    <message>
+        <location filename="mucjoinwidget.ui" line="20"/>
+        <source>Chat name:</source>
+        <translation>Chat name:</translation>
+    </message>
+    <message utf8="true">
+        <location filename="mucjoinwidget.ui" line="30"/>
+        <source>Filter…</source>
+        <translation>Filter…</translation>
+    </message>
+    <message>
+        <location filename="mucjoinwidget.ui" line="50"/>
+        <source>Users:</source>
+        <translation>Users:</translation>
     </message>
 </context>
 <context>
