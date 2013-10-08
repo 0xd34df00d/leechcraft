aefb222a78249aafcdd38da73c2bc78e85768414
0xd34df00d <0xd34df00d@gmail.com>
2013-10-08 18:02:40 +0400

    [Azoth OTRoid] Updated translations.

--- src/plugins/azoth/plugins/otroid/leechcraft_azoth_otroid_en.ts
+++ src/plugins/azoth/plugins/otroid/leechcraft_azoth_otroid_en.ts
@@ -4,12 +4,22 @@
 <context>
     <name>LeechCraft::Azoth::OTRoid::Plugin</name>
     <message>
-        <location filename="otroid.cpp" line="130"/>
+        <location filename="otroid.cpp" line="208"/>
         <source>Azoth OTRoid adds support for Off-the-Record deniable encryption system.</source>
         <translation>Azoth OTRoid adds support for Off-the-Record deniable encryption system.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="354"/>
+        <location filename="otroid.cpp" line="303"/>
+        <source>Private keys for account %1 need to be generated. This may take a while (from a few seconds to a couple of minutes), and during this time LeechCraft will be unavailable. Do you still wish to continue?&lt;br/&gt;&lt;br/&gt;As funny as it may sound, moving mouse while the keys are generated may actually speed up generation considerably.</source>
+        <translation>Private keys for account %1 need to be generated. This may take a while (from a few seconds to a couple of minutes), and during this time LeechCraft will be unavailable. Do you still wish to continue?&lt;br/&gt;&lt;br/&gt;As funny as it may sound, moving mouse while the keys are generated may actually speed up generation considerably.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="327"/>
+        <source>Keys are generated. Thanks for your patience and sorry for this not being in a separate thread.</source>
+        <translation>Keys are generated. Thanks for your patience and sorry for this not being in a separate thread.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="506"/>
         <source>Enable OTR</source>
         <translation>Enable OTR</translation>
     </message>
--- src/plugins/azoth/plugins/otroid/leechcraft_azoth_otroid_ru_RU.ts
+++ src/plugins/azoth/plugins/otroid/leechcraft_azoth_otroid_ru_RU.ts
@@ -4,12 +4,22 @@
 <context>
     <name>LeechCraft::Azoth::OTRoid::Plugin</name>
     <message>
-        <location filename="otroid.cpp" line="130"/>
+        <location filename="otroid.cpp" line="208"/>
         <source>Azoth OTRoid adds support for Off-the-Record deniable encryption system.</source>
         <translation>Azoth OTRoid добавляет поддержку системы шифрования Off-the-Record.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="354"/>
+        <location filename="otroid.cpp" line="303"/>
+        <source>Private keys for account %1 need to be generated. This may take a while (from a few seconds to a couple of minutes), and during this time LeechCraft will be unavailable. Do you still wish to continue?&lt;br/&gt;&lt;br/&gt;As funny as it may sound, moving mouse while the keys are generated may actually speed up generation considerably.</source>
+        <translation>Требуется сгенерировать приватные ключи для учётной записи %1. Это может занять некоторое время (от нескольких секунд до пары минут), и в это время LeechCraft будет недоступен. Вы всё еще хотите продолжить?&lt;br/&gt;&lt;br/&gt;Как бы забавно это не звучало, двиганье мышкой в процессе генерации может существенно её ускорить.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="327"/>
+        <source>Keys are generated. Thanks for your patience and sorry for this not being in a separate thread.</source>
+        <translation>Ключи созданы. Благодарим за ожидание и приносим свои извинения за то, что это происходило не в отдельном потоке.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="506"/>
         <source>Enable OTR</source>
         <translation>Включить OTR</translation>
     </message>
