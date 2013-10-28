a8b255a0284cd0910d66d20afdec6d274a4e5ee6
0xd34df00d <0xd34df00d@gmail.com>
2013-10-28 14:30:42 +0400

    [Azoth] Documented IUpdatableChatEntry.

--- src/plugins/azoth/interfaces/azoth/iupdatablechatentry.h
+++ src/plugins/azoth/interfaces/azoth/iupdatablechatentry.h
@@ -35,12 +35,47 @@ namespace LeechCraft
 {
 namespace Azoth
 {
+	/** @brief Interface for entries that can modify messages bodies.
+	 *
+	 * This interface is to be implemented by entries (implementing
+	 * ICLEntry) that can modify the bodies of the messages after the
+	 * messages were received and handled by Azoth.
+	 *
+	 * When the body of the message is to be changed, the performJS()
+	 * should be emitted with the corresponding JavaScript code that
+	 * changes the message in the opened chat tabs. IMessage::GetBody()
+	 * should also return the new message body after this signal.
+	 *
+	 * For example, a social network protocol plugin may emit an
+	 * ICLEntry::gotMessage() signal when the message is received, but
+	 * request additional information from the social network about the
+	 * attachments, emitting IUpdatableChatEntry::performJS() after that
+	 * info is fetched.
+	 *
+	 * @sa ICLEntry
+	 */
 	class IUpdatableChatEntry
 	{
 	public:
 		virtual ~IUpdatableChatEntry () {}
 	protected:
-		virtual void performJS (const QString&) = 0;
+		/** @brief Emitted when a message body is to be changed.
+		 *
+		 * This signal should be emitted by entries when the contents of
+		 * one or more messages should be changed. The corresponding
+		 * \em js code is executed in an opened chat tab corresponding to
+		 * the entry (if any).
+		 *
+		 * Typically, a message will contain a <code>div</code> or a
+		 * <code>span</code> element with an unique  <code>id</code>
+		 * attribute which is to be manipulated by the \em js code.
+		 *
+		 * @note This function is expected to be a signal.
+		 *
+		 * @param[out] js The code to execute in a chat tab corresponding
+		 * to this entry, if any.
+		 */
+		virtual void performJS (const QString& js) = 0;
 	};
 }
 }
