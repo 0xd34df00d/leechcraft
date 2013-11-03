43e3e4f825ea7a43ff0684b9f78401679a9f7ad8
Oleg Linkin <MaledictusDeMagog@gmail.com>
2013-11-03 13:42:29 +0300

    [NSM] add filter

--- src/plugins/netstoremanager/filesproxymodel.cpp
+++ src/plugins/netstoremanager/filesproxymodel.cpp
@@ -27,8 +27,15 @@ namespace NetStoreManager
 	: QSortFilterProxyModel (parent)
 	{
 		setDynamicSortFilter (true);
+		setFilterCaseSensitivity (Qt::CaseInsensitive);
 		setSortCaseSensitivity (Qt::CaseInsensitive);
 		setSortLocaleAware (true);
+	}
+
+	bool FilesProxyModel::filterAcceptsRow (int sourceRow, const QModelIndex& sourceParent) const
+	{
+		const QModelIndex& index = sourceModel ()->index (sourceRow, 0, sourceParent);
+		return sourceModel ()->data (index).toString ().contains (filterRegExp ());
 	}
 
 	bool FilesProxyModel::lessThan (const QModelIndex& left, const QModelIndex& right) const
