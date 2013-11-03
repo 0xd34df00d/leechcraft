f520e5d30db357a30f8d6bcee2b0d54ef95ccf9d
Oleg Linkin <MaledictusDeMagog@gmail.com>
2013-11-03 16:44:37 +0300

    [Blogique Metida] update coloring friends in friends list

--- src/plugins/blogique/plugins/metida/frienditemdelegate.cpp
+++ src/plugins/blogique/plugins/metida/frienditemdelegate.cpp
@@ -33,6 +33,7 @@
 #include <QTreeView>
 #include <QtDebug>
 #include "xmlsettingsmanager.h"
+#include "friendsproxymodel.h"
 
 namespace LeechCraft
 {
@@ -54,14 +55,13 @@ namespace Metida
 			const QStyleOptionViewItem& option, const QModelIndex& index) const
 	{
 		QStyleOptionViewItemV4 o = option;
-		const QRect& r = o.rect;
-		const QString& backgroundColor = index.sibling (index.row (), Columns::UserName)
+		auto proxyModel = static_cast<FriendsProxyModel*> (View_->model ());
+		const QString& backgroundColor = proxyModel->mapToSource (index.sibling (index.row (), Columns::UserName))
 				.data (ItemColorRoles::BackgroundColor).toString ();
-		const QString& foregroundColor = index.sibling (index.row (), Columns::UserName)
+		const QString& foregroundColor = proxyModel->mapToSource (index.sibling (index.row (), Columns::UserName))
 				.data (ItemColorRoles::ForegroundColor).toString ();
 
-		if (index.parent ().isValid () &&
-				ColoringItems_)
+		if (ColoringItems_)
 		{
 			if (!backgroundColor.isEmpty ())
 				painter->fillRect (o.rect, QColor (backgroundColor));
@@ -70,25 +70,6 @@ namespace Metida
 		}
 
 		QStyledItemDelegate::paint (painter, o, index);
-
-		painter->save ();
-		if (!index.parent ().isValid () &&
-				index.column () == Columns::UserName)
-		{
-			const int textWidth = o.fontMetrics.width (index.data ().value<QString> () + " ");
-			const int rem = r.width () - textWidth;
-
-			const QString& str = QString (" (%1) ")
-					.arg (index.model ()->rowCount (index));
-
-			if (o.state & QStyle::State_Selected)
-				painter->setPen (o.palette.color (QPalette::HighlightedText));
-
-			const QRect numRect (r.left () + textWidth - 1, r.top () + CPadding,
-					rem - 1, r.height () - 2 * CPadding);
-			painter->drawText (numRect, Qt::AlignVCenter | Qt::AlignLeft, str);
-		}
-		painter->restore ();
 	}
 
 	void FriendItemDelegate::handleColoringItemChanged ()
