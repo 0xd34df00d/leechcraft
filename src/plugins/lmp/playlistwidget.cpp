b9049fb0f6ae6785d7657b754051255d1fc5b5c2
0xd34df00d <0xd34df00d@gmail.com>
2013-11-06 18:07:08 +0400

    [LMP] Clear playlist search line edit on toggling.

--- src/plugins/lmp/playlistwidget.cpp
+++ src/plugins/lmp/playlistwidget.cpp
@@ -530,6 +530,10 @@ namespace LMP
 				SIGNAL (toggled (bool)),
 				Ui_.SearchPlaylist_,
 				SLOT (setFocus ()));
+		connect (ActionToggleSearch_,
+				SIGNAL (toggled (bool)),
+				Ui_.SearchPlaylist_,
+				SLOT (clear ()));
 		Ui_.SearchPlaylist_->setVisible (false);
 	}
 
