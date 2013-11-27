bc248aa5de351d203706b92fad592a7db990ace4
0xd34df00d <0xd34df00d@gmail.com>
2013-11-27 16:54:19 +0400

    [Syncer] Save max seq ID when needed.

--- src/plugins/syncer/singlesyncable.cpp
+++ src/plugins/syncer/singlesyncable.cpp
@@ -83,6 +83,31 @@ namespace Syncer
 		return settings;
 	}
 
+	namespace
+	{
+		template<typename T>
+		void DoWithMaxID (const std::vector<Laretz::Operation>& ops, const T& f)
+		{
+			uint64_t maxSeq = 0;
+			bool found = false;
+			for (const auto& op : ops)
+			{
+				const auto& items = op.getItems ();
+				const auto maxElemPos = std::max_element (items.begin (), items.end (),
+						[] (const Laretz::Item& left, const Laretz::Item& right)
+							{ return left.getSeq () < right.getSeq (); });
+				if (maxElemPos != items.end ())
+				{
+					found = true;
+					maxSeq = maxElemPos->getSeq ();
+				}
+			}
+
+			if (found)
+				f (maxSeq);
+		}
+	}
+
 	void SingleSyncable::HandleList (const Laretz::ParseResult& reply)
 	{
 		auto deletedList = std::find_if (reply.operations.begin (), reply.operations.end (),
@@ -94,6 +119,11 @@ namespace Syncer
 		{
 			QList<Laretz::Operation> ourOps;
 			Proxy_->Merge (ourOps, { *deletedList });
+			DoWithMaxID ({ *deletedList },
+					[this] (uint64_t id)
+					{
+						GetSettings ()->setValue ("LastSyncID", static_cast<quint64> (id));
+					});
 		}
 
 		if (knownList != reply.operations.end () &&
@@ -138,6 +168,19 @@ namespace Syncer
 	{
 		auto ourOps = Proxy_->GetNewOps ();
 		Proxy_->Merge (ourOps, ToQList (reply.operations));
+
+		auto settings = GetSettings ();
+		uint64_t maxId = settings->value ("LastSyncID").value<quint64> ();
+
+		DoWithMaxID (reply.operations,
+				[&settings, &maxId] (uint64_t id) -> void
+				{
+					if (id > maxId)
+					{
+						maxId = id;
+						settings->setValue ("LastSyncID", static_cast<quint64> (id));
+					}
+				});
 
 		if (ourOps.empty ())
 		{
