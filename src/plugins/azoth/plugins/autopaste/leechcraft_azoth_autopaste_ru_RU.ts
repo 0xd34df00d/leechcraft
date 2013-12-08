9c0f64981f0195decfbcb342364b5ded8748a228
0xd34df00d <0xd34df00d@gmail.com>
2013-12-08 16:33:10 +0400

    [Azoth Autopaste] Updated ru translations.

--- src/plugins/azoth/plugins/autopaste/leechcraft_azoth_autopaste_ru_RU.ts
+++ src/plugins/azoth/plugins/autopaste/leechcraft_azoth_autopaste_ru_RU.ts
@@ -2,31 +2,52 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
-    <name>LeechCraft::Azoth::Autopaste::Plugin</name>
+    <name>LeechCraft::Azoth::Autopaste::PasteServiceBase</name>
     <message>
-        <location filename="autopaste.cpp" line="67"/>
-        <source>Detects long messages and suggests pasting them to a pastebin.</source>
-        <translation>Определяет длинные сообщения и предлагает отправить их на pastebin&apos;ы.</translation>
-    </message>
-    <message>
-        <location filename="autopaste.cpp" line="150"/>
-        <source>Confirm pasting</source>
-        <translation>Подтверждение отправки</translation>
-    </message>
-    <message>
-        <location filename="autopaste.cpp" line="151"/>
-        <source>This message is too long according to current settings. Would you like to paste it on a pastebin?</source>
-        <translation>Согласно текущим настройкам, это сообщение слишком длинное. Вы хотите отправить его на pastebin?</translation>
-    </message>
-    <message>
-        <location filename="autopaste.cpp" line="178"/>
+        <location filename="pasteservicebase.cpp" line="73"/>
         <source>Text pasted</source>
         <translation>Текст отправлен</translation>
     </message>
     <message>
-        <location filename="autopaste.cpp" line="179"/>
+        <location filename="pasteservicebase.cpp" line="74"/>
         <source>Your text has been pasted: %1. The URL has been copied to the clipboard.</source>
         <translation>Текст был отправлен: %1. URL скопирован в буфер обмена.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Azoth::Autopaste::Plugin</name>
+    <message>
+        <location filename="autopaste.cpp" line="78"/>
+        <source>Detects long messages and suggests pasting them to a pastebin.</source>
+        <translation>Определяет длинные сообщения и предлагает отправить их на pastebin&apos;ы.</translation>
+    </message>
+</context>
+<context>
+    <name>PasteDialog</name>
+    <message>
+        <location filename="pastedialog.ui" line="14"/>
+        <source>Paste options</source>
+        <translation>Опции отправки</translation>
+    </message>
+    <message>
+        <location filename="pastedialog.ui" line="20"/>
+        <source>This message is too long according to current settings. Would you like to paste it on a pastebin?</source>
+        <translation>Согласно текущим настройкам, это сообщение слишком длинное. Вы хотите отправить его на pastebin?</translation>
+    </message>
+    <message>
+        <location filename="pastedialog.ui" line="32"/>
+        <source>Service:</source>
+        <translation>Сервис:</translation>
+    </message>
+    <message>
+        <location filename="pastedialog.ui" line="42"/>
+        <source>Highlight:</source>
+        <translation>Подсветка:</translation>
+    </message>
+    <message>
+        <location filename="pastedialog.ui" line="50"/>
+        <source>None</source>
+        <translation>Нет</translation>
     </message>
 </context>
 <context>
@@ -63,13 +84,8 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="8"/>
-        <source>Behavior</source>
-        <translation>Поведение</translation>
-    </message>
-    <message>
-        <location filename="dummy.cpp" line="9"/>
-        <source>Confirm pasting</source>
-        <translation>Подтверждать отправку</translation>
+        <source>Number of symbols to be detected as a paste:</source>
+        <translation>Предельное число символов:</translation>
     </message>
 </context>
 </TS>
