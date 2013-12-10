eec58a39bc8f6599da1338dea464414aca560f7f
0xd34df00d <0xd34df00d@gmail.com>
2013-12-10 15:56:37 +0400

    [Azoth Vader] Updated translations.

--- src/plugins/azoth/plugins/vader/leechcraft_azoth_vader_en.ts
+++ src/plugins/azoth/plugins/vader/leechcraft_azoth_vader_en.ts
@@ -4,42 +4,47 @@
 <context>
     <name>LeechCraft::Azoth::Vader::MRIMAccount</name>
     <message>
-        <location filename="mrimaccount.cpp" line="115"/>
+        <location filename="mrimaccount.cpp" line="130"/>
         <source>Open mailbox...</source>
         <translation>Open mailbox...</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="342"/>
+        <location filename="mrimaccount.cpp" line="357"/>
         <source>from</source>
         <translation>from</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="471"/>
+        <location filename="mrimaccount.cpp" line="440"/>
+        <source>Authentication error for account %1: server reports %2.</source>
+        <translation>Authentication error for account %1: server reports %2.</translation>
+    </message>
+    <message>
+        <location filename="mrimaccount.cpp" line="515"/>
         <source>database error</source>
         <translation>database error</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="474"/>
+        <location filename="mrimaccount.cpp" line="518"/>
         <source>no such user</source>
         <translation>no such user</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="477"/>
+        <location filename="mrimaccount.cpp" line="521"/>
         <source>rate limit exceeded</source>
         <translation>rate limit exceeded</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="480"/>
+        <location filename="mrimaccount.cpp" line="524"/>
         <source>unknown error</source>
         <translation>unknown error</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="485"/>
+        <location filename="mrimaccount.cpp" line="529"/>
         <source>Error fetching user info for %1: %2.</source>
         <translation>Error fetching user info for %1: %2.</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="620"/>
+        <location filename="mrimaccount.cpp" line="676"/>
         <source>New mail from %1: %2.</source>
         <translation>New mail from %1: %2.</translation>
     </message>
@@ -47,32 +52,47 @@
 <context>
     <name>LeechCraft::Azoth::Vader::MRIMBuddy</name>
     <message>
-        <location filename="mrimbuddy.cpp" line="47"/>
+        <location filename="mrimbuddy.cpp" line="60"/>
+        <source>Update phone number...</source>
+        <translation>Update phone number...</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="61"/>
         <source>Send SMS...</source>
         <translation>Send SMS...</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="105"/>
+        <location filename="mrimbuddy.cpp" line="123"/>
         <source>Unauthorized</source>
         <translation>Unauthorized</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="333"/>
+        <location filename="mrimbuddy.cpp" line="370"/>
         <source>Mail.Ru Agent</source>
         <translation>Mail.Ru Agent</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="395"/>
+        <location filename="mrimbuddy.cpp" line="402"/>
+        <source>Update number</source>
+        <translation>Update number</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="403"/>
+        <source>Enter new number in international format:</source>
+        <translation>Enter new number in international format:</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="447"/>
         <source>SMS has been sent to %1.</source>
         <translation>SMS has been sent to %1.</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="406"/>
+        <location filename="mrimbuddy.cpp" line="458"/>
         <source>Failed to send SMS to %1: bad parameters.</source>
         <translation>Failed to send SMS to %1: bad parameters.</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="417"/>
+        <location filename="mrimbuddy.cpp" line="469"/>
         <source>Failed to send SMS to %1: service unavailable.</source>
         <translation>Failed to send SMS to %1: service unavailable.</translation>
     </message>
@@ -80,7 +100,7 @@
 <context>
     <name>LeechCraft::Azoth::Vader::Plugin</name>
     <message>
-        <location filename="vader.cpp" line="74"/>
+        <location filename="vader.cpp" line="86"/>
         <source>Support for the Mail.ru Agent protocol.</source>
         <translation>Support for the Mail.ru Agent protocol.</translation>
     </message>
@@ -88,52 +108,52 @@
 <context>
     <name>LeechCraft::Azoth::Vader::VCardDialog</name>
     <message>
-        <location filename="vcarddialog.cpp" line="51"/>
+        <location filename="vcarddialog.cpp" line="62"/>
         <source>Nickname: %1.</source>
         <translation>Nickname: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="52"/>
+        <location filename="vcarddialog.cpp" line="63"/>
         <source>First name: %1.</source>
         <translation>First name: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="53"/>
+        <location filename="vcarddialog.cpp" line="64"/>
         <source>Last name: %1.</source>
         <translation>Last name: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="56"/>
+        <location filename="vcarddialog.cpp" line="67"/>
         <source>male</source>
         <translation>male</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="56"/>
+        <location filename="vcarddialog.cpp" line="67"/>
         <source>female</source>
         <translation>female</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="57"/>
+        <location filename="vcarddialog.cpp" line="68"/>
         <source>Gender: %1.</source>
         <translation>Gender: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="59"/>
+        <location filename="vcarddialog.cpp" line="70"/>
         <source>Birthday: %1.</source>
         <translation>Birthday: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="60"/>
+        <location filename="vcarddialog.cpp" line="71"/>
         <source>Zodiac sign: %1.</source>
         <translation>Zodiac sign: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="61"/>
+        <location filename="vcarddialog.cpp" line="72"/>
         <source>Location: %1.</source>
         <translation>Location: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="62"/>
+        <location filename="vcarddialog.cpp" line="73"/>
         <source>Phone number: %1.</source>
         <translation>Phone number: %1.</translation>
     </message>
@@ -154,22 +174,22 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="vaderutil.cpp" line="44"/>
+        <location filename="vaderutil.cpp" line="72"/>
         <source>MyWorld@Mail.ru</source>
         <translation>MyWorld@Mail.ru</translation>
     </message>
     <message>
-        <location filename="vaderutil.cpp" line="51"/>
+        <location filename="vaderutil.cpp" line="79"/>
         <source>Photo@Mail.ru</source>
         <translation>Photo@Mail.ru</translation>
     </message>
     <message>
-        <location filename="vaderutil.cpp" line="58"/>
+        <location filename="vaderutil.cpp" line="86"/>
         <source>Video@Mail.ru</source>
         <translation>Video@Mail.ru</translation>
     </message>
     <message>
-        <location filename="vaderutil.cpp" line="65"/>
+        <location filename="vaderutil.cpp" line="93"/>
         <source>Blogs@Mail.ru</source>
         <translation>Blogs@Mail.ru</translation>
     </message>
--- src/plugins/azoth/plugins/vader/leechcraft_azoth_vader_ru_RU.ts
+++ src/plugins/azoth/plugins/vader/leechcraft_azoth_vader_ru_RU.ts
@@ -4,42 +4,47 @@
 <context>
     <name>LeechCraft::Azoth::Vader::MRIMAccount</name>
     <message>
-        <location filename="mrimaccount.cpp" line="115"/>
+        <location filename="mrimaccount.cpp" line="130"/>
         <source>Open mailbox...</source>
         <translation>Открыть почтовый ящик...</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="342"/>
+        <location filename="mrimaccount.cpp" line="357"/>
         <source>from</source>
         <translation>от</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="471"/>
+        <location filename="mrimaccount.cpp" line="440"/>
+        <source>Authentication error for account %1: server reports %2.</source>
+        <translation>Ошиба аутентификации учётной записи %1: сервер сообщает %2.</translation>
+    </message>
+    <message>
+        <location filename="mrimaccount.cpp" line="515"/>
         <source>database error</source>
         <translation>ошибка базы данных</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="474"/>
+        <location filename="mrimaccount.cpp" line="518"/>
         <source>no such user</source>
         <translation>нет такого пользователя</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="477"/>
+        <location filename="mrimaccount.cpp" line="521"/>
         <source>rate limit exceeded</source>
         <translation>превышен предел количества запросов</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="480"/>
+        <location filename="mrimaccount.cpp" line="524"/>
         <source>unknown error</source>
         <translation>неизвестная ошибка</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="485"/>
+        <location filename="mrimaccount.cpp" line="529"/>
         <source>Error fetching user info for %1: %2.</source>
         <translation>Ошибка получения информации о пользователе %1: %2.</translation>
     </message>
     <message>
-        <location filename="mrimaccount.cpp" line="620"/>
+        <location filename="mrimaccount.cpp" line="676"/>
         <source>New mail from %1: %2.</source>
         <translation>Новое письмо от %1: %2.</translation>
     </message>
@@ -47,32 +52,47 @@
 <context>
     <name>LeechCraft::Azoth::Vader::MRIMBuddy</name>
     <message>
-        <location filename="mrimbuddy.cpp" line="47"/>
+        <location filename="mrimbuddy.cpp" line="60"/>
+        <source>Update phone number...</source>
+        <translation>Обновить номер телефона…</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="61"/>
         <source>Send SMS...</source>
         <translation>Отправить SMS...</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="105"/>
+        <location filename="mrimbuddy.cpp" line="123"/>
         <source>Unauthorized</source>
         <translation>Неавторизованные</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="333"/>
+        <location filename="mrimbuddy.cpp" line="370"/>
         <source>Mail.Ru Agent</source>
         <translation>Mail.Ru Агент</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="395"/>
+        <location filename="mrimbuddy.cpp" line="402"/>
+        <source>Update number</source>
+        <translation>Обновить номер</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="403"/>
+        <source>Enter new number in international format:</source>
+        <translation>Введите новый номер в международном формате:</translation>
+    </message>
+    <message>
+        <location filename="mrimbuddy.cpp" line="447"/>
         <source>SMS has been sent to %1.</source>
         <translation>SMS для %1 отправлено.</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="406"/>
+        <location filename="mrimbuddy.cpp" line="458"/>
         <source>Failed to send SMS to %1: bad parameters.</source>
         <translation>Ошибка отправки SMS для %1: некорректные параметры.</translation>
     </message>
     <message>
-        <location filename="mrimbuddy.cpp" line="417"/>
+        <location filename="mrimbuddy.cpp" line="469"/>
         <source>Failed to send SMS to %1: service unavailable.</source>
         <translation>Ошибка отправки SMS для %1: сервис недоступен.</translation>
     </message>
@@ -80,7 +100,7 @@
 <context>
     <name>LeechCraft::Azoth::Vader::Plugin</name>
     <message>
-        <location filename="vader.cpp" line="74"/>
+        <location filename="vader.cpp" line="86"/>
         <source>Support for the Mail.ru Agent protocol.</source>
         <translation>Поддержка протокола Mail.ru-агента.</translation>
     </message>
@@ -88,52 +108,52 @@
 <context>
     <name>LeechCraft::Azoth::Vader::VCardDialog</name>
     <message>
-        <location filename="vcarddialog.cpp" line="51"/>
+        <location filename="vcarddialog.cpp" line="62"/>
         <source>Nickname: %1.</source>
         <translation>Ник: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="52"/>
+        <location filename="vcarddialog.cpp" line="63"/>
         <source>First name: %1.</source>
         <translation>Имя: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="53"/>
+        <location filename="vcarddialog.cpp" line="64"/>
         <source>Last name: %1.</source>
         <translation>Фамилия: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="56"/>
+        <location filename="vcarddialog.cpp" line="67"/>
         <source>male</source>
         <translation>мужской</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="56"/>
+        <location filename="vcarddialog.cpp" line="67"/>
         <source>female</source>
         <translation>женский</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="57"/>
+        <location filename="vcarddialog.cpp" line="68"/>
         <source>Gender: %1.</source>
         <translation>Пол: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="59"/>
+        <location filename="vcarddialog.cpp" line="70"/>
         <source>Birthday: %1.</source>
         <translation>День рожденья: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="60"/>
+        <location filename="vcarddialog.cpp" line="71"/>
         <source>Zodiac sign: %1.</source>
         <translation>Знак зодиака: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="61"/>
+        <location filename="vcarddialog.cpp" line="72"/>
         <source>Location: %1.</source>
         <translation>Местоположение: %1.</translation>
     </message>
     <message>
-        <location filename="vcarddialog.cpp" line="62"/>
+        <location filename="vcarddialog.cpp" line="73"/>
         <source>Phone number: %1.</source>
         <translation>Номер телефона: %1.</translation>
     </message>
@@ -154,22 +174,22 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="vaderutil.cpp" line="44"/>
+        <location filename="vaderutil.cpp" line="72"/>
         <source>MyWorld@Mail.ru</source>
         <translation>Мой Мир@Mail.ru</translation>
     </message>
     <message>
-        <location filename="vaderutil.cpp" line="51"/>
+        <location filename="vaderutil.cpp" line="79"/>
         <source>Photo@Mail.ru</source>
         <translation>Фото@Mail.ru</translation>
     </message>
     <message>
-        <location filename="vaderutil.cpp" line="58"/>
+        <location filename="vaderutil.cpp" line="86"/>
         <source>Video@Mail.ru</source>
         <translation>Видео@Mail.ru</translation>
     </message>
     <message>
-        <location filename="vaderutil.cpp" line="65"/>
+        <location filename="vaderutil.cpp" line="93"/>
         <source>Blogs@Mail.ru</source>
         <translation>Блоги@Mail.ru</translation>
     </message>
