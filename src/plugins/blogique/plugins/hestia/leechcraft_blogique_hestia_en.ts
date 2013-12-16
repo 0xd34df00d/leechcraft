7db889691ef29d555341b4a910079b7dcb46fae8
0xd34df00d <0xd34df00d@gmail.com>
2013-12-16 16:07:55 +0400

    [Blogique Hestia] Added translations.

---
+++ src/plugins/blogique/plugins/hestia/leechcraft_blogique_hestia_en.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>AccountConfigurationDialog</name>
+    <message>
+        <location filename="accountconfigurationdialog.ui" line="14"/>
+        <source>Edit account</source>
+        <translation>Edit account</translation>
+    </message>
+</context>
+<context>
+    <name>AccountConfigurationWidget</name>
+    <message>
+        <location filename="accountconfigurationwidget.ui" line="32"/>
+        <source>Browse...</source>
+        <translation>Browse...</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::Hestia::AccountConfigurationWidget</name>
+    <message>
+        <location filename="accountconfigurationwidget.cpp" line="51"/>
+        <source>Select new account base</source>
+        <translation>Select new account base</translation>
+    </message>
+    <message>
+        <location filename="accountconfigurationwidget.cpp" line="52"/>
+        <source>Open existing account base</source>
+        <translation>Open existing account base</translation>
+    </message>
+    <message>
+        <location filename="accountconfigurationwidget.cpp" line="60"/>
+        <source>Select account base</source>
+        <translation>Select account base</translation>
+    </message>
+    <message>
+        <location filename="accountconfigurationwidget.cpp" line="62"/>
+        <location filename="accountconfigurationwidget.cpp" line="66"/>
+        <source>Account bases (*.db)</source>
+        <translation>Account bases (*.db)</translation>
+    </message>
+    <message>
+        <location filename="accountconfigurationwidget.cpp" line="64"/>
+        <source>Open account base</source>
+        <translation>Open account base</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::Hestia::LocalBlogAccount</name>
+    <message>
+        <location filename="localblogaccount.cpp" line="51"/>
+        <source>All entries</source>
+        <translation>All entries</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::Hestia::LocalBloggingPlatform</name>
+    <message>
+        <location filename="localbloggingplatform.cpp" line="193"/>
+        <source>You have invalid account data.</source>
+        <translation>You have invalid account data.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Blogique::Hestia::Plugin</name>
+    <message>
+        <location filename="hestia.cpp" line="84"/>
+        <source>Local blogging platform plugin for Blogique.</source>
+        <oldsource>Platform for local blogging</oldsource>
+        <translation>Local blogging platform plugin for Blogique.</translation>
+    </message>
+</context>
+</TS>
