ea7c299f8d81239d36ffdc4f869b1cdb3a326c24
0xd34df00d <0xd34df00d@gmail.com>
2013-12-16 16:20:45 +0400

    [CSTP] Updated translations.

--- src/plugins/cstp/leechcraft_cstp_ru_RU.ts
+++ src/plugins/cstp/leechcraft_cstp_ru_RU.ts
@@ -2,160 +2,191 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru">
 <context>
-    <name>LeechCraft::Plugins::CSTP::AddTask</name>
+    <name>AddTask</name>
     <message>
-        <location filename="addtask.cpp" line="107"/>
+        <location filename="addtask.ui" line="14"/>
+        <source>Add task</source>
+        <translation>Добавить задачу</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="32"/>
+        <source>Local path:</source>
+        <translation>Локальный путь:</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="44"/>
+        <source>Browse...</source>
+        <translation>Обзор…</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="56"/>
+        <source>Filename:</source>
+        <translation>Имя файла:</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="66"/>
+        <source>Comment:</source>
+        <translation>Комментарий:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::CSTP::AddTask</name>
+    <message>
+        <location filename="addtask.cpp" line="116"/>
         <source>Directory %1 doesn&apos;t exist, would you like to select another?</source>
         <translation>Каталог %1 не существует, хотите выбрать другой?</translation>
     </message>
     <message>
-        <location filename="addtask.cpp" line="110"/>
+        <location filename="addtask.cpp" line="119"/>
         <source>Directory %1 isn&apos;t readable, would you like to select another?</source>
         <translation>Каталог %1 недоступен для чтения, хотите выбрать другой?</translation>
     </message>
     <message>
-        <location filename="addtask.cpp" line="113"/>
+        <location filename="addtask.cpp" line="122"/>
         <source>Directory %1 isn&apos;t writable, would you like to select another?</source>
         <translation>Каталог %1 недоступен для записи, хотите выбрать другой?</translation>
     </message>
     <message>
-        <location filename="addtask.cpp" line="116"/>
+        <location filename="addtask.cpp" line="125"/>
         <source>%1 isn&apos;t a directory at all, would you like to select another?</source>
         <translation>%1 не является каталогом, хотите выбрать другой?</translation>
     </message>
     <message>
-        <location filename="addtask.cpp" line="157"/>
+        <location filename="addtask.cpp" line="166"/>
         <source>Select directory</source>
         <translation>Выбор каталога</translation>
     </message>
 </context>
 <context>
-    <name>LeechCraft::Plugins::CSTP::CSTP</name>
+    <name>LeechCraft::CSTP::CSTP</name>
     <message>
-        <location filename="cstp.cpp" line="206"/>
+        <location filename="cstp.cpp" line="219"/>
         <source>Remove</source>
         <translation>Удалить</translation>
     </message>
     <message>
-        <location filename="cstp.cpp" line="213"/>
+        <location filename="cstp.cpp" line="226"/>
         <source>Remove all</source>
         <translation>Удалить все</translation>
     </message>
     <message>
-        <location filename="cstp.cpp" line="222"/>
+        <location filename="cstp.cpp" line="235"/>
         <source>Start</source>
         <translation>Старт</translation>
     </message>
     <message>
-        <location filename="cstp.cpp" line="229"/>
+        <location filename="cstp.cpp" line="242"/>
         <source>Stop</source>
         <translation>Стоп</translation>
     </message>
     <message>
-        <location filename="cstp.cpp" line="236"/>
+        <location filename="cstp.cpp" line="249"/>
         <source>Start all</source>
-        <translation>Стартовать все</translation>
+        <translation>Запустить всё</translation>
     </message>
     <message>
-        <location filename="cstp.cpp" line="243"/>
+        <location filename="cstp.cpp" line="256"/>
         <source>Stop all</source>
-        <translation>Остановить все</translation>
+        <translation>Остановить всё</translation>
     </message>
     <message>
-        <location filename="cstp.cpp" line="255"/>
+        <location filename="cstp.cpp" line="268"/>
         <source>File exists</source>
         <translation>Файл существует</translation>
     </message>
     <message>
-        <location filename="cstp.cpp" line="256"/>
+        <location filename="cstp.cpp" line="269"/>
         <source>File %1 already exists, continue download?</source>
         <translation>Файл %1 уже существует, продолжить закачку?</translation>
     </message>
 </context>
 <context>
-    <name>LeechCraft::Plugins::CSTP::Core</name>
+    <name>LeechCraft::CSTP::Core</name>
     <message>
-        <location filename="core.cpp" line="61"/>
-        <source>URL</source>
-        <translation>URL</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="62"/>
+        <location filename="core.cpp" line="72"/>
         <source>State</source>
         <translation>Состояние</translation>
     </message>
     <message>
-        <location filename="core.cpp" line="63"/>
+        <location filename="core.cpp" line="72"/>
         <source>Progress</source>
         <translation>Прогресс</translation>
     </message>
     <message>
-        <location filename="core.cpp" line="417"/>
-        <source>%1 (ETA: %2)</source>
-        <translation>%1 (ETA: %2)</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="429"/>
-        <source>%1% (%2 of %3 at %4)</source>
-        <oldsource>%1% (%2 of %3 at %44)</oldsource>
-        <translation>%1% (%2 из %3 на %4)</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="521"/>
-        <source>Could not open file %1: %2</source>
-        <translation>Невозможно открыть файл %1: %2</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="580"/>
-        <source>Download finished: %1
-%2</source>
-        <oldsource>Download finished</oldsource>
-        <translation>Закачка завершена: %1
-%2</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="277"/>
+        <location filename="core.cpp" line="281"/>
         <source>Could not truncate file </source>
         <translation>Невозможно сократить файл</translation>
     </message>
     <message>
-        <location filename="core.cpp" line="433"/>
+        <location filename="core.cpp" line="413"/>
+        <source>%1 (ETA: %2)</source>
+        <translation>%1 (ETA: %2)</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="425"/>
+        <source>%1% (%2 of %3 at %4)</source>
+        <translation>%1% (%2 из %3 на %4)</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="429"/>
         <source>/s</source>
         <translation>/c</translation>
     </message>
-</context>
-<context>
-    <name>LeechCraft::Plugins::CSTP::MainViewDelegate</name>
     <message>
-        <location filename="mainviewdelegate.cpp" line="71"/>
-        <source>Idle</source>
-        <translation>Бездействующий</translation>
+        <location filename="core.cpp" line="525"/>
+        <source>Could not open file %1: %2</source>
+        <translation>Невозможно открыть файл %1: %2</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="586"/>
+        <source>Failed downloading %1 (%2).</source>
+        <translation>Ошибка скачивания %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="589"/>
+        <source>Finished downloading %1 (%2).</source>
+        <translation>Загрузка %1 завершена (%2).</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="603"/>
+        <source>Handle...</source>
+        <translation>Обработать…</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="610"/>
+        <source>Open externally</source>
+        <translation>Открыть внешним приложением</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="615"/>
+        <source>Show folder</source>
+        <translation>Показать в каталоге</translation>
     </message>
 </context>
 <context>
-    <name>LeechCraft::Plugins::CSTP::Task</name>
+    <name>LeechCraft::CSTP::Task</name>
     <message>
-        <location filename="task.cpp" line="213"/>
+        <location filename="task.cpp" line="239"/>
         <source>Stopped</source>
         <translation>Остановлена</translation>
     </message>
     <message>
-        <location filename="task.cpp" line="215"/>
+        <location filename="task.cpp" line="241"/>
         <source>Finished</source>
         <translation>Закончена</translation>
     </message>
     <message>
-        <location filename="task.cpp" line="217"/>
+        <location filename="task.cpp" line="243"/>
         <source>Running</source>
         <translation>Выполняется</translation>
     </message>
     <message>
-        <location filename="task.cpp" line="238"/>
+        <location filename="task.cpp" line="264"/>
         <source>Task isn&apos;t initialized properly</source>
         <translation>Задача некорректно инициализирована</translation>
     </message>
     <message>
-        <location filename="task.cpp" line="476"/>
+        <location filename="task.cpp" line="486"/>
         <source>Error writing to file %1: %2</source>
         <translation>Ошибка записи в файл %1: %2</translation>
     </message>
@@ -225,56 +256,61 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="15"/>
+        <source>LeechCraft</source>
+        <translation>LeechCraft</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="16"/>
         <source>Firefox 3.0.1 on Linux i686</source>
         <translation>Firefox 3.0.1 на Linux i686</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="16"/>
+        <location filename="dummy.cpp" line="17"/>
         <source>Internet Explorer 6.0 on Windows XP</source>
         <translation>Internet Explorer 6.0 на Windows XP</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="17"/>
+        <location filename="dummy.cpp" line="18"/>
         <source>Internet Explorer 7.0 on Windows Vista</source>
         <translation>Internet Explorer 7.0 на Windows Vista</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="18"/>
+        <location filename="dummy.cpp" line="19"/>
         <source>Internet Explorer 4.0 on Windows 98</source>
         <translation>Internet Explorer 4.0 на Windows 98</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="19"/>
+        <location filename="dummy.cpp" line="20"/>
         <source>Internet Explorer 2.0 on Windows 95</source>
         <translation>Internet Explorer 2.0 на Windows 95</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="20"/>
+        <location filename="dummy.cpp" line="21"/>
         <source>Konqueror 4.0</source>
         <translation>Konqueror 4.0</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="21"/>
+        <location filename="dummy.cpp" line="22"/>
         <source>SeaMonkey 1.1.11 on Linux i686</source>
         <translation>SeaMonkey 1.1.11 на Linux i686</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="22"/>
+        <location filename="dummy.cpp" line="23"/>
         <source>Opera 9.52 on Linux x86_64</source>
         <translation>Opera 9.52 на Linux x86_64</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="23"/>
+        <location filename="dummy.cpp" line="24"/>
         <source>Wget 1.9.1</source>
         <translation>Wget 1.9.1</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="24"/>
+        <location filename="dummy.cpp" line="25"/>
         <source>w3m 0.5.2</source>
         <translation>w3m 0.5.2</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="25"/>
+        <location filename="dummy.cpp" line="26"/>
         <source>Lynx 2.8.7 with OpenSSL</source>
         <translation>Lynx 2.8.7 с OpenSSL</translation>
     </message>
