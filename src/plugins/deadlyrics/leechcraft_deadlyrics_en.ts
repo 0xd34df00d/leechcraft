f0ef5de824c92a5dfb5adca8fb9857d41c6438f7
0xd34df00d <0xd34df00d@gmail.com>
2013-12-16 17:49:58 +0400

    [DeadLyrics] Updated translations.

--- src/plugins/deadlyrics/leechcraft_deadlyrics_en.ts
+++ src/plugins/deadlyrics/leechcraft_deadlyrics_en.ts
@@ -2,54 +2,12 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
-    <name>LeechCraft::Plugins::DeadLyrics::Core</name>
+    <name>LeechCraft::DeadLyrics::DeadLyRicS</name>
     <message>
-        <location filename="core.cpp" line="73"/>
-        <location filename="core.cpp" line="78"/>
-        <source>lyrics</source>
-        <translation>lyrics</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Plugins::DeadLyrics::DeadLyRicS</name>
-    <message>
-        <location filename="deadlyrics.cpp" line="64"/>
-        <source>Lyrics Searcher</source>
-        <translation>Lyrics Searcher</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Plugins::DeadLyrics::FindProxy</name>
-    <message>
-        <location filename="findproxy.cpp" line="151"/>
-        <source>Searching...</source>
-        <translation>Searching...</translation>
-    </message>
-</context>
-<context>
-    <name>QObject</name>
-    <message>
-        <location filename="searcher.cpp" line="60"/>
-        <source>Unknown %1 %2</source>
-        <translation>Unknown %1 %2</translation>
-    </message>
-</context>
-<context>
-    <name>deadlyricssettings</name>
-    <message>
-        <location filename="dummy.cpp" line="2"/>
-        <source>Appearance</source>
-        <translation>Appearance</translation>
-    </message>
-    <message>
-        <location filename="dummy.cpp" line="3"/>
-        <source>Lyrics view</source>
-        <translation>Lyrics view</translation>
-    </message>
-    <message>
-        <location filename="dummy.cpp" line="4"/>
-        <source>Use a web browser (if available)</source>
-        <translation>Use a web browser (if available)</translation>
+        <location filename="deadlyrics.cpp" line="74"/>
+        <source>Song lyrics searcher.</source>
+        <oldsource>Lyrics Searcher</oldsource>
+        <translation>Song lyrics searcher.</translation>
     </message>
 </context>
 </TS>
--- src/plugins/deadlyrics/leechcraft_deadlyrics_ru_RU.ts
+++ src/plugins/deadlyrics/leechcraft_deadlyrics_ru_RU.ts
@@ -2,46 +2,10 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
-    <name>LeechCraft::Plugins::DeadLyrics::Core</name>
+    <name>LeechCraft::DeadLyrics::DeadLyRicS</name>
     <message>
-        <source>lyrics</source>
-        <translation>тексты</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Plugins::DeadLyrics::DeadLyRicS</name>
-    <message>
-        <source>Lyrics Searcher</source>
-        <translation>Поиск текстов песен</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Plugins::DeadLyrics::FindProxy</name>
-    <message>
-        <source>Searching...</source>
-        <translation>Поиск...</translation>
-    </message>
-</context>
-<context>
-    <name>QObject</name>
-    <message>
-        <source>Unknown %1 %2</source>
-        <translation>Неизвестный %1 %2</translation>
-    </message>
-</context>
-<context>
-    <name>deadlyricssettings</name>
-    <message>
-        <source>Appearance</source>
-        <translation>Внешний вид</translation>
-    </message>
-    <message>
-        <source>Lyrics view</source>
-        <translation>Просмотр текстов</translation>
-    </message>
-    <message>
-        <source>Use a web browser (if available)</source>
-        <translation>Использовать веб-браузер (если доступен)</translation>
+        <source>Song lyrics searcher.</source>
+        <translation>Модуль поиска текстов песен.</translation>
     </message>
 </context>
 </TS>
