572312a1a0a3b43254cee7a1d67164811f8d2eb2
0xd34df00d <0xd34df00d@gmail.com>
2013-12-21 16:29:33 +0400

    [NSM] Updated translations.

--- src/plugins/netstoremanager/leechcraft_netstoremanager_en.ts
+++ src/plugins/netstoremanager/leechcraft_netstoremanager_en.ts
@@ -35,91 +35,269 @@
 <context>
     <name>LeechCraft::NetStoreManager::AccountsManager</name>
     <message>
-        <location filename="accountsmanager.cpp" line="33"/>
+        <location filename="accountsmanager.cpp" line="45"/>
         <source>Account</source>
         <translation>Account</translation>
     </message>
     <message>
-        <location filename="accountsmanager.cpp" line="33"/>
+        <location filename="accountsmanager.cpp" line="45"/>
         <source>Storage</source>
         <translation>Storage</translation>
     </message>
 </context>
 <context>
+    <name>LeechCraft::NetStoreManager::DirectoryWidget</name>
+    <message>
+        <location filename="directorywidget.cpp" line="71"/>
+        <source>Select directory</source>
+        <translation>Select directory</translation>
+    </message>
+    <message>
+        <location filename="directorywidget.cpp" line="80"/>
+        <source>Account hasn&apos;t been selected.
+You should select an account in first column.</source>
+        <oldsource>Account hasn&apos;t been selected.
+You should select an account in first column</oldsource>
+        <translation>Account hasn&apos;t been selected.
+You should select an account in first column.</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::NetStoreManager::ManagerTab</name>
     <message>
-        <location filename="managertab.cpp" line="40"/>
+        <location filename="managertab.cpp" line="84"/>
         <source>Copy URL...</source>
         <translation>Copy URL...</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="45"/>
-        <source>Prolongate selected</source>
-        <translation>Prolongate selected</translation>
+        <location filename="managertab.cpp" line="68"/>
+        <source>Name</source>
+        <translation>Name</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="50"/>
-        <source>Delete selected</source>
-        <translation>Delete selected</translation>
+        <location filename="managertab.cpp" line="68"/>
+        <source>Used space</source>
+        <translation>Used space</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="233"/>
+        <location filename="managertab.cpp" line="68"/>
+        <source>Modify</source>
+        <translation>Modify</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="78"/>
+        <source>Open file</source>
+        <translation>Open file</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="90"/>
+        <source>Copy...</source>
+        <translation>Copy...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="96"/>
+        <source>Move...</source>
+        <translation>Move...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="102"/>
+        <source>Rename...</source>
+        <translation>Rename...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="108"/>
+        <source>Paste</source>
+        <translation>Paste</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="114"/>
+        <source>Delete...</source>
+        <translation>Delete...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="120"/>
+        <source>Move to trash</source>
+        <translation>Move to trash</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="126"/>
+        <source>Restore from trash</source>
+        <translation>Restore from trash</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="132"/>
+        <source>Empty trash</source>
+        <translation>Empty trash</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="138"/>
+        <source>Create directory</source>
+        <translation>Create directory</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="144"/>
+        <location filename="managertab.cpp" line="887"/>
+        <source>Upload...</source>
+        <translation>Upload...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="150"/>
+        <source>Download</source>
+        <translation>Download</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="263"/>
+        <source>Refresh</source>
+        <translation>Refresh</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="268"/>
+        <source>Upload</source>
+        <translation>Upload</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="278"/>
+        <location filename="managertab.cpp" line="865"/>
+        <source>Open trash</source>
+        <translation>Open trash</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="287"/>
+        <source>Trash</source>
+        <translation>Trash</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="537"/>
         <source>Error</source>
         <translation>Error</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="234"/>
+        <location filename="managertab.cpp" line="538"/>
         <source>You first need to add an account.</source>
         <translation>You first need to add an account.</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="239"/>
+        <location filename="managertab.cpp" line="543"/>
+        <location filename="managertab.cpp" line="831"/>
         <source>Select file for upload</source>
         <translation>Select file for upload</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="738"/>
+        <source>New name:</source>
+        <translation>New name:</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="813"/>
+        <source>New directory name:</source>
+        <translation>New directory name:</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="865"/>
+        <source>Close trash</source>
+        <translation>Close trash</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="927"/>
+        <source>Export to...</source>
+        <translation>Export to...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="1008"/>
+        <source>File URL has been copied to the clipboard.</source>
+        <translation>File URL has been copied to the clipboard.</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::NetStoreManager::Plugin</name>
     <message>
-        <location filename="netstoremanager.cpp" line="82"/>
+        <location filename="netstoremanager.cpp" line="115"/>
         <source>Allows one to manage his network storages like Yandex.Disk.</source>
         <translation>Allows one to manage his network storages like Yandex.Disk.</translation>
     </message>
     <message>
-        <location filename="netstoremanager.cpp" line="100"/>
+        <location filename="netstoremanager.cpp" line="135"/>
         <source>Net storage</source>
         <translation>Net storage</translation>
+    </message>
+    <message>
+        <location filename="netstoremanager.cpp" line="197"/>
+        <source>No account for service name %1.</source>
+        <translation>No account for service name %1.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::RemoteDirectorySelectDialog</name>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="58"/>
+        <source>New directory...</source>
+        <oldsource>New Folder...</oldsource>
+        <translation>New directory...</translation>
+    </message>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="65"/>
+        <source>Directory</source>
+        <translation>Directory</translation>
+    </message>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="132"/>
+        <source>Enter new directory name:</source>
+        <oldsource>Enter new folder name</oldsource>
+        <translation>Enter new directory name:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::SyncWidget</name>
+    <message>
+        <location filename="syncwidget.cpp" line="55"/>
+        <source>Account</source>
+        <translation>Account</translation>
+    </message>
+    <message>
+        <location filename="syncwidget.cpp" line="56"/>
+        <source>Local directory</source>
+        <translation>Local directory</translation>
+    </message>
+    <message>
+        <location filename="syncwidget.cpp" line="56"/>
+        <source>Remote directory</source>
+        <translation>Remote directory</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::NetStoreManager::UpManager</name>
     <message>
-        <location filename="upmanager.cpp" line="83"/>
+        <location filename="upmanager.cpp" line="123"/>
         <source>%1 is already uploading to %2.</source>
         <translation>%1 is already uploading to %2.</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="96"/>
+        <location filename="upmanager.cpp" line="137"/>
         <source>Uploading %1 to %2...</source>
         <translation>Uploading %1 to %2...</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="100"/>
+        <location filename="upmanager.cpp" line="141"/>
         <source>Initializing...</source>
         <translation>Initializing...</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="116"/>
-        <source>%1 is successfully uploaded, URL is pasted into clipboard.</source>
-        <translation>%1 is successfully uploaded, URL is pasted into clipboard.</translation>
+        <location filename="upmanager.cpp" line="176"/>
+        <source>URL is pasted into clipboard.</source>
+        <translation>URL is pasted into clipboard.</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="130"/>
+        <location filename="upmanager.cpp" line="189"/>
         <source>Failed to upload %1: %2.</source>
         <translation>Failed to upload %1: %2.</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="152"/>
+        <location filename="upmanager.cpp" line="209"/>
+        <source>File %1 was uploaded successfully</source>
+        <translation>File %1 was uploaded successfully</translation>
+    </message>
+    <message>
+        <location filename="upmanager.cpp" line="241"/>
         <source>%1 of %2</source>
         <translation>%1 of %2</translation>
     </message>
@@ -127,14 +305,31 @@
 <context>
     <name>ManagerTab</name>
     <message>
-        <location filename="managertab.ui" line="28"/>
-        <source>Upload...</source>
-        <translation>Upload...</translation>
+        <location filename="managertab.ui" line="45"/>
+        <source>Filter...</source>
+        <translation>Filter...</translation>
+    </message>
+</context>
+<context>
+    <name>RemoteDirectorySelectDialog</name>
+    <message>
+        <location filename="remotedirectoryselectdialog.ui" line="20"/>
+        <source>Select remote directory for synchronization:</source>
+        <oldsource>Selecte remote directory for synchronization:</oldsource>
+        <translation>Select remote directory for synchronization:</translation>
+    </message>
+</context>
+<context>
+    <name>SynchronizationWidget</name>
+    <message>
+        <location filename="syncwidget.ui" line="32"/>
+        <source>Add</source>
+        <translation>Add</translation>
     </message>
     <message>
-        <location filename="managertab.ui" line="35"/>
-        <source>Update filelist</source>
-        <translation>Update filelist</translation>
+        <location filename="syncwidget.ui" line="39"/>
+        <source>Remove</source>
+        <translation>Remove</translation>
     </message>
 </context>
 <context>
@@ -146,8 +341,33 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="3"/>
-        <source>Upload</source>
-        <translation>Upload</translation>
+        <source>Behavior</source>
+        <translation>Behavior</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Copy URL to clipboard after uploading item</source>
+        <translation>Copy URL to clipboard after uploading item</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Synchronization</source>
+        <translation>Synchronization</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Directories</source>
+        <translation>Directories</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Exceptions</source>
+        <translation>Exceptions</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Ignore synchronization for files with following masks:</source>
+        <translation>Ignore synchronization for files with following masks:</translation>
     </message>
 </context>
 </TS>
--- src/plugins/netstoremanager/leechcraft_netstoremanager_ru_RU.ts
+++ src/plugins/netstoremanager/leechcraft_netstoremanager_ru_RU.ts
@@ -35,91 +35,269 @@
 <context>
     <name>LeechCraft::NetStoreManager::AccountsManager</name>
     <message>
-        <location filename="accountsmanager.cpp" line="33"/>
+        <location filename="accountsmanager.cpp" line="45"/>
         <source>Account</source>
         <translation>Учётная запись</translation>
     </message>
     <message>
-        <location filename="accountsmanager.cpp" line="33"/>
+        <location filename="accountsmanager.cpp" line="45"/>
         <source>Storage</source>
         <translation>Хранилище</translation>
     </message>
 </context>
 <context>
+    <name>LeechCraft::NetStoreManager::DirectoryWidget</name>
+    <message>
+        <location filename="directorywidget.cpp" line="71"/>
+        <source>Select directory</source>
+        <translation>Выбор директории</translation>
+    </message>
+    <message>
+        <location filename="directorywidget.cpp" line="80"/>
+        <source>Account hasn&apos;t been selected.
+You should select an account in first column.</source>
+        <oldsource>Account hasn&apos;t been selected.
+You should select an account in first column</oldsource>
+        <translation>Учётная запись не выбрана.
+Вы должны выбрать учётную запись в первой колонке.</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::NetStoreManager::ManagerTab</name>
     <message>
-        <location filename="managertab.cpp" line="40"/>
+        <location filename="managertab.cpp" line="84"/>
         <source>Copy URL...</source>
         <translation>Скопировать URL...</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="45"/>
-        <source>Prolongate selected</source>
-        <translation>Продлить выбранное</translation>
+        <location filename="managertab.cpp" line="68"/>
+        <source>Name</source>
+        <translation>Имя</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="50"/>
-        <source>Delete selected</source>
-        <translation>Удалить выбранное</translation>
+        <location filename="managertab.cpp" line="68"/>
+        <source>Used space</source>
+        <translation>Использовано места</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="233"/>
+        <location filename="managertab.cpp" line="68"/>
+        <source>Modify</source>
+        <translation>Изменить</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="78"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="90"/>
+        <source>Copy...</source>
+        <translation>Копировать…</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="96"/>
+        <source>Move...</source>
+        <translation>Переместить…</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="102"/>
+        <source>Rename...</source>
+        <translation>Переименовать…</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="108"/>
+        <source>Paste</source>
+        <translation>Вставить</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="114"/>
+        <source>Delete...</source>
+        <translation>Удалить…</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="120"/>
+        <source>Move to trash</source>
+        <translation>Переместить в корзину</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="126"/>
+        <source>Restore from trash</source>
+        <translation>Восстановить из корзины</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="132"/>
+        <source>Empty trash</source>
+        <translation>Очистить корзину</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="138"/>
+        <source>Create directory</source>
+        <translation>Создать директорию</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="144"/>
+        <location filename="managertab.cpp" line="887"/>
+        <source>Upload...</source>
+        <translation>Загрузить...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="150"/>
+        <source>Download</source>
+        <translation>Скачать</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="263"/>
+        <source>Refresh</source>
+        <translation>Обновить</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="268"/>
+        <source>Upload</source>
+        <translation>Загрузить</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="278"/>
+        <location filename="managertab.cpp" line="865"/>
+        <source>Open trash</source>
+        <translation>Открыть корзину</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="287"/>
+        <source>Trash</source>
+        <translation>Корзина</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="537"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="234"/>
+        <location filename="managertab.cpp" line="538"/>
         <source>You first need to add an account.</source>
         <translation>Сначала нужно добавить учётную запись.</translation>
     </message>
     <message>
-        <location filename="managertab.cpp" line="239"/>
+        <location filename="managertab.cpp" line="543"/>
+        <location filename="managertab.cpp" line="831"/>
         <source>Select file for upload</source>
         <translation>Выбор файла для загрузки</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="738"/>
+        <source>New name:</source>
+        <translation>Новое имя:</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="813"/>
+        <source>New directory name:</source>
+        <translation>Новое имя директории:</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="865"/>
+        <source>Close trash</source>
+        <translation>Закрыть корзину</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="927"/>
+        <source>Export to...</source>
+        <translation>Экспортировать в…</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="1008"/>
+        <source>File URL has been copied to the clipboard.</source>
+        <translation>URL файла был скопирован в буфер обмена.</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::NetStoreManager::Plugin</name>
     <message>
-        <location filename="netstoremanager.cpp" line="82"/>
+        <location filename="netstoremanager.cpp" line="115"/>
         <source>Allows one to manage his network storages like Yandex.Disk.</source>
         <translation>Позволяет управлять сетевыми хранилищами данных типа Яндекс.Диска.</translation>
     </message>
     <message>
-        <location filename="netstoremanager.cpp" line="100"/>
+        <location filename="netstoremanager.cpp" line="135"/>
         <source>Net storage</source>
         <translation>Сетевое хранилище</translation>
+    </message>
+    <message>
+        <location filename="netstoremanager.cpp" line="197"/>
+        <source>No account for service name %1.</source>
+        <translation>Нет учётной записи для сервиса %1.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::RemoteDirectorySelectDialog</name>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="58"/>
+        <source>New directory...</source>
+        <oldsource>New Folder...</oldsource>
+        <translation>Новая директория…</translation>
+    </message>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="65"/>
+        <source>Directory</source>
+        <translation>Директория</translation>
+    </message>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="132"/>
+        <source>Enter new directory name:</source>
+        <oldsource>Enter new folder name</oldsource>
+        <translation>Введите имя новой директории:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::SyncWidget</name>
+    <message>
+        <location filename="syncwidget.cpp" line="55"/>
+        <source>Account</source>
+        <translation>Учётная запись</translation>
+    </message>
+    <message>
+        <location filename="syncwidget.cpp" line="56"/>
+        <source>Local directory</source>
+        <translation>Локальная директория</translation>
+    </message>
+    <message>
+        <location filename="syncwidget.cpp" line="56"/>
+        <source>Remote directory</source>
+        <translation>Удалённая директория</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::NetStoreManager::UpManager</name>
     <message>
-        <location filename="upmanager.cpp" line="83"/>
+        <location filename="upmanager.cpp" line="123"/>
         <source>%1 is already uploading to %2.</source>
         <translation>%1 уже загружается в %2.</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="96"/>
+        <location filename="upmanager.cpp" line="137"/>
         <source>Uploading %1 to %2...</source>
         <translation>Загрузка %1 в %2...</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="100"/>
+        <location filename="upmanager.cpp" line="141"/>
         <source>Initializing...</source>
         <translation>Инициализация...</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="116"/>
-        <source>%1 is successfully uploaded, URL is pasted into clipboard.</source>
-        <translation>Файл %1 успешно загружен, URL скопирован в буфер обмена.</translation>
+        <location filename="upmanager.cpp" line="176"/>
+        <source>URL is pasted into clipboard.</source>
+        <translation>URL скопирован в буфер обмена.</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="130"/>
+        <location filename="upmanager.cpp" line="189"/>
         <source>Failed to upload %1: %2.</source>
         <translation>Не удалось загрузить %1: %2.</translation>
     </message>
     <message>
-        <location filename="upmanager.cpp" line="152"/>
+        <location filename="upmanager.cpp" line="209"/>
+        <source>File %1 was uploaded successfully</source>
+        <translation>Файл %1 успешно загружен</translation>
+    </message>
+    <message>
+        <location filename="upmanager.cpp" line="241"/>
         <source>%1 of %2</source>
         <translation>%1 из %2</translation>
     </message>
@@ -127,14 +305,31 @@
 <context>
     <name>ManagerTab</name>
     <message>
-        <location filename="managertab.ui" line="28"/>
-        <source>Upload...</source>
-        <translation>Загрузить...</translation>
+        <location filename="managertab.ui" line="45"/>
+        <source>Filter...</source>
+        <translation>Поиск…</translation>
+    </message>
+</context>
+<context>
+    <name>RemoteDirectorySelectDialog</name>
+    <message>
+        <location filename="remotedirectoryselectdialog.ui" line="20"/>
+        <source>Select remote directory for synchronization:</source>
+        <oldsource>Selecte remote directory for synchronization:</oldsource>
+        <translation>Выберите удалённую директорию для синхронизации:</translation>
+    </message>
+</context>
+<context>
+    <name>SynchronizationWidget</name>
+    <message>
+        <location filename="syncwidget.ui" line="32"/>
+        <source>Add</source>
+        <translation>Добавить</translation>
     </message>
     <message>
-        <location filename="managertab.ui" line="35"/>
-        <source>Update filelist</source>
-        <translation>Обновить список файлов</translation>
+        <location filename="syncwidget.ui" line="39"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
     </message>
 </context>
 <context>
@@ -146,8 +341,33 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="3"/>
-        <source>Upload</source>
-        <translation>Загрузка</translation>
+        <source>Behavior</source>
+        <translation>Поведение</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Copy URL to clipboard after uploading item</source>
+        <translation>Копировать URL в буфер обмена после загрузки файлов</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Synchronization</source>
+        <translation>Синхронизация</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Directories</source>
+        <translation>Директории</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Exceptions</source>
+        <translation>Исключения</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Ignore synchronization for files with following masks:</source>
+        <translation>Игнорировать файлы со следующими масками:</translation>
     </message>
 </context>
 </TS>
