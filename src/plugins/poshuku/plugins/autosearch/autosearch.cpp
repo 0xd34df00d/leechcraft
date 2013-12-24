d1814a78ca18a59c34fc003471ece9dbea921ad5
0xd34df00d <0xd34df00d@gmail.com>
2013-12-24 16:45:01 +0400

    [Poshuku AS] Added translations.

--- src/plugins/poshuku/plugins/autosearch/autosearch.cpp
+++ src/plugins/poshuku/plugins/autosearch/autosearch.cpp
@@ -34,6 +34,7 @@
 #include <QDomDocument>
 #include <interfaces/core/icoreproxy.h>
 #include <interfaces/poshuku/iurlcompletionmodel.h>
+#include <util/util.h>
 
 namespace LeechCraft
 {
@@ -43,6 +44,8 @@ namespace Autosearch
 {
 	void Plugin::Init (ICoreProxy_ptr proxy)
 	{
+		Util::InstallTranslator ("poshuku_autosearch");
+
 		Proxy_ = proxy;
 	}
 
---
+++ src/plugins/poshuku/plugins/autosearch/leechcraft_poshuku_autosearch_ru_RU.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::Poshuku::Autosearch::Plugin</name>
+    <message>
+        <location filename="autosearch.cpp" line="69"/>
+        <source>Provides support for Google search suggestions.</source>
+        <translation>Обеспечивает поддержку автодополнения поиска Google.</translation>
+    </message>
+</context>
+</TS>
