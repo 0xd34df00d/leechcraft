9082a6c9d23ac30aaf6311bfe11640b3a8019486
0xd34df00d <0xd34df00d@gmail.com>
2013-12-25 20:52:09 +0400

    [Summary] Updated translations.
    Cleared out all request parser/normalizer/etc stuff.

--- src/plugins/summary/leechcraft_summary_en.ts
+++ src/plugins/summary/leechcraft_summary_en.ts
@@ -4,95 +4,21 @@
 <context>
     <name>LeechCraft::Summary::Core</name>
     <message>
-        <location filename="core.cpp" line="121"/>
-        <location filename="core.cpp" line="356"/>
+        <location filename="core.cpp" line="254"/>
+        <location filename="core.cpp" line="268"/>
         <source>Summary</source>
         <translation>Summary</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="188"/>
-        <source>Name</source>
-        <translation>Name</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="189"/>
-        <source>Status</source>
-        <translation>Status</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="190"/>
-        <source>State</source>
-        <translation>State</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Summary::OperationalModel</name>
-    <message>
-        <location filename="operationalmodel.cpp" line="28"/>
-        <source>Entity</source>
-        <translation>Entity</translation>
-    </message>
-    <message>
-        <location filename="operationalmodel.cpp" line="29"/>
-        <source>Category</source>
-        <translation>Category</translation>
-    </message>
-    <message>
-        <location filename="operationalmodel.cpp" line="30"/>
-        <source>Information</source>
-        <translation>Information</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Summary::RequestNormalizer</name>
-    <message>
-        <location filename="requestnormalizer.cpp" line="49"/>
-        <source>Name</source>
-        <translation>Name</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="50"/>
-        <source>Status</source>
-        <translation>Status</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="51"/>
-        <source>State</source>
-        <translation>State</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="67"/>
-        <source>Request validation error: %1</source>
-        <translation>Request validation error: %1</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="83"/>
-        <source>Request parsing error: %1</source>
-        <translation>Request parsing error: %1</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="107"/>
-        <source>Open/close braces mismatch: %1</source>
-        <translation>Open/close braces mismatch: %1</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Summary::SearchWidget</name>
-    <message>
-        <location filename="searchwidget.cpp" line="33"/>
-        <source>Search categories</source>
-        <translation>Search categories</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::Summary::Summary</name>
     <message>
-        <location filename="summary.cpp" line="70"/>
+        <location filename="summary.cpp" line="81"/>
         <source>Summary</source>
         <translation>Summary</translation>
     </message>
     <message>
-        <location filename="summary.cpp" line="102"/>
+        <location filename="summary.cpp" line="113"/>
         <source>Summary of downloads and recent events</source>
         <translation>Summary of downloads and recent events</translation>
     </message>
@@ -100,57 +26,9 @@
 <context>
     <name>LeechCraft::Summary::SummaryWidget</name>
     <message>
-        <location filename="summarywidget.cpp" line="51"/>
-        <source>Ctrl+F</source>
-        <translation>Ctrl+F</translation>
-    </message>
-    <message>
-        <location filename="summarywidget.cpp" line="518"/>
-        <source>S: %1 [%2]</source>
-        <translation>S: %1 [%2]</translation>
-    </message>
-    <message>
-        <location filename="summarywidget.cpp" line="522"/>
-        <source>Summary [%1]</source>
-        <translation>Summary [%1]</translation>
-    </message>
-</context>
-<context>
-    <name>SearchWidget</name>
-    <message>
-        <location filename="searchwidget.ui" line="14"/>
-        <source>Search parameters</source>
-        <translation>Search parameters</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="73"/>
-        <source>String</source>
-        <translation>String</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="78"/>
-        <source>Wildcard</source>
-        <translation>Wildcard</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="83"/>
-        <source>Regexp</source>
-        <translation>Regexp</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="88"/>
-        <source>Tags</source>
-        <translation>Tags</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="39"/>
-        <source>AND</source>
-        <translation>AND</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="49"/>
-        <source>OR</source>
-        <translation>OR</translation>
+        <location filename="summarywidget.cpp" line="68"/>
+        <source>Search...</source>
+        <translation>Search...</translation>
     </message>
 </context>
 </TS>
--- src/plugins/summary/leechcraft_summary_ru_RU.ts
+++ src/plugins/summary/leechcraft_summary_ru_RU.ts
@@ -4,95 +4,21 @@
 <context>
     <name>LeechCraft::Summary::Core</name>
     <message>
-        <location filename="core.cpp" line="121"/>
-        <location filename="core.cpp" line="356"/>
+        <location filename="core.cpp" line="254"/>
+        <location filename="core.cpp" line="268"/>
         <source>Summary</source>
         <translation>Сводка</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="188"/>
-        <source>Name</source>
-        <translation>Имя</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="189"/>
-        <source>Status</source>
-        <translation>Статус</translation>
-    </message>
-    <message>
-        <location filename="core.cpp" line="190"/>
-        <source>State</source>
-        <translation>Состояние</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Summary::OperationalModel</name>
-    <message>
-        <location filename="operationalmodel.cpp" line="28"/>
-        <source>Entity</source>
-        <translation>Сущность</translation>
-    </message>
-    <message>
-        <location filename="operationalmodel.cpp" line="29"/>
-        <source>Category</source>
-        <translation>Категория</translation>
-    </message>
-    <message>
-        <location filename="operationalmodel.cpp" line="30"/>
-        <source>Information</source>
-        <translation>Информация</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Summary::RequestNormalizer</name>
-    <message>
-        <location filename="requestnormalizer.cpp" line="49"/>
-        <source>Name</source>
-        <translation>Имя</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="50"/>
-        <source>Status</source>
-        <translation>Статус</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="51"/>
-        <source>State</source>
-        <translation>Состояние</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="67"/>
-        <source>Request validation error: %1</source>
-        <translation>Ошибка валидации запроса: %1</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="83"/>
-        <source>Request parsing error: %1</source>
-        <translation>Ошибка разбора запроса: %1</translation>
-    </message>
-    <message>
-        <location filename="requestnormalizer.cpp" line="107"/>
-        <source>Open/close braces mismatch: %1</source>
-        <translation>Несоответствие открывающих и закрывающих скобок: %1</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Summary::SearchWidget</name>
-    <message>
-        <location filename="searchwidget.cpp" line="33"/>
-        <source>Search categories</source>
-        <translation>Категории поиска</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::Summary::Summary</name>
     <message>
-        <location filename="summary.cpp" line="70"/>
+        <location filename="summary.cpp" line="81"/>
         <source>Summary</source>
         <translation>Сводка</translation>
     </message>
     <message>
-        <location filename="summary.cpp" line="102"/>
+        <location filename="summary.cpp" line="113"/>
         <source>Summary of downloads and recent events</source>
         <translation>Сводка по закачкам и недавним событиям</translation>
     </message>
@@ -100,57 +26,9 @@
 <context>
     <name>LeechCraft::Summary::SummaryWidget</name>
     <message>
-        <location filename="summarywidget.cpp" line="51"/>
-        <source>Ctrl+F</source>
-        <translation>Ctrl+F</translation>
-    </message>
-    <message>
-        <location filename="summarywidget.cpp" line="518"/>
-        <source>S: %1 [%2]</source>
-        <translation>C: %1 [%2]</translation>
-    </message>
-    <message>
-        <location filename="summarywidget.cpp" line="522"/>
-        <source>Summary [%1]</source>
-        <translation>Сводка [%1]</translation>
-    </message>
-</context>
-<context>
-    <name>SearchWidget</name>
-    <message>
-        <location filename="searchwidget.ui" line="14"/>
-        <source>Search parameters</source>
-        <translation>Параметры поиска</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="73"/>
-        <source>String</source>
-        <translation>Строка</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="78"/>
-        <source>Wildcard</source>
-        <translation>Маска</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="83"/>
-        <source>Regexp</source>
-        <translation>Regexp</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="88"/>
-        <source>Tags</source>
-        <translation>Теги</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="39"/>
-        <source>AND</source>
-        <translation>И</translation>
-    </message>
-    <message>
-        <location filename="searchwidget.ui" line="49"/>
-        <source>OR</source>
-        <translation>ИЛИ</translation>
+        <location filename="summarywidget.cpp" line="68"/>
+        <source>Search...</source>
+        <translation>Поиск…</translation>
     </message>
 </context>
 </TS>
