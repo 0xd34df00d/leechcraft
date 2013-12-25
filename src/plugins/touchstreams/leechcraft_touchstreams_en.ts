3a9dcac993b768bf54e7c760cf9af2f802fa3c27
0xd34df00d <0xd34df00d@gmail.com>
2013-12-25 20:56:12 +0400

    [TS] Updated translations.

--- src/plugins/touchstreams/leechcraft_touchstreams_en.ts
+++ src/plugins/touchstreams/leechcraft_touchstreams_en.ts
@@ -2,19 +2,38 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
-    <name>LeechCraft::TouchStreams::AuthManager</name>
+    <name>LeechCraft::TouchStreams::AlbumsManager</name>
     <message>
-        <location filename="authmanager.cpp" line="64"/>
-        <source>VK.com authentication</source>
-        <translation>VK.com authentication</translation>
+        <location filename="albumsmanager.cpp" line="69"/>
+        <location filename="albumsmanager.cpp" line="88"/>
+        <source>VKontakte: your audio</source>
+        <translation>VKontakte: your audio</translation>
+    </message>
+    <message>
+        <location filename="albumsmanager.cpp" line="154"/>
+        <source>Uncategorized</source>
+        <translation>Uncategorized</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::TouchStreams::FriendsManager</name>
+    <message>
+        <location filename="friendsmanager.cpp" line="65"/>
+        <source>VKontakte: friends</source>
+        <translation>VKontakte: friends</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::TouchStreams::Plugin</name>
     <message>
-        <location filename="touchstreams.cpp" line="69"/>
+        <location filename="touchstreams.cpp" line="97"/>
         <source>VK.com music streamer.</source>
         <translation>VK.com music streamer.</translation>
+    </message>
+    <message>
+        <location filename="touchstreams.cpp" line="112"/>
+        <source>VKontakte</source>
+        <translation>VKontakte</translation>
     </message>
 </context>
 <context>
@@ -26,6 +45,11 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="3"/>
+        <source>Request friends&apos; audio tracks</source>
+        <translation>Request friends&apos; audio tracks</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
         <source>Login and allow access...</source>
         <translation>Login and allow access...</translation>
     </message>
--- src/plugins/touchstreams/leechcraft_touchstreams_ru_RU.ts
+++ src/plugins/touchstreams/leechcraft_touchstreams_ru_RU.ts
@@ -2,19 +2,38 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
-    <name>LeechCraft::TouchStreams::AuthManager</name>
+    <name>LeechCraft::TouchStreams::AlbumsManager</name>
     <message>
-        <location filename="authmanager.cpp" line="64"/>
-        <source>VK.com authentication</source>
-        <translation>Аутентификация на VK.com</translation>
+        <location filename="albumsmanager.cpp" line="69"/>
+        <location filename="albumsmanager.cpp" line="88"/>
+        <source>VKontakte: your audio</source>
+        <translation>ВКонтакте: ваши аудиозаписи</translation>
+    </message>
+    <message>
+        <location filename="albumsmanager.cpp" line="154"/>
+        <source>Uncategorized</source>
+        <translation>Некатегоризованное</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::TouchStreams::FriendsManager</name>
+    <message>
+        <location filename="friendsmanager.cpp" line="65"/>
+        <source>VKontakte: friends</source>
+        <translation>ВКонтакте: друзья</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::TouchStreams::Plugin</name>
     <message>
-        <location filename="touchstreams.cpp" line="69"/>
+        <location filename="touchstreams.cpp" line="97"/>
         <source>VK.com music streamer.</source>
         <translation>Стример музыки из ВКонтакте.</translation>
+    </message>
+    <message>
+        <location filename="touchstreams.cpp" line="112"/>
+        <source>VKontakte</source>
+        <translation>ВКонтакте</translation>
     </message>
 </context>
 <context>
@@ -26,6 +45,11 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="3"/>
+        <source>Request friends&apos; audio tracks</source>
+        <translation>Запрашивать аудиозаписи друзей</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
         <source>Login and allow access...</source>
         <translation>Войти и разрешить доступ...</translation>
     </message>
