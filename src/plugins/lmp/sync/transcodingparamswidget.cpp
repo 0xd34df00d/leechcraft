be021746227314ee32fbc5fb502500efe28116df
0xd34df00d <0xd34df00d@gmail.com>
2014-01-09 03:12:21 +0400

    [LMP] Fixed Quality label width calculation.

--- src/plugins/lmp/sync/transcodingparamswidget.cpp
+++ src/plugins/lmp/sync/transcodingparamswidget.cpp
@@ -42,8 +42,8 @@ namespace LMP
 	{
 		Ui_.setupUi (this);
 		const auto& fm = fontMetrics ();
-		const auto qualityWidth = std::min (fm.width (" 9999 kbps "),
-				fm.width (tr ("Quality %1").arg (10)));
+		const auto qualityWidth = std::max (fm.width (" 9999 kbps "),
+				fm.width (" " + tr ("Quality %1").arg (10) + " "));
 		Ui_.QualityDisplay_->setFixedWidth (qualityWidth);
 		Ui_.QualityDisplay_->setFrameShape (Ui_.ThreadsDisplay_->frameShape ());
 
