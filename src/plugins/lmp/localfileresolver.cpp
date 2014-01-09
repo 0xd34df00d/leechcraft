3387736a748486bc9dec032d66a5eab516a3ad99
0xd34df00d <0xd34df00d@gmail.com>
2014-01-09 21:20:05 +0400

    [LMP] Accurate file resolving.

--- src/plugins/lmp/localfileresolver.cpp
+++ src/plugins/lmp/localfileresolver.cpp
@@ -62,7 +62,7 @@ namespace LMP
 #ifdef Q_OS_WIN32
 		return TagLib::FileRef (reinterpret_cast<const wchar_t*> (file.utf16 ()));
 #else
-		return TagLib::FileRef (file.toUtf8 ().constData ());
+		return TagLib::FileRef (file.toUtf8 ().constData (), true, TagLib::AudioProperties::Accurate);
 #endif
 	}
 
