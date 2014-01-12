11dac2a3f3f748c4926f904e897089dca5f0ab87
0xd34df00d <0xd34df00d@gmail.com>
2014-01-12 20:58:06 +0400

    [Overall] Oh and happy New Year BTW.

--- src/plugins/choroid/choroid.h
+++ src/plugins/choroid/choroid.h
@@ -1,6 +1,6 @@
 /**********************************************************************
  * LeechCraft - modular cross-platform feature rich internet client.
- * Copyright (C) 2006-2013  Georg Rudoy
+ * Copyright (C) 2006-2014  Georg Rudoy
  * Copyright (C) 2011 ForNeVeR
  *
  * Boost Software License - Version 1.0 - August 17th, 2003
