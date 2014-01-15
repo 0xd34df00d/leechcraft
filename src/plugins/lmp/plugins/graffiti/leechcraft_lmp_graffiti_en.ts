242facd0aec017d1566227dc0a550dca65bbc9b4
0xd34df00d <0xd34df00d@gmail.com>
2014-01-15 12:23:13 +0400

    [LMP Graffiti] Updated translations.

--- src/plugins/lmp/plugins/graffiti/leechcraft_lmp_graffiti_en.ts
+++ src/plugins/lmp/plugins/graffiti/leechcraft_lmp_graffiti_en.ts
@@ -4,50 +4,68 @@
 <context>
     <name>GraffitiTab</name>
     <message>
-        <location filename="graffititab.ui" line="48"/>
+        <location filename="graffititab.ui" line="61"/>
         <source>Artist:</source>
         <translation>Artist:</translation>
     </message>
     <message>
-        <location filename="graffititab.ui" line="64"/>
+        <location filename="graffititab.ui" line="77"/>
         <source>Album:</source>
         <translation>Album:</translation>
     </message>
     <message>
-        <location filename="graffititab.ui" line="71"/>
+        <location filename="graffititab.ui" line="84"/>
         <source>Title:</source>
         <translation>Title:</translation>
     </message>
     <message>
-        <location filename="graffititab.ui" line="78"/>
+        <location filename="graffititab.ui" line="91"/>
         <source>Year:</source>
         <translation>Year:</translation>
     </message>
     <message>
-        <location filename="graffititab.ui" line="95"/>
+        <location filename="graffititab.ui" line="108"/>
         <source>Genre:</source>
         <translation>Genre:</translation>
     </message>
 </context>
 <context>
+    <name>LeechCraft::LMP::Graffiti::CueSplitter</name>
+    <message>
+        <location filename="cuesplitter.cpp" line="261"/>
+        <source>Cue file is invalid</source>
+        <translation>Cue file is invalid</translation>
+    </message>
+    <message>
+        <location filename="cuesplitter.cpp" line="286"/>
+        <source>No such file %1.</source>
+        <translation>No such file %1.</translation>
+    </message>
+    <message>
+        <location filename="cuesplitter.cpp" line="381"/>
+        <source>Failed to start recoder: %1.</source>
+        <translation>Failed to start recoder: %1.</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::LMP::Graffiti::FilesModel</name>
     <message>
-        <location filename="filesmodel.cpp" line="38"/>
+        <location filename="filesmodel.cpp" line="49"/>
         <source>Track</source>
         <translation>Track</translation>
     </message>
     <message>
-        <location filename="filesmodel.cpp" line="38"/>
+        <location filename="filesmodel.cpp" line="49"/>
         <source>Album</source>
         <translation>Album</translation>
     </message>
     <message>
-        <location filename="filesmodel.cpp" line="38"/>
+        <location filename="filesmodel.cpp" line="49"/>
         <source>Artist</source>
         <translation>Artist</translation>
     </message>
     <message>
-        <location filename="filesmodel.cpp" line="38"/>
+        <location filename="filesmodel.cpp" line="49"/>
         <source>File name</source>
         <translation>File name</translation>
     </message>
@@ -55,22 +73,37 @@
 <context>
     <name>LeechCraft::LMP::Graffiti::GraffitiTab</name>
     <message>
-        <location filename="graffititab.cpp" line="69"/>
+        <location filename="graffititab.cpp" line="159"/>
+        <source>Scanning path %1...</source>
+        <translation>Scanning path %1...</translation>
+    </message>
+    <message>
+        <location filename="graffititab.cpp" line="217"/>
         <source>Save</source>
         <translation>Save</translation>
     </message>
     <message>
-        <location filename="graffititab.cpp" line="74"/>
+        <location filename="graffititab.cpp" line="222"/>
         <source>Revert</source>
         <translation>Revert</translation>
     </message>
     <message>
-        <location filename="graffititab.cpp" line="80"/>
+        <location filename="graffititab.cpp" line="228"/>
         <source>Rename files</source>
         <translation>Rename files</translation>
     </message>
+    <message>
+        <location filename="graffititab.cpp" line="234"/>
+        <source>Fetch tags</source>
+        <translation>Fetch tags</translation>
+    </message>
+    <message>
+        <location filename="graffititab.cpp" line="238"/>
+        <source>Split CUE...</source>
+        <translation>Split CUE...</translation>
+    </message>
     <message numerus="yes">
-        <location filename="graffititab.cpp" line="181"/>
+        <location filename="graffititab.cpp" line="327"/>
         <source>Do you really want to accept changes to %n file(s)?</source>
         <translation>
             <numerusform>Do you really want to accept changes to %n file?</numerusform>
@@ -78,7 +111,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="graffititab.cpp" line="223"/>
+        <location filename="graffititab.cpp" line="369"/>
         <source>Do you really want to revert changes to %n file(s)?</source>
         <translation>
             <numerusform>Do you really want to revert changes to %n file?</numerusform>
@@ -86,38 +119,82 @@
         </translation>
     </message>
     <message>
-        <location filename="graffititab.cpp" line="244"/>
+        <location filename="graffititab.cpp" line="390"/>
         <source>You have unsaved files with changed tags. Do you want to save or discard those changes?</source>
         <translation>You have unsaved files with changed tags. Do you want to save or discard those changes?</translation>
+    </message>
+    <message>
+        <location filename="graffititab.cpp" line="461"/>
+        <source>No cue sheets are available in this directory.</source>
+        <translation>No cue sheets are available in this directory.</translation>
+    </message>
+    <message>
+        <location filename="graffititab.cpp" line="470"/>
+        <source>Select cue sheet to use for splitting:</source>
+        <translation>Select cue sheet to use for splitting:</translation>
+    </message>
+    <message>
+        <location filename="graffititab.cpp" line="607"/>
+        <source>Finished splitting CUE file</source>
+        <translation>Finished splitting CUE file</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::LMP::Graffiti::Plugin</name>
     <message>
-        <location filename="graffiti.cpp" line="62"/>
+        <location filename="graffiti.cpp" line="81"/>
         <source>Allows one to manipulate audio files tags.</source>
         <translation>Allows one to manipulate audio files tags.</translation>
     </message>
 </context>
 <context>
+    <name>LeechCraft::LMP::Graffiti::ProgressManager</name>
+    <message>
+        <location filename="progressmanager.cpp" line="57"/>
+        <source>Fetching tags...</source>
+        <translation>Fetching tags...</translation>
+    </message>
+    <message>
+        <location filename="progressmanager.cpp" line="58"/>
+        <source>Fetching...</source>
+        <translation>Fetching...</translation>
+    </message>
+    <message>
+        <location filename="progressmanager.cpp" line="85"/>
+        <location filename="progressmanager.cpp" line="131"/>
+        <source>%1 of %2</source>
+        <translation>%1 of %2</translation>
+    </message>
+    <message>
+        <location filename="progressmanager.cpp" line="94"/>
+        <source>Splitting CUE %1...</source>
+        <translation>Splitting CUE %1...</translation>
+    </message>
+    <message>
+        <location filename="progressmanager.cpp" line="95"/>
+        <source>Splitting...</source>
+        <translation>Splitting...</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::LMP::Graffiti::RenameDialog</name>
     <message>
-        <location filename="renamedialog.cpp" line="39"/>
+        <location filename="renamedialog.cpp" line="50"/>
         <source>The following variables are allowed in the pattern: %1.</source>
         <translation>The following variables are allowed in the pattern: %1.</translation>
     </message>
     <message>
-        <location filename="renamedialog.cpp" line="58"/>
+        <location filename="renamedialog.cpp" line="69"/>
         <source>Source name</source>
         <translation>Source name</translation>
     </message>
     <message>
-        <location filename="renamedialog.cpp" line="58"/>
+        <location filename="renamedialog.cpp" line="69"/>
         <source>Target name</source>
         <translation>Target name</translation>
     </message>
     <message numerus="yes">
-        <location filename="renamedialog.cpp" line="104"/>
+        <location filename="renamedialog.cpp" line="115"/>
         <source>Are you sure you want to rename %n file(s)?</source>
         <translation>
             <numerusform>Are you sure you want to rename %n file?</numerusform>
