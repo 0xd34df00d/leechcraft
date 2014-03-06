815959d964447db182ae6e694165c6af57ffe247
0xd34df00d <0xd34df00d@gmail.com>
2014-03-06 14:02:27 +0400

    [Azoth OTRoid] Updated translations.

--- src/plugins/azoth/plugins/otroid/leechcraft_azoth_otroid_en.ts
+++ src/plugins/azoth/plugins/otroid/leechcraft_azoth_otroid_en.ts
@@ -34,52 +34,92 @@
         <translation>Received message contains malformed data.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="176"/>
+        <location filename="otroid.cpp" line="150"/>
+        <source>OTR encryption error, the message has not been sent.</source>
+        <translation>OTR encryption error, the message has not been sent.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="153"/>
+        <source>Trying to send unencrypted message while our policy requires OTR encryption.</source>
+        <translation>Trying to send unencrypted message while our policy requires OTR encryption.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="156"/>
+        <source>Private conversation could not be set up. Error %1, source %2.</source>
+        <translation>Private conversation could not be set up. Error %1, source %2.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="161"/>
+        <source>Received our own OTR message.</source>
+        <translation>Received our own OTR message.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="164"/>
+        <source>The previous message has been resent.</source>
+        <translation>The previous message has been resent.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="167"/>
+        <source>Received (and discarded) message for other client instance.</source>
+        <translation>Received (and discarded) message for other client instance.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="170"/>
+        <source>Received general OTR error.</source>
+        <translation>Received general OTR error.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="181"/>
+        <source>Original OTR message: %1.</source>
+        <translation>Original OTR message: %1.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="207"/>
         <source>You have received a new fingerprint from %1: %2</source>
         <translation>You have received a new fingerprint from %1: %2</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="186"/>
+        <location filename="otroid.cpp" line="217"/>
         <source>Private conversation started</source>
         <translation>Private conversation started</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="195"/>
+        <location filename="otroid.cpp" line="226"/>
         <source>Private conversation lost</source>
         <translation>Private conversation lost</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="297"/>
+        <location filename="otroid.cpp" line="336"/>
         <source>Azoth OTRoid adds support for Off-the-Record deniable encryption system.</source>
         <translation>Azoth OTRoid adds support for Off-the-Record deniable encryption system.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="437"/>
+        <location filename="otroid.cpp" line="476"/>
         <source>Private keys for account %1 need to be generated. This takes quite some time (from a few seconds to a couple of minutes), and while you can use LeechCraft in the meantime, all the messages will be sent unencrypted until keys are generated. You will be notified when this process finishes. Do you want to generate keys now?&lt;br /&gt;&lt;br /&gt;You can also move mouse randomily to help generating entropy.</source>
         <translation>Private keys for account %1 need to be generated. This takes quite some time (from a few seconds to a couple of minutes), and while you can use LeechCraft in the meantime, all the messages will be sent unencrypted until keys are generated. You will be notified when this process finishes. Do you want to generate keys now?&lt;br /&gt;&lt;br /&gt;You can also move mouse randomily to help generating entropy.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="473"/>
+        <location filename="otroid.cpp" line="512"/>
         <source>Keys are generated. Thanks for your patience.</source>
         <translation>Keys are generated. Thanks for your patience.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="578"/>
+        <location filename="otroid.cpp" line="622"/>
         <source>%1 has ended the private conversation with you, you should do the same.</source>
         <translation>%1 has ended the private conversation with you, you should do the same.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="716"/>
+        <location filename="otroid.cpp" line="760"/>
         <source>Private conversation closed</source>
         <translation>Private conversation closed</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="723"/>
+        <location filename="otroid.cpp" line="767"/>
         <source>Attempting to start a private conversation</source>
         <translation>Attempting to start a private conversation</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="682"/>
+        <location filename="otroid.cpp" line="726"/>
         <source>Enable OTR</source>
         <translation>Enable OTR</translation>
     </message>
@@ -87,7 +127,7 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="otroid.cpp" line="204"/>
+        <location filename="otroid.cpp" line="235"/>
         <source>Private conversation refreshed</source>
         <translation>Private conversation refreshed</translation>
     </message>
--- src/plugins/azoth/plugins/otroid/leechcraft_azoth_otroid_ru_RU.ts
+++ src/plugins/azoth/plugins/otroid/leechcraft_azoth_otroid_ru_RU.ts
@@ -34,52 +34,92 @@
         <translation>Полученное сообщение содержит некорректные данные.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="176"/>
+        <location filename="otroid.cpp" line="150"/>
+        <source>OTR encryption error, the message has not been sent.</source>
+        <translation>Ошибка шифрования OTR, сообщение не было отправлено.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="153"/>
+        <source>Trying to send unencrypted message while our policy requires OTR encryption.</source>
+        <translation>Попытка отправить незашифрованное сообщение, хотя наша политика требует шифрования OTR.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="156"/>
+        <source>Private conversation could not be set up. Error %1, source %2.</source>
+        <translation>Защищённая беседа не может быть установлена. Ошибка %1, источник %2.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="161"/>
+        <source>Received our own OTR message.</source>
+        <translation>Получено наше сообственное OTR-сообщение.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="164"/>
+        <source>The previous message has been resent.</source>
+        <translation>Предыдущее сообщение было повторно отправлено.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="167"/>
+        <source>Received (and discarded) message for other client instance.</source>
+        <translation>Получено (и проигнорировано) сообщение для другого клиента.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="170"/>
+        <source>Received general OTR error.</source>
+        <translation>Общая ошибка OTR.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="181"/>
+        <source>Original OTR message: %1.</source>
+        <translation>Исходное сообщение OTR: %1.</translation>
+    </message>
+    <message>
+        <location filename="otroid.cpp" line="207"/>
         <source>You have received a new fingerprint from %1: %2</source>
         <translation>Вы получили новый отпечаток от %1: %2</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="186"/>
+        <location filename="otroid.cpp" line="217"/>
         <source>Private conversation started</source>
         <translation>Защищённая беседа начата</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="195"/>
+        <location filename="otroid.cpp" line="226"/>
         <source>Private conversation lost</source>
         <translation>Защищённая беседа потеряна</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="297"/>
+        <location filename="otroid.cpp" line="336"/>
         <source>Azoth OTRoid adds support for Off-the-Record deniable encryption system.</source>
         <translation>Azoth OTRoid добавляет поддержку системы шифрования Off-the-Record.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="437"/>
+        <location filename="otroid.cpp" line="476"/>
         <source>Private keys for account %1 need to be generated. This takes quite some time (from a few seconds to a couple of minutes), and while you can use LeechCraft in the meantime, all the messages will be sent unencrypted until keys are generated. You will be notified when this process finishes. Do you want to generate keys now?&lt;br /&gt;&lt;br /&gt;You can also move mouse randomily to help generating entropy.</source>
         <translation>Необходимо создать приватные ключи для учётной записи %1. Это может занять некоторое время (от нескольких секунд до нескольких минут), и, хотя вы можете использовать LeechCraft в это время, все исходящие сообщения не будут зашифрованы, пока ключи не будут созданы. Вы получите уведомление, когда этот процесс завершится. Вы хотите создать ключи сейчас?&lt;br/&gt;&lt;br/&gt;Вы также можете случайно двигать мышкой, чтобы помочь создать побольше энтропии.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="473"/>
+        <location filename="otroid.cpp" line="512"/>
         <source>Keys are generated. Thanks for your patience.</source>
         <translation>Ключи созданы. Спасибо за терпение.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="578"/>
+        <location filename="otroid.cpp" line="622"/>
         <source>%1 has ended the private conversation with you, you should do the same.</source>
         <translation>%1 завершил(а) защищённую беседу с вами, вы должны сделать то же.</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="716"/>
+        <location filename="otroid.cpp" line="760"/>
         <source>Private conversation closed</source>
         <translation>Защищённая беседа завершена</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="723"/>
+        <location filename="otroid.cpp" line="767"/>
         <source>Attempting to start a private conversation</source>
         <translation>Попытка начать защищённую беседу</translation>
     </message>
     <message>
-        <location filename="otroid.cpp" line="682"/>
+        <location filename="otroid.cpp" line="726"/>
         <source>Enable OTR</source>
         <translation>Включить OTR</translation>
     </message>
@@ -87,7 +127,7 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="otroid.cpp" line="204"/>
+        <location filename="otroid.cpp" line="235"/>
         <source>Private conversation refreshed</source>
         <translation>Защищённая беседа обновлена</translation>
     </message>
