2b72ce4986e79c3d20bac4c944fde9195a82e98d
0xd34df00d <0xd34df00d@gmail.com>
2014-03-07 20:38:40 +0400

    [AnHero] Use --restart flag instead of shell kludge.

--- src/plugins/anhero/crashprocess/crashdialog.cpp
+++ src/plugins/anhero/crashprocess/crashdialog.cpp
@@ -124,9 +124,12 @@ namespace CrashProcess
 
 	void CrashDialog::done (int res)
 	{
+		auto cmdlist = CmdLine_.split (' ', QString::SkipEmptyParts);
+		cmdlist << "--restart";
+
 		if (!CmdLine_.isEmpty () &&
 				Ui_.RestartBox_->checkState () == Qt::Checked)
-			QProcess::startDetached ("/bin/sh", { "-c", "sleep 2; " + CmdLine_ });
+			QProcess::startDetached (Info_.Path_, cmdlist);
 
 		QDialog::done (res);
 	}
