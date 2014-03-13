ea1dee3ae77e254406576d8f6b9abc6adffa2b77
0xd34df00d <0xd34df00d@gmail.com>
2014-03-13 21:30:27 +0400

    [Tools] Removed ts -> po conversion scripts.

--- tools/scripts/utrans.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-mkdir ~/Programming/leechcraft/translations/$1/
-lupdate *.cpp *.h *.ui -ts ~/Programming/leechcraft/translations/$1/leechcraft_$1.ts
-cp *.ts ~/Programming/leechcraft/translations/$1/
-cd ~/Programming/leechcraft/translations/$1/
-for i in `ls *.ts`; do ts2po -i $i -o "${i%.*}".po; rm $i; done
-mv leechcraft_$1.po leechcraft_$1.pot
