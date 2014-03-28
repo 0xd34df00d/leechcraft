aadf0e4118d57774b76686dfc85f7bb9483dd507
0xd34df00d <0xd34df00d@gmail.com>
2014-03-28 21:43:40 +0400

    [LMP] Smaller timeout on insertion.

--- src/plugins/lmp/engine/path.cpp
+++ src/plugins/lmp/engine/path.cpp
@@ -215,7 +215,7 @@ namespace LMP
 				gst_element_link_many (NextWholeElems_.at (0), elem, NextWholeElems_.at (1), nullptr);
 
 				GstState wholeCurrent, pending;
-				gst_element_get_state (GetWholeOut (), &wholeCurrent, &pending, GST_SECOND);
+				gst_element_get_state (GetWholeOut (), &wholeCurrent, &pending, 0.1 * GST_SECOND);
 				gst_element_set_state (elem, std::max (wholeCurrent, pending));
 
 				NextWholeElems_.insert (1, elem);
