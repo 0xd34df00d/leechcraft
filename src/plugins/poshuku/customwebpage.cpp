a940528870c13b11a3089034d36e4928f12e189b
0xd34df00d <0xd34df00d@gmail.com>
2014-04-10 14:21:34 +0400

    [Poshuku] Add Function.prototype.bind, fixes #1580.

--- src/plugins/poshuku/customwebpage.cpp
+++ src/plugins/poshuku/customwebpage.cpp
@@ -330,6 +330,31 @@ namespace Poshuku
 
 		frame->addToJavaScriptWindowObject ("JSProxy", JSProxy_.get ());
 		frame->addToJavaScriptWindowObject ("external", ExternalProxy_.get ());
+		frame->evaluateJavaScript (R"delim(
+			if (!Function.prototype.bind) {
+			Function.prototype.bind = function (oThis) {
+				if (typeof this !== "function") {
+				// closest thing possible to the ECMAScript 5 internal IsCallable function
+				throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+				}
+
+				var aArgs = Array.prototype.slice.call(arguments, 1),
+					fToBind = this,
+					fNOP = function () {},
+					fBound = function () {
+					return fToBind.apply(this instanceof fNOP && oThis
+											? this
+											: oThis,
+										aArgs.concat(Array.prototype.slice.call(arguments)));
+					};
+
+				fNOP.prototype = this.prototype;
+				fBound.prototype = new fNOP();
+
+				return fBound;
+			};
+			}
+		)delim");
 	}
 
 	void CustomWebPage::handleGeometryChangeRequested (const QRect& rect)
