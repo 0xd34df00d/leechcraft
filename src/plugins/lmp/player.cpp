bd3efe90c936ac0bf4521e7f9346af0dcb1b576f
0xd34df00d <0xd34df00d@gmail.com>
2014-04-29 22:10:21 +0400

    [LMP] Pass the URL object in the AN entity for #1611.

--- src/plugins/lmp/player.cpp
+++ src/plugins/lmp/player.cpp
@@ -844,8 +844,7 @@ namespace LMP
 				"org.LeechCraft.LMP.PlaybackStatus", {}, 0, 1, str);
 		e.Mime_ += "+advanced";
 		e.Additional_ [AN::Field::MediaPlaybackStatus] = stateStr;
-		e.Additional_ [AN::Field::MediaPlayerURL] =
-				Source_->GetCurrentSource ().ToUrl ().toEncoded ();
+		e.Additional_ [AN::Field::MediaPlayerURL] = Source_->GetCurrentSource ().ToUrl ();
 
 		e.Additional_ [AN::Field::MediaArtist] = mediaInfo.Artist_;
 		e.Additional_ [AN::Field::MediaAlbum] = mediaInfo.Album_;
