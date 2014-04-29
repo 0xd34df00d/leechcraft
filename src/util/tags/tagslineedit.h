7451245838c32ad197ca412e925b3ea279d76bcd
0xd34df00d <0xd34df00d@gmail.com>
2014-04-29 17:14:14 +0400

    [Util] Code style.

--- src/util/tags/tagslineedit.h
+++ src/util/tags/tagslineedit.h
@@ -27,8 +27,8 @@
  * DEALINGS IN THE SOFTWARE.
  **********************************************************************/
 
-#ifndef UTIL_TAGSLINEEDIT_H
-#define UTIL_TAGSLINEEDIT_H
+#pragma once
+
 #include <memory>
 #include <QLineEdit>
 #include <util/utilconfig.h>
@@ -36,102 +36,99 @@
 
 namespace LeechCraft
 {
-	namespace Util
-	{
-		class TagsCompleter;
+namespace Util
+{
+	class TagsCompleter;
 
-		/** @brief A line edit class suitable for use with TagsCompleter.
+	/** @brief A line edit class suitable for use with TagsCompleter.
+	 *
+	 * One would need this extra class because of custom behavior of both
+	 * tags completer and line edit semantics.
+	 *
+	 * @sa TagsCompleter
+	 */
+	class UTIL_API TagsLineEdit : public QLineEdit
+	{
+		Q_OBJECT
+
+		friend class TagsCompleter;
+
+		std::unique_ptr<CategorySelector> CategorySelector_;
+		TagsCompleter *Completer_;
+
+		QString Separator_;
+	public:
+		/** @brief Constructs the line edit widget.
 		 *
-		 * One would need this extra class because of custom behavior of both
-		 * tags completer and line edit semantics.
+		 * Creates the line edit widget.
+		 *
+		 * @param[in] parent Parent widget.
+		 */
+		TagsLineEdit (QWidget *parent);
+
+		/** @brief Adds the selector widget to the line edit.
+		 *
+		 * Because this function uses the completion model, it should be
+		 * used after a TagsCompleter has been set on this line edit.
 		 *
 		 * @sa TagsCompleter
 		 */
-		class UTIL_API TagsLineEdit : public QLineEdit
-		{
-			Q_OBJECT
+		void AddSelector ();
 
-			friend class TagsCompleter;
+		/** @brief Returns the separator for the tags.
+		 *
+		 * The default separator is "; ".
+		 *
+		 * @sa SetSeparator()
+		 */
+		QString GetSeparator () const;
 
-			std::unique_ptr<CategorySelector> CategorySelector_;
-			TagsCompleter *Completer_;
+		/** @brief Sets the separator for the tags.
+		 *
+		 * This function doesn't update the text in the line edit.
+		 *
+		 * @sa GetSeparator()
+		 */
+		void SetSeparator (const QString&);
+	public slots:
+		/** @brief Completes the string.
+		 *
+		 * Completes the current text in line edit with completion passed
+		 * throught string parameter.
+		 *
+		 * @param[in] string String with completion.
+		 */
+		void insertTag (const QString& string);
 
-			QString Separator_;
-		public:
-			/** @brief Constructs the line edit widget.
-			 *
-			 * Creates the line edit widget.
-			 *
-			 * @param[in] parent Parent widget.
-			 */
-			TagsLineEdit (QWidget *parent);
+		/** @brief Sets thew new list of the available tags.
+		 *
+		 * The list of tags will be passed to the selector if it was
+		 * added via AddSelector().
+		 *
+		 * @param[in] allTags The list of new available tags.
+		 */
+		void handleTagsUpdated (const QStringList& allTags);
 
-			/** @brief Adds the selector widget to the line edit.
-			 *
-			 * Because this function uses the completion model, it should be
-			 * used after a TagsCompleter has been set on this line edit.
-			 *
-			 * @sa TagsCompleter
-			 */
-			void AddSelector ();
-
-			/** @brief Returns the separator for the tags.
-			 *
-			 * The default separator is "; ".
-			 *
-			 * @sa SetSeparator()
-			 */
-			QString GetSeparator () const;
-
-			/** @brief Sets the separator for the tags.
-			 *
-			 * This function doesn't update the text in the line edit.
-			 *
-			 * @sa GetSeparator()
-			 */
-			void SetSeparator (const QString&);
-		public slots:
-			/** @brief Completes the string.
-			 *
-			 * Completes the current text in line edit with completion passed
-			 * throught string parameter.
-			 *
-			 * @param[in] string String with completion.
-			 */
-			void insertTag (const QString& string);
-
-			/** @brief Sets thew new list of the available tags.
-			 *
-			 * The list of tags will be passed to the selector if it was
-			 * added via AddSelector().
-			 *
-			 * @param[in] allTags The list of new available tags.
-			 */
-			void handleTagsUpdated (const QStringList& allTags);
-
-			/** @brief Sets the currently selected tags.
-			 *
-			 * Sets the line edit text to tags joined by separator. If
-			 * tags selector is installed via AddSelector(), the selector
-			 * is updated as well.
-			 *
-			 * @param[in] tags The list of selected tags.
-			 */
-			void setTags (const QStringList& tags);
-		private slots:
-			void handleSelectionChanged (const QStringList&);
-		protected:
-			virtual void keyPressEvent (QKeyEvent*);
-			virtual void focusInEvent (QFocusEvent*);
-			virtual void contextMenuEvent (QContextMenuEvent*);
-			void SetCompleter (TagsCompleter*);
-		private:
-			QString textUnderCursor () const;
-		signals:
-			void tagsChosen ();
-		};
+		/** @brief Sets the currently selected tags.
+		 *
+		 * Sets the line edit text to tags joined by separator. If
+		 * tags selector is installed via AddSelector(), the selector
+		 * is updated as well.
+		 *
+		 * @param[in] tags The list of selected tags.
+		 */
+		void setTags (const QStringList& tags);
+	private slots:
+		void handleSelectionChanged (const QStringList&);
+	protected:
+		virtual void keyPressEvent (QKeyEvent*);
+		virtual void focusInEvent (QFocusEvent*);
+		virtual void contextMenuEvent (QContextMenuEvent*);
+		void SetCompleter (TagsCompleter*);
+	private:
+		QString textUnderCursor () const;
+	signals:
+		void tagsChosen ();
 	};
-};
-
-#endif
-
+}
+}
