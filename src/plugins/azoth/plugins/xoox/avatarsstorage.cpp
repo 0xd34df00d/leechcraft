0377b3b3a3952f7528bdae9daf095b3f6a8a92d4
0xd34df00d <0xd34df00d@gmail.com>
2014-05-05 16:52:49 +0400

    [Azoth Xoox] Async threaded avatars saving.

--- src/plugins/azoth/plugins/xoox/avatarsstorage.cpp
+++ src/plugins/azoth/plugins/xoox/avatarsstorage.cpp
@@ -28,6 +28,7 @@
  **********************************************************************/
 
 #include "avatarsstorage.h"
+#include <memory>
 #include <QTimer>
 #include <QImage>
 #include <QtConcurrentRun>
@@ -68,20 +69,20 @@ namespace Xoox
 	 */
 	void AvatarsStorage::StoreAvatar (const QImage& image, const QByteArray& hash)
 	{
-		QFile file (AvatarsDir_.absoluteFilePath (hash));
-		if (file.exists () && file.size ())
+		auto file = std::make_shared<QFile> (AvatarsDir_.absoluteFilePath (hash));
+		if (file->exists () && file->size ())
 			return;
 
-		if (!file.open (QIODevice::WriteOnly | QIODevice::Truncate))
+		if (!file->open (QIODevice::WriteOnly | QIODevice::Truncate))
 		{
 			qWarning () << Q_FUNC_INFO
 					<< "unable to open file"
-					<< file.fileName ()
+					<< file->fileName ()
 					<< "for writing";
 			return;
 		}
 
-		image.save (&file, "PNG", 0);
+		QtConcurrent::run ([file, image] { image.save (file.get (), "PNG", 0); });
 	}
 
 	QImage AvatarsStorage::GetAvatar (const QByteArray& hash) const
