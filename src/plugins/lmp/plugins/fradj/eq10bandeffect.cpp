6284c414ceb1412e0ee95cc97a3e902b813f47bc
0xd34df00d <0xd34df00d@gmail.com>
2014-05-18 20:28:24 +0400

    [LMP FA] Eq10BandEffect::SetGains().

--- src/plugins/lmp/plugins/fradj/eq10bandeffect.cpp
+++ src/plugins/lmp/plugins/fradj/eq10bandeffect.cpp
@@ -28,6 +28,7 @@
  **********************************************************************/
 
 #include "eq10bandeffect.h"
+#include <QtDebug>
 #include <gst/gst.h>
 #include "eqconfigurator.h"
 
@@ -78,6 +79,22 @@ namespace Fradj
 
 	void Eq10BandEffect::SetGains (const QList<double>& gains)
 	{
+		if (gains.size () != GetFixedBands ().size ())
+		{
+			qWarning () << Q_FUNC_INFO
+					<< "unexpected gains count:"
+					<< gains;
+			return;
+		}
+
+		for (int i = 0; i < gains.size (); ++i)
+		{
+			const auto& optName = "band" + QByteArray::number (i);
+			g_object_set (Equalizer_,
+					optName.constData (),
+					static_cast<gdouble> (gains.at (i)),
+					nullptr);
+		}
 	}
 
 	GstElement* Eq10BandEffect::GetElement () const
