4bda052548192925ace1553bac225fe33e2a35c6
0xd34df00d <0xd34df00d@gmail.com>
2014-05-28 13:27:40 +0400

    [Interfaces] Documented IANRulesStorage.

--- src/interfaces/an/ianrulesstorage.h
+++ src/interfaces/an/ianrulesstorage.h
@@ -39,15 +39,42 @@ namespace LeechCraft
 	struct Entity;
 }
 
+/** @brief Interface for plugins managing Advanced Notifications rules.
+ *
+ * This interface should be implemented by plugins that provide means to
+ * store and manage the Advanced Notifications rules. Other plugins may
+ * use this interface to query for different rules and their options.
+ */
 class IANRulesStorage
 {
 public:
 	virtual ~IANRulesStorage () {}
 
+	/** @brief Returns all rules matching the \em category.
+	 *
+	 * The category can be either one of the predefined categories (like
+	 * LeechCraft::AN::CatIM) or empty, in which case all the rules are
+	 * returned.
+	 *
+	 * @param[in] category The category of the rules to return, or empty
+	 * for all rules.
+	 * @return The list of all rules matching the given \em category.
+	 */
 	virtual QList<LeechCraft::Entity> GetAllRules (const QString& category) const = 0;
 
+	/** @brief Requests opening the configuration of the given \em rule.
+	 *
+	 * @param[in] rule One of the rules returned from GetAllRules().
+	 */
 	virtual void RequestRuleConfiguration (const LeechCraft::Entity& rule) = 0;
 protected:
+	/** @brief Emitted when the rules change.
+	 *
+	 * This signal is emitted either when a new rule has been added, or an
+	 * already existing rule has been deleted or modified.
+	 *
+	 * @note This function is expected to be a signal.
+	 */
 	virtual void rulesChanged () = 0;
 };
 
