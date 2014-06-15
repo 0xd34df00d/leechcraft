ec18e1d195b1c1193ac4428675d00975bccab356
0xd34df00d <0xd34df00d@gmail.com>
2014-06-15 23:19:59 +0400

    [ER] Updated translations.

--- src/plugins/eleeminator/leechcraft_eleeminator_en.ts
+++ src/plugins/eleeminator/leechcraft_eleeminator_en.ts
@@ -2,37 +2,98 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
+    <name>CloseDialog</name>
+    <message>
+        <location filename="closedialog.ui" line="14"/>
+        <source>Close tab</source>
+        <translation>Close tab</translation>
+    </message>
+    <message>
+        <location filename="closedialog.ui" line="20"/>
+        <source>This tab has the following processes running. Are you sure you want to close it?</source>
+        <translation>This tab has the following processes running. Are you sure you want to close it?</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Eleeminator</name>
     <message>
-        <location filename="termtab.cpp" line="48"/>
+        <location filename="termtab.cpp" line="63"/>
         <source>Terminal toolbar</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminal toolbar</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::Eleeminator::Plugin</name>
     <message>
-        <location filename="eleeminator.cpp" line="44"/>
+        <location filename="eleeminator.cpp" line="51"/>
+        <source>Close terminal tab</source>
+        <translation>Close terminal tab</translation>
+    </message>
+    <message>
+        <location filename="eleeminator.cpp" line="57"/>
+        <source>Copy selected text to clipboard</source>
+        <translation>Copy selected text to clipboard</translation>
+    </message>
+    <message>
+        <location filename="eleeminator.cpp" line="63"/>
+        <source>Paste text from clipboard</source>
+        <translation>Paste text from clipboard</translation>
+    </message>
+    <message>
+        <location filename="eleeminator.cpp" line="73"/>
         <source>Terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminal</translation>
     </message>
     <message>
-        <location filename="eleeminator.cpp" line="45"/>
+        <location filename="eleeminator.cpp" line="74"/>
         <source>Termianl emulator.</source>
-        <translation type="unfinished"></translation>
+        <translation>Termianl emulator.</translation>
     </message>
     <message>
-        <location filename="eleeminator.cpp" line="72"/>
+        <location filename="eleeminator.cpp" line="101"/>
         <source>Embedded LeechCraft terminal emulator.</source>
-        <translation type="unfinished"></translation>
+        <translation>Embedded LeechCraft terminal emulator.</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::Eleeminator::TermTab</name>
     <message>
-        <location filename="termtab.cpp" line="94"/>
+        <location filename="termtab.cpp" line="156"/>
         <source>Color scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Color scheme</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="200"/>
+        <source>Select font...</source>
+        <translation>Select font...</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="231"/>
+        <source>Copy selected text</source>
+        <translation>Copy selected text</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="237"/>
+        <source>Paste from clipboard</source>
+        <translation>Paste from clipboard</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="processgraphbuilder.cpp" line="169"/>
+        <source>PID</source>
+        <translation>PID</translation>
+    </message>
+    <message>
+        <location filename="processgraphbuilder.cpp" line="169"/>
+        <source>Command</source>
+        <translation>Command</translation>
+    </message>
+    <message>
+        <location filename="processgraphbuilder.cpp" line="169"/>
+        <source>Arguments</source>
+        <translation>Arguments</translation>
     </message>
 </context>
 </TS>
--- src/plugins/eleeminator/leechcraft_eleeminator_ru_RU.ts
+++ src/plugins/eleeminator/leechcraft_eleeminator_ru_RU.ts
@@ -2,9 +2,22 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
+    <name>CloseDialog</name>
+    <message>
+        <location filename="closedialog.ui" line="14"/>
+        <source>Close tab</source>
+        <translation>Закрытие вкладки</translation>
+    </message>
+    <message>
+        <location filename="closedialog.ui" line="20"/>
+        <source>This tab has the following processes running. Are you sure you want to close it?</source>
+        <translation>В этой вкладке выполняются следующие процессы. Вы уверены, что хотите закрыть её?</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Eleeminator</name>
     <message>
-        <location filename="termtab.cpp" line="48"/>
+        <location filename="termtab.cpp" line="63"/>
         <source>Terminal toolbar</source>
         <translation>Панель инструментов терминала</translation>
     </message>
@@ -12,17 +25,32 @@
 <context>
     <name>LeechCraft::Eleeminator::Plugin</name>
     <message>
-        <location filename="eleeminator.cpp" line="44"/>
+        <location filename="eleeminator.cpp" line="51"/>
+        <source>Close terminal tab</source>
+        <translation>Закрыть вкладку терминала</translation>
+    </message>
+    <message>
+        <location filename="eleeminator.cpp" line="57"/>
+        <source>Copy selected text to clipboard</source>
+        <translation>Скопировать выделенный текст в буфер обмена</translation>
+    </message>
+    <message>
+        <location filename="eleeminator.cpp" line="63"/>
+        <source>Paste text from clipboard</source>
+        <translation>Вставить текст из буфера обмена</translation>
+    </message>
+    <message>
+        <location filename="eleeminator.cpp" line="73"/>
         <source>Terminal</source>
         <translation>Терминал</translation>
     </message>
     <message>
-        <location filename="eleeminator.cpp" line="45"/>
+        <location filename="eleeminator.cpp" line="74"/>
         <source>Termianl emulator.</source>
         <translation>Эмулятор терминала.</translation>
     </message>
     <message>
-        <location filename="eleeminator.cpp" line="72"/>
+        <location filename="eleeminator.cpp" line="101"/>
         <source>Embedded LeechCraft terminal emulator.</source>
         <translation>Встроенный в LeechCraft эмулятор терминала.</translation>
     </message>
@@ -30,9 +58,42 @@
 <context>
     <name>LeechCraft::Eleeminator::TermTab</name>
     <message>
-        <location filename="termtab.cpp" line="94"/>
+        <location filename="termtab.cpp" line="156"/>
         <source>Color scheme</source>
         <translation>Цветовая схема</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="200"/>
+        <source>Select font...</source>
+        <translation>Выбрать шрифт…</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="231"/>
+        <source>Copy selected text</source>
+        <translation>Скопировать выбранный текст</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="237"/>
+        <source>Paste from clipboard</source>
+        <translation>Вставить из буфера обмена</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="processgraphbuilder.cpp" line="169"/>
+        <source>PID</source>
+        <translation>PID</translation>
+    </message>
+    <message>
+        <location filename="processgraphbuilder.cpp" line="169"/>
+        <source>Command</source>
+        <translation>Команда</translation>
+    </message>
+    <message>
+        <location filename="processgraphbuilder.cpp" line="169"/>
+        <source>Arguments</source>
+        <translation>Аргументы</translation>
     </message>
 </context>
 </TS>
