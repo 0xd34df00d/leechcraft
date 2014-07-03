28d50c556e6e6b40b4f30357b934afe73f03ffc2
0xd34df00d <0xd34df00d@gmail.com>
2014-07-03 23:20:48 +0400

    [HH] Fixed FreeBSD build.

--- src/plugins/htthare/requesthandler.cpp
+++ src/plugins/htthare/requesthandler.cpp
@@ -309,14 +309,12 @@ namespace HttHare
 					const auto transferred = rc > 0 ? rc : 0;
 					const auto errCode = rc > 0 ? 0 : errno;
 #elif defined (Q_OS_FREEBSD)
-					auto transferred = toTransfer;
+					off_t transferred = toTransfer;
 					const auto rc = sendfile (File_->handle (), Sock_.native_handle (),
 							offset, toTransfer, nullptr, &transferred, 0);
 					if (rc == -1)
-					{
 						transferred = 0;
-						errCode = errno;
-					}
+					const auto errCode = rc == -1 ? errno : 0;
 #elif defined (Q_OS_MAC)
 					auto transferred = toTransfer;
 					auto errCode = sendfile (File_->handle (),
