569e6fefcabaa77bcaf7e815f45ab15c080e18fb
0xd34df00d <0xd34df00d@gmail.com>
2014-07-18 23:21:02 +0400

    [Azoth MuC] Updated translations.

--- src/plugins/azoth/plugins/mucommands/leechcraft_azoth_mucommands_en.ts
+++ src/plugins/azoth/plugins/mucommands/leechcraft_azoth_mucommands_en.ts
@@ -4,7 +4,7 @@
 <context>
     <name>LeechCraft::Azoth::MuCommands::Plugin</name>
     <message>
-        <location filename="mucommands.cpp" line="197"/>
+        <location filename="mucommands.cpp" line="221"/>
         <source>Provides some common commands, both for conferences and for private chats, for Azoth.</source>
         <translation>Provides some common commands, both for conferences and for private chats, for Azoth.</translation>
     </message>
@@ -12,153 +12,206 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="commands.cpp" line="101"/>
+        <location filename="commands.cpp" line="102"/>
         <source>MUC&apos;s participants: </source>
         <translation>MUC&apos;s participants: </translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="139"/>
+        <location filename="commands.cpp" line="140"/>
         <source>Sorry, no links found, chat more!</source>
         <translation>Sorry, no links found, chat more!</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="140"/>
+        <location filename="commands.cpp" line="141"/>
         <source>Found links:</source>
         <translation>Found links:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="345"/>
+        <location filename="commands.cpp" line="346"/>
         <source>Unable to resolve %1.</source>
         <translation>Unable to resolve %1.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="360"/>
+        <location filename="commands.cpp" line="361"/>
         <source>%1 doesn&apos;t support extended metainformation.</source>
         <translation>%1 doesn&apos;t support extended metainformation.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="368"/>
+        <location filename="commands.cpp" line="369"/>
         <source>no information, would wait for next vcard update...</source>
         <translation>no information, would wait for next vcard update...</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="401"/>
+        <location filename="commands.cpp" line="402"/>
         <source>Type</source>
         <translation>Type</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="402"/>
+        <location filename="commands.cpp" line="403"/>
         <source>Name</source>
         <translation>Name</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="403"/>
+        <location filename="commands.cpp" line="404"/>
         <source>Version</source>
         <translation>Version</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="404"/>
+        <location filename="commands.cpp" line="405"/>
         <source>OS</source>
         <translation>OS</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="424"/>
+        <location filename="commands.cpp" line="425"/>
         <source>Client information for %1:</source>
         <translation>Client information for %1:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="429"/>
+        <location filename="commands.cpp" line="430"/>
         <source>no information available.</source>
         <translation>no information available.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="469"/>
+        <location filename="commands.cpp" line="470"/>
         <source>%1 does not support querying time.</source>
         <translation>%1 does not support querying time.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="495"/>
+        <location filename="commands.cpp" line="496"/>
         <source>Current time for %1:</source>
         <translation>Current time for %1:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="498"/>
+        <location filename="commands.cpp" line="499"/>
         <source>Local time: %1</source>
         <translation>Local time: %1</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="501"/>
+        <location filename="commands.cpp" line="502"/>
         <source>Timezone: %1</source>
         <translation>Timezone: %1</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="506"/>
+        <location filename="commands.cpp" line="507"/>
         <source>UTC time: %1</source>
         <translation>UTC time: %1</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="531"/>
+        <location filename="commands.cpp" line="532"/>
         <source>Entity time for %1:</source>
         <translation>Entity time for %1:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="545"/>
+        <location filename="commands.cpp" line="546"/>
         <source>%1&apos;s account does not support service discovery.</source>
         <translation>%1&apos;s account does not support service discovery.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="557"/>
+        <location filename="commands.cpp" line="558"/>
         <source>Unable to create service discovery session for %1.</source>
         <translation>Unable to create service discovery session for %1.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="713"/>
+        <location filename="commands.cpp" line="715"/>
+        <location filename="commands.cpp" line="825"/>
+        <source>%1 (or its protocol %2) does not support permissions.</source>
+        <translation>%1 (or its protocol %2) does not support permissions.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="732"/>
+        <source>Permission class %1 (%2):</source>
+        <translation>Permission class %1 (%2):</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="741"/>
+        <source>Available role classes and their values:</source>
+        <translation>Available role classes and their values:</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="842"/>
+        <source>Invalid syntax. Type %1 for more information.</source>
+        <translation>Invalid syntax. Type %1 for more information.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="860"/>
+        <source>Unknown mode %1. Type %2 for more information.</source>
+        <translation>Unknown mode %1. Type %2 for more information.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="874"/>
+        <source>Unknown participant %1.</source>
+        <translation>Unknown participant %1.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="890"/>
+        <location filename="commands.cpp" line="901"/>
+        <source>Unknown permission class %1, available classes are: %2</source>
+        <translation>Unknown permission class %1, available classes are: %2</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="918"/>
+        <source>Cannot change %1&apos;s role of class %2 (%3) to %4 (%5).</source>
+        <translation>Cannot change %1&apos;s role of class %2 (%3) to %4 (%5).</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="949"/>
         <source>Last activity of %1: %2.</source>
         <translation>Last activity of %1: %2.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="715"/>
+        <location filename="commands.cpp" line="951"/>
         <source>Last connection of %1: %2.</source>
         <translation>Last connection of %1: %2.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="717"/>
+        <location filename="commands.cpp" line="953"/>
         <source>%1&apos;s uptime: %2.</source>
         <translation>%1&apos;s uptime: %2.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="734"/>
+        <location filename="commands.cpp" line="970"/>
         <source>%1 does not support last activity.</source>
         <translation>%1 does not support last activity.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="789"/>
-        <location filename="commands.cpp" line="812"/>
+        <location filename="commands.cpp" line="1025"/>
+        <location filename="commands.cpp" line="1048"/>
         <source>Invited %1 to %2.</source>
         <translation>Invited %1 to %2.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="799"/>
+        <location filename="commands.cpp" line="1035"/>
         <source>Unable to resolve multiuser chat for %1.</source>
         <translation>Unable to resolve multiuser chat for %1.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="807"/>
+        <location filename="commands.cpp" line="1043"/>
         <source>%1 is not a multiuser chat.</source>
         <translation>%1 is not a multiuser chat.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="834"/>
+        <location filename="commands.cpp" line="1066"/>
+        <location filename="commands.cpp" line="1099"/>
         <source>Unable to find participant %1.</source>
         <translation>Unable to find participant %1.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="856"/>
+        <location filename="commands.cpp" line="1074"/>
+        <source>Unable to get real ID of %1.</source>
+        <translation>Unable to get real ID of %1.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="1078"/>
+        <source>%1&apos;s real ID: %2.</source>
+        <translation>%1&apos;s real ID: %2.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="1121"/>
         <source>%1 does not support pinging.</source>
         <translation>%1 does not support pinging.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="868"/>
+        <location filename="commands.cpp" line="1133"/>
         <source>Pong from %1: %2 ms.</source>
         <translation>Pong from %1: %2 ms.</translation>
     </message>
--- src/plugins/azoth/plugins/mucommands/leechcraft_azoth_mucommands_ru_RU.ts
+++ src/plugins/azoth/plugins/mucommands/leechcraft_azoth_mucommands_ru_RU.ts
@@ -4,7 +4,7 @@
 <context>
     <name>LeechCraft::Azoth::MuCommands::Plugin</name>
     <message>
-        <location filename="mucommands.cpp" line="197"/>
+        <location filename="mucommands.cpp" line="221"/>
         <source>Provides some common commands, both for conferences and for private chats, for Azoth.</source>
         <translation>Предоставляет некоторые привычные команды и для конференций, и для обычных бесед.</translation>
     </message>
@@ -12,153 +12,206 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="commands.cpp" line="101"/>
+        <location filename="commands.cpp" line="102"/>
         <source>MUC&apos;s participants: </source>
         <translation>Участники конференции:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="139"/>
+        <location filename="commands.cpp" line="140"/>
         <source>Sorry, no links found, chat more!</source>
         <translation>Не найдено ни одной ссылки, пообщайтесь ещё!</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="140"/>
+        <location filename="commands.cpp" line="141"/>
         <source>Found links:</source>
         <translation>Найдены ссылки:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="345"/>
+        <location filename="commands.cpp" line="346"/>
         <source>Unable to resolve %1.</source>
         <translation>Невозможно найти запись для %1.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="360"/>
+        <location filename="commands.cpp" line="361"/>
         <source>%1 doesn&apos;t support extended metainformation.</source>
         <translation>%1 не поддерживает расширенную метаинформацию.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="368"/>
+        <location filename="commands.cpp" line="369"/>
         <source>no information, would wait for next vcard update...</source>
         <translation>нет информации, ждём обновления vcard…</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="401"/>
+        <location filename="commands.cpp" line="402"/>
         <source>Type</source>
         <translation>Тип</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="402"/>
+        <location filename="commands.cpp" line="403"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="403"/>
+        <location filename="commands.cpp" line="404"/>
         <source>Version</source>
         <translation>Версия</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="404"/>
+        <location filename="commands.cpp" line="405"/>
         <source>OS</source>
         <translation>ОС</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="424"/>
+        <location filename="commands.cpp" line="425"/>
         <source>Client information for %1:</source>
         <translation>Информация о клиенте %1:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="429"/>
+        <location filename="commands.cpp" line="430"/>
         <source>no information available.</source>
         <translation>нет информации.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="469"/>
+        <location filename="commands.cpp" line="470"/>
         <source>%1 does not support querying time.</source>
         <translation>%1 не поддерживает запрос времени.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="495"/>
+        <location filename="commands.cpp" line="496"/>
         <source>Current time for %1:</source>
         <translation>Текущее время %1:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="498"/>
+        <location filename="commands.cpp" line="499"/>
         <source>Local time: %1</source>
         <translation>Локальное время: %1</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="501"/>
+        <location filename="commands.cpp" line="502"/>
         <source>Timezone: %1</source>
         <translation>Временная зона: %1</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="506"/>
+        <location filename="commands.cpp" line="507"/>
         <source>UTC time: %1</source>
         <translation>Время UTC: %1</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="531"/>
+        <location filename="commands.cpp" line="532"/>
         <source>Entity time for %1:</source>
         <translation>Время %1:</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="545"/>
+        <location filename="commands.cpp" line="546"/>
         <source>%1&apos;s account does not support service discovery.</source>
         <translation>Учётная запись %1 не поддерживает обзор сервисов.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="557"/>
+        <location filename="commands.cpp" line="558"/>
         <source>Unable to create service discovery session for %1.</source>
         <translation>Невозможно создать сессию обзора сервисов для %1.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="713"/>
+        <location filename="commands.cpp" line="715"/>
+        <location filename="commands.cpp" line="825"/>
+        <source>%1 (or its protocol %2) does not support permissions.</source>
+        <translation>%1 (или его протокол %2) не поддерживает разрешения.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="732"/>
+        <source>Permission class %1 (%2):</source>
+        <translation>Класс разрешений %1 (%2):</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="741"/>
+        <source>Available role classes and their values:</source>
+        <translation>Доступные классы и их значения:</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="842"/>
+        <source>Invalid syntax. Type %1 for more information.</source>
+        <translation>Некорректный синтаксис. Наберите %1 для справки.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="860"/>
+        <source>Unknown mode %1. Type %2 for more information.</source>
+        <translation>Неизвестный режим %1. Наберите %2 для справки.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="874"/>
+        <source>Unknown participant %1.</source>
+        <translation>Неизвестный участник %1.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="890"/>
+        <location filename="commands.cpp" line="901"/>
+        <source>Unknown permission class %1, available classes are: %2</source>
+        <translation>Неизвестный класс разрешений %1, доступные классы: %2</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="918"/>
+        <source>Cannot change %1&apos;s role of class %2 (%3) to %4 (%5).</source>
+        <translation>Невозможно установить роль класса %2 (%3) участника %1 в %4 (%5).</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="949"/>
         <source>Last activity of %1: %2.</source>
         <translation>Последняя активность %1: %2.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="715"/>
+        <location filename="commands.cpp" line="951"/>
         <source>Last connection of %1: %2.</source>
         <translation>Последнее соединение %1: %2.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="717"/>
+        <location filename="commands.cpp" line="953"/>
         <source>%1&apos;s uptime: %2.</source>
         <translation>Аптайм %1: %2.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="734"/>
+        <location filename="commands.cpp" line="970"/>
         <source>%1 does not support last activity.</source>
         <translation>%1 не поддерживает запрос времени бездействия.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="789"/>
-        <location filename="commands.cpp" line="812"/>
+        <location filename="commands.cpp" line="1025"/>
+        <location filename="commands.cpp" line="1048"/>
         <source>Invited %1 to %2.</source>
         <translation>Контакт %1 приглашён в %2.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="799"/>
+        <location filename="commands.cpp" line="1035"/>
         <source>Unable to resolve multiuser chat for %1.</source>
         <translation>Невозможно найти многопользовательскую конференцию для %1.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="807"/>
+        <location filename="commands.cpp" line="1043"/>
         <source>%1 is not a multiuser chat.</source>
         <translation>%1 — не многопользовательский чат.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="834"/>
+        <location filename="commands.cpp" line="1066"/>
+        <location filename="commands.cpp" line="1099"/>
         <source>Unable to find participant %1.</source>
         <translation>Невозможно найти участника %1.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="856"/>
+        <location filename="commands.cpp" line="1074"/>
+        <source>Unable to get real ID of %1.</source>
+        <translation>Невозможно получить настоящий ID участника %1.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="1078"/>
+        <source>%1&apos;s real ID: %2.</source>
+        <translation>Настоящий ID участника %1: %2.</translation>
+    </message>
+    <message>
+        <location filename="commands.cpp" line="1121"/>
         <source>%1 does not support pinging.</source>
         <translation>%1 не поддерживает пинги.</translation>
     </message>
     <message>
-        <location filename="commands.cpp" line="868"/>
+        <location filename="commands.cpp" line="1133"/>
         <source>Pong from %1: %2 ms.</source>
         <translation>Получен ответ от %1 за %2 мс.</translation>
     </message>
