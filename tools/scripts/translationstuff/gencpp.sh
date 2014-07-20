1cbc917f44a064af5d5353e96a518d7233bc82ba
0xd34df00d <0xd34df00d@gmail.com>
2014-07-20 22:22:27 +0400

    [Tools] Append to dummy.cpp instead of overwriting in gencpp.sh.

--- tools/scripts/translationstuff/gencpp.sh
+++ tools/scripts/translationstuff/gencpp.sh
@@ -2,4 +2,4 @@
 
 stripped=$(/usr/bin/basename $1 .xml)
 stylepath=`dirname $0`
-xsltproc $stylepath/transform.xsl $1 | grep QT_TRANSL | sed s/__FILENAME__/$stripped/g > dummy.cpp
+xsltproc $stylepath/transform.xsl $1 | grep QT_TRANSL | sed s/__FILENAME__/$stripped/g >> dummy.cpp
