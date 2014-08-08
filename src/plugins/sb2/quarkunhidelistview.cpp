fb562852eac85284f32b862a52a29e6dced1d46b
0xd34df00d <0xd34df00d@gmail.com>
2014-08-09 02:20:09 +0400

    [SB2] Removed unused include.

--- src/plugins/sb2/quarkunhidelistview.cpp
+++ src/plugins/sb2/quarkunhidelistview.cpp
@@ -28,7 +28,6 @@
  **********************************************************************/
 
 #include "quarkunhidelistview.h"
-#include <QDeclarativeView>
 #include <QGraphicsObject>
 #include <QtDebug>
 #include <util/util.h>
