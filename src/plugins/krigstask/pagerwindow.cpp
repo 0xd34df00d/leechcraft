a44e480f95fa58fa4a3e6408f7665287974e73dd
0xd34df00d <0xd34df00d@gmail.com>
2014-08-15 03:44:49 +0400

    [KT] Line-by-line (but not working) window snapshotting port.

--- src/plugins/krigstask/pagerwindow.cpp
+++ src/plugins/krigstask/pagerwindow.cpp
@@ -32,6 +32,7 @@
 #include <QStandardItemModel>
 #include <QDesktopWidget>
 #include <QApplication>
+#include <QSysInfo>
 
 #if QT_VERSION < 0x050000
 #include <QDeclarativeContext>
@@ -55,6 +56,13 @@
 #include <X11/extensions/Xrender.h>
 #include <X11/extensions/Xfixes.h>
 #include <X11/extensions/Xcomposite.h>
+
+#if QT_VERSION >= 0x050000
+#include <xcb/xcb.h>
+#include <xcb/render.h>
+#include <xcb/composite.h>
+#include <xcb/xcb_renderutil.h>
+#endif
 
 namespace LeechCraft
 {
@@ -221,13 +229,7 @@ namespace Krigstask
 
 	namespace
 	{
-#if QT_VERSION >= 0x050000
-		QImage XPixmap2Image (Pixmap pixmap)
-		{
-			return {};
-		}
-#endif
-
+#if QT_VERSION < 0x050000
 		QImage GrabWindow (ulong wid)
 		{
 			auto disp = Util::XWrapper::Instance ().GetDisplay ();
@@ -261,10 +263,8 @@ namespace Krigstask
 			auto xpixmap = XCreatePixmap (disp,
 					Util::XWrapper::Instance ().GetRootWindow (),
 					attrs.width, attrs.height, attrs.depth);
-#if QT_VERSION < 0x050000
 			auto pixmap = QPixmap::fromX11Pixmap (xpixmap);
 			pixmap.fill ();
-#endif
 
 			XRenderComposite (disp,
 					PictOpOver,
@@ -274,11 +274,7 @@ namespace Krigstask
 					0, 0, 0, 0,
 					0, 0, attrs.width, attrs.height);
 
-#if QT_VERSION < 0x050000
 			const auto& image = pixmap.toImage ();
-#else
-			const auto& image = XPixmap2Image (xpixmap);
-#endif
 
 			XFreePixmap (disp, xpixmap);
 
@@ -288,6 +284,249 @@ namespace Krigstask
 
 			return image;
 		}
+#else
+		const xcb_format_t* GetFormat (uint8_t depth)
+		{
+			const auto setup = xcb_get_setup (QX11Info::connection ());
+			for (auto i = xcb_setup_pixmap_formats_iterator (setup); i.rem; xcb_format_next (&i))
+					if (i.data->depth == depth)
+						return i.data;
+
+			return {};
+		}
+
+		QImage::Format GetImageFormat (uint8_t depth, const xcb_visualtype_t *visual)
+		{
+			const auto format = GetFormat (depth);
+
+			if (!visual || !format)
+				return QImage::Format_Invalid;
+
+			if (depth == 32 &&
+					format->bits_per_pixel == 32 &&
+					visual->red_mask == 0xff0000 &&
+					visual->green_mask == 0xff00 &&
+					visual->blue_mask == 0xff)
+				return QImage::Format_ARGB32_Premultiplied;
+
+			if (depth == 24 &&
+					format->bits_per_pixel == 32 &&
+					visual->red_mask == 0xff0000 &&
+					visual->green_mask == 0xff00 &&
+					visual->blue_mask == 0xff)
+				return QImage::Format_RGB32;
+
+			if (depth == 16 &&
+					format->bits_per_pixel == 16 &&
+					visual->red_mask == 0xf800 &&
+					visual->green_mask == 0x7e0 &&
+					visual->blue_mask == 0x1f)
+				return QImage::Format_RGB16;
+
+			return QImage::Format_Invalid;
+		}
+
+		const xcb_visualtype_t* GetVisual (const xcb_visualid_t visualId)
+		{
+			const auto sit = xcb_setup_roots_iterator (xcb_get_setup (QX11Info::connection ()));
+
+			for (auto dit = xcb_screen_allowed_depths_iterator (sit.data);
+					dit.rem; xcb_depth_next (&dit))
+				 for (auto vit = xcb_depth_visuals_iterator (dit.data);
+						vit.rem; xcb_visualtype_next (&vit))
+					  if (vit.data->visual_id == visualId)
+						  return vit.data;
+
+			return {};
+		}
+
+		QImage FromXcb (xcb_pixmap_t pixmap,
+				int width, int height, int depth, xcb_visualid_t visualId)
+		{
+			const auto conn = QX11Info::connection ();
+			const auto getCookie = xcb_get_image_unchecked (conn,
+					XCB_IMAGE_FORMAT_Z_PIXMAP, pixmap,
+					0, 0, width, height, 0xffffffff);
+
+			const std::shared_ptr<xcb_get_image_reply_t> imageReply
+			{
+				xcb_get_image_reply (conn, getCookie, nullptr),
+				&free
+			};
+
+			if (!imageReply)
+				return {};
+
+			auto data = xcb_get_image_data (imageReply.get ());
+			const auto length = xcb_get_image_data_length (imageReply.get ());
+
+			const auto format = GetImageFormat (depth, GetVisual (visualId));
+			if (format == QImage::Format_Invalid)
+				return {};
+
+			const auto bpl = length / height;
+
+			QImage image { data, width, height, bpl, format };
+			const auto byteOrder = xcb_get_setup (conn)->image_byte_order;
+
+			if ((QSysInfo::ByteOrder == QSysInfo::LittleEndian && byteOrder == XCB_IMAGE_ORDER_MSB_FIRST) ||
+				(QSysInfo::ByteOrder == QSysInfo::BigEndian && byteOrder == XCB_IMAGE_ORDER_LSB_FIRST))
+			{
+				for (int i = 0; i < height; i++)
+					switch (format)
+					{
+					case QImage::Format_RGB16:
+					{
+						auto px = reinterpret_cast<ushort*> (image.scanLine (i));
+						const auto end = px + width;
+						for (; px != end; ++px)
+							*px = ((*px << 8) & 0xff00) | ((*px >> 8) & 0x00ff);
+						break;
+					}
+					case QImage::Format_RGB32:
+					case QImage::Format_ARGB32_Premultiplied:
+					{
+						auto px = reinterpret_cast<ushort*> (image.scanLine (i));
+						const auto end = px + width;
+						for (; px != end; ++px)
+							*px = ((*px << 24) & 0xff000000) | ((*px << 8) & 0x00ff0000) |
+									((*px >> 8) & 0x0000ff00) | ((*px >> 24) & 0x000000ff);
+						break;
+					}
+					default:
+						return {};
+					}
+			}
+
+			if (format == QImage::Format_RGB32)
+			{
+				auto bits = reinterpret_cast<QRgb*> (image.bits ());
+				for (int y = 0; y < height; ++y)
+				{
+					for (int x = 0; x < width; ++x)
+						bits [x] |= 0xff000000;
+					bits += bpl / 4;
+				}
+			}
+
+			return image.copy ();
+		}
+
+		QImage GrabWindow (ulong wid)
+		{
+			const auto conn = QX11Info::connection ();
+
+			const auto sizeCookie = xcb_get_geometry (conn, wid);
+			const std::shared_ptr<xcb_get_geometry_reply_t> sizeReply
+				{ xcb_get_geometry_reply (conn, sizeCookie, nullptr), &free };
+
+			const auto attrsCookie = xcb_get_window_attributes (conn, wid);
+			const std::shared_ptr<xcb_get_window_attributes_reply_t> attrsReply
+				{ xcb_get_window_attributes_reply (conn, attrsCookie, nullptr), &free };
+
+			xcb_composite_redirect_window (conn, wid, XCB_COMPOSITE_REDIRECT_AUTOMATIC);
+
+			const auto backing = xcb_generate_id (conn);
+			xcb_composite_name_window_pixmap (conn, wid, backing);
+
+			const auto formatsCookie = xcb_render_query_pict_formats_unchecked (conn);
+			const std::shared_ptr<xcb_render_query_pict_formats_reply_t> formats
+			{
+				xcb_render_query_pict_formats_reply (conn, formatsCookie, nullptr),
+				&free
+			};
+
+			const auto format = xcb_render_util_find_visual_format (formats.get (), attrsReply->visual);
+			if (!format)
+				return {};
+
+			const auto picture = xcb_generate_id (conn);
+			const uint32_t params [] = { XCB_SUBWINDOW_MODE_INCLUDE_INFERIORS };
+			xcb_render_create_picture (conn,
+					picture,
+					backing,
+					format->format,
+					XCB_RENDER_CP_SUBWINDOW_MODE,
+					params);
+
+			const auto region = xcb_generate_id (conn);
+			xcb_xfixes_create_region_from_window (conn, region, wid, XCB_SHAPE_SK_BOUNDING);
+			xcb_xfixes_translate_region (conn, region, sizeReply->x, sizeReply->y);
+			xcb_xfixes_set_picture_clip_region (conn, picture, region, 0, 0);
+			xcb_xfixes_destroy_region (conn, region);
+
+			const auto xpixmap = xcb_generate_id (conn);
+			xcb_create_pixmap (conn, sizeReply->depth, xpixmap,
+					Util::XWrapper::Instance ().GetRootWindow (),
+					sizeReply->width, sizeReply->height);
+
+			xcb_render_composite (conn, XCB_RENDER_PICT_OP_OVER, picture, 0, xpixmap,
+					0, 0, 0, 0,
+					0, 0, sizeReply->width, sizeReply->height);
+
+			const auto& image = FromXcb (xpixmap,
+					sizeReply->width, sizeReply->height, sizeReply->depth, attrsReply->visual);
+
+			xcb_free_pixmap (conn, xpixmap);
+			xcb_render_free_picture (conn, picture);
+			xcb_free_pixmap (conn, backing);
+			xcb_composite_unredirect_window (conn, wid, XCB_COMPOSITE_REDIRECT_AUTOMATIC);
+
+			return {};
+		}
+/*
+		void CreatePicture (xcb_pixmap_t pixmap, int depth)
+		{
+			if (pixmap == XCB_PIXMAP_NONE)
+				return;
+
+			static QHash<int, xcb_render_pictformat_t> renderFormats;
+			if (renderFormats.contains (depth))
+				return;
+
+			const auto conn = QX11Info::connection ();
+			const auto cookie = xcb_render_query_pict_formats_unchecked (conn);
+			const std::shared_ptr<xcb_render_query_pict_formats_reply_t> formats
+			{
+				xcb_render_query_pict_formats_reply (conn, cookie, nullptr),
+				&free
+			};
+
+			if (!formats)
+				return;
+
+			for (auto it = xcb_render_query_pict_formats_formats_iterator (formats.get ());
+					it.rem; xcb_render_pictforminfo_next (&it))
+				if (it.data->depth == depth)
+				{
+					renderFormats [depth] = it.data->id;
+					break;
+				}
+		}
+
+		QImage GrabWindow (ulong wid)
+		{
+			const auto conn = QX11Info::connection ();
+
+			xcb_composite_redirect_window (conn, wid, XCB_COMPOSITE_REDIRECT_AUTOMATIC);
+
+			const auto sizeCookie = xcb_get_geometry (conn, wid);
+			const std::shared_ptr<xcb_get_geometry_reply_t> sizeReply
+				{ xcb_get_geometry_reply (conn, sizeCookie, nullptr), &free };
+
+			const auto xpixmap = xcb_generate_id (conn);
+			xcb_create_pixmap (conn, 32, xpixmap, wid, sizeReply->width, sizeReply->height);
+
+			CreatePicture (xpixmap, 32);
+
+			xcb_render_composite (conn, XCB_RENDER_PICT_OP_SRC, nullptr, XCB_RENDER_PICTURE_NONE,
+
+			xcb_composite_unredirect_window (conn, wid, XCB_COMPOSITE_REDIRECT_AUTOMATIC);
+
+			return {};
+		}
+		*/
+#endif
 	}
 
 	void PagerWindow::FillSubmodel (SingleDesktopModel *model,
