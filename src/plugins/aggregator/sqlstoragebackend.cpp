00abf57e12d2e8f57746f19a1ebc4e3f58d83e28
0xd34df00d <0xd34df00d@gmail.com>
2014-08-21 19:36:01 +0400

    [Aggregator] Fixed sqlite query for unread item count getter.

--- src/plugins/aggregator/sqlstoragebackend.cpp
+++ src/plugins/aggregator/sqlstoragebackend.cpp
@@ -221,20 +221,20 @@ namespace Aggregator
 		UnreadItemsCounter_ = QSqlQuery (DB_);
 		switch (Type_)
 		{
-			case SBSQLite:
-				UnreadItemsCounter_.prepare ("SELECT COUNT (unread) "
-						"FROM items "
-						"WHERE channel_id = :channel_id "
-						"AND unread = \"true\"");
-				break;
-			case SBPostgres:
-				UnreadItemsCounter_.prepare ("SELECT COUNT (1) "
-						"FROM items "
-						"WHERE channel_id = :channel_id "
-						"AND unread");
-				break;
-			case SBMysql:
-				break;
+		case SBSQLite:
+			UnreadItemsCounter_.prepare ("SELECT COUNT (unread) "
+					"FROM items "
+					"WHERE channel_id = :channel_id "
+					"AND unread = 1");
+			break;
+		case SBPostgres:
+			UnreadItemsCounter_.prepare ("SELECT COUNT (1) "
+					"FROM items "
+					"WHERE channel_id = :channel_id "
+					"AND unread");
+			break;
+		case SBMysql:
+			break;
 		}
 
 		ItemsShortSelector_ = QSqlQuery (DB_);
