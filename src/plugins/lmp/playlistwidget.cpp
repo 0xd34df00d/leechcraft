d8e33fb0ec24b816af08020de2a2a6335fc613c8
0xd34df00d <0xd34df00d@gmail.com>
2014-08-28 19:23:42 +0400

    [LMP] Compare new custom criteria to currently set criteria.
    Fixes #1702 again.

--- src/plugins/lmp/playlistwidget.cpp
+++ src/plugins/lmp/playlistwidget.cpp
@@ -792,27 +792,26 @@ namespace LMP
 	void PlaylistWidget::handleCustomSort ()
 	{
 		const auto& var = XmlSettingsManager::Instance ().property ("LastCustomSortCriteria");
-		auto current = LoadCriteria (var);
+		auto lastCustom = LoadCriteria (var);
 
-		if (current.isEmpty ())
-			current = Player_->GetSortingCriteria ();
+		const auto& current = Player_->GetSortingCriteria ();
+		if (lastCustom.isEmpty ())
+			lastCustom = current;
 
 		SortingCriteriaDialog dia (this);
-		dia.SetCriteria (current);
+		dia.SetCriteria (lastCustom);
 		if (dia.exec () != QDialog::Accepted)
 			return;
 
 		const auto& newCriteria = dia.GetCriteria ();
-		if (newCriteria == current)
-			return;
-
 		if (!newCriteria.isEmpty ())
 		{
 			const auto& var = SaveCriteria (newCriteria);
 			XmlSettingsManager::Instance ().setProperty ("LastCustomSortCriteria", var);
 		}
 
-		Player_->SetSortingCriteria (newCriteria);
+		if (newCriteria != current)
+			Player_->SetSortingCriteria (newCriteria);
 	}
 
 	void PlaylistWidget::removeSelectedSongs ()
