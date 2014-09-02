a49f753b9076fe6988fb3f3a1e457aa7f0c01216
0xd34df00d <0xd34df00d@gmail.com>
2014-09-02 06:00:33 +0400

    [Azoth MuC] QTest::toString() for Boost and STL types.

--- src/plugins/azoth/plugins/mucommands/tests/commandstest.cpp
+++ src/plugins/azoth/plugins/mucommands/tests/commandstest.cpp
@@ -34,6 +34,58 @@
 
 QTEST_MAIN (LeechCraft::Azoth::MuCommands::CommandsTest)
 
+namespace
+{
+	struct PrintVisitor : boost::static_visitor<QString>
+	{
+		QString operator() (const std::string& str) const
+		{
+			return QString::fromStdString (str);
+		}
+
+		template<typename T>
+		QString operator() (const T& t) const
+		{
+			QString result;
+			QDebug { &result } << t;
+			return result;
+		}
+
+		template<typename T1, typename T2>
+		QString operator() (const std::pair<T1, T2>& t) const
+		{
+			QString result;
+			QDebug { &result } << "std::pair { " << (*this) (t.first) << "; " << (*this) (t.second) << " }";
+			return result;
+		}
+
+		template<typename... Args>
+		QString operator() (const boost::variant<Args...>& variant) const
+		{
+			auto result = QString { "Variant with type %1, value: { %2 }" }
+					.arg (variant.which ())
+					.arg (boost::apply_visitor (PrintVisitor {}, variant));
+			return result;
+		}
+	};
+
+	template<typename... Args>
+	char* PrintVar (const boost::variant<Args...>& variant)
+	{
+		const auto& result = PrintVisitor {} (variant);
+		return qstrdup (result.toUtf8 ().constData ());
+	}
+}
+
+namespace QTest
+{
+	template<>
+	char* toString (const LeechCraft::Azoth::MuCommands::Status_t& acc)
+	{
+		return PrintVar (acc);
+	}
+}
+
 namespace LeechCraft
 {
 namespace Azoth
