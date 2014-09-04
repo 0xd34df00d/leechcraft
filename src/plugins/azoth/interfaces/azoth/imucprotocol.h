daf1479e88bf9c6b42db21ffb0abbb603ec5b458
0xd34df00d <0xd34df00d@gmail.com>
2014-09-05 03:00:17 +0400

    [Azoth] Added IMUCProtocol::TryGuessMUCIdentifyingData() for #1662.

--- src/plugins/azoth/interfaces/azoth/imucprotocol.h
+++ src/plugins/azoth/interfaces/azoth/imucprotocol.h
@@ -29,6 +29,10 @@
 
 #pragma once
 
+#include <QVariantMap>
+
+class QWidget;
+
 namespace LeechCraft
 {
 namespace Azoth
@@ -60,6 +64,38 @@ namespace Azoth
 		 * @sa IMUCJoinWidget
 		 */
 		virtual QWidget* GetMUCJoinWidget () = 0;
+
+		/** @brief Tries to guess MUC identifying data from user input.
+		 *
+		 * This method is used by Azoth core and other plugins to try to
+		 * guess MUC identifying data from the given user \em input.
+		 * The \em input, for example, may be a string like
+		 * <em>c_plus_plus@conference.jabber.ru</em> in case of a XMPP
+		 * MUC.
+		 *
+		 * The method should return a variant map suitable to passing
+		 * to IMUCJoinWidget::SetIdentifyingData().
+		 *
+		 * The \em entryObj can be used to get additional information
+		 * about the context of the user input. For example, if only room
+		 * name is given, it can be used to get the corresponding server
+		 * address.
+		 *
+		 * @param[in] input The user input, like a human-readable MUC
+		 * name.
+		 * @param[in] accountObj The entry this input is relevant to.
+		 * @return The MUC identifying data suitable for passing to
+		 * IMUCJoinWidget::SetIdentifyingData(), or an empty variantmap
+		 * if guessing failed.
+		 *
+		 * @sa IMUCJoinWidget::SetIdentifyingData()
+		 */
+		virtual QVariantMap TryGuessMUCIdentifyingData (const QString& input, QObject *entryObj)
+		{
+			Q_UNUSED (input);
+			Q_UNUSED (entryObj);
+			return {};
+		}
 	};
 }
 }
