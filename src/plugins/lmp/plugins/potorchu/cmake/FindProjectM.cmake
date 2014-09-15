57914aa20357afeae1510da3a6e73bfb4291acbb
0xd34df00d <0xd34df00d@gmail.com>
2014-09-16 01:13:06 +0400

    [LMP Potorchu] Added FindProjectM.cmake for #1699.

---
+++ src/plugins/lmp/plugins/potorchu/cmake/FindProjectM.cmake
@@ -0,0 +1,21 @@
+find_package (PkgConfig)
+pkg_check_modules (PC_PROJECTM QUIET libprojectM)
+
+# Include dir
+find_path (ProjectM_INCLUDE_DIR
+	NAMES libprojectM/projectM.hpp
+	PATHS ${PC_PROJECTM_INCLUDE_DIRS}
+	)
+
+# Finally the library itself
+find_library(ProjectM_LIBRARIES
+	NAMES projectM
+	PATHS ${PC_PROJECTM_LIBRARY_DIRS}
+	)
+
+if (ProjectM_INCLUDE_DIR AND ProjectM_LIBRARIES)
+	message (STATUS "Found ProjectM includes at: ${ProjectM_INCLUDE_DIR}")
+	message (STATUS "Found ProjectM libraries at: ${ProjectM_LIBRARIES}")
+else ()
+	message (FATAL_ERROR "Could NOT find ProjectM")
+endif ()
