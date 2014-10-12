309a215fabcfb093584a252eaff907a2935f6fe2
0xd34df00d <0xd34df00d@gmail.com>
2014-10-12 18:54:53 +0400

    [Azoth Murm] Updated translations.

--- src/plugins/azoth/plugins/murm/leechcraft_azoth_murm_ru_RU.ts
+++ src/plugins/azoth/plugins/murm/leechcraft_azoth_murm_ru_RU.ts
@@ -45,27 +45,42 @@
 <context>
     <name>LeechCraft::Azoth::Murm::EntryBase</name>
     <message>
-        <location filename="entrybase.cpp" line="147"/>
+        <location filename="entrybase.cpp" line="162"/>
         <source>Enqueue</source>
         <translation>Добавить в очередь</translation>
     </message>
     <message>
-        <location filename="entrybase.cpp" line="151"/>
+        <location filename="entrybase.cpp" line="166"/>
         <source>Play</source>
         <translation>Проиграть</translation>
     </message>
     <message>
-        <location filename="entrybase.cpp" line="155"/>
+        <location filename="entrybase.cpp" line="170"/>
         <source>Download</source>
         <translation>Скачать</translation>
     </message>
     <message>
-        <location filename="entrybase.cpp" line="307"/>
+        <location filename="entrybase.cpp" line="205"/>
+        <source>%1 document, size: %2</source>
+        <translation>Документ %1, размер: %2</translation>
+    </message>
+    <message>
+        <location filename="entrybase.cpp" line="216"/>
+        <source>Page:</source>
+        <translation>Страница:</translation>
+    </message>
+    <message>
+        <location filename="entrybase.cpp" line="325"/>
+        <source>Forwarded message from %1</source>
+        <translation>Пересланное сообщение от %1</translation>
+    </message>
+    <message>
+        <location filename="entrybase.cpp" line="374"/>
         <source>Posted on: %1</source>
         <translation>Опубликовано: %1</translation>
     </message>
     <message numerus="yes">
-        <location filename="entrybase.cpp" line="310"/>
+        <location filename="entrybase.cpp" line="377"/>
         <source>%n like(s)</source>
         <translation>
             <numerusform>%n пользователю это нравится</numerusform>
@@ -74,13 +89,41 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="entrybase.cpp" line="312"/>
+        <location filename="entrybase.cpp" line="379"/>
         <source>%n repost(s)</source>
         <translation>
             <numerusform>%n пользователь опубликовал это у себя</numerusform>
             <numerusform>%n пользователя опубликовали это у себя</numerusform>
             <numerusform>%n пользователей опубликовали это у себя</numerusform>
         </translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Azoth::Murm::PendingUpload</name>
+    <message>
+        <location filename="pendingupload.cpp" line="131"/>
+        <source>Unable to get upload server from VKontakte, protocol error %1: %2.</source>
+        <translation>Невозможно получить сервер для загрузки В Контакте, ошибка протокола %1: %2.</translation>
+    </message>
+    <message>
+        <location filename="pendingupload.cpp" line="144"/>
+        <source>Unable to get upload server from VKontakte</source>
+        <translation>Невозможно получить сервер загрузки В Контакте</translation>
+    </message>
+    <message>
+        <location filename="pendingupload.cpp" line="196"/>
+        <source>Unable to save document, error %1: %2.</source>
+        <translation>Невозможно сохранить документ, ошибка %1: %2.</translation>
+    </message>
+    <message>
+        <location filename="pendingupload.cpp" line="228"/>
+        <source>Unable to upload document, network error %1.</source>
+        <translation>Невозможно загрузить документ, ошибка сети %1.</translation>
+    </message>
+    <message>
+        <location filename="pendingupload.cpp" line="242"/>
+        <source>Unable to upload document, server error: %1.</source>
+        <translation>Невозможно загрузить документ, ошибка сервера: %1.</translation>
     </message>
 </context>
 <context>
@@ -107,12 +150,12 @@
 <context>
     <name>LeechCraft::Azoth::Murm::VkAccount</name>
     <message>
-        <location filename="vkaccount.cpp" line="284"/>
+        <location filename="vkaccount.cpp" line="302"/>
         <source>me</source>
         <translation>я</translation>
     </message>
     <message>
-        <location filename="vkaccount.cpp" line="435"/>
+        <location filename="vkaccount.cpp" line="444"/>
         <source>%1 is invalid VKontake ID</source>
         <translation>%1 — неправильный ID В Контакте</translation>
     </message>
@@ -120,12 +163,12 @@
 <context>
     <name>LeechCraft::Azoth::Murm::VkChatEntry</name>
     <message>
-        <location filename="vkchatentry.cpp" line="171"/>
+        <location filename="vkchatentry.cpp" line="170"/>
         <source>Chats</source>
         <translation>Чаты</translation>
     </message>
     <message>
-        <location filename="vkchatentry.cpp" line="275"/>
+        <location filename="vkchatentry.cpp" line="274"/>
         <source>Participants of %1</source>
         <translation>Участники %1</translation>
     </message>
@@ -143,62 +186,62 @@
         <translation>%1 покинул(а) сайт</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="412"/>
+        <location filename="vkentry.cpp" line="411"/>
         <source>Self contact</source>
         <translation>Я</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="415"/>
+        <location filename="vkentry.cpp" line="414"/>
         <source>Non-friends</source>
         <translation>Недрузья</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="536"/>
+        <location filename="vkentry.cpp" line="535"/>
         <source>First name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="537"/>
+        <location filename="vkentry.cpp" line="536"/>
         <source>Last name</source>
         <translation>Фамилия</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="538"/>
+        <location filename="vkentry.cpp" line="537"/>
         <source>Nick</source>
         <translation>Ник</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="539"/>
+        <location filename="vkentry.cpp" line="538"/>
         <source>Photo</source>
         <translation>Фото</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="540"/>
+        <location filename="vkentry.cpp" line="539"/>
         <source>Birthday</source>
         <translation>День рожденья</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="541"/>
+        <location filename="vkentry.cpp" line="540"/>
         <source>Home phone</source>
         <translation>Домашний телефон</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="542"/>
+        <location filename="vkentry.cpp" line="541"/>
         <source>Mobile phone</source>
         <translation>Мобильный телефон</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="543"/>
+        <location filename="vkentry.cpp" line="542"/>
         <source>Timezone</source>
         <translation>Временная зона</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="544"/>
+        <location filename="vkentry.cpp" line="543"/>
         <source>City</source>
         <translation>Город</translation>
     </message>
     <message>
-        <location filename="vkentry.cpp" line="545"/>
+        <location filename="vkentry.cpp" line="544"/>
         <source>Country</source>
         <translation>Страна</translation>
     </message>
