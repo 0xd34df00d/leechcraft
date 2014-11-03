c9692122f315f3d86627cd99c91d992eb9ffd291
0xd34df00d <0xd34df00d@gmail.com>
2014-11-03 23:34:23 +0300

    [Azoth] Documented ISupportMediaCalls.

--- src/plugins/azoth/interfaces/azoth/isupportmediacalls.h
+++ src/plugins/azoth/interfaces/azoth/isupportmediacalls.h
@@ -27,38 +27,91 @@
  * DEALINGS IN THE SOFTWARE.
  **********************************************************************/
 
-#ifndef PLUGINS_AZOTH_INTERFACES_ISUPPORTMEDIACALLS_H
-#define PLUGINS_AZOTH_INTERFACES_ISUPPORTMEDIACALLS_H
+#pragma once
+
 #include <QtPlugin>
 
 namespace LeechCraft
 {
 namespace Azoth
 {
+	/** @brief Interface for accounts supporting audio/video calls.
+	 *
+	 * This interface should be implemented by IAccount instances wishing
+	 * to advertise the support for audio or video calls to the rest of
+	 * Azoth.
+	 *
+	 * @sa IAccount
+	 * @sa IMediaCall
+	 */
 	class ISupportMediaCalls
 	{
 	public:
 		virtual ~ISupportMediaCalls () {}
 
+		/** @brief Describes supported media call features.
+		 */
 		enum MediaCallFeature
 		{
+			/** @brief No particular features.
+			 */
 			MCFNoFeatures,
+
+			/** @brief The account supports audio calls.
+			 */
 			MCFSupportsAudioCalls = 0x01,
+
+			/** @brief The accounts supports video calls.
+			 */
 			MCFSupportsVideoCalls = 0x02
 		};
 
 		Q_DECLARE_FLAGS (MediaCallFeatures, MediaCallFeature)
 
+		/** @brief Returns the media features supported by this account.
+		 *
+		 * @return The account media call features.
+		 */
 		virtual MediaCallFeatures GetMediaCallFeatures () const = 0;
 
+		/** @brief Tries to call a contact list entry.
+		 *
+		 * The entry is identified by its \em id, which is the ID returned
+		 * by ICLEntry::GetEntryID().
+		 *
+		 * If the corresponding protocol supports multiple variants per
+		 * entry, the \em variant parameter specifies which variant (from
+		 * the ones returned from ICLEntry::Variants()) of the entry
+		 * should be called.
+		 *
+		 * Returns either a IMediaCall object or a nullptr if the call
+		 * initialization failed.
+		 *
+		 * @note If this method returns a valid IMediaCall object, it
+		 * should also be advertised via the called() signal.
+		 *
+		 * @param[in] id The ID of the entry to call (as in ICLEntry::GetEntryID()).
+		 * @param[in] variant The variant of the entry to call to.
+		 * @return The call object implementing IMediaCall, or a nullptr
+		 * if it is impossible to make the call.
+		 */
 		virtual QObject* Call (const QString& id, const QString& variant) = 0;
-
-		virtual void called (QObject*) = 0;
+	protected:
+		/** @brief Emitted when a new call is established.
+		 *
+		 * This signal should be emitted whenever a new call is
+		 * established, either an incoming call or an outgoing call
+		 * initiated via the Call() method.
+		 *
+		 * @note This function is expected to be a signal.
+		 *
+		 * @param[out] callObj The object representing the call and
+		 * implementing IMediaCall.
+		 */
+		virtual void called (QObject *callObj) = 0;
 	};
 }
 }
 
 Q_DECLARE_INTERFACE (LeechCraft::Azoth::ISupportMediaCalls,
 		"org.Deviant.LeechCraft.Azoth.ISupportMediaCalls/1.0");
-
-#endif
