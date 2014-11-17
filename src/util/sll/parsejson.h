6b990ff2e49a6b545eb52d9c7c20bc25a5770851
0xd34df00d <0xd34df00d@gmail.com>
2014-11-17 23:47:55 +0000

    [Util] Documented ParseJson.

--- src/util/sll/parsejson.h
+++ src/util/sll/parsejson.h
@@ -44,6 +44,16 @@ namespace LeechCraft
 {
 namespace Util
 {
+	/** @brief Parses JSON content in the given bytearray.
+	 *
+	 * This functions uses QJson on Qt4 (don't forget to link to it!) and
+	 * Qt's native JSON parsers on Qt5.
+	 *
+	 * @param[in] bytes The byte array to parse JSON from.
+	 * @param[in] context The context string to be used in logging
+	 * messages.
+	 * @return The recursive QVariant with JSON contents.
+	 */
 	inline QVariant ParseJson (const QByteArray& bytes, const char *context)
 	{
 #if QT_VERSION < 0x050000
@@ -73,6 +83,17 @@ namespace Util
 		return result;
 	}
 
+	/** @brief Utility function parsing JSON from the \em device.
+	 *
+	 * This function reads all available data from the \em device and
+	 * passes it to the other ParseJson() overload.
+	 *
+	 * @param[in] device The device from which JSON-encoded data should
+	 * be read.
+	 * @param[in] context The context string to be used in logging
+	 * messages.
+	 * @return The recursive QVariant with JSON contents.
+	 */
 	inline QVariant ParseJson (QIODevice *device, const char *context)
 	{
 		return ParseJson (device->readAll (), context);
