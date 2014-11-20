097c4eabf2fbac8e4da2f31c6fd9d9e863cbbf61
0xd34df00d <0xd34df00d@gmail.com>
2014-11-20 04:36:25 +0300

    [Zalil] Added translations.

---
+++ src/plugins/zalil/leechcraft_zalil_en.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LeechCraft::Zalil</name>
+    <message>
+        <location filename="pendinguploadbase.cpp" line="51"/>
+        <source>Uploading %1</source>
+        <translation>Uploading %1</translation>
+    </message>
+    <message>
+        <location filename="pendinguploadbase.cpp" line="53"/>
+        <source>Uploading...</source>
+        <translation>Uploading...</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Zalil::PendingUploadBase</name>
+    <message>
+        <location filename="pendinguploadbase.cpp" line="103"/>
+        <source>%1 of %2</source>
+        <translation>%1 of %2</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Zalil::Plugin</name>
+    <message>
+        <location filename="zalil.cpp" line="72"/>
+        <source>Uploads files to filebin services without registration, like dump.bitcheese.net.</source>
+        <translation>Uploads files to filebin services without registration, like dump.bitcheese.net.</translation>
+    </message>
+</context>
+</TS>
--- src/plugins/zalil/zalil.cpp
+++ src/plugins/zalil/zalil.cpp
@@ -30,6 +30,7 @@
 #include "zalil.h"
 #include <QIcon>
 #include <QStandardItemModel>
+#include <util/util.h>
 #include "servicesmanager.h"
 #include "pendinguploadbase.h"
 
@@ -39,6 +40,8 @@ namespace Zalil
 {
 	void Plugin::Init (ICoreProxy_ptr proxy)
 	{
+		Util::InstallTranslator ("zalil");
+
 		Manager_ = std::make_shared<ServicesManager> (proxy);
 		connect (Manager_.get (),
 				SIGNAL (fileUploaded (QString, QUrl)),
