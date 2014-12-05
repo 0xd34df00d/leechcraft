f84c84d9797bf0df85a9d829e81ebc462ff56310
0xd34df00d <0xd34df00d@gmail.com>
2014-12-06 00:45:16 +0300

    [ER] Updated translations.

--- src/plugins/eleeminator/leechcraft_eleeminator_en.ts
+++ src/plugins/eleeminator/leechcraft_eleeminator_en.ts
@@ -17,7 +17,7 @@
 <context>
     <name>LeechCraft::Eleeminator</name>
     <message>
-        <location filename="termtab.cpp" line="65"/>
+        <location filename="termtab.cpp" line="67"/>
         <source>Terminal toolbar</source>
         <translation>Terminal toolbar</translation>
     </message>
@@ -31,26 +31,31 @@
     </message>
     <message>
         <location filename="eleeminator.cpp" line="59"/>
+        <source>Clear terminal window</source>
+        <translation>Clear terminal window</translation>
+    </message>
+    <message>
+        <location filename="eleeminator.cpp" line="65"/>
         <source>Copy selected text to clipboard</source>
         <translation>Copy selected text to clipboard</translation>
     </message>
     <message>
-        <location filename="eleeminator.cpp" line="65"/>
+        <location filename="eleeminator.cpp" line="71"/>
         <source>Paste text from clipboard</source>
         <translation>Paste text from clipboard</translation>
     </message>
     <message>
-        <location filename="eleeminator.cpp" line="75"/>
+        <location filename="eleeminator.cpp" line="81"/>
         <source>Terminal</source>
         <translation>Terminal</translation>
     </message>
     <message>
-        <location filename="eleeminator.cpp" line="76"/>
+        <location filename="eleeminator.cpp" line="82"/>
         <source>Termianl emulator.</source>
         <translation>Termianl emulator.</translation>
     </message>
     <message>
-        <location filename="eleeminator.cpp" line="106"/>
+        <location filename="eleeminator.cpp" line="112"/>
         <source>Embedded LeechCraft terminal emulator.</source>
         <translation>Embedded LeechCraft terminal emulator.</translation>
     </message>
@@ -58,42 +63,57 @@
 <context>
     <name>LeechCraft::Eleeminator::TermTab</name>
     <message>
-        <location filename="termtab.cpp" line="175"/>
+        <location filename="termtab.cpp" line="181"/>
+        <source>Clear window</source>
+        <translation>Clear window</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="192"/>
         <source>Color scheme</source>
         <translation>Color scheme</translation>
     </message>
     <message>
-        <location filename="termtab.cpp" line="219"/>
+        <location filename="termtab.cpp" line="236"/>
         <source>Select font...</source>
         <translation>Select font...</translation>
     </message>
     <message>
-        <location filename="termtab.cpp" line="264"/>
+        <location filename="termtab.cpp" line="281"/>
         <source>Open URL</source>
         <translation>Open URL</translation>
     </message>
     <message>
-        <location filename="termtab.cpp" line="269"/>
+        <location filename="termtab.cpp" line="286"/>
         <source>Copy URL</source>
         <translation>Copy URL</translation>
     </message>
     <message>
-        <location filename="termtab.cpp" line="282"/>
+        <location filename="termtab.cpp" line="308"/>
         <source>Copy selected text</source>
         <translation>Copy selected text</translation>
     </message>
     <message>
-        <location filename="termtab.cpp" line="288"/>
+        <location filename="termtab.cpp" line="314"/>
         <source>Paste from clipboard</source>
         <translation>Paste from clipboard</translation>
     </message>
     <message>
-        <location filename="termtab.cpp" line="423"/>
+        <location filename="termtab.cpp" line="327"/>
+        <source>Open file</source>
+        <translation>Open file</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="334"/>
+        <source>Open file externally</source>
+        <translation>Open file externally</translation>
+    </message>
+    <message>
+        <location filename="termtab.cpp" line="491"/>
         <source>Bell in terminal.</source>
         <translation>Bell in terminal.</translation>
     </message>
     <message>
-        <location filename="termtab.cpp" line="426"/>
+        <location filename="termtab.cpp" line="494"/>
         <source>Bell</source>
         <translation>Bell</translation>
     </message>
@@ -147,6 +167,21 @@
         <location filename="dummy.cpp" line="7"/>
         <source>Shift</source>
         <translation>Shift</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Finite scrollback history</source>
+        <translation>Finite scrollback history</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>History size:</source>
+        <translation>History size:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source> lines</source>
+        <translation> lines</translation>
     </message>
 </context>
 </TS>
