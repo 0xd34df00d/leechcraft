36e8d49d3f321de640efba888a3b980d09879b0b
0xd34df00d <0xd34df00d@gmail.com>
2014-12-20 16:48:54 +0300

    [Azoth Rosenthal] Updated translations.

--- src/plugins/azoth/plugins/rosenthal/leechcraft_azoth_rosenthal_ru_RU.ts
+++ src/plugins/azoth/plugins/rosenthal/leechcraft_azoth_rosenthal_ru_RU.ts
@@ -2,53 +2,11 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
-    <name>LeechCraft::Azoth::Rosenthal::KnownDictsManager</name>
-    <message>
-        <location filename="knowndictsmanager.cpp" line="153"/>
-        <source>Locale</source>
-        <translation>Локаль</translation>
-    </message>
-    <message>
-        <location filename="knowndictsmanager.cpp" line="153"/>
-        <source>Language</source>
-        <translation>Язык</translation>
-    </message>
-    <message>
-        <location filename="knowndictsmanager.cpp" line="153"/>
-        <source>Country</source>
-        <translation>Страна</translation>
-    </message>
-</context>
-<context>
     <name>LeechCraft::Azoth::Rosenthal::Plugin</name>
     <message>
-        <location filename="rosenthal.cpp" line="96"/>
+        <location filename="rosenthal.cpp" line="83"/>
         <source>Provides spellchecking for Azoth.</source>
         <translation>Обеспечивает проверку правописания в Azoth.</translation>
-    </message>
-</context>
-<context>
-    <name>azothrosenthalsettings</name>
-    <message>
-        <location filename="dummy.cpp" line="2"/>
-        <source>Azoth Rosenthal</source>
-        <translation>Azoth Rosenthal</translation>
-    </message>
-    <message>
-        <location filename="dummy.cpp" line="3"/>
-        <source>Languages</source>
-        <oldsource>Languages to use</oldsource>
-        <translation>Языки</translation>
-    </message>
-    <message>
-        <location filename="dummy.cpp" line="4"/>
-        <source>Primary language:</source>
-        <translation>Основной язык:</translation>
-    </message>
-    <message>
-        <location filename="dummy.cpp" line="5"/>
-        <source>Download and install dictionaries...</source>
-        <translation>Скачать и установить словари…</translation>
     </message>
 </context>
 </TS>
