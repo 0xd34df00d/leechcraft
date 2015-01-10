11c5ebeb48a45cb8c4abb20dc3448d2093a0cd19
0xd34df00d <0xd34df00d@gmail.com>
2015-01-10 20:09:36 +0000

    [Monocle] Documented IKnowFileExtensions.

--- src/plugins/monocle/interfaces/monocle/iknowfileextensions.h
+++ src/plugins/monocle/interfaces/monocle/iknowfileextensions.h
@@ -36,17 +36,48 @@ namespace LeechCraft
 {
 namespace Monocle
 {
+	/** @brief Interface for backend plugins providing default extensions
+	 * for their relevant file types.
+	 *
+	 * This interface is used by Monocle core to know which file types
+	 * with which extensions can be handled. It may then, for example,
+	 * provide separate filters for each of the document types in a file
+	 * open dialog.
+	 *
+	 * For example, a PDF backend plugin may know that PDF documents
+	 * typically has \em .pdf extension.
+	 */
 	class IKnowFileExtensions
 	{
 	protected:
 		virtual ~IKnowFileExtensions () {}
 	public:
+		/** @brief Describes a single typical file type.
+		 *
+		 * A single file type may have multiple typical extensions in the
+		 * wild, thus it contains a list of extensions in the Extensions_
+		 * field.
+		 */
 		struct ExtInfo
 		{
+			/** @brief Human-readable description.
+			 *
+			 * For example, "DjVu files".
+			 */
 			QString Description_;
+
+			/** @brief Typically used extensions for the file type.
+			 *
+			 * For example, for DjVu format this will have \em djvu and
+			 * \em djvu extensions.
+			 */
 			QList<QString> Extensions_;
 		};
 
+		/** @brief Returns the list of supported typical file extensions.
+		 *
+		 * @return The list of ExtInfo objects.
+		 */
 		virtual QList<ExtInfo> GetKnownFileExtensions () const = 0;
 	};
 }
