fde75b7bbd6f9e0ac30ec3ea980b3775bd349d11
0xd34df00d <0xd34df00d@gmail.com>
2015-01-25 22:27:19 +0300

    [Azoth] Documented {Is,Set}ShownInRoster().

--- src/plugins/azoth/interfaces/azoth/iaccount.h
+++ src/plugins/azoth/interfaces/azoth/iaccount.h
@@ -267,11 +267,40 @@ namespace Azoth
 		 */
 		virtual QObject* GetTransferManager () const = 0;
 
+		/** @brief Whether the account should be shown in the roster.
+		 *
+		 * The default implementation simply returns the value of a the
+		 * return variable set by SetShownInRoster().
+		 *
+		 * By default, accounts should be shown in the roster.
+		 *
+		 * @return Whether the account should be shown in roster.
+		 *
+		 * @sa SetShownInRoster()
+		 */
 		virtual bool IsShownInRoster () const
 		{
 			return IsShown_;
 		}
 
+		/** @brief Sets whether the account should be shown in the roster.
+		 *
+		 * The default implementation simply sets the corresponding
+		 * private variable that is returned from IsShownInRoster().
+		 *
+		 * Calling this function does not have to result in any kind of
+		 * account saving, signal emission and so on. This function (and
+		 * its IsShownInRoster() counterpart) are provided solely for
+		 * Azoth core convenience.
+		 *
+		 * After calling this function the IsShownInRoster() method
+		 * should return the last value of \em shown.
+		 *
+		 * @param[in] shown Whether the account should be shown in the
+		 * roster.
+		 *
+		 * @sa IsShownInRoster()
+		 */
 		virtual void SetShownInRoster (bool shown)
 		{
 			IsShown_ = shown;
