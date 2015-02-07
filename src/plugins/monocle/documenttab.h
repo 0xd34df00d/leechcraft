54926197746283407019969b943fda749b357507
0xd34df00d <0xd34df00d@gmail.com>
2015-02-08 00:27:24 +0300

    [Monocle] Override markers.

--- src/plugins/monocle/documenttab.h
+++ src/plugins/monocle/documenttab.h
@@ -149,7 +149,7 @@ namespace Monocle
 		QPoint GetCurrentCenter () const;
 		void CenterOn (const QPoint&);
 
-		bool eventFilter (QObject*, QEvent*);
+		bool eventFilter (QObject*, QEvent*) override;
 	protected:
 		void dragEnterEvent (QDragEnterEvent*) override;
 		void dropEvent (QDropEvent*) override;
