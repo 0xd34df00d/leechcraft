06199b8161bddebc42e62703cdbf15da42892291
0xd34df00d <0xd34df00d@gmail.com>
2015-02-08 21:14:12 +0300

    [Poshuku Keywords] Updated translations.

--- src/plugins/poshuku/plugins/keywords/leechcraft_poshuku_keywords_en.ts
+++ src/plugins/poshuku/plugins/keywords/leechcraft_poshuku_keywords_en.ts
@@ -2,6 +2,50 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
+    <name>EditKeywordDialog</name>
+    <message>
+        <location filename="editkeyworddialog.ui" line="14"/>
+        <source>Edit keyword</source>
+        <translation>Edit keyword</translation>
+    </message>
+    <message>
+        <location filename="editkeyworddialog.ui" line="48"/>
+        <source>URL:</source>
+        <oldsource>Url</oldsource>
+        <translation>URL:</translation>
+    </message>
+    <message>
+        <location filename="editkeyworddialog.ui" line="55"/>
+        <source>Format specifier &lt;em&gt;%1&lt;/em&gt; can be used in the URL, for example keyword &lt;em&gt;lf&lt;/em&gt; with &lt;em&gt;http://www.last.fm/user/%1&lt;/em&gt; as the URL will expand to &lt;em&gt;http://www.last.fm/user/nobodyzzz&lt;/em&gt; on a request like &lt;em&gt;lf nobodyzzz&lt;/em&gt;.</source>
+        <oldsource>Format specifier can be used in url e.g. keyword lf with &apos;http://www.last.fm/user/%1&apos; in url will expand &apos;lf nobodyzzz&apos; to http://www.last.fm/user/nobodyzzz</oldsource>
+        <translation>Format specifier &lt;em&gt;%1&lt;/em&gt; can be used in the URL, for example keyword &lt;em&gt;lf&lt;/em&gt; with &lt;em&gt;http://www.last.fm/user/%1&lt;/em&gt; as the URL will expand to &lt;em&gt;http://www.last.fm/user/nobodyzzz&lt;/em&gt; on a request like &lt;em&gt;lf nobodyzzz&lt;/em&gt;.</translation>
+    </message>
+    <message>
+        <location filename="editkeyworddialog.ui" line="62"/>
+        <source>Keyword:</source>
+        <oldsource>Keyword</oldsource>
+        <translation>Keyword:</translation>
+    </message>
+</context>
+<context>
+    <name>KeywordsManagerWidget</name>
+    <message>
+        <location filename="keywordsmanagerwidget.ui" line="27"/>
+        <source>Add</source>
+        <translation>Add</translation>
+    </message>
+    <message>
+        <location filename="keywordsmanagerwidget.ui" line="34"/>
+        <source>Modify...</source>
+        <translation>Modify...</translation>
+    </message>
+    <message>
+        <location filename="keywordsmanagerwidget.ui" line="41"/>
+        <source>Remove</source>
+        <translation>Remove</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Poshuku::Keywords::KeywordsManagerWidget</name>
     <message>
         <location filename="keywordsmanagerwidget.cpp" line="69"/>
@@ -17,17 +61,17 @@
 <context>
     <name>LeechCraft::Poshuku::Keywords::Plugin</name>
     <message>
-        <location filename="keywords.cpp" line="51"/>
+        <location filename="keywords.cpp" line="52"/>
         <source>Keyword</source>
         <translation>Keyword</translation>
     </message>
     <message>
-        <location filename="keywords.cpp" line="52"/>
+        <location filename="keywords.cpp" line="53"/>
         <source>Url</source>
         <translation>Url</translation>
     </message>
     <message>
-        <location filename="keywords.cpp" line="96"/>
+        <location filename="keywords.cpp" line="97"/>
         <source>URL keywords support for the Poshuku browser.</source>
         <translation>URL keywords support for the Poshuku browser.</translation>
     </message>
