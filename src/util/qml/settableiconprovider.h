49721cc267fc2583859855ac3a572178b638dcd8
0xd34df00d <0xd34df00d@gmail.com>
2015-02-15 18:24:36 +0300

    [Util] documented SettableIconProvider.

--- src/util/qml/settableiconprovider.h
+++ src/util/qml/settableiconprovider.h
@@ -37,13 +37,54 @@ namespace LeechCraft
 {
 namespace Util
 {
+	/** @brief QML image provider with settable icons for each path.
+	 *
+	 * This class implements a QML image provider that provides preset
+	 * icons for given paths. The icons are set via SetIcon() and
+	 * ClearIcon().
+	 *
+	 * @sa WidthIconProvider
+	 */
 	class UTIL_QML_API SettableIconProvider : public WidthIconProvider
 	{
 		QHash<QStringList, QIcon> Icons_;
 	public:
-		void SetIcon (const QStringList& path, const QIcon&);
+		/** @brief Sets the \em icon for the given \em path.
+		 *
+		 * If there is already an icon for this \em path, it is replaced
+		 * by the new \em icon.
+		 *
+		 * The icon set with this function is available for this \em path
+		 * via the GetIcon() method.
+		 *
+		 * @param[in] path The path associated with the \em icon.
+		 * @param[in] icon The icon to associate with the \em path.
+		 *
+		 * @sa ClearIcon()
+		 * @sa GetIcon()
+		 */
+		void SetIcon (const QStringList& path, const QIcon& icon);
+
+		/** @brief Clears the icon associated with the given \em path.
+		 *
+		 * @param[in] path The path to clear.
+		 *
+		 * @sa SetIcon()
+		 * @sa GetIcon()
+		 */
 		void ClearIcon (const QStringList& path);
-		QIcon GetIcon (const QStringList& path);
+
+		/** @brief Returns the icon for the \em path previously set with
+		 * SetIcon().
+		 *
+		 * @param[in] path The path for which to return an the icon.
+		 * @return The icon for the \em path, or a null icon of no icon
+		 * is associated with it.
+		 *
+		 * @sa SetIcon()
+		 * @sa ClearIcon()
+		 */
+		QIcon GetIcon (const QStringList& path) override;
 	};
 }
 }
