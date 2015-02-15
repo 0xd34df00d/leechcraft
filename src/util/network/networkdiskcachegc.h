a5127c015f9308a300f191734b742d8bac1a721a
0xd34df00d <0xd34df00d@gmail.com>
2015-02-15 18:05:30 +0300

    [Util] Documented NetworkDiskCacheGC.

--- src/util/network/networkdiskcachegc.h
+++ src/util/network/networkdiskcachegc.h
@@ -42,6 +42,12 @@ namespace LeechCraft
 {
 namespace Util
 {
+	/** @brief Garbage collection for a set of network disk caches.
+	 *
+	 * This GC manager class aids having multiple network disk caches at
+	 * the same path and running garbage collection periodically on them,
+	 * but only once per each path.
+	 */
 	class NetworkDiskCacheGC : public QObject
 	{
 		Q_OBJECT
@@ -58,10 +64,44 @@ namespace Util
 		NetworkDiskCacheGC (const NetworkDiskCacheGC&) = delete;
 		NetworkDiskCacheGC& operator= (const NetworkDiskCacheGC&) = delete;
 
+		/** @brief Returns a single global instance of the GC manager.
+		 *
+		 * @return The single global instance of the GC manager.
+		 */
 		static NetworkDiskCacheGC& Instance ();
 
+		/** @brief Schedules calculation of the \em path total size.
+		 *
+		 * The calculation is performed asynchronously in a separate
+		 * thread, and a future object is returned which can be used to
+		 * be notified when the calculation finishes.
+		 *
+		 * @param[in] path The path which total size should be calculated
+		 * @return The future object for the asynchronous path size
+		 * calculation.
+		 */
 		QFuture<qint64> GetCurrentSize (const QString& path) const;
 
+		/** @brief Registers the given cache \em path.
+		 *
+		 * Registers the given \em path to be collected periodically and
+		 * returns a guard object that unregisters the path when it is
+		 * destroyed.
+		 *
+		 * The \em path stops being collected as soon as the last guard
+		 * object returned from this method is destroyed.
+		 *
+		 * This function also takes a size getter functor returning the
+		 * desired cache size for the given \em path. If the same path is
+		 * registered multiple times with size getters returning
+		 * different values, the minimum one is used.
+		 *
+		 * @param[in] path The path to register for garbage collection.
+		 * @param[in] sizeGetter The functor returning the desired total
+		 * size of files under the \em path.
+		 * @return A guard object unregistering the path when it is
+		 * destroyed.
+		 */
 		std::shared_ptr<void> RegisterDirectory (const QString& path,
 				const std::function<int ()>& sizeGetter);
 	private:
