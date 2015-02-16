36422583b621eb233881372848582ebb16410a11
0xd34df00d <0xd34df00d@gmail.com>
2015-02-17 00:18:17 +0300

    [Aggregator] Removed old MSVC workaround.

--- src/plugins/aggregator/interfaces/aggregator/item.h
+++ src/plugins/aggregator/interfaces/aggregator/item.h
@@ -37,13 +37,6 @@
 #include <QMetaType>
 #include "common.h"
 
-// Workaround stupid MSVS behaviour.
-#ifdef Q_CC_MSVC
-#define PROPER_NAMESPACE ::
-#else
-#define PROPER_NAMESPACE
-#endif
-
 namespace LeechCraft
 {
 namespace Aggregator
@@ -124,7 +117,7 @@ namespace Aggregator
 		Enclosure (const IDType_t& itemId, const IDType_t& encId);
 	private:
 		Enclosure ();
-		friend QDataStream& PROPER_NAMESPACE operator>> (QDataStream&, QList<Enclosure>&);
+		friend QDataStream& operator>> (QDataStream&, QList<Enclosure>&);
 	};
 
 	bool operator== (const Enclosure&, const Enclosure&);
@@ -142,7 +135,7 @@ namespace Aggregator
 		MRSSThumbnail (const IDType_t& entryId, const IDType_t& thisId);
 	private:
 		MRSSThumbnail ();
-		friend QDataStream& PROPER_NAMESPACE operator>> (QDataStream&, QList<MRSSThumbnail>&);
+		friend QDataStream& operator>> (QDataStream&, QList<MRSSThumbnail>&);
 	};
 
 	bool operator== (const MRSSThumbnail&, const MRSSThumbnail&);
@@ -158,7 +151,7 @@ namespace Aggregator
 		MRSSCredit (const IDType_t& entryId, const IDType_t& thisId);
 	private:
 		MRSSCredit ();
-		friend QDataStream& PROPER_NAMESPACE operator>> (QDataStream&, QList<MRSSCredit>&);
+		friend QDataStream& operator>> (QDataStream&, QList<MRSSCredit>&);
 	};
 
 	bool operator== (const MRSSCredit&, const MRSSCredit&);
@@ -174,7 +167,7 @@ namespace Aggregator
 		MRSSComment (const IDType_t& entryId, const IDType_t& thisId);
 	private:
 		MRSSComment ();
-		friend QDataStream& PROPER_NAMESPACE operator>> (QDataStream&, QList<MRSSComment>&);
+		friend QDataStream& operator>> (QDataStream&, QList<MRSSComment>&);
 	};
 
 	bool operator== (const MRSSComment&, const MRSSComment&);
@@ -190,7 +183,7 @@ namespace Aggregator
 		MRSSPeerLink (const IDType_t& entryId, const IDType_t& thisId);
 	private:
 		MRSSPeerLink ();
-		friend QDataStream& PROPER_NAMESPACE operator>> (QDataStream&, QList<MRSSPeerLink>&);
+		friend QDataStream& operator>> (QDataStream&, QList<MRSSPeerLink>&);
 	};
 
 	bool operator== (const MRSSPeerLink&, const MRSSPeerLink&);
@@ -208,7 +201,7 @@ namespace Aggregator
 		MRSSScene (const IDType_t& entryId, const IDType_t& thisId);
 	private:
 		MRSSScene ();
-		friend QDataStream& PROPER_NAMESPACE operator>> (QDataStream&, QList<MRSSScene>&);
+		friend QDataStream& operator>> (QDataStream&, QList<MRSSScene>&);
 	};
 
 	bool operator== (const MRSSScene&, const MRSSScene&);
@@ -256,7 +249,7 @@ namespace Aggregator
 		MRSSEntry (const IDType_t& itemId, const IDType_t& entryId);
 	private:
 		MRSSEntry ();
-		friend QDataStream& PROPER_NAMESPACE operator>> (QDataStream&, QList<MRSSEntry>&);
+		friend QDataStream& operator>> (QDataStream&, QList<MRSSEntry>&);
 	};
 
 	bool operator== (const MRSSEntry&, const MRSSEntry&);
@@ -402,9 +395,6 @@ namespace Aggregator
 	bool IsModified (Item_ptr, Item_ptr);
 }
 }
-
-// Not needed now:
-#undef PROPER_NAMESPACE
 
 Q_DECLARE_METATYPE (LeechCraft::Aggregator::Item_ptr)
 Q_DECLARE_METATYPE (LeechCraft::Aggregator::ItemShort)
