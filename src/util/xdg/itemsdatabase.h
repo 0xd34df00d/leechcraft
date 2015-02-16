af314be8ac90d857aa1923398ba7ea5e88abc0ff
0xd34df00d <0xd34df00d@gmail.com>
2015-02-16 19:22:49 +0300

    [Util] Documented ItemsDatabase.

--- src/util/xdg/itemsdatabase.h
+++ src/util/xdg/itemsdatabase.h
@@ -39,13 +39,35 @@ namespace Util
 {
 namespace XDG
 {
+	/** @brief An ItemsFinder automatically watching for changes in
+	 * <code>.desktop</code> files.
+	 *
+	 * The only difference between this class and ItemsFinder is in this
+	 * class automatically watching for changes in the directories
+	 * matching the types passed to its constructor. The changes include
+	 * both updates to the existing files as well as addition of new files
+	 * and removal of already existing ones.
+	 *
+	 * Refer to the documentation for ItemsFinder for more information.
+	 *
+	 * @sa ItemsFinder
+	 */
 	class UTIL_XDG_API ItemsDatabase : public ItemsFinder
 	{
 		Q_OBJECT
 
-		QFileSystemWatcher *Watcher_;
+		QFileSystemWatcher * const Watcher_;
 	public:
-		ItemsDatabase (ICoreProxy_ptr, const QList<Type>&, QObject* = 0);
+		/** @brief Creates the ItemsDatabase for the given \em types.
+		 *
+		 * @param[in] proxy The proxy to use to get the icons of the
+		 * items that were found.
+		 * @param[in] types The item types to watch for.
+		 * @param[in] parent The parent object of this finder.
+		 *
+		 * @sa ItemsFinder::ItemsFinder
+		 */
+		ItemsDatabase (ICoreProxy_ptr proxy, const QList<Type>& types, QObject *parent = nullptr);
 	};
 }
 }
