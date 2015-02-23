410ad2f488815326abd2343231f77ccf5d158b89
0xd34df00d <0xd34df00d@gmail.com>
2015-02-23 22:10:32 +0300

    [WKPlugins] Added translations.

---
+++ src/plugins/wkplugins/leechcraft_wkplugins_en.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LeechCraft::WKPlugins::Plugin</name>
+    <message>
+        <location filename="wkplugins.cpp" line="72"/>
+        <source>Provides support for spellchecking and HTML5 notifications to WebKit.</source>
+        <translation>Provides support for spellchecking and HTML5 notifications to WebKit.</translation>
+    </message>
+</context>
+<context>
+    <name>wkpluginssettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>WKPlugins</source>
+        <translation>WKPlugins</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Spell checking</source>
+        <translation>Spell checking</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Automatically correct spelling errors</source>
+        <translation>Automatically correct spelling errors</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/wkplugins/leechcraft_wkplugins_ru_RU.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::WKPlugins::Plugin</name>
+    <message>
+        <location filename="wkplugins.cpp" line="72"/>
+        <source>Provides support for spellchecking and HTML5 notifications to WebKit.</source>
+        <translation>Обеспечивает поддержку проверки правописания и HTML5-уведомлений в WebKit.</translation>
+    </message>
+</context>
+<context>
+    <name>wkpluginssettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>WKPlugins</source>
+        <translation>WKPlugins</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Spell checking</source>
+        <translation>Проверка правописания</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Automatically correct spelling errors</source>
+        <translation>Автоматически исправлять ошибки правописания</translation>
+    </message>
+</context>
+</TS>
