414a69cb086a80609725354ca2fd70d9a7c6d82f
0xd34df00d <0xd34df00d@gmail.com>
2015-03-05 02:10:15 +0300

    [LMP] Flush LocalFileResolver cache on recoding changes.
    This finally fixes #1891.

--- src/plugins/lmp/localfileresolver.cpp
+++ src/plugins/lmp/localfileresolver.cpp
@@ -57,6 +57,11 @@ namespace LMP
 	LocalFileResolver::LocalFileResolver (QObject *parent)
 	: QObject (parent)
 	{
+		XmlSettingsManager::Instance ().RegisterObject ({
+					"EnableLocalTagsRecoding",
+					"TagsRecodingRegion"
+				},
+				this, "flushCache");
 	}
 
 	TagLib::FileRef LocalFileResolver::GetFileRef (const QString& file) const
