5ce93a679261806b6d383c658255ebf671610286
0xd34df00d <0xd34df00d@gmail.com>
2015-03-08 15:38:50 +0300

    [Liznoo] Better virtuality for FakeQWidgetWinAPI.

--- src/plugins/liznoo/platform/events/fakeqwidgetwinapi.h
+++ src/plugins/liznoo/platform/events/fakeqwidgetwinapi.h
@@ -34,7 +34,7 @@
 #include <QString>
 #include <windows.h>
 
-namespace LeechCraft 
+namespace LeechCraft
 {
 namespace Liznoo
 {
@@ -44,11 +44,11 @@ namespace Liznoo
 	public:
 		FakeQWidgetWinAPI (QWidget *parent = NULL);
 	protected:
-		virtual void prepareSchemeChange (PPOWERBROADCAST_SETTING setting);
-		virtual void preparePowerSourceChange (PPOWERBROADCAST_SETTING setting);
-		virtual void prepareBatteryStateChange (PPOWERBROADCAST_SETTING setting);
+		void prepareSchemeChange (PPOWERBROADCAST_SETTING setting);
+		void preparePowerSourceChange (PPOWERBROADCAST_SETTING setting);
+		void prepareBatteryStateChange (PPOWERBROADCAST_SETTING setting);
 
-		virtual bool winEvent (MSG *message, long *result);
+		bool winEvent (MSG *message, long *result) override;
 	signals:
 		void schemeChanged (QString schemeName);
 		void powerSourceChanged (QString powerSource);
