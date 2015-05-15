f5cdefb4d49c9ca808bf9e53747c21d53dfcc26e
0xd34df00d <0xd34df00d@gmail.com>
2015-05-15 15:52:36 +0100

    [Util] Documented StandardNameFactory.

--- src/util/qml/standardnamfactory.h
+++ src/util/qml/standardnamfactory.h
@@ -48,6 +48,19 @@ namespace LeechCraft
 {
 namespace Util
 {
+	/** @brief A standard QML QNetworkAccessManager factory.
+	 *
+	 * StandardNAMFactory allows easily creating QNetworkAccessManager
+	 * instances in QML contexts.
+	 *
+	 * The created managers are all using the same cache, located at the
+	 * cache path passed and limited by the maximum size passed to the
+	 * constructor.
+	 *
+	 * Several different factories may be created sharing the same cache
+	 * location. In this case, the minimum value of the cache size would
+	 * be used as the maximum.
+	 */
 #if QT_VERSION < 0x050000
 	class UTIL_QML_API StandardNAMFactory : public QDeclarativeNetworkAccessManagerFactory
 #else
@@ -56,10 +69,25 @@ namespace Util
 	{
 		const QString Subpath_;
 	public:
-		typedef std::function<int ()> CacheSizeGetter_f;
+		/** @brief The type of the function used to query the cache size
+		 * by the factory.
+		 */
+		using CacheSizeGetter_f = std::function<int ()>;
 	private:
 		CacheSizeGetter_f CacheSizeGetter_;
 	public:
+		/** @brief Constructs a new StandardNAMFactory.
+		 *
+		 * The cache uses a subdirectory \em subpath in the \em network
+		 * directory of the user cache location.
+		 *
+		 * @param[in] subpath The subpath in cache user location.
+		 * @param[in] getter The function that would be queried during
+		 * periodical cache garbage collection to fetch the current
+		 * maximum cache size.
+		 * @param[in] engine The QML engine where this factory should be
+		 * installed, if not null.
+		 */
 		StandardNAMFactory (const QString& subpath,
 				CacheSizeGetter_f getter,
 #if QT_VERSION < 0x050000
@@ -68,7 +96,21 @@ namespace Util
 				QQmlEngine *engine = nullptr);
 #endif
 
-		QNetworkAccessManager* create (QObject*);
+		/** @brief Creates the network access manager with the given
+		 * \em parent.
+		 *
+		 * This function implements a pure virtual in Qt's base factory
+		 * class (QDeclarativeNetworkAccessManagerFactory for Qt4 or
+		 * QQmlNetworkAccessManagerFactory for Qt5).
+		 *
+		 * The ownership of the returned QNetworkAccessManager is passed
+		 * to the caller.
+		 *
+		 * @param[in] parent The parent of the QNetworkAccessManager to
+		 * be created.
+		 * @return A new QNetworkAccessManager.
+		 */
+		QNetworkAccessManager* create (QObject *parent);
 	};
 }
 }
