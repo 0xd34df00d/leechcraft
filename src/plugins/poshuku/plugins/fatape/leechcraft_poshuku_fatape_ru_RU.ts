9d5f43cac9423f549a3541fbaa97833d415a4936
0xd34df00d <0xd34df00d@gmail.com>
2015-07-05 23:50:10 +0300

    [Poshuku FA] Updated translations.

--- src/plugins/poshuku/plugins/fatape/leechcraft_poshuku_fatape_ru_RU.ts
+++ src/plugins/poshuku/plugins/fatape/leechcraft_poshuku_fatape_ru_RU.ts
@@ -2,54 +2,80 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
+    <name>CreateScriptDialog</name>
+    <message>
+        <location filename="createscriptdialog.ui" line="14"/>
+        <source>Create script</source>
+        <translation>Создать скрипт</translation>
+    </message>
+    <message>
+        <location filename="createscriptdialog.ui" line="20"/>
+        <source>Namespace:</source>
+        <translation>Пространство имён:</translation>
+    </message>
+    <message>
+        <location filename="createscriptdialog.ui" line="34"/>
+        <source>Name:</source>
+        <translation>Имя:</translation>
+    </message>
+    <message>
+        <location filename="createscriptdialog.ui" line="44"/>
+        <source>Description:</source>
+        <translation>Описание:</translation>
+    </message>
+    <message>
+        <location filename="createscriptdialog.ui" line="54"/>
+        <source>Author:</source>
+        <translation>Автор:</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Poshuku::FatApe::Plugin</name>
     <message>
-        <location filename="fatape.cpp" line="83"/>
+        <location filename="fatape.cpp" line="92"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="fatape.cpp" line="84"/>
+        <location filename="fatape.cpp" line="92"/>
         <source>Description</source>
         <translation>Описание</translation>
     </message>
     <message>
-        <location filename="fatape.cpp" line="115"/>
+        <location filename="fatape.cpp" line="128"/>
         <source>GreaseMonkey support layer for the Poshuku browser.</source>
         <translation>Поддержка GreaseMonkey-скриптов для браузера Poshuku.</translation>
     </message>
 </context>
 <context>
+    <name>LeechCraft::Poshuku::FatApe::UserScriptCreator</name>
+    <message>
+        <location filename="userscriptcreator.cpp" line="90"/>
+        <source>Unable to create file %1: %2. Do you want to try again changing the file name?</source>
+        <translation>Невозможно создать файл %1: %2. Вы хотите попробовать ещё раз с другим именем файла?</translation>
+    </message>
+    <message>
+        <location filename="userscriptcreator.cpp" line="98"/>
+        <source>Enter new filename for the user script %1:</source>
+        <translation>Введите новое имя файла для пользовательского скрипта %1:</translation>
+    </message>
+</context>
+<context>
     <name>LeechCraft::Poshuku::FatApe::UserScriptInstallerDialog</name>
     <message>
-        <location filename="userscriptinstallerdialog.cpp" line="53"/>
+        <location filename="userscriptinstallerdialog.cpp" line="73"/>
         <source>Fetching script...</source>
         <translation>Получение скрипта...</translation>
     </message>
     <message>
-        <location filename="userscriptinstallerdialog.cpp" line="82"/>
+        <location filename="userscriptinstallerdialog.cpp" line="103"/>
         <source>runs on:</source>
         <translation>работает на:</translation>
     </message>
     <message>
-        <location filename="userscriptinstallerdialog.cpp" line="87"/>
+        <location filename="userscriptinstallerdialog.cpp" line="108"/>
         <source>does not run on:</source>
         <translation>не работает на:</translation>
-    </message>
-</context>
-<context>
-    <name>LeechCraft::Poshuku::FatApe::UserScriptsManagerWidget</name>
-    <message>
-        <location filename="userscriptsmanagerwidget.cpp" line="76"/>
-        <location filename="userscriptsmanagerwidget.cpp" line="97"/>
-        <source>Disable</source>
-        <translation>Отключить</translation>
-    </message>
-    <message>
-        <location filename="userscriptsmanagerwidget.cpp" line="76"/>
-        <location filename="userscriptsmanagerwidget.cpp" line="97"/>
-        <source>Enable</source>
-        <translation>Включить</translation>
     </message>
 </context>
 <context>
@@ -93,14 +119,14 @@
 <context>
     <name>UserScriptsManagerWidget</name>
     <message>
-        <location filename="userscriptsmanagerwidget.ui" line="27"/>
+        <location filename="userscriptsmanagerwidget.ui" line="31"/>
         <source>Edit</source>
         <translation>Редактировать</translation>
     </message>
     <message>
-        <location filename="userscriptsmanagerwidget.ui" line="34"/>
-        <source>Disable</source>
-        <translation>Отключить</translation>
+        <location filename="userscriptsmanagerwidget.ui" line="58"/>
+        <source>Create...</source>
+        <translation>Создать…</translation>
     </message>
     <message>
         <location filename="userscriptsmanagerwidget.ui" line="41"/>
