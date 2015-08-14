8ad67545382bf29ca6efc11fd7324d4eae351686
0xd34df00d <0xd34df00d@gmail.com>
2015-08-15 00:00:54 +0300

    [Azoth Xoox] Added caps tables creation scripts.

---
+++ src/plugins/azoth/plugins/xoox/resources/sql/create_features.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Features (
+	Ver VARCHAR(28) NOT NULL,
+	Feature TEXT
+);
---
+++ src/plugins/azoth/plugins/xoox/resources/sql/create_identities.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Identities (
+	Ver VARCHAR(28) NOT NULL,
+	Category TEXT,
+	Language TEXT,
+	Name TEXT,
+	Type TEXT
+);
