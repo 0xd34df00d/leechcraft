d5bf6d1f785270f5edd8eb4b52c14b45c4a0dcd8
0xd34df00d <0xd34df00d@gmail.com>
2015-09-12 18:48:29 +0300

    [Azoth] Added AvatarReply.

--- src/plugins/azoth/chattabnetworkaccessmanager.cpp
+++ src/plugins/azoth/chattabnetworkaccessmanager.cpp
@@ -28,11 +28,82 @@
  **********************************************************************/
 
 #include "chattabnetworkaccessmanager.h"
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QtDebug>
+#include <QBuffer>
+#include <util/sll/delayedexecutor.h>
+#include <util/threads/futures.h>
+#include "avatarsmanager.h"
+#include "core.h"
+#include "resourcesmanager.h"
 
 namespace LeechCraft
 {
 namespace Azoth
 {
+	namespace
+	{
+		class AvatarReply : public QNetworkReply
+		{
+			QBuffer Buffer_;
+		public:
+			AvatarReply (const QNetworkRequest&, AvatarsManager*);
+
+			qint64 bytesAvailable () const override;
+			qint64 readData (char* data, qint64 maxlen) override;
+			void abort () override;
+		private:
+			void HandleImage (const QImage&);
+		};
+
+		AvatarReply::AvatarReply (const QNetworkRequest& req, AvatarsManager *am)
+		{
+			open (QIODevice::ReadOnly);
+
+			const auto& entryIdPath = req.url ().path ().section ('/', 1, 1);
+			const auto& entryId = QString::fromUtf8 (QByteArray::fromBase64 (entryIdPath.toLatin1 ()));
+
+			const auto entryObj = Core::Instance ().GetEntry (entryId);
+			if (!entryObj)
+			{
+				Util::ExecuteLater ([this]
+						{ HandleImage (ResourcesManager::Instance ().GetDefaultAvatar (32)); });
+				return;
+			}
+
+			Util::Sequence (this, am->GetAvatar (entryObj, IHaveAvatars::Size::Thumbnail)) >>
+					[this] (const QImage& image) { HandleImage (image); };
+		}
+
+		qint64 AvatarReply::bytesAvailable () const
+		{
+			return QNetworkReply::bytesAvailable () + Buffer_.bytesAvailable ();
+		}
+
+		qint64 AvatarReply::readData (char *data, qint64 maxlen)
+		{
+			return Buffer_.read (data, maxlen);
+		}
+
+		void AvatarReply::abort ()
+		{
+			qWarning () << Q_FUNC_INFO
+					<< "cannot abort";
+		}
+
+		void AvatarReply::HandleImage (const QImage& image)
+		{
+			Buffer_.open (QIODevice::WriteOnly);
+			image.save (&Buffer_, "PNG", 100);
+			Buffer_.close ();
+
+			Buffer_.open (QIODevice::ReadOnly);
+
+			emit readyRead ();
+		}
+	}
+
 	ChatTabNetworkAccessManager::ChatTabNetworkAccessManager (AvatarsManager *am, QObject *parent)
 	: QNetworkAccessManager { parent }
 	, AvatarsMgr_ { am }
