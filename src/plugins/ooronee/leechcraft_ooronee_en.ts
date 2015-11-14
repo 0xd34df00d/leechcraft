b3daf3f35ef7504cad43adce7c35cbd81f0f6d5a
0xd34df00d <0xd34df00d@gmail.com>
2015-11-14 22:39:11 +0300

    [Ooronee] Translated.

---
+++ src/plugins/ooronee/leechcraft_ooronee_en.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LeechCraft::Ooronee::Plugin</name>
+    <message>
+        <location filename="ooronee.cpp" line="79"/>
+        <source>Provides a quark for handling image and text droppend onto it via other data filter plugins.</source>
+        <translation>Provides a quark for handling image and text droppend onto it via other data filter plugins.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Ooronee::QuarkProxy</name>
+    <message>
+        <location filename="quarkproxy.cpp" line="134"/>
+        <source>Handle text</source>
+        <translation>Handle text</translation>
+    </message>
+    <message>
+        <location filename="quarkproxy.cpp" line="135"/>
+        <source>Select the data filter to handle the dropped text:</source>
+        <translation>Select the data filter to handle the dropped text:</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/ooronee/leechcraft_ooronee_ru_RU.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::Ooronee::Plugin</name>
+    <message>
+        <location filename="ooronee.cpp" line="79"/>
+        <source>Provides a quark for handling image and text droppend onto it via other data filter plugins.</source>
+        <translation>Предоставляет кварк для обработки перетянутых на него изображений и текста при помощи прочих модулей обработки данных.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Ooronee::QuarkProxy</name>
+    <message>
+        <location filename="quarkproxy.cpp" line="134"/>
+        <source>Handle text</source>
+        <translation>Обработать текст</translation>
+    </message>
+    <message>
+        <location filename="quarkproxy.cpp" line="135"/>
+        <source>Select the data filter to handle the dropped text:</source>
+        <translation>Выберите фильтр данных для обработки перетянутого текста:</translation>
+    </message>
+</context>
+</TS>
