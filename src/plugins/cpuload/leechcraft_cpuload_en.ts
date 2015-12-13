4e02a31b4ccbdbaaddb2f7bb33d3fff6412662c3
0xd34df00d <0xd34df00d@gmail.com>
2015-12-13 19:00:24 +0000

    [CL] Added translations.

---
+++ src/plugins/cpuload/leechcraft_cpuload_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LeechCraft::CpuLoad::Plugin</name>
+    <message>
+        <location filename="cpuload.cpp" line="86"/>
+        <source>Quark for monitoring CPU load.</source>
+        <translation>Quark for monitoring CPU load.</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/cpuload/leechcraft_cpuload_ru_RU.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LeechCraft::CpuLoad::Plugin</name>
+    <message>
+        <location filename="cpuload.cpp" line="86"/>
+        <source>Quark for monitoring CPU load.</source>
+        <translation>Кварк для наблюдения за загрузкой CPU.</translation>
+    </message>
+</context>
+</TS>
