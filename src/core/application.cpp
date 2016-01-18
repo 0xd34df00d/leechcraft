215d2cb73cd2717446eda146133ef779748993f1
0xd34df00d <0xd34df00d@gmail.com>
2016-01-19 02:43:27 +0300

    [Core] Better code style.

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -449,7 +449,11 @@ namespace LeechCraft
 	}
 
 #if QT_VERSION >= 0x050000
-	#define qInstallMsgHandler(x) qInstallMessageHandler([] (QtMsgType type, const QMessageLogContext&, const QString& msg) { (x) (type, msg.toUtf8 ().constData ()); })
+	#define qInstallMsgHandler(x) \
+		qInstallMessageHandler([] (QtMsgType type, const QMessageLogContext&, const QString& msg) \
+		{ \
+			(x) (type, msg.toLocal8Bit ().constData ()); \
+		})
 #endif
 
 	void Application::ParseCommandLine ()
