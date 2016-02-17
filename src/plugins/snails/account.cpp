38bfefc072d14f3737eecf7440b7c3c4f6783c72
0xd34df00d <0xd34df00d@gmail.com>
2016-02-17 02:46:01 +0000

    [Snails] Use GetAccountThread().

--- src/plugins/snails/account.cpp
+++ src/plugins/snails/account.cpp
@@ -168,7 +168,7 @@ namespace Snails
 
 	void Account::SynchronizeImpl (const QList<QStringList>& folders, const QByteArray& last)
 	{
-		const auto& future = Thread_->Schedule (&AccountThreadWorker::Synchronize, folders, last);
+		const auto& future = GetAccountThread (Thread::LowPriority)->Schedule (&AccountThreadWorker::Synchronize, folders, last);
 		Util::Sequence (this, future) >>
 				[=] (const auto& result)
 				{
@@ -205,7 +205,7 @@ namespace Snails
 
 	QFuture<WrapReturnType_t<FetchWholeMessageResult_t>> Account::FetchWholeMessage (const Message_ptr& msg)
 	{
-		auto future = MessageFetchThread_->Schedule (&AccountThreadWorker::FetchWholeMessage, msg);
+		auto future = GetAccountThread (Thread::HighPriority)->Schedule (&AccountThreadWorker::FetchWholeMessage, msg);
 
 		Util::Sequence (this, future) >>
 			[this] (const auto& result)
@@ -236,19 +236,19 @@ namespace Snails
 			pair.second = UserEmail_;
 		msg->SetAddress (Message::Address::From, pair);
 
-		return MessageFetchThread_->Schedule (&AccountThreadWorker::SendMessage, msg);
+		return GetAccountThread (Thread::HighPriority)->Schedule (&AccountThreadWorker::SendMessage, msg);
 	}
 
 	void Account::FetchAttachment (const Message_ptr& msg,
 			const QString& attName, const QString& path)
 	{
-		MessageFetchThread_->Schedule (&AccountThreadWorker::FetchAttachment,
+		GetAccountThread (Thread::LowPriority)->Schedule (&AccountThreadWorker::FetchAttachment,
 				msg, attName, path);
 	}
 
 	void Account::SetReadStatus (bool read, const QList<QByteArray>& ids, const QStringList& folder)
 	{
-		const auto& future = MessageFetchThread_->Schedule (&AccountThreadWorker::SetReadStatus, read, ids, folder);
+		const auto& future = GetAccountThread (Thread::HighPriority)->Schedule (&AccountThreadWorker::SetReadStatus, read, ids, folder);
 		Util::Sequence (this, future) >>
 				[=] (const auto& result)
 				{
@@ -264,7 +264,7 @@ namespace Snails
 
 	void Account::CopyMessages (const QList<QByteArray>& ids, const QStringList& from, const QList<QStringList>& to)
 	{
-		MessageFetchThread_->Schedule (&AccountThreadWorker::CopyMessages, ids, from, to);
+		GetAccountThread (Thread::HighPriority)->Schedule (&AccountThreadWorker::CopyMessages, ids, from, to);
 	}
 
 	void Account::MoveMessages (const QList<QByteArray>& ids, const QStringList& from, const QList<QStringList>& to)
@@ -537,8 +537,8 @@ namespace Snails
 
 	void Account::UpdateNoopInterval ()
 	{
-		Thread_->Schedule (&AccountThreadWorker::SetNoopTimeout, KeepAliveInterval_);
-		MessageFetchThread_->Schedule (&AccountThreadWorker::SetNoopTimeout, KeepAliveInterval_);
+		GetAccountThread (Thread::LowPriority)->Schedule (&AccountThreadWorker::SetNoopTimeout, KeepAliveInterval_);
+		GetAccountThread (Thread::HighPriority)->Schedule (&AccountThreadWorker::SetNoopTimeout, KeepAliveInterval_);
 	}
 
 	QString Account::BuildInURL ()
@@ -694,7 +694,7 @@ namespace Snails
 
 	void Account::RequestMessageCount (const QStringList& folder)
 	{
-		Util::Sequence (this, Thread_->Schedule (&AccountThreadWorker::GetMessageCount, folder)) >>
+		Util::Sequence (this, GetAccountThread (Thread::LowPriority)->Schedule (&AccountThreadWorker::GetMessageCount, folder)) >>
 				[=] (const auto& counts)
 				{
 					Util::Visit (counts.AsVariant (),
