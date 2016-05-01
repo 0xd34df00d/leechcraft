a3a9397bcdbb45fdceb1ca899041735df3548e67
0xd34df00d <0xd34df00d@gmail.com>
2016-05-01 16:08:27 -0400

    [Azoth] Initial history sync step.

--- src/plugins/azoth/historysyncer.cpp
+++ src/plugins/azoth/historysyncer.cpp
@@ -28,6 +28,12 @@
  **********************************************************************/
 
 #include "historysyncer.h"
+#include <QFutureSynchronizer>
+#include <QtDebug>
+#include <util/sll/slotclosure.h>
+#include <util/sll/either.h>
+#include <util/sll/prelude.h>
+#include <util/threads/futures.h>
 #include "interfaces/azoth/iaccount.h"
 #include "interfaces/azoth/ihistoryplugin.h"
 #include "interfaces/azoth/ihaveserverhistory.h"
@@ -46,11 +52,92 @@ namespace Azoth
 		Storages_ << storage;
 	}
 
+	namespace
+	{
+		bool IsOnline (State st)
+		{
+			switch (st)
+			{
+			case SOffline:
+			case SError:
+			case SConnecting:
+			case SInvalid:
+				return false;
+			default:
+				return true;
+			}
+		}
+	}
+
 	void HistorySyncer::AddAccount (IAccount *acc)
 	{
 		const auto ihsh = qobject_cast<IHaveServerHistory*> (acc->GetQObject ());
 		if (!ihsh)
 			return;
+
+		const auto accObj = acc->GetQObject ();
+		new Util::SlotClosure<Util::NoDeletePolicy>
+		{
+			[this, acc]
+			{
+				if (!IsOnline (acc->GetState ().State_))
+					CurrentlyOnline_.remove (acc);
+				else if (!CurrentlyOnline_.contains (acc))
+				{
+					CurrentlyOnline_ << acc;
+					StartAccountSync (acc);
+				}
+			},
+			accObj,
+			SIGNAL (statusChanged (EntryStatus)),
+			accObj
+		};
+	}
+
+	void HistorySyncer::StartAccountSync (IAccount *acc)
+	{
+		qDebug () << Q_FUNC_INFO
+				<< acc->GetAccountID ();
+
+		if (Storages_.isEmpty ())
+		{
+			qDebug () << Q_FUNC_INFO
+					<< "no history storage plugins are loaded";
+			return;
+		}
+
+		using RetType_t = Util::UnwrapFutureType_t<decltype (Storages_ [0]->RequestMaxTimestamp (acc))>;
+
+		auto allStorages = std::make_shared<QFutureSynchronizer<RetType_t>> ();
+		for (const auto& storage : Storages_)
+			allStorages->addFuture (storage->RequestMaxTimestamp (acc));
+
+		Util::Sequence (this, QtConcurrent::run ([allStorages] { allStorages->waitForFinished (); })) >>
+				[=]
+				{
+					const auto& results = Util::Map (allStorages->futures (),
+							[] (auto future) { return future.result (); });
+
+					const auto& partition = Util::PartitionEithers (results);
+					if (!partition.first.isEmpty ())
+					{
+						qWarning () << Q_FUNC_INFO
+								<< "got storage errors:"
+								<< partition.first
+								<< "; aborting sync";
+						return;
+					}
+
+					const auto& minDate = *std::min_element (partition.second.begin (), partition.second.end ());
+					RequestAccountFrom (acc, minDate);
+				};
+	}
+
+	void HistorySyncer::RequestAccountFrom (IAccount *acc, const QDateTime& from)
+	{
+		qDebug () << Q_FUNC_INFO
+				<< acc->GetAccountID ()
+				<< from;
 	}
 }
 }
--- src/plugins/azoth/historysyncer.h
+++ src/plugins/azoth/historysyncer.h
@@ -30,6 +30,10 @@
 #pragma once
 
 #include <QObject>
+#include <QSet>
+
+template<typename>
+class QFuture;
 
 namespace LeechCraft
 {
@@ -43,11 +47,16 @@ namespace Azoth
 		Q_OBJECT
 
 		QList<IHistoryPlugin*> Storages_;
+
+		QSet<IAccount*> CurrentlyOnline_;
 	public:
 		HistorySyncer (QObject* = nullptr);
 
 		void AddStorage (IHistoryPlugin*);
 		void AddAccount (IAccount*);
+	private:
+		void StartAccountSync (IAccount*);
+		void RequestAccountFrom (IAccount*, const QDateTime&);
 	};
 }
 }
