0bd3bb6d0a84174459724ff5ce59b1067885d56b
0xd34df00d <0xd34df00d@gmail.com>
2016-07-06 00:26:43 -0400

    [Aggregator] DBUpdateThread's dtor does the thread stop job for us.

--- src/plugins/aggregator/core.cpp
+++ src/plugins/aggregator/core.cpp
@@ -110,19 +110,13 @@ namespace Aggregator
 
 	void Core::Release ()
 	{
-		if (DBUpThread_->isRunning ())
-			DBUpThread_->quit ();
+		delete DBUpThread_;
 
 		delete JobHolderRepresentation_;
 		delete ChannelsFilterModel_;
 		delete ChannelsModel_;
 
 		StorageBackend_.reset ();
-
-		if (DBUpThread_->isRunning ())
-			DBUpThread_->wait (1000);
-		if (DBUpThread_->isRunning ())
-			DBUpThread_->terminate ();
 
 		XmlSettingsManager::Instance ()->Release ();
 	}
