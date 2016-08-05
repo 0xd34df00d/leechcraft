c9fbb0fc17d626bdfec3adf544251bc272efeaed
0xd34df00d <0xd34df00d@gmail.com>
2016-08-04 23:58:01 -0400

    [Poshuku DCAC] Updated translations.

--- src/plugins/poshuku/plugins/dcac/leechcraft_poshuku_dcac_en.ts
+++ src/plugins/poshuku/plugins/dcac/leechcraft_poshuku_dcac_en.ts
@@ -4,7 +4,7 @@
 <context>
     <name>LeechCraft::Poshuku::DCAC::Plugin</name>
     <message>
-        <location filename="dcac.cpp" line="72"/>
+        <location filename="dcac.cpp" line="74"/>
         <source>Inverts the colors of web pages.</source>
         <translation>Inverts the colors of web pages.</translation>
     </message>
@@ -12,7 +12,7 @@
 <context>
     <name>LeechCraft::Poshuku::DCAC::ViewsManager</name>
     <message>
-        <location filename="viewsmanager.cpp" line="65"/>
+        <location filename="viewsmanager.cpp" line="105"/>
         <source>Night mode</source>
         <translation>Night mode</translation>
     </message>
@@ -32,14 +32,59 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="4"/>
+        <source>Night mode effect:</source>
+        <translation>Night mode effect:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Invert colors</source>
+        <translation>Invert colors</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Reduce lightness</source>
+        <translation>Reduce lightness</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Color temperature</source>
+        <translation>Color temperature</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Color invert effect</source>
+        <translation>Color invert effect</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
         <source>Average brightness threshold:</source>
         <oldsource>Average brightness threshold</oldsource>
         <translation>Average brightness threshold:</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="5"/>
+        <location filename="dummy.cpp" line="10"/>
         <source>Night mode will only be enabled for the given page if its average grayness is bigger than this value.</source>
         <translation>Night mode will only be enabled for the given page if its average grayness is bigger than this value.</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Reduce lightness effect</source>
+        <translation>Reduce lightness effect</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="12"/>
+        <source>Factor:</source>
+        <translation>Factor:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <source>Color temperature effect</source>
+        <translation>Color temperature effect</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="14"/>
+        <source>Color temperature:</source>
+        <translation>Color temperature:</translation>
     </message>
 </context>
 </TS>
--- src/plugins/poshuku/plugins/dcac/leechcraft_poshuku_dcac_ru_RU.ts
+++ src/plugins/poshuku/plugins/dcac/leechcraft_poshuku_dcac_ru_RU.ts
@@ -4,7 +4,7 @@
 <context>
     <name>LeechCraft::Poshuku::DCAC::Plugin</name>
     <message>
-        <location filename="dcac.cpp" line="72"/>
+        <location filename="dcac.cpp" line="74"/>
         <source>Inverts the colors of web pages.</source>
         <translation>Инвертирует цвета веб-страниц.</translation>
     </message>
@@ -12,7 +12,7 @@
 <context>
     <name>LeechCraft::Poshuku::DCAC::ViewsManager</name>
     <message>
-        <location filename="viewsmanager.cpp" line="65"/>
+        <location filename="viewsmanager.cpp" line="105"/>
         <source>Night mode</source>
         <translation>Ночной режим</translation>
     </message>
@@ -32,14 +32,59 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="4"/>
+        <source>Night mode effect:</source>
+        <translation>Эффект для ночного режима:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Invert colors</source>
+        <translation>Инвертирование цветов</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Reduce lightness</source>
+        <translation>Уменьшение яркости</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Color temperature</source>
+        <translation>Цветовая температура</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Color invert effect</source>
+        <translation>Эффект инвертирования цветов</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
         <source>Average brightness threshold:</source>
         <oldsource>Average brightness threshold</oldsource>
         <translation>Порог средней яркости:</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="5"/>
+        <location filename="dummy.cpp" line="10"/>
         <source>Night mode will only be enabled for the given page if its average grayness is bigger than this value.</source>
         <translation>Ночной режим будет включён только в том случае, если её среднее значение яркости больше, чем это значение.</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Reduce lightness effect</source>
+        <translation>Эффект уменьшения яркости</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="12"/>
+        <source>Factor:</source>
+        <translation>Делитель:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <source>Color temperature effect</source>
+        <translation>Эффект цветовой температуры</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="14"/>
+        <source>Color temperature:</source>
+        <translation>Цветовая температура:</translation>
     </message>
 </context>
 </TS>
