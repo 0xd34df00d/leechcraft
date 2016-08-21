a1ea4db79eb6cbaa07cba1bf354d68ae40435353
0xd34df00d <0xd34df00d@gmail.com>
2016-08-21 19:48:28 -0400

    [Util] Added optional hook checking in BaseHookInterconnector.

--- src/util/xpc/basehookinterconnector.cpp
+++ src/util/xpc/basehookinterconnector.cpp
@@ -51,11 +51,85 @@ namespace Util
 			return method.parameterTypes ().value (0) == "LeechCraft::IHookProxy_ptr";
 		}
 
+#if QT_VERSION >= 0x050000
+		QHash<QByteArray, QMetaMethod> BuildHookSlots (const QObject *obj)
+		{
+			const auto objMo = obj->metaObject ();
+
+			QHash<QByteArray, QMetaMethod> hookSlots;
+			for (int i = 0, size = objMo->methodCount (); i < size; ++i)
+			{
+				const auto& method = objMo->method (i);
+
+				if (!IsHookMethod (method))
+					continue;
+
+				const auto& name = method.name ();
+				if (hookSlots.contains (name))
+					qWarning () << Q_FUNC_INFO
+							<< obj
+							<< "has several hook methods with name"
+							<< name;
+
+				hookSlots [name] = method;
+			}
+
+			return hookSlots;
+		}
+#endif
+
+		void CheckMatchingSigs (const QObject *snd, const QObject *rcv)
+		{
+#if QT_VERSION >= 0x050000
+			if (!qEnvironmentVariableIsSet ("LC_VERBOSE_HOOK_CHECKS"))
+				return;
+
+			const auto& hookSlots = BuildHookSlots (snd);
+
+			const auto rcvMo = rcv->metaObject ();
+
+			for (int i = 0, size = rcvMo->methodCount (); i < size; ++i)
+			{
+				const auto& rcvMethod = rcvMo->method (i);
+				if (!IsHookMethod (rcvMethod))
+					continue;
+
+				const auto& rcvName = rcvMethod.name ();
+				if (!hookSlots.contains (rcvName))
+				{
+					qWarning () << Q_FUNC_INFO
+							<< "no method matching method"
+							<< rcvName
+							<< "(receiver"
+							<< rcv
+							<< ") in sender object"
+							<< snd;
+					continue;
+				}
+
+				if (!QMetaObject::checkConnectArgs (hookSlots [rcvName], rcvMethod))
+					qWarning () << Q_FUNC_INFO
+							<< "incompatible signatures for hook"
+							<< rcvName
+							<< "in"
+							<< snd
+							<< "and"
+							<< rcv;
+			}
+#else
+			Q_UNUSED (sender)
+			Q_UNUSED (receiver)
+#endif
+		}
+
 #define LC_N(a) (QMetaObject::normalizedSignature(a))
 #define LC_TOSLOT(a) ('1' + QByteArray(a))
 #define LC_TOSIGNAL(a) ('2' + QByteArray(a))
 		void ConnectHookSignals (QObject *sender, QObject *receiver, bool destSlot)
 		{
+			if (destSlot)
+				CheckMatchingSigs (sender, receiver);
+
 			const QMetaObject *mo = sender->metaObject ();
 			for (int i = 0, size = mo->methodCount (); i < size; ++i)
 			{
