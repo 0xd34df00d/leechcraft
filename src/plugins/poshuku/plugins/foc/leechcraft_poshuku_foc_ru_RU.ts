251769806629490a91916705ae6390feaa075a7c
0xd34df00d <0xd34df00d@gmail.com>
2016-08-27 16:23:49 -0400

    [Poshuku FOC] Stub translations generation.

---
+++ src/plugins/poshuku/plugins/foc/leechcraft_poshuku_foc_ru_RU.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>FlashOnClickWhitelist</name>
+    <message>
+        <location filename="flashonclickwhitelist.ui" line="25"/>
+        <source>Add...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="flashonclickwhitelist.ui" line="32"/>
+        <source>Modify...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="flashonclickwhitelist.ui" line="39"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FlashPlaceHolder</name>
+    <message>
+        <location filename="flashplaceholder.ui" line="46"/>
+        <source>Load Flash</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::FOC::FlashOnClickWhitelist</name>
+    <message>
+        <location filename="flashonclickwhitelist.cpp" line="49"/>
+        <source>Whitelist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="flashonclickwhitelist.cpp" line="116"/>
+        <source>Add URL to whitelist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="flashonclickwhitelist.cpp" line="117"/>
+        <source>Please enter the URL to add to the FlashOnClick&apos;s whitelist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="flashonclickwhitelist.cpp" line="132"/>
+        <source>This URL is already matched by another whitelist entry.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::FOC::FlashPlaceHolder</name>
+    <message>
+        <location filename="flashplaceholder.cpp" line="141"/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="flashplaceholder.cpp" line="147"/>
+        <source>Add URL to whitelist...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="flashplaceholder.cpp" line="152"/>
+        <source>Add host to whitelist...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="flashplaceholder.cpp" line="162"/>
+        <source>Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::FOC::Plugin</name>
+    <message>
+        <location filename="foc.cpp" line="74"/>
+        <source>Shows the Flash-on-Click placeholder instead of flash player clips.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>poshukufocsettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>FlashOnClick</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Enable FlashOnClick</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
