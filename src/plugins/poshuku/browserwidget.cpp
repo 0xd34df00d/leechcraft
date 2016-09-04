72e5ac946153be0d4b436b0773fbd77f8d338eeb
0xd34df00d <0xd34df00d@gmail.com>
2016-09-04 03:41:02 +0100

    [Poshuku] Don't set `target="_blank"` attribute on all links in deown mode.
    It's not browser widget's responsibility, but rather the caller's one.

--- src/plugins/poshuku/browserwidget.cpp
+++ src/plugins/poshuku/browserwidget.cpp
@@ -684,19 +684,6 @@ namespace Poshuku
 		Ui_.URLFrame_->GetEdit ()->clear ();
 		HtmlMode_ = true;
 		WebView_->SetContent (html.toUtf8 (), "text/html", base);
-
-		if (!Own_)
-		{
-			auto links = WebView_->page ()->mainFrame ()->findAllElements ("a");
-			for (int i = 0; i < links.count (); ++i)
-			{
-				auto elem = links.at (i);
-				if (elem.attribute ("target") == "_blank")
-					continue;
-
-				elem.setAttribute ("target", "_blank");
-			}
-		}
 	}
 
 	void BrowserWidget::SetNavBarVisible (bool visible)
