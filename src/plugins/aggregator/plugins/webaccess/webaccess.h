96c536414a69f0f4c321b496534ec32a3c7ef2ea
0xd34df00d <0xd34df00d@gmail.com>
2016-11-06 22:19:37 -0500

    [Aggregator WA] Use override markers.

--- src/plugins/aggregator/plugins/webaccess/webaccess.h
+++ src/plugins/aggregator/plugins/webaccess/webaccess.h
@@ -73,19 +73,19 @@ namespace WebAccess
 
 		Util::AddressesModelManager *AddrMgr_;
 	public:
-		void Init (ICoreProxy_ptr);
-		void SecondInit ();
-		QByteArray GetUniqueID () const;
-		void Release ();
-		QString GetName () const;
-		QString GetInfo () const;
-		QIcon GetIcon () const;
+		void Init (ICoreProxy_ptr) override;
+		void SecondInit () override;
+		QByteArray GetUniqueID () const override;
+		void Release () override;
+		QString GetName () const override;
+		QString GetInfo () const override;
+		QIcon GetIcon () const override;
 
-		QSet<QByteArray> GetPluginClasses () const;
+		QSet<QByteArray> GetPluginClasses () const override;
 
-		Util::XmlSettingsDialog_ptr GetSettingsDialog () const;
+		Util::XmlSettingsDialog_ptr GetSettingsDialog () const override;
 
-		void InitPlugin (IProxyObject*);
+		void InitPlugin (IProxyObject*) override;
 	};
 }
 }
