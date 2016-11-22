20f5f2b0d750b2709b7d7158c76b7f8c1053e4bc
0xd34df00d <0xd34df00d@gmail.com>
2016-11-22 00:37:17 -0500

    [Poshuku FUA] Moved all the substitutions to substs.json.

--- src/plugins/poshuku/plugins/fua/fua.cpp
+++ src/plugins/poshuku/plugins/fua/fua.cpp
@@ -171,45 +171,33 @@ namespace Fua
 			}
 
 			const auto& json = Util::ParseJson (&file, Q_FUNC_INFO);
-			qDebug () << json;
-			return {};
+
+			QList<QPair<QString, QString>> result;
+
+			for (const auto& itemVar : json.toList ())
+			{
+				const auto& map = itemVar.toMap ();
+				const auto& name = map ["n"].toString ();
+				const auto& value = map ["v"].toString ();
+				if (name.isEmpty () || value.isEmpty ())
+				{
+					qWarning () << Q_FUNC_INFO
+							<< "cannot parse"
+							<< itemVar;
+					continue;
+				}
+
+				result.append ({ name, value });
+			}
+
+			return result;
 		}
 	}
 
 	void FUA::initPlugin (QObject*)
 	{
-		Browser2ID_ = QList<QPair<QString, QString>>
-		{
-			{ tr ("LeechCraft (this machine)"), "" },
-			{ "Android Webkit Browser (Android 2.2.1, HTC Desire Z)", "Mozilla/5.0 (Linux; U; Android 2.2.1; en-gb; HTC_DesireZ_A7272 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1" },
-			{ "Android Webkit Browser (Android 4.0.3)", "Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" },
-			{ "Chromium 28.0 (Linux x86)", "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/28.0.1500.71 Chrome/28.0.1500.71 Safari/537.36" },
-			{ "Chromium 30.0 (Linux x86_64)", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36" },
-			{ "Chrome 30.0 (Windows 7)", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36" },
-			{ "Chrome 30.0 (Mac OS X 10.8)", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36" },
-			{ "Chrome 41.0 (Mac OS X 10.10)", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36" },
-			{ "Epiphany 2.30.6 (Ubuntu 10.04)", "Mozilla/5.0 (X11; U; Linux x86_64; en_US) AppleWebKit/534.26+ (KHTML, like Gecko) Ubuntu/11.04 Epiphany/2.30.6" },
-			{ "Firefox 24.0 (Linux x86_64)", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0" },
-			{ "Firefox 24.0 (Windows 7)", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0" },
-			{ "Firefox 24.0 (Mac OS X 10.8)", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0" },
-			{ "Firefox 36.0 (Windows 8.1)", "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0" },
-			{ "IE 5.0 (Mac PPC)", "Mozilla/4.0 (compatible; MSIE 5.0; Mac_PowerPC)" },
-			{ "IE 6.0 (Windows XP)", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)" },
-			{ "IE 8.0 (Windows XP SP3)", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; InfoPath.1; .NET CLR 3.5.21022; .NET CLR 1.1.4322; OfficeLiveConnector.1.4; OfficeLivePatch.1.3; .NET CLR 3.0.4506.2152)" },
-			{ "IE 8.0 (Windows 7)", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3)" },
-			{ "IE 9.0 (Windows 7)", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)" },
-			{ "IE 10.0 (Windows 7)", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)" },
-			{ "IE 11.0 (Windows 8.1)", "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko" },
-			{ "Opera 4.03 (Windows NT 4.0)", "Opera/4.03 (Windows NT 4.0; U)" },
-			{ "Opera 12.16 (Windows 7)", "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.16" },
-			{ "Opera Mini 9.80 (S60)", "Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54" },
-			{ "Opera Mobile 12.02 (Android 4.1)", "Opera/12.02 (Android 4.1; Linux; Opera Mobi/ADR-1111101157; U; en-US) Presto/2.9.201 Version/12.02" },
-			{ "Safari 2 (Mac OS X PowerPC)", "Mozilla/5.0 (Macintosh; U; PPC Mac OS X; appLanguage) AppleWebKit/412 (KHTML, like Gecko) Safari/412" },
-			{ "Safari 5 (Windows 7)", "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru-RU) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16" },
-			{ "Safari 6.0 (Mac OS X 10.8)", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1" },
-			{ "Safari 7.0 (Mac OS X 10.9)", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9) AppleWebKit/537.71 (KHTML, like Gecko) Version/7.0 Safari/537.71" },
-			{ "UCWeb 7 (Windows Mobile 5)", "HTC_P3400-Mozilla/4.0 Mozilla/4.0 (compatible; MSIE 4.01; Windows CE; PPC)/UCWEB7.0.0.41/31/400" }
-		};
+		Browser2ID_ = LoadFromJson ();
+		Browser2ID_.prepend ({ tr ("LeechCraft (this machine)"), "" });
 
 		BackLookup_ = MakeLookupMap (Browser2ID_);
 
