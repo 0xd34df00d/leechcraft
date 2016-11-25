725fc9f4fe1f350335b65cc684b20ddedad7764b
0xd34df00d <0xd34df00d@gmail.com>
2016-11-24 19:06:59 -0500

    [Poshuku FUA] Removed unused var.

--- src/plugins/poshuku/plugins/fua/settings.cpp
+++ src/plugins/poshuku/plugins/fua/settings.cpp
@@ -77,7 +77,6 @@ namespace Fua
 
 		domain = changer.GetDomain ();
 		identification = changer.GetID ();
-		QList<QStandardItem*> items;
 		Model_->item (cur.row (), 0)->setText (domain);
 		Model_->item (cur.row (), 1)->setText (Fua_->GetBackLookupMap () [identification]);
 		Model_->item (cur.row (), 2)->setText (identification);
