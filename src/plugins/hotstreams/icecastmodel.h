b30d4446629d776e4170be6ef8e56ed51bf99a33
0xd34df00d <0xd34df00d@gmail.com>
2017-01-08 20:58:14 -0500

    [HotStreams] Slight code style fix.

--- src/plugins/hotstreams/icecastmodel.h
+++ src/plugins/hotstreams/icecastmodel.h
@@ -57,10 +57,10 @@ namespace HotStreams
 	public:
 		IcecastModel (QObject* = nullptr);
 
-		QModelIndex index (int row, int column, const QModelIndex& parent = QModelIndex()) const override;
+		QModelIndex index (int row, int column, const QModelIndex& parent = {}) const override;
 		QModelIndex parent (const QModelIndex& child) const override;
-		int rowCount (const QModelIndex& parent = QModelIndex()) const override;
-		int columnCount (const QModelIndex& parent = QModelIndex()) const override;
+		int rowCount (const QModelIndex& parent = {}) const override;
+		int columnCount (const QModelIndex& parent = {}) const override;
 		Qt::ItemFlags flags (const QModelIndex& index) const override;
 		QVariant data (const QModelIndex& index, int role = Qt::DisplayRole) const override;
 
