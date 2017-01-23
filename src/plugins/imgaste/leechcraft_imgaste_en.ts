56b4572235c7621b803386bb9f9b0e265f6cc07c
0xd34df00d <0xd34df00d@gmail.com>
2017-01-22 23:48:21 -0500

    [Imgaste] Updated translations.

--- src/plugins/imgaste/leechcraft_imgaste_en.ts
+++ src/plugins/imgaste/leechcraft_imgaste_en.ts
@@ -4,14 +4,19 @@
 <context>
     <name>LeechCraft::Imgaste::Plugin</name>
     <message>
-        <location filename="imgaste.cpp" line="71"/>
+        <location filename="imgaste.cpp" line="75"/>
         <source>Simple image uploader to imagebin services like dump.bitcheese.net.</source>
         <translation>Simple image uploader to imagebin services like dump.bitcheese.net.</translation>
     </message>
     <message>
-        <location filename="imgaste.cpp" line="121"/>
+        <location filename="imgaste.cpp" line="118"/>
         <source>Upload image</source>
         <translation>Upload image</translation>
+    </message>
+    <message>
+        <location filename="imgaste.cpp" line="213"/>
+        <source>Unknown upload service: %1.</source>
+        <translation>Unknown upload service: %1.</translation>
     </message>
 </context>
 <context>
@@ -32,18 +37,18 @@
         <translation>Page parse failed</translation>
     </message>
     <message>
-        <location filename="poster.cpp" line="117"/>
+        <location filename="poster.cpp" line="116"/>
         <source>Image pasted: %1, the URL was copied to the clipboard</source>
         <translation>Image pasted: %1, the URL was copied to the clipboard</translation>
     </message>
     <message>
-        <location filename="poster.cpp" line="133"/>
+        <location filename="poster.cpp" line="132"/>
         <source>Image upload failed: %1</source>
         <oldsource>Upload of screenshot failed: %1</oldsource>
         <translation>Image upload failed: %1</translation>
     </message>
     <message>
-        <location filename="poster.cpp" line="145"/>
+        <location filename="poster.cpp" line="142"/>
         <source>%1 of %2</source>
         <translation>%1 of %2</translation>
     </message>
--- src/plugins/imgaste/leechcraft_imgaste_ru_RU.ts
+++ src/plugins/imgaste/leechcraft_imgaste_ru_RU.ts
@@ -4,14 +4,19 @@
 <context>
     <name>LeechCraft::Imgaste::Plugin</name>
     <message>
-        <location filename="imgaste.cpp" line="71"/>
+        <location filename="imgaste.cpp" line="75"/>
         <source>Simple image uploader to imagebin services like dump.bitcheese.net.</source>
         <translation>Простой загрузчик изображений на сервисы вроде dump.bitcheese.net.</translation>
     </message>
     <message>
-        <location filename="imgaste.cpp" line="121"/>
+        <location filename="imgaste.cpp" line="118"/>
         <source>Upload image</source>
         <translation>Загрузить изображение</translation>
+    </message>
+    <message>
+        <location filename="imgaste.cpp" line="213"/>
+        <source>Unknown upload service: %1.</source>
+        <translation>Неизвестный сервис загрузки изображений: %1.</translation>
     </message>
 </context>
 <context>
@@ -32,18 +37,18 @@
         <translation>Разбор страницы не удался</translation>
     </message>
     <message>
-        <location filename="poster.cpp" line="117"/>
+        <location filename="poster.cpp" line="116"/>
         <source>Image pasted: %1, the URL was copied to the clipboard</source>
         <translation>Изображение загружено: %1, URL был скопирован в буфер обмена</translation>
     </message>
     <message>
-        <location filename="poster.cpp" line="133"/>
+        <location filename="poster.cpp" line="132"/>
         <source>Image upload failed: %1</source>
         <oldsource>Upload of screenshot failed: %1</oldsource>
         <translation>Ошибка загрузки изображения: %1</translation>
     </message>
     <message>
-        <location filename="poster.cpp" line="145"/>
+        <location filename="poster.cpp" line="142"/>
         <source>%1 of %2</source>
         <translation>%1 из %2</translation>
     </message>
