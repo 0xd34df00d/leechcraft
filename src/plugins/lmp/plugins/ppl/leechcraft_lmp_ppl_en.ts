91f0c3d6a2f079c16846965da14c81a0a80f794e
0xd34df00d <0xd34df00d@gmail.com>
2017-02-12 22:47:45 -0500

    [LMP PPL] Translation stubs.

---
+++ src/plugins/lmp/plugins/ppl/leechcraft_lmp_ppl_en.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>LeechCraft::LMP::PPL::LocalCollectionScrobbler</name>
+    <message>
+        <location filename="loghandler.cpp" line="83"/>
+        <source>Local collection</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::LMP::PPL::Plugin</name>
+    <message>
+        <location filename="ppl.cpp" line="50"/>
+        <source>Sync scrobbling log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ppl.cpp" line="58"/>
+        <source>Select .scrobbler.log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ppl.cpp" line="60"/>
+        <source>Scrobbler log (*.scrobbler.log)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ppl.cpp" line="99"/>
+        <source>Synchronizes the logs of songs played on portable devices with LMP and scrobbling services like Last.FM.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::LMP::PPL::TracksSelectorDialog</name>
+    <message>
+        <location filename="tracksselectordialog.cpp" line="106"/>
+        <source>Artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tracksselectordialog.cpp" line="107"/>
+        <source>Album</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tracksselectordialog.cpp" line="108"/>
+        <source>Track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tracksselectordialog.cpp" line="109"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tracksselectordialog.cpp" line="254"/>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TracksSelectorDialog</name>
+    <message>
+        <location filename="tracksselectordialog.ui" line="14"/>
+        <source>Listened tracks</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
