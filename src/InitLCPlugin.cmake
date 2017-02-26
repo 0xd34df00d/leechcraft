4f6180ee38a2884c47ee871826b4341d77f9b288
0xd34df00d <0xd34df00d@gmail.com>
2017-02-26 00:24:57 +0000

    [Overall] Ditch support for Qt4 in split build.

--- src/InitLCPlugin.cmake
+++ src/InitLCPlugin.cmake
@@ -1,13 +1,9 @@
-if (NOT QT_USE_FILE)
+if (NOT LEECHCRAFT_LIBRARIES)
 	cmake_policy (SET CMP0003 NEW)
 
 	set (CMAKE_MODULE_PATH "/usr/local/share/leechcraft/cmake;/usr/share/leechcraft/cmake;${CMAKE_MODULE_PATH}")
 	set (CMAKE_MODULE_PATH "/usr/local/share/apps/cmake/modules;/usr/share/apps/cmake/modules;${CMAKE_MODULE_PATH}")
 
 	find_package (Boost REQUIRED)
-	if (USE_QT5)
-		find_package (LeechCraft-qt5 REQUIRED)
-	else ()
-		find_package (LeechCraft REQUIRED)
-	endif ()
+	find_package (LeechCraft-qt5 REQUIRED)
 endif ()
