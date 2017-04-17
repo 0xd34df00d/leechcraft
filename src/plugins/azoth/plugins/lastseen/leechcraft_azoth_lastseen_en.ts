6bab1df08c549901b9ae9226f07dd878428d2196
0xd34df00d <0xd34df00d@gmail.com>
2017-04-17 00:07:50 -0400

    [Azoth LastSeen] Updated translations.

--- src/plugins/azoth/plugins/lastseen/leechcraft_azoth_lastseen_en.ts
+++ src/plugins/azoth/plugins/lastseen/leechcraft_azoth_lastseen_en.ts
@@ -1,25 +1,35 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LeechCraft::Azoth::LastSeen::Plugin</name>
     <message>
-        <location filename="lastseen.cpp" line="79"/>
+        <location filename="lastseen.cpp" line="98"/>
         <source>Azoth LastSeen displays when a contact has been online and available for the last time.</source>
         <translation>Azoth LastSeen displays when a contact has been online and available for the last time.</translation>
     </message>
     <message>
-        <location filename="lastseen.cpp" line="198"/>
+        <location filename="lastseen.cpp" line="155"/>
+        <source>Uniting keys</source>
+        <translation>Uniting keys</translation>
+    </message>
+    <message>
+        <location filename="lastseen.cpp" line="178"/>
+        <source>Writing the database</source>
+        <translation>Writing the database</translation>
+    </message>
+    <message>
+        <location filename="lastseen.cpp" line="267"/>
         <source>Was available: %1</source>
         <translation>Was available: %1</translation>
     </message>
     <message>
-        <location filename="lastseen.cpp" line="211"/>
+        <location filename="lastseen.cpp" line="280"/>
         <source>Was online: %1</source>
         <translation>Was online: %1</translation>
     </message>
     <message>
-        <location filename="lastseen.cpp" line="221"/>
+        <location filename="lastseen.cpp" line="290"/>
         <source>Last status change: %1</source>
         <translation>Last status change: %1</translation>
     </message>
