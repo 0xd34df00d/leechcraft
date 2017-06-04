3a73538d32fa5defe8cb12ce00fd9be91ec66df3
0xd34df00d <0xd34df00d@gmail.com>
2017-06-03 22:18:20 -0400

    [Azoth Autoidler] Added Romanian translations.

---
+++ src/plugins/azoth/plugins/autoidler/leechcraft_azoth_autoidler_ro.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ro_RO" version="2.0">
+<context>
+    <name>LeechCraft::Azoth::Autoidler::Plugin</name>
+    <message>
+        <location filename="autoidler.cpp" line="86"/>
+        <source>Automatically updates statuses depending on idle time.</source>
+        <translation>Actualizează automat statusuri în funcție timpul de așteptare.</translation>
+    </message>
+</context>
+<context>
+    <name>azothautoidlersettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Azoth Autoidler</source>
+        <translation>Azoth Autoidler</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Enable autoidler</source>
+        <translation>Activare apelare automată</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>&apos;Away&apos; timeout:</source>
+        <translation>Timp de expirare pentru &apos;Plecat&apos;:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <location filename="dummy.cpp" line="7"/>
+        <source> min</source>
+        <translation> min</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>&apos;Not Available&apos; timeout:</source>
+        <translation>Timp de expirare pentru &apos;Nu sunt disponibil&apos;</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>&apos;Away&apos; status text:</source>
+        <translation>Text status &apos;Plecat&apos;:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>&apos;Not Available&apos; status text:</source>
+        <translation>Text status &apos;Nu sunt disponibil&apos;:</translation>
+    </message>
+</context>
+</TS>
