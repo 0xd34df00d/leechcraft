0xd34df00d/leechcraft

commit 85f23ebe565b0f5dcacec2d1ba8da6b1f036e742

[AN] Enable QML unconditionally.
Hey, ages since Qt 4.6 release!

src/plugins/advancednotifications/systemtrayhandler.cpp
@@ -41,10 +41,7 @@
 #include "generalhandler.h"
 #include "xmlsettingsmanager.h"
 #include "core.h"
-
-#ifdef HAVE_QML
-#include "qml/visualnotificationsview.h"
-#endif
+#include "visualnotificationsview.h"
 
 namespace LeechCraft
 {
@@ -151,8 +148,7 @@ namespace AdvancedNotifications
 				this,
 				SLOT (handleTrayActivated (QSystemTrayIcon::ActivationReason)));
 
-#ifdef HAVE_QML
-		VisualNotificationsView *vnv = new VisualNotificationsView;
+		const auto vnv = new VisualNotificationsView;
 		connect (vnv,
 				SIGNAL (actionTriggered (const QString&, int)),
 				this,
@@ -165,7 +161,6 @@ namespace AdvancedNotifications
 
 		if (XmlSettingsManager::Instance ().property ("HideOnHoverOut").toBool ())
 			new Util::UnhoverDeleteMixin (vnv, SLOT (hide ()));
-#endif
 	}
 
 	void SystemTrayHandler::PrepareLCTrayAction (const QString& category)
@@ -184,8 +179,7 @@ namespace AdvancedNotifications
 
 		emit gotActions (QList<QAction*> () << action, ActionsEmbedPlace::LCTray);
 
-#ifdef HAVE_QML
-		VisualNotificationsView *vnv = new VisualNotificationsView;
+		const auto vnv = new VisualNotificationsView;
 		connect (vnv,
 				SIGNAL (actionTriggered (const QString&, int)),
 				this,
@@ -198,7 +192,6 @@ namespace AdvancedNotifications
 
 		if (XmlSettingsManager::Instance ().property ("HideOnHoverOut").toBool ())
 			new Util::UnhoverDeleteMixin (vnv, SLOT (hide ()));
-#endif
 	}
 
 	void SystemTrayHandler::UpdateMenu (QMenu *menu, const QString& event, const EventData& data)
@@ -243,10 +236,8 @@ namespace AdvancedNotifications
 		Q_FOREACH (QAction *action, actsDel)
 			action->menu ()->clear ();
 
-#ifdef HAVE_QML
 		EventsForIcon_.clear ();
 		EventsForAction_.clear ();
-#endif
 
 		QSet<QSystemTrayIcon*> visibleIcons;
 		QSet<QAction*> actsUpd;
@@ -266,18 +257,15 @@ namespace AdvancedNotifications
 			actsDel.remove (action);
 			actsUpd << action;
 
-#ifdef HAVE_QML
 			if (icon)
 				EventsForIcon_ [icon] << data;
 			EventsForAction_ [action] << data;
-#endif
 
 			if (icon)
 				UpdateMenu (icon->contextMenu (), event, data);
 			UpdateMenu (action->menu (), event, data);
 		}
 
-#ifdef HAVE_QML
 		Q_FOREACH (QSystemTrayIcon *icon, Category2Icon_.values ())
 		{
 			VisualNotificationsView *view = Icon2NotificationView_ [icon];
@@ -301,7 +289,6 @@ namespace AdvancedNotifications
 			if (events.isEmpty ())
 				view->hide ();
 		}
-#endif
 
 		Q_FOREACH (QSystemTrayIcon *icon, visibleIcons)
 		{
@@ -426,7 +413,6 @@ namespace AdvancedNotifications
 		if (reason != QSystemTrayIcon::Trigger)
 			return;
 
-#ifdef HAVE_QML
 		QSystemTrayIcon *trayIcon = qobject_cast<QSystemTrayIcon*> (sender ());
 		if (!trayIcon)
 		{
@@ -437,7 +423,6 @@ namespace AdvancedNotifications
 		}
 
 		ShowVNV (Icon2NotificationView_ [trayIcon], EventsForIcon_ [trayIcon]);
-#endif
 	}
 
 	void SystemTrayHandler::handleLCAction ()
@@ -451,11 +436,7 @@ namespace AdvancedNotifications
 			return;
 		}
 
-#ifndef HAVE_QML
 		action->menu ()->show ();
-#else
-		ShowVNV (Action2NotificationView_ [action], EventsForAction_ [action]);
-#endif
 	}
 }
 }

src/plugins/advancednotifications/CMakeLists.txt
@@ -28,9 +28,9 @@ set (SRCS
 	rulesmanager.cpp
 	quarkproxy.cpp
 	actionsmodel.cpp
-	qml/visualnotificationsview.cpp
-	qml/eventproxyobject.cpp
-	qml/actionsproxyobject.cpp
+	visualnotificationsview.cpp
+	eventproxyobject.cpp
+	actionsproxyobject.cpp
 	)
 set (FORMS
 	notificationruleswidget.ui
@@ -39,8 +39,6 @@ set (FORMS
 	intmatcherconfigwidget.ui
 	stringlikematcherconfigwidget.ui
 	)
-
-add_definitions(-DHAVE_QML=1)
 
 set (RESOURCES advancednotificationsresources.qrc)
 

src/plugins/advancednotifications/eventproxyobject.h
@@ -31,7 +31,7 @@
 #define PLUGINS_ADVANCEDNOTIFICATIONS_QML_EVENTPROXYOBJECT_H
 #include <QObject>
 #include <QUrl>
-#include "../eventdata.h"
+#include "eventdata.h"
 
 namespace LeechCraft
 {

src/plugins/advancednotifications/systemtrayhandler.h
@@ -45,9 +45,7 @@ namespace LeechCraft
 {
 namespace AdvancedNotifications
 {
-#ifdef HAVE_QML
 	class VisualNotificationsView;
-#endif
 
 	class SystemTrayHandler : public ConcreteHandlerBase
 	{
@@ -57,13 +55,11 @@ namespace AdvancedNotifications
 		QMap<QString, QAction*> Category2Action_;
 		QMap<QString, EventData> Events_;
 
-#ifdef HAVE_QML
 		QMap<QSystemTrayIcon*, QList<EventData>> EventsForIcon_;
 		QMap<QSystemTrayIcon*, VisualNotificationsView*> Icon2NotificationView_;
 
 		QMap<QAction*, QList<EventData>> EventsForAction_;
 		QMap<QAction*, VisualNotificationsView*> Action2NotificationView_;
-#endif
 	public:
 		SystemTrayHandler ();
 		~SystemTrayHandler ();

src/plugins/advancednotifications/visualnotificationsview.cpp
@@ -45,7 +45,7 @@
 #include <util/qml/themeimageprovider.h>
 #include <interfaces/core/icoreproxy.h>
 #include "eventproxyobject.h"
-#include "../core.h"
+#include "core.h"
 
 namespace LeechCraft
 {

commit ad0225e4ede422d4450d5fdd1a265372e5d84c6a

[AN] Restored visual notifications view instead of QMenu.
Broke that in 85f23ebe565b0f5dcacec2d1ba8da6b1f036e742.

src/plugins/advancednotifications/systemtrayhandler.cpp
@@ -442,7 +442,7 @@ namespace AdvancedNotifications
 			return;
 		}
 
-		action->menu ()->show ();
+		ShowVNV (Action2NotificationView_ [action], EventsForAction_ [action]);
 	}
 }
 }