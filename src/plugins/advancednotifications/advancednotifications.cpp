5d5b9660cba5451305d5c4938f429bc778e7e27b
0xd34df00d <0xd34df00d@gmail.com>
2017-06-06 23:26:04 -0400

    [AN] Use std::make_shared().

--- src/plugins/advancednotifications/advancednotifications.cpp
+++ src/plugins/advancednotifications/advancednotifications.cpp
@@ -54,7 +54,7 @@ namespace AdvancedNotifications
 		Proxy_ = proxy;
 		Core::Instance ().SetProxy (proxy);
 
-		SettingsDialog_.reset (new Util::XmlSettingsDialog ());
+		SettingsDialog_ = std::make_shared<Util::XmlSettingsDialog> ();
 		SettingsDialog_->RegisterObject (&XmlSettingsManager::Instance (),
 				"advancednotificationssettings.xml");
 		SettingsDialog_->SetCustomWidget ("RulesWidget",
@@ -68,7 +68,7 @@ namespace AdvancedNotifications
 				this,
 				SIGNAL (gotActions (QList<QAction*>, LeechCraft::ActionsEmbedPlace)));
 
-		Component_.reset (new QuarkComponent ("advancednotifications", "ANQuark.qml"));
+		Component_ = std::make_shared<QuarkComponent> ("advancednotifications", "ANQuark.qml");
 		Component_->StaticProps_.push_back ({ "AN_quarkTooltip", tr ("Toggle Advanced Notifications rules...") });
 		Component_->DynamicProps_.push_back ({ "AN_rulesManager", Core::Instance ().GetRulesManager () });
 		Component_->DynamicProps_.push_back ({ "AN_proxy", new QuarkProxy });
