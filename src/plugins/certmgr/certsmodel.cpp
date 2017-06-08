17b2677285ef781207a0a194934831f5c2738940
0xd34df00d <0xd34df00d@gmail.com>
2017-06-07 23:43:47 -0400

    [CM] Remove Qt4 code.

--- src/plugins/certmgr/certsmodel.cpp
+++ src/plugins/certmgr/certsmodel.cpp
@@ -102,15 +102,10 @@ namespace CertMgr
 				result += "<td>" + val + "</td></tr>";
 			};
 
-#if QT_VERSION < 0x050000
-			auto addStdFields = [&add] (std::function<QString (QSslCertificate::SubjectInfo)> getter) -> void
-			{
-#else
 			auto addStdFields = [&add] (auto&& listGetter)
 			{
 				const auto getter = [listGetter] (QSslCertificate::SubjectInfo key)
 						{ return listGetter (key).join ("; "); };
-#endif
 				add (CertsModel::tr ("Organization"),
 						getter (QSslCertificate::Organization));
 				add (CertsModel::tr ("Unit"),
@@ -147,11 +142,7 @@ namespace CertMgr
 					QLocale {}.toString (cert.expiryDate (), QLocale::ShortFormat));
 			result += "</table><br />";
 
-#if QT_VERSION < 0x050000
-			const auto& subjs = cert.alternateSubjectNames ();
-#else
 			const auto& subjs = cert.subjectAlternativeNames ();
-#endif
 			if (!subjs.isEmpty ())
 			{
 				result += "<strong>" + CertsModel::tr ("Alternate names") + ":</strong>";
@@ -204,13 +195,8 @@ namespace CertMgr
 
 			if (!name.isEmpty () && !org.isEmpty ())
 				return QString ("%1 (%2)")
-#if QT_VERSION < 0x050000
-						.arg (name)
-						.arg (org);
-#else
 						.arg (name.join ("; "))
 						.arg (org.join ("; "));
-#endif
 			else if (!name.isEmpty ())
 				return name;
 			else
@@ -315,12 +301,7 @@ namespace CertMgr
 			auto issuer = cert.issuerInfo (QSslCertificate::Organization);
 			if (issuer.isEmpty ())
 				issuer = cert.issuerInfo (QSslCertificate::CommonName);
-
-#if QT_VERSION < 0x050000
-			return issuer;
-#else
 			return issuer.join ("; ");
-#endif
 		}
 	}
 
