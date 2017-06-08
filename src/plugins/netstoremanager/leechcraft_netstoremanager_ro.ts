4e847f9c1386c179b50e8bda9c4e9be5bedcea14
0xd34df00d <0xd34df00d@gmail.com>
2017-06-07 23:58:49 -0400

    [Summary] Added Romanian translations.

---
+++ src/plugins/netstoremanager/leechcraft_netstoremanager_ro.ts
@@ -0,0 +1,399 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ro_RO" version="2.1">
+<context>
+    <name>AccountsListWidget</name>
+    <message>
+        <location filename="accountslistwidget.ui" line="28"/>
+        <source>Add...</source>
+        <translation>Adăugare...</translation>
+    </message>
+    <message>
+        <location filename="accountslistwidget.ui" line="35"/>
+        <source>Remove</source>
+        <translation>Eliminare</translation>
+    </message>
+</context>
+<context>
+    <name>AddAccountDialog</name>
+    <message>
+        <location filename="addaccountdialog.ui" line="14"/>
+        <source>Add account</source>
+        <translation>Adaugă cont</translation>
+    </message>
+    <message>
+        <location filename="addaccountdialog.ui" line="25"/>
+        <source>Storage:</source>
+        <translation>Stocare:</translation>
+    </message>
+    <message>
+        <location filename="addaccountdialog.ui" line="35"/>
+        <source>Account name:</source>
+        <translation>Nume cont:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::AccountsManager</name>
+    <message>
+        <location filename="accountsmanager.cpp" line="45"/>
+        <source>Account</source>
+        <translation>Cont</translation>
+    </message>
+    <message>
+        <location filename="accountsmanager.cpp" line="45"/>
+        <source>Storage</source>
+        <translation>Stocare</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::DirectoryWidget</name>
+    <message>
+        <location filename="directorywidget.cpp" line="71"/>
+        <source>Select directory</source>
+        <translation>Selectați directorul</translation>
+    </message>
+    <message>
+        <location filename="directorywidget.cpp" line="80"/>
+        <source>Account hasn't been selected.
+You should select an account in first column.</source>
+        <oldsource>Account hasn't been selected.
+You should select an account in first column</oldsource>
+        <translation>Contul nu a fost selectat.
+Trebuie să selectați un cont în prima coloană.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::DownManager</name>
+    <message>
+        <location filename="downmanager.cpp" line="60"/>
+        <source>Could not find plugin to download %1.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::ManagerTab</name>
+    <message>
+        <location filename="managertab.cpp" line="100"/>
+        <source>Copy URL...</source>
+        <translation>Copiere URL...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="77"/>
+        <source>Name</source>
+        <translation>Nume</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="77"/>
+        <source>Used space</source>
+        <translation>Spațiu utilizat</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="77"/>
+        <source>Modify</source>
+        <translation>Modificare</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="94"/>
+        <source>Open file</source>
+        <translation>Deschide fișier</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="106"/>
+        <source>Copy...</source>
+        <translation>Copiază...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="112"/>
+        <source>Move...</source>
+        <translation>Mută...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="118"/>
+        <source>Rename...</source>
+        <translation>Redenumire...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="124"/>
+        <source>Paste</source>
+        <translation>Lipiți</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="130"/>
+        <source>Delete...</source>
+        <translation>Ștergere...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="136"/>
+        <source>Move to trash</source>
+        <translation>Mută la gunoi</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="142"/>
+        <source>Restore from trash</source>
+        <translation>Restaurează de la gunoi</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="148"/>
+        <source>Empty trash</source>
+        <translation>Golire coș gunoi</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="154"/>
+        <source>Create directory</source>
+        <translation>Creare director</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="160"/>
+        <location filename="managertab.cpp" line="991"/>
+        <source>Upload...</source>
+        <translation>Încărcare...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="166"/>
+        <source>Download</source>
+        <translation>Descărcare</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="256"/>
+        <source>Refresh</source>
+        <translation>Reîmprospătare</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="262"/>
+        <source>Upload</source>
+        <translation>Încărcare</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="272"/>
+        <location filename="managertab.cpp" line="968"/>
+        <source>Open trash</source>
+        <translation>Deschidere gunoi</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="281"/>
+        <source>Trash</source>
+        <translation>Gunoi</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="431"/>
+        <source>Unable to get file listing for the account %1: %2.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="585"/>
+        <source>Error</source>
+        <translation>Eroare</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="586"/>
+        <source>You first need to add an account.</source>
+        <translation>Trebuie mai întâi să adăugați un cont.</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="593"/>
+        <location filename="managertab.cpp" line="883"/>
+        <source>Select file for upload</source>
+        <translation>Selectați fișierul de încărcat</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="787"/>
+        <source>New name:</source>
+        <translation>Nume nou:</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="864"/>
+        <source>New directory name:</source>
+        <translation>Nume directorului nou:</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="managertab.cpp" line="918"/>
+        <source>Download %n file(s)</source>
+        <translation type="unfinished"><numerusform></numerusform><numerusform></numerusform><numerusform></numerusform></translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="962"/>
+        <source>Unable to request file URL.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="968"/>
+        <source>Close trash</source>
+        <translation>Închideți gunoi</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="1031"/>
+        <source>Export to...</source>
+        <translation>Exportare la...</translation>
+    </message>
+    <message>
+        <location filename="managertab.cpp" line="948"/>
+        <source>File URL has been copied to the clipboard.</source>
+        <translation>URL-ul fișierului a fost copiat în clipboard.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::Plugin</name>
+    <message>
+        <location filename="netstoremanager.cpp" line="116"/>
+        <source>Allows one to manage his network storages like Yandex.Disk.</source>
+        <translation>Permite unuia să își gestioneze stocarea în rețea precum Yandex.Disc.</translation>
+    </message>
+    <message>
+        <location filename="netstoremanager.cpp" line="136"/>
+        <source>Net storage</source>
+        <translation>Stocare în Rețea</translation>
+    </message>
+    <message>
+        <location filename="netstoremanager.cpp" line="198"/>
+        <source>No account for service name %1.</source>
+        <translation>Nici un cont pentru serviciul cu numele %1.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::RemoteDirectorySelectDialog</name>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="64"/>
+        <source>New directory...</source>
+        <oldsource>New Folder...</oldsource>
+        <translation>Director nou...</translation>
+    </message>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="71"/>
+        <source>Directory</source>
+        <translation>Director</translation>
+    </message>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="104"/>
+        <source>Unable to get file listing for the account.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="remotedirectoryselectdialog.cpp" line="140"/>
+        <source>Enter new directory name:</source>
+        <oldsource>Enter new folder name</oldsource>
+        <translation>Introduceți numele noului director:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::SyncWidget</name>
+    <message>
+        <location filename="syncwidget.cpp" line="55"/>
+        <source>Account</source>
+        <translation>Cont</translation>
+    </message>
+    <message>
+        <location filename="syncwidget.cpp" line="56"/>
+        <source>Local directory</source>
+        <translation>Director local</translation>
+    </message>
+    <message>
+        <location filename="syncwidget.cpp" line="56"/>
+        <source>Remote directory</source>
+        <translation>Director la distanță</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::NetStoreManager::UpManager</name>
+    <message>
+        <location filename="upmanager.cpp" line="124"/>
+        <source>%1 is already uploading to %2.</source>
+        <translation>%1 se deja încărcarea pe %2.</translation>
+    </message>
+    <message>
+        <location filename="upmanager.cpp" line="139"/>
+        <source>Uploading %1 to %2...</source>
+        <translation>Se încărcă %1 în %2...</translation>
+    </message>
+    <message>
+        <location filename="upmanager.cpp" line="143"/>
+        <source>Initializing...</source>
+        <translation>Se inițializează...</translation>
+    </message>
+    <message>
+        <location filename="upmanager.cpp" line="172"/>
+        <source>Failed to upload %1: %2.</source>
+        <translation>Nu s-a reușit încărcarea %1: %2.</translation>
+    </message>
+    <message>
+        <location filename="upmanager.cpp" line="193"/>
+        <source>File %1 was uploaded successfully</source>
+        <translation>Fișierul %1 a fost încărcat cu succes.</translation>
+    </message>
+    <message>
+        <location filename="upmanager.cpp" line="211"/>
+        <source>Failed to auto-share file %1.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="upmanager.cpp" line="223"/>
+        <source>%1 of %2</source>
+        <translation>%1 din %2</translation>
+    </message>
+</context>
+<context>
+    <name>ManagerTab</name>
+    <message>
+        <location filename="managertab.ui" line="45"/>
+        <source>Filter...</source>
+        <translation>Filtrare...</translation>
+    </message>
+</context>
+<context>
+    <name>RemoteDirectorySelectDialog</name>
+    <message>
+        <location filename="remotedirectoryselectdialog.ui" line="20"/>
+        <source>Select remote directory for synchronization:</source>
+        <oldsource>Selecte remote directory for synchronization:</oldsource>
+        <translation>Selectați directorul la distanță pentru sincronizare:</translation>
+    </message>
+</context>
+<context>
+    <name>SynchronizationWidget</name>
+    <message>
+        <location filename="syncwidget.ui" line="32"/>
+        <source>Add</source>
+        <translation>Adăugare</translation>
+    </message>
+    <message>
+        <location filename="syncwidget.ui" line="39"/>
+        <source>Remove</source>
+        <translation>Eliminare</translation>
+    </message>
+</context>
+<context>
+    <name>netstoremanagersettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Accounts</source>
+        <translation>Conturi</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Behavior</source>
+        <translation>Comportament</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Copy URL to clipboard after uploading item</source>
+        <translation>Copiați URL-ul în clipboard după încărcarea elementului</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Synchronization</source>
+        <translation>Sincronizare</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Directories</source>
+        <translation>Directoare</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Exceptions</source>
+        <translation>Excepții</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Ignore synchronization for files with following masks:</source>
+        <translation>Ignora sincronizarea pentru fișierele cu următoarele măști:</translation>
+    </message>
+</context>
+</TS>
