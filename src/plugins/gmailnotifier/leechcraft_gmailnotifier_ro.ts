eccf1026fa3ac6023449c5318afec08a47c740e3
0xd34df00d <0xd34df00d@gmail.com>
2017-06-15 22:03:14 -0400

    [GN] Added Romanian translations.

---
+++ src/plugins/gmailnotifier/leechcraft_gmailnotifier_ro.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ro_RO" version="2.0">
+<context>
+    <name>LeechCraft::GmailNotifier::GmailChecker</name>
+    <message>
+        <location filename="gmailchecker.cpp" line="122"/>
+        <location filename="gmailchecker.cpp" line="173"/>
+        <source>Error</source>
+        <translation>Eroare</translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="128"/>
+        <source>Cannot parse XML data</source>
+        <translation>Nu pot analiza datele XML</translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="97"/>
+        <source>Username for the GMail checker isn&apos;t set. You can enable it in GMail Notifier settings.</source>
+        <translation>Numele de Utilizator pentru verificatorul GMail nu este stabilit. Îl puteți activa în configurările Notificatorului GMail.</translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="104"/>
+        <source>Password isn&apos;t set</source>
+        <translation>Parola nu este setată</translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="178"/>
+        <source>The username or password is incorrect</source>
+        <translation>Numele de utilizator sau parola sunt incorecte.</translation>
+    </message>
+    <message>
+        <location filename="gmailchecker.cpp" line="180"/>
+        <source>Connection timeout</source>
+        <translation>Timpul de conectare a expirat</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::GmailNotifier::GmailNotifier</name>
+    <message>
+        <location filename="gmailnotifier.cpp" line="120"/>
+        <source>Google mail notification plugin</source>
+        <translation>Modul de notificare Google mail</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::GmailNotifier::MailListView</name>
+    <message>
+        <location filename="maillistview.cpp" line="99"/>
+        <source>Yesterday, %1</source>
+        <translation>Ieri, %1</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::GmailNotifier::Notifier</name>
+    <message>
+        <location filename="notifier.cpp" line="69"/>
+        <source>No subject</source>
+        <translation>Fără subiect</translation>
+    </message>
+    <message>
+        <location filename="notifier.cpp" line="69"/>
+        <source>from</source>
+        <translation>expeditor</translation>
+    </message>
+    <message>
+        <location filename="notifier.cpp" line="73"/>
+        <source>at</source>
+        <translation>la</translation>
+    </message>
+    <message>
+        <location filename="notifier.cpp" line="84"/>
+        <source>and %1 more</source>
+        <translation>și %1 mai mult</translation>
+    </message>
+</context>
+<context>
+    <name>gmailnotifiersettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>GMail Notifier</source>
+        <translation>Notificator Gmail</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Login settings</source>
+        <translation>Configurări Autentificare</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Login:</source>
+        <translation>Utilizator:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Password:</source>
+        <translation>Parola:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Update settings</source>
+        <translation>Actualizare Configurări</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Enable checking</source>
+        <translation>Activare Verificare</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Update interval:</source>
+        <translation>Interval de actualizare:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source> s</source>
+        <translation> s</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Appearance</source>
+        <translation>Aspect</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Number of messages shown:</source>
+        <translation>Numărul de mesaje afișate:</translation>
+    </message>
+</context>
+</TS>
