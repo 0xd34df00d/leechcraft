889788370aad08ed786af50081a95d3d967cb756
0xd34df00d <0xd34df00d@gmail.com>
2017-06-16 00:19:28 -0400

    [Azoth Rosenthal] Added Romanian translations.

---
+++ src/plugins/azoth/plugins/rosenthal/leechcraft_azoth_rosenthal_ro.ts
@@ -0,0 +1,10 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ro_RO" version="2.0">
+<context>
+    <name>LeechCraft::Azoth::Rosenthal::Plugin</name>
+    <message>
+        <location filename="rosenthal.cpp" line="83"/>
+        <source>Provides spellchecking for Azoth.</source>
+        <translation>Oferă verificare ortografică pentru Azoth.</translation>
+    </message>
+</context>
+</TS>
