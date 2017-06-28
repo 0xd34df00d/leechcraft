ac25b7b569e72f07e4768b244cd41be41419ccb5
0xd34df00d <0xd34df00d@gmail.com>
2017-06-27 23:08:11 -0400

    [CSTP] Added Romanian translations.

---
+++ src/plugins/cstp/leechcraft_cstp_ro.ts
@@ -0,0 +1,316 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ro_RO" version="2.0">
+<context>
+    <name>AddTask</name>
+    <message>
+        <location filename="addtask.ui" line="14"/>
+        <source>Add task</source>
+        <translation>Adaugă sarcină</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="32"/>
+        <source>Local path:</source>
+        <translation>Calea locală:</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="44"/>
+        <source>Browse...</source>
+        <translation>Răsfoire...</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="56"/>
+        <source>Filename:</source>
+        <translation>Nume fișier:</translation>
+    </message>
+    <message>
+        <location filename="addtask.ui" line="66"/>
+        <source>Comment:</source>
+        <translation>Comentariu:</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::CSTP::AddTask</name>
+    <message>
+        <location filename="addtask.cpp" line="116"/>
+        <source>Directory %1 doesn&apos;t exist, would you like to select another?</source>
+        <translation>Directorul %1 nu există, doriți să selectați un altul?</translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="119"/>
+        <source>Directory %1 isn&apos;t readable, would you like to select another?</source>
+        <translation>Directorul %1 nu poate fi citit, doriți să selectați un altul?</translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="122"/>
+        <source>Directory %1 isn&apos;t writable, would you like to select another?</source>
+        <translation>Directorul %1 nu poate fi scris, doriți să selectați un altul?</translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="125"/>
+        <source>%1 isn&apos;t a directory at all, would you like to select another?</source>
+        <translation>%1 nu este deloc un director, doriți să selectați un altul?</translation>
+    </message>
+    <message>
+        <location filename="addtask.cpp" line="166"/>
+        <source>Select directory</source>
+        <translation>Selectează director</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::CSTP::CSTP</name>
+    <message>
+        <location filename="cstp.cpp" line="219"/>
+        <source>Remove</source>
+        <translation>Eliminare</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="226"/>
+        <source>Remove all</source>
+        <translation>Șterge tot</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="235"/>
+        <source>Start</source>
+        <translation>Pornire</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="242"/>
+        <source>Stop</source>
+        <translation>Oprire</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="249"/>
+        <source>Start all</source>
+        <translation>Pornire tot</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="256"/>
+        <source>Stop all</source>
+        <translation>Oprire tot</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="268"/>
+        <source>File exists</source>
+        <translation>Fișierul există</translation>
+    </message>
+    <message>
+        <location filename="cstp.cpp" line="269"/>
+        <source>File %1 already exists, continue download?</source>
+        <translation>Fișierul %1 există deja, continuați descărcarea?</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::CSTP::Core</name>
+    <message>
+        <location filename="core.cpp" line="72"/>
+        <source>State</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="72"/>
+        <source>Progress</source>
+        <translation>Progres</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="281"/>
+        <source>Could not truncate file </source>
+        <translation>Nu s-a putut trunchia fișierul</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="413"/>
+        <source>%1 (ETA: %2)</source>
+        <translation>%1 (ETA: %2)</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="425"/>
+        <source>%1% (%2 of %3 at %4)</source>
+        <translation>%1% (%2 din %3 la %4)</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="429"/>
+        <source>/s</source>
+        <translation>/s</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="525"/>
+        <source>Could not open file %1: %2</source>
+        <translation>Nu s-a putut deschide fișierul %1: %2</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="586"/>
+        <source>Failed downloading %1 (%2).</source>
+        <translation>Descărcarea eșuată %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="589"/>
+        <source>Finished downloading %1 (%2).</source>
+        <translation>S-a terminat descărcarea %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="603"/>
+        <source>Handle...</source>
+        <translation>Gestionare...</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="610"/>
+        <source>Open externally</source>
+        <translation>Deschidere externă</translation>
+    </message>
+    <message>
+        <location filename="core.cpp" line="615"/>
+        <source>Show folder</source>
+        <translation>Afișare dosar</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::CSTP::Task</name>
+    <message>
+        <location filename="task.cpp" line="239"/>
+        <source>Stopped</source>
+        <translation>Oprit</translation>
+    </message>
+    <message>
+        <location filename="task.cpp" line="241"/>
+        <source>Finished</source>
+        <translation>Finalizat</translation>
+    </message>
+    <message>
+        <location filename="task.cpp" line="243"/>
+        <source>Running</source>
+        <translation>Rulează</translation>
+    </message>
+    <message>
+        <location filename="task.cpp" line="264"/>
+        <source>Task isn&apos;t initialized properly</source>
+        <translation>Sarcina nu este inițializată corect</translation>
+    </message>
+    <message>
+        <location filename="task.cpp" line="486"/>
+        <source>Error writing to file %1: %2</source>
+        <translation>Eroare la scrierea în fișierul %1: %2</translation>
+    </message>
+</context>
+<context>
+    <name>cstpsettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Local settings</source>
+        <translation>Configurări locale</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Behavior</source>
+        <translation>Comportament</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Alert about errors</source>
+        <translation>Alertă despre erori</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Network settings</source>
+        <translation>Configurări de rețea</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Use text transfer mode:</source>
+        <translation>Folosește modul de transfer text</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>Identification</source>
+        <translation>Identificare</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>HTTP</source>
+        <translation>HTTP</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Login:</source>
+        <translation>Utilizator:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="12"/>
+        <source>Password:</source>
+        <translation>Parola:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <source>Custom user agent:</source>
+        <translation>Agent utilizator personalizat:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="14"/>
+        <source>Predefined user agent:</source>
+        <translation>Agent utilizator predefinit:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="15"/>
+        <source>LeechCraft</source>
+        <translation>LeechCraft</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="16"/>
+        <source>Firefox 3.0.1 on Linux i686</source>
+        <translation>Firefox 3.0.1 pe Linux i686</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="17"/>
+        <source>Internet Explorer 6.0 on Windows XP</source>
+        <translation>Internet Explorer 6.0 pe Windows XP</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="18"/>
+        <source>Internet Explorer 7.0 on Windows Vista</source>
+        <translation>Internet Explorer 7.0 pe Windows Vista</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="19"/>
+        <source>Internet Explorer 4.0 on Windows 98</source>
+        <translation>Internet Explorer 4.0 pe Windows 98</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="20"/>
+        <source>Internet Explorer 2.0 on Windows 95</source>
+        <translation>Internet Explorer 2.0 pe Windows 95</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="21"/>
+        <source>Konqueror 4.0</source>
+        <translation>Konqueror 4.0</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="22"/>
+        <source>SeaMonkey 1.1.11 on Linux i686</source>
+        <translation>SeaMonkey 1.1.11 pe Linux i686</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="23"/>
+        <source>Opera 9.52 on Linux x86_64</source>
+        <translation>Opera 9.52 pe Linux x86_64</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="24"/>
+        <source>Wget 1.9.1</source>
+        <translation>Wget 1.9.1</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="25"/>
+        <source>w3m 0.5.2</source>
+        <translation>w3m 0.5.2</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="26"/>
+        <source>Lynx 2.8.7 with OpenSSL</source>
+        <translation>Lynx 2.8.7 cu OpenSSL</translation>
+    </message>
+</context>
+</TS>
