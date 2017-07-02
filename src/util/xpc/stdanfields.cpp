6c09e46d21905ce735ab2711ea366887b1ddfc68
0xd34df00d <0xd34df00d@gmail.com>
2017-07-01 21:52:15 -0400

    [Util] No need for MakeMap in GetStdANFields() now we're Qt5-only.

--- src/util/xpc/stdanfields.cpp
+++ src/util/xpc/stdanfields.cpp
@@ -32,7 +32,6 @@
 #include <QList>
 #include <interfaces/an/ianemitter.h>
 #include <interfaces/an/constants.h>
-#include <util/util.h>
 
 namespace LeechCraft
 {
@@ -40,225 +39,226 @@ namespace Util
 {
 	QList<ANFieldData> GetStdANFields (const QString& type)
 	{
-		static const auto values = MakeMap<QString, QList<ANFieldData>> ({
+		static const QMap<QString, QList<ANFieldData>> values
+		{
+			{
+				AN::TypeIMEventTuneChange,
 				{
-					AN::TypeIMEventTuneChange,
 					{
+						AN::Field::MediaTitle,
+						QObject::tr ("Title metadata"),
+						QObject::tr ("Title of the track being played."),
+						QVariant::String,
 						{
-							AN::Field::MediaTitle,
-							QObject::tr ("Title metadata"),
-							QObject::tr ("Title of the track being played."),
-							QVariant::String,
-							{
-								AN::TypeIMEventTuneChange
-							}
-						},
-						{
-							AN::Field::MediaAlbum,
-							QObject::tr ("Album metadata"),
-							QObject::tr ("Album of the track being played."),
-							QVariant::String,
-							{
-								AN::TypeIMEventTuneChange
-							}
-						},
-						{
-							AN::Field::MediaArtist,
-							QObject::tr ("Artist metadata"),
-							QObject::tr ("Artist of the track being played."),
-							QVariant::String,
-							{
-								AN::TypeIMEventTuneChange
-							}
-						},
-						{
-							AN::Field::MediaLength,
-							QObject::tr ("Length metadata"),
-							QObject::tr ("Length of the track being played."),
-							QVariant::Int,
-							{
-								AN::TypeIMEventTuneChange
-							}
+							AN::TypeIMEventTuneChange
 						}
-					}
-				},
-				{
-					AN::TypeIMEventActivityChange,
+					},
 					{
+						AN::Field::MediaAlbum,
+						QObject::tr ("Album metadata"),
+						QObject::tr ("Album of the track being played."),
+						QVariant::String,
 						{
-							AN::Field::IMActivityGeneral,
-							QObject::tr ("General activity"),
-							QObject::tr ("General activity of the contact."),
-							QVariant::String,
-							{
-								AN::TypeIMEventActivityChange
-							}
-						},
-						{
-							AN::Field::IMActivitySpecific,
-							QObject::tr ("Specific activity"),
-							QObject::tr ("Specific activity of the contact within the given general activity."),
-							QVariant::String,
-							{
-								AN::TypeIMEventActivityChange
-							}
-						},
-						{
-							AN::Field::IMActivityText,
-							QObject::tr ("Activity text"),
-							QObject::tr ("The comment set by the contact."),
-							QVariant::String,
-							{
-								AN::TypeIMEventActivityChange
-							}
+							AN::TypeIMEventTuneChange
 						}
-					}
-				},
-				{
-					AN::TypeIMEventMoodChange,
+					},
 					{
+						AN::Field::MediaArtist,
+						QObject::tr ("Artist metadata"),
+						QObject::tr ("Artist of the track being played."),
+						QVariant::String,
 						{
-							AN::Field::IMMoodGeneral,
-							QObject::tr ("Mood"),
-							QObject::tr ("The mood of the contact."),
-							QVariant::String,
-							{
-								AN::TypeIMEventMoodChange
-							}
-						},
-						{
-							AN::Field::IMMoodText,
-							QObject::tr ("Mood explanation text"),
-							QObject::tr ("The text accompanying the mood set by the contact."),
-							QVariant::String,
-							{
-								AN::TypeIMEventMoodChange
-							}
+							AN::TypeIMEventTuneChange
 						}
-					}
-				},
-				{
-					AN::TypeIMEventLocationChange,
+					},
 					{
+						AN::Field::MediaLength,
+						QObject::tr ("Length metadata"),
+						QObject::tr ("Length of the track being played."),
+						QVariant::Int,
 						{
-							AN::Field::IMLocationLatitude,
-							QObject::tr ("Latitude"),
-							QObject::tr ("The latitude of the contact's current position."),
-							QVariant::Double,
-							{
-								AN::TypeIMEventLocationChange
-							}
-						},
-						{
-							AN::Field::IMLocationLongitude,
-							QObject::tr ("Longitude"),
-							QObject::tr ("The longitude of the contact's current position."),
-							QVariant::Double,
-							{
-								AN::TypeIMEventLocationChange
-							}
-						},
-						{
-							AN::Field::IMLocationCountry,
-							QObject::tr ("Country"),
-							QObject::tr ("The country the contact is currently in."),
-							QVariant::String,
-							{
-								AN::TypeIMEventLocationChange
-							}
-						},
-						{
-							AN::Field::IMLocationLocality,
-							QObject::tr ("Longitude"),
-							QObject::tr ("The the exact locality (like a city or a town) the contact is currently in."),
-							QVariant::String,
-							{
-								AN::TypeIMEventLocationChange
-							}
-						}
-					}
-				},
-				{
-					AN::CatMediaPlayer,
-					{
-						{
-							AN::Field::MediaPlayerURL,
-							QObject::tr ("File URL"),
-							QObject::tr ("URL to the file being played."),
-							QVariant::Url,
-							{
-								AN::TypeMediaPlaybackStatus
-							}
-						},
-						{
-							AN::Field::MediaPlaybackStatus,
-							QObject::tr ("Playback status"),
-							QObject::tr ("The status of the currently playing media file."),
-							QVariant::String,
-							{
-								AN::TypeMediaPlaybackStatus
-							},
-							{
-								"Stopped",
-								"Paused",
-								"Playing"
-							}
-						},
-						{
-							AN::Field::MediaTitle,
-							QObject::tr ("Title metadata"),
-							QObject::tr ("Title of the track being played."),
-							QVariant::String,
-							{
-								AN::TypeMediaPlaybackStatus
-							}
-						},
-						{
-							AN::Field::MediaAlbum,
-							QObject::tr ("Album metadata"),
-							QObject::tr ("Album of the track being played."),
-							QVariant::String,
-							{
-								AN::TypeMediaPlaybackStatus
-							}
-						},
-						{
-							AN::Field::MediaArtist,
-							QObject::tr ("Artist metadata"),
-							QObject::tr ("Artist of the track being played."),
-							QVariant::String,
-							{
-								AN::TypeMediaPlaybackStatus
-							}
-						},
-						{
-							AN::Field::MediaLength,
-							QObject::tr ("Length metadata"),
-							QObject::tr ("Length of the track being played."),
-							QVariant::Int,
-							{
-								AN::TypeMediaPlaybackStatus
-							}
-						}
-					}
-				},
-				{
-					AN::CatTerminal,
-					{
-						{
-							AN::Field::TerminalActive,
-							QObject::tr ("Terminal is active"),
-							QObject::tr ("The terminal tab that caused the notification is active."),
-							QVariant::Bool,
-							{
-								AN::TypeTerminalBell,
-								AN::TypeTerminalActivity,
-								AN::TypeTerminalInactivity
-							}
+							AN::TypeIMEventTuneChange
 						}
 					}
 				}
-			});
+			},
+			{
+				AN::TypeIMEventActivityChange,
+				{
+					{
+						AN::Field::IMActivityGeneral,
+						QObject::tr ("General activity"),
+						QObject::tr ("General activity of the contact."),
+						QVariant::String,
+						{
+							AN::TypeIMEventActivityChange
+						}
+					},
+					{
+						AN::Field::IMActivitySpecific,
+						QObject::tr ("Specific activity"),
+						QObject::tr ("Specific activity of the contact within the given general activity."),
+						QVariant::String,
+						{
+							AN::TypeIMEventActivityChange
+						}
+					},
+					{
+						AN::Field::IMActivityText,
+						QObject::tr ("Activity text"),
+						QObject::tr ("The comment set by the contact."),
+						QVariant::String,
+						{
+							AN::TypeIMEventActivityChange
+						}
+					}
+				}
+			},
+			{
+				AN::TypeIMEventMoodChange,
+				{
+					{
+						AN::Field::IMMoodGeneral,
+						QObject::tr ("Mood"),
+						QObject::tr ("The mood of the contact."),
+						QVariant::String,
+						{
+							AN::TypeIMEventMoodChange
+						}
+					},
+					{
+						AN::Field::IMMoodText,
+						QObject::tr ("Mood explanation text"),
+						QObject::tr ("The text accompanying the mood set by the contact."),
+						QVariant::String,
+						{
+							AN::TypeIMEventMoodChange
+						}
+					}
+				}
+			},
+			{
+				AN::TypeIMEventLocationChange,
+				{
+					{
+						AN::Field::IMLocationLatitude,
+						QObject::tr ("Latitude"),
+						QObject::tr ("The latitude of the contact's current position."),
+						QVariant::Double,
+						{
+							AN::TypeIMEventLocationChange
+						}
+					},
+					{
+						AN::Field::IMLocationLongitude,
+						QObject::tr ("Longitude"),
+						QObject::tr ("The longitude of the contact's current position."),
+						QVariant::Double,
+						{
+							AN::TypeIMEventLocationChange
+						}
+					},
+					{
+						AN::Field::IMLocationCountry,
+						QObject::tr ("Country"),
+						QObject::tr ("The country the contact is currently in."),
+						QVariant::String,
+						{
+							AN::TypeIMEventLocationChange
+						}
+					},
+					{
+						AN::Field::IMLocationLocality,
+						QObject::tr ("Longitude"),
+						QObject::tr ("The the exact locality (like a city or a town) the contact is currently in."),
+						QVariant::String,
+						{
+							AN::TypeIMEventLocationChange
+						}
+					}
+				}
+			},
+			{
+				AN::CatMediaPlayer,
+				{
+					{
+						AN::Field::MediaPlayerURL,
+						QObject::tr ("File URL"),
+						QObject::tr ("URL to the file being played."),
+						QVariant::Url,
+						{
+							AN::TypeMediaPlaybackStatus
+						}
+					},
+					{
+						AN::Field::MediaPlaybackStatus,
+						QObject::tr ("Playback status"),
+						QObject::tr ("The status of the currently playing media file."),
+						QVariant::String,
+						{
+							AN::TypeMediaPlaybackStatus
+						},
+						{
+							"Stopped",
+							"Paused",
+							"Playing"
+						}
+					},
+					{
+						AN::Field::MediaTitle,
+						QObject::tr ("Title metadata"),
+						QObject::tr ("Title of the track being played."),
+						QVariant::String,
+						{
+							AN::TypeMediaPlaybackStatus
+						}
+					},
+					{
+						AN::Field::MediaAlbum,
+						QObject::tr ("Album metadata"),
+						QObject::tr ("Album of the track being played."),
+						QVariant::String,
+						{
+							AN::TypeMediaPlaybackStatus
+						}
+					},
+					{
+						AN::Field::MediaArtist,
+						QObject::tr ("Artist metadata"),
+						QObject::tr ("Artist of the track being played."),
+						QVariant::String,
+						{
+							AN::TypeMediaPlaybackStatus
+						}
+					},
+					{
+						AN::Field::MediaLength,
+						QObject::tr ("Length metadata"),
+						QObject::tr ("Length of the track being played."),
+						QVariant::Int,
+						{
+							AN::TypeMediaPlaybackStatus
+						}
+					}
+				}
+			},
+			{
+				AN::CatTerminal,
+				{
+					{
+						AN::Field::TerminalActive,
+						QObject::tr ("Terminal is active"),
+						QObject::tr ("The terminal tab that caused the notification is active."),
+						QVariant::Bool,
+						{
+							AN::TypeTerminalBell,
+							AN::TypeTerminalActivity,
+							AN::TypeTerminalInactivity
+						}
+					}
+				}
+			}
+		};
 
 		if (!type.isEmpty ())
 			return values.value (type);
