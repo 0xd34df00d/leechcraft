0973f8a3e51e878baadb9808b40d0d3c10869b25
0xd34df00d <0xd34df00d@gmail.com>
2017-07-29 13:28:05 -0400

    [Poshuku QRd] Updated translations.

--- src/plugins/poshuku/plugins/qrd/leechcraft_poshuku_qrd_en.ts
+++ src/plugins/poshuku/plugins/qrd/leechcraft_poshuku_qrd_en.ts
@@ -1,27 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LeechCraft::Poshuku::QRd::Plugin</name>
     <message>
-        <location filename="qrd.cpp" line="68"/>
+        <location filename="qrd.cpp" line="72"/>
         <source>Shows the QR code with the URL of an opened web page.</source>
         <translation>Shows the QR code with the URL of an opened web page.</translation>
     </message>
     <message>
-        <location filename="qrd.cpp" line="94"/>
+        <location filename="qrd.cpp" line="97"/>
         <source>Generate QR code...</source>
         <translation>Generate QR code...</translation>
     </message>
     <message>
-        <location filename="qrd.cpp" line="114"/>
+        <location filename="qrd.cpp" line="104"/>
+        <source>Generate QR code for the link...</source>
+        <translation>Generate QR code for the link...</translation>
+    </message>
+    <message>
+        <location filename="qrd.cpp" line="126"/>
         <source>Failed to generate QR code for the page.</source>
         <translation>Failed to generate QR code for the page.</translation>
     </message>
     <message>
-        <location filename="qrd.cpp" line="132"/>
+        <location filename="qrd.cpp" line="149"/>
         <source>Sorry, but the QR code is bigger than your display.</source>
         <translation>Sorry, but the QR code is bigger than your display.</translation>
+    </message>
+    <message>
+        <location filename="qrd.cpp" line="158"/>
+        <source>QR code for %1</source>
+        <translation>QR code for %1</translation>
     </message>
 </context>
 </TS>
--- src/plugins/poshuku/plugins/qrd/leechcraft_poshuku_qrd_ru_RU.ts
+++ src/plugins/poshuku/plugins/qrd/leechcraft_poshuku_qrd_ru_RU.ts
@@ -1,27 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LeechCraft::Poshuku::QRd::Plugin</name>
     <message>
-        <location filename="qrd.cpp" line="68"/>
+        <location filename="qrd.cpp" line="72"/>
         <source>Shows the QR code with the URL of an opened web page.</source>
         <translation>Отображает QR-код с адресом открытой страницы.</translation>
     </message>
     <message>
-        <location filename="qrd.cpp" line="94"/>
+        <location filename="qrd.cpp" line="97"/>
         <source>Generate QR code...</source>
         <translation>Сгенерировать QR-код…</translation>
     </message>
     <message>
-        <location filename="qrd.cpp" line="114"/>
+        <location filename="qrd.cpp" line="104"/>
+        <source>Generate QR code for the link...</source>
+        <translation>Сгенерировать QR-код для ссылки…</translation>
+    </message>
+    <message>
+        <location filename="qrd.cpp" line="126"/>
         <source>Failed to generate QR code for the page.</source>
         <translation>Не удалось создать QR-код для этой страницы.</translation>
     </message>
     <message>
-        <location filename="qrd.cpp" line="132"/>
+        <location filename="qrd.cpp" line="149"/>
         <source>Sorry, but the QR code is bigger than your display.</source>
         <translation>Очень жаль, но, похоже, QR-код получается больше, чем ваш экран.</translation>
+    </message>
+    <message>
+        <location filename="qrd.cpp" line="158"/>
+        <source>QR code for %1</source>
+        <translation>QR-код для %1</translation>
     </message>
 </context>
 </TS>
