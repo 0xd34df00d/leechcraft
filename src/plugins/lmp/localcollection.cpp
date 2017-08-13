af7a4f70b834bdcb4c5ba8bd6138b2af179d4d23
0xd34df00d <0xd34df00d@gmail.com>
2017-08-12 23:33:08 -0400

    [LMP] Use Levenshtein distance for equality checks.
    Helps to dedup some of the tracks from different albums.

--- src/plugins/lmp/localcollection.cpp
+++ src/plugins/lmp/localcollection.cpp
@@ -478,6 +478,66 @@ namespace LMP
 			}
 		}
 
+		uint8_t LevDist (const QString& str1, const QString& str2)
+		{
+			const auto len1 = str1.size ();
+			const auto len2 = str2.size ();
+
+			const auto max = std::numeric_limits<uint8_t>::max ();
+			if (len1 >= max || len2 >= max)
+				return max;
+
+			std::vector<uint8_t> lastColumn (len2 + 1);
+
+			auto prevColumn = lastColumn;
+			std::iota (prevColumn.begin (), prevColumn.end (), 0);
+
+			for (int i = 0; i < len1; ++i)
+			{
+				lastColumn [0] = i + 1;
+				for (int j = 0; j < len2; ++j)
+					lastColumn [j + 1] = std::min ({
+							prevColumn [1 + j] + 1,
+							lastColumn [j] + 1,
+							prevColumn [j] + (str1 [i] != str2 [j])
+						});
+
+				using std::swap;
+				swap (lastColumn, prevColumn);
+			}
+
+			return prevColumn [len2];
+		}
+
+		/* Note that operator== and operator< aren't fully consistent.
+		 * Presumably that's OK as operator< is used to sort the list of
+		 * tracks, and only then operator== is used to remove duplicates
+		 * in that list. operator<'s induced equality is more strict, and
+		 * if the proxied strings are compared last, chances are they are
+		 * effectively equal from the user's point of view if they are next
+		 * to each other in the track list, as all other fields like track
+		 * number or length should match for this. In this particular case
+		 * it's safe for operator== to induce a more coarse equivalence
+		 * relation.
+		 */
+		struct LevProxy
+		{
+			const QString& Str_;
+
+			bool operator== (const LevProxy& other) const
+			{
+				if (Str_ == other.Str_)
+					return true;
+
+				return LevDist (Str_, other.Str_) <= 3;
+			}
+
+			bool operator< (const LevProxy& other) const
+			{
+				return Str_ < other.Str_;
+			}
+		};
+
 		bool UniteSplitTryMerge (Collection::Artists_t& artists, const QList<Collection::Album_ptr>& albumsSet)
 		{
 			DumpAlbumsSet (albumsSet, "initial state");
@@ -505,7 +565,7 @@ namespace LMP
 			for (const auto& album : albumsSet)
 			{
 				const auto toTuple = [] (const auto& track)
-						{ return std::make_tuple (track.Number_, track.Name_, track.Length_); };
+						{ return std::make_tuple (track.Number_, track.Length_, LevProxy { track.Name_ }); };
 
 				std::sort (album->Tracks_.begin (), album->Tracks_.end (), Util::ComparingBy (toTuple));
 				const auto unique = std::unique (album->Tracks_.begin (), album->Tracks_.end (),
