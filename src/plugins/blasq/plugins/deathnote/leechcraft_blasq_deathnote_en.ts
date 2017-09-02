840bca03a61e3c614d3be4263d5ef6390da600f6
0xd34df00d <0xd34df00d@gmail.com>
2017-09-02 13:53:07 -0400

    [Blasq DN] Updated translations.

--- src/plugins/blasq/plugins/deathnote/leechcraft_blasq_deathnote_en.ts
+++ src/plugins/blasq/plugins/deathnote/leechcraft_blasq_deathnote_en.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>AlbumSettingsDialog</name>
     <message>
@@ -37,23 +37,18 @@
 <context>
     <name>LeechCraft::Blasq::DeathNote::FotoBilderAccount</name>
     <message>
-        <location filename="fotobilderaccount.cpp" line="75"/>
-        <location filename="fotobilderaccount.cpp" line="769"/>
+        <location filename="fotobilderaccount.cpp" line="78"/>
+        <location filename="fotobilderaccount.cpp" line="728"/>
         <source>Name</source>
         <translation>Name</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="144"/>
-        <source>Enter password</source>
-        <translation>Enter password</translation>
-    </message>
-    <message>
-        <location filename="fotobilderaccount.cpp" line="308"/>
+        <location filename="fotobilderaccount.cpp" line="314"/>
         <source>%1 (original message: %2)</source>
         <translation>%1 (original message: %2)</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="771"/>
+        <location filename="fotobilderaccount.cpp" line="730"/>
         <source>All photos</source>
         <translation>All photos</translation>
     </message>
@@ -61,7 +56,7 @@
 <context>
     <name>LeechCraft::Blasq::DeathNote::Plugin</name>
     <message>
-        <location filename="deathnote.cpp" line="75"/>
+        <location filename="deathnote.cpp" line="68"/>
         <source>LiveJournal FotoBilder support module for Blasq.</source>
         <translation>LiveJournal FotoBilder support module for Blasq.</translation>
     </message>
@@ -73,109 +68,109 @@
         <source>Groups</source>
         <translation>Groups</translation>
     </message>
-    <message>
-        <location filename="selectgroupsdialog.cpp" line="78"/>
-        <source>Enter password</source>
-        <translation>Enter password</translation>
-    </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="fotobilderaccount.cpp" line="236"/>
+        <location filename="fotobilderaccount.cpp" line="226"/>
         <source>User error</source>
         <translation>User error</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="238"/>
+        <location filename="fotobilderaccount.cpp" line="228"/>
         <source>No user specified</source>
         <translation>No user specified</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="240"/>
+        <location filename="fotobilderaccount.cpp" line="230"/>
         <source>Invalid user</source>
         <translation>Invalid user</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="242"/>
+        <location filename="fotobilderaccount.cpp" line="232"/>
         <source>Unknown user</source>
         <translation>Unknown user</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="244"/>
+        <location filename="fotobilderaccount.cpp" line="234"/>
         <source>Client error</source>
         <translation>Client error</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="246"/>
+        <location filename="fotobilderaccount.cpp" line="236"/>
         <source>Invalid mode</source>
         <translation>Invalid mode</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="248"/>
+        <location filename="fotobilderaccount.cpp" line="238"/>
         <source>Invalid argument</source>
         <translation>Invalid argument</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="250"/>
+        <location filename="fotobilderaccount.cpp" line="240"/>
         <source>Missing required argument</source>
         <translation>Missing required argument</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="252"/>
+        <location filename="fotobilderaccount.cpp" line="242"/>
         <source>Invalid image for upload</source>
         <translation>Invalid image for upload</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="254"/>
+        <location filename="fotobilderaccount.cpp" line="244"/>
         <source>Access error</source>
         <translation>Access error</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="256"/>
+        <location filename="fotobilderaccount.cpp" line="246"/>
         <source>No auth specified</source>
         <translation>No auth specified</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="258"/>
+        <location filename="fotobilderaccount.cpp" line="248"/>
         <source>Invalid auth</source>
         <translation>Invalid auth</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="260"/>
+        <location filename="fotobilderaccount.cpp" line="250"/>
         <source>Account status does not allow upload</source>
         <translation>Account status does not allow upload</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="262"/>
+        <location filename="fotobilderaccount.cpp" line="252"/>
         <source>Limit error</source>
         <translation>Limit error</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="264"/>
+        <location filename="fotobilderaccount.cpp" line="254"/>
         <source>No disk space remaining</source>
         <translation>No disk space remaining</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="266"/>
+        <location filename="fotobilderaccount.cpp" line="256"/>
         <source>Insufficient disk space remaining</source>
         <translation>Insufficient disk space remaining</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="268"/>
+        <location filename="fotobilderaccount.cpp" line="258"/>
         <source>Internal Server Error</source>
         <translation>Internal Server Error</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="270"/>
+        <location filename="fotobilderaccount.cpp" line="260"/>
         <source>Error creating pic</source>
         <oldsource>Error creating gpic</oldsource>
         <translation>Error creating pic</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="272"/>
+        <location filename="fotobilderaccount.cpp" line="262"/>
         <source>Error creating gallery</source>
         <translation>Error creating gallery</translation>
+    </message>
+    <message>
+        <location filename="util.cpp" line="52"/>
+        <source>Enter password for LiveJournal FotoBilder account %1:</source>
+        <translation>Enter password for LiveJournal FotoBilder account %1:</translation>
     </message>
 </context>
 <context>
--- src/plugins/blasq/plugins/deathnote/leechcraft_blasq_deathnote_ru_RU.ts
+++ src/plugins/blasq/plugins/deathnote/leechcraft_blasq_deathnote_ru_RU.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>AlbumSettingsDialog</name>
     <message>
@@ -37,23 +37,18 @@
 <context>
     <name>LeechCraft::Blasq::DeathNote::FotoBilderAccount</name>
     <message>
-        <location filename="fotobilderaccount.cpp" line="75"/>
-        <location filename="fotobilderaccount.cpp" line="769"/>
+        <location filename="fotobilderaccount.cpp" line="78"/>
+        <location filename="fotobilderaccount.cpp" line="728"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="144"/>
-        <source>Enter password</source>
-        <translation>Введите пароль</translation>
-    </message>
-    <message>
-        <location filename="fotobilderaccount.cpp" line="308"/>
+        <location filename="fotobilderaccount.cpp" line="314"/>
         <source>%1 (original message: %2)</source>
         <translation>%1 (оригинальное сообщение: %2)</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="771"/>
+        <location filename="fotobilderaccount.cpp" line="730"/>
         <source>All photos</source>
         <translation>Все фото</translation>
     </message>
@@ -61,7 +56,7 @@
 <context>
     <name>LeechCraft::Blasq::DeathNote::Plugin</name>
     <message>
-        <location filename="deathnote.cpp" line="75"/>
+        <location filename="deathnote.cpp" line="68"/>
         <source>LiveJournal FotoBilder support module for Blasq.</source>
         <translation>Модуль поддержки LiveJournal FotoBilder для Blasq.</translation>
     </message>
@@ -73,109 +68,109 @@
         <source>Groups</source>
         <translation>Группы</translation>
     </message>
-    <message>
-        <location filename="selectgroupsdialog.cpp" line="78"/>
-        <source>Enter password</source>
-        <translation>Введите пароль</translation>
-    </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="fotobilderaccount.cpp" line="236"/>
+        <location filename="fotobilderaccount.cpp" line="226"/>
         <source>User error</source>
         <translation>Ошибка пользователя</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="238"/>
+        <location filename="fotobilderaccount.cpp" line="228"/>
         <source>No user specified</source>
         <translation>Пользователь не указан</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="240"/>
+        <location filename="fotobilderaccount.cpp" line="230"/>
         <source>Invalid user</source>
         <translation>Неправильный пользователь</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="242"/>
+        <location filename="fotobilderaccount.cpp" line="232"/>
         <source>Unknown user</source>
         <translation>Неизвестный пользователь</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="244"/>
+        <location filename="fotobilderaccount.cpp" line="234"/>
         <source>Client error</source>
         <translation>Ошибка клиента</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="246"/>
+        <location filename="fotobilderaccount.cpp" line="236"/>
         <source>Invalid mode</source>
         <translation>Неправильный режим</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="248"/>
+        <location filename="fotobilderaccount.cpp" line="238"/>
         <source>Invalid argument</source>
         <translation>Неправильный аргумент</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="250"/>
+        <location filename="fotobilderaccount.cpp" line="240"/>
         <source>Missing required argument</source>
         <translation>Отсутствует параметр аргумента</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="252"/>
+        <location filename="fotobilderaccount.cpp" line="242"/>
         <source>Invalid image for upload</source>
         <translation>Неправильное изображение для загрузки</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="254"/>
+        <location filename="fotobilderaccount.cpp" line="244"/>
         <source>Access error</source>
         <translation>Ошибка доступа</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="256"/>
+        <location filename="fotobilderaccount.cpp" line="246"/>
         <source>No auth specified</source>
         <translation>Не указаны данные аутентификации</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="258"/>
+        <location filename="fotobilderaccount.cpp" line="248"/>
         <source>Invalid auth</source>
         <translation>Неправильные данные аутентификации</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="260"/>
+        <location filename="fotobilderaccount.cpp" line="250"/>
         <source>Account status does not allow upload</source>
         <translation>Статус учётной записи не допускает загрузки изображений</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="262"/>
+        <location filename="fotobilderaccount.cpp" line="252"/>
         <source>Limit error</source>
         <translation>Ошибка ограничений</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="264"/>
+        <location filename="fotobilderaccount.cpp" line="254"/>
         <source>No disk space remaining</source>
         <translation>Нет места на диске</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="266"/>
+        <location filename="fotobilderaccount.cpp" line="256"/>
         <source>Insufficient disk space remaining</source>
         <translation>Недостаточно места на диске</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="268"/>
+        <location filename="fotobilderaccount.cpp" line="258"/>
         <source>Internal Server Error</source>
         <translation>Внутренняя ошибка сервера</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="270"/>
+        <location filename="fotobilderaccount.cpp" line="260"/>
         <source>Error creating pic</source>
         <oldsource>Error creating gpic</oldsource>
         <translation>Ошибка создания изображения</translation>
     </message>
     <message>
-        <location filename="fotobilderaccount.cpp" line="272"/>
+        <location filename="fotobilderaccount.cpp" line="262"/>
         <source>Error creating gallery</source>
         <translation>Ошибка создания галереи</translation>
+    </message>
+    <message>
+        <location filename="util.cpp" line="52"/>
+        <source>Enter password for LiveJournal FotoBilder account %1:</source>
+        <translation>Введите пароль для учётной записи %1 сервиса LiveJournal FotoBilder:</translation>
     </message>
 </context>
 <context>
