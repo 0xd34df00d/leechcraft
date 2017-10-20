97effddc76a828d42eae96ca85e01177044f2f96
0xd34df00d <0xd34df00d@gmail.com>
2017-10-19 23:39:35 -0400

    [Tools] Better error handling.

--- tools/docker/build_bases.sh
+++ tools/docker/build_bases.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
-for i in `ls base`; do docker build --force-rm --no-cache -t leechcraft/ci_$i:latest $i; docker push leechcraft/ci_$i:latest; done
+set -e
+
+for i in `ls base`; do docker build --force-rm --no-cache -t leechcraft/ci_$i:latest $i && docker push leechcraft/ci_$i:latest; done
