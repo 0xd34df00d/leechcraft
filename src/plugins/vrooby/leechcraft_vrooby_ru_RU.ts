753dbe3b2f8f8f9796bcc275fd72a98555eb5de6
0xd34df00d <0xd34df00d@gmail.com>
2017-10-26 22:02:40 -0400

    [Vrooby] Updated translations.

--- src/plugins/vrooby/leechcraft_vrooby_ru_RU.ts
+++ src/plugins/vrooby/leechcraft_vrooby_ru_RU.ts
@@ -1,20 +1,20 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LeechCraft::Vrooby::FlatMountableItems</name>
     <message>
-        <location filename="flatmountableitems.cpp" line="56"/>
+        <location filename="flatmountableitems.cpp" line="69"/>
         <source>total size: %1</source>
         <translation>общий размер: %1</translation>
     </message>
     <message>
-        <location filename="flatmountableitems.cpp" line="62"/>
+        <location filename="flatmountableitems.cpp" line="75"/>
         <source>available size: %1</source>
         <translation>доступно: %1</translation>
     </message>
     <message>
-        <location filename="flatmountableitems.cpp" line="74"/>
+        <location filename="flatmountableitems.cpp" line="87"/>
         <source>Mounted at %1</source>
         <translation>Смонтировано на %1</translation>
     </message>
@@ -22,12 +22,17 @@
 <context>
     <name>LeechCraft::Vrooby::Plugin</name>
     <message>
-        <location filename="vrooby.cpp" line="47"/>
+        <location filename="vrooby.cpp" line="87"/>
+        <source>No backends are available, tried the following: %1.</source>
+        <translation>Нет доступных платформ управления съёмными носителями, были испробованы: %1.</translation>
+    </message>
+    <message>
+        <location filename="vrooby.cpp" line="173"/>
         <source>Removable devices...</source>
         <translation>Съемные накопители...</translation>
     </message>
     <message>
-        <location filename="vrooby.cpp" line="80"/>
+        <location filename="vrooby.cpp" line="126"/>
         <source>Removable storage devices manager for LeechCraft.</source>
         <translation>Менеджер съемных накопителей данных для LeechCraft.</translation>
     </message>
@@ -35,7 +40,7 @@
 <context>
     <name>LeechCraft::Vrooby::TrayView</name>
     <message>
-        <location filename="trayview.cpp" line="72"/>
+        <location filename="trayview.cpp" line="156"/>
         <source>Removable devices</source>
         <translation>Съемные накопители</translation>
     </message>
