03503489eae7a3ece5a99a37ed235cf41515a456
0xd34df00d <0xd34df00d@gmail.com>
2017-10-28 22:53:12 -0400

    [Azoth] Initialize the mood list just once.

--- src/plugins/azoth/mooddialog.cpp
+++ src/plugins/azoth/mooddialog.cpp
@@ -87,106 +87,110 @@ namespace Azoth
 
 	QMap<QString, QPair<QVariant, QIcon>> MoodDialog::BuildHumanReadableList ()
 	{
-		const char* moodStr[] =
+		static const auto list = []
 		{
-			QT_TR_NOOP ("afraid"),
-			QT_TR_NOOP ("amazed"),
-			QT_TR_NOOP ("amorous"),
-			QT_TR_NOOP ("angry"),
-			QT_TR_NOOP ("annoyed"),
-			QT_TR_NOOP ("anxious"),
-			QT_TR_NOOP ("aroused"),
-			QT_TR_NOOP ("ashamed"),
-			QT_TR_NOOP ("bored"),
-			QT_TR_NOOP ("brave"),
-			QT_TR_NOOP ("calm"),
-			QT_TR_NOOP ("cautious"),
-			QT_TR_NOOP ("cold"),
-			QT_TR_NOOP ("confident"),
-			QT_TR_NOOP ("confused"),
-			QT_TR_NOOP ("contemplative"),
-			QT_TR_NOOP ("contented"),
-			QT_TR_NOOP ("cranky"),
-			QT_TR_NOOP ("crazy"),
-			QT_TR_NOOP ("creative"),
-			QT_TR_NOOP ("curious"),
-			QT_TR_NOOP ("dejected"),
-			QT_TR_NOOP ("depressed"),
-			QT_TR_NOOP ("disappointed"),
-			QT_TR_NOOP ("disgusted"),
-			QT_TR_NOOP ("dismayed"),
-			QT_TR_NOOP ("distracted"),
-			QT_TR_NOOP ("embarrassed"),
-			QT_TR_NOOP ("envious"),
-			QT_TR_NOOP ("excited"),
-			QT_TR_NOOP ("flirtatious"),
-			QT_TR_NOOP ("frustrated"),
-			QT_TR_NOOP ("grateful"),
-			QT_TR_NOOP ("grieving"),
-			QT_TR_NOOP ("grumpy"),
-			QT_TR_NOOP ("guilty"),
-			QT_TR_NOOP ("happy"),
-			QT_TR_NOOP ("hopeful"),
-			QT_TR_NOOP ("hot"),
-			QT_TR_NOOP ("humbled"),
-			QT_TR_NOOP ("humiliated"),
-			QT_TR_NOOP ("hungry"),
-			QT_TR_NOOP ("hurt"),
-			QT_TR_NOOP ("impressed"),
-			QT_TR_NOOP ("in_awe"),
-			QT_TR_NOOP ("in_love"),
-			QT_TR_NOOP ("indignant"),
-			QT_TR_NOOP ("interested"),
-			QT_TR_NOOP ("intoxicated"),
-			QT_TR_NOOP ("invincible"),
-			QT_TR_NOOP ("jealous"),
-			QT_TR_NOOP ("lonely"),
-			QT_TR_NOOP ("lost"),
-			QT_TR_NOOP ("lucky"),
-			QT_TR_NOOP ("mean"),
-			QT_TR_NOOP ("moody"),
-			QT_TR_NOOP ("nervous"),
-			QT_TR_NOOP ("neutral"),
-			QT_TR_NOOP ("offended"),
-			QT_TR_NOOP ("outraged"),
-			QT_TR_NOOP ("playful"),
-			QT_TR_NOOP ("proud"),
-			QT_TR_NOOP ("relaxed"),
-			QT_TR_NOOP ("relieved"),
-			QT_TR_NOOP ("remorseful"),
-			QT_TR_NOOP ("restless"),
-			QT_TR_NOOP ("sad"),
-			QT_TR_NOOP ("sarcastic"),
-			QT_TR_NOOP ("satisfied"),
-			QT_TR_NOOP ("serious"),
-			QT_TR_NOOP ("shocked"),
-			QT_TR_NOOP ("shy"),
-			QT_TR_NOOP ("sick"),
-			QT_TR_NOOP ("sleepy"),
-			QT_TR_NOOP ("spontaneous"),
-			QT_TR_NOOP ("stressed"),
-			QT_TR_NOOP ("strong"),
-			QT_TR_NOOP ("surprised"),
-			QT_TR_NOOP ("thankful"),
-			QT_TR_NOOP ("thirsty"),
-			QT_TR_NOOP ("tired"),
-			QT_TR_NOOP ("undefined"),
-			QT_TR_NOOP ("weak"),
-			QT_TR_NOOP ("worried")
-		};
+			const char* moodStr[] =
+			{
+				QT_TR_NOOP ("afraid"),
+				QT_TR_NOOP ("amazed"),
+				QT_TR_NOOP ("amorous"),
+				QT_TR_NOOP ("angry"),
+				QT_TR_NOOP ("annoyed"),
+				QT_TR_NOOP ("anxious"),
+				QT_TR_NOOP ("aroused"),
+				QT_TR_NOOP ("ashamed"),
+				QT_TR_NOOP ("bored"),
+				QT_TR_NOOP ("brave"),
+				QT_TR_NOOP ("calm"),
+				QT_TR_NOOP ("cautious"),
+				QT_TR_NOOP ("cold"),
+				QT_TR_NOOP ("confident"),
+				QT_TR_NOOP ("confused"),
+				QT_TR_NOOP ("contemplative"),
+				QT_TR_NOOP ("contented"),
+				QT_TR_NOOP ("cranky"),
+				QT_TR_NOOP ("crazy"),
+				QT_TR_NOOP ("creative"),
+				QT_TR_NOOP ("curious"),
+				QT_TR_NOOP ("dejected"),
+				QT_TR_NOOP ("depressed"),
+				QT_TR_NOOP ("disappointed"),
+				QT_TR_NOOP ("disgusted"),
+				QT_TR_NOOP ("dismayed"),
+				QT_TR_NOOP ("distracted"),
+				QT_TR_NOOP ("embarrassed"),
+				QT_TR_NOOP ("envious"),
+				QT_TR_NOOP ("excited"),
+				QT_TR_NOOP ("flirtatious"),
+				QT_TR_NOOP ("frustrated"),
+				QT_TR_NOOP ("grateful"),
+				QT_TR_NOOP ("grieving"),
+				QT_TR_NOOP ("grumpy"),
+				QT_TR_NOOP ("guilty"),
+				QT_TR_NOOP ("happy"),
+				QT_TR_NOOP ("hopeful"),
+				QT_TR_NOOP ("hot"),
+				QT_TR_NOOP ("humbled"),
+				QT_TR_NOOP ("humiliated"),
+				QT_TR_NOOP ("hungry"),
+				QT_TR_NOOP ("hurt"),
+				QT_TR_NOOP ("impressed"),
+				QT_TR_NOOP ("in_awe"),
+				QT_TR_NOOP ("in_love"),
+				QT_TR_NOOP ("indignant"),
+				QT_TR_NOOP ("interested"),
+				QT_TR_NOOP ("intoxicated"),
+				QT_TR_NOOP ("invincible"),
+				QT_TR_NOOP ("jealous"),
+				QT_TR_NOOP ("lonely"),
+				QT_TR_NOOP ("lost"),
+				QT_TR_NOOP ("lucky"),
+				QT_TR_NOOP ("mean"),
+				QT_TR_NOOP ("moody"),
+				QT_TR_NOOP ("nervous"),
+				QT_TR_NOOP ("neutral"),
+				QT_TR_NOOP ("offended"),
+				QT_TR_NOOP ("outraged"),
+				QT_TR_NOOP ("playful"),
+				QT_TR_NOOP ("proud"),
+				QT_TR_NOOP ("relaxed"),
+				QT_TR_NOOP ("relieved"),
+				QT_TR_NOOP ("remorseful"),
+				QT_TR_NOOP ("restless"),
+				QT_TR_NOOP ("sad"),
+				QT_TR_NOOP ("sarcastic"),
+				QT_TR_NOOP ("satisfied"),
+				QT_TR_NOOP ("serious"),
+				QT_TR_NOOP ("shocked"),
+				QT_TR_NOOP ("shy"),
+				QT_TR_NOOP ("sick"),
+				QT_TR_NOOP ("sleepy"),
+				QT_TR_NOOP ("spontaneous"),
+				QT_TR_NOOP ("stressed"),
+				QT_TR_NOOP ("strong"),
+				QT_TR_NOOP ("surprised"),
+				QT_TR_NOOP ("thankful"),
+				QT_TR_NOOP ("thirsty"),
+				QT_TR_NOOP ("tired"),
+				QT_TR_NOOP ("undefined"),
+				QT_TR_NOOP ("weak"),
+				QT_TR_NOOP ("worried")
+			};
 
-		const auto& theme = XmlSettingsManager::Instance ().property ("MoodIcons").toString () + '/';
-		const auto rl = ResourcesManager::Instance ().GetResourceLoader (ResourcesManager::RLTMoodIconLoader);
+			const auto& theme = XmlSettingsManager::Instance ().property ("MoodIcons").toString () + '/';
+			const auto rl = ResourcesManager::Instance ().GetResourceLoader (ResourcesManager::RLTMoodIconLoader);
 
-		QMap<QString, QPair<QVariant, QIcon>> list;
-		for (uint i = 0; i < sizeof (moodStr) / sizeof (moodStr [0]); ++i)
-		{
-			QString name (moodStr [i]);
-			name [0] = name.at (0).toUpper ();
-			QIcon icon (rl->GetIconPath (theme + name));
+			QMap<QString, QPair<QVariant, QIcon>> list;
+			for (uint i = 0; i < sizeof (moodStr) / sizeof (moodStr [0]); ++i)
+			{
+				QString name (moodStr [i]);
+				name [0] = name.at (0).toUpper ();
+				QIcon icon (rl->GetIconPath (theme + name));
 
-			list [tr (moodStr [i])] = qMakePair<QVariant, QIcon> (QString (moodStr [i]), icon);
-		}
+				list [tr (moodStr [i])] = qMakePair<QVariant, QIcon> (QString (moodStr [i]), icon);
+			}
+			return list;
+		} ();
 		return list;
 	}
 }
