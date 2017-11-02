a722764974495b227fe5873102fc7fb68324d0a3
0xd34df00d <0xd34df00d@gmail.com>
2017-11-02 01:53:30 -0400

    [Util] Delay type erasure in MakeInserter().

--- src/util/db/oral.h
+++ src/util/db/oral.h
@@ -390,7 +390,7 @@ namespace oral
 		};
 
 		template<typename T>
-		std::function<void (T)> MakeInserter (CachedFieldsData data, QSqlQuery_ptr insertQuery, bool bindPrimaryKey)
+		auto MakeInserter (CachedFieldsData data, QSqlQuery_ptr insertQuery, bool bindPrimaryKey)
 		{
 			return [data, insertQuery, bindPrimaryKey] (const T& t)
 			{
