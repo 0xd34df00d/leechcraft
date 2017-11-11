dd740f7b0cc05654367b44fdfd7096fb651ae6c1
0xd34df00d <0xd34df00d@gmail.com>
2017-11-11 15:08:50 -0500

    [Poshuku DCAC] Retain all bench points in the parser.

--- src/plugins/poshuku/plugins/dcac/benchana/src/Data/BenchAna/Parser.hs
+++ src/plugins/poshuku/plugins/dcac/benchana/src/Data/BenchAna/Parser.hs
@@ -8,12 +8,13 @@ import qualified Data.Attoparsec.Combinator as AT
 import qualified Data.Map as M
 import Control.Arrow
 import Data.Functor
+import Data.Monoid
 import Data.Maybe
 
 import Data.BenchAna.Types
 
 parse :: BS.ByteString -> BenchResults
-parse = M.fromList . map (second pure) . mapMaybe parseLine . BS.lines
+parse = M.fromListWith (<>) . map (second pure) . mapMaybe parseLine . BS.lines
 
 parseLine :: BS.ByteString -> Maybe (BenchConfig, Int)
 parseLine line | Right r <- AT.parseOnly lineParser line = Just r
