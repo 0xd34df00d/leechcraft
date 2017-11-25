ffac55ad2653e5bcca2b37f462fed8133d14b63a
0xd34df00d <0xd34df00d@gmail.com>
2017-11-25 17:24:58 -0500

    [Overall] Remove explicit CMP0003 policy setting.
    The new behaviour will be enabled by default given the minimum required
    cmake version.

--- src/InitLCPlugin.cmake
+++ src/InitLCPlugin.cmake
@@ -1,5 +1,4 @@
 if (NOT LEECHCRAFT_LIBRARIES)
-	cmake_policy (SET CMP0003 NEW)
 
 	set (CMAKE_MODULE_PATH "/usr/local/share/leechcraft/cmake;/usr/share/leechcraft/cmake;${CMAKE_MODULE_PATH}")
 	set (CMAKE_MODULE_PATH "/usr/local/share/apps/cmake/modules;/usr/share/apps/cmake/modules;${CMAKE_MODULE_PATH}")
