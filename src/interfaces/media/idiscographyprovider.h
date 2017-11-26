3651ee79cda5108dfa610b6e9f572d6819c178ff
0xd34df00d <0xd34df00d@gmail.com>
2017-11-26 17:03:12 -0500

    [Interfaces] QFuture-based interface for IDiscographyProvider.

--- src/interfaces/media/idiscographyprovider.h
+++ src/interfaces/media/idiscographyprovider.h
@@ -29,7 +29,11 @@
 
 #pragma once
 
+#include <util/sll/either.h>
 #include "audiostructs.h"
+
+template<typename>
+class QFuture;
 
 namespace Media
 {
@@ -112,70 +116,6 @@ namespace Media
 		QList<QList<ReleaseTrackInfo>> TrackInfos_;
 	};
 
-	/** @brief Pending discography request handle.
-	 *
-	 * Interface to a pending discography search in a
-	 * IDiscographyProvider. A descendant of this class is returned from
-	 * IDiscographyProvider::GetDiscography() or
-	 * IDiscographyProvider::GetReleaseInfo(). In the former case
-	 * GetReleases() contains all releases of the given artist, while in
-	 * the latter — only those matching the requested release.
-	 *
-	 * This class has some signals (ready() and error()), and one can use
-	 * the GetQObject() method to get an object of this class as a
-	 * QObject and connect to those signals.
-	 *
-	 * @note The object of this class should schedule its deletion (via
-	 * <code>QObject::deleteLater()</code>, for example) after ready() or
-	 * error() signal is emitted. Thus the calling code should never
-	 * delete it explicitly, neither it should use this object after
-	 * ready() or error() signals or connect to this signals via
-	 * <code>Qt::QueuedConnection</code>.
-	 *
-	 * @sa IDiscographyProvider
-	 */
-	class Q_DECL_EXPORT IPendingDisco
-	{
-	public:
-		virtual ~IPendingDisco () {}
-
-		/** @brief Returns this object as a QObject.
-		 *
-		 * This function can be used to connect to the signals of this
-		 * class.
-		 *
-		 * @return This object as a QObject.
-		 */
-		virtual QObject* GetQObject () = 0;
-
-		/** @brief Returns the list of found releases.
-		 *
-		 * This function returns the found releases, or an empty list if
-		 * no releases are found, an error has occurred or search isn't
-		 * completed yet.
-		 *
-		 * @return The fetched artist biography.
-		 */
-		virtual QList<ReleaseInfo> GetReleases () const = 0;
-	protected:
-		/** @brief Emitted when the biography is ready and fetched.
-		 *
-		 * The object will be invalid after this signal is emitted and
-		 * the event loop is run.
-		 */
-		virtual void ready () = 0;
-
-		/** @brief Emitted when there is an error fetching the biography.
-		 *
-		 * The object will be invalid after this signal is emitted and
-		 * the event loop is run.
-		 *
-		 * @param[out] reason The human-readable string describing the
-		 * error.
-		 */
-		virtual void error (const QString& reason) = 0;
-	};
-
 	/** @brief Interface for plugins supporting getting artist discography.
 	 *
 	 * Plugins that support fetching artists discography from sources
@@ -189,6 +129,8 @@ namespace Media
 	{
 	public:
 		virtual ~IDiscographyProvider () {}
+
+		using QueryResult_t = LeechCraft::Util::Either<QString, QList<ReleaseInfo>>;
 
 		/** @brief Returns the service name.
 		 *
@@ -214,7 +156,7 @@ namespace Media
 		 * @param[in] artist The artist name.
 		 * @return The pending discography search handle.
 		 */
-		virtual IPendingDisco* GetDiscography (const QString& artist) = 0;
+		virtual QFuture<QueryResult_t> GetDiscography (const QString& artist) = 0;
 
 		/** @brief Fetches contents of the given release by the artist.
 		 *
@@ -232,9 +174,8 @@ namespace Media
 		 * @param[in] release The release name to search for.
 		 * @return The pending discography search handle.
 		 */
-		virtual IPendingDisco* GetReleaseInfo (const QString& artist, const QString& release) = 0;
+		virtual QFuture<QueryResult_t> GetReleaseInfo (const QString& artist, const QString& release) = 0;
 	};
 }
 
-Q_DECLARE_INTERFACE (Media::IPendingDisco, "org.LeechCraft.Media.IPendingDisco/1.0")
 Q_DECLARE_INTERFACE (Media::IDiscographyProvider, "org.LeechCraft.Media.IDiscographyProvider/1.0")
