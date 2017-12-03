0b0820de1e2f340d6d8fdde410fc9c52d72b5b5e
0xd34df00d <0xd34df00d@gmail.com>
2017-12-03 01:54:47 -0500

    [MZ] Updated translations.

--- src/plugins/musiczombie/leechcraft_musiczombie_en.ts
+++ src/plugins/musiczombie/leechcraft_musiczombie_en.ts
@@ -1,28 +1,28 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LeechCraft::MusicZombie::PendingDisco</name>
     <message>
-        <location filename="pendingdisco.cpp" line="111"/>
+        <location filename="pendingdisco.cpp" line="115"/>
         <source>Error getting artist MBID.</source>
         <translation>Error getting artist MBID.</translation>
     </message>
     <message>
-        <location filename="pendingdisco.cpp" line="127"/>
-        <source>Unable to parse MusicBrainz reply.</source>
-        <translation>Unable to parse MusicBrainz reply.</translation>
+        <location filename="pendingdisco.cpp" line="146"/>
+        <source>No artists were found.</source>
+        <translation>No artists were found.</translation>
     </message>
     <message>
-        <location filename="pendingdisco.cpp" line="211"/>
-        <source>Error performing artist lookup: %1.</source>
-        <translation>Error performing artist lookup: %1.</translation>
+        <location filename="pendingdisco.cpp" line="277"/>
+        <source>Unable to parse MusicBrainz reply.</source>
+        <translation>Unable to parse MusicBrainz reply.</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::MusicZombie::Plugin</name>
     <message>
-        <location filename="musiczombie.cpp" line="73"/>
+        <location filename="musiczombie.cpp" line="77"/>
         <source>Client for the MusicBrainz.org service.</source>
         <translation>Client for the MusicBrainz.org service.</translation>
     </message>
--- src/plugins/musiczombie/leechcraft_musiczombie_ru_RU.ts
+++ src/plugins/musiczombie/leechcraft_musiczombie_ru_RU.ts
@@ -1,28 +1,28 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LeechCraft::MusicZombie::PendingDisco</name>
     <message>
-        <location filename="pendingdisco.cpp" line="111"/>
+        <location filename="pendingdisco.cpp" line="115"/>
         <source>Error getting artist MBID.</source>
         <translation>Ошибка получения MBID исполнителя.</translation>
     </message>
     <message>
-        <location filename="pendingdisco.cpp" line="127"/>
-        <source>Unable to parse MusicBrainz reply.</source>
-        <translation>Невозможно разобрать ответ от MusicBrainz.</translation>
+        <location filename="pendingdisco.cpp" line="146"/>
+        <source>No artists were found.</source>
+        <translation>Исполнители не найдены.</translation>
     </message>
     <message>
-        <location filename="pendingdisco.cpp" line="211"/>
-        <source>Error performing artist lookup: %1.</source>
-        <translation>Ошибка поиска исполнителя: %1.</translation>
+        <location filename="pendingdisco.cpp" line="277"/>
+        <source>Unable to parse MusicBrainz reply.</source>
+        <translation>Невозможно разобрать ответ от MusicBrainz.</translation>
     </message>
 </context>
 <context>
     <name>LeechCraft::MusicZombie::Plugin</name>
     <message>
-        <location filename="musiczombie.cpp" line="73"/>
+        <location filename="musiczombie.cpp" line="77"/>
         <source>Client for the MusicBrainz.org service.</source>
         <translation>Клиент для сервиса MusicBrainz.org.</translation>
     </message>
