69e8f4870d36934648df220294c9009acf34215a
0xd34df00d <0xd34df00d@gmail.com>
2017-12-23 18:46:10 -0500

    [Util] Fix gcc build.

--- src/util/db/oral.h
+++ src/util/db/oral.h
@@ -1034,7 +1034,7 @@ namespace oral
 					QString {} :
 					(", " + constraints.join (", "));
 
-			const auto& statements = Util::ZipWith (types, data.Fields_,
+			const auto& statements = Util::ZipWith (types, static_cast<const QList<QString>&> (data.Fields_),
 					[] (const QString& type, const QString& field) { return field + " " + type; });
 			return "CREATE TABLE " +
 					data.Table_ +
