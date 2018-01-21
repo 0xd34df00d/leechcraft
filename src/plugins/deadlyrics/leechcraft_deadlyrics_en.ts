fcc39f86c0a473cd03b4ac73c284c2474d1498ee
0xd34df00d <0xd34df00d@gmail.com>
2018-01-21 15:31:04 -0500

    [DeadLyrics] Updated translations.

--- src/plugins/deadlyrics/leechcraft_deadlyrics_en.ts
+++ src/plugins/deadlyrics/leechcraft_deadlyrics_en.ts
@@ -1,13 +1,26 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LeechCraft::DeadLyrics::DeadLyRicS</name>
     <message>
-        <location filename="deadlyrics.cpp" line="74"/>
+        <location filename="deadlyrics.cpp" line="72"/>
         <source>Song lyrics searcher.</source>
         <oldsource>Lyrics Searcher</oldsource>
         <translation>Song lyrics searcher.</translation>
+    </message>
+    <message>
+        <location filename="deadlyrics.cpp" line="84"/>
+        <source>Not enough parameters to build a search query.</source>
+        <translation>Not enough parameters to build a search query.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::DeadLyrics::HascirylSearcher</name>
+    <message>
+        <location filename="hascirylsearcher.cpp" line="131"/>
+        <source>Lyrics not found.</source>
+        <translation>Lyrics not found.</translation>
     </message>
 </context>
 </TS>
--- src/plugins/deadlyrics/leechcraft_deadlyrics_ru_RU.ts
+++ src/plugins/deadlyrics/leechcraft_deadlyrics_ru_RU.ts
@@ -1,11 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LeechCraft::DeadLyrics::DeadLyRicS</name>
     <message>
         <source>Song lyrics searcher.</source>
         <translation>Модуль поиска текстов песен.</translation>
+    </message>
+    <message>
+        <source>Not enough parameters to build a search query.</source>
+        <translation>Недостаточно параметров для поискового запроса.</translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::DeadLyrics::HascirylSearcher</name>
+    <message>
+        <source>Lyrics not found.</source>
+        <translation>Текст не найден.</translation>
     </message>
 </context>
 </TS>
