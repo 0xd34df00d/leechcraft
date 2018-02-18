5a1c643f45d51daf5b1aca1b970002b7c29db793
0xd34df00d <0xd34df00d@gmail.com>
2018-02-18 13:15:58 -0500

    [Azoth Xoox] Bump mam version to :2.

--- src/plugins/azoth/plugins/xoox/xep0313manager.cpp
+++ src/plugins/azoth/plugins/xoox/xep0313manager.cpp
@@ -43,7 +43,7 @@ namespace Azoth
 {
 namespace Xoox
 {
-	const QString NsMam = "urn:xmpp:mam:0";
+	const QString NsMam = "urn:xmpp:mam:2";
 
 	bool Xep0313Manager::Supports0313 (const QStringList& features)
 	{
