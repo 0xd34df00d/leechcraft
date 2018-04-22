004eb1b08ffaced59254e18cb7352d1df1564645
0xd34df00d <0xd34df00d@gmail.com>
2018-04-22 17:54:56 -0400

    [Aggregator] Drop the option to hide Aggregator in dashboards.
    That should be the job of the dashboard/summary plugins.

--- src/plugins/aggregator/jobholderrepresentation.cpp
+++ src/plugins/aggregator/jobholderrepresentation.cpp
@@ -31,7 +31,6 @@
 #include <QTimer>
 #include <QtDebug>
 #include "aggregator.h"
-#include "xmlsettingsmanager.h"
 
 namespace LeechCraft
 {
@@ -41,9 +40,6 @@ namespace Aggregator
 	: QSortFilterProxyModel (parent)
 	{
 		setDynamicSortFilter (true);
-		XmlSettingsManager::Instance ()->RegisterObject ("ShowInDashboard",
-				this,
-				"invalidate");
 	}
 	
 	QModelIndex JobHolderRepresentation::SelectionChanged (const QModelIndex& index)
@@ -61,11 +57,8 @@ namespace Aggregator
 		// The row won't show up anyway in the job list if it was empty, so
 		// we can just check if it has unread items or selected. Later means
 		// that user's just clicked last unread item there.
-		if (XmlSettingsManager::Instance ()->property ("ShowInDashboard").toBool ())
-			return sourceModel ()->index (row, 1).data ().toInt () ||
+		return sourceModel ()->index (row, 1).data ().toInt () ||
 				(Selected_.isValid () ? row == Selected_.row () : false);
-		else
-			return false;
 	}
 }
 }
