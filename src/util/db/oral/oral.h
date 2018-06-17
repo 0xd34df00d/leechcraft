8c56ce0f3345c5575d1e73a5d2921b9f3f113dab
0xd34df00d <0xd34df00d@gmail.com>
2018-06-17 17:22:59 -0400

    [Util] Minor code style.

--- src/util/db/oral/oral.h
+++ src/util/db/oral/oral.h
@@ -994,19 +994,37 @@ namespace oral
 				template<typename NewSel>
 				auto Select (NewSel&& selector) &&
 				{
-					return Builder<NewSel, TreeT, OrderT> { W_, std::forward<NewSel> (selector), std::move (Tree_), std::move (Order_) };
+					return Builder<NewSel, TreeT, OrderT>
+					{
+						W_,
+						std::forward<NewSel> (selector),
+						std::move (Tree_),
+						std::move (Order_)
+					};
 				}
 
 				template<typename NewTree>
 				auto Where (NewTree&& tree) &&
 				{
-					return Builder<SelectorT, NewTree, OrderT> { W_, std::move (Selector_), std::forward<NewTree> (tree), std::move (Order_) };
+					return Builder<SelectorT, NewTree, OrderT>
+					{
+						W_,
+						std::move (Selector_),
+						std::forward<NewTree> (tree),
+						std::move (Order_)
+					};
 				}
 
 				template<typename NewOrder>
 				auto Order (NewOrder&& order) &&
 				{
-					return Builder<SelectorT, TreeT, NewOrder> { W_, std::move (Selector_), std::move (Tree_), std::forward<NewOrder> (order) };
+					return Builder<SelectorT, TreeT, NewOrder>
+					{
+						W_,
+						std::move (Selector_),
+						std::move (Tree_),
+						std::forward<NewOrder> (order)
+					};
 				}
 
 				auto operator() () &&
