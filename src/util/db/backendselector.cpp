d8d7cf062d7b75fd4ec177e672e4ce70542ecbfa
0xd34df00d <0xd34df00d@gmail.com>
2018-06-23 17:50:34 -0400

    [Util] Drop SQLite options configuration widget.

--- src/util/db/backendselector.cpp
+++ src/util/db/backendselector.cpp
@@ -62,11 +62,6 @@ void BackendSelector::FillUI ()
 	Ui_->StorageType_->setCurrentIndex (index);
 	Ui_->Settings_->setCurrentIndex (index);
 
-	Ui_->SQLiteVacuum_->setCheckState (Manager_->Property ("SQLiteVacuum", false).toBool () ? Qt::Checked : Qt::Unchecked);
-	Ui_->SQLiteJournalMode_->setCurrentIndex (Ui_->SQLiteJournalMode_->findText (Manager_->Property ("SQLiteJournalMode", "TRUNCATE").toString ()));
-	Ui_->SQLiteTempStore_->setCurrentIndex (Ui_->SQLiteTempStore_->findText (Manager_->Property ("SQLiteTempStore", "MEMORY").toString ()));
-	Ui_->SQLiteSynchronous_->setCurrentIndex (Ui_->SQLiteSynchronous_->findText (Manager_->Property ("SQLiteSynchronous", "OFF").toString ()));
-
 	Ui_->PostgresHostname_->setText (Manager_->Property ("PostgresHostname", "localhost").toString ());
 	Ui_->PostgresPort_->setValue (Manager_->Property ("PostgresPort", 5432).toInt ());
 	Ui_->PostgresDBName_->setText (Manager_->Property ("PostgresDBName", "").toString ());
@@ -83,11 +78,6 @@ void BackendSelector::FillUI ()
 void BackendSelector::accept ()
 {
 	Manager_->setProperty ("StorageType", Ui_->StorageType_->currentText ());
-
-	Manager_->setProperty ("SQLiteVacuum", Ui_->SQLiteVacuum_->checkState () == Qt::Checked);
-	Manager_->setProperty ("SQLiteJournalMode", Ui_->SQLiteJournalMode_->currentText ());
-	Manager_->setProperty ("SQLiteTempStore", Ui_->SQLiteTempStore_->currentText ());
-	Manager_->setProperty ("SQLiteSynchronous", Ui_->SQLiteSynchronous_->currentText ());
 
 	Manager_->setProperty ("PostgresHostname", Ui_->PostgresHostname_->text ());
 	Manager_->setProperty ("PostgresPort", Ui_->PostgresPort_->value ());
