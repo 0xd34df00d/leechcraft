f0943e1eda4932f67e9c1e51c5944beba1e1842d
0xd34df00d <0xd34df00d@gmail.com>
2018-07-29 00:11:11 -0400

    [LackMan] Get rid of kludges for clang diagnostics against boost.

--- src/plugins/lackman/deptreebuilder.cpp
+++ src/plugins/lackman/deptreebuilder.cpp
@@ -29,17 +29,7 @@
 
 #include "deptreebuilder.h"
 #include <boost/graph/visitors.hpp>
-
-#ifdef __clang__
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-variable"
-#pragma clang diagnostic ignored "-Wunneeded-internal-declaration"
-#endif
 #include <boost/graph/depth_first_search.hpp>
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
-
 #include <boost/graph/filtered_graph.hpp>
 #include <boost/graph/topological_sort.hpp>
 #include "core.h"
