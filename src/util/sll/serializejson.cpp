6e411b32daa95cfad4ddbb47d17c7a6cdb67ed9e
0xd34df00d <0xd34df00d@gmail.com>
2018-08-04 00:23:57 -0400

    [Util] Moved SerializeJson() to a .cpp (and added it to the project).

--- src/util/sll/serializejson.cpp
+++ src/util/sll/serializejson.cpp
@@ -27,29 +27,17 @@
  * DEALINGS IN THE SOFTWARE.
  **********************************************************************/
 
-#pragma once
-
-#include "sllconfig.h"
-
-class QByteArray;
-class QVariant;
+#include "serializejson.h"
+#include <QJsonDocument>
 
 namespace LeechCraft
 {
 namespace Util
 {
-	/** @brief Serializes the given \em var to JSON representation.
-	 *
-	 * This function abstracts away differences between Qt4 and Qt5. It
-	 * uses QJson on Qt4 (don't forget to link to it!) and native JSON
-	 * functions on Qt5.
-	 *
-	 * @param[in] var The recursive variant to be serialized to JSON.
-	 * @param[in] compact Whether the output should be compacitified
-	 * (this parameter may have no effect).
-	 * @return The serialized representation of \em var.
-	 */
-	UTIL_SLL_API QByteArray SerializeJson (const QVariant& var, bool compact = true);
+	QByteArray SerializeJson (const QVariant& var, bool compact)
+	{
+		return QJsonDocument::fromVariant (var)
+				.toJson (compact ? QJsonDocument::Compact : QJsonDocument::Indented);
+	}
 }
 }
-
