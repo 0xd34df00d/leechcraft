8525b845f046bd6fe1283071b8a5b131951664b5
0xd34df00d <0xd34df00d@gmail.com>
2018-09-08 16:18:10 -0400

    [Azoth Sarin] Use pkgconfig to find tox.

--- src/plugins/azoth/plugins/sarin/cmake/FindTox.cmake
+++ src/plugins/azoth/plugins/sarin/cmake/FindTox.cmake
@@ -1,27 +1,10 @@
-find_path (TOX_INCLUDE_DIRS
-	NAMES tox/tox.h
-	PATHS
-		/usr/local/include
-		/usr/include
-	)
+find_package(PkgConfig)
+pkg_check_modules(TOX REQUIRED IMPORTED_TARGET toxcore)
 
-find_library (TOXCORE_LIBRARY
-	NAMES toxcore
-	PATHS
-		/usr/local/lib
-		/usr/lib
-	)
-find_library (TOXAV_LIBRARY
-	NAMES toxav
-	PATHS
-		/usr/local/lib
-		/usr/lib
-	)
-
-if (TOX_INCLUDE_DIRS AND TOXCORE_LIBRARY AND TOXAV_LIBRARY)
+if (TOX_INCLUDE_DIRS AND TOX_LIBRARIES)
 	message (STATUS "Found Tox includes at: ${TOX_INCLUDE_DIRS}")
-	message (STATUS "Found Tox core library at: ${TOXCORE_LIBRARY}")
-	message (STATUS "Found Tox AV library at: ${TOXAV_LIBRARY}")
+	message (STATUS "Found Tox libraries: ${TOX_LIBRARIES}")
+	message (STATUS "Found Tox libraries at: ${TOX_LIBRARY_DIRS}")
 else ()
 	message (FATAL_ERROR "Could NOT find Tox")
 endif ()
