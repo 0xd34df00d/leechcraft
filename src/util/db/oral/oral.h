fc3e45d00a24f8eca2e96bbef4e391aa598ee450
0xd34df00d <0xd34df00d@gmail.com>
2018-10-07 18:13:29 -0400

    [Oral] Added a constexpr ExprTree::HasAdditionalTables()

--- src/util/db/oral/oral.h
+++ src/util/db/oral/oral.h
@@ -694,6 +694,12 @@ namespace oral
 			{
 				return Left_.template AdditionalTables<T> () + Right_.template AdditionalTables<T> ();
 			}
+
+			template<typename T>
+			constexpr static bool HasAdditionalTables () noexcept
+			{
+				return L::template HasAdditionalTables<T> () || R::template HasAdditionalTables<T> ();
+			}
 		};
 
 		template<auto... Ptr>
@@ -727,6 +733,12 @@ namespace oral
 					return { Seq::ClassName () };
 			}
 
+			template<typename T>
+			constexpr static bool HasAdditionalTables () noexcept
+			{
+				return !std::is_same_v<MemberPtrStruct_t<Ptr>, T>;
+			}
+
 			template<typename R>
 			auto operator= (const R&) const noexcept;
 		};
@@ -756,6 +768,12 @@ namespace oral
 			QSet<QString> AdditionalTables () const noexcept
 			{
 				return {};
+			}
+
+			template<typename>
+			constexpr static bool HasAdditionalTables () noexcept
+			{
+				return false;
 			}
 		};
 
