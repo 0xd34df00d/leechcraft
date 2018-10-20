930f1d33bbf6f83713b85ab94ef9a2f4aa2f8bf9
0xd34df00d <0xd34df00d@gmail.com>
2018-10-20 17:42:53 -0400

    [Snails] Don't add the current index to selected IDs in multiselect mode

--- src/plugins/snails/mailtab.cpp
+++ src/plugins/snails/mailtab.cpp
@@ -404,25 +404,23 @@ namespace Snails
 
 	QList<QByteArray> MailTab::GetSelectedIds () const
 	{
-		QList<QByteArray> ids;
 		switch (MailListMode_)
 		{
 		case MailListMode::Normal:
+		{
+			QList<QByteArray> ids;
 			for (const auto& index : Ui_.MailTree_->selectionModel ()->selectedRows ())
 				ids << index.data (MailModel::MailRole::ID).toByteArray ();
-			break;
-		case MailListMode::MultiSelect:
-			if (MailModel_)
-				ids = MailModel_->GetCheckedIds ();
-			break;
+
+			const auto& currentId = Ui_.MailTree_->currentIndex ()
+					.data (MailModel::MailRole::ID).toByteArray ();
+			if (!currentId.isEmpty () && !ids.contains (currentId))
+				ids << currentId;
+			return ids;
 		}
-
-		const auto& currentId = Ui_.MailTree_->currentIndex ()
-				.data (MailModel::MailRole::ID).toByteArray ();
-		if (!currentId.isEmpty () && !ids.contains (currentId))
-			ids << currentId;
-
-		return ids;
+		case MailListMode::MultiSelect:
+			return MailModel_->GetCheckedIds ();
+		}
 	}
 
 	QList<Folder> MailTab::GetActualFolders () const
