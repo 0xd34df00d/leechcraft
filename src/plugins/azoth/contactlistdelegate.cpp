3c6c90cd531851e793491f85765cd544aaf38e0a
0xd34df00d <0xd34df00d@gmail.com>
2018-11-05 00:13:28 -0500

    [Azoth] Our own drawing of indicators and all that stuff

--- src/plugins/azoth/contactlistdelegate.cpp
+++ src/plugins/azoth/contactlistdelegate.cpp
@@ -232,8 +232,23 @@ namespace Azoth
 				o.widget->style () :
 				QApplication::style ();
 
-		style->drawPrimitive (HighlightGroups_ ? QStyle::PE_PanelButtonCommand : QStyle::PE_PanelItemViewRow,
+		if (HighlightGroups_)
+			o.features |= QStyleOptionViewItem::Alternate;
+		style->drawPrimitive (QStyle::PE_PanelItemViewItem,
 				&o, painter, o.widget);
+
+		{
+			const auto isOpen = o.state & QStyle::State_Open;
+			auto indicatorOpt = o;
+			const auto width = indicatorOpt.rect.height ();
+			indicatorOpt.rect.setWidth (width);
+			style->drawPrimitive (isOpen ? QStyle::PE_IndicatorArrowDown : QStyle::PE_IndicatorArrowRight,
+					&indicatorOpt, painter, o.widget);
+			o.rect.setLeft (o.rect.left () + width);
+		}
+
+		if (o.state & QStyle::State_Selected)
+			painter->setPen (o.palette.color (QPalette::HighlightedText));
 
 		const int unread = index.data (Core::CLRUnreadMsgCount).toInt ();
 		if (unread)
@@ -255,9 +270,9 @@ namespace Azoth
 			o.rect.setLeft (unreadSpace + o.rect.left ());
 		}
 
-		QStyledItemDelegate::paint (painter, o, index);
-
-		const int textWidth = o.fontMetrics.width (index.data ().value<QString> () + " ");
+		const auto& groupName = index.data ().toString ();
+		painter->drawText (r, Qt::AlignVCenter | Qt::AlignLeft, groupName);
+		const int textWidth = o.fontMetrics.width (groupName);
 		const int rem = r.width () - textWidth;
 
 		const auto& counts = GetCounts (index);
@@ -272,8 +287,6 @@ namespace Azoth
 
 		if (rem >= o.fontMetrics.width (str))
 		{
-			if (o.state & QStyle::State_Selected)
-				painter->setPen (o.palette.color (QPalette::HighlightedText));
 
 			QFont font = painter->font ();
 			font.setItalic (true);
