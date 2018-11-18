a43bb9ae67fc4c9b4aa3744869d43b830a120a23
0xd34df00d <0xd34df00d@gmail.com>
2018-11-18 17:05:04 -0500

    [Snails] Send noop directly instead of scheduling on the current thread

--- src/plugins/snails/accountthreadworker.cpp
+++ src/plugins/snails/accountthreadworker.cpp
@@ -710,13 +710,8 @@ namespace Snails
 
 	void AccountThreadWorker::SendNoop ()
 	{
-		const auto at = static_cast<AccountThread*> (QThread::currentThread ());
-		at->Schedule (TaskPriority::Low,
-				[this] (AccountThreadWorker*)
-				{
-					if (CachedStore_)
-						CachedStore_->noop ();
-				});
+		if (CachedStore_)
+			CachedStore_->noop ();
 	}
 
 	void AccountThreadWorker::SetNoopTimeoutChangeNotifier (const std::shared_ptr<AccountThreadNotifier<int>>& notifier)
