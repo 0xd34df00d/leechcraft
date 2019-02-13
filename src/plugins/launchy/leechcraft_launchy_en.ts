919caf3b10db3b28eec73b244cc9c8a5e7dec8bd
0xd34df00d <0xd34df00d@gmail.com>
2019-02-13 18:22:44 -0500

    [Launchy] Updated translations

--- src/plugins/launchy/leechcraft_launchy_en.ts
+++ src/plugins/launchy/leechcraft_launchy_en.ts
@@ -1,85 +1,85 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LeechCraft::Launchy::FSDisplayer</name>
     <message>
-        <location filename="fsdisplayer.cpp" line="214"/>
+        <location filename="fsdisplayer.cpp" line="210"/>
         <source>Recent</source>
         <translation>Recent</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="218"/>
+        <location filename="fsdisplayer.cpp" line="214"/>
         <source>Favorites</source>
         <translation>Favorites</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="264"/>
+        <location filename="fsdisplayer.cpp" line="260"/>
         <source>Accessories</source>
         <translation>Accessories</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="265"/>
+        <location filename="fsdisplayer.cpp" line="261"/>
         <source>Development</source>
         <translation>Development</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="266"/>
+        <location filename="fsdisplayer.cpp" line="262"/>
         <source>Education</source>
         <translation>Education</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="267"/>
+        <location filename="fsdisplayer.cpp" line="263"/>
         <source>Games</source>
         <translation>Games</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="268"/>
+        <location filename="fsdisplayer.cpp" line="264"/>
         <source>Graphics</source>
         <translation>Graphics</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="269"/>
+        <location filename="fsdisplayer.cpp" line="265"/>
         <source>Internet</source>
         <translation>Internet</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="270"/>
+        <location filename="fsdisplayer.cpp" line="266"/>
         <source>Multimedia</source>
         <translation>Multimedia</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="271"/>
+        <location filename="fsdisplayer.cpp" line="267"/>
         <source>Office</source>
         <translation>Office</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="272"/>
+        <location filename="fsdisplayer.cpp" line="268"/>
         <source>Other</source>
         <translation>Other</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="273"/>
+        <location filename="fsdisplayer.cpp" line="269"/>
         <source>Settings</source>
         <translation>Settings</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="274"/>
+        <location filename="fsdisplayer.cpp" line="270"/>
         <source>Science</source>
         <translation>Science</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="275"/>
+        <location filename="fsdisplayer.cpp" line="271"/>
         <source>System</source>
         <translation>System</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="276"/>
+        <location filename="fsdisplayer.cpp" line="272"/>
         <source>Toys</source>
         <translation>Toys</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="277"/>
+        <location filename="fsdisplayer.cpp" line="273"/>
         <source>Utilities</source>
         <translation>Utilities</translation>
     </message>
--- src/plugins/launchy/leechcraft_launchy_ru_RU.ts
+++ src/plugins/launchy/leechcraft_launchy_ru_RU.ts
@@ -1,85 +1,85 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LeechCraft::Launchy::FSDisplayer</name>
     <message>
-        <location filename="fsdisplayer.cpp" line="214"/>
+        <location filename="fsdisplayer.cpp" line="210"/>
         <source>Recent</source>
         <translation>Недавние</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="218"/>
+        <location filename="fsdisplayer.cpp" line="214"/>
         <source>Favorites</source>
         <translation>Избранные</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="264"/>
+        <location filename="fsdisplayer.cpp" line="260"/>
         <source>Accessories</source>
         <translation>Аксессуары</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="265"/>
+        <location filename="fsdisplayer.cpp" line="261"/>
         <source>Development</source>
         <translation>Разработка</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="266"/>
+        <location filename="fsdisplayer.cpp" line="262"/>
         <source>Education</source>
         <translation>Образование</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="267"/>
+        <location filename="fsdisplayer.cpp" line="263"/>
         <source>Games</source>
         <translation>Игры</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="268"/>
+        <location filename="fsdisplayer.cpp" line="264"/>
         <source>Graphics</source>
         <translation>Графика</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="269"/>
+        <location filename="fsdisplayer.cpp" line="265"/>
         <source>Internet</source>
         <translation>Интернет</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="270"/>
+        <location filename="fsdisplayer.cpp" line="266"/>
         <source>Multimedia</source>
         <translation>Мультимедиа</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="271"/>
+        <location filename="fsdisplayer.cpp" line="267"/>
         <source>Office</source>
         <translation>Офис</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="272"/>
+        <location filename="fsdisplayer.cpp" line="268"/>
         <source>Other</source>
         <translation>Другое</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="273"/>
+        <location filename="fsdisplayer.cpp" line="269"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="274"/>
+        <location filename="fsdisplayer.cpp" line="270"/>
         <source>Science</source>
         <translation>Наука</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="275"/>
+        <location filename="fsdisplayer.cpp" line="271"/>
         <source>System</source>
         <translation>Система</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="276"/>
+        <location filename="fsdisplayer.cpp" line="272"/>
         <source>Toys</source>
         <translation>Игрушки</translation>
     </message>
     <message>
-        <location filename="fsdisplayer.cpp" line="277"/>
+        <location filename="fsdisplayer.cpp" line="273"/>
         <source>Utilities</source>
         <translation>Служебные</translation>
     </message>
