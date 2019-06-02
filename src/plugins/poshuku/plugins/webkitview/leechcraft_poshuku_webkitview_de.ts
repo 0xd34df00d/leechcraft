e8d40b6d88dedca25a2fac30ddcd3c50ac531e6d
0xd34df00d <0xd34df00d@gmail.com>
2019-06-02 16:55:14 -0400

    [Poshuku WKV] Added German translations

---
+++ src/plugins/poshuku/plugins/webkitview/leechcraft_poshuku_webkitview_de.ts
@@ -0,0 +1,420 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>LeechCraft::Poshuku::WebKitView::CustomWebPage</name>
+    <message>
+        <location filename="customwebpage.cpp" line="237"/>
+        <source>Unable to send the request to %1. Please try submitting it again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="463"/>
+        <source>&lt;a href=&quot;http://downforeveryoneorjustme.com/{host}&quot; target=&quot;_blank&quot;&gt;check&lt;/a&gt; if the site &lt;strong&gt;{host}&lt;/strong&gt; is down for you only;</source>
+        <comment>{host} would be substituded with site&apos;s host name.</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="466"/>
+        <source>try again later</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="467"/>
+        <source>contact remote server&apos;s administrator (typically at &lt;a href=&quot;mailto:webmaster@{host}&quot;&gt;webmaster@{host}&lt;/a&gt;)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="469"/>
+        <source>contact your system/network administrator, especially if you can&apos;t load any single page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="471"/>
+        <source>check your proxy settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="482"/>
+        <location filename="customwebpage.cpp" line="515"/>
+        <source>check if the URL is written correctly;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="483"/>
+        <source>try changing your DNS servers;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="484"/>
+        <source>make sure that LeechCraft is allowed to access the Internet and particularly web sites;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="489"/>
+        <source>check whether some downloads consume too much bandwidth: try limiting their speed or reducing number of connections for them;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="494"/>
+        <source>try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="496"/>
+        <source>make sure that remote server is really what it claims to be;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="516"/>
+        <source>go to web site&apos;s &lt;a href=&quot;{schema}://{host}/&quot;&gt;main page&lt;/a&gt; and find the required page from there.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="518"/>
+        <source>check the login and password you entered and try again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="522"/>
+        <source>check if the URL is written correctly, particularly, the part before the &apos;://&apos;;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="523"/>
+        <source>try installing plugins that are known to support this protocol;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="563"/>
+        <source>Error loading %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="568"/>
+        <source>%1 (%2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="573"/>
+        <source>%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="575"/>
+        <source>The page you tried to access cannot be loaded now.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebpage.cpp" line="582"/>
+        <source>Try doing the following:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::WebKitView::CustomWebView</name>
+    <message>
+        <location filename="customwebview.cpp" line="168"/>
+        <location filename="customwebview.cpp" line="178"/>
+        <source>Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="564"/>
+        <source>Installed plugins</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="565"/>
+        <source>No plugins installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="566"/>
+        <source>File name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="567"/>
+        <source>MIME type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="568"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="569"/>
+        <source>Suffixes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="570"/>
+        <source>Enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="571"/>
+        <source>No</source>
+        <translation>Nein</translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="572"/>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="590"/>
+        <location filename="customwebview.cpp" line="592"/>
+        <source>Welcome to LeechCraft!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="594"/>
+        <source>Welcome to LeechCraft, the integrated internet-client.&lt;br /&gt;More info is available on the &lt;a href=&apos;https://leechcraft.org&apos;&gt;project&apos;s site&lt;/a&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="customwebview.cpp" line="626"/>
+        <source>Print web page</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::WebKitView::Plugin</name>
+    <message>
+        <location filename="webkitview.cpp" line="129"/>
+        <source>Provides QtWebKit-based backend for Poshuku.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::WebKitView::SslStateDialog</name>
+    <message>
+        <location filename="sslstatedialog.cpp" line="56"/>
+        <source>SSL encryption is not used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sslstatedialog.cpp" line="60"/>
+        <source>Some SSL errors where encountered.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sslstatedialog.cpp" line="64"/>
+        <source>Some elements were loaded via unencrypted connection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sslstatedialog.cpp" line="68"/>
+        <source>Everything is secure!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeechCraft::Poshuku::WebKitView::WebViewSslWatcherHandler</name>
+    <message>
+        <location filename="webviewsslwatcherhandler.cpp" line="77"/>
+        <source>Some SSL errors where encountered.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="webviewsslwatcherhandler.cpp" line="81"/>
+        <source>Some elements were loaded via unencrypted connection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="webviewsslwatcherhandler.cpp" line="85"/>
+        <source>Everything is secure!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="interceptadaptor.cpp" line="162"/>
+        <source>Blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="interceptadaptor.cpp" line="164"/>
+        <source>Blocked: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SslStateDialog</name>
+    <message>
+        <location filename="sslstatedialog.ui" line="14"/>
+        <source>SSL state</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sslstatedialog.ui" line="55"/>
+        <source>Certificate information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sslstatedialog.ui" line="63"/>
+        <source>Certificate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sslstatedialog.ui" line="86"/>
+        <source>Insecure elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sslstatedialog.ui" line="108"/>
+        <source>SSL errors</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>poshukuwebkitviewsettings</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>Poshuku WebKitView</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source>Allow Java</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Offline storage database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Offline web application cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Enable HTML5 notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Developer extras</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>Fix palettes with non-white background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>User CSS URL:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <location filename="dummy.cpp" line="12"/>
+        <source>Rendering</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <source>Antialias primitives if possible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="14"/>
+        <source>Antialias text if possible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="15"/>
+        <source>Smooth pixmap transform</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="16"/>
+        <source>Security</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="17"/>
+        <source>Store local link history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="18"/>
+        <source>Cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="19"/>
+        <source>Offline storage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="20"/>
+        <source>Offline storage default quota:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="21"/>
+        <source> KB</source>
+        <translation> KB</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="22"/>
+        <source>Clear favicon database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="23"/>
+        <source>In-memory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="24"/>
+        <source>Prefetch DNS entries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="25"/>
+        <source>Maximum pages:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="26"/>
+        <source>Minimum dead capacity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="27"/>
+        <location filename="dummy.cpp" line="29"/>
+        <location filename="dummy.cpp" line="31"/>
+        <source> MB</source>
+        <translation> MB</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="28"/>
+        <source>Maximum dead capacity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="30"/>
+        <source>Total capacity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="32"/>
+        <source>Clear in-memory caches</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
