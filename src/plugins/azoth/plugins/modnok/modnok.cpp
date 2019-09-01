a53c166f8e6b185046f7ae655c4e16521d7aa04a
0xd34df00d <0xd34df00d@gmail.com>
2019-09-01 10:15:13 -0400

    [Azoth Modnok] Avoid deprecated byteCount()

--- src/plugins/azoth/plugins/modnok/modnok.cpp
+++ src/plugins/azoth/plugins/modnok/modnok.cpp
@@ -34,6 +34,7 @@
 #include <QTranslator>
 #include <interfaces/azoth/imessage.h>
 #include <interfaces/azoth/irichtextmessage.h>
+#include <util/compat/imagebytes.h>
 #include <util/util.h>
 #include <util/sys/paths.h>
 #include <util/sll/qtutil.h>
@@ -170,7 +171,7 @@ namespace Modnok
 		process.waitForFinished (5000);
 
 		QImage img (filename);
-		FormulasCache_.insert (formula, new QImage (filename), img.byteCount () / 1024);
+		FormulasCache_.insert (formula, new QImage (filename), Util::Compat::SizeInBytes (img) / 1024);
 
 		QFile (filename).remove ();
 
