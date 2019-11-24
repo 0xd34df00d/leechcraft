f6a3e989a4e774c79a393d172acdeae506717ec8
0xd34df00d <0xd34df00d@gmail.com>
2019-11-24 10:56:22 -0500

    [SB2] Use override markers

--- src/plugins/sb2/sb2.h
+++ src/plugins/sb2/sb2.h
@@ -74,18 +74,18 @@ namespace SB2
 
 		Util::ShortcutManager *ShortcutMgr_;
 	public:
-		void Init (ICoreProxy_ptr);
-		void SecondInit ();
-		QByteArray GetUniqueID () const;
-		void Release ();
-		QString GetName () const;
-		QString GetInfo () const;
-		QIcon GetIcon () const;
+		void Init (ICoreProxy_ptr) override;
+		void SecondInit () override;
+		QByteArray GetUniqueID () const override;
+		void Release () override;
+		QString GetName () const override;
+		QString GetInfo () const override;
+		QIcon GetIcon () const override;
 
-		QSet<QByteArray> GetPluginClasses () const;
+		QSet<QByteArray> GetPluginClasses () const override;
 
-		QMap<QString, ActionInfo> GetActionInfo () const;
-		void SetShortcut (const QString&, const QKeySequences_t&);
+		QMap<QString, ActionInfo> GetActionInfo () const override;
+		void SetShortcut (const QString&, const QKeySequences_t&) override;
 	public slots:
 		void hookGonnaShowStatusBar (LeechCraft::IHookProxy_ptr, bool);
 		void hookDockWidgetActionVisToggled (LeechCraft::IHookProxy_ptr,
