9ea96b24d78e46f565c18cd8e9a3bca77c527c66
0xd34df00d <0xd34df00d@gmail.com>
2020-04-06 15:36:30 -0400

    [Rosenthal] Avoid cmake warning about package/var naming

--- src/plugins/rosenthal/cmake/FindHunspell.cmake
+++ src/plugins/rosenthal/cmake/FindHunspell.cmake
@@ -1,15 +1,15 @@
-# - Try to find HUNSPELL
+# - Try to find Hunspell
 # Once done this will define
 #
-#  HUNSPELL_FOUND - system has HUNSPELL
-#  HUNSPELL_INCLUDE_DIR - the HUNSPELL include directory
-#  HUNSPELL_LIBRARIES - The libraries needed to use HUNSPELL
-#  HUNSPELL_DEFINITIONS - Compiler switches required for using HUNSPELL
+#  Hunspell_FOUND - system has Hunspell
+#  Hunspell_INCLUDE_DIR - the Hunspell include directory
+#  Hunspell_LIBRARIES - The libraries needed to use Hunspell
+#  Hunspell_DEFINITIONS - Compiler switches required for using Hunspell
 
 
-if (HUNSPELL_INCLUDE_DIR AND HUNSPELL_LIBRARIES)
+if (Hunspell_INCLUDE_DIR AND Hunspell_LIBRARIES)
   # Already in cache, be silent
-  set(HUNSPELL_FIND_QUIETLY TRUE)
+  set(Hunspell_FIND_QUIETLY TRUE)
 endif ()
 
 include(FindPackageHandleStandardArgs)
@@ -22,27 +22,27 @@ if (WIN32)
 		endif ()
 	endif ()
 
-	set (HUNSPELL_INCLUDE_WIN32 ${HUNSPELL_DIR}/src/)
+	set (Hunspell_INCLUDE_WIN32 ${Hunspell_DIR}/src/)
 
 	set (PROBE_DIR
-		${HUNSPELL_DIR}/src/win_api/Release_dll/libhunspell)
+		${Hunspell_DIR}/src/win_api/Release_dll/libhunspell)
 endif ()
 
-find_library (HUNSPELL_LIBRARIES NAMES hunspell-1.7 hunspell-1.6 hunspell-1.5 hunspell-1.4 hunspell-1.3 hunspell-1.2 libhunspell HINTS ${HUNSPELL_DIR} ${PROBE_DIR})
-find_path (HUNSPELL_INCLUDE_DIR hunspell/hunspell.hxx HINTS ${HUNSPELL_DIR} ${HUNSPELL_INCLUDE_WIN32})
+find_library (Hunspell_LIBRARIES NAMES hunspell-1.7 hunspell-1.6 hunspell-1.5 hunspell-1.4 hunspell-1.3 hunspell-1.2 libhunspell HINTS ${Hunspell_DIR} ${PROBE_DIR})
+find_path (Hunspell_INCLUDE_DIR hunspell/hunspell.hxx HINTS ${Hunspell_DIR} ${Hunspell_INCLUDE_WIN32})
 
-# handle the QUIETLY and REQUIRED arguments and set HUNSPELL_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set Hunspell_FOUND to TRUE if
 # all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS (HUNSPELL DEFAULT_MSG HUNSPELL_LIBRARIES HUNSPELL_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (Hunspell DEFAULT_MSG Hunspell_LIBRARIES Hunspell_INCLUDE_DIR)
 
-if (HUNSPELL_FOUND)
-  if (NOT HUNSPELL_FIND_QUIETLY AND HUNSPELLCONFIG_EXECUTABLE)
-    message(STATUS "Hunspell found: ${HUNSPELL_LIBRARIES}")
+if (Hunspell_FOUND)
+  if (NOT Hunspell_FIND_QUIETLY AND HunspellCONFIG_EXECUTABLE)
+    message(STATUS "Hunspell found: ${Hunspell_LIBRARIES}")
   endif()
 else()
-  if(HUNSPELL_FIND_REQUIRED)
+  if(Hunspell_FIND_REQUIRED)
     message(FATAL_ERROR "Could not find Hunspell")
   endif()
 endif()
 
-mark_as_advanced(HUNSPELL_INCLUDE_DIR HUNSPELL_LIBRARIES)
+mark_as_advanced(Hunspell_INCLUDE_DIR Hunspell_LIBRARIES)
