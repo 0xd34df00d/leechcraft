200f140b3c126f335d2aaa7cc93ddd5deef06dfe
0xd34df00d <0xd34df00d@gmail.com>
2020-05-24 11:17:02 -0400

    [CL] United translations for the quark

--- src/plugins/cpuload/leechcraft_cpuload_en.ts
+++ src/plugins/cpuload/leechcraft_cpuload_en.ts
@@ -1,12 +1,58 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
+<context>
+    <name>CpuLoadQuark</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>CpuLoad settings</source>
+        <translation>CpuLoad settings</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Update interval:</source>
+        <translation>Update interval:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source> ms</source>
+        <translation> ms</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Show IO time in popup window</source>
+        <translation>Show IO time in popup window</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Show low priority time in popup window</source>
+        <translation>Show low priority time in popup window</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Show medium priority time in popup window</source>
+        <translation>Show medium priority time in popup window</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Show high priority time in popup window</source>
+        <translation>Show high priority time in popup window</translation>
+    </message>
+</context>
 <context>
     <name>LC::CpuLoad::Plugin</name>
     <message>
-        <location filename="cpuload.cpp" line="86"/>
+        <location filename="cpuload.cpp" line="88"/>
         <source>Quark for monitoring CPU load.</source>
         <translation>Quark for monitoring CPU load.</translation>
+    </message>
+</context>
+<context>
+    <name>Tooltip</name>
+    <message>
+        <location filename="share/qml/cpuload/Tooltip.qml" line="113"/>
+        <source>Load, %</source>
+        <translation>Load, %</translation>
     </message>
 </context>
 </TS>
--- src/plugins/cpuload/leechcraft_cpuload_ru_RU.ts
+++ src/plugins/cpuload/leechcraft_cpuload_ru_RU.ts
@@ -1,12 +1,58 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>CpuLoadQuark</name>
+    <message>
+        <location filename="dummy.cpp" line="2"/>
+        <source>CpuLoad settings</source>
+        <translation>Настройки CpuLoad</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="3"/>
+        <source>Update interval:</source>
+        <translation>Интервал обновления:</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="4"/>
+        <source> ms</source>
+        <translation> мс</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="5"/>
+        <source>Show IO time in popup window</source>
+        <translation>Показывать время ожидания ввода-вывода во всплывающем окне</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
+        <source>Show low priority time in popup window</source>
+        <translation>Показывать низкоприоритетное время во всплывающем окне</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="7"/>
+        <source>Show medium priority time in popup window</source>
+        <translation>Показывать среднеприоритетное время во всплывающем окне</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Show high priority time in popup window</source>
+        <translation>Показывать высокоприоритетное время во всплывающем окне</translation>
+    </message>
+</context>
 <context>
     <name>LC::CpuLoad::Plugin</name>
     <message>
-        <location filename="cpuload.cpp" line="86"/>
+        <location filename="cpuload.cpp" line="88"/>
         <source>Quark for monitoring CPU load.</source>
         <translation>Кварк для наблюдения за загрузкой CPU.</translation>
+    </message>
+</context>
+<context>
+    <name>Tooltip</name>
+    <message>
+        <location filename="share/qml/cpuload/Tooltip.qml" line="113"/>
+        <source>Load, %</source>
+        <translation>Нагрузка, %</translation>
     </message>
 </context>
 </TS>
