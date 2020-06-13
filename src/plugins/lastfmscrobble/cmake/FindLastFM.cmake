769a6e02fb8b4e2af2b985d9ad52f068072c1bd0
0xd34df00d <0xd34df00d@gmail.com>
2020-06-13 11:46:16 -0400

    [LastFMScrobble] Make liblastfm finding module more concise

--- src/plugins/lastfmscrobble/cmake/FindLastFM.cmake
+++ src/plugins/lastfmscrobble/cmake/FindLastFM.cmake
@@ -12,60 +12,20 @@
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
-if (LastFM_LIBRARIES AND LastFM_INCLUDE_DIRS)
-	# in cache already
+if (LastFM_LIBRARY AND LastFM_INCLUDE_DIR)
 	set (LastFM_FOUND TRUE)
 else ()
-	find_library (LastFM_LIBRARY
-		NAMES
-		lastfm-qt5
-		lastfm5
-		lastfm
-		PATHS
-		/usr/lib
-		/usr/local/lib
-		/usr/local/lib
-		/opt/local/lib
-		/sw/lib
-		${LASTFM_DIR}/lib
-	)
-	find_path (LastFM_INCLUDE_DIR
-		NAMES
-		global.h
-		PATH_SUFFIXES
-		lastfm5
-		lastfm
-		PATHS
-		/usr/include
-		/usr/local/include
-		/usr/local/include
-		/opt/local/include
-		/sw/include
-		${LASTFM_DIR}/include
-	)
+	find_library (LastFM_LIBRARY NAMES lastfm-qt5 lastfm5 lastfm)
+	find_path (LastFM_INCLUDE_DIR NAMES global.h PATH_SUFFIXES lastfm5 lastfm)
 
-	set (LastFM_INCLUDE_DIRS
-		${LastFM_INCLUDE_DIR}
-	)
-	set (LastFM_LIBRARIES
-		${LastFM_LIBRARY}
-	)
-
-	if (LastFM_INCLUDE_DIRS AND LastFM_LIBRARIES)
+	if (LastFM_INCLUDE_DIR AND LastFM_LIBRARY)
 		set (LastFM_FOUND TRUE)
+		message (STATUS "Found lastfm libraries at ${LastFM_LIBRARY}")
+		message (STATUS "Found lastfm headers at ${LastFM_INCLUDE_DIR}")
+	elseif (LastFM_FIND_REQUIRED)
+		message (FATAL_ERROR "Could not find liblastfm")
 	endif ()
-
-	if (LastFM_FOUND)
-		if (NOT LastFM_FIND_QUIETLY)
-			message (STATUS "Found liblastfm: ${LastFM_LIBRARIES}")
-		endif ()
-	else ()
-		if (LastFM_FIND_REQUIRED)
-			message (FATAL_ERROR "Could not find liblastfm")
-		endif ()
-	endif ()
-
-	# show the LastFM_INCLUDE_DIRS and LastFM_LIBRARIES variables only in the advanced view
-	mark_as_advanced (LastFM_INCLUDE_DIRS LastFM_LIBRARIES)
-
 endif ()
+
+set (LastFM_INCLUDE_DIRS ${LastFM_INCLUDE_DIR})
+set (LastFM_LIBRARIES ${LastFM_LIBRARY})
