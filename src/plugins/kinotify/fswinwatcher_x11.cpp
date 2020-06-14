2ad141aec9d7c09c2fac506ad35c9096da8cf55c
0xd34df00d <0xd34df00d@gmail.com>
2020-06-14 11:04:03 -0400

    [Kinotify] Slightly better X11 code

--- src/plugins/kinotify/fswinwatcher_x11.cpp
+++ src/plugins/kinotify/fswinwatcher_x11.cpp
@@ -45,28 +45,23 @@ namespace Kinotify
 
 	namespace
 	{
-		bool getSize (Display *dpy, Window win, int *width,int *height)
+		std::optional<QSize> GetSize (Display *dpy, Window win)
 		{
 			XWindowAttributes windowattr;
 			if (XGetWindowAttributes (dpy, win, &windowattr) == 0)
-				return false;
-
-			*width = windowattr.width;
-			*height = windowattr.height;
-			return true;
+				return {};
+			return QSize { windowattr.width, windowattr.height };
 		}
 	}
 
 	bool FSWinWatcher::IsCurrentFS ()
 	{
-		Display *display = QX11Info::display ();
-		Window focusWin;
-		int reverToReturn;
-		int screen = QX11Info::appScreen ();
-		int screenWidth, screenHeight, width, height;
+		auto display = QX11Info::display ();
 		if (!display)
 			return false;
 
+		Window focusWin;
+		int reverToReturn;
 		XGetInputFocus (display, &focusWin, &reverToReturn);
 
 		auto rootWM = Proxy_->GetRootWindowsManager ();
@@ -74,11 +69,9 @@ namespace Kinotify
 			if (rootWM->GetMainWindow (i)->effectiveWinId () == focusWin)
 				return false;
 
-		if (!(getSize (display, RootWindow (display, screen), &screenWidth, &screenHeight) &&
-				getSize (display, focusWin, &width, &height)))
-			return false;
-
-		return screenWidth == width && screenHeight == height;
+		const auto rootSize = GetSize (display, RootWindow (display, QX11Info::appScreen ()));
+		const auto focusSize = GetSize (display, focusWin);
+		return rootSize && focusSize && *rootSize == *focusSize;
 	}
 }
 }
