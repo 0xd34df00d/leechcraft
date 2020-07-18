1d4aa0d21e40a28aea7398b4ec19d05d37261efe
0xd34df00d <0xd34df00d@gmail.com>
2020-07-18 11:08:41 -0400

    [Vrooby] Unembed the icon

--- src/plugins/vrooby/vrooby.cpp
+++ src/plugins/vrooby/vrooby.cpp
@@ -35,6 +35,8 @@
 #include <util/gui/geometry.h>
 #include <util/gui/unhoverdeletemixin.h>
 #include <util/sll/delayedexecutor.h>
+#include <interfaces/core/icoreproxy.h>
+#include <interfaces/core/iiconthememanager.h>
 #include <interfaces/core/ientitymanager.h>
 
 #ifdef ENABLE_UDISKS
@@ -128,8 +130,7 @@ namespace Vrooby
 
 	QIcon Plugin::GetIcon () const
 	{
-		static QIcon icon ("lcicons:/vrooby/resources/images/vrooby.svg");
-		return icon;
+		return GetProxyHolder ()->GetIconThemeManager ()->GetPluginIcon ();
 	}
 
 	bool Plugin::SupportsDevType (DeviceType type) const
