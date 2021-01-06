a4c526f0cd685fcdeb22e9ffc0765b58990b7855
0xd34df00d <0xd34df00d@gmail.com>
2021-01-06 09:37:00 -0600

    [Util] Fix whitespaces

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -19,232 +19,236 @@
 #include <QAction>
 #include <QBuffer>
 #include <QAction>
+#include <QModelIndexList>
 #include <QtDebug>
 
-QString LC::Util::GetAsBase64Src (const QImage& pix)
+namespace LC::Util
 {
-	QBuffer buf;
-	buf.open (QIODevice::ReadWrite);
-	pix.save (&buf, "PNG", 100);
-	return QString ("data:image/png;base64,%1")
-			.arg (QString (buf.buffer ().toBase64 ()));
-}
-
-QString LC::Util::GetUserText (const Entity& p)
-{
-	QString string = QObject::tr ("Too long to show");
-	if (p.Additional_.contains ("UserVisibleName") &&
-			p.Additional_ ["UserVisibleName"].canConvert<QString> ())
-		string = p.Additional_ ["UserVisibleName"].toString ();
-	else if (p.Entity_.canConvert<QByteArray> ())
+	QString GetAsBase64Src (const QImage& pix)
 	{
-		QByteArray entity = p.Entity_.toByteArray ();
-		if (entity.size () < 100)
-			string = QTextCodec::codecForName ("UTF-8")->toUnicode (entity);
-	}
-	else if (p.Entity_.canConvert<QUrl> ())
-	{
-		string = p.Entity_.toUrl ().toString ();
-		if (string.size () > 100)
-			string = string.left (97) + "...";
-	}
-	else
-		string = QObject::tr ("Binary entity");
-
-	if (!p.Mime_.isEmpty ())
-		string += QObject::tr ("<br /><br />of type <code>%1</code>").arg (p.Mime_);
-
-	if (!p.Additional_ ["SourceURL"].toUrl ().isEmpty ())
-	{
-		QString urlStr = p.Additional_ ["SourceURL"].toUrl ().toString ();
-		if (urlStr.size () > 63)
-			urlStr = urlStr.left (60) + "...";
-		string += QObject::tr ("<br />from %1")
-			.arg (urlStr);
+		QBuffer buf;
+		buf.open (QIODevice::ReadWrite);
+		const auto compression = 100;
+		pix.save (&buf, "PNG", compression);
+		return QString ("data:image/png;base64,%1")
+				.arg (QString (buf.buffer ().toBase64 ()));
 	}
 
-	return string;
-}
-
-namespace
-{
-	QString MakePrettySizeWith (qint64 sourceSize, const QStringList& units)
+	QString GetUserText (const Entity& p)
 	{
-		int strNum = 0;
-		long double size = sourceSize;
-
-		for (; strNum < 3 && size >= 1024; ++strNum, size /= 1024)
-			;
-
-		return QString::number (size, 'f', 1) + units.value (strNum);
-	}
-}
-
-QString LC::Util::MakePrettySize (qint64 sourcesize)
-{
-	static QStringList units
-	{
-		QObject::tr (" b"),
-		QObject::tr (" KiB"),
-		QObject::tr (" MiB"),
-		QObject::tr (" GiB")
-	};
-
-	return MakePrettySizeWith (sourcesize, units);
-}
-
-QString LC::Util::MakePrettySizeShort (qint64 sourcesize)
-{
-	static const QStringList units
-	{
-		QObject::tr ("b", "Short one-character unit for bytes."),
-		QObject::tr ("K", "Short one-character unit for kilobytes."),
-		QObject::tr ("M", "Short one-character unit for megabytes."),
-		QObject::tr ("G", "Short one-character unit for gigabytes.")
-	};
-
-	return MakePrettySizeWith (sourcesize, units);
-}
-
-QString LC::Util::MakeTimeFromLong (ulong time)
-{
-	int d = time / 86400;
-	time -= d * 86400;
-	QString result;
-	if (d)
-		result += QObject::tr ("%n day(s), ", "", d);
-	result += QTime (0, 0, 0).addSecs (time).toString ();
-	return result;
-}
-
-QTranslator* LC::Util::LoadTranslator (const QString& baseName,
-		const QString& localeName,
-		const QString& prefix,
-		const QString& appName)
-{
-	auto filename = prefix;
-	filename.append ("_");
-	if (!baseName.isEmpty ())
-		filename.append (baseName).append ("_");
-	filename.append (localeName);
-
-	auto transl = new QTranslator;
-#ifdef Q_OS_WIN32
-	Q_UNUSED (appName)
-	if (transl->load (filename, ":/") ||
-			transl->load (filename,
-					QCoreApplication::applicationDirPath () + "/translations"))
-#elif defined (Q_OS_MAC) && !defined (USE_UNIX_LAYOUT)
-	Q_UNUSED (appName)
-	if (transl->load (filename, ":/") ||
-			transl->load (filename,
-					QCoreApplication::applicationDirPath () + "/../Resources/translations"))
-#elif defined (INSTALL_PREFIX)
-	if (transl->load (filename, ":/") ||
-			transl->load (filename,
-					QString (INSTALL_PREFIX "/share/%1/translations").arg (appName)))
-#else
-	if (transl->load (filename, ":/") ||
-			transl->load (filename,
-					QString ("/usr/local/share/%1/translations").arg (appName)) ||
-			transl->load (filename,
-					QString ("/usr/share/%1/translations").arg (appName)))
-#endif
-		return transl;
-
-	delete transl;
-
-	return nullptr;
-}
-
-QTranslator* LC::Util::InstallTranslator (const QString& baseName,
-		const QString& prefix,
-		const QString& appName)
-{
-	const auto& localeName = GetLocaleName ();
-	if (auto transl = LoadTranslator (baseName, localeName, prefix, appName))
-	{
-		qApp->installTranslator (transl);
-		return transl;
-	}
-
-	qWarning () << Q_FUNC_INFO
-			<< "could not load translation file for locale"
-			<< localeName
-			<< baseName
-			<< prefix
-			<< appName;
-	return nullptr;
-}
-
-QString LC::Util::GetLocaleName ()
-{
-	QSettings settings (QCoreApplication::organizationName (),
-			QCoreApplication::applicationName ());
-	QString localeName = settings.value ("Language", "system").toString ();
-
-	if (localeName == "system")
-	{
-		localeName = QString (::getenv ("LANG")).left (5);
-
-		if (localeName == "C" || localeName.isEmpty ())
-			localeName = "en_US";
-
-		if (localeName.isEmpty () || localeName.size () != 5)
-			localeName = QLocale::system ().name ();
-		localeName = localeName.left (5);
-	}
-
-	if (localeName.size () == 2)
-	{
-		auto lang = QLocale (localeName).language ();
-		const auto& cs = QLocale::countriesForLanguage (lang);
-		if (cs.isEmpty ())
-			localeName += "_00";
-		else
-			localeName = QLocale (lang, cs.at (0)).name ();
-	}
-
-	return localeName;
-}
-
-QString LC::Util::GetInternetLocaleName (const QLocale& locale)
-{
-	if (locale.language () == QLocale::AnyLanguage)
-		return "*";
-
-	auto locStr = locale.name ();
-	locStr.replace ('_', '-');
-	return locStr;
-}
-
-QString LC::Util::GetLanguage ()
-{
-	return GetLocaleName ().left (2);
-}
-
-QModelIndexList LC::Util::GetSummarySelectedRows (QObject *sender)
-{
-	QAction *senderAct = qobject_cast<QAction*> (sender);
-	if (!senderAct)
-	{
-		QString debugString;
+		QString string = QObject::tr ("Too long to show");
+		if (p.Additional_.contains ("UserVisibleName") &&
+				p.Additional_ ["UserVisibleName"].canConvert<QString> ())
+			string = p.Additional_ ["UserVisibleName"].toString ();
+		else if (p.Entity_.canConvert<QByteArray> ())
 		{
-			QDebug d (&debugString);
-			d << "sender is not a QAction*"
-					<< sender;
+			QByteArray entity = p.Entity_.toByteArray ();
+			if (entity.size () < 100)
+				string = QTextCodec::codecForName ("UTF-8")->toUnicode (entity);
 		}
-		throw std::runtime_error (qPrintable (debugString));
+		else if (p.Entity_.canConvert<QUrl> ())
+		{
+			string = p.Entity_.toUrl ().toString ();
+			if (string.size () > 100)
+				string = string.left (97) + "...";
+		}
+		else
+			string = QObject::tr ("Binary entity");
+
+		if (!p.Mime_.isEmpty ())
+			string += QObject::tr ("<br /><br />of type <code>%1</code>").arg (p.Mime_);
+
+		if (!p.Additional_ ["SourceURL"].toUrl ().isEmpty ())
+		{
+			QString urlStr = p.Additional_ ["SourceURL"].toUrl ().toString ();
+			if (urlStr.size () > 63)
+				urlStr = urlStr.left (60) + "...";
+			string += QObject::tr ("<br />from %1")
+				.arg (urlStr);
+		}
+
+		return string;
 	}
 
-	return senderAct->
-			property ("SelectedRows").value<QList<QModelIndex>> ();
-}
+	namespace
+	{
+		QString MakePrettySizeWith (qint64 sourceSize, const QStringList& units)
+		{
+			int strNum = 0;
+			long double size = sourceSize;
 
-QAction* LC::Util::CreateSeparator (QObject *parent)
-{
-	QAction *result = new QAction (parent);
-	result->setSeparator (true);
-	return result;
-}
+			for (; strNum < 3 && size >= 1024; ++strNum, size /= 1024)
+				;
 
+			return QString::number (size, 'f', 1) + units.value (strNum);
+		}
+	}
+
+	QString MakePrettySize (qint64 sourcesize)
+	{
+		static QStringList units
+		{
+			QObject::tr (" b"),
+			QObject::tr (" KiB"),
+			QObject::tr (" MiB"),
+			QObject::tr (" GiB")
+		};
+
+		return MakePrettySizeWith (sourcesize, units);
+	}
+
+	QString MakePrettySizeShort (qint64 sourcesize)
+	{
+		static const QStringList units
+		{
+			QObject::tr ("b", "Short one-character unit for bytes."),
+			QObject::tr ("K", "Short one-character unit for kilobytes."),
+			QObject::tr ("M", "Short one-character unit for megabytes."),
+			QObject::tr ("G", "Short one-character unit for gigabytes.")
+		};
+
+		return MakePrettySizeWith (sourcesize, units);
+	}
+
+	QString MakeTimeFromLong (ulong time)
+	{
+		int d = time / 86400;
+		time -= d * 86400;
+		QString result;
+		if (d)
+			result += QObject::tr ("%n day(s), ", "", d);
+		result += QTime (0, 0, 0).addSecs (time).toString ();
+		return result;
+	}
+
+	QTranslator* LoadTranslator (const QString& baseName,
+			const QString& localeName,
+			const QString& prefix,
+			const QString& appName)
+	{
+		auto filename = prefix;
+		filename.append ("_");
+		if (!baseName.isEmpty ())
+			filename.append (baseName).append ("_");
+		filename.append (localeName);
+
+		auto transl = new QTranslator;
+	#ifdef Q_OS_WIN32
+		Q_UNUSED (appName)
+		if (transl->load (filename, ":/") ||
+				transl->load (filename,
+						QCoreApplication::applicationDirPath () + "/translations"))
+	#elif defined (Q_OS_MAC) && !defined (USE_UNIX_LAYOUT)
+		Q_UNUSED (appName)
+		if (transl->load (filename, ":/") ||
+				transl->load (filename,
+						QCoreApplication::applicationDirPath () + "/../Resources/translations"))
+	#elif defined (INSTALL_PREFIX)
+		if (transl->load (filename, ":/") ||
+				transl->load (filename,
+						QString (INSTALL_PREFIX "/share/%1/translations").arg (appName)))
+	#else
+		if (transl->load (filename, ":/") ||
+				transl->load (filename,
+						QString ("/usr/local/share/%1/translations").arg (appName)) ||
+				transl->load (filename,
+						QString ("/usr/share/%1/translations").arg (appName)))
+	#endif
+			return transl;
+
+		delete transl;
+
+		return nullptr;
+	}
+
+	QTranslator* InstallTranslator (const QString& baseName,
+			const QString& prefix,
+			const QString& appName)
+	{
+		const auto& localeName = GetLocaleName ();
+		if (auto transl = LoadTranslator (baseName, localeName, prefix, appName))
+		{
+			qApp->installTranslator (transl);
+			return transl;
+		}
+
+		qWarning () << Q_FUNC_INFO
+				<< "could not load translation file for locale"
+				<< localeName
+				<< baseName
+				<< prefix
+				<< appName;
+		return nullptr;
+	}
+
+	QString GetLocaleName ()
+	{
+		QSettings settings (QCoreApplication::organizationName (),
+				QCoreApplication::applicationName ());
+		QString localeName = settings.value ("Language", "system").toString ();
+
+		if (localeName == "system")
+		{
+			localeName = QString (::getenv ("LANG")).left (5);
+
+			if (localeName == "C" || localeName.isEmpty ())
+				localeName = "en_US";
+
+			if (localeName.isEmpty () || localeName.size () != 5)
+				localeName = QLocale::system ().name ();
+			localeName = localeName.left (5);
+		}
+
+		if (localeName.size () == 2)
+		{
+			auto lang = QLocale (localeName).language ();
+			const auto& cs = QLocale::countriesForLanguage (lang);
+			if (cs.isEmpty ())
+				localeName += "_00";
+			else
+				localeName = QLocale (lang, cs.at (0)).name ();
+		}
+
+		return localeName;
+	}
+
+	QString GetInternetLocaleName (const QLocale& locale)
+	{
+		if (locale.language () == QLocale::AnyLanguage)
+			return "*";
+
+		auto locStr = locale.name ();
+		locStr.replace ('_', '-');
+		return locStr;
+	}
+
+	QString GetLanguage ()
+	{
+		return GetLocaleName ().left (2);
+	}
+
+	QModelIndexList GetSummarySelectedRows (QObject *sender)
+	{
+		QAction *senderAct = qobject_cast<QAction*> (sender);
+		if (!senderAct)
+		{
+			QString debugString;
+			{
+				QDebug d (&debugString);
+				d << "sender is not a QAction*"
+						<< sender;
+			}
+			throw std::runtime_error (qPrintable (debugString));
+		}
+
+		return senderAct->
+				property ("SelectedRows").value<QList<QModelIndex>> ();
+	}
+
+	QAction* CreateSeparator (QObject *parent)
+	{
+		QAction *result = new QAction (parent);
+		result->setSeparator (true);
+		return result;
+	}
+}
--- src/util/util.h
+++ src/util/util.h
@@ -20,220 +20,217 @@ class QTranslator;
 class QLocale;
 class QAction;
 
-namespace LC
+namespace LC::Util
 {
-	namespace Util
+	/** @brief Returns the given image in a Base64-encoded form.
+	 *
+	 * The return result is suitable for inserting into
+	 * <code>&lt;img></code>'s <code>src</code> attribute as is.
+	 *
+	 * @param[in] image The image to represent as Base64-encoded
+	 * form.
+	 * @return The source string.
+	 */
+	UTIL_API QString GetAsBase64Src (const QImage& image);
+
+	/** @brief Return the user-readable representation of the entity.
+	 *
+	 * @param[in] entity The Entity from which to make the
+	 * user-readable representation.
+	 * @return The user-readable string describing the entity.
+	 */
+	UTIL_API QString GetUserText (const Entity& entity);
+
+	/** @brief Makes a formatted size from number.
+	 *
+	 * Converts, for example, 1048576 to 1.0 MB.
+	 *
+	 * @param[in] sourceSize Size in bytes.
+	 * @return Formatted string.
+	 *
+	 * @sa MakeTimeFromLong()
+	 * @sa MakePrettySizeShort()
+	 */
+	UTIL_API QString MakePrettySize (qint64 sourceSize);
+
+	/** @brief Converts a bytes count to a string representation with
+	 * appropriately chosen units.
+	 *
+	 * Converts, for example, <em>1048576</em> to <em>1.0 M</em>.
+	 *
+	 * As opposed to MakePrettySize(), this function tries to keep the
+	 * returned string short.
+	 *
+	 * @param[in] size Size in bytes.
+	 * @return Formatted string in relevant units.
+	 *
+	 * @sa MakeTimeFromLong()
+	 * @sa MakePrettySize()
+	 */
+	UTIL_API QString MakePrettySizeShort (qint64 size);
+
+	/** @brief Makes a formatted time from number.
+	 *
+	 * Converts, for example 256 to 00:04:16.
+	 *
+	 * @param[in] time Time interval in seconds.
+	 * @return DateTime object.
+	 *
+	 * @sa MakePrettySize
+	 */
+	UTIL_API QString MakeTimeFromLong (ulong time);
+
+	UTIL_API QTranslator* LoadTranslator (const QString& base,
+			const QString& locale,
+			const QString& prefix = "leechcraft",
+			const QString& appname = "leechcraft");
+
+	/** @brief Loads and installs a translator.
+	 *
+	 * Attempts to load and install a translator for the current
+	 * locale. The name is formed like this:
+	 * 'prefix_' + base + '_' + locale
+	 * If base is an empty string, the second _ isn't appended.
+	 *
+	 * First resources are searched (:/), then APPDIR/translations
+	 * on Windows and /usr/[local/]share/appname/translations on
+	 * Unix.
+	 *
+	 * @param[in] base Base name of the translation file.
+	 * @param[in] prefix The optional prefix of the translation
+	 * (useful if it's not LC's one).
+	 * @param[in] appname Base name of the application.
+	 * @return The translator object if loading is successful, NULL
+	 * otherwise.
+	 */
+	UTIL_API QTranslator* InstallTranslator (const QString& base,
+			const QString& prefix = "leechcraft",
+			const QString& appname = "leechcraft");
+
+	/** @brief Returns the current locale name, like en_US.
+	 *
+	 * First, this function checks the locale value stored in
+	 * "Language" key of settings object with organizationName() and
+	 * applicationName(). If it's equal to "system", this function
+	 * queries the LANG environment variable, and if it is empty or
+	 * in invalid format (not like en_US), it takes the value of
+	 * QLocale::system().name().
+	 *
+	 * Then, if the resulting name the name of the language only,
+	 * GetLocaleName() tries to find any countries for that
+	 * language. If any countries are found, the code of the first
+	 * found country is appended, else "_00" is appended.
+	 *
+	 * @return Current locale name.
+	 *
+	 * @sa GetLanguage()
+	 */
+	UTIL_API QString GetLocaleName ();
+
+	UTIL_API QString GetInternetLocaleName (const QLocale&);
+
+	/** @brief Returns the current language name.
+	 *
+	 * This function works as GetLocaleName() except it doesn't
+	 * return (and doesn't query for) country name.
+	 *
+	 * @return Current language name.
+	 *
+	 * @sa GetLocaleName()
+	 */
+	UTIL_API QString GetLanguage ();
+
+
+	UTIL_API QList<QModelIndex> GetSummarySelectedRows (QObject *sender);
+
+	/** @brief Returns the action that is set to act as a separator.
+	 *
+	 * That is the action with setSeparator(true);
+	 *
+	 * @param[in] parent The parent of the action.
+	 * @return The separator action.
+	 */
+	UTIL_API QAction* CreateSeparator (QObject *parent);
+
+	/** @brief Returns an element for a given tags list.
+	 *
+	 * This function tries to implement projection from tags to a
+	 * hierarchical structure in form of XML. It traverses the tags
+	 * list and creates child nodes from the document, appending
+	 * the hierarchical structure's tree root to the node. It
+	 * returns the parent element to which the item should be
+	 * appended.
+	 *
+	 * For empty tags list it just returns node converted to the
+	 * QDomElement.
+	 *
+	 * tagSetter is a function or functor that should be able to
+	 * take two parameters, a QDomElement and a QString, and set
+	 * tags for it.
+	 *
+	 * tagGetter is a function or functor that should be able to
+	 * take one parameter, a QDomElement, and return a QString for
+	 * it with tags previously set with tagSetter.
+	 *
+	 * @param[in] tags List of tags.
+	 * @param[in] node The parent-most node to which all other nodes
+	 * are appended.
+	 * @param[in] document The document containing all these nodes.
+	 * @param[in] elementName The name of the XML element that
+	 * carries info about the tags.
+	 * @param[in] tagSetter Setter function for the tags for the
+	 * given element.
+	 * @param[in] tagGetter Getter function for the tags for the
+	 * given element.
+	 * @return Parent element of the item with tags.
+	 */
+	template<typename TagGetter, typename TagSetter>
+	QDomElement GetElementForTags (const QStringList& tags,
+			QDomNode& node,
+			QDomDocument& document,
+			const QString& elementName,
+			TagGetter tagGetter,
+			TagSetter tagSetter)
 	{
-		/** @brief Returns the given image in a Base64-encoded form.
-		 *
-		 * The return result is suitable for inserting into
-		 * <code>&lt;img></code>'s <code>src</code> attribute as is.
-		 *
-		 * @param[in] image The image to represent as Base64-encoded
-		 * form.
-		 * @return The source string.
-		 */
-		UTIL_API QString GetAsBase64Src (const QImage& image);
-
-		/** @brief Return the user-readable representation of the entity.
-		 *
-		 * @param[in] entity The Entity from which to make the
-		 * user-readable representation.
-		 * @return The user-readable string describing the entity.
-		 */
-		UTIL_API QString GetUserText (const Entity& entity);
-
-		/** @brief Makes a formatted size from number.
-		 *
-		 * Converts, for example, 1048576 to 1.0 MB.
-		 *
-		 * @param[in] sourceSize Size in bytes.
-		 * @return Formatted string.
-		 *
-		 * @sa MakeTimeFromLong()
-		 * @sa MakePrettySizeShort()
-		 */
-		UTIL_API QString MakePrettySize (qint64 sourceSize);
-
-		/** @brief Converts a bytes count to a string representation with
-		 * appropriately chosen units.
-		 *
-		 * Converts, for example, <em>1048576</em> to <em>1.0 M</em>.
-		 *
-		 * As opposed to MakePrettySize(), this function tries to keep the
-		 * returned string short.
-		 *
-		 * @param[in] size Size in bytes.
-		 * @return Formatted string in relevant units.
-		 *
-		 * @sa MakeTimeFromLong()
-		 * @sa MakePrettySize()
-		 */
-		UTIL_API QString MakePrettySizeShort (qint64 size);
-
-		/** @brief Makes a formatted time from number.
-		 *
-		 * Converts, for example 256 to 00:04:16.
-		 *
-		 * @param[in] time Time interval in seconds.
-		 * @return DateTime object.
-		 *
-		 * @sa MakePrettySize
-		 */
-		UTIL_API QString MakeTimeFromLong (ulong time);
-
-		UTIL_API QTranslator* LoadTranslator (const QString& base,
-				const QString& locale,
-				const QString& prefix = "leechcraft",
-				const QString& appname = "leechcraft");
-
-		/** @brief Loads and installs a translator.
-		 *
-		 * Attempts to load and install a translator for the current
-		 * locale. The name is formed like this:
-		 * 'prefix_' + base + '_' + locale
-		 * If base is an empty string, the second _ isn't appended.
-		 *
-		 * First resources are searched (:/), then APPDIR/translations
-		 * on Windows and /usr/[local/]share/appname/translations on
-		 * Unix.
-		 *
-		 * @param[in] base Base name of the translation file.
-		 * @param[in] prefix The optional prefix of the translation
-		 * (useful if it's not LC's one).
-		 * @param[in] appname Base name of the application.
-		 * @return The translator object if loading is successful, NULL
-		 * otherwise.
-		 */
-		UTIL_API QTranslator* InstallTranslator (const QString& base,
-				const QString& prefix = "leechcraft",
-				const QString& appname = "leechcraft");
-
-		/** @brief Returns the current locale name, like en_US.
-		 *
-		 * First, this function checks the locale value stored in
-		 * "Language" key of settings object with organizationName() and
-		 * applicationName(). If it's equal to "system", this function
-		 * queries the LANG environment variable, and if it is empty or
-		 * in invalid format (not like en_US), it takes the value of
-		 * QLocale::system().name().
-		 *
-		 * Then, if the resulting name the name of the language only,
-		 * GetLocaleName() tries to find any countries for that
-		 * language. If any countries are found, the code of the first
-		 * found country is appended, else "_00" is appended.
-		 *
-		 * @return Current locale name.
-		 *
-		 * @sa GetLanguage()
-		 */
-		UTIL_API QString GetLocaleName ();
-
-		UTIL_API QString GetInternetLocaleName (const QLocale&);
-
-		/** @brief Returns the current language name.
-		 *
-		 * This function works as GetLocaleName() except it doesn't
-		 * return (and doesn't query for) country name.
-		 *
-		 * @return Current language name.
-		 *
-		 * @sa GetLocaleName()
-		 */
-		UTIL_API QString GetLanguage ();
-
-
-		UTIL_API QModelIndexList GetSummarySelectedRows (QObject *sender);
-
-		/** @brief Returns the action that is set to act as a separator.
-		 *
-		 * That is the action with setSeparator(true);
-		 *
-		 * @param[in] parent The parent of the action.
-		 * @return The separator action.
-		 */
-		UTIL_API QAction* CreateSeparator (QObject *parent);
-
-		/** @brief Returns an element for a given tags list.
-		 *
-		 * This function tries to implement projection from tags to a
-		 * hierarchical structure in form of XML. It traverses the tags
-		 * list and creates child nodes from the document, appending
-		 * the hierarchical structure's tree root to the node. It
-		 * returns the parent element to which the item should be
-		 * appended.
-		 *
-		 * For empty tags list it just returns node converted to the
-		 * QDomElement.
-		 *
-		 * tagSetter is a function or functor that should be able to
-		 * take two parameters, a QDomElement and a QString, and set
-		 * tags for it.
-		 *
-		 * tagGetter is a function or functor that should be able to
-		 * take one parameter, a QDomElement, and return a QString for
-		 * it with tags previously set with tagSetter.
-		 *
-		 * @param[in] tags List of tags.
-		 * @param[in] node The parent-most node to which all other nodes
-		 * are appended.
-		 * @param[in] document The document containing all these nodes.
-		 * @param[in] elementName The name of the XML element that
-		 * carries info about the tags.
-		 * @param[in] tagSetter Setter function for the tags for the
-		 * given element.
-		 * @param[in] tagGetter Getter function for the tags for the
-		 * given element.
-		 * @return Parent element of the item with tags.
-		 */
-		template<typename TagGetter, typename TagSetter>
-		QDomElement GetElementForTags (const QStringList& tags,
-				QDomNode& node,
-				QDomDocument& document,
-				const QString& elementName,
-				TagGetter tagGetter,
-				TagSetter tagSetter)
+		if (!tags.size ())
 		{
-			if (!tags.size ())
-			{
-				qWarning () << Q_FUNC_INFO
-					<< "no tags"
-					<< elementName;
-				return node.toElement ();
-			}
-
-			QDomNodeList elements = node.childNodes ();
-			for (int i = 0; i < elements.size (); ++i)
-			{
-				QDomElement elem = elements.at (i).toElement ();
-				if (tagGetter (elem) == tags.at (0))
-				{
-					if (tags.size () > 1)
-					{
-						QStringList childTags = tags;
-						childTags.removeAt (0);
-						return GetElementForTags (childTags, elem,
-								document, elementName,
-								tagGetter, tagSetter);
-					}
-					else
-						return elem;
-				}
-			}
-
-			QDomElement result = document.createElement (elementName);
-			tagSetter (result, tags.at (0));
-			node.appendChild (result);
-			if (tags.size () > 1)
-			{
-				QStringList childTags = tags;
-				childTags.removeAt (0);
-				return GetElementForTags (childTags, result,
-						document, elementName,
-						tagGetter, tagSetter);
-			}
-			else
-				return result;
+			qWarning () << Q_FUNC_INFO
+				<< "no tags"
+				<< elementName;
+			return node.toElement ();
 		}
+
+		QDomNodeList elements = node.childNodes ();
+		for (int i = 0; i < elements.size (); ++i)
+		{
+			QDomElement elem = elements.at (i).toElement ();
+			if (tagGetter (elem) == tags.at (0))
+			{
+				if (tags.size () > 1)
+				{
+					QStringList childTags = tags;
+					childTags.removeAt (0);
+					return GetElementForTags (childTags, elem,
+							document, elementName,
+							tagGetter, tagSetter);
+				}
+				else
+					return elem;
+			}
+		}
+
+		QDomElement result = document.createElement (elementName);
+		tagSetter (result, tags.at (0));
+		node.appendChild (result);
+		if (tags.size () > 1)
+		{
+			QStringList childTags = tags;
+			childTags.removeAt (0);
+			return GetElementForTags (childTags, result,
+					document, elementName,
+					tagGetter, tagSetter);
+		}
+		else
+			return result;
 	}
 }
