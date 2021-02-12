ab9884a0175ca74af8fadf3dd2824babbc3e050c
0xd34df00d <0xd34df00d@gmail.com>
2021-02-12 09:14:47 -0600

    [BT] AlertDispatcher fixes

--- src/plugins/bittorrent/alertdispatcher.h
+++ src/plugins/bittorrent/alertdispatcher.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <list>
+#include <memory>
 #include <QHash>
 #include <util/sll/typegetter.h>
 
@@ -20,34 +21,34 @@ namespace libtorrent
 
 namespace LC::BitTorrent
 {
+	using AlertHandler_f = std::function<bool (const libtorrent::alert&)>;
+
+	class AlertDispatcherRegGuard
+	{
+		std::list<AlertHandler_f>& HandlerList_;
+		std::list<AlertHandler_f>::iterator Pos_;
+	public:
+		AlertDispatcherRegGuard (std::list<AlertHandler_f>& list, std::list<AlertHandler_f>::iterator pos)
+		: HandlerList_ { list }
+		, Pos_ { pos }
+		{
+		}
+
+		AlertDispatcherRegGuard (AlertDispatcherRegGuard&&) = delete;
+		AlertDispatcherRegGuard (const AlertDispatcherRegGuard&) = delete;
+		AlertDispatcherRegGuard& operator= (AlertDispatcherRegGuard&&) = delete;
+		AlertDispatcherRegGuard& operator= (const AlertDispatcherRegGuard&) = delete;
+
+		~AlertDispatcherRegGuard ()
+		{
+			HandlerList_.erase (Pos_);
+		}
+	};
+
 	class AlertDispatcher
 	{
 		libtorrent::session& Session_;
-
-		using Handler_f = std::function<bool (const libtorrent::alert&)>;
-		QHash<int, std::list<Handler_f>> Handlers_;
-
-		class RegGuard
-		{
-			std::list<Handler_f>& HandlerList_;
-			std::list<Handler_f>::iterator Pos_;
-
-			RegGuard (std::list<Handler_f>& list, std::list<Handler_f>::iterator pos)
-			: HandlerList_ { list }
-			, Pos_ { pos }
-			{
-			}
-		public:
-			RegGuard (RegGuard&&) = delete;
-			RegGuard (const RegGuard&) = delete;
-			RegGuard& operator= (RegGuard&&) = delete;
-			RegGuard& operator= (const RegGuard&) = delete;
-
-			~RegGuard ()
-			{
-				HandlerList_.erase (Pos_);
-			}
-		};
+		QHash<int, std::list<AlertHandler_f>> Handlers_;
 	public:
 		explicit AlertDispatcher (libtorrent::session&);
 
@@ -55,7 +56,8 @@ namespace LC::BitTorrent
 		int RegisterHandler (F&& f)
 		{
 			using AlertType_t = std::decay_t<Util::ArgType_t<F, 0>>;
-			auto& list = Handlers_ [AlertType_t::alert_type];
+			constexpr int type = AlertType_t::alert_type;
+			auto& list = Handlers_ [type];
 			list.push_front ([f] (const libtorrent::alert& alert)
 					{
 						if constexpr (std::is_same_v<Util::RetType_t<F>, bool>)
@@ -66,15 +68,15 @@ namespace LC::BitTorrent
 							return true;
 						}
 					});
-			return AlertType_t::alert_type;
+			return type;
 		}
 
 		template<typename F>
-		RegGuard RegisterTemporaryHandler (F&& f)
+		std::unique_ptr<AlertDispatcherRegGuard> RegisterTemporaryHandler (F&& f)
 		{
-			auto alertType = RegisterPermanentHandler (std::forward<F> (f));
+			auto alertType = RegisterHandler (std::forward<F> (f));
 			auto& list = Handlers_ [alertType];
-			return RegGuard { list, list.begin () };
+			return std::make_unique<AlertDispatcherRegGuard> (list, list.begin ());
 		}
 
 		void Swallow (int alertType, bool logging);
