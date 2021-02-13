6c15f1b62f32cad58d52b2196e597048faeb6dfc
0xd34df00d <0xd34df00d@gmail.com>
2021-02-13 12:20:15 -0600

    [Util] Simplify TypeGetter
    No need to work around voids, `std::tuple<void>` is totally fine in this
    context.

--- src/util/sll/typegetter.h
+++ src/util/sll/typegetter.h
@@ -9,51 +9,32 @@
 #pragma once
 
 #include <tuple>
-#include <type_traits>
 
-namespace LC
-{
-namespace Util
+namespace LC::Util
 {
 	namespace detail
 	{
-		struct ReturnsVoid;
-
-		template<typename T>
-		using VoidSafe = std::conditional_t<
-				std::is_same<T, void> {}, // C++17
-				ReturnsVoid,
-				T
-			>;
-
 		template<typename R, typename... Args>
-		std::tuple<VoidSafe<R>, Args...> TypeGetter (R (*) (Args...));
+		std::tuple<R, Args...> TypeGetter (R (*) (Args...));
 
 		template<typename F>
 		auto TypeGetter (F&& f) -> decltype (TypeGetter (+f));
 
 		template<typename C, typename R, typename... Args>
-		std::tuple<VoidSafe<R>, Args...> TypeGetter (R (C::*) (Args...) const);
+		std::tuple<R, Args...> TypeGetter (R (C::*) (Args...) const);
 
 		template<typename C, typename R, typename... Args>
-		std::tuple<VoidSafe<R>, Args...> TypeGetter (R (C::*) (Args...));
+		std::tuple<R, Args...> TypeGetter (R (C::*) (Args...));
 
 		template<typename C>
 		decltype (TypeGetter (&C::operator ())) TypeGetter (const C& c);
-
-		template<typename F>
-		using RetTypeRaw_t = std::tuple_element_t<0, decltype (TypeGetter (*static_cast<F*> (nullptr)))>;
 	}
 
 	template<typename F, size_t Idx>
 	using ArgType_t = std::tuple_element_t<Idx + 1, decltype (detail::TypeGetter (*static_cast<F*> (nullptr)))>;
 
 	template<typename F>
-	using RetType_t = std::conditional_t<
-			std::is_same_v<detail::RetTypeRaw_t<F>, detail::ReturnsVoid>,
-			void,
-			detail::RetTypeRaw_t<F>
-		>;
+	using RetType_t = std::tuple_element_t<0, decltype (detail::TypeGetter (*static_cast<F*> (nullptr)))>;
 
 	namespace detail
 	{
@@ -81,5 +62,4 @@ namespace Util
 
 	template<auto Ptr>
 	using MemberPtrStruct_t = MemberTypeStruct_t<decltype (Ptr)>;
-}
 }
