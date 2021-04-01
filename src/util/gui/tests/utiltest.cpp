97daef12df31ce0b7b9719e6566e76af3c87a2d0
0xd34df00d <0xd34df00d@gmail.com>
2021-04-01 09:46:19 -0500

    [Util] Skeleton GUI util tests

---
+++ src/util/gui/tests/utiltest.cpp
@@ -0,0 +1,13 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2014  Georg Rudoy
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
+ **********************************************************************/
+
+#include "utiltest.h"
+#include <QtTest>
+#include "util.h"
+
+QTEST_MAIN (LC::Util::UtilTest)
