331dd84af6feadb435fda3de8de0ddeadea67490
0xd34df00d <0xd34df00d@gmail.com>
2021-06-28 01:29:54 -0500

    [Interfaces] Rename structuresconstants → entityconstants

--- src/util/xpc/anconstantsdefs.cpp
+++ src/util/xpc/anconstantsdefs.cpp
@@ -6,7 +6,7 @@
  * (See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
  **********************************************************************/
 
-#include <interfaces/structuresconstants.h>
+#include <interfaces/entityconstants.h>
 #include <interfaces/an/constants.h>
 #include <interfaces/an/entityfields.h>
 
