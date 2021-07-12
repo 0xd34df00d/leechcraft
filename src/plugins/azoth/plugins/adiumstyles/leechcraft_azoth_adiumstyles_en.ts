b3f7540686249dba96f10cfbf81316b6f59b695d
0xd34df00d <0xd34df00d@gmail.com>
2021-07-12 00:31:29 -0500

    [Azoth AS] Update translations

--- src/plugins/azoth/plugins/adiumstyles/leechcraft_azoth_adiumstyles_en.ts
+++ src/plugins/azoth/plugins/adiumstyles/leechcraft_azoth_adiumstyles_en.ts
@@ -1,30 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LC::Azoth::AdiumStyles::Plugin</name>
     <message>
-        <location filename="adiumstyles.cpp" line="68"/>
+        <location filename="adiumstyles.cpp" line="49"/>
         <source>Support for Adium chat styles.</source>
         <translation>Support for Adium chat styles.</translation>
-    </message>
-</context>
-<context>
-    <name>QObject</name>
-    <message>
-        <location filename="plistparser.cpp" line="60"/>
-        <source>Unable to open file: %2.</source>
-        <translation>Unable to open file: %2.</translation>
-    </message>
-    <message>
-        <location filename="plistparser.cpp" line="69"/>
-        <source>Parse error: %1 (%2:%3).</source>
-        <translation>Parse error: %1 (%2:%3).</translation>
-    </message>
-    <message>
-        <location filename="plistparser.cpp" line="82"/>
-        <source>State mismatch: expected key tag.</source>
-        <translation>State mismatch: expected key tag.</translation>
     </message>
 </context>
 </TS>
--- src/plugins/azoth/plugins/adiumstyles/leechcraft_azoth_adiumstyles_ru_RU.ts
+++ src/plugins/azoth/plugins/adiumstyles/leechcraft_azoth_adiumstyles_ru_RU.ts
@@ -1,30 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LC::Azoth::AdiumStyles::Plugin</name>
     <message>
-        <location filename="adiumstyles.cpp" line="68"/>
+        <location filename="adiumstyles.cpp" line="49"/>
         <source>Support for Adium chat styles.</source>
         <translation>Поддержка Adium-стилей окна разговора.</translation>
-    </message>
-</context>
-<context>
-    <name>QObject</name>
-    <message>
-        <location filename="plistparser.cpp" line="60"/>
-        <source>Unable to open file: %2.</source>
-        <translation>Невозможно открыть файл: %2.</translation>
-    </message>
-    <message>
-        <location filename="plistparser.cpp" line="69"/>
-        <source>Parse error: %1 (%2:%3).</source>
-        <translation>Ошибка разбора: %1 (%2:%3).</translation>
-    </message>
-    <message>
-        <location filename="plistparser.cpp" line="82"/>
-        <source>State mismatch: expected key tag.</source>
-        <translation>Некорректное состояние: ожидался ключ.</translation>
     </message>
 </context>
 </TS>
