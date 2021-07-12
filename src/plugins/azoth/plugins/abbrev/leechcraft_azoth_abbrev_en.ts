00c8d659467d3704721fdfdb337a1d1375af6bf9
0xd34df00d <0xd34df00d@gmail.com>
2021-07-12 00:25:45 -0500

    [Azoth Abbrev] Update translations

--- src/plugins/azoth/plugins/abbrev/leechcraft_azoth_abbrev_en.ts
+++ src/plugins/azoth/plugins/abbrev/leechcraft_azoth_abbrev_en.ts
@@ -1,48 +1,48 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LC::Azoth::Abbrev::AbbrevsManager</name>
     <message>
-        <location filename="abbrevsmanager.cpp" line="52"/>
+        <location filename="abbrevsmanager.cpp" line="27"/>
         <source>Abbreviation with this pattern already exists.</source>
         <translation>Abbreviation with this pattern already exists.</translation>
     </message>
     <message>
-        <location filename="abbrevsmanager.cpp" line="55"/>
+        <location filename="abbrevsmanager.cpp" line="30"/>
         <source>Abbeviation pattern is empty.</source>
         <translation>Abbeviation pattern is empty.</translation>
     </message>
     <message>
-        <location filename="abbrevsmanager.cpp" line="58"/>
+        <location filename="abbrevsmanager.cpp" line="33"/>
         <source>Abbeviation expansion is empty.</source>
         <translation>Abbeviation expansion is empty.</translation>
     </message>
     <message>
-        <location filename="abbrevsmanager.cpp" line="123"/>
-        <source>Too much expansions during abbreviations application. Check your rules.</source>
-        <translation>Too much expansions during abbreviations application. Check your rules.</translation>
+        <location filename="abbrevsmanager.cpp" line="105"/>
+        <source>Too many expansions during abbreviations application. Check your rules.</source>
+        <translation>Too many expansions during abbreviations application. Check your rules.</translation>
     </message>
 </context>
 <context>
     <name>LC::Azoth::Abbrev::Plugin</name>
     <message>
-        <location filename="abbrev.cpp" line="69"/>
+        <location filename="abbrev.cpp" line="45"/>
         <source>Adds a new abbreviation to the list of abbreviations.</source>
         <translation>Adds a new abbreviation to the list of abbreviations.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="83"/>
+        <location filename="abbrev.cpp" line="59"/>
         <source>Lists all abbreviations that were previously added.</source>
         <translation>Lists all abbreviations that were previously added.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="99"/>
+        <location filename="abbrev.cpp" line="75"/>
         <source>Removes a previously added abbreviation.</source>
         <translation>Removes a previously added abbreviation.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="70"/>
+        <location filename="abbrev.cpp" line="46"/>
         <source>Usage: @/abbrev@ _pattern_ _text_
 
 Adds a new _pattern_ that expands to the given _text_, which can span multiple lines.
@@ -55,17 +55,17 @@ Adds a new _pattern_ that expands to the given _text_, which can span multiple l
 @/listabbrevs@ lists all available abbreviations and @/unabbrev@ allows removing them.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="65"/>
+        <location filename="abbrev.cpp" line="41"/>
         <source>Pattern %1 has been added successfully.</source>
         <translation>Pattern %1 has been added successfully.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="95"/>
+        <location filename="abbrev.cpp" line="71"/>
         <source>Pattern %1 has been removed successfully.</source>
         <translation>Pattern %1 has been removed successfully.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="100"/>
+        <location filename="abbrev.cpp" line="76"/>
         <source>Usage: @/unabbrev@ &lt;_pattern_|_index_&gt;
 
 Removes a previously added abbrevation either by its _pattern_ or by its _index_ in the list returned by @/listabbrevs@.</source>
@@ -74,12 +74,12 @@ Removes a previously added abbrevation either by its _pattern_ or by its _index_
 Removes a previously added abbrevation either by its _pattern_ or by its _index_ in the list returned by @/listabbrevs@.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="129"/>
+        <location filename="abbrev.cpp" line="105"/>
         <source>Provides support for automatically expanding abbreviations for Azoth.</source>
         <translation>Provides support for automatically expanding abbreviations for Azoth.</translation>
     </message>
     <message numerus="yes">
-        <location filename="abbrev.cpp" line="167"/>
+        <location filename="abbrev.cpp" line="143"/>
         <source>%n abbreviation(s):</source>
         <translation>
             <numerusform>%n abbreviation:</numerusform>
@@ -87,7 +87,7 @@ Removes a previously added abbrevation either by its _pattern_ or by its _index_
         </translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="195"/>
+        <location filename="abbrev.cpp" line="171"/>
         <source>Unable to find abbreviation %1.</source>
         <translation>Unable to find abbreviation %1.</translation>
     </message>
@@ -95,7 +95,7 @@ Removes a previously added abbrevation either by its _pattern_ or by its _index_
 <context>
     <name>LC::Azoth::Abbrev::ShortcutsManager</name>
     <message>
-        <location filename="shortcutsmanager.cpp" line="76"/>
+        <location filename="shortcutsmanager.cpp" line="45"/>
         <source>Expand abbreviations in current message edit text.</source>
         <translation>Expand abbreviations in current message edit text.</translation>
     </message>
--- src/plugins/azoth/plugins/abbrev/leechcraft_azoth_abbrev_ru_RU.ts
+++ src/plugins/azoth/plugins/abbrev/leechcraft_azoth_abbrev_ru_RU.ts
@@ -1,48 +1,48 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LC::Azoth::Abbrev::AbbrevsManager</name>
     <message>
-        <location filename="abbrevsmanager.cpp" line="52"/>
+        <location filename="abbrevsmanager.cpp" line="27"/>
         <source>Abbreviation with this pattern already exists.</source>
         <translation>Сокращение с этим шаблоном уже существует.</translation>
     </message>
     <message>
-        <location filename="abbrevsmanager.cpp" line="55"/>
+        <location filename="abbrevsmanager.cpp" line="30"/>
         <source>Abbeviation pattern is empty.</source>
         <translation>Шаблон сокращения пуст.</translation>
     </message>
     <message>
-        <location filename="abbrevsmanager.cpp" line="58"/>
+        <location filename="abbrevsmanager.cpp" line="33"/>
         <source>Abbeviation expansion is empty.</source>
         <translation>Расширение сокращения пусто.</translation>
     </message>
     <message>
-        <location filename="abbrevsmanager.cpp" line="123"/>
-        <source>Too much expansions during abbreviations application. Check your rules.</source>
-        <translation>Слишком много расширений при применении сокращений. Проверьте ваши правила.</translation>
+        <location filename="abbrevsmanager.cpp" line="105"/>
+        <source>Too many expansions during abbreviations application. Check your rules.</source>
+        <translation>Слишком много срабатываний при применении сокращений. Проверьте ваши правила.</translation>
     </message>
 </context>
 <context>
     <name>LC::Azoth::Abbrev::Plugin</name>
     <message>
-        <location filename="abbrev.cpp" line="69"/>
+        <location filename="abbrev.cpp" line="45"/>
         <source>Adds a new abbreviation to the list of abbreviations.</source>
         <translation>Добавляет новое сокращение.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="83"/>
+        <location filename="abbrev.cpp" line="59"/>
         <source>Lists all abbreviations that were previously added.</source>
         <translation>Отображает все ранее добавленные сокращения.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="99"/>
+        <location filename="abbrev.cpp" line="75"/>
         <source>Removes a previously added abbreviation.</source>
         <translation>Удаляет ранее добавленное сокращение.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="70"/>
+        <location filename="abbrev.cpp" line="46"/>
         <source>Usage: @/abbrev@ _pattern_ _text_
 
 Adds a new _pattern_ that expands to the given _text_, which can span multiple lines.
@@ -55,17 +55,17 @@ Adds a new _pattern_ that expands to the given _text_, which can span multiple l
 @/listabbrevs@ перечисляет все доступные сокращения, а @/unabbrev@ позволяет их удалять.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="65"/>
+        <location filename="abbrev.cpp" line="41"/>
         <source>Pattern %1 has been added successfully.</source>
         <translation>Шаблон %1 был успешно добавлен.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="95"/>
+        <location filename="abbrev.cpp" line="71"/>
         <source>Pattern %1 has been removed successfully.</source>
         <translation>Шаблон %1 был успешно удалён.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="100"/>
+        <location filename="abbrev.cpp" line="76"/>
         <source>Usage: @/unabbrev@ &lt;_pattern_|_index_&gt;
 
 Removes a previously added abbrevation either by its _pattern_ or by its _index_ in the list returned by @/listabbrevs@.</source>
@@ -74,12 +74,12 @@ Removes a previously added abbrevation either by its _pattern_ or by its _index_
 Удаляет ранее добавленное сокращение либо по соответствующему _шаблон_у, либо по _индекс_у в выдаче команды @/listabbrevs@.</translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="129"/>
+        <location filename="abbrev.cpp" line="105"/>
         <source>Provides support for automatically expanding abbreviations for Azoth.</source>
         <translation>Обеспечивает поддержку автоматически разворачиваемых сокращений для Azoth.</translation>
     </message>
     <message numerus="yes">
-        <location filename="abbrev.cpp" line="167"/>
+        <location filename="abbrev.cpp" line="143"/>
         <source>%n abbreviation(s):</source>
         <translation>
             <numerusform>%n сокращение:</numerusform>
@@ -88,7 +88,7 @@ Removes a previously added abbrevation either by its _pattern_ or by its _index_
         </translation>
     </message>
     <message>
-        <location filename="abbrev.cpp" line="195"/>
+        <location filename="abbrev.cpp" line="171"/>
         <source>Unable to find abbreviation %1.</source>
         <translation>Невозможно найти сокращение %1.</translation>
     </message>
@@ -96,7 +96,7 @@ Removes a previously added abbrevation either by its _pattern_ or by its _index_
 <context>
     <name>LC::Azoth::Abbrev::ShortcutsManager</name>
     <message>
-        <location filename="shortcutsmanager.cpp" line="76"/>
+        <location filename="shortcutsmanager.cpp" line="45"/>
         <source>Expand abbreviations in current message edit text.</source>
         <translation>Развернуть сокращения в текущем тексте редактора сообщения.</translation>
     </message>
