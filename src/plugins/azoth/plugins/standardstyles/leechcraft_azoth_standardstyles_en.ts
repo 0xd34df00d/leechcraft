023b9358e14c14dca60f9e43a90dfac5f1322255
0xd34df00d <0xd34df00d@gmail.com>
2021-07-12 00:29:51 -0500

    [Azoth SS] Update translations

--- src/plugins/azoth/plugins/standardstyles/leechcraft_azoth_standardstyles_en.ts
+++ src/plugins/azoth/plugins/standardstyles/leechcraft_azoth_standardstyles_en.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LC::Azoth::StandardStyles::Plugin</name>
     <message>
-        <location filename="standardstyles.cpp" line="69"/>
+        <location filename="standardstyles.cpp" line="44"/>
         <source>Support for standard Azoth chat styles.</source>
         <oldsource>Support for standard Azoth chat styles</oldsource>
         <translation>Support for standard Azoth chat styles.</translation>
--- src/plugins/azoth/plugins/standardstyles/leechcraft_azoth_standardstyles_ru_RU.ts
+++ src/plugins/azoth/plugins/standardstyles/leechcraft_azoth_standardstyles_ru_RU.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LC::Azoth::StandardStyles::Plugin</name>
     <message>
-        <location filename="standardstyles.cpp" line="69"/>
+        <location filename="standardstyles.cpp" line="44"/>
         <source>Support for standard Azoth chat styles.</source>
         <oldsource>Support for standard Azoth chat styles</oldsource>
         <translation>Поддержка стандартных стилей окна разговора Azoth.</translation>
