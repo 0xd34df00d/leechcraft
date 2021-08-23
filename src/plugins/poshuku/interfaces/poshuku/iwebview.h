75dd5ae2e5853c1c444ec0d0d0a936f4bd88bc2d
0xd34df00d <0xd34df00d@gmail.com>
2021-08-23 00:20:15 -0500

    [Poshuku] Add IWebView::NavigationType enum

--- src/plugins/poshuku/interfaces/poshuku/iwebview.h
+++ src/plugins/poshuku/interfaces/poshuku/iwebview.h
@@ -112,6 +112,17 @@ namespace Poshuku
 			ScrollAnimatorEnabled
 		};
 
+		enum class NavigationType
+		{
+			LinkClicked,
+			Typed,
+			FormSubmitted,
+			BackForward,
+			Reload,
+			Redirect,
+			Other,
+		};
+
 		class IFeatureSecurityOrigin
 		{
 		protected:
