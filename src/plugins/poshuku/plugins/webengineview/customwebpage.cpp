eec218daa4871908508b8eba0f3e685fa9e9a407
0xd34df00d <0xd34df00d@gmail.com>
2021-08-29 11:22:38 -0500

    [Poshuku WEV] Register CustomWebPage as hookable

--- src/plugins/poshuku/plugins/webengineview/customwebpage.cpp
+++ src/plugins/poshuku/plugins/webengineview/customwebpage.cpp
@@ -19,6 +19,8 @@ namespace LC::Poshuku::WebEngineView
 	, PoshukuProxy_ { poshukuProxy }
 	, LinkOpenModifier_ { poshukuProxy->GetLinkOpenModifier () }
 	{
+		poshukuProxy->RegisterHookable (this);
+
 		QTimer::singleShot (0, this,
 				[this]
 				{
