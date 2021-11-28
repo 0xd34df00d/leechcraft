d264aedf27e25c248dc2c137b4f1c073924a9647
0xd34df00d <0xd34df00d@gmail.com>
2021-11-28 09:53:47 -0600

    [Util] Add static compile-time hash

---
+++ src/util/sll/statichash.h
@@ -0,0 +1,51 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2014  Georg Rudoy
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
+ **********************************************************************/
+
+#pragma once
+
+#include <string_view>
+
+namespace LC::Util
+{
+	namespace
+	{
+		template<typename K, typename V>
+		struct KVPair
+		{
+			const K Key_;
+			const V Val_;
+
+			consteval KVPair (K name, V val)
+			: Key_ { name }
+			, Val_ { val }
+			{
+			}
+		};
+	}
+
+	template<typename K, typename V, auto Hasher>
+	consteval auto MakeHash (auto&&... commands)
+	{
+		const std::initializer_list<KVPair<K, V>> commandsList { commands... };
+		for (auto i = commandsList.begin (); i != std::prev (commandsList.end ()); ++i)
+			for (auto j = std::next (i); j != commandsList.end (); ++j)
+				if (Hasher (i->Key_) == Hasher (j->Key_))
+					throw "duplicate hashes";
+
+		return [=] (std::string_view str)
+		{
+			const auto strHash = Hasher (str);
+			V result {};
+			std::string_view foundName {};
+			(void) ((Hasher (commands.Key_) == strHash && (result = commands.Val_, foundName = commands.Key_, true)) || ...);
+			if (foundName != str)
+				return V {};
+			return result;
+		};
+	}
+}
