d4bf8d639533c014255779f5bc24b5753479e6fb
0xd34df00d <0xd34df00d@gmail.com>
2022-01-15 11:20:06 -0600

    [Poshuku SD] Backwards compatibility with previous LC versions

--- src/plugins/poshuku/plugins/speeddial/speeddial.cpp
+++ src/plugins/poshuku/plugins/speeddial/speeddial.cpp
@@ -20,12 +20,13 @@ namespace LC::Poshuku::SpeedDial
 {
 	void Plugin::Init (ICoreProxy_ptr proxy)
 	{
+		qRegisterMetaType<AddrList_t> ("LC::Poshuku::SpeedDial::AddrList_t");
+		qRegisterMetaType<AddrList_t> ("LeechCraft::Poshuku::SpeedDial::AddrList_t");
+		qRegisterMetaTypeStreamOperators<AddrList_t> ();
+
 		Util::InstallTranslator ("poshuku_speeddial");
 
 		Cache_ = new ImageCache { proxy };
-
-		qRegisterMetaType<AddrList_t> ("LC::Poshuku::SpeedDial::AddrList_t");
-		qRegisterMetaTypeStreamOperators<AddrList_t> ();
 
 		XSD_ = std::make_shared<Util::XmlSettingsDialog> ();
 		XSD_->RegisterObject (&XmlSettingsManager::Instance (), "poshukuspeeddialsettings.xml");
