4c74976b5c994cce5fcc9b709eb909c29ac7cf16
0xd34df00d <0xd34df00d@gmail.com>
2022-01-15 11:16:36 -0600

    [Poshuku SD] Update translations

--- src/plugins/poshuku/plugins/speeddial/leechcraft_poshuku_speeddial_en.ts
+++ src/plugins/poshuku/plugins/speeddial/leechcraft_poshuku_speeddial_en.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
-    <name>LC::Poshuku::SpeedDial</name>
+    <name>LC::Poshuku::SpeedDial::CustomSitesManager</name>
     <message>
-        <location filename="customsitesmanager.cpp" line="45"/>
+        <location filename="customsitesmanager.cpp" line="20"/>
         <source>Site name</source>
         <translation>Site name</translation>
     </message>
@@ -12,7 +12,7 @@
 <context>
     <name>LC::Poshuku::SpeedDial::Plugin</name>
     <message>
-        <location filename="speeddial.cpp" line="79"/>
+        <location filename="speeddial.cpp" line="59"/>
         <source>Adds a special speed dial page.</source>
         <translation>Adds a special speed dial page.</translation>
     </message>
@@ -20,17 +20,17 @@
 <context>
     <name>LC::Poshuku::SpeedDial::ViewHandler</name>
     <message>
-        <location filename="viewhandler.cpp" line="190"/>
+        <location filename="viewhandler.cpp" line="170"/>
         <source>Top pages</source>
         <translation>Top pages</translation>
     </message>
     <message>
-        <location filename="viewhandler.cpp" line="191"/>
+        <location filename="viewhandler.cpp" line="171"/>
         <source>Top sites</source>
         <translation>Top sites</translation>
     </message>
     <message>
-        <location filename="viewhandler.cpp" line="204"/>
+        <location filename="viewhandler.cpp" line="193"/>
         <source>Speed dial</source>
         <translation>Speed dial</translation>
     </message>
@@ -54,6 +54,11 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="5"/>
+        <source>Enable JavaScript during snapshotting</source>
+        <translation>Enable JavaScript during snapshotting</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
         <source>Use static pages list</source>
         <translation>Use static pages list</translation>
     </message>
--- src/plugins/poshuku/plugins/speeddial/leechcraft_poshuku_speeddial_ru_RU.ts
+++ src/plugins/poshuku/plugins/speeddial/leechcraft_poshuku_speeddial_ru_RU.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
-    <name>LC::Poshuku::SpeedDial</name>
+    <name>LC::Poshuku::SpeedDial::CustomSitesManager</name>
     <message>
-        <location filename="customsitesmanager.cpp" line="45"/>
+        <location filename="customsitesmanager.cpp" line="20"/>
         <source>Site name</source>
         <translation>Имя сайта</translation>
     </message>
@@ -12,7 +12,7 @@
 <context>
     <name>LC::Poshuku::SpeedDial::Plugin</name>
     <message>
-        <location filename="speeddial.cpp" line="79"/>
+        <location filename="speeddial.cpp" line="59"/>
         <source>Adds a special speed dial page.</source>
         <translation>Добавляет специальную страницу с быстрым доступом в Poshuku.</translation>
     </message>
@@ -20,17 +20,17 @@
 <context>
     <name>LC::Poshuku::SpeedDial::ViewHandler</name>
     <message>
-        <location filename="viewhandler.cpp" line="190"/>
+        <location filename="viewhandler.cpp" line="170"/>
         <source>Top pages</source>
         <translation>Топ-страницы</translation>
     </message>
     <message>
-        <location filename="viewhandler.cpp" line="191"/>
+        <location filename="viewhandler.cpp" line="171"/>
         <source>Top sites</source>
         <translation>Топ-сайты</translation>
     </message>
     <message>
-        <location filename="viewhandler.cpp" line="204"/>
+        <location filename="viewhandler.cpp" line="193"/>
         <source>Speed dial</source>
         <translation>Быстрый доступ</translation>
     </message>
@@ -54,6 +54,11 @@
     </message>
     <message>
         <location filename="dummy.cpp" line="5"/>
+        <source>Enable JavaScript during snapshotting</source>
+        <translation>Разрешить JavaScript при создании снапшотов</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="6"/>
         <source>Use static pages list</source>
         <translation>Использовать статический список страниц</translation>
     </message>
