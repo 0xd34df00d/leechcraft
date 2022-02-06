543737046d53138f32b1afae45bb475b93efce79
0xd34df00d <0xd34df00d@gmail.com>
2022-02-06 10:42:11 -0600

    [Util] Remove unused include and make sure the header is standalone

--- src/util/tags/tagscompletionmodel.cpp
+++ src/util/tags/tagscompletionmodel.cpp
@@ -6,9 +6,8 @@
  * (See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
  **********************************************************************/
 
-#include <QLineEdit>
-#include <QtDebug>
 #include "tagscompletionmodel.h"
+#include <QLineEdit>
 
 namespace LC::Util
 {
