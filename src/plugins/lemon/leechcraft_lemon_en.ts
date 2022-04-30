d750ed5acb6f54debb02a907c045d5a779caf4aa
0xd34df00d <0xd34df00d@gmail.com>
2022-04-29 22:38:05 -0500

    [Lemon] Update translations

--- src/plugins/lemon/leechcraft_lemon_en.ts
+++ src/plugins/lemon/leechcraft_lemon_en.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LC::Lemon::Plugin</name>
     <message>
-        <location filename="lemon.cpp" line="82"/>
+        <location filename="lemon.cpp" line="64"/>
         <source>Global network status monitor.</source>
         <translation>Global network status monitor.</translation>
     </message>
@@ -12,52 +12,52 @@
 <context>
     <name>LC::Lemon::TrafficDialog</name>
     <message>
-        <location filename="trafficdialog.cpp" line="55"/>
+        <location filename="trafficdialog.cpp" line="30"/>
         <source>RX</source>
         <translation>RX</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="56"/>
+        <location filename="trafficdialog.cpp" line="31"/>
         <source>TX</source>
         <translation>TX</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="57"/>
+        <location filename="trafficdialog.cpp" line="32"/>
         <source>Average RX</source>
         <translation>Average RX</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="58"/>
+        <location filename="trafficdialog.cpp" line="33"/>
         <source>Average TX</source>
         <translation>Average TX</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="61"/>
+        <location filename="trafficdialog.cpp" line="36"/>
         <source>Traffic for %1</source>
         <translation>Traffic for %1</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="73"/>
+        <location filename="trafficdialog.cpp" line="48"/>
         <source>Download, KiB/s</source>
         <translation>Download, KiB/s</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="74"/>
+        <location filename="trafficdialog.cpp" line="49"/>
         <source>Upload, KiB/s</source>
         <translation>Upload, KiB/s</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="80"/>
+        <location filename="trafficdialog.cpp" line="55"/>
         <source>Traffic, KiB/s</source>
         <translation>Traffic, KiB/s</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="210"/>
-        <location filename="trafficdialog.cpp" line="211"/>
-        <location filename="trafficdialog.cpp" line="215"/>
-        <location filename="trafficdialog.cpp" line="216"/>
-        <location filename="trafficdialog.cpp" line="223"/>
-        <location filename="trafficdialog.cpp" line="224"/>
+        <location filename="trafficdialog.cpp" line="191"/>
+        <location filename="trafficdialog.cpp" line="192"/>
+        <location filename="trafficdialog.cpp" line="196"/>
+        <location filename="trafficdialog.cpp" line="197"/>
+        <location filename="trafficdialog.cpp" line="204"/>
+        <location filename="trafficdialog.cpp" line="205"/>
         <source>/s</source>
         <translation>/s</translation>
     </message>
@@ -65,14 +65,80 @@
 <context>
     <name>LC::Lemon::TrafficManager</name>
     <message>
-        <location filename="trafficmanager.cpp" line="251"/>
+        <location filename="trafficmanager.cpp" line="132"/>
+        <source>Ethernet</source>
+        <translation>Ethernet</translation>
+    </message>
+    <message>
+        <location filename="trafficmanager.cpp" line="133"/>
+        <source>Virtual interface</source>
+        <translation>Virtual interface</translation>
+    </message>
+    <message>
+        <location filename="trafficmanager.cpp" line="138"/>
+        <source>Unknown type</source>
+        <translation>Unknown type</translation>
+    </message>
+    <message>
+        <location filename="trafficmanager.cpp" line="212"/>
         <source>Download speed: %1/s</source>
         <translation>Download speed: %1/s</translation>
     </message>
     <message>
-        <location filename="trafficmanager.cpp" line="253"/>
+        <location filename="trafficmanager.cpp" line="214"/>
         <source>Upload speed: %1/s</source>
         <translation>Upload speed: %1/s</translation>
+    </message>
+</context>
+<context>
+    <name>LemonQuark.qml.settings</name>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Lemon settings</source>
+        <translation>Lemon settings</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>Show text labels with current upload/download speeds</source>
+        <translation>Show text labels with current upload/download speeds</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Show color bars matching current upload/download speeds</source>
+        <translation>Show color bars matching current upload/download speeds</translation>
+    </message>
+</context>
+<context>
+    <name>TrafficDialog</name>
+    <message>
+        <location filename="trafficdialog.ui" line="68"/>
+        <source>Average RX speed:</source>
+        <translation>Average RX speed:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="82"/>
+        <source>Average TX speed:</source>
+        <translation>Average TX speed:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="96"/>
+        <source>Max RX speed:</source>
+        <translation>Max RX speed:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="110"/>
+        <source>Max TX speed:</source>
+        <translation>Max TX speed:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="124"/>
+        <source>RX speed:</source>
+        <translation>RX speed:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="138"/>
+        <source>TX speed:</source>
+        <translation>TX speed:</translation>
     </message>
 </context>
 <context>
--- src/plugins/lemon/leechcraft_lemon_ru_RU.ts
+++ src/plugins/lemon/leechcraft_lemon_ru_RU.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LC::Lemon::Plugin</name>
     <message>
-        <location filename="lemon.cpp" line="82"/>
+        <location filename="lemon.cpp" line="64"/>
         <source>Global network status monitor.</source>
         <translation>Системный монитор за состоянием сети.</translation>
     </message>
@@ -12,52 +12,52 @@
 <context>
     <name>LC::Lemon::TrafficDialog</name>
     <message>
-        <location filename="trafficdialog.cpp" line="55"/>
+        <location filename="trafficdialog.cpp" line="30"/>
         <source>RX</source>
-        <translation>Скорость получения</translation>
+        <translation>Скорость скачивания</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="56"/>
+        <location filename="trafficdialog.cpp" line="31"/>
         <source>TX</source>
-        <translation>Скорость отправки</translation>
+        <translation>Скорость отдачи</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="57"/>
+        <location filename="trafficdialog.cpp" line="32"/>
         <source>Average RX</source>
-        <translation>Средняя скорость получения</translation>
+        <translation>Средняя скорость скачивания</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="58"/>
+        <location filename="trafficdialog.cpp" line="33"/>
         <source>Average TX</source>
-        <translation>Средняя скорость отправки</translation>
+        <translation>Средняя скорость отдачи</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="61"/>
+        <location filename="trafficdialog.cpp" line="36"/>
         <source>Traffic for %1</source>
         <translation>Трафик для %1</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="73"/>
+        <location filename="trafficdialog.cpp" line="48"/>
         <source>Download, KiB/s</source>
-        <translation>Скорость получения, КБ/с</translation>
+        <translation>Скорость скачивания, КБ/с</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="74"/>
+        <location filename="trafficdialog.cpp" line="49"/>
         <source>Upload, KiB/s</source>
         <translation>Скорость отдачи, КБ/с</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="80"/>
+        <location filename="trafficdialog.cpp" line="55"/>
         <source>Traffic, KiB/s</source>
         <translation>Трафик, КБ/с</translation>
     </message>
     <message>
-        <location filename="trafficdialog.cpp" line="210"/>
-        <location filename="trafficdialog.cpp" line="211"/>
-        <location filename="trafficdialog.cpp" line="215"/>
-        <location filename="trafficdialog.cpp" line="216"/>
-        <location filename="trafficdialog.cpp" line="223"/>
-        <location filename="trafficdialog.cpp" line="224"/>
+        <location filename="trafficdialog.cpp" line="191"/>
+        <location filename="trafficdialog.cpp" line="192"/>
+        <location filename="trafficdialog.cpp" line="196"/>
+        <location filename="trafficdialog.cpp" line="197"/>
+        <location filename="trafficdialog.cpp" line="204"/>
+        <location filename="trafficdialog.cpp" line="205"/>
         <source>/s</source>
         <translation>/с</translation>
     </message>
@@ -65,14 +65,80 @@
 <context>
     <name>LC::Lemon::TrafficManager</name>
     <message>
-        <location filename="trafficmanager.cpp" line="251"/>
+        <location filename="trafficmanager.cpp" line="132"/>
+        <source>Ethernet</source>
+        <translation>Проводное соединение</translation>
+    </message>
+    <message>
+        <location filename="trafficmanager.cpp" line="133"/>
+        <source>Virtual interface</source>
+        <translation>Виртуальный интерфейс</translation>
+    </message>
+    <message>
+        <location filename="trafficmanager.cpp" line="138"/>
+        <source>Unknown type</source>
+        <translation>Неизвестный тип</translation>
+    </message>
+    <message>
+        <location filename="trafficmanager.cpp" line="212"/>
         <source>Download speed: %1/s</source>
         <translation>Скорость скачивания: %1/s</translation>
     </message>
     <message>
-        <location filename="trafficmanager.cpp" line="253"/>
+        <location filename="trafficmanager.cpp" line="214"/>
         <source>Upload speed: %1/s</source>
-        <translation>Скорость загрузки: %1/s</translation>
+        <translation>Скорость отдачи: %1/s</translation>
+    </message>
+</context>
+<context>
+    <name>LemonQuark.qml.settings</name>
+    <message>
+        <location filename="dummy.cpp" line="8"/>
+        <source>Lemon settings</source>
+        <translation>Настройки Lemon</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="9"/>
+        <source>Show text labels with current upload/download speeds</source>
+        <translation>Отображать текущую скорость загрузки/отдачи</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="10"/>
+        <source>Show color bars matching current upload/download speeds</source>
+        <translation>Отображать столбцы, соответствующие текущей скорости загрузки и отдачи</translation>
+    </message>
+</context>
+<context>
+    <name>TrafficDialog</name>
+    <message>
+        <location filename="trafficdialog.ui" line="68"/>
+        <source>Average RX speed:</source>
+        <translation>Средняя скорость скачивания:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="82"/>
+        <source>Average TX speed:</source>
+        <translation>Средняя скорость отдачи:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="96"/>
+        <source>Max RX speed:</source>
+        <translation>Макс. скорость скачивания:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="110"/>
+        <source>Max TX speed:</source>
+        <translation>Макс. скорость отдачи:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="124"/>
+        <source>RX speed:</source>
+        <translation>Скорость скачивания:</translation>
+    </message>
+    <message>
+        <location filename="trafficdialog.ui" line="138"/>
+        <source>TX speed:</source>
+        <translation>Скорость отдачи:</translation>
     </message>
 </context>
 <context>
