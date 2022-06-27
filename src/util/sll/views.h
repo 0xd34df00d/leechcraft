0b9c3dcd613b77b6649bc55ded7a80d3398cee7e
0xd34df00d <0xd34df00d@gmail.com>
2022-06-27 00:49:16 -0500

    [Util] Simplify the zip view

--- src/util/sll/views.h
+++ src/util/sll/views.h
@@ -8,107 +8,68 @@
 
 #pragma once
 
-#include <iterator>
-#include <boost/iterator/zip_iterator.hpp>
-#include <boost/range.hpp>
-
-namespace LC
-{
-namespace Util
-{
-namespace Views
+namespace LC::Util::Views
 {
 	namespace detail
 	{
-		template<template<typename, typename> class PairType, typename FirstIt, typename SecondIt>
-		using ValueType_t = PairType<typename std::iterator_traits<FirstIt>::value_type, typename std::iterator_traits<SecondIt>::value_type>;
-
-		template<template<typename, typename> class PairType, typename FirstIt, typename SecondIt>
-		class PairIterator : public std::iterator<std::forward_iterator_tag, ValueType_t<PairType, FirstIt, SecondIt>>
+		template<template<typename, typename> class PairType, typename I1, typename I2>
+		struct ZipIterator
 		{
-			bool IsSentinel_;
+			I1 It1_;
+			const I1 It1End_;
+			I2 It2_;
+			const I2 It2End_;
 
-			FirstIt First_;
-			FirstIt FirstEnd_;
-			SecondIt Second_;
-			SecondIt SecondEnd_;
-		public:
-			PairIterator ()
-			: IsSentinel_ { true }
+			bool operator== (const ZipIterator& other) const
 			{
-			}
+				if (IsSentinel () || other.IsSentinel ())
+					return IsSentinel () == other.IsSentinel ();
 
-			PairIterator (const FirstIt& first, const FirstIt& firstEnd,
-					const SecondIt& second, const SecondIt& secondEnd)
-			: IsSentinel_ { false }
-			, First_ { first }
-			, FirstEnd_ { firstEnd }
-			, Second_ { second }
-			, SecondEnd_ { secondEnd }
-			{
-			}
-
-			bool operator== (const PairIterator& other) const
-			{
-				return (IsSentinel () && other.IsSentinel ()) ||
-						(First_ == other.First_ && Second_ == other.Second_);
-			}
-
-			bool operator!= (const PairIterator& other) const
-			{
-				return !(*this == other);
+				return It1_ == other.It1_ && It2_ == other.It2_;
 			}
 
 			bool IsSentinel () const
 			{
-				return IsSentinel_ || First_ == FirstEnd_ || Second_ == SecondEnd_;
+				return It1_ == It1End_ || It2_ == It2End_;
 			}
 
-			PairIterator& operator++ ()
+			auto& operator++ ()
 			{
-				++First_;
-				++Second_;
+				++It1_;
+				++It2_;
 				return *this;
 			}
 
-			PairIterator operator++ (int)
+			auto& operator++ (int)
 			{
 				auto it = *this;
 
-				++First_;
-				++Second_;
+				++It1_;
+				++It2_;
 
 				return it;
 			}
 
-			PairType<typename std::iterator_traits<FirstIt>::value_type, typename std::iterator_traits<SecondIt>::value_type> operator* () const
+			auto operator* () const
 			{
-				return { *First_, *Second_ };
-			}
-		};
-
-		template<typename I1, typename I2, template<typename, typename> class PairType>
-		class ZipRange : public boost::iterator_range<PairIterator<PairType, I1, I2>>
-		{
-			using IteratorType_t = PairIterator<PairType, I1, I2>;
-		public:
-			template<typename C1, typename C2>
-			ZipRange (C1&& c1, C2&& c2)
-			: boost::iterator_range<IteratorType_t>
-			{
-				IteratorType_t { c1.begin (), c1.end (), c2.begin (), c2.end () },
-				IteratorType_t {}
-			}
-			{
+				return PairType { *It1_, *It2_ };
 			}
 		};
 	}
 
 	template<template<typename, typename> class PairType = QPair, typename C1, typename C2>
-	detail::ZipRange<typename C1::const_iterator, typename C2::const_iterator, PairType> Zip (const C1& c1, const C2& c2)
+	auto Zip (C1&& c1, C2&& c2)
 	{
-		return { c1, c2 };
+		using ZIt = detail::ZipIterator<PairType, typename std::decay_t<C1>::const_iterator, typename std::decay_t<C2>::const_iterator>;
+		struct Range
+		{
+			C1 C1_;
+			C2 C2_;
+
+			auto begin () const { return ZIt { C1_.cbegin (), C1_.cend (), C2_.cbegin (), C2_.cend () }; }
+			auto end () const { return ZIt { C1_.cend (), C1_.cend (), C2_.cend (), C2_.cend () }; }
+		};
+
+		return Range { std::forward<C1> (c1), std::forward<C2> (c2) };
 	}
-}
-}
 }
