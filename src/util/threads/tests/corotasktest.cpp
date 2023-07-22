5385308cbaeb339e9951b522fa3d7ebe8c98f767
0xd34df00d <0xd34df00d@gmail.com>
2023-07-22 14:20:08 -0500

    [Util] Yay tdd for coros!

---
+++ src/util/threads/tests/corotasktest.cpp
@@ -0,0 +1,55 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2014  Georg Rudoy
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
+ **********************************************************************/
+
+#include "corotasktest.h"
+#include <QEventLoop>
+#include <QtTest>
+#include <corotask.h>
+
+QTEST_GUILESS_MAIN (LC::Util::CoroTaskTest)
+
+namespace LC::Util
+{
+	namespace
+	{
+		Task<int> CoroInt ()
+		{
+			co_return 42;
+		}
+
+		template<typename T>
+		T GetTaskResult (Task<T> task)
+		{
+			T result;
+
+			QEventLoop loop;
+			bool done = false;
+			[&] () -> Task<void>
+			{
+				result = co_await task;
+				done = true;
+				loop.quit ();
+			} ();
+			if (!done)
+				loop.exec ();
+
+			return result;
+		}
+	}
+
+	void CoroTaskTest::testReturn ()
+	{
+		auto task = CoroInt ();
+		auto result = GetTaskResult (task);
+		QCOMPARE (result, 42);
+	}
+
+	void CoroTaskTest::testFuture ()
+	{
+	}
+}
