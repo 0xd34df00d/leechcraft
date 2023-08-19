3f899fad7f42ce320f595c2763cecac1ef2424d8
0xd34df00d <0xd34df00d@gmail.com>
2023-08-19 14:14:55 -0500

    [Util] Add mock NAM and network reply

--- src/util/threads/tests/corotasktest.cpp
+++ src/util/threads/tests/corotasktest.cpp
@@ -10,6 +10,7 @@
 #include <QEventLoop>
 #include <QtTest>
 #include <coro.h>
+#include <coro/networkresult.h>
 
 QTEST_GUILESS_MAIN (LC::Util::CoroTaskTest)
 
@@ -60,5 +61,72 @@ namespace LC::Util
 		auto result = GetTaskResult (task);
 		QCOMPARE (result, 42);
 		QCOMPARE (timer.elapsed () > 100, true);
+	}
+
+	namespace
+	{
+		// almost the Public Morozov pattern
+		class MockReply : public QNetworkReply
+		{
+			QBuffer Buffer_;
+		public:
+			using QNetworkReply::QNetworkReply;
+
+			using QNetworkReply::setAttribute;
+			using QNetworkReply::setError;
+			using QNetworkReply::setFinished;
+			using QNetworkReply::setHeader;
+			using QNetworkReply::setOperation;
+			using QNetworkReply::setRawHeader;
+			using QNetworkReply::setRequest;
+			using QNetworkReply::setUrl;
+
+			void SetData (const QByteArray& data)
+			{
+				Buffer_.setData (data);
+				Buffer_.open (QIODevice::ReadOnly);
+				open (QIODevice::ReadOnly);
+			}
+		protected:
+			qint64 readData (char *data, qint64 maxSize) override
+			{
+				return Buffer_.read (data, maxSize);
+			}
+
+			void abort () override
+			{
+			}
+		};
+
+		class MockNAM : public QNetworkAccessManager
+		{
+			std::unique_ptr<MockReply> Reply_;
+		public:
+			explicit MockNAM (std::unique_ptr<MockReply> reply)
+			: Reply_ { std::move (reply) }
+			{
+			}
+
+			MockReply& GetReply ()
+			{
+				return *Reply_;
+			}
+		protected:
+			QNetworkReply* createRequest (Operation op, const QNetworkRequest& req, QIODevice*) override
+			{
+				Reply_->setUrl (req.url ());
+				Reply_->setOperation (op);
+				Reply_->setRequest (req);
+				return Reply_.get ();
+			}
+		};
+
+		auto MkSuccessfulReply (const QByteArray& data)
+		{
+			auto reply = std::make_unique<MockReply> ();
+			reply->setAttribute (QNetworkRequest::HttpStatusCodeAttribute, 200);
+			reply->SetData (data);
+			return reply;
+		}
 	}
 }
