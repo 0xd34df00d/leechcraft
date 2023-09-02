114b239647da453ecdb3038f52ede9ced9428447
0xd34df00d <0xd34df00d@gmail.com>
2023-09-02 12:34:36 -0500

    [Util] Some more parallelized abstractions

--- src/util/threads/coro/inparallel.h
+++ src/util/threads/coro/inparallel.h
@@ -22,6 +22,36 @@ namespace LC::Util
 		co_return result;
 	}
 
+	namespace detail
+	{
+		template<typename F, typename Input>
+		struct ParallelTraits
+		{
+			using TaskType_t = std::invoke_result_t<F, Input>;
+
+			using OrigResultType_t = typename TaskType_t::ResultType_t;
+			using ResultType_t = TaskType_t::template ReplaceResult_t<QVector<OrigResultType_t>>;
+		};
+	}
+
+	template<typename Input, typename F>
+	auto InParallel (QVector<Input>&& inputs, F&& mkTask) -> detail::ParallelTraits<F, Input>::ResultType_t
+	{
+		QVector<typename detail::ParallelTraits<F, Input>::OrigResultType_t> result;
+		for (auto&& input : inputs)
+			result << co_await mkTask (std::move (input));
+		co_return result;
+	}
+
+	template<typename Input, typename F>
+	auto InParallelSemigroup (QVector<Input>&& inputs, F&& mkTask) -> detail::ParallelTraits<F, Input>::TaskType_t
+	{
+		typename detail::ParallelTraits<F, Input>::OrigResultType_t result;
+		for (auto&& input : inputs)
+			result += co_await mkTask (std::move (input));
+		co_return result;
+	}
+
 	template<typename... Ts, template<typename> typename... Exts>
 	Task<std::tuple<Ts...>, Exts...> InParallel (Task<Ts, Exts...>... tasks)
 	{
