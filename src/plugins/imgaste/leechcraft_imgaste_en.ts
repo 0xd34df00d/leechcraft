93ede61264d33898a04afc2af308c499e08811b3
0xd34df00d <0xd34df00d@gmail.com>
2023-12-09 10:40:36 -0600

    [Imgaste] Update translations

--- src/plugins/imgaste/leechcraft_imgaste_en.ts
+++ src/plugins/imgaste/leechcraft_imgaste_en.ts
@@ -4,63 +4,70 @@
 <context>
     <name>LC::Imgaste::Plugin</name>
     <message>
-        <location filename="imgaste.cpp" line="53"/>
+        <location filename="imgaste.cpp" line="55"/>
         <source>Simple image uploader to imagebin services like pomf.cat or imagebin.ca.</source>
         <translation>Simple image uploader to imagebin services like pomf.cat or imagebin.ca.</translation>
     </message>
     <message>
-        <location filename="imgaste.cpp" line="96"/>
+        <location filename="imgaste.cpp" line="98"/>
         <source>Upload image</source>
         <translation>Upload image</translation>
     </message>
+    <message>
+        <location filename="imgaste.cpp" line="173"/>
+        <source>Unable to open file: %1.</source>
+        <translation>Unable to open file: %1.</translation>
+    </message>
+    <message>
+        <location filename="imgaste.cpp" line="198"/>
+        <source>Unable to serialize image.</source>
+        <translation>Unable to serialize image.</translation>
+    </message>
 </context>
 <context>
-    <name>LC::Imgaste::SingleServiceUploader</name>
+    <name>QObject</name>
     <message>
-        <location filename="singleserviceuploader.cpp" line="35"/>
+        <location filename="uploader.cpp" line="56"/>
         <source>Image upload</source>
         <translation>Image upload</translation>
     </message>
     <message>
-        <location filename="singleserviceuploader.cpp" line="36"/>
+        <location filename="uploader.cpp" line="57"/>
         <source>Uploading...</source>
         <translation>Uploading...</translation>
     </message>
     <message>
-        <location filename="singleserviceuploader.cpp" line="50"/>
+        <location filename="uploader.cpp" line="83"/>
         <source>%1 of %2</source>
         <translation>%1 of %2</translation>
     </message>
-</context>
-<context>
-    <name>LC::Imgaste::Uploader</name>
     <message>
-        <location filename="uploader.cpp" line="57"/>
-        <source>Unknown upload service: %1.</source>
-        <translation>Unknown upload service: %1.</translation>
-    </message>
-    <message>
-        <location filename="uploader.cpp" line="72"/>
+        <location filename="uploader.cpp" line="94"/>
         <source>Try another service...</source>
         <translation>Try another service...</translation>
     </message>
     <message>
-        <location filename="uploader.cpp" line="94"/>
+        <location filename="uploader.cpp" line="107"/>
         <source>Image pasted: %1, the URL was copied to the clipboard</source>
         <translation>Image pasted: %1, the URL was copied to the clipboard</translation>
     </message>
     <message>
-        <location filename="uploader.cpp" line="114"/>
+        <location filename="uploader.cpp" line="121"/>
+        <source>Unknown upload service: %1.</source>
+        <translation>Unknown upload service: %1.</translation>
+    </message>
+    <message>
+        <location filename="uploader.cpp" line="141"/>
         <source>Image upload failed: %1</source>
         <translation>Image upload failed: %1</translation>
     </message>
     <message>
-        <location filename="uploader.cpp" line="123"/>
+        <location filename="uploader.cpp" line="151"/>
         <source>Image upload to %1 failed: service error.</source>
         <translation>Image upload to %1 failed: service error.</translation>
     </message>
     <message>
-        <location filename="uploader.cpp" line="149"/>
+        <location filename="uploader.cpp" line="166"/>
         <source>Please select another service to try:</source>
         <translation>Please select another service to try:</translation>
     </message>
--- src/plugins/imgaste/leechcraft_imgaste_ru_RU.ts
+++ src/plugins/imgaste/leechcraft_imgaste_ru_RU.ts
@@ -4,63 +4,70 @@
 <context>
     <name>LC::Imgaste::Plugin</name>
     <message>
-        <location filename="imgaste.cpp" line="53"/>
+        <location filename="imgaste.cpp" line="55"/>
         <source>Simple image uploader to imagebin services like pomf.cat or imagebin.ca.</source>
         <translation>Простой загрузчик изображений на сервисы вроде pomf.cat или imagebin.ca.</translation>
     </message>
     <message>
-        <location filename="imgaste.cpp" line="96"/>
+        <location filename="imgaste.cpp" line="98"/>
         <source>Upload image</source>
         <translation>Загрузить изображение</translation>
     </message>
+    <message>
+        <location filename="imgaste.cpp" line="173"/>
+        <source>Unable to open file: %1.</source>
+        <translation>Невозможно открыть файл: %1.</translation>
+    </message>
+    <message>
+        <location filename="imgaste.cpp" line="198"/>
+        <source>Unable to serialize image.</source>
+        <translation>Невозможно сохранить картинку.</translation>
+    </message>
 </context>
 <context>
-    <name>LC::Imgaste::SingleServiceUploader</name>
+    <name>QObject</name>
     <message>
-        <location filename="singleserviceuploader.cpp" line="35"/>
+        <location filename="uploader.cpp" line="56"/>
         <source>Image upload</source>
         <translation>Загрузка изображения</translation>
     </message>
     <message>
-        <location filename="singleserviceuploader.cpp" line="36"/>
+        <location filename="uploader.cpp" line="57"/>
         <source>Uploading...</source>
         <translation>Загрузка…</translation>
     </message>
     <message>
-        <location filename="singleserviceuploader.cpp" line="50"/>
+        <location filename="uploader.cpp" line="83"/>
         <source>%1 of %2</source>
         <translation>%1 из %2</translation>
     </message>
-</context>
-<context>
-    <name>LC::Imgaste::Uploader</name>
     <message>
-        <location filename="uploader.cpp" line="57"/>
-        <source>Unknown upload service: %1.</source>
-        <translation>Неизвестный сервис загрузки изображений: %1.</translation>
-    </message>
-    <message>
-        <location filename="uploader.cpp" line="72"/>
+        <location filename="uploader.cpp" line="94"/>
         <source>Try another service...</source>
         <translation>Попробовать другой сервис…</translation>
     </message>
     <message>
-        <location filename="uploader.cpp" line="94"/>
+        <location filename="uploader.cpp" line="107"/>
         <source>Image pasted: %1, the URL was copied to the clipboard</source>
         <translation>Изображение загружено: %1, URL был скопирован в буфер обмена</translation>
     </message>
     <message>
-        <location filename="uploader.cpp" line="114"/>
+        <location filename="uploader.cpp" line="121"/>
+        <source>Unknown upload service: %1.</source>
+        <translation>Неизвестный сервис загрузки изображений: %1.</translation>
+    </message>
+    <message>
+        <location filename="uploader.cpp" line="141"/>
         <source>Image upload failed: %1</source>
         <translation>Ошибка загрузки изображения: %1</translation>
     </message>
     <message>
-        <location filename="uploader.cpp" line="123"/>
+        <location filename="uploader.cpp" line="151"/>
         <source>Image upload to %1 failed: service error.</source>
         <translation>Ошибка сервиса %1 при загрузке изображения.</translation>
     </message>
     <message>
-        <location filename="uploader.cpp" line="149"/>
+        <location filename="uploader.cpp" line="166"/>
         <source>Please select another service to try:</source>
         <translation>Пожалуйста, выберите другой сервис:</translation>
     </message>
