0a443600525ae3bb4e995cf13dde01cae22da50c
0xd34df00d <0xd34df00d@gmail.com>
2023-12-28 15:41:21 -0600

    [Monocle Boop] Welp, apparently parallelization does more harm than good

--- src/plugins/monocle/plugins/boop/epubloader.cpp
+++ src/plugins/monocle/plugins/boop/epubloader.cpp
@@ -9,12 +9,10 @@
 #include "epubloader.h"
 #include <QDomDocument>
 #include <QTextDocument>
-#include <QtConcurrentMap>
 #include <quazip/quazip.h>
 #include <quazip/quazipfile.h>
 #include <util/sll/domchildrenrange.h>
 #include <util/sll/timer.h>
-#include <util/sll/prelude.h>
 #include <util/sll/qtutil.h>
 #include <util/monocle/textdocumentformatconfig.h>
 #include "document.h"
@@ -77,9 +75,15 @@ namespace LC::Monocle::Boop
 			return GetAttr (rootfile, "full-path"_qs);
 		}
 
+		struct PathItem
+		{
+			QString Path_;
+			QString Mime_;
+		};
+
 		struct Manifest
 		{
-			QHash<QString, QString> Id2Path_;
+			QHash<QString, PathItem> Id2Item_;
 			QVector<QString> Spine_;
 		};
 
@@ -92,7 +96,9 @@ namespace LC::Monocle::Boop
 			{
 				const auto& id = GetAttr (item, "id"_qs);
 				const auto& href = GetAttr (item, "href"_qs);
-				manifest.Id2Path_ [id] = QUrl { opfFile }.resolved (QUrl { href }).toString ();
+				const auto& resolvedPath = QUrl { opfFile }.resolved (QUrl { href }).toString ();
+				const auto& mime = GetAttr (item, "media-type"_qs);
+				manifest.Id2Item_ [id] = PathItem { resolvedPath, mime };
 			}
 
 			const auto& spine = GetElem (doc.documentElement(), "spine"_qs);
@@ -127,23 +133,13 @@ namespace LC::Monocle::Boop
 			QVector<QString> paths;
 			for (const auto& partId : manifest.Spine_)
 			{
-				const auto& path = manifest.Id2Path_.value (partId);
-				if (path.isEmpty ())
-					throw InvalidEpub { "unknown ref " + path };
-				paths << path;
+				const auto& item = manifest.Id2Item_.value (partId);
+				if (item.Path_.isEmpty ())
+					throw InvalidEpub { "unknown contents " + partId };
+				paths << item.Path_;
 			}
 
-			using Elems_t = QVector<QDomElement>;
-
-			constexpr auto parallelThreshold = 50;
-			if (paths.size () > parallelThreshold)
-			{
-				auto subresults = QtConcurrent::blockingMapped<QVector<Elems_t>> (paths,
-						std::function { [&] (const QString& path) { return ExtractBodyChildren (epubFile, path); } });
-				return Util::Concat (std::move (subresults));
-			}
-
-			Elems_t bodiesChildren;
+			QVector<QDomElement> bodiesChildren;
 			for (const auto& path : paths)
 				bodiesChildren += ExtractBodyChildren (epubFile, path);
 			return bodiesChildren;
