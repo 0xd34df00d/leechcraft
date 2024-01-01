3f7c5fddb68e7fc28296db73ed814738e6bc2625
0xd34df00d <0xd34df00d@gmail.com>
2024-01-01 14:47:18 -0600

    [Monocle] No need to (re)document the implemented interfaces

--- src/plugins/monocle/util/monocle/textdocumentadapter.h
+++ src/plugins/monocle/util/monocle/textdocumentadapter.h
@@ -53,82 +53,14 @@ namespace LC::Monocle
 
 		~TextDocumentAdapter () override;
 
-		/** @brief Checks if a document is set.
-		 */
 		bool IsValid () const override;
-
-		/** @brief Returns the pages count in the underlying document.
-		 *
-		 * @note If IsValid() returns false, the behavior is undefined.
-		 *
-		 * @return Pages count.
-		 */
 		int GetNumPages () const override;
-
-		/** @brief Returns the size of the \em page.
-		 *
-		 * @note If IsValid() returns false, the behavior is undefined.
-		 *
-		 * @param[in] page The index of the page to query.
-		 * @return The size of the given \em page in pixels.
-		 */
 		QSize GetPageSize (int page) const override;
-
-		/** @brief Renders the given \em page.
-		 *
-		 * The hints set via SetRenderHint() are used during rendering.
-		 *
-		 * @note If IsValid() returns false, the behavior is undefined.
-		 *
-		 * @param[in] page The index of the page to render.
-		 * @param[in] xScale The scale in the X dimension.
-		 * @param[in] yScale The scale in the Y dimension.
-		 *
-		 * @return The rendered image of the given page.
-		 *
-		 * @sa SetRenderHint()
-		 */
 		QFuture<QImage> RenderPage (int page, double xScale, double yScale) override;
-
-		/** @brief Returns the links found on the given \em page.
-		 *
-		 * The implementation currently always returns an empty list.
-		 *
-		 * @note If IsValid() returns false, the behavior is undefined.
-		 *
-		 * @param[in] page The index of the page to query.
-		 *
-		 * @return The list of links found on the \em page.
-		 */
 		QList<ILink_ptr> GetPageLinks (int page) override;
 
-		/** @brief Paints the given \em page using the \em painter.
-		 *
-		 * The hints set via SetRenderHint() are used during rendering.
-		 *
-		 * @note If IsValid() returns false, the behavior is undefined.
-		 *
-		 * @param[in] painter The painter to use.
-		 * @param[in] page The index of the page to paint.
-		 * @param[in] xScale The X-axis scale which should be used during
-		 * painting.
-		 * @param[in] yScale The Y-axis scale which should be used during
-		 * painting.
-		 *
-		 * @sa SetRenderHint()
-		 */
 		void PaintPage (QPainter *painter, int page, double xScale, double yScale) override;
 
-		/** @brief Searches for the given \em text and returns its positions.
-		 *
-		 * @note If IsValid() returns false, the behavior is undefined.
-		 *
-		 * @param[in] text The text to search for.
-		 * @param[in] cs The case sensitivity of the search.
-		 *
-		 * @return The list of positions of the given \em text string for
-		 * each page.
-		 */
 		QMap<int, QList<QRectF>> GetTextPositions (const QString& text, Qt::CaseSensitivity cs) override;
 
 		/** @brief Sets the underlying document and any additional information.
