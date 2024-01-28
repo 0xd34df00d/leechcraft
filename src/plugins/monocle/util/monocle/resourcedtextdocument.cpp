efc0d43b850d7835a94ae3c6d5327f7b8b017b38
0xd34df00d <0xd34df00d@gmail.com>
2024-01-28 12:39:25 -0600

    [Monocle] Use imagemagick for sharpening images

--- src/plugins/monocle/util/monocle/resourcedtextdocument.cpp
+++ src/plugins/monocle/util/monocle/resourcedtextdocument.cpp
@@ -8,6 +8,7 @@
 
 #include "resourcedtextdocument.h"
 #include <QtDebug>
+#include <Magick++.h>
 #include <util/sll/udls.h>
 
 namespace LC::Monocle
@@ -24,6 +25,54 @@ namespace LC::Monocle
 		ImagesCache_.clear ();
 	}
 
+	namespace
+	{
+		Magick::Geometry ToMagick (QSize size)
+		{
+			return { static_cast<size_t> (size.width ()), static_cast<size_t> (size.height ()) };
+		}
+
+		std::string ToMagick (QImage::Format fmt)
+		{
+			switch (fmt)
+			{
+			case QImage::Format_RGB32:
+				return "RGBA";
+			case QImage::Format_ARGB32:
+				return "RGBA";
+			default:
+				qWarning () << "unsupported fmt" << fmt;
+				throw std::runtime_error { "unsupported ToMagick fmt" };
+			}
+		}
+
+		QImage Downscale (QImage&& image, QSize size)
+		{
+			if (size == image.size ())
+				return image;
+
+			const auto noSharpenFactor = 2;
+			if (size.width () * noSharpenFactor > image.width () || size.height () * noSharpenFactor > image.height ())
+				return image.scaled (size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+			Magick::Image magick
+			{
+				static_cast<size_t> (image.width ()),
+				static_cast<size_t> (image.height ()),
+				ToMagick (image.format ()),
+				Magick::CharPixel,
+				image.constBits ()
+			};
+			magick.filterType (MagickCore::Lanczos2SharpFilter);
+			magick.resize (ToMagick (size));
+			magick.sharpen (2, 2);
+
+			QImage result { size, QImage::Format_ARGB32 };
+			magick.write (0, 0, size.width (), size.height (), ToMagick (result.format ()), Magick::CharPixel, result.bits ());
+			return result;
+		}
+	}
+
 	QVariant ResourcedTextDocument::loadResource (int type, const QUrl& name)
 	{
 		if (type != QTextDocument::ImageResource)
@@ -37,7 +86,7 @@ namespace LC::Monocle
 			return QTextDocument::loadResource (type, name);
 
 		const auto& maxSize = MaxImageSizes_.value (name);
-		const auto& loaded = image.Load_ (maxSize.isValid () ? maxSize : image.NativeSize_);
+		const auto& loaded = Downscale (image.Load_ (image.NativeSize_), maxSize.isValid () ? maxSize : image.NativeSize_);
 		ImagesCache_.insert (name, new QImage { loaded }, loaded.sizeInBytes ());
 		return loaded;
 	}
