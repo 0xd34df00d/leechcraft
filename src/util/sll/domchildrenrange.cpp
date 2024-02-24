9ba2bd9e4803a7ecef31ea440843be24bd1226e3
0xd34df00d <0xd34df00d@gmail.com>
2024-02-24 13:08:17 -0600

    [Util] Improve DomDescendants perf for bigger lists
    Apparently QDomNodeList performance gets slower even for read-only
    operations on bigger lists (O(n²) somewhere?), so for lists of 3000-4000
    elements `at()` takes more that 1 ms. Explicit recursion over the DOM
    tree turns out to be much more efficient in cases like this.

--- src/util/sll/domchildrenrange.cpp
+++ src/util/sll/domchildrenrange.cpp
@@ -11,14 +11,38 @@
 
 namespace LC::Util
 {
+	namespace
+	{
+		void DomDescendants (const QDomElement& parent, const QString& tag, QVector<QDomElement>& result)
+		{
+			for (const auto& elem : DomChildren (parent, {}))
+			{
+				if (elem.tagName () == tag)
+					result << elem;
+
+				DomDescendants (elem, tag, result);
+			}
+		}
+	}
+
 	QVector<QDomElement> DomDescendants (const QDomElement& parent, const QString& tag)
 	{
 		const auto& allElems = parent.elementsByTagName (tag);
+		const auto elemsCount = allElems.size ();
 
 		QVector<QDomElement> result;
-		result.reserve (allElems.size ());
-		for (int i = 0; i < allElems.size (); ++i)
-			result << allElems.at (i).toElement ();
+		result.reserve (elemsCount);
+
+		// QDomNodeList operations are slower than explicit recursion for bigger lists,
+		// and this is a somewhat empirical threshold.
+		constexpr auto countThreshold = 200;
+
+		if (elemsCount < countThreshold)
+			for (int i = 0; i < allElems.size (); ++i)
+				result << allElems.at (i).toElement ();
+		else
+			DomDescendants (parent, tag, result);
+
 		return result;
 	}
 }
