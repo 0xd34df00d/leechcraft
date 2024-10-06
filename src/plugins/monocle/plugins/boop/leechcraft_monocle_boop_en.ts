15c447e3c9df785be9b5babd60717e059a128dcc
0xd34df00d <0xd34df00d@gmail.com>
2024-10-06 10:38:54 -0500

    [Monocle Boop] Add ru and en translations

---
+++ src/plugins/monocle/plugins/boop/leechcraft_monocle_boop_en.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>LC::Monocle::Boop::Plugin</name>
+    <message>
+        <location filename="boop.cpp" line="41"/>
+        <source>ePub backend for Monocle.</source>
+        <translation>ePub backend for Monocle.</translation>
+    </message>
+    <message>
+        <location filename="boop.cpp" line="74"/>
+        <source>ePub files</source>
+        <translation>ePub files</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/monocle/plugins/boop/leechcraft_monocle_boop_ru_RU.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>LC::Monocle::Boop::Plugin</name>
+    <message>
+        <location filename="boop.cpp" line="41"/>
+        <source>ePub backend for Monocle.</source>
+        <translation>Модуль поддержки ePub для Monocle.</translation>
+    </message>
+    <message>
+        <location filename="boop.cpp" line="74"/>
+        <source>ePub files</source>
+        <translation>Файлы ePub</translation>
+    </message>
+</context>
+</TS>
