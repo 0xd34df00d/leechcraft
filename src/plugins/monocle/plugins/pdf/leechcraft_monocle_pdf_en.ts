054d76c1193639d41705ea3b383661a387773733
0xd34df00d <0xd34df00d@gmail.com>
2024-11-17 10:02:35 -0600

    [Monocle PDF] Update translations

--- src/plugins/monocle/plugins/pdf/leechcraft_monocle_pdf_en.ts
+++ src/plugins/monocle/plugins/pdf/leechcraft_monocle_pdf_en.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LC::Monocle::PDF::Document</name>
     <message>
-        <location filename="document.cpp" line="282"/>
+        <location filename="document.cpp" line="343"/>
         <source>saving encrypted documents is not supported</source>
         <translation>saving encrypted documents is not supported</translation>
     </message>
@@ -12,12 +12,12 @@
 <context>
     <name>LC::Monocle::PDF::Plugin</name>
     <message>
-        <location filename="pdf.cpp" line="72"/>
+        <location filename="pdf.cpp" line="50"/>
         <source>PDF backend for Monocle.</source>
         <translation>PDF backend for Monocle.</translation>
     </message>
     <message>
-        <location filename="pdf.cpp" line="124"/>
+        <location filename="pdf.cpp" line="93"/>
         <source>PDF files</source>
         <translation>PDF files</translation>
     </message>
@@ -26,53 +26,63 @@
     <name>monoclepdfsettings</name>
     <message>
         <location filename="dummy.cpp" line="2"/>
-        <source>Monocle PDF</source>
-        <translation>Monocle PDF</translation>
-    </message>
-    <message>
-        <location filename="dummy.cpp" line="3"/>
         <source>Rendering</source>
         <translation>Rendering</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="4"/>
+        <location filename="dummy.cpp" line="3"/>
         <source>Antialiasing</source>
         <translation>Antialiasing</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="5"/>
+        <location filename="dummy.cpp" line="4"/>
         <source>Text antialiasing</source>
         <translation>Text antialiasing</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="6"/>
+        <location filename="dummy.cpp" line="5"/>
         <source>Text hinting</source>
         <translation>Text hinting</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="7"/>
+        <location filename="dummy.cpp" line="6"/>
         <source>Slight text hinting</source>
         <translation>Slight text hinting</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="8"/>
+        <location filename="dummy.cpp" line="7"/>
         <source>Thin line enhancement:</source>
         <translation>Thin line enhancement:</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="9"/>
+        <location filename="dummy.cpp" line="8"/>
         <source>none</source>
         <translation>none</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="10"/>
+        <location filename="dummy.cpp" line="9"/>
         <source>solid</source>
         <translation>solid</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="11"/>
+        <location filename="dummy.cpp" line="10"/>
         <source>shape</source>
         <translation>shape</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Performance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="12"/>
+        <source>Character-level text selection precision</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <source>When enabled, the text selection tool allows for more precise, character-level text selection at the cost of slightly higher CPU and noticeably higher memory use during selection. When disabled, only whole words can be selected.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- src/plugins/monocle/plugins/pdf/leechcraft_monocle_pdf_ru_RU.ts
+++ src/plugins/monocle/plugins/pdf/leechcraft_monocle_pdf_ru_RU.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LC::Monocle::PDF::Document</name>
     <message>
-        <location filename="document.cpp" line="282"/>
+        <location filename="document.cpp" line="343"/>
         <source>saving encrypted documents is not supported</source>
         <translation>сохранение зашифрованных документов не поддерживается</translation>
     </message>
@@ -12,12 +12,12 @@
 <context>
     <name>LC::Monocle::PDF::Plugin</name>
     <message>
-        <location filename="pdf.cpp" line="72"/>
+        <location filename="pdf.cpp" line="50"/>
         <source>PDF backend for Monocle.</source>
         <translation>Модуль поддержки PDF для Monocle.</translation>
     </message>
     <message>
-        <location filename="pdf.cpp" line="124"/>
+        <location filename="pdf.cpp" line="93"/>
         <source>PDF files</source>
         <translation>Файлы PDF</translation>
     </message>
@@ -26,53 +26,64 @@
     <name>monoclepdfsettings</name>
     <message>
         <location filename="dummy.cpp" line="2"/>
-        <source>Monocle PDF</source>
-        <translation>Monocle PDF</translation>
-    </message>
-    <message>
-        <location filename="dummy.cpp" line="3"/>
         <source>Rendering</source>
         <translation>Отображение</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="4"/>
+        <location filename="dummy.cpp" line="3"/>
         <source>Antialiasing</source>
         <translation>Сглаживание</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="5"/>
+        <location filename="dummy.cpp" line="4"/>
         <source>Text antialiasing</source>
         <translation>Сглаживание текста</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="6"/>
+        <location filename="dummy.cpp" line="5"/>
         <source>Text hinting</source>
         <translation>Хинтинг текста</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="7"/>
+        <location filename="dummy.cpp" line="6"/>
         <source>Slight text hinting</source>
         <translation>Лёгкий хинтинг текста</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="8"/>
+        <location filename="dummy.cpp" line="7"/>
         <source>Thin line enhancement:</source>
         <translation>Улучшение тонких линий:</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="9"/>
+        <location filename="dummy.cpp" line="8"/>
         <source>none</source>
         <translation>нет</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="10"/>
+        <location filename="dummy.cpp" line="9"/>
         <source>solid</source>
         <translation>сплошное</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="11"/>
+        <location filename="dummy.cpp" line="10"/>
         <source>shape</source>
         <translation>по форме</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="11"/>
+        <source>Performance</source>
+        <translation>Производительность</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="12"/>
+        <source>Character-level text selection precision</source>
+        <translation>Выделение текста с точностью до символов</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="13"/>
+        <source>When enabled, the text selection tool allows for more precise, character-level text selection at the cost of slightly higher CPU and noticeably higher memory use during selection. When disabled, only whole words can be selected.</source>
+        <translation>Если включено, то инструмент выделения текста позволяет выделять отдельные символы ценой чуть большего потребления CPU и заметно большего потребления памяти во время выделения.
+Если выключено, то можно выделять только целые слова.</translation>
     </message>
 </context>
 </TS>
