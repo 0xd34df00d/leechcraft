3099383c5633c10a7ea357a7d37dea49144f6138
0xd34df00d <0xd34df00d@gmail.com>
2024-12-21 09:41:08 -0600

    [XSD] Port to `add_util_library`

--- src/xmlsettingsdialog/xsdconfig.h
+++ src/xmlsettingsdialog/xsdconfig.h
@@ -10,7 +10,7 @@
 
 #include <QtGlobal>
 
-#if defined(leechcraft_xsd_EXPORTS) || defined(leechcraft_xsd_qt5_EXPORTS)
+#if defined(leechcraft_xsd_EXPORTS)
 #  define XMLSETTINGSMANAGER_API Q_DECL_EXPORT
 # else
 #  define XMLSETTINGSMANAGER_API Q_DECL_IMPORT
