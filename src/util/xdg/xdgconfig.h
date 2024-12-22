5629bb087ebca95c06b8b8cd5904d005b6719327
0xd34df00d <0xd34df00d@gmail.com>
2024-12-22 09:41:33 -0600

    [Util] Port xdg to add_util_library

--- src/util/xdg/xdgconfig.h
+++ src/util/xdg/xdgconfig.h
@@ -10,7 +10,7 @@
 
 #include <QtGlobal>
 
-#if defined(leechcraft_util_xdg_EXPORTS) || defined(leechcraft_util_xdg_qt5_EXPORTS)
+#if defined(leechcraft_util_xdg_EXPORTS)
 #  define UTIL_XDG_API Q_DECL_EXPORT
 #else
 #  define UTIL_XDG_API Q_DECL_IMPORT
