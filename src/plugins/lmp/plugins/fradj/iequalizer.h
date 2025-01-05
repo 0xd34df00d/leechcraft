8ee53734b5e1f54eddd0bea28754e2cfc86ad3eb
0xd34df00d <0xd34df00d@gmail.com>
2025-01-05 09:58:12 -0600

    [LMP FrAdj] Remove QStringList fwd decl

--- src/plugins/lmp/plugins/fradj/iequalizer.h
+++ src/plugins/lmp/plugins/fradj/iequalizer.h
@@ -7,11 +7,11 @@
  **********************************************************************/
 
 #pragma once
+
+#include <QStringList>
 #include "bandinfo.h"
 
 class QByteArray;
-
-class QStringList;
 
 namespace LC
 {
