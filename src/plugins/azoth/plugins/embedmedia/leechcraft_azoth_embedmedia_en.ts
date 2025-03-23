a5c389b43bc2492e2c89b4ab09535c7199671a57
0xd34df00d <0xd34df00d@gmail.com>
2025-03-23 10:30:57 -0500

    [Azoth EM] Add translations

---
+++ src/plugins/azoth/plugins/embedmedia/leechcraft_azoth_embedmedia_en.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>LC::Azoth::EmbedMedia::Plugin</name>
+    <message>
+        <location filename="embedmedia.cpp" line="52"/>
+        <source>Displays media objects right in chat windows.</source>
+        <oldsource>Enables displaying media objects right in chat windows.</oldsource>
+        <translation>Displays media objects right in chat windows.</translation>
+    </message>
+    <message>
+        <location filename="embedmedia.cpp" line="119"/>
+        <source>Downloading image...</source>
+        <translation>Downloading image...</translation>
+    </message>
+    <message>
+        <location filename="embedmedia.cpp" line="131"/>
+        <source>Downloading image of %1...</source>
+        <translation>Downloading image of %1...</translation>
+    </message>
+    <message>
+        <location filename="embedmedia.cpp" line="153"/>
+        <source>Unable to fetch the image: %1</source>
+        <translation>Unable to fetch the image: %1</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/azoth/plugins/embedmedia/leechcraft_azoth_embedmedia_ru_RU.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>LC::Azoth::EmbedMedia::Plugin</name>
+    <message>
+        <location filename="embedmedia.cpp" line="52"/>
+        <source>Displays media objects right in chat windows.</source>
+        <oldsource>Enables displaying media objects right in chat windows.</oldsource>
+        <translation>Отображает медиа в окнах разговоров.</translation>
+    </message>
+    <message>
+        <location filename="embedmedia.cpp" line="119"/>
+        <source>Downloading image...</source>
+        <translation>Скачивание изображения...</translation>
+    </message>
+    <message>
+        <location filename="embedmedia.cpp" line="131"/>
+        <source>Downloading image of %1...</source>
+        <translation>Скачивание изображения размером %1...</translation>
+    </message>
+    <message>
+        <location filename="embedmedia.cpp" line="153"/>
+        <source>Unable to fetch the image: %1</source>
+        <translation>Невозможно скачать изображение: %1</translation>
+    </message>
+</context>
+</TS>
