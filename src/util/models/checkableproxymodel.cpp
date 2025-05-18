565dbd4517ab21eef5e806a54bd2ffacbbad27c6
0xd34df00d <0xd34df00d@gmail.com>
2025-05-18 10:41:58 -0500

    [Aggregator] Move more of CheckableProxyModel to the Base non-template class

--- src/util/models/checkableproxymodel.cpp
+++ src/util/models/checkableproxymodel.cpp
@@ -10,4 +10,74 @@
 
 namespace LC::Util
 {
+	CheckableProxyModelBase::CheckableProxyModelBase (int idRole, QObject *parent)
+	: QIdentityProxyModel { parent }
+	, IdRole_ { idRole }
+	{
+	}
+
+	Qt::ItemFlags CheckableProxyModelBase::flags (const QModelIndex& index) const
+	{
+		auto flags = QIdentityProxyModel::flags (index);
+		if (index.column () == 0)
+			flags |= Qt::ItemIsUserCheckable;
+		return flags;
+	}
+
+	QVariant CheckableProxyModelBase::data (const QModelIndex& index, int role) const
+	{
+		if (role == Qt::CheckStateRole && index.column () == 0)
+			return IsChecked (index.data (IdRole_)) ? Qt::Checked : Qt::Unchecked;
+
+		return QIdentityProxyModel::data (index, role);
+	}
+
+	bool CheckableProxyModelBase::setData (const QModelIndex& index, const QVariant& value, int role)
+	{
+		if (role == Qt::CheckStateRole && index.column () == 0)
+		{
+			SetChecked (index.data (IdRole_), value.value<Qt::CheckState> () == Qt::Checked);
+			emit selectionChanged ();
+			emit dataChanged (index, index, { Qt::CheckStateRole });
+			return true;
+		}
+
+		return QIdentityProxyModel::setData (index, value, role);
+	}
+
+	void CheckableProxyModelBase::CheckAll ()
+	{
+		const auto rc = sourceModel ()->rowCount ();
+		for (int i = 0; i < rc; ++i)
+		{
+			const auto srcIndex = sourceModel ()->index (i, 0);
+			const auto& idVar = srcIndex.data (IdRole_);
+			if (!IsChecked (idVar))
+			{
+				SetChecked (idVar, true);
+				const auto& index = mapFromSource (srcIndex);
+				emit dataChanged (index, index, { Qt::CheckStateRole });
+			}
+		}
+
+		emit selectionChanged ();
+	}
+
+	void CheckableProxyModelBase::CheckNone ()
+	{
+		const auto rc = sourceModel ()->rowCount ();
+		for (int i = 0; i < rc; ++i)
+		{
+			const auto srcIndex = sourceModel ()->index (i, 0);
+			const auto& idVar = srcIndex.data (IdRole_);
+			if (IsChecked (idVar))
+			{
+				SetChecked (idVar, false);
+				const auto& index = mapFromSource (srcIndex);
+				emit dataChanged (index, index, { Qt::CheckStateRole });
+			}
+		}
+
+		emit selectionChanged ();
+	}
 }
--- src/util/models/checkableproxymodel.h
+++ src/util/models/checkableproxymodel.h
@@ -17,8 +17,20 @@ namespace LC::Util
 	class UTIL_MODELS_API CheckableProxyModelBase : public QIdentityProxyModel
 	{
 		Q_OBJECT
+	protected:
+		const int IdRole_;
 	public:
-		using QIdentityProxyModel::QIdentityProxyModel;
+		explicit CheckableProxyModelBase (int idRole, QObject *parent = nullptr);
+
+		Qt::ItemFlags flags (const QModelIndex& index) const override;
+		QVariant data (const QModelIndex& index, int role) const override;
+		bool setData (const QModelIndex& index, const QVariant& value, int role) override;
+
+		void CheckAll ();
+		void CheckNone ();
+	protected:
+		virtual bool IsChecked (const QVariant& idVar) const = 0;
+		virtual void SetChecked (const QVariant& idVar, bool checked) = 0;
 	signals:
 		void selectionChanged ();
 	};
@@ -26,52 +38,9 @@ namespace LC::Util
 	template<typename IdType>
 	class CheckableProxyModel : public CheckableProxyModelBase
 	{
-		const int IdRole_;
 		QSet<IdType> Unchecked_;
 	public:
-		explicit CheckableProxyModel (int idRole, QObject *parent = nullptr)
-		: CheckableProxyModelBase { parent }
-		, IdRole_ { idRole }
-		{
-		}
-
-		Qt::ItemFlags flags (const QModelIndex& index) const override
-		{
-			auto flags = QIdentityProxyModel::flags (index);
-			if (index.column () == 0)
-				flags |= Qt::ItemIsUserCheckable;
-			return flags;
-		}
-
-		QVariant data (const QModelIndex& index, int role) const override
-		{
-			if (role == Qt::CheckStateRole && index.column () == 0)
-			{
-				const bool isChecked = !Unchecked_.contains (index.data (IdRole_).value<IdType> ());
-				return isChecked ? Qt::Checked : Qt::Unchecked;
-			}
-
-			return QIdentityProxyModel::data (index, role);
-		}
-
-		bool setData (const QModelIndex& index, const QVariant& value, int role) override
-		{
-			if (role == Qt::CheckStateRole && index.column () == 0)
-			{
-				const auto id = index.data (IdRole_).value<IdType> ();
-				if (value.value<Qt::CheckState> () == Qt::Checked)
-					Unchecked_.remove (id);
-				else
-					Unchecked_ << id;
-
-				emit selectionChanged ();
-				emit dataChanged (index, index, { Qt::CheckStateRole });
-
-				return true;
-			}
-
-			return QIdentityProxyModel::setData (index, value, role);
-		}
+		using CheckableProxyModelBase::CheckableProxyModelBase;
 
 		QSet<IdType> GetUnchecked () const
 		{
@@ -94,42 +63,19 @@ namespace LC::Util
 
 			return result;
 		}
-
-		void CheckAll ()
+	protected:
+		bool IsChecked (const QVariant& idVar) const override
 		{
-			const auto unchecked = std::exchange (Unchecked_, {});
-
-			const auto rc = sourceModel ()->rowCount ();
-			for (int i = 0; i < rc; ++i)
-			{
-				const auto srcIndex = sourceModel ()->index (i, 0);
-				const auto rowId = srcIndex.data (IdRole_).template value<IdType> ();
-				if (unchecked.contains (rowId))
-				{
-					const auto& index = mapFromSource (srcIndex);
-					emit dataChanged (index, index, { Qt::CheckStateRole });
-				}
-			}
-
-			emit selectionChanged ();
+			return !Unchecked_.contains (idVar.value<IdType> ());
 		}
 
-		void CheckNone ()
+		void SetChecked (const QVariant& idVar, bool checked) override
 		{
-			const auto rc = sourceModel ()->rowCount ();
-			for (int i = 0; i < rc; ++i)
-			{
-				const auto srcIndex = sourceModel ()->index (i, 0);
-				const auto rowId = srcIndex.data (IdRole_).template value<IdType> ();
-				if (!Unchecked_.contains (rowId))
-				{
-					const auto& index = mapFromSource (srcIndex);
-					Unchecked_ << rowId;
-					emit dataChanged (index, index, { Qt::CheckStateRole });
-				}
-			}
-
-			emit selectionChanged ();
+			const auto id = idVar.value<IdType> ();
+			if (checked)
+				Unchecked_.remove (id);
+			else
+				Unchecked_ << id;
 		}
 	};
 }
