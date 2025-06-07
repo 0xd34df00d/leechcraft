a6fcd9982329055f93a3a46163b873f435a09c9f
0xd34df00d <0xd34df00d@gmail.com>
2025-06-07 00:07:28 -0500

    [Util] Qt 6.9 defines debug operator<< for tuples as well

--- src/util/db/tests/oraltest.cpp
+++ src/util/db/tests/oraltest.cpp
@@ -107,11 +107,13 @@ ORAL_ADAPT_STRUCT (ComplexConstraintsRecord,
 
 TOSTRING (ComplexConstraintsRecord)
 
+#if QT_VERSION < QT_VERSION_CHECK (6, 9, 0)
 template<typename... Args>
 QDebug operator<< (QDebug dbg, const std::tuple<Args...>& tup)
 {
 	return std::apply ([&] (auto&&... args) { return ((dbg.nospace () << args << ' '), ...); }, tup);
 }
+#endif
 
 namespace LC
 {
