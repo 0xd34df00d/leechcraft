676d04638856079267c1e1677e3d24be59f5d01a
0xd34df00d <0xd34df00d@gmail.com>
2025-08-17 09:37:34 -0500

    [Poshuku] No need for a sequence of `if`s

--- src/plugins/poshuku/historymodel.cpp
+++ src/plugins/poshuku/historymodel.cpp
@@ -47,11 +47,11 @@ namespace Poshuku
 			QDate orig = current.date ();
 			if (date.daysTo (current) == 0)
 				return 0;
-			else if (date.daysTo (current) == 1)
+			if (date.daysTo (current) == 1)
 				return 1;
-			else if (date.daysTo (current) == 2)
+			if (date.daysTo (current) == 2)
 				return 2;
-			else if (date.daysTo (current) <= 7)
+			if (date.daysTo (current) <= 7)
 				return 3;
 
 			int i = 0;
