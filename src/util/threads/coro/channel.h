923f3658ca52de16f7fa9f7ef7d24ac0ecb1c2c9
0xd34df00d <0xd34df00d@gmail.com>
2025-12-07 12:37:05 -0600

    [Util] Stub coro-friendly channel

---
+++ src/util/threads/coro/channel.h
@@ -0,0 +1,125 @@
+/**********************************************************************
+ * LeechCraft - modular cross-platform feature rich internet client.
+ * Copyright (C) 2006-2014  Georg Rudoy
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
+ **********************************************************************/
+
+#pragma once
+
+#include <atomic>
+#include <coroutine>
+#include <deque>
+#include <mutex>
+#include <optional>
+#include <stdexcept>
+#include <utility>
+
+namespace LC::Util
+{
+	template<typename T>
+	class Channel
+	{
+		struct PopAwaiter
+		{
+			Channel& Ch_;
+			std::optional<T> Slot_;
+			std::coroutine_handle<> Handle_;
+
+			explicit PopAwaiter (Channel& ch)
+			: Ch_ { ch }
+			{
+			}
+
+			~PopAwaiter ()
+			{
+				if (Handle_)
+				{
+					std::lock_guard guard { Ch_.Lock_ };
+					std::erase (Ch_.Awaiters_, this);
+				}
+			}
+
+			bool await_ready () const noexcept
+			{
+				return false;
+			}
+
+			bool await_suspend (std::coroutine_handle<> handle)
+			{
+				std::lock_guard guard { Ch_.Lock_ };
+				if (!Ch_.Elems_.empty ())
+				{
+					Slot_ = Ch_.Elems_.front ();
+					Ch_.Elems_.pop_front ();
+					return false;
+				}
+
+				if (Ch_.Closed_)
+					return false;
+
+				Ch_.Awaiters_.push_back (this);
+				Handle_ = handle;
+				return true;
+			}
+
+			std::optional<T> await_resume () noexcept
+			{
+				return std::move (Slot_);
+			}
+		};
+
+		mutable std::mutex Lock_;
+		std::deque<T> Elems_;
+		std::deque<PopAwaiter*> Awaiters_;
+
+		bool Closed_ = false;
+	public:
+		Channel () = default;
+
+		void Close ()
+		{
+			std::deque<PopAwaiter*> awaiters;
+
+			{
+				std::lock_guard guard { Lock_ };
+				Closed_ = true;
+				awaiters = std::exchange (Awaiters_, {});
+			}
+
+			for (auto awaiter : awaiters)
+				awaiter->Handle_ ();
+		}
+
+		template<typename U = T>
+		void Push (U&& value)
+		{
+			PopAwaiter *next = nullptr;
+			{
+				std::lock_guard guard { Lock_ };
+				if (Closed_)
+					throw std::runtime_error { "pushing into a closed channel" };
+
+				if (!Awaiters_.empty ())
+				{
+					next = Awaiters_.front ();
+					Awaiters_.pop_front ();
+				}
+				else
+					Elems_.emplace_back (std::forward<U> (value));
+			}
+
+			if (next)
+			{
+				next->Slot_.emplace (std::forward<U> (value));
+				next->Handle_ ();
+			}
+		}
+
+		PopAwaiter Pop ()
+		{
+			return PopAwaiter { *this };
+		}
+	};
+}
