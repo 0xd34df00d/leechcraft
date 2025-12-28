4655320c21048c09506983becf5b786dee218b3a
0xd34df00d <0xd34df00d@gmail.com>
2025-12-28 13:15:55 -0600

    [Vrooby] Updated translations

--- src/plugins/vrooby/leechcraft_vrooby_en.ts
+++ src/plugins/vrooby/leechcraft_vrooby_en.ts
@@ -2,47 +2,115 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
-    <name>LC::Vrooby::FlatMountableItems</name>
-    <message>
-        <location filename="flatmountableitems.cpp" line="69"/>
-        <source>total size: %1</source>
-        <translation>total size: %1</translation>
-    </message>
-    <message>
-        <location filename="flatmountableitems.cpp" line="75"/>
-        <source>available size: %1</source>
-        <translation>available size: %1</translation>
-    </message>
-    <message>
-        <location filename="flatmountableitems.cpp" line="87"/>
-        <source>Mounted at %1</source>
-        <translation>Mounted at %1</translation>
-    </message>
-</context>
-<context>
     <name>LC::Vrooby::Plugin</name>
     <message>
-        <location filename="vrooby.cpp" line="87"/>
+        <location filename="vrooby.cpp" line="65"/>
         <source>No backends are available, tried the following: %1.</source>
         <translation>No backends are available, tried the following: %1.</translation>
     </message>
     <message>
-        <location filename="vrooby.cpp" line="173"/>
+        <location filename="vrooby.cpp" line="149"/>
         <source>Removable devices...</source>
         <translation>Removable devices...</translation>
     </message>
     <message>
-        <location filename="vrooby.cpp" line="126"/>
+        <location filename="vrooby.cpp" line="103"/>
         <source>Removable storage devices manager for LeechCraft.</source>
         <translation>Removable storage devices manager for LeechCraft.</translation>
     </message>
 </context>
 <context>
+    <name>LC::Vrooby::TrayModel</name>
+    <message>
+        <location filename="trayview.cpp" line="70"/>
+        <source>total size: %1</source>
+        <translation>total size: %1</translation>
+    </message>
+    <message>
+        <location filename="trayview.cpp" line="75"/>
+        <source>available size: %1</source>
+        <translation>available size: %1</translation>
+    </message>
+    <message>
+        <location filename="trayview.cpp" line="86"/>
+        <source>Mounted at %1</source>
+        <translation>Mounted at %1</translation>
+    </message>
+</context>
+<context>
     <name>LC::Vrooby::TrayView</name>
     <message>
-        <location filename="trayview.cpp" line="156"/>
+        <location filename="trayview.cpp" line="199"/>
         <source>Removable devices</source>
         <translation>Removable devices</translation>
+    </message>
+</context>
+<context>
+    <name>LC::Vrooby::UDisks2::Backend</name>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="113"/>
+        <source>permission denied</source>
+        <translation>permission denied</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="114"/>
+        <source>not authorized</source>
+        <translation>not authorized</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="115"/>
+        <source>the device is busy</source>
+        <translation>the device is busy</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="116"/>
+        <source>the operation has failed</source>
+        <translation>the operation has failed</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="117"/>
+        <source>the operation has been cancelled</source>
+        <translation>the operation has been cancelled</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="118"/>
+        <source>invalid mount options were given</source>
+        <translation>invalid mount options were given</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="119"/>
+        <source>unsupported filesystem</source>
+        <translation>unsupported filesystem</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="121"/>
+        <source>unknown error</source>
+        <translation>unknown error</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="138"/>
+        <source>Device has been successfully mounted at %1.</source>
+        <translation>Device has been successfully mounted at %1.</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="145"/>
+        <source>Failed to mount the device: %1 (%2).</source>
+        <translation>Failed to mount the device: %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="158"/>
+        <source>Device has been successfully unmounted.</source>
+        <translation>Device has been successfully unmounted.</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="165"/>
+        <source>Failed to unmount the device: %1 (%2).</source>
+        <translation>Failed to unmount the device: %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="279"/>
+        <source>Partition %1</source>
+        <translation>Partition %1</translation>
     </message>
 </context>
 </TS>
--- src/plugins/vrooby/leechcraft_vrooby_ru_RU.ts
+++ src/plugins/vrooby/leechcraft_vrooby_ru_RU.ts
@@ -2,47 +2,115 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
-    <name>LC::Vrooby::FlatMountableItems</name>
-    <message>
-        <location filename="flatmountableitems.cpp" line="69"/>
-        <source>total size: %1</source>
-        <translation>общий размер: %1</translation>
-    </message>
-    <message>
-        <location filename="flatmountableitems.cpp" line="75"/>
-        <source>available size: %1</source>
-        <translation>доступно: %1</translation>
-    </message>
-    <message>
-        <location filename="flatmountableitems.cpp" line="87"/>
-        <source>Mounted at %1</source>
-        <translation>Смонтировано на %1</translation>
-    </message>
-</context>
-<context>
     <name>LC::Vrooby::Plugin</name>
     <message>
-        <location filename="vrooby.cpp" line="87"/>
+        <location filename="vrooby.cpp" line="65"/>
         <source>No backends are available, tried the following: %1.</source>
         <translation>Нет доступных платформ управления съёмными носителями, были испробованы: %1.</translation>
     </message>
     <message>
-        <location filename="vrooby.cpp" line="173"/>
+        <location filename="vrooby.cpp" line="149"/>
         <source>Removable devices...</source>
         <translation>Съемные накопители...</translation>
     </message>
     <message>
-        <location filename="vrooby.cpp" line="126"/>
+        <location filename="vrooby.cpp" line="103"/>
         <source>Removable storage devices manager for LeechCraft.</source>
         <translation>Менеджер съемных накопителей данных для LeechCraft.</translation>
     </message>
 </context>
 <context>
+    <name>LC::Vrooby::TrayModel</name>
+    <message>
+        <location filename="trayview.cpp" line="70"/>
+        <source>total size: %1</source>
+        <translation>общий размер: %1</translation>
+    </message>
+    <message>
+        <location filename="trayview.cpp" line="75"/>
+        <source>available size: %1</source>
+        <translation>доступно: %1</translation>
+    </message>
+    <message>
+        <location filename="trayview.cpp" line="86"/>
+        <source>Mounted at %1</source>
+        <translation>Смонтировано на %1</translation>
+    </message>
+</context>
+<context>
     <name>LC::Vrooby::TrayView</name>
     <message>
-        <location filename="trayview.cpp" line="156"/>
+        <location filename="trayview.cpp" line="199"/>
         <source>Removable devices</source>
         <translation>Съемные накопители</translation>
+    </message>
+</context>
+<context>
+    <name>LC::Vrooby::UDisks2::Backend</name>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="113"/>
+        <source>permission denied</source>
+        <translation>доступ запрещён</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="114"/>
+        <source>not authorized</source>
+        <translation>не авторизовано</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="115"/>
+        <source>the device is busy</source>
+        <translation>устройство занято</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="116"/>
+        <source>the operation has failed</source>
+        <translation>операция завершилась с ошибкой</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="117"/>
+        <source>the operation has been cancelled</source>
+        <translation>операция была отменена</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="118"/>
+        <source>invalid mount options were given</source>
+        <translation>некорректные опции монтирования</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="119"/>
+        <source>unsupported filesystem</source>
+        <translation>неподдерживаемая файловая система</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="121"/>
+        <source>unknown error</source>
+        <translation>неизвестная ошибка</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="138"/>
+        <source>Device has been successfully mounted at %1.</source>
+        <translation>Устройство успешно смонтировано на %1.</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="145"/>
+        <source>Failed to mount the device: %1 (%2).</source>
+        <translation>Не удалось смонтировать устройство: %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="158"/>
+        <source>Device has been successfully unmounted.</source>
+        <translation>Устройство успешно отмонтировано.</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="165"/>
+        <source>Failed to unmount the device: %1 (%2).</source>
+        <translation>Не удалось отмонтировать устройство: %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="279"/>
+        <source>Partition %1</source>
+        <translation>Раздел %1</translation>
     </message>
 </context>
 </TS>
