b04879f5cb565530a37d34f01eb6ed25061daebc
0xd34df00d <0xd34df00d@gmail.com>
2026-01-01 12:08:23 -0600

    [Util] Use C++26 structured bindings pack intro for `GetField`

--- src/util/models/flatitemsmodel.h
+++ src/util/models/flatitemsmodel.h
@@ -15,148 +15,23 @@ namespace LC::Util
 {
 	namespace detail
 	{
-		struct Any
+		template<typename T, typename... Ts>
+		QVariant GetFieldImpl (int idx, T& head, Ts&... tail)
 		{
-			template<typename T>
-			constexpr operator T ();
-		};
+			if (!idx)
+				return head;
 
-		template<int>
-		struct FC {};
-
-		template<typename T>
-		constexpr int GetFieldsCount ()
-		{
-			Any any;
-
-			if constexpr (requires { T { any, any, any, any, any, any }; })
-				return 6;
-			else if constexpr (requires { T { any, any, any, any, any }; })
-				return 5;
-			else if constexpr (requires { T { any, any, any, any }; })
-				return 4;
-			else if constexpr (requires { T { any, any, any }; })
-				return 3;
-			else if constexpr (requires { T { any, any }; })
-				return 2;
-			else if constexpr (requires { T { any }; })
-				return 1;
+			if constexpr (sizeof... (Ts))
+				return GetFieldImpl (idx - 1, tail...);
 			else
-				static_assert (std::is_same_v<T, struct Dummy>, "Don't know how to handle this type");
-		}
-
-		template<typename T>
-		QVariant GetFieldImpl (const T& item, int idx, FC<1>)
-		{
-			auto [a0] = item;
-			switch (idx)
-			{
-			case 0:
-				return a0;
-			default:
-				return {};
-			}
-		}
-
-		template<typename T>
-		QVariant GetFieldImpl (const T& item, int idx, FC<2>)
-		{
-			auto [a0, a1] = item;
-			switch (idx)
-			{
-			case 0:
-				return a0;
-			case 1:
-				return a1;
-			default:
-				return {};
-			}
-		}
-
-		template<typename T>
-		QVariant GetFieldImpl (const T& item, int idx, FC<3>)
-		{
-			auto [a0, a1, a2] = item;
-			switch (idx)
-			{
-			case 0:
-				return a0;
-			case 1:
-				return a1;
-			case 2:
-				return a2;
-			default:
-				return {};
-			}
-		}
-
-		template<typename T>
-		QVariant GetFieldImpl (const T& item, int idx, FC<4>)
-		{
-			auto [a0, a1, a2, a3] = item;
-			switch (idx)
-			{
-			case 0:
-				return a0;
-			case 1:
-				return a1;
-			case 2:
-				return a2;
-			case 3:
-				return a3;
-			default:
-				return {};
-			}
-		}
-
-		template<typename T>
-		QVariant GetFieldImpl (const T& item, int idx, FC<5>)
-		{
-			auto [a0, a1, a2, a3, a4] = item;
-			switch (idx)
-			{
-			case 0:
-				return a0;
-			case 1:
-				return a1;
-			case 2:
-				return a2;
-			case 3:
-				return a3;
-			case 4:
-				return a4;
-			default:
-				return {};
-			}
-		}
-
-		template<typename T>
-		QVariant GetFieldImpl (const T& item, int idx, FC<6>)
-		{
-			auto [a0, a1, a2, a3, a4, a5] = item;
-			switch (idx)
-			{
-			case 0:
-				return a0;
-			case 1:
-				return a1;
-			case 2:
-				return a2;
-			case 3:
-				return a3;
-			case 4:
-				return a4;
-			case 5:
-				return a5;
-			default:
-				return {};
-			}
+				throw std::runtime_error { "out of bounds tuple-like access" };
 		}
 
 		template<typename T>
 		QVariant GetField (const T& item, int idx)
 		{
-			return GetFieldImpl (item, idx, FC<GetFieldsCount<T> ()> {});
+			auto&& [...elems] = item;
+			return GetFieldImpl (idx, elems...);
 		}
 	}
 
