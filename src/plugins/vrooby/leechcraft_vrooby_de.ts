b3084cde8b0d0a1a03f9d9f1e1e305a52446d23a
0xd34df00d <0xd34df00d@gmail.com>
2026-01-05 19:15:51 -0600

    [Vrooby] Added German translation

---
+++ src/plugins/vrooby/leechcraft_vrooby_de.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>LC::Vrooby::Plugin</name>
+    <message>
+        <location filename="vrooby.cpp" line="65"/>
+        <source>No backends are available, tried the following: %1.</source>
+        <translation>Keine Backends sind verfügbar, die folgenden wurden versucht: %1.</translation>
+    </message>
+    <message>
+        <location filename="vrooby.cpp" line="103"/>
+        <source>Removable storage devices manager for LeechCraft.</source>
+        <translation>Manager für Wechseldatenträger für LeechCraft.</translation>
+    </message>
+    <message>
+        <location filename="vrooby.cpp" line="149"/>
+        <source>Removable devices...</source>
+        <translation>Wechseldatenträger...</translation>
+    </message>
+</context>
+<context>
+    <name>LC::Vrooby::TrayProxyModel</name>
+    <message>
+        <location filename="trayproxymodel.cpp" line="49"/>
+        <source>total size: %1</source>
+        <translation>Gesamtgröße: %1</translation>
+    </message>
+    <message>
+        <location filename="trayproxymodel.cpp" line="54"/>
+        <source>available size: %1</source>
+        <translation>Verfügbare Größe: %1</translation>
+    </message>
+    <message>
+        <location filename="trayproxymodel.cpp" line="65"/>
+        <source>Mounted at %1</source>
+        <translation>Eingehängt unter %1</translation>
+    </message>
+</context>
+<context>
+    <name>LC::Vrooby::TrayView</name>
+    <message>
+        <location filename="trayview.cpp" line="44"/>
+        <source>Removable devices</source>
+        <translation>Wechseldatenträger</translation>
+    </message>
+</context>
+<context>
+    <name>LC::Vrooby::UDisks2::Backend</name>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="104"/>
+        <source>permission denied</source>
+        <translation>Zugriff verweigert</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="105"/>
+        <source>not authorized</source>
+        <translation>nicht autorisiert</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="106"/>
+        <source>the device is busy</source>
+        <translation>Gerät belegt</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="107"/>
+        <source>the operation has failed</source>
+        <translation>Vorgang fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="108"/>
+        <source>the operation has been cancelled</source>
+        <translation>Vorgang abgebrochen</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="109"/>
+        <source>invalid mount options were given</source>
+        <translation>ungültige Einhängeoptionen angegeben</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="110"/>
+        <source>unsupported filesystem</source>
+        <translation>nicht unterstütztes Dateisystem</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="112"/>
+        <source>unknown error</source>
+        <translation>unbekannter Fehler</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="129"/>
+        <source>Device has been successfully mounted at %1.</source>
+        <translation>Das Gerät wurde erfolgreich unter %1 eingehängt.</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="136"/>
+        <source>Failed to mount the device: %1 (%2).</source>
+        <translation>Fehler beim Einhängen des Geräts: %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="149"/>
+        <source>Device has been successfully unmounted.</source>
+        <translation>Das Gerät wurde erfolgreich ausgehängt.</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="156"/>
+        <source>Failed to unmount the device: %1 (%2).</source>
+        <translation>Fehler beim Aushängen des Geräts: %1 (%2).</translation>
+    </message>
+    <message>
+        <location filename="backends/udisks2/udisks2backend.cpp" line="272"/>
+        <source>Partition %1</source>
+        <translation>Partition %1</translation>
+    </message>
+</context>
+</TS>
