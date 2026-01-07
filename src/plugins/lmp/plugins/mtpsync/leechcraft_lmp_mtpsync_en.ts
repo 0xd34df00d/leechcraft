196bf39a8440764a8607dbda1173ccd3aadaa4fa
0xd34df00d <0xd34df00d@gmail.com>
2026-01-06 19:54:59 -0600

    [LMP MTPSync] Add translations

---
+++ src/plugins/lmp/plugins/mtpsync/leechcraft_lmp_mtpsync_en.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>LC::LMP::MTPSync::Mtp</name>
+    <message>
+        <location filename="mtp.cpp" line="147"/>
+        <source>Unable to open device %1.</source>
+        <oldsource>Unknown device %1.</oldsource>
+        <translation>Unable to open device %1.</translation>
+    </message>
+    <message>
+        <location filename="mtp.cpp" line="154"/>
+        <source>Unable to open storage %1 on %2.</source>
+        <oldsource>Unknown storage %1 on %2.</oldsource>
+        <translation>Unable to open storage %1 on %2.</translation>
+    </message>
+    <message>
+        <location filename="mtp.cpp" line="167"/>
+        <source>Error writing track: %1.</source>
+        <translation>Error writing track: %1.</translation>
+    </message>
+    <message>
+        <location filename="mtp.cpp" line="174"/>
+        <source>Error creating track album.</source>
+        <translation>Error creating track album.</translation>
+    </message>
+    <message>
+        <location filename="mtp.cpp" line="180"/>
+        <source>Error appending track to the album.</source>
+        <translation>Error appending track to the album.</translation>
+    </message>
+</context>
+<context>
+    <name>LC::LMP::MTPSync::Plugin</name>
+    <message>
+        <location filename="mtpsync.cpp" line="113"/>
+        <source>Music collection synchronization with MTP-enabled portable media players.</source>
+        <oldsource>Adds support for synchronization with MTP-enabled portable media players.</oldsource>
+        <translation>Music collection synchronization with MTP-enabled portable media players.</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/lmp/plugins/mtpsync/leechcraft_lmp_mtpsync_ru_RU.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>LC::LMP::MTPSync::Mtp</name>
+    <message>
+        <location filename="mtp.cpp" line="147"/>
+        <source>Unable to open device %1.</source>
+        <oldsource>Unknown device %1.</oldsource>
+        <translation>Невозможно открыть устройство %1.</translation>
+    </message>
+    <message>
+        <location filename="mtp.cpp" line="154"/>
+        <source>Unable to open storage %1 on %2.</source>
+        <oldsource>Unknown storage %1 on %2.</oldsource>
+        <translation>Невозможно открыть хранилище %1 на %2.</translation>
+    </message>
+    <message>
+        <location filename="mtp.cpp" line="167"/>
+        <source>Error writing track: %1.</source>
+        <translation>Ошибка записи трека: %1.</translation>
+    </message>
+    <message>
+        <location filename="mtp.cpp" line="174"/>
+        <source>Error creating track album.</source>
+        <translation>Ошибка создания альбома для трека.</translation>
+    </message>
+    <message>
+        <location filename="mtp.cpp" line="180"/>
+        <source>Error appending track to the album.</source>
+        <translation>Ошибка добавления трека в альбом.</translation>
+    </message>
+</context>
+<context>
+    <name>LC::LMP::MTPSync::Plugin</name>
+    <message>
+        <location filename="mtpsync.cpp" line="113"/>
+        <source>Music collection synchronization with MTP-enabled portable media players.</source>
+        <oldsource>Adds support for synchronization with MTP-enabled portable media players.</oldsource>
+        <translation>Поддержка синхронизации с поддерживающими MTP портативными аудиоплеерами.</translation>
+    </message>
+</context>
+</TS>
