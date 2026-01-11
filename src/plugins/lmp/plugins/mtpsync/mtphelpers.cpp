1bea3595cfb36b4ac0c77aceb19da12d03d82266
0xd34df00d <0xd34df00d@gmail.com>
2026-01-11 09:40:11 -0600

    [LMP MTPSync] Set max capacity to, well, max capacity

--- src/plugins/lmp/plugins/mtpsync/mtphelpers.cpp
+++ src/plugins/lmp/plugins/mtpsync/mtphelpers.cpp
@@ -66,7 +66,7 @@ namespace LC::LMP::MTPSync::Helpers
 			{
 				storage->id,
 				QString::fromUtf8 (storage->StorageDescription),
-				storage->FreeSpaceInBytes,
+				storage->MaxCapacity,
 			};
 			result << part;
 			storage = storage->next;
