0xd34df00d/leechcraft

commit 58a05bbcce23fbb2fc3b58a5a0c860d908bff21d

[Azoth Xoox] Proper ref-qualification for the form builder

src/plugins/azoth/plugins/xoox/formbuilder.cpp
@@ -65,7 +65,7 @@ namespace Xoox
 			Widget2Field_.clear ();
 		}
 	protected:
-		virtual QWidget* CreateWidgetImpl (QXmppDataForm::Field&, QFormLayout*) = 0;
+		virtual QWidget* CreateWidgetImpl (const QXmppDataForm::Field&, QFormLayout*) = 0;
 		virtual QVariant GetData (QWidget*) = 0;
 	};
 
@@ -119,7 +119,7 @@ namespace Xoox
 	class BooleanHandler : public TypedFieldHandler<QCheckBox>
 	{
 	protected:
-		QWidget* CreateWidgetImpl (QXmppDataForm::Field& field, QFormLayout *layout) override
+		QWidget* CreateWidgetImpl (const QXmppDataForm::Field& field, QFormLayout *layout) override
 		{
 			auto box = new QCheckBox (field.label ());
 			box->setChecked (field.value ().toBool ());
@@ -136,7 +136,7 @@ namespace Xoox
 	class FixedHandler : public FieldHandler
 	{
 	protected:
-		QWidget* CreateWidgetImpl (QXmppDataForm::Field& field, QFormLayout *layout) override
+		QWidget* CreateWidgetImpl (const QXmppDataForm::Field& field, QFormLayout *layout) override
 		{
 			auto label = new QLabel (field.value ().toString ());
 			layout->addRow (field.label (), label);
@@ -152,7 +152,7 @@ namespace Xoox
 	class NullHandler : public FieldHandler
 	{
 	protected:
-		QWidget* CreateWidgetImpl (QXmppDataForm::Field&, QFormLayout*) override
+		QWidget* CreateWidgetImpl (const QXmppDataForm::Field&, QFormLayout*) override
 		{
 			return nullptr;
 		}
@@ -166,7 +166,7 @@ namespace Xoox
 	class MultiTextHandler : public TypedFieldHandler<QTextEdit>
 	{
 	protected:
-		QWidget* CreateWidgetImpl (QXmppDataForm::Field& field, QFormLayout *layout) override
+		QWidget* CreateWidgetImpl (const QXmppDataForm::Field& field, QFormLayout *layout) override
 		{
 			auto edit = new QTextEdit;
 			edit->setAcceptRichText (false);
@@ -192,7 +192,7 @@ namespace Xoox
 		{
 		}
 	protected:
-		QWidget* CreateWidgetImpl (QXmppDataForm::Field& field, QFormLayout *layout) override
+		QWidget* CreateWidgetImpl (const QXmppDataForm::Field& field, QFormLayout *layout) override
 		{
 			auto edit = new QLineEdit (field.value ().toString ());
 			if (IsPassword_)
@@ -219,7 +219,7 @@ namespace Xoox
 		{
 		}
 	protected:
-		QWidget* CreateWidgetImpl (QXmppDataForm::Field& field, QFormLayout *layout) override
+		QWidget* CreateWidgetImpl (const QXmppDataForm::Field& field, QFormLayout *layout) override
 		{
 			auto tree = new QTreeWidget ();
 			tree->setSelectionMode (SelMode_);

commit b538ce91940fdf2a54d145d367bebd36b665764a

[Azoth Xoox] Copy the fields over to avoid dangling references
Fixes the regression introduced in
58a05bbcce23fbb2fc3b58a5a0c860d908bff21d and
47d80085b8a98105b02b64653c6f6eff2d9f6b4a.

src/plugins/azoth/plugins/xoox/formbuilder.cpp
@@ -29,11 +29,11 @@ namespace Xoox
 {
 	class FieldHandler
 	{
-		QMap<QWidget*, QXmppDataForm::Field*> Widget2Field_;
+		QMap<QWidget*, QXmppDataForm::Field> Widget2Field_;
 	public:
 		virtual ~FieldHandler () = default;
 
-		void CreateWidget (QXmppDataForm::Field& field, QFormLayout *layout)
+		void CreateWidget (const QXmppDataForm::Field& field, QFormLayout *layout)
 		{
 			QWidget *w = CreateWidgetImpl (field, layout);
 			if (!w)
@@ -47,22 +47,27 @@ namespace Xoox
 
 			w->setProperty ("Azoth/Xoox/IsRequired", field.isRequired ());
 
-			Widget2Field_ [w] = &field;
-		}
-
-		void Save ()
-		{
-			for (const auto& [widget, field] : Util::Stlize (Widget2Field_))
-			{
-				if (const auto& var = GetData (widget); !var.isNull ())
-					field->setValue (var);
-			}
+			Widget2Field_ [w] = field;
 		}
 
 		void Clear ()
 		{
 			qDeleteAll (Widget2Field_.keys ());
 			Widget2Field_.clear ();
+		}
+
+		QHash<QString, QXmppDataForm::Field> GetFields ()
+		{
+			QHash<QString, QXmppDataForm::Field> result;
+			for (const auto& [widget, field] : Widget2Field_.asKeyValueRange ())
+				if (const auto& var = GetData (widget);
+					!var.isNull ())
+				{
+					auto& updatedField = result [field.key ()];
+					updatedField = field;
+					updatedField.setValue (var);
+				}
+			return result;
 		}
 	protected:
 		virtual QWidget* CreateWidgetImpl (const QXmppDataForm::Field&, QFormLayout*) = 0;
@@ -323,11 +328,21 @@ namespace Xoox
 		return widget;
 	}
 
-	QXmppDataForm FormBuilder::GetForm ()
+	QXmppDataForm FormBuilder::GetForm () const
 	{
+		QHash<QString, QXmppDataForm::Field> updatedFields;
 		for (const auto& handler : Type2Handler_)
-			handler->Save ();
-		return Form_;
+			updatedFields.insert (handler->GetFields ());
+
+		auto fields = std::as_const (Form_).fields ();
+		for (auto& field : fields)
+			if (const auto it = updatedFields.find (field.key ());
+				it != updatedFields.end ())
+				field = *it;
+
+		auto result = Form_;
+		result.setFields (fields);
+		return result;
 	}
 
 	namespace

src/plugins/azoth/plugins/xoox/formbuilder.h
@@ -41,7 +41,7 @@ namespace Xoox
 		XMPPBobManager* BobManager () const;
 
 		QWidget* CreateForm (const QXmppDataForm&, QWidget* = 0);
-		QXmppDataForm GetForm ();
+		QXmppDataForm GetForm () const;
 
 		QString GetSavedUsername () const;
 		QString GetSavedPass () const;