1d69655ec97ac4052f4587ee0786f892364fcf23
0xd34df00d <0xd34df00d@gmail.com>
2026-09-06 01:11:36 -0500

    [LastFMScrobble] Updated translations

--- src/plugins/lastfmscrobble/leechcraft_lastfmscrobble_en.ts
+++ src/plugins/lastfmscrobble/leechcraft_lastfmscrobble_en.ts
@@ -1,119 +1,74 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>LC::Lastfmscrobble::Authenticator</name>
     <message>
-        <location filename="authenticator.cpp" line="101"/>
-        <source>Enter password for Last.fm account with login %1:</source>
-        <translation>Enter password for Last.fm account with login %1:</translation>
-    </message>
-</context>
-<context>
-    <name>LC::Lastfmscrobble::LastFmRadioStation</name>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="47"/>
-        <source>Library</source>
-        <translation>Library</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="48"/>
-        <source>Recommendations</source>
-        <translation>Recommendations</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="49"/>
-        <source>Loved tracks</source>
-        <translation>Loved tracks</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="50"/>
-        <source>Neighbourhood</source>
-        <translation>Neighbourhood</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="63"/>
-        <source>Similar to &quot;%1&quot; radio</source>
-        <translation>Similar to &quot;%1&quot; radio</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="71"/>
-        <source>Tag &quot;%1&quot; radio</source>
-        <translation>Tag &quot;%1&quot; radio</translation>
+        <location filename="authenticator.cpp" line="81"/>
+        <source>Enter password for Last.fm account %1:</source>
+        <translation>Enter password for Last.fm account %1:</translation>
     </message>
 </context>
 <context>
     <name>LC::Lastfmscrobble::Plugin</name>
     <message>
-        <location filename="lastfmscrobble.cpp" line="89"/>
-        <source>Library</source>
-        <translation>Library</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="90"/>
-        <source>Recommendations</source>
-        <translation>Recommendations</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="91"/>
-        <source>Loved</source>
-        <translation>Loved</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="92"/>
-        <source>Neighbourhood</source>
-        <translation>Neighbourhood</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="94"/>
-        <source>Similar artists</source>
-        <translation>Similar artists</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="97"/>
-        <source>Global tag</source>
-        <translation>Global tag</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="122"/>
+        <location filename="lastfmscrobble.cpp" line="67"/>
         <source>Client for the Last.FM social music service.</source>
         <translation>Client for the Last.FM social music service.</translation>
+    </message>
+    <message>
+        <location filename="lastfmscrobble.cpp" line="176"/>
+        <source>Unsupported hype type</source>
+        <translation>Unsupported hype type</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="albumartfetcher.cpp" line="33"/>
+        <source>Unable to parse reply.</source>
+        <translation>Unable to parse reply.</translation>
     </message>
 </context>
 <context>
     <name>lastfmscrobblesettings</name>
     <message>
-        <location filename="dummy.cpp" line="2"/>
+        <location filename="dummy.cpp" line="5"/>
         <source>Settings</source>
         <translation>Settings</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="3"/>
+        <location filename="dummy.cpp" line="7"/>
         <source>Last.FM</source>
         <translation>Last.FM</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="4"/>
+        <location filename="dummy.cpp" line="9"/>
         <source>Login:</source>
         <translation>Login:</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="5"/>
+        <location filename="dummy.cpp" line="11"/>
         <source>Behavior</source>
         <translation>Behavior</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="6"/>
+        <location filename="dummy.cpp" line="13"/>
         <source>Language to fetch info in:</source>
         <translation>Language to fetch info in:</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="8"/>
+        <location filename="dummy.cpp" line="15"/>
+        <source>The two-letter ISO code of the language (like &quot;en&quot; or &quot;ru&quot;), or empty to use system default language.</source>
+        <translation>The two-letter ISO code of the language (like &quot;en&quot; or &quot;ru&quot;), or empty to use system default language.</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="17"/>
         <source>Use HTTP GET instead of HTTP POST</source>
         <translation>Use HTTP GET instead of HTTP POST</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="9"/>
+        <location filename="dummy.cpp" line="19"/>
         <source>This may help if Last.FM service is experiencing issues.</source>
         <translation>This may help if Last.FM service is experiencing issues.</translation>
     </message>
--- src/plugins/lastfmscrobble/leechcraft_lastfmscrobble_ru_RU.ts
+++ src/plugins/lastfmscrobble/leechcraft_lastfmscrobble_ru_RU.ts
@@ -1,119 +1,74 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>LC::Lastfmscrobble::Authenticator</name>
     <message>
-        <location filename="authenticator.cpp" line="101"/>
-        <source>Enter password for Last.fm account with login %1:</source>
-        <translation>Введите пароль для учётной записи Last.fm с именем пользователя %1:</translation>
-    </message>
-</context>
-<context>
-    <name>LC::Lastfmscrobble::LastFmRadioStation</name>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="47"/>
-        <source>Library</source>
-        <translation>Библиотека</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="48"/>
-        <source>Recommendations</source>
-        <translation>Рекомендации</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="49"/>
-        <source>Loved tracks</source>
-        <translation>Избранные треки</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="50"/>
-        <source>Neighbourhood</source>
-        <translation>Соседи</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="63"/>
-        <source>Similar to &quot;%1&quot; radio</source>
-        <translation>Радио похожих на «%1»</translation>
-    </message>
-    <message>
-        <location filename="lastfmradiostation.cpp" line="71"/>
-        <source>Tag &quot;%1&quot; radio</source>
-        <translation>Радио метки «%1»</translation>
+        <location filename="authenticator.cpp" line="81"/>
+        <source>Enter password for Last.fm account %1:</source>
+        <translation>Введите пароль для учётной записи Last.fm %1:</translation>
     </message>
 </context>
 <context>
     <name>LC::Lastfmscrobble::Plugin</name>
     <message>
-        <location filename="lastfmscrobble.cpp" line="89"/>
-        <source>Library</source>
-        <translation>Библиотека</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="90"/>
-        <source>Recommendations</source>
-        <translation>Рекомендации</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="91"/>
-        <source>Loved</source>
-        <translation>Избранное</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="92"/>
-        <source>Neighbourhood</source>
-        <translation>Соседи</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="94"/>
-        <source>Similar artists</source>
-        <translation>Похожие исполнители</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="97"/>
-        <source>Global tag</source>
-        <translation>Глобальная метка</translation>
-    </message>
-    <message>
-        <location filename="lastfmscrobble.cpp" line="122"/>
+        <location filename="lastfmscrobble.cpp" line="67"/>
         <source>Client for the Last.FM social music service.</source>
         <translation>Клиент для социального музыкального сервиса Last.FM.</translation>
+    </message>
+    <message>
+        <location filename="lastfmscrobble.cpp" line="176"/>
+        <source>Unsupported hype type</source>
+        <translation>Неподдерживаемый тип запроса</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="albumartfetcher.cpp" line="33"/>
+        <source>Unable to parse reply.</source>
+        <translation>Невозможно разобрать ответ.</translation>
     </message>
 </context>
 <context>
     <name>lastfmscrobblesettings</name>
     <message>
-        <location filename="dummy.cpp" line="2"/>
+        <location filename="dummy.cpp" line="5"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="3"/>
+        <location filename="dummy.cpp" line="7"/>
         <source>Last.FM</source>
         <translation>Last.FM</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="4"/>
+        <location filename="dummy.cpp" line="9"/>
         <source>Login:</source>
         <translation>Имя пользователя:</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="5"/>
+        <location filename="dummy.cpp" line="11"/>
         <source>Behavior</source>
         <translation>Поведение</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="6"/>
+        <location filename="dummy.cpp" line="13"/>
         <source>Language to fetch info in:</source>
         <translation>Язык получения информации:</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="8"/>
+        <location filename="dummy.cpp" line="15"/>
+        <source>The two-letter ISO code of the language (like &quot;en&quot; or &quot;ru&quot;), or empty to use system default language.</source>
+        <translation>Двухбуквенный ISO-код языка (вроде «en» или «ru»), или пустая строка для использования системного языка.</translation>
+    </message>
+    <message>
+        <location filename="dummy.cpp" line="17"/>
         <source>Use HTTP GET instead of HTTP POST</source>
         <translation>Использовать HTTP GET вместо HTTP POST</translation>
     </message>
     <message>
-        <location filename="dummy.cpp" line="9"/>
+        <location filename="dummy.cpp" line="19"/>
         <source>This may help if Last.FM service is experiencing issues.</source>
         <translation>Это может помочь в случае проблем с сервисами Last.FM.</translation>
     </message>
