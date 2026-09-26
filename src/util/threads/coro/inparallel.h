7cb9aa2f023082e1703d42796a21b1920a3ffde4
0xd34df00d <0xd34df00d@gmail.com>
2026-09-26 00:05:59 -0500

    [Util] `std::forward` inside a loop makes no sense

--- src/util/threads/coro/inparallel.h
+++ src/util/threads/coro/inparallel.h
@@ -55,7 +55,7 @@ namespace LC::Util
 	{
 		QVector<Task> tasks;
 		for (auto&& input : inputs)
-			tasks << std::invoke (mkTask, std::move (input), std::forward<MkTaskArgs> (mkTaskArgs)...);
+			tasks << std::invoke (mkTask, std::move (input), mkTaskArgs...);
 
 		if constexpr (IsVoid)
 			for (const auto& task : tasks)
